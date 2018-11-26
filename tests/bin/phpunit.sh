d116d2f1e1bd4bb36f4bb06a3b9647d58a11660d
Kelly Dwan <ryelle@users.noreply.github.com>
2018-11-26 14:18:15 -0500

    Fix typo in plugin name

--- tests/bin/phpunit.sh
+++ tests/bin/phpunit.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 WORKING_DIR="$PWD"
-cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-gutenberg-products-admin/"
+cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-gutenberg-products-block/"
 phpunit --version
 phpunit -c phpunit.xml.dist
 TEST_RESULT=$?
