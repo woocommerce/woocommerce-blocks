277580e469e25222f4b57fd100e4551b4cb65e17
Claudio Sanches <contato@claudiosanches.com>
2018-11-27 19:18:32 -0200

    Product attributes controller

--- woocommerce-gutenberg-products-block.php
+++ woocommerce-gutenberg-products-block.php
@@ -159,12 +159,16 @@ add_action( 'admin_print_footer_scripts', 'wgpb_print_script_settings', 1 );
 function wgpb_register_api_routes() {
 	include_once dirname( __FILE__ ) . '/includes/class-wgpb-products-controller.php';
 	include_once dirname( __FILE__ ) . '/includes/class-wgpb-product-categories-controller.php';
+	include_once dirname( __FILE__ ) . '/includes/class-wgpb-product-attributes-controller.php';
 
 	$products = new WGPB_Products_Controller();
 	$products->register_routes();
 
 	$categories = new WGPB_Product_Categories_Controller();
 	$categories->register_routes();
+
+	$attributes = new WGPB_Product_Attributes_Controller();
+	$attributes->register_routes();
 }
 
 /**
