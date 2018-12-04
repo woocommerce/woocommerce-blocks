1329927d840dd0385ba6b7b818fd4193bc3fa8d1
Kelly Dwan <ryelle@users.noreply.github.com>
2018-12-03 19:13:11 -0500

    Remove redundant setState call

--- assets/js/components/product-category-control/index.js
+++ assets/js/components/product-category-control/index.js
@@ -27,7 +27,6 @@ class ProductCategoryControl extends Component {
 	}
 
 	componentDidMount() {
-		this.setState( { loading: true } );
 		apiFetch( {
 			path: addQueryArgs( '/wc-pb/v3/products/categories', { per_page: -1 } ),
 		} )
