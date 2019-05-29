bb994a8ad81572edb08f98dde95b481efab57492
Mike Jolley <mike.jolley@me.com>
2019-05-29 13:46:08 +0100

    Variation search + counts showing

--- assets/js/components/product-control/index.js
+++ assets/js/components/product-control/index.js
@@ -1,54 +1,219 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, _n, sprintf } from '@wordpress/i18n';
 import { Component, Fragment } from '@wordpress/element';
-import { debounce, find } from 'lodash';
+import { addQueryArgs } from '@wordpress/url';
+import apiFetch from '@wordpress/api-fetch';
+import { debounce, find, escapeRegExp } from 'lodash';
 import PropTypes from 'prop-types';
-import ProductSearchListControl from '../../components/product-search-list-control';
+import {
+	SearchListControl,
+	SearchListItem,
+} from '@woocommerce/components';
+import { Spinner, MenuItem } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import { isLargeCatalog, getProducts } from '../utils';
+import {
+	IconRadioSelected,
+	IconRadioUnselected,
+} from '../icons';
+
+function getHighlightedName( name, search ) {
+	if ( ! search ) {
+		return name;
+	}
+	const re = new RegExp( escapeRegExp( search ), 'ig' );
+	return name.replace( re, '<strong>$&</strong>' );
+}
+
+const getInteractionIcon = ( isSelected = false ) => {
+	return isSelected ? <IconRadioSelected /> : <IconRadioUnselected />;
+};
 
 class ProductControl extends Component {
 	constructor() {
 		super( ...arguments );
 		this.state = {
-			list: [],
+			products: [],
+			product: 0,
+			variationsList: {},
+			variationsLoading: true,
 			loading: true,
 		};
 
 		this.debouncedOnSearch = debounce( this.onSearch.bind( this ), 400 );
+		this.debouncedGetVariations = debounce( this.getVariations.bind( this ), 200 );
+		this.onSelectProduct = this.onSelectProduct.bind( this );
+		this.renderItem = this.renderItem.bind( this );
 	}
 
 	componentDidMount() {
 		const { selected } = this.props;
 
 		getProducts( { selected } )
-			.then( ( list ) => {
-				this.setState( { list, loading: false } );
+			.then( ( products ) => {
+				products = products.map( ( product ) => {
+					const count = product.variations ? product.variations.length : 0;
+					return {
+						...product,
+						parent: 0,
+						count: count,
+					};
+				} );
+				this.setState( { products, loading: false } );
 			} )
 			.catch( () => {
-				this.setState( { list: [], loading: false } );
+				this.setState( { products: [], loading: false } );
 			} );
+	}
+
+	componentDidUpdate( prevProps, prevState ) {
+		if ( prevState.product !== this.state.product ) {
+			this.debouncedGetVariations();
+		}
+	}
+
+	getVariations() {
+		const { product, variationsList } = this.state;
+		if ( ! product ) {
+			return;
+		}
+		if ( ! variationsList[ product ] ) {
+			this.setState( { variationsLoading: true } );
+		}
+
+		apiFetch( {
+			// Use v4 which returns variation names.
+			path: addQueryArgs( `/wc/v4/products/${ product }/variations`, {
+				per_page: -1,
+			} ),
+		} )
+			.then( ( variations ) => {
+				variations = variations.map( ( variation ) => ( { ...variation, parent: product } ) );
+				this.setState( ( prevState ) => ( {
+					variationsList: { ...prevState.variationsList, [ product ]: variations },
+					variationsLoading: false,
+				} ) );
+			} )
+			.catch( () => {
+				this.setState( { termsLoading: false } );
+			} );
+	}
+
+	onSelectProduct( item ) {
+		return () => {
+			this.props.onChange( [] );
+			this.setState( {
+				product: item.id === this.state.product ? 0 : item.id,
+			} );
+		};
 	}
 
 	onSearch( search ) {
 		const { selected } = this.props;
 		getProducts( { selected, search } )
-			.then( ( list ) => {
-				this.setState( { list, loading: false } );
+			.then( ( products ) => {
+				this.setState( { products, loading: false } );
 			} )
 			.catch( () => {
-				this.setState( { list: [], loading: false } );
+				this.setState( { products: [], loading: false } );
 			} );
 	}
 
+	renderItem( args ) {
+		const { item, search, depth = 0 } = args;
+		const { product, variationsLoading } = this.state;
+		const classes = [
+			'woocommerce-search-product__item',
+			'woocommerce-search-list__item',
+		];
+		classes.push( `depth-${ depth }` );
+		classes.push( 'is-radio-button' );
+
+		if ( search.length ) {
+			classes.push( 'is-searching' );
+		}
+		if ( depth === 0 && item.parent !== 0 ) {
+			classes.push( 'is-skip-level' );
+		}
+
+		if ( ! item.breadcrumbs.length ) {
+			return [
+				<MenuItem
+					key={ `product-${ item.id }` }
+					{ ...args }
+					className={ classes.join( ' ' ) }
+					isSelected
+					onClick={ this.onSelectProduct( item ) }
+					role={ 'menuitemradio' }
+					aria-label={ sprintf(
+						_n(
+							'%s, has %d variation',
+							'%s, has %d variations',
+							item.count,
+							'woo-gutenberg-products-block'
+						),
+						item.name,
+						item.count
+					) }
+				>
+					<span className="woocommerce-search-list__item-state">
+						{ getInteractionIcon( product === item.id ) }
+					</span>
+
+					<span className="woocommerce-search-list__item-label">
+						<span
+							className="woocommerce-search-list__item-name"
+							dangerouslySetInnerHTML={ {
+								__html: getHighlightedName( item.name, search ),
+							} }
+						/>
+					</span>
+
+					<span className="woocommerce-search-list__item-count">
+						{ sprintf(
+							_n(
+								'%d variation',
+								'%d variations',
+								item.count,
+								'woo-gutenberg-products-block'
+							),
+							item.count
+						) }
+					</span>
+				</MenuItem>,
+				product === item.id && item.count > 0 && variationsLoading && (
+					<div
+						key="loading"
+						className={
+							'woocommerce-search-list__item woocommerce-search-product__item' +
+							'depth-1 is-loading is-not-active'
+						}
+					>
+						<Spinner />
+					</div>
+				),
+			];
+		}
+
+		return (
+			<SearchListItem
+				className={ classes.join( ' ' ) }
+				{ ...args }
+				aria-label={ `${ item.breadcrumbs[ 0 ] }: ${ item.name }` }
+			/>
+		);
+	}
+
 	render() {
-		const { list, loading } = this.state;
+		const { products, loading, product, variationsList } = this.state;
 		const { onChange, selected } = this.props;
+		const currentVariations = variationsList[ product ] || [];
+		const currentList = [ ...products, ...currentVariations ];
 		const messages = {
 			list: __( 'Products', 'woo-gutenberg-products-block' ),
 			noItems: __(
@@ -65,18 +230,20 @@ class ProductControl extends Component {
 			),
 		};
 
-		// Note: selected prop still needs to be array for SearchListControl.
 		return (
 			<Fragment>
-				<ProductSearchListControl
+				<SearchListControl
 					className="woocommerce-products"
-					list={ list }
+					list={ currentList }
 					isLoading={ loading }
 					isSingle
-					selected={ [ find( list, { id: selected } ) ] }
+					selected={ [ find( currentList, { id: selected } ) ] }
 					onChange={ onChange }
 					onSearch={ isLargeCatalog ? this.debouncedOnSearch : null }
+					onSelect={ this.onSelectProduct }
 					messages={ messages }
+					renderItem={ this.renderItem }
+					isHierarchical
 				/>
 			</Fragment>
 		);
