93bd57ab08b41498ca7397f0bb49f7221f318276
Kelly Dwan <ryelle@users.noreply.github.com>
2019-06-11 17:31:46 -0400

    Rewrite component into a function

--- assets/js/blocks/product-categories/block.js
+++ assets/js/blocks/product-categories/block.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Component } from '@wordpress/element';
 import { noop } from 'lodash';
 import { SelectControl } from '@wordpress/components';
 import PropTypes from 'prop-types';
@@ -12,60 +11,54 @@ import PropTypes from 'prop-types';
  */
 import { buildTermsTree } from './hierarchy';
 
-/**
- * Component to handle edit mode of "On Sale Products".
- */
-class ProductCategoriesBlock extends Component {
-	getCategories() {
-		const { hasEmpty, isDropdown, isHierarchical } = this.props.attributes;
-		const categories = wc_product_block_data.productCategories.filter(
-			( cat ) => hasEmpty || !! cat.count
-		);
-		return ! isDropdown && isHierarchical ? buildTermsTree( categories ) : categories;
-	}
-
-	renderList( items ) {
-		const { hasCount } = this.props.attributes;
-		const parentKey = 'parent-' + items[ 0 ].term_id;
-		return (
-			<ul key={ parentKey }>
-				{ items.map( ( cat ) => {
-					const count = hasCount ? <span>({ cat.count })</span> : null;
-					return [
-						<li key={ cat.term_id }><a>{ cat.name }</a> { count }</li>, // eslint-disable-line
-						!! cat.children &&
-							!! cat.children.length &&
-							this.renderList( cat.children ),
-					];
-				} ) }
-			</ul>
-		);
-	}
-
-	renderDropdown( items ) {
-		const { hasCount } = this.props.attributes;
-		return (
-			<SelectControl
-				label={ __( 'Select a category', 'woo-gutenberg-products-block' ) }
-				options={ items.map( ( cat ) => ( {
-					label: hasCount ? `${ cat.name } (${ cat.count })` : cat.name,
-					value: cat.term_id,
-				} ) ) }
-				onChange={ noop }
-			/>
-		);
-	}
-
-	render() {
-		const { isDropdown } = this.props.attributes;
-		const categories = this.getCategories();
-		return (
-			<div className="wc-block-product-categories">
-				{ isDropdown ? this.renderDropdown( categories ) : this.renderList( categories ) }
-			</div>
-		);
-	}
+function getCategories( { hasEmpty, isDropdown, isHierarchical } ) {
+	const categories = wc_product_block_data.productCategories.filter(
+		( cat ) => hasEmpty || !! cat.count
+	);
+	return ! isDropdown && isHierarchical ?
+		buildTermsTree( categories ) :
+		categories;
 }
+
+/**
+ * Component displaying the categories as dropdown or list.
+ */
+const ProductCategoriesBlock = ( { attributes } ) => {
+	const { hasCount, isDropdown } = attributes;
+	const categories = getCategories( attributes );
+	const parentKey = 'parent-' + categories[ 0 ].term_id;
+
+	const renderList = ( items ) => (
+		<ul key={ parentKey }>
+			{ items.map( ( cat ) => {
+				const count = hasCount ? <span>({ cat.count })</span> : null;
+				return [
+					<li key={ cat.term_id }><a>{ cat.name }</a> { count }</li>, // eslint-disable-line
+					!! cat.children &&
+						!! cat.children.length &&
+						renderList( cat.children ),
+				];
+			} ) }
+		</ul>
+	);
+
+	return (
+		<div className="wc-block-product-categories">
+			{ isDropdown ? (
+				<SelectControl
+					label={ __( 'Select a category', 'woo-gutenberg-products-block' ) }
+					options={ categories.map( ( cat ) => ( {
+						label: hasCount ? `${ cat.name } (${ cat.count })` : cat.name,
+						value: cat.term_id,
+					} ) ) }
+					onChange={ noop }
+				/>
+			) : (
+				renderList( categories )
+			) }
+		</div>
+	);
+};
 
 ProductCategoriesBlock.propTypes = {
 	/**
