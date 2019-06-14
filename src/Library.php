0fa68e2bef60a94d8c93dd3649b7577d15710b9a
Mike Jolley <mike.jolley@me.com>
2019-06-14 22:46:15 +0100

    Add library code to new class

---
+++ src/Library.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Initializes blocks in WordPress.
+ *
+ * @package WooCommerce/Blocks
+ */
+
+namespace WooCommerce\Blocks;
+
+defined( 'ABSPATH' ) || exit;
+
+use WooCommerce\Blocks\Utilities\SingletonTrait;
+
+/**
+ * Library class.
+ */
+class Library {
+	use SingletonTrait;
+
+	/**
+	 * Initialize block library features.
+	 */
+	public function init() {
+		add_action( 'init', array( $this, 'register_blocks' ) );
+
+		Assets::instance()->init();
+		RestApi::instance()->init();
+	}
+
+	/**
+	 * Register blocks, hooking up assets and render functions as needed.
+	 */
+	public function register_blocks() {
+		$blocks = [
+			'HandpickedProducts',
+			'ProductBestSellers',
+			'ProductCategory',
+			'ProductNew',
+			'ProductOnSale',
+			'ProductTopRated',
+			'ProductsByAttribute',
+			'FeaturedProduct',
+		];
+
+		foreach ( $blocks as $block_class ) {
+			$class = __NAMESPACE__ . '\\BlockTypes\\' . $block_class;
+			$block = new $class();
+			$block->register_block_type();
+		}
+	}
+}
