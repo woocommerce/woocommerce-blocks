38192751ca2925e238115aa6bc03c24b56500538
Mike Jolley <mike.jolley@me.com>
2019-06-27 12:38:46 +0100

    AbstractBlock class for general block registration

---
+++ src/BlockTypes/AbstractBlock.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Abstract block class.
+ *
+ * @package WooCommerce/Blocks
+ */
+
+namespace Automattic\WooCommerce\Blocks\BlockTypes;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * AbstractBlock class.
+ */
+abstract class AbstractBlock {
+
+	/**
+	 * Block namespace.
+	 *
+	 * @var string
+	 */
+	protected $namespace = 'woocommerce';
+
+	/**
+	 * Block namespace.
+	 *
+	 * @var string
+	 */
+	protected $block_name = '';
+
+	/**
+	 * Registers the block type with WordPress.
+	 */
+	public function register_block_type() {
+		register_block_type(
+			$this->namespace . '/' . $this->block_name,
+			array(
+				'render_callback' => array( $this, 'render' ),
+				'editor_script'   => 'wc-' . $this->block_name,
+				'editor_style'    => 'wc-block-editor',
+				'style'           => 'wc-block-style',
+				'attributes'      => $this->get_attributes(),
+			)
+		);
+	}
+
+	/**
+	 * Include and render the dynamic block.
+	 *
+	 * @param array  $attributes Block attributes. Default empty array.
+	 * @param string $content    Block content. Default empty string.
+	 * @return string Rendered block type output.
+	 */
+	abstract public function render( $attributes = array(), $content = '' );
+
+	/**
+	 * Get block attributes.
+	 *
+	 * @return array
+	 */
+	protected function get_attributes() {
+		return array();
+	}
+}
