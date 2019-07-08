ce35168b6bf814b5fab6ffcaacae9919583b607f
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-07-08 12:28:18 +0200

    Add dangerouslySetInnerHTML explanatory comment

--- assets/js/blocks/reviews-by-product/utils.js
+++ assets/js/blocks/reviews-by-product/utils.js
@@ -30,6 +30,9 @@ export function renderReview( attributes, review = {} ) {
 			<span
 				className="wc-block-reviews-by-product__text"
 				dangerouslySetInnerHTML={ {
+					// `text` is the `review` parameter returned by the `reviews` endpoint.
+					// It's filtered with `wp_filter_post_kses()`, which removes dangerous HTML tags,
+					// so using it inside `dangerouslySetInnerHTML` is safe.
 					__html: text || '',
 				} }
 			/>
