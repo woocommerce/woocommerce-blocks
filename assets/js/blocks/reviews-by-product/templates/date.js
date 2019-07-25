941ad8e920a708fa9459961a7cfbb28942118d75
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-07-25 15:33:54 +0200

    Remove old todo comment

--- assets/js/blocks/reviews-by-product/templates/date.js
+++ assets/js/blocks/reviews-by-product/templates/date.js
@@ -3,7 +3,7 @@
  */
 import { format } from '@wordpress/date';
 
-const dateTemplate = ( { date } ) => { // @todo export it to it's own file
+const dateTemplate = ( { date } ) => {
 	return (
 		<time className="wc-block-reviews-by-product__published-date" dateTime={ date }>{ format( 'F j, Y', date ) }</time>
 	);
