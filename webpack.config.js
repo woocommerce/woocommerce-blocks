b65f43a2d545b1a389e71f99dde8fed3af4ca2b5
Mike Jolley <mike.jolley@me.com>
2019-07-26 16:07:46 +0100

    Babel config to skip wp.element

--- webpack.config.js
+++ webpack.config.js
@@ -101,7 +101,12 @@ const GutenbergBlocksConfig = {
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader?cacheDirectory',
+				use: {
+					loader: 'babel-loader?cacheDirectory',
+					options: {
+						presets: [ '@wordpress/default' ],
+					},
+				},
 			},
 			{
 				test: /\.s[c|a]ss$/,
@@ -152,7 +157,25 @@ const BlocksFrontendConfig = {
 			{
 				test: /\.jsx?$/,
 				exclude: /node_modules/,
-				loader: 'babel-loader?cacheDirectory',
+				use: {
+					loader: 'babel-loader?cacheDirectory',
+					options: {
+						presets: [
+							[ '@babel/preset-env', {
+								modules: false,
+								targets: {
+									browsers: [ 'extends @wordpress/browserslist-config' ],
+								},
+							} ],
+						],
+						plugins: [
+							'@babel/plugin-transform-react-jsx',
+							'@babel/plugin-transform-runtime',
+							'transform-object-rest-spread',
+							'transform-async-generator-functions',
+						],
+					},
+				},
 			},
 		],
 	},
