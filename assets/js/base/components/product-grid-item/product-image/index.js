24b0f9c1b8fab07b95f2007c4a4d58892e440197
Mike Jolley <mike.jolley@me.com>
2019-08-23 14:48:53 +0100

    Image handling

--- assets/js/base/components/product-grid-item/product-image/index.js
+++ assets/js/base/components/product-grid-item/product-image/index.js
@@ -3,12 +3,41 @@
  */
 import PropTypes from 'prop-types';
 
+/**
+ * Internal dependencies
+ */
+import { PLACEHOLDER_IMG_SRC, THUMBNAIL_SIZE } from '../../../../constants';
+
 const ProductImage = ( { className, product = {} } ) => {
+	let image = null;
+
+	if ( product.images.length ) {
+		const mainImage = product.images[ 0 ];
+		image = (
+			<img
+				className={ className + '__image' }
+				src={ mainImage.thumbnail }
+				srcSet={ mainImage.srcset }
+				sizes={ mainImage.sizes }
+				alt={ mainImage.alt }
+				style={ { width: `${ THUMBNAIL_SIZE }px` } }
+			/>
+		);
+	} else {
+		image = (
+			<img
+				className={ className + '__image' }
+				src={ PLACEHOLDER_IMG_SRC }
+				alt=""
+				style={ { width: `${ THUMBNAIL_SIZE }px` } }
+			/>
+		);
+	}
+
 	return (
-		<div
-			className={ className }
-			dangerouslySetInnerHTML={ { __html: product.thumbnail_html } }
-		/>
+		<div className={ className }>
+			{ image }
+		</div>
 	);
 };
 
