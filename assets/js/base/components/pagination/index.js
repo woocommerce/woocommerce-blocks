0295db0d9c895299d56c3ada810546f932e79e4d
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-09-03 13:53:31 +0200

    Refactor pagination so it behaves like core pagination

--- assets/js/base/components/pagination/index.js
+++ assets/js/base/components/pagination/index.js
@@ -12,14 +12,18 @@ import Label from '../label';
 import { getIndexes } from './utils.js';
 import './style.scss';
 
-const Pagination = ( { currentPage, pagesToDisplay, onPageChange, totalPages } ) => {
+const Pagination = ( { currentPage, displayFirstAndLastPages, displayNextAndPreviousArrows, pagesToDisplay, onPageChange, totalPages } ) => {
 	const { minIndex, maxIndex } = getIndexes( pagesToDisplay, currentPage, totalPages );
 	const pages = [];
 	for ( let i = minIndex; i <= maxIndex; i++ ) {
 		pages.push( i );
 	}
-	const showPreviousArrow = minIndex !== 1;
-	const showNextArrow = maxIndex !== totalPages;
+	const showFirstPage = displayFirstAndLastPages && Boolean( minIndex !== 1 );
+	const showLastPage = displayFirstAndLastPages && Boolean( maxIndex !== totalPages );
+	const showFirstPageEllipsis = displayFirstAndLastPages && Boolean( minIndex > 2 );
+	const showLastPageEllipsis = displayFirstAndLastPages && Boolean( maxIndex < totalPages - 1 );
+	const showPreviousArrow = displayNextAndPreviousArrows && Boolean( currentPage !== 1 );
+	const showNextArrow = displayNextAndPreviousArrows && Boolean( currentPage !== totalPages );
 
 	return (
 		<div className="wc-block-pagination">
@@ -29,14 +33,27 @@ const Pagination = ( { currentPage, pagesToDisplay, onPageChange, totalPages } )
 			{ showPreviousArrow && (
 				<button
 					className="wc-block-pagination-page"
-					onClick={ () => onPageChange( 1 ) }
-					title={ __( 'First page', 'woo-gutenberg-products-block' ) }
+					onClick={ () => onPageChange( currentPage - 1 ) }
+					title={ __( 'Previous page', 'woo-gutenberg-products-block' ) }
 				>
 					<Label
 						label="<"
-						screenReaderLabel={ __( 'First page', 'woo-gutenberg-products-block' ) }
+						screenReaderLabel={ __( 'Previous page', 'woo-gutenberg-products-block' ) }
 					/>
 				</button>
+			) }
+			{ showFirstPage && (
+				<button
+					className="wc-block-pagination-page"
+					onClick={ () => onPageChange( 1 ) }
+				>
+					1
+				</button>
+			) }
+			{ showFirstPageEllipsis && (
+				<span className="wc-block-pagination-ellipsis" aria-hidden="true">
+					{ __( '…', 'woo-gutenberg-products-block' ) }
+				</span>
 			) }
 			{ pages.map( ( page ) => {
 				return (
@@ -51,15 +68,28 @@ const Pagination = ( { currentPage, pagesToDisplay, onPageChange, totalPages } )
 					</button>
 				);
 			} ) }
-			{ showNextArrow && (
+			{ showLastPageEllipsis && (
+				<span className="wc-block-pagination-ellipsis" aria-hidden="true">
+					{ __( '…', 'woo-gutenberg-products-block' ) }
+				</span>
+			) }
+			{ showLastPage && (
 				<button
 					className="wc-block-pagination-page"
 					onClick={ () => onPageChange( totalPages ) }
-					title={ __( 'Last page', 'woo-gutenberg-products-block' ) }
+				>
+					{ totalPages }
+				</button>
+			) }
+			{ showNextArrow && (
+				<button
+					className="wc-block-pagination-page"
+					onClick={ () => onPageChange( currentPage + 1 ) }
+					title={ __( 'Next page', 'woo-gutenberg-products-block' ) }
 				>
 					<Label
 						label=">"
-						screenReaderLabel={ __( 'Last page', 'woo-gutenberg-products-block' ) }
+						screenReaderLabel={ __( 'Next page', 'woo-gutenberg-products-block' ) }
 					/>
 				</button>
 			) }
@@ -77,17 +107,28 @@ Pagination.propTypes = {
 	 */
 	totalPages: PropTypes.number.isRequired,
 	/**
+	 * Displays first and last pages if they are not in the current range of pages displayed.
+	 */
+	displayFirstAndLastPages: PropTypes.bool,
+	/**
+	 * Displays arrows to navigate to the previous and next pages.
+	 */
+	displayNextAndPreviousArrows: PropTypes.bool,
+	/**
 	 * Callback function called when the user triggers a page change.
 	 */
 	onPageChange: PropTypes.func,
 	/**
 	 * Number of pages to display at the same time, including the active page
-	 * and the pages displayed before and after it.
+	 * and the pages displayed before and after it. It doesn't include the first
+	 * and last pages.
 	 */
 	pagesToDisplay: PropTypes.number,
 };
 
 Pagination.defaultProps = {
+	displayFirstAndLastPages: true,
+	displayNextAndPreviousArrows: true,
 	pagesToDisplay: 3,
 };
 
