aa5f996bf2b84faebddb255acac31ac411d1602c
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-09-03 13:00:11 +0200

    Update product order select options to match Shop core page

--- assets/js/base/components/product-order-select/index.js
+++ assets/js/base/components/product-order-select/index.js
@@ -14,12 +14,15 @@ const ProductOrderSelect = ( { defaultValue, onChange, readOnly, value } ) => {
 		<OrderSelect
 			className="wc-block-product-order-select"
 			defaultValue={ defaultValue }
+			name="orderby"
 			onChange={ onChange }
 			options={ [
-				{ key: 'highest-rating', label: __( 'Highest rating', 'woo-gutenberg-products-block' ) },
-				{ key: 'alphabetical', label: __( 'Alphabetical', 'woo-gutenberg-products-block' ) },
-				{ key: 'highest-price', label: __( 'Highest price', 'woo-gutenberg-products-block' ) },
-				{ key: 'lowest-price', label: __( 'Lowest price', 'woo-gutenberg-products-block' ) },
+				{ key: 'menu_order', label: __( 'Default sorting', 'woo-gutenberg-products-block' ) },
+				{ key: 'popularity', label: __( 'Popularity', 'woo-gutenberg-products-block' ) },
+				{ key: 'rating', label: __( 'Average rating', 'woo-gutenberg-products-block' ) },
+				{ key: 'date', label: __( 'Latest', 'woo-gutenberg-products-block' ) },
+				{ key: 'price', label: __( 'Price: low to high', 'woo-gutenberg-products-block' ) },
+				{ key: 'price-desc', label: __( 'Price: high to low', 'woo-gutenberg-products-block' ) },
 			] }
 			readOnly={ readOnly }
 			screenReaderLabel={ __( 'Order products by', 'woo-gutenberg-products-block' ) }
@@ -29,10 +32,10 @@ const ProductOrderSelect = ( { defaultValue, onChange, readOnly, value } ) => {
 };
 
 ProductOrderSelect.propTypes = {
-	defaultValue: PropTypes.oneOf( [ 'highest-rating', 'alphabetical', 'highest-price', 'lowest-price' ] ),
+	defaultValue: PropTypes.oneOf( [ 'menu_order', 'popularity', 'rating', 'date', 'price', 'price-desc' ] ),
 	onChange: PropTypes.func,
 	readOnly: PropTypes.bool,
-	value: PropTypes.oneOf( [ 'highest-rating', 'alphabetical', 'highest-price', 'lowest-price' ] ),
+	value: PropTypes.oneOf( [ 'menu_order', 'popularity', 'rating', 'date', 'price', 'price-desc' ] ),
 };
 
 export default ProductOrderSelect;
