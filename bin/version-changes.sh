ab313becdd290795301b29292986da110f6cc91e
Darren Ethier <darren@roughsmootheng.in>
2019-09-06 12:19:19 -0400

    switch to perl expression

--- bin/version-changes.sh
+++ bin/version-changes.sh
@@ -1,40 +1,20 @@
 #!/bin/sh
 VERSION=${VERSION:=\$VID\:\$}
 IS_PRE_RELEASE=${IS_PRE_RELEASE:=false}
-OS=`uname`
 
-if [ $OS = "Darwin" ]; then
-	# replace all instances of $VID:$ with the release version but only when not pre-release.
-	if [ $IS_PRE_RELEASE = false ]; then
-		find ./src -name "*.php" -print0 | xargs -0 sed -i '' 's/\$VID:\$/'${VERSION}'/g'
-	fi
-
-	# Update version number in readme.txt
-	sed -i '' -E 's/Stable tag:*.+/Stable tag: '${VERSION}'/' readme.txt
-
-	# Update version in main plugin file
-	sed -i '' -E 's/Version:*.+/Version: '${VERSION}'/' woocommerce-gutenberg-products-block.php
-
-	# Update version in package.json
-	sed -i '' -E 's/"version":*.+/"version": "'${VERSION}'",/' package.json
-
-	# Update version in main file
-	sed -i '' -E "s/VERSION =*.+/VERSION = '${VERSION}';/" src/Package.php
-else
-	# replace all instances of $VID:$ with the release version but only when not pre-release.
-	if [ $IS_PRE_RELEASE = false ]; then
-		find ./src -name "*.php" -print0 | xargs -0 sed -i 's/\$VID:\$/'${VERSION}'/g'
-	fi
-
-	# Update version number in readme.txt
-	sed -i -E 's/Stable tag:*.+/Stable tag: '${VERSION}'/' readme.txt
-
-	# Update version in main plugin file
-	sed -i -E 's/Version:*.+/Version: '${VERSION}'/' woocommerce-gutenberg-products-block.php
-
-	# Update version in package.json
-	sed -i -E 's/"version":*.+/"version": "'${VERSION}'",/' package.json
-
-	# Update version in main file
-	sed -i -E "s/VERSION =*.+/VERSION = '${VERSION}';/" src/Package.php
+# replace all instances of $VID:$ with the release version but only when not pre-release.
+if [ $IS_PRE_RELEASE = false ]; then
+	find ./src -name "*.php" -print0 | xargs -0 perl -i -pe 's/\$VID:\$/'${VERSION}'/g'
 fi
+
+# Update version number in readme.txt
+perl -i -pe 's/Stable tag:*.+/Stable tag: '${VERSION}'/' readme.txt
+
+# Update version in main plugin file
+perl -i -pe 's/Version:*.+/Version: '${VERSION}'/' woocommerce-gutenberg-products-block.php
+
+# Update version in package.json
+perl -i -pe 's/"version":*.+/"version": "'${VERSION}'",/' package.json
+
+# Update version in main file
+perl -i -pe "s/VERSION =*.+/VERSION = '${VERSION}';/" src/Package.php
