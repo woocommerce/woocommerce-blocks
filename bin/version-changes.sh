5c818171b1bc80b256f52e0f6d990ca06089bef2
Darren Ethier <darren@roughsmootheng.in>
2019-09-06 11:34:27 -0400

    conditionally use correct sed for environment.

--- bin/version-changes.sh
+++ bin/version-changes.sh
@@ -1,20 +1,40 @@
 #!/bin/sh
 VERSION=${VERSION:=\$VID\:\$}
 IS_PRE_RELEASE=${IS_PRE_RELEASE:=false}
+OS=`uname`
 
-# replace all instances of $VID:$ with the release version but only when not pre-release.
-if [ $IS_PRE_RELEASE = false ]; then
-	find ./src -name "*.php" -print0 | xargs -0 sed -i '' 's/\$VID:\$/'${VERSION}'/g'
+if [ $OS = "Darwin" ]; then
+	# replace all instances of $VID:$ with the release version but only when not pre-release.
+	if [ $IS_PRE_RELEASE = false ]; then
+		find ./src -name "*.php" -print0 | xargs -0 sed -i '' 's/\$VID:\$/'${VERSION}'/g'
+	fi
+
+	# Update version number in readme.txt
+	sed -i '' -E 's/Stable tag:*.+/Stable tag: '${VERSION}'/' readme.txt
+
+	# Update version in main plugin file
+	sed -i '' -E 's/Version:*.+/Version: '${VERSION}'/' woocommerce-gutenberg-products-block.php
+
+	# Update version in package.json
+	sed -i '' -E 's/"version":*.+/"version": "'${VERSION}'",/' package.json
+
+	# Update version in main file
+	sed -i '' -E "s/VERSION =*.+/VERSION = '${VERSION}';/" src/Package.php
+else
+	# replace all instances of $VID:$ with the release version but only when not pre-release.
+	if [ $IS_PRE_RELEASE = false ]; then
+		find ./src -name "*.php" -print0 | xargs -0 sed -i 's/\$VID:\$/'${VERSION}'/g'
+	fi
+
+	# Update version number in readme.txt
+	sed -i -E 's/Stable tag:*.+/Stable tag: '${VERSION}'/' readme.txt
+
+	# Update version in main plugin file
+	sed -i -E 's/Version:*.+/Version: '${VERSION}'/' woocommerce-gutenberg-products-block.php
+
+	# Update version in package.json
+	sed -i -E 's/"version":*.+/"version": "'${VERSION}'",/' package.json
+
+	# Update version in main file
+	sed -i -E "s/VERSION =*.+/VERSION = '${VERSION}';/" src/Package.php
 fi
-
-# Update version number in readme.txt
-sed -i '' -E 's/Stable tag:*.+/Stable tag: '${VERSION}'/' readme.txt
-
-# Update version in main plugin file
-sed -i '' -E 's/Version:*.+/Version: '${VERSION}'/' woocommerce-gutenberg-products-block.php
-
-# Update version in package.json
-sed -i '' -E 's/"version":*.+/"version": "'${VERSION}'",/' package.json
-
-# Update version in main file
-sed -i '' -E "s/VERSION =*.+/VERSION = '${VERSION}';/" src/Package.php
