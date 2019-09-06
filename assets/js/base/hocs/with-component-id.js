0754fd0c8bbe68e203bdf81ea9b0e6523dc318cb
Mike Jolley <mike.jolley@me.com>
2019-09-06 15:48:41 +0100

    Update withComponetId to stop component ID incrementing too many times
    Co-Authored-By: Darren Ethier <darren@roughsmootheng.in>

--- assets/js/base/hocs/with-component-id.js
+++ assets/js/base/hocs/with-component-id.js
@@ -1,35 +1,27 @@
 import { Component } from 'react';
 
-const ids = [];
-
 /**
  * HOC that gives a component a unique ID.
  *
  * This is an alternative for withInstanceId from @wordpress/compose to avoid using that dependency on the frontend.
  */
 const withComponentId = ( OriginalComponent ) => {
-	return class WrappedComponent extends Component {
-		generateUniqueID() {
-			const group = WrappedComponent.name;
-
-			if ( ! ids[ group ] ) {
-				ids[ group ] = 0;
-			}
-
-			ids[ group ]++;
-
-			return ids[ group ];
+	let instances = 0;
+	class WrappedComponent extends Component {
+		constructor() {
+			super( ...arguments );
+			this.instanceId = instances++;
 		}
 
 		render() {
-			const componentId = this.generateUniqueID();
-
 			return <OriginalComponent
 				{ ...this.props }
-				componentId={ componentId }
+				componentId={ this.instanceId }
 			/>;
 		}
-	};
+	}
+	WrappedComponent.displayName = 'withComponentId';
+	return WrappedComponent;
 };
 
 export default withComponentId;
