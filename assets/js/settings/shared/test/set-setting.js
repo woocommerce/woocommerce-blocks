5568daeb549771360880205e92fb74c5b46d13bc
Darren Ethier <darren@roughsmootheng.in>
2019-09-20 11:19:04 -0400

    improve test assertion

--- assets/js/settings/shared/test/set-setting.js
+++ assets/js/settings/shared/test/set-setting.js
@@ -5,17 +5,13 @@ import { setSetting } from '../set-setting';
 import { getSetting } from '../get-setting';
 
 describe( 'setSetting', () => {
-	it(
-		'should add a new value to the settings state for value not present',
-		() => {
-			setSetting( 'aSetting', 42 );
-			expect( getSetting( 'aSetting' ) ).toBe( 42 );
-		}
-	);
+	it( 'should add a new value to the settings state for value not present', () => {
+		setSetting( 'aSetting', 42 );
+		expect( getSetting( 'aSetting' ) ).toBe( 42 );
+	} );
 	it( 'should replace existing value', () => {
-		const original = getSetting( 'adminUrl' );
 		setSetting( 'adminUrl', 'not original' );
-		expect( getSetting( 'adminUrl' ) ).not.toBe( original );
+		expect( getSetting( 'adminUrl' ) ).toBe( 'not original' );
 	} );
 	it( 'should save the value run through the provided validator', () => {
 		setSetting( 'aSetting', 'who', () => 42 );
