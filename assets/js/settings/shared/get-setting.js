64434c2f4e6f5da694f8dd0ab56ab927a8b62044
Darren Ethier <darren@roughsmootheng.in>
2019-09-20 11:17:35 -0400

    improve jsdoc spacing

--- assets/js/settings/shared/get-setting.js
+++ assets/js/settings/shared/get-setting.js
@@ -7,20 +7,19 @@ import { allSettings } from './settings-init';
  * Retrieves a setting value from the setting state.
  *
  * @export
- * @param {string} name                               The identifier for the
- *                                                    setting.
- * @param {mixed}  [fallback=false]                   The value to use as a
- *                                                    fallback if the setting is
- *                                                    not in the state.
- * @param {function} [validation=( value ) => value]  A callback to use for
- *                                                    validating or sanitizing
- *                                                    the setting value.
+ * @param {string}   name                         The identifier for the setting.
+ * @param {mixed}    [fallback=false]             The value to use as a fallback
+ *                                                if the setting is not in the
+ *                                                state.
+ * @param {function} [validation=( val ) => val]  A callback to use for
+ *                                                validating or sanitizing the
+ *                                                setting value.
  * @returns {mixed}
  */
 export function getSetting(
 	name,
 	fallback = false,
-	validation = ( value ) => value
+	validation = ( val ) => val
 ) {
 	const value = allSettings.hasOwnProperty( name )
 		? allSettings[ name ]
