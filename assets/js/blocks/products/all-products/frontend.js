194062495900eb324638ff45aacfca1eaaf8ac71
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-09-30 11:43:20 +0200

    Use renderFrontend util in All Products block

--- assets/js/blocks/products/all-products/frontend.js
+++ assets/js/blocks/products/all-products/frontend.js
@@ -1,28 +1,12 @@
 /**
- * External dependencies
- */
-import { render } from 'react-dom';
-
-/**
  * Internal dependencies
  */
 import Block from './block';
+import renderFrontend from '../../../utils/render-frontend.js';
 
-const containers = document.querySelectorAll(
-	'.wp-block-woocommerce-all-products'
-);
+const getProps = ( el, i ) => ( {
+	attributes: JSON.parse( el.dataset.attributes ),
+	urlParameterSuffix: i > 0 ? `_${ i + 1 }` : '',
+} );
 
-if ( containers.length ) {
-	// Use Array.forEach for IE11 compatibility
-	Array.prototype.forEach.call( containers, ( el, i ) => {
-		const attributes = JSON.parse( el.dataset.attributes );
-
-		render(
-			<Block
-				attributes={ attributes }
-				urlParameterSuffix={ i > 0 ? `_${ i + 1 }` : '' }
-			/>,
-			el
-		);
-	} );
-}
+renderFrontend( '.wp-block-woocommerce-all-products', Block, getProps );
