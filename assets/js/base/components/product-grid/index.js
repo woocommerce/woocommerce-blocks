5f2022b81e194f04f3b654895170514304fce2e6
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-10-01 15:49:29 +0200

    Scroll to top when switching pages in All Pages block

--- assets/js/base/components/product-grid/index.js
+++ assets/js/base/components/product-grid/index.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { Component, createRef } from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
@@ -13,64 +14,100 @@ import ProductGridItem from '../product-grid-item';
 import withProducts from '../../hocs/with-products';
 import './style.scss';
 
-const ProductGrid = ( {
-	attributes,
-	currentPage,
-	onOrderChange,
-	onPageChange,
-	orderValue,
-	products,
-	totalProducts,
-} ) => {
-	const getClassnames = () => {
-		const { columns, rows, className, alignButtons, align } = attributes;
-		const alignClass = typeof align !== 'undefined' ? 'align' + align : '';
+class ProductGrid extends Component {
+	constructor() {
+		super();
 
-		return classnames(
-			'wc-block-grid',
-			className,
-			alignClass,
-			'has-' + columns + '-columns',
-			{
-				'has-multiple-rows': rows > 1,
-				'has-aligned-buttons': alignButtons,
-			}
-		);
-	};
-
-	const perPage = attributes.columns * attributes.rows;
-	const totalPages = Math.ceil( totalProducts / perPage );
-	if ( ! products.length ) {
-		products = Array.from( { length: perPage } );
+		this.scrollPointRef = createRef();
 	}
 
-	return (
-		<div className={ getClassnames() }>
-			{ attributes.showOrderby && (
-				<ProductOrderSelect
-					onChange={ onOrderChange }
-					value={ orderValue }
+	onPaginationChange = ( newPage ) => {
+		const { onPageChange } = this.props;
+
+		const srollPointRefYPosition = this.scrollPointRef.current.getBoundingClientRect()
+			.bottom;
+		const isScrollPointRefVisible =
+			srollPointRefYPosition >= 0 &&
+			srollPointRefYPosition <= window.innerHeight;
+		if ( ! isScrollPointRefVisible ) {
+			this.scrollPointRef.current.scrollIntoView();
+		}
+
+		onPageChange( newPage );
+	};
+
+	render() {
+		const {
+			attributes,
+			currentPage,
+			onOrderChange,
+			orderValue,
+			products,
+			totalProducts,
+		} = this.props;
+
+		const getClassnames = () => {
+			const {
+				columns,
+				rows,
+				className,
+				alignButtons,
+				align,
+			} = attributes;
+			const alignClass =
+				typeof align !== 'undefined' ? 'align' + align : '';
+
+			return classnames(
+				'wc-block-grid',
+				className,
+				alignClass,
+				'has-' + columns + '-columns',
+				{
+					'has-multiple-rows': rows > 1,
+					'has-aligned-buttons': alignButtons,
+				}
+			);
+		};
+
+		const perPage = attributes.columns * attributes.rows;
+		const totalPages = Math.ceil( totalProducts / perPage );
+		const gridProducts = products.length
+			? products
+			: Array.from( { length: perPage } );
+
+		return (
+			<div className={ getClassnames() }>
+				<div
+					className="wc-block-grid__products__scroll-point"
+					ref={ this.scrollPointRef }
+					aria-hidden
 				/>
-			) }
-			<ul className="wc-block-grid__products">
-				{ products.map( ( product = {}, i ) => (
-					<ProductGridItem
-						key={ product.id || i }
-						attributes={ attributes }
-						product={ product }
+				{ attributes.showOrderby && (
+					<ProductOrderSelect
+						onChange={ onOrderChange }
+						value={ orderValue }
 					/>
-				) ) }
-			</ul>
-			{ totalProducts > perPage && (
-				<Pagination
-					currentPage={ currentPage }
-					onPageChange={ onPageChange }
-					totalPages={ totalPages }
-				/>
-			) }
-		</div>
-	);
-};
+				) }
+				<ul className="wc-block-grid__products">
+					{ gridProducts.map( ( product = {}, i ) => (
+						<ProductGridItem
+							key={ product.id || i }
+							attributes={ attributes }
+							product={ product }
+						/>
+					) ) }
+				</ul>
+				{ totalProducts > perPage && (
+					<Pagination
+						currentPage={ currentPage }
+						onPageChange={ this.onPaginationChange }
+						totalPages={ totalPages }
+					/>
+				) }
+			</div>
+		);
+	}
+}
 
 ProductGrid.propTypes = {
 	attributes: PropTypes.object.isRequired,
