ae7a5acfca91439ed48c22df1cc7ee938da812b2
Darren Ethier <darren@roughsmootheng.in>
2019-10-08 12:51:19 -0400

    install compare-versions and export on `@woocommerce/settings` external

--- assets/js/settings/shared/index.js
+++ assets/js/settings/shared/index.js
@@ -1,3 +1,4 @@
 export { getSetting } from './get-setting';
 export { setSetting } from './set-setting';
 export * from './default-constants';
+export { default as compareVersions } from 'compare-versions';
