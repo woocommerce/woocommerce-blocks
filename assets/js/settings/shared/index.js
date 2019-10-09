a8de9eefdecd5c19046837dacd3328208f7cb205
Darren Ethier <darren@roughsmootheng.in>
2019-10-09 13:51:42 -0400

    export a compareWithWpVersion function for easier version checking

--- assets/js/settings/shared/index.js
+++ assets/js/settings/shared/index.js
@@ -1,4 +1,24 @@
-export { getSetting } from './get-setting';
-export { setSetting } from './set-setting';
 export * from './default-constants';
-export { default as compareVersions } from 'compare-versions';
+export { setSetting } from './set-setting';
+
+/**
+ * Internal dependencies
+ */
+import { getSetting } from './get-setting';
+import compareVersions from 'compare-versions';
+
+/**
+ * Note: this attempts to coerce the wp_version to a semver for comparison
+ * This will result in dropping any beta/rc values.
+ *
+ * `5.3-beta1-4252` would get converted to `5.3.4252`
+ */
+export const compareWithWpVersion = ( version, operator ) => {
+	return compareVersions.compare(
+		version,
+		getSetting( 'wp_version', '' ).replace( /-[a-zA-Z0-9]*?-/, '.' ),
+		operator
+	);
+};
+
+export { compareVersions, getSetting };
