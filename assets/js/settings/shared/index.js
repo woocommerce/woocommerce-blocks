fa9f3df0d0e4a65fb0399db1b4b803aa8bb92113
Darren Ethier <darren@roughsmootheng.in>
2019-10-10 14:21:38 -0400

    improve semver coercing for wp version check
    also add tests

--- assets/js/settings/shared/index.js
+++ assets/js/settings/shared/index.js
@@ -11,14 +11,22 @@ import compareVersions from 'compare-versions';
  * Note: this attempts to coerce the wp_version to a semver for comparison
  * This will result in dropping any beta/rc values.
  *
- * `5.3-beta1-4252` would get converted to `5.3.4252`
+ * `5.3-beta1-4252` would get converted to `5.3.0-rc.4252`
+ * `5.3-beta1` would get converted to `5.3.0-rc`.
+ * `5.3` would not be touched.
+ *
+ * For the purpose of these comparisons all pre-release versions are normalized
+ * to `rc`.
  */
 export const compareWithWpVersion = ( version, operator ) => {
-	return compareVersions.compare(
-		version,
-		getSetting( 'wp_version', '' ).replace( /-[a-zA-Z0-9]*?-/, '.' ),
-		operator
+	let replacement = getSetting( 'wp_version', '' ).replace(
+		/-[a-zA-Z0-9]*[\-]*/,
+		'.0-rc.'
 	);
+	replacement = replacement.endsWith( '.' )
+		? replacement.substring( 0, replacement.length - 1 )
+		: replacement;
+	return compareVersions.compare( version, replacement, operator );
 };
 
 export { compareVersions, getSetting };
