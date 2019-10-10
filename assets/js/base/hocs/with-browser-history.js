a22b047a7d9cc7618b3b414876bf6268947e77ce
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-10-10 17:05:47 +0200

    Remove HOCs no longer used

--- assets/js/base/hocs/with-browser-history.js
+++
@@ -1,9 +0,0 @@
-/**
- * Internal dependencies
- */
-import withBrowserWindowProp from './with-browser-window-prop.js';
-
-const withBrowserHistory = ( propMap ) =>
-	withBrowserWindowProp( 'history', propMap );
-
-export default withBrowserHistory;
--- assets/js/base/hocs/with-browser-location.js
+++
@@ -1,9 +0,0 @@
-/**
- * Internal dependencies
- */
-import withBrowserWindowProp from './with-browser-window-prop.js';
-
-const withBrowserLocation = ( propMap ) =>
-	withBrowserWindowProp( 'location', propMap );
-
-export default withBrowserLocation;
