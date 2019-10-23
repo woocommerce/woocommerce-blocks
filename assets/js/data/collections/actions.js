4a332fa7e72eff58adb70c980bd19ca48e3558bf
Darren Ethier <darren@roughsmootheng.in>
2019-10-23 13:12:50 -0400

    Fix spacing in inline docs
    Co-Authored-By: Albert Juhé Lluveras <aljullu@gmail.com>

--- assets/js/data/collections/actions.js
+++ assets/js/data/collections/actions.js
@@ -19,7 +19,7 @@ Headers = Headers
  *                                    model.
  * @param {Object}   [response={}]    An object containing the response from the
  *                                    collection request.
- * @param {Array<*>} response.items	  An array of items for the given collection.
+ * @param {Array<*>} response.items	An array of items for the given collection.
  * @param {Headers}  response.headers A Headers object from the response
  *                                    @link https://developer.mozilla.org/en-US/docs/Web/API/Headers
  * @param {bool}     [replace=false]  If true, signals to replace the current
