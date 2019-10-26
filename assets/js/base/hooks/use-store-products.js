31e00d3776017683c610531457d5b660dd6363d4
Darren Ethier <darren@roughsmootheng.in>
2019-10-26 16:18:13 -0400

    add doc blocks for useStoreProducts hooks

--- assets/js/base/hooks/use-store-products.js
+++ assets/js/base/hooks/use-store-products.js
@@ -14,6 +14,24 @@ const DEFAULT_OPTIONS = {
 	modelName: 'products',
 };
 
+/**
+ * This is a custom hook that is wired up to the `wc/store/collections` data
+ * store. Given a query object, this will ensure a component is kept up to date
+ * with the products matching that query in the store state.
+ *
+ * @param {Object} query   An object containing any query arguments to be
+ *                         included with the collection request for the
+ *                         products. Does not have to be included.
+ * @param {Object} options An optional object for adjusting the namespace and
+ *                         modelName for the products query.
+ *
+ * @return {Object} This hook will return an object with three properties:
+ *                  - products        An array of product objects.
+ *                  - totalProducts   The total number of products that match the
+ *                                    given query parameters.
+ *                  - productsLoading A boolean indicating whether the products
+ *                                    are still loading or not.
+ */
 export const useStoreProducts = ( query, options = DEFAULT_OPTIONS ) => {
 	const { namespace, modelName } = options;
 	if ( ! namespace || ! modelName ) {
