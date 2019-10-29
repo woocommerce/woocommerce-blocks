af2dfb75696bbf351f13e938e942e289fdefef1e
Mike Jolley <mike.jolley@me.com>
2019-10-29 13:17:30 +0000

    update from master

--- assets/js/utils/get-shortcode.js
+++ assets/js/utils/get-shortcode.js
@@ -89,7 +89,8 @@ export default function getShortcode( props, name ) {
 
 	// Build the shortcode string out of the set shortcode attributes.
 	let shortcode = '[products';
-	for ( const [ key, value ] of shortcodeAtts ) { /* eslint-disable-line */
+	for ( const [ key, value ] of shortcodeAtts ) {
+		/* eslint-disable-line */
 		shortcode += ' ' + key + '="' + value + '"';
 	}
 	shortcode += ']';
