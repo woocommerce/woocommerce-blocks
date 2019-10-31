9d701e9d1e48300074513f7dc69bfb9c7e8678e4
Mike Jolley <mike.jolley@me.com>
2019-10-31 12:43:24 +0000

    Remove old settings from merge conflict

--- assets/js/settings/admin-url.js
+++
@@ -1,8 +0,0 @@
-/**
- * Wrapper for the wcSettings global, which sets defaults if data is missing.
- *
- * Only settings used by blocks are defined here. Component settings are left out.
- */
-const adminUrl = wcSettings.adminUrl || '';
-
-export default adminUrl;
