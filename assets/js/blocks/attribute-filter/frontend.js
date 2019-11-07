18562a0747b4212f669e8c1472d0ace8d879a2c9
Mike Jolley <mike.jolley@me.com>
2019-11-07 11:22:50 +0000

    Use renderFrontend

--- assets/js/blocks/attribute-filter/frontend.js
+++ assets/js/blocks/attribute-filter/frontend.js
@@ -1,30 +1,21 @@
 /**
  * External dependencies
  */
-import { render } from 'react-dom';
+import renderFrontend from '../../utils/render-frontend.js';
 
 /**
  * Internal dependencies
  */
 import Block from './block.js';
 
-const containers = document.querySelectorAll(
-	'.wp-block-woocommerce-attribute-filter'
-);
-
-if ( containers.length ) {
-	Array.prototype.forEach.call( containers, ( el ) => {
-		const attributes = {
+const getProps = ( el ) => {
+	return {
+		attributes: {
 			attributeId: parseInt( el.dataset.attributeid || 0, 10 ),
 			showCounts: el.dataset.showcounts === 'true',
 			queryType: el.dataset.querytype,
-		};
-		el.classList.remove( 'is-loading' );
+		},
+	};
+};
 
-		if ( ! attributes.attributeId ) {
-			return null;
-		}
-
-		render( <Block attributes={ attributes } />, el );
-	} );
-}
+renderFrontend( '.wp-block-woocommerce-attribute-filter', Block, getProps );
