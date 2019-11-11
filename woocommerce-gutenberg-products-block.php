8f3572b86a6eba2c5d41b5511f4a6a7f395dc18d
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-11-11 15:49:46 +0100

    Fix wrong values for minimum WP and PHP versions

--- woocommerce-gutenberg-products-block.php
+++ woocommerce-gutenberg-products-block.php
@@ -7,8 +7,8 @@
  * Author: Automattic
  * Author URI: https://woocommerce.com
  * Text Domain:  woo-gutenberg-products-block
- * Requires at least: 5.2.0
- * Requires PHP: 5.6.20
+ * Requires at least: 5.0
+ * Requires PHP: 5.6
  * WC requires at least: 3.6.0
  * WC tested up to: 3.8.0
  *
