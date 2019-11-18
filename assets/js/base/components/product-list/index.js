52d43480e9c97ab5a83fa6d54dfb684bcbef162b
Mike Jolley <mike.jolley@me.com>
2019-11-18 11:11:00 +0000

    Remove product-grid

--- assets/js/base/components/product-list/index.js
+++ assets/js/base/components/product-list/index.js
@@ -65,18 +65,11 @@ const ProductList = ( {
 	);
 	// These are possible filters.
 	const [ productAttributes, setProductAttributes ] = useQueryStateByKey(
-		'product-grid',
 		'attributes',
 		[]
 	);
-	const [ minPrice, setMinPrice ] = useQueryStateByKey(
-		'product-grid',
-		'min_price'
-	);
-	const [ maxPrice, setMaxPrice ] = useQueryStateByKey(
-		'product-grid',
-		'max_price'
-	);
+	const [ minPrice, setMinPrice ] = useQueryStateByKey( 'min_price' );
+	const [ maxPrice, setMaxPrice ] = useQueryStateByKey( 'max_price' );
 	const onPaginationChange = ( newPage ) => {
 		scrollToTop( { focusableSelector: 'a, button' } );
 		onPageChange( newPage );
