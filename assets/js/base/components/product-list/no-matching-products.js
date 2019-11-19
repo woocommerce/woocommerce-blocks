a9fa9dc52408f7fc38c631d5a879b89f03f1635b
Darren Ethier <darren@roughsmootheng.in>
2019-11-19 09:41:45 -0500

    add missing context

--- assets/js/base/components/product-list/no-matching-products.js
+++ assets/js/base/components/product-list/no-matching-products.js
@@ -3,19 +3,25 @@
  */
 import { __ } from '@wordpress/i18n';
 import { WC_BLOCKS_ASSET_URL } from '@woocommerce/block-settings';
+import { useProductLayoutContext } from '@woocommerce/base-context/product-layout-context';
 
 const NoMatchingProducts = ( { resetCallback = () => {} } ) => {
+	const { layoutStyleClassPrefix } = useProductLayoutContext();
 	return (
-		<div className="wc-block-grid__no-products">
+		<div className={ `${ layoutStyleClassPrefix }__no-products` }>
 			<img
 				src={ WC_BLOCKS_ASSET_URL + 'img/no-matching-products.svg' }
 				alt={ __( 'No products', 'woo-gutenberg-products-block' ) }
-				className="wc-block-grid__no-products-image"
+				className={ `${ layoutStyleClassPrefix }__no-products-image` }
 			/>
-			<strong className="wc-block-grid__no-products-title">
+			<strong
+				className={ `${ layoutStyleClassPrefix }__no-products-title` }
+			>
 				{ __( 'No products found', 'woo-gutenberg-products-block' ) }
 			</strong>
-			<p className="wc-block-grid__no-products-description">
+			<p
+				className={ `${ layoutStyleClassPrefix }__no-products-description` }
+			>
 				{ __(
 					'We were unable to find any results based on your search.',
 					'woo-gutenberg-products-block'
--- assets/js/base/components/product-list/no-products.js
+++ assets/js/base/components/product-list/no-products.js
@@ -3,19 +3,25 @@
  */
 import { __ } from '@wordpress/i18n';
 import { WC_BLOCKS_ASSET_URL } from '@woocommerce/block-settings';
+import { useProductLayoutContext } from '@woocommerce/base-context/product-layout-context';
 
 const NoProducts = () => {
+	const { layoutStyleClassPrefix } = useProductLayoutContext();
 	return (
-		<div className="wc-block-grid__no-products">
+		<div className={ `${ layoutStyleClassPrefix }__no-products` }>
 			<img
 				src={ WC_BLOCKS_ASSET_URL + 'img/no-products.svg' }
 				alt={ __( 'No products', 'woo-gutenberg-products-block' ) }
-				className="wc-block-grid__no-products-image"
+				className={ `${ layoutStyleClassPrefix }__no-products-image` }
 			/>
-			<strong className="wc-block-grid__no-products-title">
+			<strong
+				className={ `${ layoutStyleClassPrefix }__no-products-title` }
+			>
 				{ __( 'No products', 'woo-gutenberg-products-block' ) }
 			</strong>
-			<p className="wc-block-grid__no-products-description">
+			<p
+				className={ `${ layoutStyleClassPrefix }__no-products-description` }
+			>
 				{ __(
 					'There are currently no products available to display.',
 					'woo-gutenberg-products-block'
