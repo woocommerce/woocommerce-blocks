d0bba8c62d2ff04367a77dbc56c5d0a0590d59e9
Mike Jolley <mike.jolley@me.com>
2019-11-19 12:31:17 +0000

    HydrateRestApiData component

--- assets/js/utils/render-frontend.js
+++ assets/js/utils/render-frontend.js
@@ -2,6 +2,44 @@
  * External dependencies
  */
 import { render } from 'react-dom';
+import { getSetting } from '@woocommerce/settings';
+import { SCHEMA_STORE_KEY } from '@woocommerce/block-data';
+import { useEffect, useRef } from '@wordpress/element';
+import { useDispatch, useSelect } from '@wordpress/data';
+import { forEach } from 'lodash';
+
+/**
+ * Hydrate Rest API data from settings to reduce the number of API requests needed.
+ *
+ * @param {mixed} children Child components.
+ */
+const HydrateRestApiData = ( { children } ) => {
+	const { receiveRoutes, startResolution, finishResolution } = useDispatch(
+		SCHEMA_STORE_KEY
+	);
+	const { isResolving, hasFinishedResolution } = useSelect( ( select ) => {
+		const store = select( SCHEMA_STORE_KEY );
+		return store;
+	} );
+	const RestApiRoutes = useRef( getSetting( 'restApiRoutes' ) );
+
+	useEffect( () => {
+		if ( RestApiRoutes.current ) {
+			forEach( RestApiRoutes.current, ( routes, namespace ) => {
+				if (
+					! isResolving( 'getRoutes', [ namespace ] ) &&
+					! hasFinishedResolution( 'getRoutes', [ namespace ] )
+				) {
+					startResolution( 'getRoutes', [ namespace ] );
+					receiveRoutes( routes, [ namespace ] );
+					finishResolution( 'getRoutes', [ namespace ] );
+				}
+			} );
+		}
+	}, [ RestApiRoutes ] );
+
+	return children;
+};
 
 /**
  * Renders a block component in the place of a specified set of selectors.
@@ -15,8 +53,7 @@ export default ( selector, Block, getProps = () => {} ) => {
 	const containers = document.querySelectorAll( selector );
 
 	if ( containers.length ) {
-		// Use Array.forEach for IE11 compatibility
-		Array.prototype.forEach.call( containers, ( el, i ) => {
+		forEach( containers, ( el, i ) => {
 			const props = getProps( el, i );
 			const attributes = {
 				...el.dataset,
@@ -25,7 +62,12 @@ export default ( selector, Block, getProps = () => {} ) => {
 
 			el.classList.remove( 'is-loading' );
 
-			render( <Block { ...props } attributes={ attributes } />, el );
+			render(
+				<HydrateRestApiData>
+					<Block { ...props } attributes={ attributes } />
+				</HydrateRestApiData>,
+				el
+			);
 		} );
 	}
 };
