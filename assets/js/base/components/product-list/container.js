2761255d46aaa2959c962abe0fdec9acef2c149f
Darren Ethier <darren@roughsmootheng.in>
2019-12-01 07:00:03 -0500

    remove with-query-string-values hoc implementation

--- assets/js/base/components/product-list/container.js
+++ assets/js/base/components/product-list/container.js
@@ -1,47 +1,45 @@
 /**
  * External dependencies
  */
-import { Component } from 'react';
+import { useState } from '@wordpress/element';
 import PropTypes from 'prop-types';
-import withQueryStringValues from '@woocommerce/base-hocs/with-query-string-values';
 
 /**
  * Internal dependencies
  */
 import ProductList from './index';
 
-class ProductListContainer extends Component {
-	onPageChange = ( newPage ) => {
-		this.props.updateQueryStringValues( {
-			product_page: newPage,
-		} );
-	};
-
-	onSortChange = ( event ) => {
-		const newSortValue = event.target.value;
-		this.props.updateQueryStringValues( {
-			product_sort: newSortValue,
-			product_page: 1,
-		} );
-	};
-
-	render() {
+const ProductListContainer = ( {
+	attributes,
+	// eslint-disable-next-line camelcase
+	product_page = 1,
+	// eslint-disable-next-line camelcase
+	product_sort,
+} ) => {
+	const [ currentPage, setPage ] = useState( product_page );
+	const [ currentSort, setSort ] = useState(
 		// eslint-disable-next-line camelcase
-		const { attributes, product_page, product_sort } = this.props;
-		const currentPage = parseInt( product_page );
-		const sortValue = product_sort || attributes.orderby; // eslint-disable-line camelcase
+		product_sort || attributes.orderby
+	);
+	const onPageChange = ( newPage ) => {
+		setPage( newPage );
+	};
+	const onSortChange = ( event ) => {
+		const newSortValue = event.target.value;
+		setSort( newSortValue );
+		setPage( 1 );
+	};
 
-		return (
-			<ProductList
-				attributes={ attributes }
-				currentPage={ currentPage }
-				onPageChange={ this.onPageChange }
-				onSortChange={ this.onSortChange }
-				sortValue={ sortValue }
-			/>
-		);
-	}
-}
+	return (
+		<ProductList
+			attributes={ attributes }
+			currentPage={ currentPage }
+			onPageChange={ onPageChange }
+			onSortChange={ onSortChange }
+			sortValue={ currentSort }
+		/>
+	);
+};
 
 ProductListContainer.propTypes = {
 	attributes: PropTypes.object.isRequired,
@@ -54,6 +52,4 @@ ProductListContainer.defaultProps = {
 	product_page: 1,
 };
 
-export default withQueryStringValues( [ 'product_page', 'product_sort' ] )(
-	ProductListContainer
-);
+export default ProductListContainer;
