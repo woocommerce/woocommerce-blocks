55c6d2507acd036dc9bf2cef6f7e471ed165f5be
Darren Ethier <darren@roughsmootheng.in>
2019-12-02 07:00:53 -0500

    remove unnecessary passed in props and clean up proptypes

--- assets/js/base/components/product-list/container.js
+++ assets/js/base/components/product-list/container.js
@@ -9,18 +9,9 @@ import PropTypes from 'prop-types';
  */
 import ProductList from './index';
 
-const ProductListContainer = ( {
-	attributes,
-	// eslint-disable-next-line camelcase
-	product_page = 1,
-	// eslint-disable-next-line camelcase
-	product_sort,
-} ) => {
-	const [ currentPage, setPage ] = useState( product_page );
-	const [ currentSort, setSort ] = useState(
-		// eslint-disable-next-line camelcase
-		product_sort || attributes.orderby
-	);
+const ProductListContainer = ( { attributes } ) => {
+	const [ currentPage, setPage ] = useState( 1 );
+	const [ currentSort, setSort ] = useState( attributes.orderby );
 	const onPageChange = ( newPage ) => {
 		setPage( newPage );
 	};
@@ -43,13 +34,6 @@ const ProductListContainer = ( {
 
 ProductListContainer.propTypes = {
 	attributes: PropTypes.object.isRequired,
-	// From withQueryStringValues
-	product_page: PropTypes.oneOfType( [ PropTypes.number, PropTypes.string ] ),
-	product_sort: PropTypes.string,
-};
-
-ProductListContainer.defaultProps = {
-	product_page: 1,
 };
 
 export default ProductListContainer;
