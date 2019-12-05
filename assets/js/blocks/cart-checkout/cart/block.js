f693afc3d362ff46224bc3a5bac10c82ac6822bc
Rua Haszard <rua.haszard@automattic.com>
2019-12-05 15:46:39 +1300

    allow merchant to toggle cart empty/full modes in block editor

--- assets/js/blocks/cart-checkout/cart/block.js
+++ assets/js/blocks/cart-checkout/cart/block.js
@@ -1,13 +1,66 @@
 /**
  * External dependencies
  */
-import { Placeholder } from '@wordpress/components';
+import { BlockControls } from '@wordpress/block-editor';
+import { Toolbar } from '@wordpress/components';
+import { Fragment, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import FullCart from './components/full-cart';
+import EmptyCart from './components/empty-cart';
 
 /**
  * Component to handle edit mode of "Cart Block".
  */
 const Cart = () => {
-	return <Placeholder>Cart block coming soon…</Placeholder>;
+	const [ isFullCartMode, setFullCartMode ] = useState( true );
+
+	const toggleFullCartMode = () => {
+		setFullCartMode( ! isFullCartMode );
+	};
+
+	const getBlockControls = () => {
+		return (
+			<BlockControls>
+				<Toolbar
+					controls={ [
+						{
+							// Using dashicons for now, but we need to decide whether we're using
+							// icons or text. These icons are not ideal :)
+							icon: 'yes-alt',
+							title: __(
+								'Full Cart',
+								'woo-gutenberg-products-block'
+							),
+							onClick: toggleFullCartMode,
+							isActive: isFullCartMode,
+						},
+						{
+							icon: 'marker',
+							title: __(
+								'Empty Cart',
+								'woo-gutenberg-products-block'
+							),
+							onClick: toggleFullCartMode,
+							isActive: ! isFullCartMode,
+						},
+					] }
+				/>
+			</BlockControls>
+		);
+	};
+
+	const cart = isFullCartMode ? <FullCart /> : <EmptyCart />;
+
+	return (
+		<Fragment>
+			{ getBlockControls() }
+			{ cart }
+		</Fragment>
+	);
 };
 
 Cart.propTypes = {};
