cc7bf0362bf9176cc23020189e4908d5d4c18b40
Rua Haszard <rua.haszard@automattic.com>
2019-12-06 13:35:50 +1300

    fake data for editing full cart + show line count in header

--- assets/js/blocks/cart-checkout/cart/components/full-cart.js
+++ assets/js/blocks/cart-checkout/cart/components/full-cart.js
@@ -1,21 +1,52 @@
 /**
  * External dependencies
  */
-import { Fragment } from '@wordpress/element';
+import { sprintf, _n } from '@wordpress/i18n';
 import PropTypes from 'prop-types';
+
+const CartProductsTable = () => {
+	return 'Coming soon!';
+}
+
+const CartLineItems = ( { className, ...props } ) => {
+	const { lineItems } = props;
+
+	const itemCountHeading = sprintf( _n( '%d item', '%d items', lineItems.length, 'woo-gutenberg-products-block' ), lineItems.length );
+
+	return (
+		<div className={ className }>
+			<h2>
+				<span>Shopping cart</span>
+				<span className='wc-blocks-cart__item-count'>{ itemCountHeading }</span>
+			</h2>
+			<CartProductsTable />
+		</div>
+	);
+
+}
+
+const sampleCartData = {
+	lineItems: [
+		{
+			name: 'Beanie',
+			itemPrice: 18.0,
+			quantity: 2,
+			linePrice: 36.0,
+		},
+		{
+			name: 'Hoodie',
+			itemPrice: 135.0,
+			quantity: 3,
+			linePrice: 36.0,
+		}
+	]
+};
 
 /**
  * Component to handle edit mode for the Cart block when user has something in cart aka "full".
  */
 const Cart = () => {
-	return (
-		<Fragment>
-			<h2>Shopping cart</h2>
-			<span>
-				Cart block <b>full state</b> coming soon…
-			</span>
-		</Fragment>
-	);
+	return ( <CartLineItems className='wc-blocks-cart' { ...sampleCartData } /> );
 };
 
 Cart.propTypes = {
