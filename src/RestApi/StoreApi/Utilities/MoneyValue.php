5a5bc77c528c625b66572ea90d2afa5a4bec5da8
Mike Jolley <mike.jolley@me.com>
2019-12-12 12:10:26 +0000

    Implement basic version of MoneyValue with decimal conversion

---
+++ src/RestApi/StoreApi/Utilities/MoneyValue.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Handles monetary values using integers by using the smallest unit of a currency.
+ *
+ * @package WooCommerce/Blocks
+ */
+
+namespace Automattic\WooCommerce\Blocks\RestApi\StoreApi\Utilities;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * MoneyValue class.
+ */
+class MoneyValue {
+
+	/**
+	 * Monetary amount. Integer stored as string.
+	 *
+	 * @var string
+	 */
+	protected $amount = '0';
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+
+	}
+
+	/**
+	 * Converts to string if this class is used as a string.
+	 */
+	public function __toString() {
+		return $this->amount;
+	}
+
+	/**
+	 * Return the price amount.
+	 *
+	 * @return string|int
+	 */
+	public function get_amount() {
+		return $this->amount > PHP_INT_MAX ? $this->amount : (int) $this->amount;
+	}
+
+	/**
+	 * Set monetary amount in subunit format from decimal format.
+	 *
+	 * @param string|float $amount Monetary amount with decimals.
+	 * @param int          $decimals Number if decimals the amount is formatted with.
+	 * @param string       $rounding_mode Valid rounding mode: PHP_ROUND_HALF_DOWN, PHP_ROUND_HALF_UP, PHP_ROUND_HALF_EVEN, PHP_ROUND_HALF_ODD.
+	 * @return string      The new amount.
+	 */
+	public function from_decimal( $amount, $decimals = 2, $rounding_mode = 'PHP_ROUND_HALF_UP' ) {
+		$this->amount = (string) intval(
+			round(
+				wc_format_decimal( $amount ) * pow( 10, $decimals ),
+				0,
+				$this->get_rounding_mode( $rounding_mode )
+			)
+		);
+
+		return $this->get_amount();
+	}
+
+	/**
+	 * Get rounding mode constant to pass from PHP round.
+	 *
+	 * @param string|int $rounding_mode Either a PHP rounding mode constant name, or number.
+	 * @return int
+	 */
+	protected function get_rounding_mode( $rounding_mode = 'PHP_ROUND_HALF_UP' ) {
+		if ( is_numeric( $rounding_mode ) ) {
+			return (int) $rounding_mode;
+		}
+
+		if ( defined( $rounding_mode ) ) {
+			return constant( $rounding_mode );
+		}
+
+		return 1;
+	}
+}
