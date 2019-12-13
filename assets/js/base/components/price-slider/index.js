44b4aec1e9038bc783caa74a00f6a7a7914d597f
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-12-13 21:27:51 +0100

    Save previous constraint while loading

--- assets/js/base/components/price-slider/index.js
+++ assets/js/base/components/price-slider/index.js
@@ -259,7 +259,7 @@ const PriceSlider = ( {
 				onMouseMove={ findClosestRange }
 				onFocus={ findClosestRange }
 			>
-				{ ! isLoading && hasValidConstraints && (
+				{ hasValidConstraints && (
 					<Fragment>
 						<div
 							className="wc-block-price-filter__range-input-progress"
@@ -278,6 +278,7 @@ const PriceSlider = ( {
 							min={ minConstraint }
 							max={ maxConstraint }
 							ref={ minRange }
+							disabled={ isLoading }
 						/>
 						<input
 							type="range"
@@ -292,6 +293,7 @@ const PriceSlider = ( {
 							min={ minConstraint }
 							max={ maxConstraint }
 							ref={ maxRange }
+							disabled={ isLoading }
 						/>
 					</Fragment>
 				) }
