741fca4cd9547e5c4a62fa2ca5b1d1954ff70b62
Albert Juhé Lluveras <contact@albertjuhe.com>
2019-12-16 16:19:34 +0100

    Refactor usePriceConstraints to DRY and add tests

---
+++ assets/js/blocks/price-filter/use-price-constraints.js
@@ -0,0 +1,23 @@
+/**
+ * Internal dependencies
+ */
+import { usePrevious } from '../../base/hooks/use-previous';
+
+export const usePriceConstraint = ( price ) => {
+	const currentConstraint = isNaN( price )
+		? null
+		: Math.floor( parseInt( price, 10 ) / 10 ) * 10;
+	const previousConstraint = usePrevious( currentConstraint, ( val ) =>
+		Number.isFinite( val )
+	);
+	return Number.isFinite( currentConstraint )
+		? currentConstraint
+		: previousConstraint;
+};
+
+export default ( { minPrice, maxPrice } ) => {
+	return {
+		minConstraint: usePriceConstraint( minPrice ),
+		maxConstraint: usePriceConstraint( maxPrice ),
+	};
+};
