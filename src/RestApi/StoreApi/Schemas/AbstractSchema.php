a051bb8c7c117a74c7b0beded72d5d552d2a1299
Mike Jolley <mike.jolley@me.com>
2019-12-18 10:45:10 +0000

    typo in todo note

--- src/RestApi/StoreApi/Schemas/AbstractSchema.php
+++ src/RestApi/StoreApi/Schemas/AbstractSchema.php
@@ -119,7 +119,7 @@ abstract class AbstractSchema {
 	/**
 	 * Prepares a list of store currency data to return in responses.
 	 *
-	 * @todo Core could use a more format currency object format, making use of
+	 * @todo Core could use a more defined currency object format, making use of
 	 * constants for currency format rather than strings, and holding this type
 	 * of information instead of plugins/blocks needed to normalize things
 	 * themselves.
