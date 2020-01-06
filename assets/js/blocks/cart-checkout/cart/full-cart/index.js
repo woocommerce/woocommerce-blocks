7a2889c4082a0418ad623b1b4032a38b50452198
Rua Haszard <rua.haszard@automattic.com>
2020-01-06 13:47:46 +1300

    reinstate work-in-progress full cart component (lost in rebase)

--- assets/js/blocks/cart-checkout/cart/full-cart/index.js
+++ assets/js/blocks/cart-checkout/cart/full-cart/index.js
@@ -1,27 +1,59 @@
 /**
- * Internal dependencies
+ * External dependencies
  */
-import CheckoutButton from './checkout-button';
-import './style.scss';
+import { sprintf, _n } from '@wordpress/i18n';
+import PropTypes from 'prop-types';
 
-/**
- * Component that renders the Cart block when user has something in cart aka "full".
- */
-const Cart = () => {
+const CartProductsTable = () => {
+	return 'Coming soon!';
+}
+
+const CartLineItems = ( { className, ...props } ) => {
+	const { lineItems } = props;
+
+	const itemCountHeading = sprintf( _n( '%d item', '%d items', lineItems.length, 'woo-gutenberg-products-block' ), lineItems.length );
+
 	return (
-		<div className="wc-block-cart">
-			<div className="wc-block-cart__main">
-				<span>
-					Cart block <b>full state</b> coming soon…
-				</span>
-			</div>
-			<div className="wc-block-cart__sidebar">
-				<CheckoutButton />
-			</div>
+		<div className={ className }>
+			<h2>
+				<span>Shopping cart</span>
+				<span className='wc-blocks-cart__item-count'>{ itemCountHeading }</span>
+			</h2>
+			<CartProductsTable />
 		</div>
 	);
+
+}
+
+const sampleCartData = {
+	lineItems: [
+		{
+			name: 'Beanie',
+			itemPrice: 18.0,
+			quantity: 2,
+			linePrice: 36.0,
+		},
+		{
+			name: 'Hoodie',
+			itemPrice: 135.0,
+			quantity: 3,
+			linePrice: 36.0,
+		}
+	]
 };
 
-Cart.propTypes = {};
+/**
+ * Component to handle edit mode for the Cart block when user has something in cart aka "full".
+ */
+const Cart = () => {
+	return ( <CartLineItems className='wc-blocks-cart' { ...sampleCartData } /> );
+};
+
+Cart.propTypes = {
+	/**
+	 * The attributes for this block.
+	 */
+	attributes: PropTypes.object.isRequired,
+};
 
 export default Cart;
