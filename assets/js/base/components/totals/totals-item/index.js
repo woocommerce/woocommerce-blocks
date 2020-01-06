4e8c5daeb0d4d99e54126013289ba14777936236
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-01-06 15:22:20 +0100

    Update totalItems shape to match API

--- assets/js/base/components/totals/totals-item/index.js
+++ assets/js/base/components/totals/totals-item/index.js
@@ -3,19 +3,25 @@
  */
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import FormattedMonetaryAmount from '@woocommerce/base-components/formatted-monetary-amount';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 
-const TotalsItem = ( { className, label, value, description } ) => {
+const TotalsItem = ( { className, currency, label, value, description } ) => {
 	return (
 		<div
 			className={ classnames( 'wc-block-totals-table-item', className ) }
 		>
 			<span className="wc-block-totals-table-item__label">{ label }</span>
-			<span className="wc-block-totals-table-item__value">{ value }</span>
+			<FormattedMonetaryAmount
+				className="wc-block-totals-table-item__value"
+				currency={ currency }
+				displayType="text"
+				value={ value }
+			/>
 			<span className="wc-block-totals-table-item__description">
 				{ description }
 			</span>
@@ -24,8 +30,9 @@ const TotalsItem = ( { className, label, value, description } ) => {
 };
 
 TotalsItem.propTypes = {
+	currency: PropTypes.object.isRequired,
 	label: PropTypes.string.isRequired,
-	value: PropTypes.string.isRequired,
+	value: PropTypes.number.isRequired,
 	className: PropTypes.string,
 	description: PropTypes.node,
 };
--- assets/js/blocks/cart-checkout/cart/full-cart/index.js
+++ assets/js/blocks/cart-checkout/cart/full-cart/index.js
@@ -8,7 +8,11 @@ import {
 	TotalsItem,
 } from '@woocommerce/base-components/totals';
 import RadioControl from '@woocommerce/base-components/radio-control';
-import { COUPONS_ENABLED } from '@woocommerce/block-settings';
+import {
+	COUPONS_ENABLED,
+	DISPLAY_PRICES_INCLUDING_TAXES,
+} from '@woocommerce/block-settings';
+import { getCurrencyFromPriceResponse } from '@woocommerce/base-utils';
 
 /**
  * Internal dependencies
@@ -26,28 +30,62 @@ const Cart = () => {
 		// eslint-disable-next-line no-console
 		console.log( 'coupon activated: ' + couponCode );
 	};
+	const cartTotals = {
+		currency: 'EUR',
+		currency_minor_unit: 2,
+		total_items: 6000,
+		total_items_tax: 0,
+		total_fees: 0,
+		total_fees_tax: 0,
+		total_discount: 0,
+		total_discount_tax: 0,
+		total_shipping: 0,
+		total_shipping_tax: 0,
+		total_tax: 0,
+		total_price: 6000,
+	};
+
+	const currency = getCurrencyFromPriceResponse( cartTotals );
 	const totalItems = [
 		{
 			label: __( 'List items:', 'woo-gutenberg-products-block' ),
-			value: '€300.00',
-		},
-		{
-			label: __( 'Subtotals', 'woo-gutenberg-products-block' ),
-			value: '€270.00',
-		},
-		{
-			label: __(
-				'Estimated shipping cost:',
-				'woo-gutenberg-products-block'
-			),
-			value: '€6.00',
-			description: __(
-				'Shipping to location (change address)',
-				'woo-gutenberg-products-block'
-			),
+			value: DISPLAY_PRICES_INCLUDING_TAXES
+				? cartTotals.total_items + cartTotals.total_items_tax
+				: cartTotals.total_items,
 		},
 	];
-	const totalValue = '€100';
+	if ( cartTotals.total_fees > 0 ) {
+		totalItems.push( {
+			label: __( 'Fees:', 'woo-gutenberg-products-block' ),
+			value: DISPLAY_PRICES_INCLUDING_TAXES
+				? cartTotals.total_fees + cartTotals.total_fees_tax
+				: cartTotals.total_fees,
+		} );
+	}
+	if ( cartTotals.total_discount > 0 ) {
+		totalItems.push( {
+			label: __( 'Discount:', 'woo-gutenberg-products-block' ),
+			value: DISPLAY_PRICES_INCLUDING_TAXES
+				? cartTotals.total_discount + cartTotals.total_discount_tax
+				: cartTotals.total_discount,
+		} );
+	}
+	if ( ! DISPLAY_PRICES_INCLUDING_TAXES ) {
+		totalItems.push( {
+			label: __( 'Taxes:', 'woo-gutenberg-products-block' ),
+			value: cartTotals.total_tax,
+		} );
+	}
+	totalItems.push( {
+		label: __( 'Shipping:', 'woo-gutenberg-products-block' ),
+		value: DISPLAY_PRICES_INCLUDING_TAXES
+			? cartTotals.total_shipping + cartTotals.total_shipping_tax
+			: cartTotals.total_shipping,
+		description: __(
+			'Shipping to location (change address)',
+			'woo-gutenberg-products-block'
+		),
+	} );
 
 	const [ selectedShippingOption, setSelectedShippingOption ] = useState(
 		placeholderShippingMethods[ 0 ].value
@@ -67,6 +105,7 @@ const Cart = () => {
 				{ totalItems.map( ( { label, value, description } ) => (
 					<TotalsItem
 						key={ label }
+						currency={ currency }
 						label={ label }
 						value={ value }
 						description={ description }
@@ -107,8 +146,9 @@ const Cart = () => {
 				) }
 				<TotalsItem
 					className="wc-block-cart__totals-footer"
+					currency={ currency }
 					label={ __( 'Total', 'woo-gutenberg-products-block' ) }
-					value={ totalValue }
+					value={ cartTotals.total_price }
 				/>
 				<CheckoutButton />
 			</div>
