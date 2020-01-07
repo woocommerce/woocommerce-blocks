4da385ce7f1c555b8a6f831a7011a4014ffd04b6
Darren Ethier <darren@roughsmootheng.in>
2020-01-07 11:02:12 -0500

    Bumping version strings to new version.

--- src/Package.php
+++ src/Package.php
@@ -84,7 +84,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '2.5.8';
+					$version = '2.5.9';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ )
