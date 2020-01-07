039505297bb26d0ca52fb18e93d8fd7360d114fd
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-01-07 14:04:10 +0100

    Use get_variation_prices tax-formatted prices

--- src/RestApi/StoreApi/Schemas/ProductSchema.php
+++ src/RestApi/StoreApi/Schemas/ProductSchema.php
@@ -281,12 +281,12 @@ class ProductSchema extends AbstractSchema {
 		$price_function   = 'incl' === $tax_display_mode ? 'wc_get_price_including_tax' : 'wc_get_price_excluding_tax';
 
 		if ( $product->is_type( 'variable' ) ) {
-			$prices = $product->get_variation_prices( false );
+			$prices = $product->get_variation_prices( true );
 
 			if ( min( $prices['price'] ) !== max( $prices['price'] ) ) {
 				return [
-					'min_amount' => $this->prepare_money_response( $price_function( $product, [ 'price' => min( $prices['price'] ) ] ), wc_get_price_decimals() ),
-					'max_amount' => $this->prepare_money_response( $price_function( $product, [ 'price' => max( $prices['price'] ) ] ), wc_get_price_decimals() ),
+					'min_amount' => $this->prepare_money_response( min( $prices['price'] ), wc_get_price_decimals() ),
+					'max_amount' => $this->prepare_money_response( max( $prices['price'] ), wc_get_price_decimals() ),
 				];
 			}
 		}
