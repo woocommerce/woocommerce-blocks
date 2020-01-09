75cd337547c28da2a67cbf123354ba5572dd1bbb
Mike Jolley <mike.jolley@me.com>
2020-01-09 17:24:16 +0000

    Calculate shipping and selected shipping from order properties, not global cart properties

--- src/RestApi/StoreApi/Controllers/CartOrder.php
+++ src/RestApi/StoreApi/Controllers/CartOrder.php
@@ -47,6 +47,13 @@ class CartOrder extends RestController {
 	protected $schema;
 
 	/**
+	 * Draft order details, if applicable.
+	 *
+	 * @var array
+	 */
+	protected $draft_order;
+
+	/**
 	 * Setup API class.
 	 */
 	public function __construct() {
@@ -101,8 +108,21 @@ class CartOrder extends RestController {
 	 */
 	public function create_item( $request ) {
 		try {
-			// If part of the request, first update shipping selections so that cart totals are correct.
-			$this->select_shipping_rates( $request );
+			$this->draft_order = WC()->session->get(
+				'store_api_draft_order',
+				[
+					'id'     => 0,
+					'hashes' => [
+						'line_items' => false,
+						'fees'       => false,
+						'coupons'    => false,
+						'taxes'      => false,
+					],
+				]
+			);
+
+			// Update session based on posted data.
+			$this->update_session( $request );
 
 			// Create or retrieve the draft order for the current cart.
 			$order_object = $this->create_order_from_cart( $request );
@@ -121,31 +141,30 @@ class CartOrder extends RestController {
 	}
 
 	/**
-	 * Select shipping rates and store in session.
+	 * Before creating anything, this method ensures the cart session is up to date and matches the data we're going
+	 * to be adding to the order.
 	 *
-	 * @todo Might be useful to included validation here. However right now it will just default to the default method
-	 * if the chosen rate id is invalid.
-	 *
-	 * @throws RestException Exception when shipping is invalid.
 	 * @param RestRequest $request Full details about the request.
+	 * @return void
 	 */
-	protected function select_shipping_rates( RestRequest $request ) {
-		if ( ! isset( $request['shipping_rates'] ) ) {
-			return;
-		}
+	protected function update_session( RestRequest $request ) {
+		$schema = $this->get_item_schema();
 
-		$chosen_shipping_methods = [];
-
-		foreach ( $request['shipping_rates'] as $shipping_rate ) {
-			if ( ! isset( $shipping_rate['rate_id'] ) ) {
-				continue;
+		if ( isset( $request['billing_address'] ) ) {
+			$allowed_billing_values = array_intersect_key( $request['billing_address'], $schema['properties']['billing_address']['properties'] );
+			foreach ( $allowed_billing_values as $key => $value ) {
+				WC()->customer->{"set_billing_$key"}( $value );
 			}
-			$chosen_shipping_methods[] = $shipping_rate['rate_id'];
 		}
 
-		WC()->session->set( 'chosen_shipping_methods', $chosen_shipping_methods );
-		WC()->cart->calculate_shipping();
-		WC()->cart->calculate_totals();
+		if ( isset( $request['shipping_address'] ) ) {
+			$allowed_shipping_values = array_intersect_key( $request['shipping_address'], $schema['properties']['shipping_address']['properties'] );
+			foreach ( $allowed_shipping_values as $key => $value ) {
+				WC()->customer->{"set_shipping_$key"}( $value );
+			}
+		}
+
+		WC()->customer->save();
 	}
 
 	/**
@@ -180,19 +199,44 @@ class CartOrder extends RestController {
 		$order->set_customer_id( get_current_user_id() );
 		$order->set_customer_ip_address( \WC_Geolocation::get_ip_address() );
 		$order->set_customer_user_agent( wc_get_user_agent() );
+		$order->set_cart_hash( WC()->cart->get_cart_hash() );
+		$order->update_meta_data( 'is_vat_exempt', WC()->cart->get_customer()->get_is_vat_exempt() ? 'yes' : 'no' );
 
-		$this->set_props_from_cart( $order, $request );
-		$this->create_line_items_from_cart( $order, $request );
 		$this->set_props_from_request( $order, $request );
+		$this->create_line_items_from_cart( $order, $request );
+		$this->select_shipping_rates( $order, $request );
 
-		$order->save();
+		// Calc totals, taxes, and save.
+		$order->calculate_totals();
 
 		remove_filter( 'woocommerce_default_order_status', array( $this, 'default_order_status' ) );
 
-		// Store Order ID in session so we can look it up later.
-		WC()->session->set( 'draft_order_id', $order->get_id() );
+		// Store Order details in session so we can look it up later.
+		WC()->session->set(
+			'store_api_draft_order',
+			[
+				'id'     => $order->get_id(),
+				'hashes' => $this->get_cart_hashes( $order, $request ),
+			]
+		);
 
 		return $order;
+	}
+
+	/**
+	 * Get hashes for items in the current cart. Useful for tracking changes.
+	 *
+	 * @param \WC_Order   $order Object to prepare for the response.
+	 * @param RestRequest $request Full details about the request.
+	 * @return array
+	 */
+	protected function get_cart_hashes( \WC_Order $order, RestRequest $request ) {
+		return [
+			'line_items' => md5( wp_json_encode( WC()->cart->get_cart() ) ),
+			'fees'       => md5( wp_json_encode( WC()->cart->get_fees() ) ),
+			'coupons'    => md5( wp_json_encode( WC()->cart->get_applied_coupons() ) ),
+			'taxes'      => md5( wp_json_encode( WC()->cart->get_taxes() ) ),
+		];
 	}
 
 	/**
@@ -201,8 +245,7 @@ class CartOrder extends RestController {
 	 * @return \WC_Order A new order object.
 	 */
 	protected function get_order_object() {
-		$draft_order_id = WC()->session->get( 'draft_order_id' );
-		$draft_order    = $draft_order_id ? wc_get_order( $draft_order_id ) : false;
+		$draft_order = $this->draft_order['id'] ? wc_get_order( $this->draft_order['id'] ) : false;
 
 		if ( $draft_order && $draft_order->has_status( 'draft' ) && 'store-api' === $draft_order->get_created_via() ) {
 			return $draft_order;
@@ -221,57 +264,132 @@ class CartOrder extends RestController {
 	}
 
 	/**
-	 * Set order totals based on those calculated by the cart.
-	 *
-	 * @param \WC_Order   $order Object to prepare for the response.
-	 * @param RestRequest $request Full details about the request.
-	 */
-	protected function set_props_from_cart( \WC_Order $order, RestRequest $request ) {
-		$order->set_shipping_total( WC()->cart->get_shipping_total() );
-		$order->set_discount_total( WC()->cart->get_discount_total() );
-		$order->set_discount_tax( WC()->cart->get_discount_tax() );
-		$order->set_cart_tax( WC()->cart->get_cart_contents_tax() + WC()->cart->get_fee_tax() );
-		$order->set_shipping_tax( WC()->cart->get_shipping_tax() );
-		$order->set_total( WC()->cart->get_total( 'edit' ) );
-		$order->update_meta_data( 'is_vat_exempt', WC()->cart->get_customer()->get_is_vat_exempt() ? 'yes' : 'no' );
-	}
-
-	/**
 	 * Create order line items.
 	 *
 	 * @todo Knowing if items changed between the order and cart can be complex. Line items are ok because there is a
 	 * hash, but no hash exists for other line item types. Having a normalised set of data between cart and order, or
-	 * additional hashes, would be useful in the future and to help refactor this code.
+	 * additional hashes, would be useful in the future and to help refactor this code. In the meantime, we're relying
+	 * on custom hashes in $this->draft_order to track if things changed.
 	 *
 	 * @param \WC_Order   $order Object to prepare for the response.
 	 * @param RestRequest $request Full details about the request.
 	 */
 	protected function create_line_items_from_cart( \WC_Order $order, RestRequest $request ) {
-		// We only need to update line items if the cart changed.
-		if ( WC()->cart->get_cart_hash() !== $order->get_cart_hash() ) {
+		$new_hashes = $this->get_cart_hashes( $order, $request );
+		$old_hashes = $this->draft_order['hashes'];
+
+		if ( $new_hashes['line_items'] !== $old_hashes['line_items'] ) {
 			$order->remove_order_items( 'line_item' );
-			$order->set_cart_hash( WC()->cart->get_cart_hash() );
 			WC()->checkout->create_order_line_items( $order, WC()->cart );
 		}
 
-		// This checks to see if coupons have changed between the cart and the order.
-		if ( array_diff( $order->get_coupon_codes(), WC()->cart->get_applied_coupons() ) ) {
+		if ( $new_hashes['coupons'] !== $old_hashes['coupons'] ) {
 			$order->remove_order_items( 'coupon' );
 			WC()->checkout->create_order_coupon_lines( $order, WC()->cart );
 		}
 
-		// There is no way to know if fees changed, so recreate them here.
-		$order->remove_order_items( 'fee' );
-		WC()->checkout->create_order_fee_lines( $order, WC()->cart );
+		if ( $new_hashes['fees'] !== $old_hashes['fees'] ) {
+			$order->remove_order_items( 'fee' );
+			WC()->checkout->create_order_fee_lines( $order, WC()->cart );
+		}
 
-		// There is no way to know if shipping changed, so recreate it here.
+		if ( $new_hashes['taxes'] !== $old_hashes['taxes'] ) {
+			$order->remove_order_items( 'tax' );
+			WC()->checkout->create_order_tax_lines( $order, WC()->cart );
+		}
+	}
+
+	/**
+	 * Select shipping rates and store to order as line items.
+	 *
+	 * @throws RestException Exception when shipping is invalid.
+	 * @param \WC_Order   $order Object to prepare for the response.
+	 * @param RestRequest $request Full details about the request.
+	 */
+	protected function select_shipping_rates( \WC_Order $order, RestRequest $request ) {
+		$packages       = $this->get_shipping_packages( $order, $request );
+		$selected_rates = isset( $request['shipping_rates'] ) ? wp_list_pluck( $request['shipping_rates'], 'rate_id' ) : [];
+		$shipping_hash  = md5( wp_json_encode( $packages ) . wp_json_encode( $selected_rates ) );
+		$stored_hash    = WC()->session->get( 'store_api_shipping_hash' );
+
+		if ( $shipping_hash === $stored_hash ) {
+			return;
+		}
+
 		$order->remove_order_items( 'shipping' );
-		WC()->checkout->create_order_shipping_lines( $order, WC()->session->get( 'chosen_shipping_methods' ), WC()->shipping()->get_packages() );
 
-		// There is no way to know if taxes changed, so recreate it here.
-		$order->remove_order_items( 'tax' );
-		WC()->checkout->create_order_tax_lines( $order, WC()->cart );
+		foreach ( $packages as $package_key => $package ) {
+			$fallback_rate_id = current( array_keys( $package['rates'] ) );
+			$selected_rate_id = isset( $selected_rates[ $package_key ] ) ? $selected_rates[ $package_key ] : $fallback_rate_id;
+			$selected_rate    = isset( $package['rates'][ $selected_rate_id ] ) ? $package['rates'][ $selected_rate_id ] : false;
 
+			if ( ! $selected_rate ) {
+				throw new RestException(
+					'invalid-shipping-rate-id',
+					sprintf(
+						/* translators: 1: Rate ID, 2: list of valid ids */
+						__( '%1$s is not a valid shipping rate ID. Select one of the following: %2$s', 'woo-gutenberg-products-block' ),
+						$selected_rate_id,
+						implode( ', ', array_keys( $package['rates'] ) )
+					),
+					403
+				);
+			}
+
+			$item = new \WC_Order_Item_Shipping();
+			$item->set_props(
+				array(
+					'method_title' => $selected_rate->label,
+					'method_id'    => $selected_rate->method_id,
+					'instance_id'  => $selected_rate->instance_id,
+					'total'        => wc_format_decimal( $selected_rate->cost ),
+					'taxes'        => array(
+						'total' => $selected_rate->taxes,
+					),
+				)
+			);
+
+			foreach ( $selected_rate->get_meta_data() as $key => $value ) {
+				$item->add_meta_data( $key, $value, true );
+			}
+
+			/**
+			 * Action hook to adjust item before save.
+			 */
+			do_action( 'woocommerce_checkout_create_order_shipping_item', $item, $package_key, $package, $order );
+
+			// Add item to order and save.
+			$order->add_item( $item );
+		}
+	}
+
+	/**
+	 * Get packages with calculated shipping.
+	 *
+	 * Based on WC_Cart::get_shipping_packages but allows the destination to be
+	 * customised based on the address in the order.
+	 *
+	 * @param \WC_Order   $order Object to prepare for the response.
+	 * @param RestRequest $request Full details about the request.
+	 * @return array of cart items
+	 */
+	protected function get_shipping_packages( \WC_Order $order, RestRequest $request ) {
+		$packages = WC()->cart->get_shipping_packages();
+
+		foreach ( $packages as $key => $package ) {
+			$packages[ $key ]['destination'] = [
+				'address_1' => $order->get_shipping_address_1(),
+				'address_2' => $order->get_shipping_address_2(),
+				'city'      => $order->get_shipping_city(),
+				'state'     => $order->get_shipping_state(),
+				'postcode'  => $order->get_shipping_postcode(),
+				'country'   => $order->get_shipping_country(),
+			];
+		}
+
+		$packages = WC()->shipping()->calculate_shipping( $packages );
+
+		return $packages;
 	}
 
 	/**
