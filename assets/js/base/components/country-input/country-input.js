8da58f13fb8daadcb56d9d4b5932bf9dc12916e3
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-01-16 12:33:21 +0100

    Create ShippingCountryInput

---
+++ assets/js/base/components/country-input/country-input.js
@@ -0,0 +1,51 @@
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { CustomSelectControl } from 'wordpress-components';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+const CountryInput = ( {
+	className,
+	countries,
+	label,
+	onChange,
+	value = '',
+} ) => {
+	const options = Object.keys( countries ).map( ( key ) => ( {
+		key,
+		name: countries[ key ],
+	} ) );
+
+	return (
+		<CustomSelectControl
+			className={ classnames( 'wc-block-country-input', className, {
+				'is-active': value,
+			} ) }
+			label={ label }
+			options={ options }
+			onChange={ ( { selectedItem } ) => {
+				onChange( selectedItem.key );
+			} }
+			value={ {
+				key: value,
+				name: countries[ value ],
+			} }
+		/>
+	);
+};
+
+CountryInput.propTypes = {
+	countries: PropTypes.objectOf( PropTypes.string ).isRequired,
+	onChange: PropTypes.func.isRequired,
+	className: PropTypes.string,
+	label: PropTypes.string,
+	value: PropTypes.string,
+};
+
+export default CountryInput;
