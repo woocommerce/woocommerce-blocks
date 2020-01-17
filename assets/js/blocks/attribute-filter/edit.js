194e7e17cdc896d23a55cc2599fd8af9f5b6c8dd
Rua Haszard <rua.haszard@automattic.com>
2020-01-17 14:04:18 +1300

    fix issue when user re-selects same attr when setting up filter block: - exit early if there's no selected attr passed in

--- assets/js/blocks/attribute-filter/edit.js
+++ assets/js/blocks/attribute-filter/edit.js
@@ -218,6 +218,10 @@ const Edit = ( { attributes, setAttributes, debouncedSpeak } ) => {
 
 	const onChange = useCallback(
 		( selected ) => {
+			if ( ! selected || ! selected.length ) {
+				return;
+			}
+
 			const selectedId = selected[ 0 ].id;
 			const productAttribute = find( ATTRIBUTES, [
 				'attribute_id',
