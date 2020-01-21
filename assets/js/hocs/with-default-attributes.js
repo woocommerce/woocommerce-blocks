a667b69fc40cb7df06b7cbe3fa23fdc2bcf6f2bd
Mike Jolley <mike.jolley@me.com>
2020-01-21 14:10:11 +0000

    HOC to set default attribute values

---
+++ assets/js/hocs/with-default-attributes.js
@@ -0,0 +1,40 @@
+/**
+ * External dependencies
+ */
+import { createHigherOrderComponent } from '@wordpress/compose';
+const { getBlockType } = wp.blocks;
+
+/**
+ *  withDefaultAttributes
+ *
+ *  Fixes a bug where block attribute "default values" are not saved in serialized data.
+ *  Extend the default BlockListBlock component and apply default values allowing
+ *  Gutenberg to serialize the settings (normally ignored).
+ *
+ *  @see		https://github.com/WordPress/gutenberg/issues/7342#issuecomment-435371583
+ *
+ *  @param	object BlockListBlock The BlockListBlock element.
+ */
+const withDefaultAttributes = createHigherOrderComponent(
+	( BlockListBlock ) => {
+		return function( props ) {
+			const blockType = getBlockType( props.block.name );
+
+			Object.keys( blockType.attributes ).map( ( key ) => {
+				if (
+					props.attributes[ key ] === undefined &&
+					blockType.defaults !== undefined &&
+					blockType.defaults[ key ] !== undefined
+				) {
+					props.attributes[ key ] = blockType.defaults[ key ];
+				}
+				return key;
+			} );
+
+			return <BlockListBlock { ...props } />;
+		};
+	},
+	'withDefaultAttributes'
+);
+
+export default withDefaultAttributes;
