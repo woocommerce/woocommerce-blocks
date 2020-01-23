f71e1293fb6532f292ccd7b591ba36850d5515b7
Seghir Nadir <nadir.seghir@gmail.com>
2020-01-23 13:37:44 +0100

    move config to webpack-helper

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@
  */
 const path = require( 'path' );
 const { kebabCase } = require( 'lodash' );
-const { DefinePlugin } = require( 'webpack' );
 const { CleanWebpackPlugin } = require( 'clean-webpack-plugin' );
 const CreateFileWebpack = require( 'create-file-webpack' );
 const ProgressBarPlugin = require( 'progress-bar-webpack-plugin' );
@@ -76,13 +75,6 @@ const CoreConfig = {
 		],
 	},
 	plugins: [
-		new DefinePlugin( {
-			// Inject the `WOOCOMMERCE_BLOCKS_PHASE` global, used for feature flagging.
-			'process.env.WOOCOMMERCE_BLOCKS_PHASE': JSON.stringify(
-				process.env.npm_package_config_WOOCOMMERCE_BLOCKS_PHASE ||
-					'experimental'
-			),
-		} ),
 		new CleanWebpackPlugin(),
 		new ProgressBarPlugin( {
 			format:
