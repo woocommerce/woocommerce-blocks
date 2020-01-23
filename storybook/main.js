f263aed34fee9dcbdad417e05d1626d34dcc503a
Rua Haszard <rua.haszard@automattic.com>
2020-01-23 15:06:22 +1300

    demo story for one of our components (with no css/styles)

--- storybook/main.js
+++ storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
-	stories: [ '../stories/**/*.stories.js' ],
+	stories: [
+		// starter stories from storybook - we can probably delete these
+		'../stories/**/*.stories.js',
+		// WooCommerce Blocks stuff (anywhere in repo!)
+		'../assets/js/**/stories/*.js',
+	],
 	addons: [ '@storybook/addon-actions', '@storybook/addon-links' ],
 };
