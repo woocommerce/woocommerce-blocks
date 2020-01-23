5c49f8a43aeed453da845de461dc31cdd7f85c39
Darren Ethier <darren@roughsmootheng.in>
2020-01-23 10:18:16 -0500

    Avoid looping through attributes after defaults have been set.

--- assets/js/filters/block-list-block.js
+++ assets/js/filters/block-list-block.js
@@ -14,23 +14,7 @@ const { addFilter } = wp.hooks;
 const withDefaultAttributes = createHigherOrderComponent(
 	( BlockListBlock ) => {
 		class WrappedComponent extends Component {
-			constructor() {
-				super( ...arguments );
-
-				this.getAttributesWithDefaults = this.getAttributesWithDefaults.bind(
-					this
-				);
-			}
-
-			componentDidMount() {
-				const { block, setAttributes } = this.props;
-
-				if ( block.name.startsWith( 'woocommerce/' ) ) {
-					setAttributes( this.getAttributesWithDefaults() );
-				}
-			}
-
-			getAttributesWithDefaults() {
+			getAttributesWithDefaults = () => {
 				const blockType = getBlockType( this.props.block.name );
 				const attributes = Object.assign(
 					{},
@@ -42,6 +26,45 @@ const withDefaultAttributes = createHigherOrderComponent(
 					typeof blockType.attributes !== 'undefined' &&
 					typeof blockType.defaults !== 'undefined'
 				) {
+					Object.keys( blockType.attributes ).map( ( key ) => {
+						if (
+							typeof attributes[ key ] === 'undefined' &&
+							typeof blockType.defaults[ key ] !== 'undefined'
+						) {
+							attributes[ key ] = blockType.defaults[ key ];
+						}
+						return key;
+					} );
+				}
+
+				return attributes;
+			};
+
+			state = { mounted: false };
+
+			componentDidMount() {
+				const { block, setAttributes } = this.props;
+
+				if ( block.name.startsWith( 'woocommerce/' ) ) {
+					setAttributes( this.getAttributesWithDefaults() );
+				}
+				this.setState( { mounted: true } );
+			}
+
+			getAttributesWithDefaults() {
+				const blockType = getBlockType( this.props.block.name );
+				let attributes = this.props.attributes;
+
+				if (
+					! this.state.mounted &&
+					this.props.block.name.startsWith( 'woocommerce/' ) &&
+					typeof blockType.attributes !== 'undefined' &&
+					typeof blockType.defaults !== 'undefined'
+				) {
+					attributes = Object.assign(
+						{},
+						this.props.attributes || {}
+					);
 					Object.keys( blockType.attributes ).map( ( key ) => {
 						if (
 							typeof attributes[ key ] === 'undefined' &&
