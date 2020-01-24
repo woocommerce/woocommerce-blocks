fe6d6292161a62b1c7e4dad0ba2e61b40d359c58
Rua Haszard <rua.haszard@automattic.com>
2020-01-24 13:32:08 +1300

    piggy-back off main webpack config for storybook module.rules (for scss)

--- storybook/webpack.config.js
+++ storybook/webpack.config.js
@@ -1,50 +1,17 @@
+/**
+ * External dependencies
+ */
 const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
+/**
+ * Internal dependencies
+ */
+const { getMainConfig } = require( '../bin/webpack-helpers.js' );
+
 module.exports = ( { config } ) => {
-	// @todo find a way to better share webpack config betwen storybook and build
-	config.module.rules.push(
-		{
-			test: /\/node_modules\/.*?style\.s?css$/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				{ loader: 'css-loader', options: { importLoaders: 1 } },
-				'postcss-loader',
-				{
-					loader: 'sass-loader',
-					query: {
-						includePaths: [ 'node_modules' ],
-						data:
-							'@import "~@wordpress/base-styles/colors"; ' +
-							'@import "~@wordpress/base-styles/variables"; ' +
-							'@import "~@wordpress/base-styles/mixins"; ' +
-							'@import "~@wordpress/base-styles/breakpoints"; ' +
-							'@import "~@wordpress/base-styles/animations"; ' +
-							'@import "~@wordpress/base-styles/z-index"; ',
-					},
-				},
-			],
-		},
-		{
-			test: /\.s?css$/,
-			exclude: /node_modules/,
-			use: [
-				MiniCssExtractPlugin.loader,
-				{ loader: 'css-loader', options: { importLoaders: 1 } },
-				'postcss-loader',
-				{
-					loader: 'sass-loader',
-					query: {
-						includePaths: [ 'assets/css/abstracts' ],
-						data:
-							'@import "_colors"; ' +
-							'@import "_variables"; ' +
-							'@import "_breakpoints"; ' +
-							'@import "_mixins"; ',
-					},
-				},
-			],
-		}
-	);
+	const mainConfig = getMainConfig();
+
+	config.module.rules.push( ...mainConfig.module.rules );
 
 	config.plugins.push(
 		new MiniCssExtractPlugin( {
