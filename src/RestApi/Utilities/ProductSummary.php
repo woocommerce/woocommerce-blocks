7fe67d1a41a6189b7e2628a9dc7a23db9d143971
Mike Jolley <mike.jolley@me.com>
2020-01-27 11:51:58 +0000

    Tests

--- src/RestApi/Utilities/ProductSummary.php
+++ src/RestApi/Utilities/ProductSummary.php
@@ -36,38 +36,44 @@ class ProductSummary {
 	/**
 	 * Return the formatted summary.
 	 *
-	 * @param int $allowed_length Maximum allowed characters for summary.
+	 * @param int $max_words Word limit for summary.
 	 * @return string
 	 */
-	public function get_summary( $allowed_length ) {
+	public function get_summary( $max_words = 25 ) {
 		$summary = $this->content;
 
-		if ( $this->get_length( $summary ) > $allowed_length ) {
-			$summary = $this->get_first_paragraph( $summary );
+		if ( $max_words && $this->get_word_count( $summary ) > $max_words ) {
+			$summary = $this->generate_summary( $summary, $max_words );
 		}
 
 		return \wc_format_content( $summary );
 	}
 
 	/**
-	 * Get the character length of some content, ignoring HTML.
+	 * Get the word count.
 	 *
 	 * @param string $content HTML Content.
 	 * @return int Length
 	 */
-	protected function get_length( $content ) {
-		return \strlen( \wp_strip_all_tags( $content ) );
+	protected function get_word_count( $content ) {
+		return \str_word_count( \wp_strip_all_tags( $content ) );
 	}
 
 	/**
 	 * Get the first paragraph, or a short excerpt, from some content.
 	 *
 	 * @param string $content HTML Content.
+	 * @param int    $max_words Maximum allowed words for summary.
 	 * @return string
 	 */
-	protected function get_first_paragraph( $content ) {
+	protected function generate_summary( $content, $max_words ) {
 		$content_p = \wpautop( $content );
+		$paragraph = \strstr( $content_p, '</p>' ) ? \substr( $content_p, 0, \strpos( $content_p, '</p>' ) + 4 ) : $content_p;
 
-		return \strstr( $content_p, '</p>' ) ? \substr( $content_p, 0, \strpos( $content_p, '</p>' ) + 4 ) : \wc_trim_string( $content, 150 );
+		if ( $this->get_word_count( $paragraph ) > $max_words ) {
+			return \wp_trim_words( $content, $max_words );
+		}
+
+		return $paragraph;
 	}
 }
