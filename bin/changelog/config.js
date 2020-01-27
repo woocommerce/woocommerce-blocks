1a8c79a3f4ed920203b4c979e3e77ae883ff95d3
Darren Ethier <darren@roughsmootheng.in>
2020-01-27 14:30:41 -0500

    rework config so it merges configs from multiple sources

--- bin/changelog/config.js
+++ bin/changelog/config.js
@@ -1,6 +1,14 @@
 'use strict';
 
 const pkg = require( '../../package.json' );
+const Config = require( 'merge-config' );
+
+const config = new Config();
+
+const changelogSrcTypes = {
+	MILESTONE: 'MILESTONE',
+	ZENHUB: 'ZENHUB_RELEASE',
+};
 
 const REPO = pkg.repository.url
 	// remove https://github.com:
@@ -8,16 +16,37 @@ const REPO = pkg.repository.url
 	// remove the .git ending.
 	.slice( 0, -4 );
 
-if ( pkg.changelog === undefined ) {
-	pkg.changelog = {
-		labelPrefix: 'type:',
-		skipLabel: 'no-changelog',
-		defaultPrefix: 'dev',
-		zenhub: false,
-	};
-}
+const DEFAULTS = {
+	labelPrefix: 'type:',
+	skipLabel: 'no-changelog',
+	defaultPrefix: 'dev',
+	changelogSrcType: changelogSrcTypes.MILESTONE,
+	devNoteLabel: 'dev-note',
+	ghApiToken: '',
+	zhApiKey: '',
+};
+
+pkg.changelog = pkg.changelog || DEFAULTS;
+
+config.merge( { ...DEFAULTS, ...pkg.changelog } );
+config.env( [
+	'LABEL_PREFIX',
+	'SKIP_LABEL',
+	'DEFAULT_PREFIX',
+	'DEV_NOTE_LABEL',
+	'CHANGELOG_SRC_TYPE',
+	'GH_API_TOKEN',
+	'ZH_API_KEY',
+] );
+config.argv( Object.keys( pkg.changelog ) );
+
+//merge in command-line arguments
 
 module.exports = {
-	pkg,
+	pkg: {
+		...pkg,
+		changelog: config.get(),
+	},
 	REPO,
+	changelogSrcTypes,
 };
