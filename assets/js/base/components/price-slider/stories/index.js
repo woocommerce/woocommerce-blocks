482fc9923430f0bbbc9e06012bbdda13be5200cc
Rua Haszard <rua.haszard@automattic.com>
2020-01-27 15:42:27 +1300

    fix PriceSlider user interaction: - PriceSlider expects client to handle onChange and pass in new min/max

--- assets/js/base/components/price-slider/stories/index.js
+++ assets/js/base/components/price-slider/stories/index.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import { React, useState } from 'react';
 
 /**
  * Internal dependencies
@@ -13,23 +13,32 @@ export default {
 	component: PriceSlider,
 };
 
-export const Default = () => (
-	<div>
-		<PriceSlider
-			onChange={ () => {} }
-			minConstraint={ 0 }
-			maxConstraint={ 1000 }
-			minPrice={ 0 }
-			maxPrice={ 1000 }
-			currency={ {
-				code: 'nzd',
-				symbol: '$',
-				thousandSeparator: ' ',
-				decimalSeparator: '.',
-				minorUnit: 2,
-				prefix: '$',
-				suffix: '',
-			} }
-		/>
-	</div>
-);
+export const Default = () => {
+	// PriceSlider expects client to update min & max price
+	const [ min, setMin ] = useState( 1000 );
+	const [ max, setMax ] = useState( 5000 );
+	return (
+		<div>
+			<PriceSlider
+				minPrice={ min }
+				maxPrice={ max }
+				onChange={ ( values ) => {
+					setMin( values[ 0 ] );
+					setMax( values[ 1 ] );
+				} }
+				minConstraint={ 1000 }
+				maxConstraint={ 5000 }
+				step={ 250 }
+				currency={ {
+					code: 'nzd',
+					symbol: '$',
+					thousandSeparator: ' ',
+					decimalSeparator: '.',
+					minorUnit: 2,
+					prefix: '$',
+					suffix: '',
+				} }
+			/>
+		</div>
+	);
+};
