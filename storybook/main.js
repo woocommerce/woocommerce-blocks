8d584dafd2abf976156ed32419712e048192b9ba
Rua Haszard <rua.haszard@automattic.com>
2020-01-30 14:12:58 +1300

    add common storybook addons (used by Gutenberg storybook)

--- storybook/main.js
+++ storybook/main.js
@@ -3,5 +3,16 @@ module.exports = {
 		// WooCommerce Blocks stuff (anywhere in repo!)
 		'../assets/js/**/stories/*.js',
 	],
-	addons: [ '@storybook/addon-actions', '@storybook/addon-links' ],
+	addons: [
+		{
+			name: '@storybook/addon-docs',
+			options: { configureJSX: true },
+		},
+		'@storybook/addon-knobs',
+		'@storybook/addon-storysource',
+		'@storybook/addon-viewport',
+		'@storybook/addon-a11y',
+		'@storybook/addon-actions',
+		'@storybook/addon-links',
+	],
 };
