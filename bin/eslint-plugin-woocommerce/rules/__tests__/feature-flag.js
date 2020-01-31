540761e314be94bc9ee5a94cc66ba3a0287ca24d
Seghir Nadir <nadir.seghir@gmail.com>
2020-01-31 19:18:28 +0100

    add eslint rule

---
+++ bin/eslint-plugin-woocommerce/rules/__tests__/feature-flag.js
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import { RuleTester } from 'eslint';
+
+/**
+ * Internal dependencies
+ */
+import rule from '../feature-flag';
+
+const ruleTester = new RuleTester( {
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 6,
+	},
+} );
+
+ruleTester.run( 'feature-flag', rule, {
+	valid: [
+		{
+			code: `
+if ( process.env.WOOCOMMERCE_BLOCKS_PHASE === 'experimental' ) {
+	registerBlockType( 'woocommerce/checkout', settings );
+}`,
+		},
+	],
+	invalid: [
+		{
+			code: `
+if ( WOOCOMMERCE_BLOCKS_PHASE === 'experimental' ) {
+	registerBlockType( 'woocommerce/checkout', settings );
+}`,
+			errors: [
+				{
+					message:
+						'The `WOOCOMMERCE_BLOCKS_PHASE` constant should be accessed using `process.env.WOOCOMMERCE_BLOCKS_PHASE`.',
+				},
+			],
+		},
+	],
+} );
