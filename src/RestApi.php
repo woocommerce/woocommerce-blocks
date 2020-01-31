a389601a2338cdbf578f8856d5152d4575b45d81
Darren Ethier <darren@roughsmootheng.in>
2020-01-31 13:41:52 -0500

    fix invalid filter usage and include WP_Error check

--- src/RestApi.php
+++ src/RestApi.php
@@ -19,7 +19,7 @@ class RestApi {
 	 */
 	public static function init() {
 		add_action( 'rest_api_init', array( __CLASS__, 'register_rest_routes' ), 10 );
-		add_filter( 'rest_authentication_errors', array( __CLASS__, 'maybe_init_cart_session' ), 1, 2 );
+		add_filter( 'rest_authentication_errors', array( __CLASS__, 'maybe_init_cart_session' ), 1 );
 	}
 
 	/**
@@ -61,12 +61,13 @@ class RestApi {
 	 *
 	 * @todo check compat < WC 3.6. Make specific to cart endpoint.
 	 * @param mixed $return Value being filtered.
-	 * @param array $request Request data.
 	 * @return mixed
 	 */
-	public static function maybe_init_cart_session( $return, $request = false ) {
+	public static function maybe_init_cart_session( $return ) {
 		$wc_instance = wc();
-		if ( ! $wc_instance instanceof \WooCommerce ) {
+		// if WooCommerce instance isn't available or already have an
+		// authentication error, just return.
+		if ( ! $wc_instance instanceof \WooCommerce || \is_wp_error( $return ) ) {
 			return $return;
 		}
 		$wc_instance->frontend_includes();
