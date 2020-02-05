27e5da59f6fb83fe5a5652fd0bf47504fd6b70d6
Mike Jolley <mike.jolley@me.com>
2020-02-05 11:58:34 -0800

    Add snapshop

--- tests/e2e-tests/specs/backend/product-search.test.js
+++ tests/e2e-tests/specs/backend/product-search.test.js
@@ -7,6 +7,7 @@ import {
 	createNewPost,
 	switchUserToAdmin,
 } from '@wordpress/e2e-test-utils';
+import { clearAndFillInput } from '@woocommerce/e2e-tests/utils';
 
 describe( 'Product Search', () => {
 	beforeEach( async () => {
@@ -36,9 +37,21 @@ describe( 'Product Search', () => {
 			'textarea.wc-block-product-search__label',
 			'I am a new label'
 		);
+
 		await expect( page ).toFill(
 			'textarea.wc-block-product-search__field',
 			'I am a new placeholder'
 		);
+
+		await clearAndFillInput(
+			'textarea.wc-block-product-search__label',
+			'The Label'
+		);
+		await clearAndFillInput(
+			'textarea.wc-block-product-search__field',
+			'The Placeholder'
+		);
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
 } );
