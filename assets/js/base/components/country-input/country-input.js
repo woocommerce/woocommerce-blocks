c48dea2c845e2ca92e66d5ab61d4c71b8826b5b6
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-02-13 10:55:45 +0100

    Split CountryInput and Select

--- assets/js/base/components/country-input/country-input.js
+++ assets/js/base/components/country-input/country-input.js
@@ -2,14 +2,12 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
-import { CustomSelectControl } from 'wordpress-components';
 import { decodeEntities } from '@wordpress/html-entities';
 
 /**
  * Internal dependencies
  */
-import './style.scss';
+import Select from '../select';
 
 const CountryInput = ( {
 	className,
@@ -22,21 +20,20 @@ const CountryInput = ( {
 		key,
 		name: decodeEntities( countries[ key ] ),
 	} ) );
-
-	return (
-		<CustomSelectControl
-			className={ classnames( 'wc-block-country-input', className, {
-				'is-active': value,
-			} ) }
-			label={ label }
-			options={ options }
-			onChange={ ( { selectedItem } ) => {
-				onChange( selectedItem.key );
-			} }
-			value={ {
+	const formattedValue = value
+		? {
 				key: value,
 				name: decodeEntities( countries[ value ] ),
-			} }
+		  }
+		: null;
+
+	return (
+		<Select
+			className={ className }
+			label={ label }
+			onChange={ onChange }
+			options={ options }
+			value={ formattedValue }
 		/>
 	);
 };
