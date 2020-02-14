ca25bb01f95c97eb0749359b1ba2cdd1b46d9234
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-02-14 16:07:51 +0100

    Fix city value not being set

--- assets/js/base/components/shipping-calculator/address.js
+++ assets/js/base/components/shipping-calculator/address.js
@@ -54,7 +54,7 @@ const ShippingCalculatorAddress = ( { address: initialAddress, onUpdate } ) => {
 				onChange={ ( newValue ) =>
 					setAddress( {
 						...address,
-						country: newValue,
+						city: newValue,
 					} )
 				}
 			/>
