39bf43a21c2292ad38b5531bb0124fb9179ca98c
Rua Haszard <rua.haszard@automattic.com>
2020-02-18 11:56:57 +1300

    render empty cart inner blocks on front end when shopper cart is empty

--- assets/js/blocks/cart-checkout/cart/frontend.js
+++ assets/js/blocks/cart-checkout/cart/frontend.js
@@ -3,18 +3,18 @@
  */
 import { withRestApiHydration } from '@woocommerce/block-hocs';
 import { useCollection } from '@woocommerce/base-hooks';
+import { RawHTML } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import FullCart from './full-cart';
-import EmptyCart from './empty-cart';
 import renderFrontend from '../../../utils/render-frontend.js';
 
 /**
  * Wrapper component to supply API data and show empty cart view as needed.
  */
-const CartFrontend = () => {
+const CartFrontend = ( { emptyCart } ) => {
 	const { results: cartData, isLoading } = useCollection( {
 		namespace: '/wc/store',
 		resourceName: 'cart',
@@ -25,31 +25,18 @@ const CartFrontend = () => {
 
 	// @todo render cart loading state e.g. animated spinner
 	return isCartEmpty ? (
-		<EmptyCart />
+		<RawHTML>{ emptyCart }</RawHTML>
 	) : (
 		<FullCart cartItems={ cartItems } cartTotals={ cartData.totals } />
 	);
 };
 
-const isCartEmpty = false; // @todo check if the cart has some products
-const selector = '.wp-block-woocommerce-cart';
+const getProps = ( el ) => ( {
+	emptyCart: el.innerHTML,
+} );
 
-if ( ! isCartEmpty ) {
-	const getProps = () => {
-		return {
-			attributes: {},
-		};
-	};
-
-	renderFrontend( selector, withRestApiHydration( CartFrontend ), getProps );
-} else {
-	// @todo Should this move to a side effect of CartFrontend component?
-	const containers = document.querySelectorAll( selector );
-
-	if ( containers.length ) {
-		// Use Array.forEach for IE11 compatibility.
-		Array.prototype.forEach.call( containers, ( el ) => {
-			el.classList.remove( 'is-loading' );
-		} );
-	}
-}
+renderFrontend(
+	'.wp-block-woocommerce-cart',
+	withRestApiHydration( CartFrontend ),
+	getProps
+);
