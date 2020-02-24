3bc4bdd6cd55fa9e6d78ad72a7e4663eec438373
Mike Jolley <mike.jolley@me.com>
2020-02-24 14:59:23 +0000

    Remove custom controls definition

--- assets/js/data/cart/index.js
+++ assets/js/data/cart/index.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { registerStore } from '@wordpress/data';
-import { controls as dataControls } from '@wordpress/data-controls';
 
 /**
  * Internal dependencies
@@ -12,12 +11,10 @@ import * as selectors from './selectors';
 import * as actions from './actions';
 import * as resolvers from './resolvers';
 import reducer from './reducers';
-import { controls } from '../collections/controls';
 
 registerStore( STORE_KEY, {
 	reducer,
 	actions,
-	controls: { ...dataControls, ...controls },
 	selectors,
 	resolvers,
 } );
