84c7a71f4ecab5e518a525dccff6cf8c048db3c0
Rua Haszard <rua.haszard@automattic.com>
2020-02-26 10:04:56 +1300

    add basic tests for checkout block: - can add & matches snapshot - can only add one block

--- tests/e2e-tests/specs/backend/checkout.test.js
+++ tests/e2e-tests/specs/backend/checkout.test.js
@@ -9,22 +9,22 @@ import {
 	switchUserToAdmin,
 } from '@wordpress/e2e-test-utils';
 
-describe( 'Cart Block', () => {
+describe( 'Checkout Block', () => {
 	beforeEach( async () => {
 		await switchUserToAdmin();
 		await createNewPost();
 	} );
 
 	it( 'can be created', async () => {
-		await insertBlock( 'Cart' );
+		await insertBlock( 'Checkout' );
 		expect( await getEditedPostContent() ).toMatchSnapshot();
 	} );
 
 	it( 'can only be inserted once', async () => {
-		await insertBlock( 'Cart' );
+		await insertBlock( 'Checkout' );
 		expect( await getAllBlocks() ).toHaveLength( 1 );
 
-		await insertBlock( 'Cart' );
+		await insertBlock( 'Checkout' );
 		expect( await getAllBlocks() ).toHaveLength( 1 );
 	} );
 } );
