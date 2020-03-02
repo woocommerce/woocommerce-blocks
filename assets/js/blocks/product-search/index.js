ebc61dfe02eab983e033d0e8b7e64fc122cde0d5
Albert Juhé Lluveras <aljullu@gmail.com>
2020-03-02 14:46:56 +0100

    Fix Product Search block in last Gutenberg release (#1841)
    * Fix Product search block in last Gutenberg release
    
    * Convert ProductSearchBlock to a functional component
    
    * Split ProductSearchBlock into two components

--- assets/js/blocks/product-search/index.js
+++ assets/js/blocks/product-search/index.js
@@ -3,9 +3,6 @@
  */
 import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
-import { InspectorControls } from '@wordpress/editor';
-import { PanelBody, ToggleControl } from '@wordpress/components';
-import { Fragment } from '@wordpress/element';
 import { IconProductSearch } from '@woocommerce/block-components/icons';
 
 /**
@@ -14,6 +11,7 @@ import { IconProductSearch } from '@woocommerce/block-components/icons';
 import './style.scss';
 import './editor.scss';
 import Block from './block.js';
+import edit from './edit.js';
 
 registerBlockType( 'woocommerce/product-search', {
 	title: __( 'Product Search', 'woo-gutenberg-products-block' ),
@@ -74,49 +72,7 @@ registerBlockType( 'woocommerce/product-search', {
 		},
 	},
 
-	/**
-	 * Renders and manages the block.
-	 */
-	edit( props ) {
-		const { attributes, setAttributes } = props;
-		const { hasLabel } = attributes;
-		return (
-			<Fragment>
-				<InspectorControls key="inspector">
-					<PanelBody
-						title={ __(
-							'Content',
-							'woo-gutenberg-products-block'
-						) }
-						initialOpen
-					>
-						<ToggleControl
-							label={ __(
-								'Show search field label',
-								'woo-gutenberg-products-block'
-							) }
-							help={
-								hasLabel
-									? __(
-											'Label is visible.',
-											'woo-gutenberg-products-block'
-									  )
-									: __(
-											'Label is hidden.',
-											'woo-gutenberg-products-block'
-									  )
-							}
-							checked={ hasLabel }
-							onChange={ () =>
-								setAttributes( { hasLabel: ! hasLabel } )
-							}
-						/>
-					</PanelBody>
-				</InspectorControls>
-				<Block { ...props } isEditor={ true } />
-			</Fragment>
-		);
-	},
+	edit,
 
 	/**
 	 * Save the props to post content.
