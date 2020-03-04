0ae5433d44c66b00af5ccda6283ae6be089d5e12
Seghir Nadir <nadir.seghir@gmail.com>
2020-03-04 18:28:39 +0100

    move selecting shipping rate logic to hook

--- assets/js/base/components/shipping-rates-control/packages.js
+++ assets/js/base/components/shipping-rates-control/packages.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
-import { useState } from '@wordpress/element';
 import { useSelectShippingRate } from '@woocommerce/base-hooks';
 
 /**
@@ -17,14 +16,8 @@ const Packages = ( {
 	renderOption,
 	shippingRates = [],
 } ) => {
-	const selectShippingRate = useSelectShippingRate();
-	const initiallySelectedRates = shippingRates.map(
-		// the API responds with those keys.
-		// eslint-disable-next-line camelcase
-		( p ) => p.shipping_rates.find( ( rate ) => rate.selected )?.rate_id
-	);
-	const [ selectedShipping, setSelectedShipping ] = useState(
-		initiallySelectedRates
+	const { selectShippingRate, selectedShippingRates } = useSelectShippingRate(
+		shippingRates
 	);
 	return shippingRates.map( ( shippingRate, i ) => (
 		<Package
@@ -32,11 +25,10 @@ const Packages = ( {
 			className={ className }
 			noResultsMessage={ noResultsMessage }
 			onChange={ ( newShippingRate ) => {
-				setSelectedShipping( [ newShippingRate ] );
 				selectShippingRate( newShippingRate, i );
 			} }
 			renderOption={ renderOption }
-			selected={ selectedShipping[ i ] }
+			selected={ selectedShippingRates[ i ] }
 			shippingRate={ shippingRate }
 			showItems={ shippingRates.length > 1 }
 			title={ shippingRates.length > 1 ? shippingRate.name : null }
