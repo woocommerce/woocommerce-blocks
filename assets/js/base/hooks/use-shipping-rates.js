46660b8384f532545cdc68661365806e0a53130c
Seghir Nadir <nadir.seghir@gmail.com>
2020-03-06 14:16:34 +0100

    refactor fields

--- assets/js/base/hooks/use-shipping-rates.js
+++ assets/js/base/hooks/use-shipping-rates.js
@@ -12,18 +12,18 @@ import { CART_STORE_KEY as storeKey } from '@woocommerce/block-data';
 import { useStoreCart } from './use-store-cart';
 import { pluckAddress } from '../utils';
 /**
- * This is a custom hook that is wired up to the `wc/store/collections` data
-store for the `wc/store/cart/shipping-rates` route. Given a query object, this
-will ensure a component is kept up to date with the shipping rates matching that
-query in the store state.
+ * This is a custom hook that is wired up to the `wc/store/cart/shipping-rates` route.
+ * Given a a set of default fields keys, this will handle shipping form state and load
+ * new rates when certain fields change.
+ *
+ * @param addressFields an object containing default fields keys.
  *
  * @return {Object} This hook will return an object with three properties:
- * - {Boolean} shippingRatesLoading A boolean indicating whether the shipping
- * rates are still loading or not.
- * - {Function} setShippingAddress  An function that optimistically update shipping address and
- * dispatches async rate fetching.
- * - {Object} shippingAddress       An object containing shipping address.
- * @param addressFields
+ *                 - {Boolean} shippingRatesLoading A boolean indicating whether the shipping
+ *                   rates are still loading or not.
+ *                 - {Function} setShippingAddress  An function that optimistically
+ *                   update shipping address and dispatches async rate fetching.
+ *                 - {Object} shippingAddress       An object containing shipping address.
  */
 export const useShippingRates = ( addressFields ) => {
 	const { shippingRates } = useStoreCart();
