19b56267a2f549d4a710f9308b466c0d10257206
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-03-09 12:40:34 +0100

    Add comma before 'etc.' in address 2 label

--- assets/js/blocks/cart-checkout/checkout/edit.js
+++ assets/js/blocks/cart-checkout/checkout/edit.js
@@ -81,7 +81,7 @@ const CheckoutEditor = ( { attributes, setAttributes } ) => {
 					) }
 					<ToggleControl
 						label={ __(
-							'Apartment, suite, unit etc',
+							'Apartment, suite, unit, etc',
 							'woo-gutenberg-products-block'
 						) }
 						checked={ showAddress2Field }
