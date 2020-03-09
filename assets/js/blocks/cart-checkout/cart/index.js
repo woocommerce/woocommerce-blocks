05505f67ef23001ffd29b2b7bfb9cb6b0f53e730
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-03-09 17:28:46 +0100

    Simplify Cart edit function

--- assets/js/blocks/cart-checkout/cart/index.js
+++ assets/js/blocks/cart-checkout/cart/index.js
@@ -14,7 +14,7 @@ import {
 /**
  * Internal dependencies
  */
-import Editor from './edit';
+import edit from './edit';
 import { example } from './example';
 import './style.scss';
 
@@ -47,14 +47,7 @@ const settings = {
 		},
 	},
 
-	/**
-	 * Renders the edit view for a block.
-	 *
-	 * @param {Object} props Props to pass to block.
-	 */
-	edit( props ) {
-		return <Editor { ...props } />;
-	},
+	edit,
 
 	/**
 	 * Save the props to post content.
