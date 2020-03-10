e4a79bc49973e5dd593325ae6b0c116c77c1c443
Mike Jolley <mike.jolley@me.com>
2020-03-10 14:06:05 +0000

    Use checkout context

--- assets/js/base/components/checkout/place-order-button/index.js
+++ assets/js/base/components/checkout/place-order-button/index.js
@@ -3,11 +3,18 @@
  */
 import { __ } from '@wordpress/i18n';
 import Button from '@woocommerce/base-components/button';
+import { useCheckoutContext } from '@woocommerce/base-context/checkout-context';
 
 const PlaceOrderButton = () => {
+	const { placeOrderLabel, placeOrderAction } = useCheckoutContext();
+
 	return (
-		<Button className="wc-block-components-checkout-place-order-button">
-			{ __( 'Place order', 'woo-gutenberg-products-block' ) }
+		<Button
+			className="wc-block-components-checkout-place-order-button"
+			onClick={ placeOrderAction }
+		>
+			{ placeOrderLabel ||
+				__( 'Place order', 'woo-gutenberg-products-block' ) }
 		</Button>
 	);
 };
