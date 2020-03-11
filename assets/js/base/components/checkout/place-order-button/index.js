0286ae9107bb9e7ac1decd8580de616b1d8874f6
Mike Jolley <mike.jolley@me.com>
2020-03-11 14:46:28 +0000

    Update context

--- assets/js/base/components/checkout/place-order-button/index.js
+++ assets/js/base/components/checkout/place-order-button/index.js
@@ -3,17 +3,17 @@
  */
 import { __ } from '@wordpress/i18n';
 import Button from '@woocommerce/base-components/button';
-import { useCheckoutContext } from '@woocommerce/base-context/checkout-context';
+import { useCheckoutContext } from '@woocommerce/base-context';
 
 const PlaceOrderButton = () => {
-	const { placeOrderLabel, placeOrderAction } = useCheckoutContext();
+	const { submitLabel, onSubmit } = useCheckoutContext();
 
 	return (
 		<Button
 			className="wc-block-components-checkout-place-order-button"
-			onClick={ placeOrderAction }
+			onClick={ onSubmit }
 		>
-			{ placeOrderLabel ||
+			{ submitLabel ||
 				__( 'Place order', 'woo-gutenberg-products-block' ) }
 		</Button>
 	);
