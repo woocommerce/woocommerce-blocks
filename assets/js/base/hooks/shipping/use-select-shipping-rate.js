062f6a4af7e128f80d8c0f227cf50d59642a8093
Seghir Nadir <nadir.seghir@gmail.com>
2020-03-11 15:34:22 +0100

    update docs

--- assets/js/base/hooks/shipping/use-select-shipping-rate.js
+++ assets/js/base/hooks/shipping/use-select-shipping-rate.js
@@ -13,20 +13,19 @@ See also: https://github.com/woocommerce/woocommerce-gutenberg-products-block/tr
  * @return {Object} This hook will return an object with two properties:
  * - selectShippingRate    A function that immediately returns the selected
  * rate and dispatches an action generator.
- * - selectedShippingRates An array of selected shipping rates, maintained
+ * - selectedShippingRates An object of selected shipping rates and package id as key, maintained
  * locally by a state and updated optimistically.
  */
 export const useSelectShippingRate = ( shippingRates ) => {
 	const initiallySelectedRates = Object.fromEntries(
 		shippingRates.map(
 			// the API responds with those keys.
+			/* eslint-disable camelcase */
 			( p ) => [
-				/* eslint-disable camelcase */
 				p.package_id,
-				// eslint-disable-next-line camelcase
 				p.shipping_rates.find( ( rate ) => rate.selected )?.rate_id,
-				/* eslint-enable */
 			]
+			/* eslint-enable */
 		)
 	);
 	const [ selectedShippingRates, setSelectedShipping ] = useState(
