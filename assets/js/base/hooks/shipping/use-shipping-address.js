5ae3801de32d1782439a82595bf9744b2ac1a9d8
Darren Ethier <darren@roughsmootheng.in>
2020-03-13 07:10:55 -0400

    restore pluck comparison to effect.
    Also added some clarification docs for why `iniitalAddress` is not included in the effect dependencies.

--- assets/js/base/hooks/shipping/use-shipping-address.js
+++ assets/js/base/hooks/shipping/use-shipping-address.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { useDispatch } from '@wordpress/data';
-import { useEffect, useReducer } from '@wordpress/element';
+import { useEffect, useState } from '@wordpress/element';
 import isShallowEqual from '@wordpress/is-shallow-equal';
 import { useDebounce } from 'use-debounce';
 import { CART_STORE_KEY as storeKey } from '@woocommerce/block-data';
@@ -13,22 +13,22 @@ import { CART_STORE_KEY as storeKey } from '@woocommerce/block-data';
 import { useStoreCart } from '../cart/use-store-cart';
 import { pluckAddress } from '../../utils';
 
-const reducer = ( state, address ) =>
-	isShallowEqual( pluckAddress( address ), pluckAddress( state ) )
-		? state
-		: { ...state, ...address };
+const shouldUpdateStore = ( oldAddress, newAddress ) =>
+	! isShallowEqual( pluckAddress( oldAddress ), pluckAddress( newAddress ) );
 
 export const useShippingAddress = () => {
 	const { shippingAddress: initialAddress } = useStoreCart();
-	const [ shippingAddress, setShippingAddress ] = useReducer(
-		reducer,
-		initialAddress
-	);
+	const [ shippingAddress, setShippingAddress ] = useState( initialAddress );
 	const [ debouncedShippingAddress ] = useDebounce( shippingAddress, 400 );
 	const { updateShippingAddress } = useDispatch( storeKey );
 
+	// Note, we're intentionally not using initialAddress as a dependency here
+	// so that the stale (previous) value is being used for comparison.
 	useEffect( () => {
-		if ( debouncedShippingAddress.country ) {
+		if (
+			debouncedShippingAddress.country &&
+			shouldUpdateStore( initialAddress, debouncedShippingAddress )
+		) {
 			updateShippingAddress( debouncedShippingAddress );
 		}
 	}, [ debouncedShippingAddress ] );
