b430d6811f88d58e617bbe0a664d991f684fc443
Darren Ethier <darren@roughsmootheng.in>
2020-03-13 23:24:00 -0400

    tweak ValidationInputError so that it can receive property name for getting error from

--- assets/js/base/components/validation/validation-input-error.js
+++ assets/js/base/components/validation/validation-input-error.js
@@ -1,15 +1,31 @@
 /**
+ * External dependencies
+ */
+import { useValidationContext } from '@woocommerce/base-context';
+import PropTypes from 'prop-types';
+
+/**
  * Internal dependencies
  */
 import './style.scss';
 
-export const ValidationInputError = ( { errorMessage = '' } ) => {
-	if ( ! errorMessage ) {
+export const ValidationInputError = ( {
+	errorMessage = '',
+	propertyName = '',
+} ) => {
+	const { getValidationError } = useValidationContext();
+	if ( ! errorMessage && ! propertyName ) {
 		return null;
 	}
+	errorMessage = errorMessage || getValidationError( propertyName );
 	return (
 		<div className="wc-block-form-input-validation-error" role="alert">
 			{ errorMessage }
 		</div>
 	);
+};
+
+ValidationInputError.propTypes = {
+	errorMessage: PropTypes.string,
+	propertyName: PropTypes.string,
 };
