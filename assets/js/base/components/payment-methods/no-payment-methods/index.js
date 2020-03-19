d5c76400548e5344791a46abd67619cba05b9a2d
Mike Jolley <mike.jolley@me.com>
2020-03-19 14:22:30 +0000

    frontend notices

--- assets/js/base/components/payment-methods/no-payment-methods/index.js
+++ assets/js/base/components/payment-methods/no-payment-methods/index.js
@@ -2,10 +2,11 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Placeholder, Button } from 'wordpress-components';
+import { Placeholder, Button, Notice } from 'wordpress-components';
 import { Icon, card } from '@woocommerce/icons';
 import { ADMIN_URL } from '@woocommerce/settings';
 import { useCheckoutContext } from '@woocommerce/base-context';
+import classnames from 'classnames';
 
 /**
  * Internal dependencies
@@ -49,7 +50,21 @@ const NoPaymentMethodsPlaceholder = () => {
 };
 
 const NoPaymentMethodsNotice = () => {
-	return <div>Hello</div>;
+	return (
+		<Notice
+			isDismissible={ false }
+			className={ classnames(
+				'wc-block-checkout__no-payment-methods-notice',
+				'woocommerce-message',
+				'woocommerce-error'
+			) }
+		>
+			{ __(
+				'There are no payment methods available. This may be an error on our side. Please contact us if you need any help placing your order.',
+				'woo-gutenberg-products-block'
+			) }
+		</Notice>
+	);
 };
 
 export default NoPaymentMethods;
