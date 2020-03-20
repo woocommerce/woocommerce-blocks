5eddcbc128001a05cde58973009c980750a0074b
Mike Jolley <mike.jolley@me.com>
2020-03-20 22:18:08 +0000

    Add container to run php unit tests in docker

--- bin/docker/wordpress-unit-tests/install-wp-tests-docker.sh
+++ bin/docker/wordpress-unit-tests/install-wp-tests-docker.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-if [ $# -lt 3 ]; then
-	echo "usage: $0 <db-name> <db-user> <db-pass> [db-host] [wp-version] [skip-database-creation]"
-	exit 1
-fi
+WP_VERSION=${1-latest}
 
-DB_NAME=$1
-DB_USER=$2
-DB_PASS=$3
-DB_HOST=${4-localhost}
-WP_VERSION=${5-latest}
-SKIP_DB_CREATE=${6-false}
+# Hardcoded to match docker-compose.
+DB_NAME=wordpress
+DB_USER=wordpress
+DB_PASS=wordpress
+DB_HOST=db
 
 # directories
 TMPDIR=${TMPDIR-/tmp}
@@ -122,40 +118,5 @@ install_test_suite() {
 
 }
 
-install_db() {
-
-	if [ ${SKIP_DB_CREATE} = "true" ]; then
-		return 0
-	fi
-
-	# parse DB_HOST for port or socket references
-	local PARTS=(${DB_HOST//\:/ })
-	local DB_HOSTNAME=${PARTS[0]};
-	local DB_SOCK_OR_PORT=${PARTS[1]};
-	local EXTRA=""
-
-	if ! [ -z $DB_HOSTNAME ] ; then
-		if [ $(echo $DB_SOCK_OR_PORT | grep -e '^[0-9]\{1,\}$') ]; then
-			EXTRA=" --host=$DB_HOSTNAME --port=$DB_SOCK_OR_PORT --protocol=tcp"
-		elif ! [ -z $DB_SOCK_OR_PORT ] ; then
-			EXTRA=" --socket=$DB_SOCK_OR_PORT"
-		elif ! [ -z $DB_HOSTNAME ] ; then
-			EXTRA=" --host=$DB_HOSTNAME --protocol=tcp"
-		fi
-	fi
-
-	# create database
-	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
-}
-
-install_deps() {
-	if [ -d "$WP_CORE_DIR/wp-content/plugins/woocommerce" ]; then
-		download https://downloads.wordpress.org/plugin/woocommerce.zip $WP_CORE_DIR/wp-content/plugins/woocommerce.zip
-		unzip $WP_CORE_DIR/wp-content/plugins/woocommerce.zip
-	fi
-}
-
 install_wp
 install_test_suite
-install_db
-install_deps
