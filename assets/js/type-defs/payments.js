4afef200df3351758eec638bfa099c1a9b6ddc3a
Darren Ethier <darren@roughsmootheng.in>
2020-04-03 16:22:14 -0400

    add type-def for payment data response objects

---
+++ assets/js/type-defs/payments.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {Object} PaymentDataResponse
+ *
+ * @property {string} key   Property for the payment data item.
+ * @property {string} value Value for the payment data item.
+ */
+
+export {};
