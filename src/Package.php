586c65e55ef8d71775b0e6b368b37b01e51192c0
Mike Jolley <mike.jolley@me.com>
2020-04-07 12:45:25 +0100

    Bump versions

--- src/Package.php
+++ src/Package.php
@@ -84,7 +84,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '2.5.15';
+					$version = '2.5.16';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ )
