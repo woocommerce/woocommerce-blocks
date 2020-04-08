87de03055315d1a0f0a0558d7becc7512204f5cc
Darren Ethier <darren@roughsmootheng.in>
2020-04-08 15:16:56 -0400

    implement canMakePayment as a function

--- assets/js/payment-method-extensions/payment-methods/stripe/payment-request/index.js
+++ assets/js/payment-method-extensions/payment-methods/stripe/payment-request/index.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { getSetting } from '@woocommerce/settings';
-
-/**
  * Internal dependencies
  */
 import { PAYMENT_METHOD_NAME } from './constants';
@@ -15,31 +10,32 @@ const ApplePayPreview = () => <img src={ applePayImage } alt="" />;
 
 const canPayStripePromise = loadStripe();
 const componentStripePromise = loadStripe();
-const cartData = getSetting( 'cartData', null );
 
 const PaymentRequestPaymentMethod = {
 	id: PAYMENT_METHOD_NAME,
 	content: <PaymentRequestExpress stripe={ componentStripePromise } />,
 	edit: <ApplePayPreview />,
-	canMakePayment: canPayStripePromise.then( ( stripe ) => {
-		if ( stripe === null || cartData === null ) {
-			return false;
-		}
-		// do a test payment request to check if payment request payment can be
-		// done.
-		// @todo, initial country and currency needs to server.
-		const paymentRequest = stripe.paymentRequest( {
-			total: {
-				label: 'Test total',
-				amount: 1000,
-			},
-			// eslint-disable-next-line camelcase
-			country: cartData?.shipping_address?.country,
-			// eslint-disable-next-line camelcase
-			currency: cartData?.totals?.currency_code?.toLowerCase(),
-		} );
-		return paymentRequest.canMakePayment().then( ( result ) => !! result );
-	} ),
+	canMakePayment: ( cartData ) =>
+		canPayStripePromise.then( ( stripe ) => {
+			if ( stripe === null ) {
+				return false;
+			}
+			// do a test payment request to check if payment request payment can be
+			// done.
+			// @todo, initial country and currency needs to server.
+			const paymentRequest = stripe.paymentRequest( {
+				total: {
+					label: 'Test total',
+					amount: 1000,
+				},
+				country: cartData?.shippingAddress?.country,
+				// eslint-disable-next-line camelcase
+				currency: cartData?.cartTotals?.currency_code?.toLowerCase(),
+			} );
+			return paymentRequest
+				.canMakePayment()
+				.then( ( result ) => !! result );
+		} ),
 };
 
 export default PaymentRequestPaymentMethod;
