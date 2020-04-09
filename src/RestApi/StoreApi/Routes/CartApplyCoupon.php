b10abc0d616cb9ef90a37f24ba3425e74391d2ba
Mike Jolley <mike.jolley@me.com>
2020-04-09 14:10:36 +0100

    Add unslash to coupon field

--- src/RestApi/StoreApi/Routes/CartApplyCoupon.php
+++ src/RestApi/StoreApi/Routes/CartApplyCoupon.php
@@ -68,7 +68,7 @@ class CartApplyCoupon extends AbstractRoute {
 
 		$controller  = new CartController();
 		$cart        = $controller->get_cart_instance();
-		$coupon_code = wc_format_coupon_code( $request['code'] );
+		$coupon_code = wc_format_coupon_code( wp_unslash( $request['code'] ) );
 
 		try {
 			$controller->apply_coupon( $coupon_code );
