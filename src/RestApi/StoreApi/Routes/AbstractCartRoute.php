bedd91fe026d235edfe8e4ac5a9202cde88de245
Mike Jolley <mike.jolley@me.com>
2020-04-22 12:32:41 +0100

    Trigger recalcs if hash changes

--- src/RestApi/StoreApi/Routes/AbstractCartRoute.php
+++ src/RestApi/StoreApi/Routes/AbstractCartRoute.php
@@ -10,6 +10,7 @@ namespace Automattic\WooCommerce\Blocks\RestApi\StoreApi\Routes;
 defined( 'ABSPATH' ) || exit;
 
 use Automattic\WooCommerce\Blocks\RestApi\StoreApi\Utilities\CartController;
+use Automattic\WooCommerce\Blocks\RestApi\StoreApi\Schemas\AbstractSchema;
 
 /**
  * Cart class.
@@ -22,6 +23,39 @@ abstract class AbstractCartRoute extends AbstractRoute {
 	 */
 	public function get_namespace() {
 		return 'wc/store';
+	}
+
+	/**
+	 * Constructor.
+	 *
+	 * @param AbstractSchema $schema Schema class for this route.
+	 */
+	public function __construct( AbstractSchema $schema ) {
+		$this->schema = $schema;
+		$this->maybe_recalculate_totals();
+	}
+
+	/**
+	 * If shipping/tax data has changed on the server since last calculation, trigger a recalculation now.
+	 *
+	 * @return void
+	 */
+	protected function maybe_recalculate_totals() {
+		$current_hash = wc()->session->get( 'store_api_calculation_hash', '' );
+		$new_hash     = md5(
+			wp_json_encode(
+				[
+					\WC_Cache_Helper::get_transient_version( 'shipping' ),
+					wc()->cart->get_cart_hash(),
+				]
+			)
+		);
+		if ( ! hash_equals( $current_hash, $new_hash ) ) {
+			wc()->session->set( 'store_api_calculation_hash', $new_hash );
+			wc()->cart->get_cart();
+			wc()->cart->calculate_shipping();
+			wc()->cart->calculate_totals();
+		}
 	}
 
 	/**
