3a707bf2274719473ac2f2179c99707d6c7aa633
Seghir Nadir <nadir.seghir@gmail.com>
2020-04-29 16:59:50 +0100

    Bumping version strings to new version.

--- src/Package.php
+++ src/Package.php
@@ -84,7 +84,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '2.6.0-dev';
+					$version = '2.6.0-beta-2';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ )
