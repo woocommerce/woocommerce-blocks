043c2a114614c059c8fea09f23fb65d2e17c4bfc
Darren Ethier <darren@roughsmootheng.in>
2020-05-12 10:41:52 -0400

    use more reliable `activePaymentMethod` for saved payment method

--- assets/js/base/context/cart-checkout/checkout/processor/index.js
+++ assets/js/base/context/cart-checkout/checkout/processor/index.js
@@ -26,20 +26,19 @@ import { useStoreCart, useStoreNotices } from '@woocommerce/base-hooks';
 /**
  * Utility function for preparing payment data for the request.
  *
- * @param {Object} paymentData Arbitrary payment data provided by the payment
- *                             method.
- * @param {boolean} shouldSave Whether to save the payment method info to
- *                             user account.
+ * @param {Object}  paymentData          Arbitrary payment data provided by the payment method.
+ * @param {boolean} shouldSave           Whether to save the payment method info to user account.
+ * @param {Object}  activePaymentMethod  The current active payment method.
  *
  * @return {PaymentDataItem[]} Returns the payment data as an array of
  *                             PaymentDataItem objects.
  */
-const preparePaymentData = ( paymentData, shouldSave ) => {
+const preparePaymentData = ( paymentData, shouldSave, activePaymentMethod ) => {
 	const apiData = Object.keys( paymentData ).map( ( property ) => {
 		const value = paymentData[ property ];
 		return { key: property, value };
 	}, [] );
-	const savePaymentMethodKey = `wc-${ paymentData.paymentMethod }-new-payment-method`;
+	const savePaymentMethodKey = `wc-${ activePaymentMethod }-new-payment-method`;
 	apiData.push( {
 		key: savePaymentMethodKey,
 		value: shouldSave,
@@ -190,7 +189,8 @@ const CheckoutProcessor = () => {
 				payment_method: paymentMethodId,
 				payment_data: preparePaymentData(
 					paymentMethodData,
-					shouldSavePayment
+					shouldSavePayment,
+					activePaymentMethod
 				),
 			};
 		}
@@ -245,6 +245,7 @@ const CheckoutProcessor = () => {
 		addErrorNotice,
 		removeNotice,
 		paymentMethodId,
+		activePaymentMethod,
 		paymentMethodData,
 		shouldSavePayment,
 		cartNeedsPayment,
