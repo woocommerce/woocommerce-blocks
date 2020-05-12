b0885ad230fe66e191654c289804157e9fdee5fd
Rua Haszard <rua.haszard@automattic.com>
2020-05-13 10:49:34 +1200

    clarify comment about saved cards and supports( tokenization )

--- src/Payments/Integrations/Stripe.php
+++ src/Payments/Integrations/Stripe.php
@@ -114,8 +114,10 @@ final class Stripe extends AbstractPaymentMethodType {
 	 * @return bool True if merchant allows shopper to save card (payment method) during checkout).
 	 */
 	private function get_allow_saved_cards() {
-		// We may also want to check `supports( 'tokenization' )`.
-		// See https://github.com/woocommerce/woocommerce-gateway-stripe/blob/ad19168b63df86176cbe35c3e95203a245687640/includes/class-wc-gateway-stripe.php#L271 .
+		// This assumes that Stripe supports `tokenization` - currently this is true, based on
+		// https://github.com/woocommerce/woocommerce-gateway-stripe/blob/master/includes/class-wc-gateway-stripe.php#L95 .
+		// See https://github.com/woocommerce/woocommerce-gateway-stripe/blob/ad19168b63df86176cbe35c3e95203a245687640/includes/class-wc-gateway-stripe.php#L271 and
+		// https://github.com/woocommerce/woocommerce/wiki/Payment-Token-API .
 		return apply_filters( 'wc_stripe_display_save_payment_method_checkbox', $this->settings['saved_cards'] );
 	}
 
