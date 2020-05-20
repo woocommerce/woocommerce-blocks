3e5ba21118cee52bc00944147770e40b8127bcc5
Mike Jolley <mike.jolley@me.com>
2020-05-20 12:27:34 +0100

    Revert BlockErrorBoundary changes

--- assets/js/blocks/single-product/edit.js
+++ assets/js/blocks/single-product/edit.js
@@ -5,11 +5,12 @@ import { __ } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
 import { useState, useEffect } from '@wordpress/element';
 import { Placeholder, Button } from '@wordpress/components';
-import { withProduct, withBlockErrorBoundary } from '@woocommerce/block-hocs';
+import { withProduct } from '@woocommerce/block-hocs';
 import { InnerBlocks } from '@wordpress/block-editor';
 import { getAllowedInnerBlocks } from '@woocommerce/atomic-utils';
 import { InnerBlockConfigurationProvider } from '@woocommerce/base-context';
 import { useSyncedLayoutConfig } from '@woocommerce/base-hooks';
+import BlockErrorBoundary from '@woocommerce/base-components/block-error-boundary';
 
 /**
  * Internal dependencies
@@ -71,64 +72,63 @@ const Editor = ( {
 
 	return (
 		<div className={ className }>
-			<EditorBlockControls
-				setIsEditing={ setIsEditing }
-				isEditing={ isEditing }
-			/>
-			{ isEditing ? (
-				<Placeholder
-					icon={ BLOCK_ICON }
-					label={ BLOCK_TITLE }
-					className="wc-block-single-product"
-				>
-					{ BLOCK_DESCRIPTION }
-					<div className="wc-block-single-product__selection">
-						<SharedProductControl
+			<BlockErrorBoundary
+				header={ __(
+					'Single Product Block Error',
+					'woo-gutenberg-products-block'
+				) }
+			>
+				<EditorBlockControls
+					setIsEditing={ setIsEditing }
+					isEditing={ isEditing }
+				/>
+				{ isEditing ? (
+					<Placeholder
+						icon={ BLOCK_ICON }
+						label={ BLOCK_TITLE }
+						className="wc-block-single-product"
+					>
+						{ BLOCK_DESCRIPTION }
+						<div className="wc-block-single-product__selection">
+							<SharedProductControl
+								attributes={ attributes }
+								setAttributes={ setAttributes }
+							/>
+							<Button
+								isDefault
+								onClick={ () => {
+									setIsEditing( false );
+								} }
+							>
+								{ __( 'Done', 'woo-gutenberg-products-block' ) }
+							</Button>
+						</div>
+					</Placeholder>
+				) : (
+					<InnerBlockConfigurationProvider
+						parentName={ BLOCK_NAME }
+						layoutStyleClassPrefix="wc-block-single-product"
+					>
+						<LayoutInspectorControls
 							attributes={ attributes }
 							setAttributes={ setAttributes }
+							onReset={ resetLayout }
 						/>
-						<Button
-							isDefault
-							onClick={ () => {
-								setIsEditing( false );
-							} }
-						>
-							{ __( 'Done', 'woo-gutenberg-products-block' ) }
-						</Button>
-					</div>
-				</Placeholder>
-			) : (
-				<InnerBlockConfigurationProvider
-					parentName={ BLOCK_NAME }
-					layoutStyleClassPrefix="wc-block-single-product"
-				>
-					<LayoutInspectorControls
-						attributes={ attributes }
-						setAttributes={ setAttributes }
-						onReset={ resetLayout }
-					/>
-					<InnerBlocks
-						template={ DEFAULT_PRODUCT_LAYOUT }
-						templateLock={ false }
-						allowedBlocks={ [
-							'core/columns',
-							'core/column',
-							...getAllowedInnerBlocks( BLOCK_NAME ),
-						] }
-						renderAppender={ false }
-					/>
-				</InnerBlockConfigurationProvider>
-			) }
+						<InnerBlocks
+							template={ DEFAULT_PRODUCT_LAYOUT }
+							templateLock={ false }
+							allowedBlocks={ [
+								'core/columns',
+								'core/column',
+								...getAllowedInnerBlocks( BLOCK_NAME ),
+							] }
+							renderAppender={ false }
+						/>
+					</InnerBlockConfigurationProvider>
+				) }
+			</BlockErrorBoundary>
 		</div>
 	);
 };
 
-export default compose( [
-	withProduct,
-	withBlockErrorBoundary( {
-		header: __(
-			'Single Product Block Error',
-			'woo-gutenberg-products-block'
-		),
-	} ),
-] )( Editor );
+export default compose( [ withProduct ] )( Editor );
