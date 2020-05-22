44850d8b85fffd3828cf8953ee0c166459da4c95
Mike Jolley <mike.jolley@me.com>
2020-05-22 17:12:00 +0100

    Remove core components from block map

--- assets/js/atomic/components/core/index.js
+++
@@ -1,2 +0,0 @@
-export { default as Columns } from './columns';
-export { default as Column } from './column';
--- assets/js/atomic/utils/get-block-map.js
+++ assets/js/atomic/utils/get-block-map.js
@@ -10,8 +10,6 @@ import {
 	ProductRating,
 	ProductSummary,
 	ProductSaleBadge,
-	Columns,
-	Column,
 } from '@woocommerce/atomic-components';
 
 /**
@@ -27,8 +25,6 @@ export const getBlockMap = ( blockName ) => ( {
 	'woocommerce/product-button': ProductButton,
 	'woocommerce/product-summary': ProductSummary,
 	'woocommerce/product-sale-badge': ProductSaleBadge,
-	'core/columns': Columns,
-	'core/column': Column,
 	...getRegisteredInnerBlocks( blockName ),
 } );
 
