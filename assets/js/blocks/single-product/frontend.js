ac3aa01da85ef2ec3c324f077016b6f961152fdc
Mike Jolley <mike.jolley@me.com>
2020-05-27 10:59:36 +0100

    Placeholder frontend js

---
+++ assets/js/blocks/single-product/frontend.js
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import { StoreNoticesProvider } from '@woocommerce/base-context';
+
+/**
+ * Internal dependencies
+ */
+import Block from './block';
+import blockAttributes from './attributes';
+import {
+	renderFrontend,
+	getAttributesFromDataset,
+} from '../../utils/render-frontend.js';
+
+/**
+ * Wrapper component to supply the notice provider.
+ *
+ * @param {*} props
+ */
+const FrontendBlock = ( props ) => {
+	return (
+		<StoreNoticesProvider context="woocommerce/single-product">
+			<Block { ...props } />
+		</StoreNoticesProvider>
+	);
+};
+
+const getProps = ( el ) => {
+	return {
+		attributes: getAttributesFromDataset( blockAttributes, el.dataset ),
+	};
+};
+
+renderFrontend(
+	'.wp-block-woocommerce-single-product',
+	FrontendBlock,
+	getProps
+);
