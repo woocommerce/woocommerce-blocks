662c9c0f42b2f8d2bf297b305da3711c8daf25ae
Mike Jolley <mike.jolley@me.com>
2020-06-04 17:04:14 +0100

    Edit product link in inspector

--- assets/js/atomic/blocks/product/sku/edit.js
+++ assets/js/atomic/blocks/product/sku/edit.js
@@ -1,8 +1,22 @@
 /**
+ * External dependencies
+ */
+import EditProductLink from '@woocommerce/block-components/edit-product-link';
+import { useProductDataContext } from '@woocommerce/shared-context';
+
+/**
  * Internal dependencies
  */
 import Block from './block';
 
 export default ( { attributes } ) => {
-	return <Block { ...attributes } />;
+	const productDataContext = useProductDataContext();
+	const product = productDataContext.product || {};
+
+	return (
+		<>
+			<EditProductLink productId={ product.id } />
+			<Block { ...attributes } />
+		</>
+	);
 };
