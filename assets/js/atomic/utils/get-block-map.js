ce6cac914014bbaa0ba27b90c8966da9fd9c8e65
Mike Jolley <mike.jolley@me.com>
2020-06-04 14:59:39 +0100

    Create frontend files instead of doing it all in block map

--- assets/js/atomic/utils/get-block-map.js
+++ assets/js/atomic/utils/get-block-map.js
@@ -1,22 +1,18 @@
 /**
  * External dependencies
  */
-import { compose } from '@wordpress/compose';
-import withFilteredAttributes from '@woocommerce/base-hocs/with-filtered-attributes';
 import { getRegisteredInnerBlocks } from '@woocommerce/blocks-registry';
 
 /**
  * Internal dependencies
  */
 import ProductButton from '../blocks/product/button/block';
-import ProductImage from '../blocks/product/image/block';
+import ProductImage from '../blocks/product/image/frontend';
 import ProductPrice from '../blocks/product/price/block';
 import ProductRating from '../blocks/product/rating/block';
 import ProductSaleBadge from '../blocks/product/sale-badge/block';
 import ProductSummary from '../blocks/product/summary/block';
-import ProductTitle from '../blocks/product/title/block';
-import ProductImageAttributes from '../blocks/product/image/attributes';
-import ProductTitleAttributes from '../blocks/product/title/attributes';
+import ProductTitle from '../blocks/product/title/frontend';
 
 /**
  * Map blocks to components suitable for use onm the frontend.
@@ -25,12 +21,8 @@ import ProductTitleAttributes from '../blocks/product/title/attributes';
  */
 export const getBlockMap = ( blockName ) => ( {
 	'woocommerce/product-price': ProductPrice,
-	'woocommerce/product-image': compose(
-		withFilteredAttributes( ProductImageAttributes )
-	)( ProductImage ),
-	'woocommerce/product-title': compose(
-		withFilteredAttributes( ProductTitleAttributes )
-	)( ProductTitle ),
+	'woocommerce/product-image': ProductImage,
+	'woocommerce/product-title': ProductTitle,
 	'woocommerce/product-rating': ProductRating,
 	'woocommerce/product-button': ProductButton,
 	'woocommerce/product-summary': ProductSummary,
