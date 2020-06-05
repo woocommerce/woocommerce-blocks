e91ee5466a2b6de2c6e06f49a065981dd7177165
Mike Jolley <mike.jolley@me.com>
2020-06-05 16:45:42 +0100

    Add edit views

--- assets/js/atomic/blocks/product/tag-list/edit.js
+++ assets/js/atomic/blocks/product/tag-list/edit.js
@@ -1,8 +1,25 @@
 /**
+ * External dependencies
+ */
+import { Disabled } from '@wordpress/components';
+import EditProductLink from '@woocommerce/block-components/edit-product-link';
+import { useProductDataContext } from '@woocommerce/shared-context';
+
+/**
  * Internal dependencies
  */
 import Block from './block';
 
 export default ( { attributes } ) => {
-	return <Block { ...attributes } />;
+	const productDataContext = useProductDataContext();
+	const product = productDataContext.product || {};
+
+	return (
+		<>
+			<EditProductLink productId={ product.id } />
+			<Disabled>
+				<Block { ...attributes } />
+			</Disabled>
+		</>
+	);
 };
