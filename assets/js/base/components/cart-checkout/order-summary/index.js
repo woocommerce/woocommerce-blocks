woocommerce/woocommerce-blocks

commit a162c54ec661b2e759dd6a357e7620f2f142b370

Update Cart & Checkout heading styles (#2597)
* Honor heading styles in Cart and Checkout blocks

* Replace Panel with DisclosureWidget and remove Card

* Remove panel-style from main entry

* Rename DisclosureWidget props

* Simplify TitleTag assign

* Add aria-expanded attribute

* Rename DisclosureWidget to Panel

* Make Panel button clickable area smaller

* Fix specificity issues with Twenty Twenty

* Reset italics in reset-typography mixin

* Make C&C titles font size large

* Add padding to shipping options when in the sidebar

assets/js/base/components/panel/style.scss
@@ -1,0 +1,36 @@
+.wc-blocks-components-panel__button {
+	&,
+	&:hover,
+	&:focus,
+	&:active {
+		@include reset-box();
+		@include reset-typography();
+		background: transparent;
+		box-shadow: none;
+		height: auto;
+		line-height: 1;
+		margin-bottom: em(6px);
+		margin-top: em(6px);
+		padding-bottom: em($gap-small - 6px);
+		padding-top: em($gap-small - 6px);
+		position: relative;
+		text-align: left;
+		width: 100%;
+	}
+
+	> .wc-blocks-components-panel__button-icon {
+		position: absolute;
+		right: 0;
+		top: 50%;
+		transform: translateY(-50%);
+		width: auto;
+	}
+}
+
+.wc-blocks-components-panel__content {
+	padding-bottom: em($gap);
+}
+
+.theme-twentytwenty .wc-blocks-components-panel__button {
+	background: transparent;
+}

assets/js/base/components/payment-methods/style.scss
@@ -3,23 +3,22 @@
 	border: 2px solid $black;
 	border-radius: 5px;
 	padding: 8px;
+	position: relative;
 
 	.wc-block-component-express-checkout__title {
-		position: relative;
 		background-color: $white;
 		padding-left: $gap-small;
 		padding-right: $gap-small;
-		margin-top: -24px;
 		margin-left: $gap-small;
-		margin-bottom: $gap-small;
-		color: $black;
-		font-weight: bold;
 		display: inline-block;
 		vertical-align: middle;
+		transform: translateY(-50%);
+		position: absolute;
+		top: 0;
 	}
 
 	.wc-block-component-express-checkout__content {
-		padding: 0 $gap-large;
+		padding: $gap $gap-large 0;
 	}
 
 	.wc-block-component-express-checkout-payment-event-buttons {
@@ -186,5 +185,14 @@
 	.wc-block-gateway-container.wc-card-cvc-element {
 		flex-basis: calc(50% - #{$gap-smaller});
 		margin-left: $gap-smaller;
+	}
+}
+
+// For Twenty Twenty we need to increase specificity of the title.
+.theme-twentytwenty {
+	.wc-block-component-express-checkout .wc-block-component-express-checkout__title {
+		padding-left: $gap-small;
+		padding-right: $gap-small;
+		margin-left: $gap-small;
 	}
 }

assets/js/base/components/sidebar-layout/style.scss
@@ -13,54 +13,26 @@
 
 .wc-block-sidebar {
 	margin: 0;
-	padding-left: percentage($gap-largest / 1060px);
+	padding-left: percentage($gap-large / 1060px);
 	width: 35%;
 
-	// Reset Gutenberg <Panel> styles when used in the sidebar.
-	.components-panel__body {
+	.wc-blocks-components-panel {
 		border-top: 1px solid $core-grey-light-600;
 		border-bottom: 1px solid $core-grey-light-600;
 
-		&.is-opened {
-			padding-left: 0;
-			padding-right: 0;
-			padding-top: 0;
+		+ .wc-blocks-components-panel {
+			border-top: none;
+		}
 
-			> .components-panel__body-title {
-				margin-bottom: 0;
-				margin-left: 0;
-				margin-right: 0;
-			}
+		> h2 {
+			@include font-size(large);
+			@include reset-box();
 		}
 	}
 
-	.components-panel__body-title {
-		@include text-heading();
-		background: transparent;
-
-		&:hover,
-		&:focus,
-		&:active {
-			background: transparent;
-		}
-	}
-
-	.components-panel__body-toggle {
-		@include reset-typography();
-		background: transparent;
-		font-weight: normal;
-		font-size: inherit;
-		padding-left: 0;
-		padding-right: 36px;
-
-		&:hover,
-		&:focus,
-		&:active {
-			background: transparent;
-		}
-
-		.components-panel__arrow {
-			right: 0;
+	.wc-blocks-components-panel__button {
+		> svg {
+			padding-right: $gap;
 		}
 	}
 }
@@ -79,17 +51,25 @@
 		.wc-block-sidebar {
 			padding: 0;
 			width: 100%;
-
-			// Reset card component styling on mobile.
-			.components-card {
-				background: transparent;
-				box-shadow: none;
-				border: none;
-			}
-			.components-card__body {
-				border: none;
-				padding: 0;
-			}
 		}
+	}
+}
+
+.is-large {
+	.wc-block-sidebar {
+		.wc-block-totals-table-item,
+		.wc-blocks-components-panel__button,
+		.wc-blocks-components-panel__content {
+			padding-left: $gap;
+			padding-right: $gap;
+		}
+	}
+}
+
+// For Twenty Twenty we need to increase specificity a bit more.
+.theme-twentytwenty {
+	.wc-block-sidebar .wc-blocks-components-panel > h2 {
+		@include font-size(large);
+		@include reset-box();
 	}
 }

assets/js/blocks/cart-checkout/cart/full-cart/style.scss
@@ -1,11 +1,6 @@
 .wc-block-cart {
 	color: $core-grey-dark-600;
 
-	.wc-block-cart__main,
-	.wc-block-cart__sidebar .components-card__body.is-size-medium {
-		padding-top: 1.3em;
-	}
-
 	.wc-block-cart__item-count {
 		float: right;
 	}
@@ -26,6 +21,7 @@ table.wc-block-cart-items td {
 	background: none !important;
 	// Remove borders on default themes.
 	border: 0;
+	margin: 0;
 }
 
 .editor-styles-wrapper table.wc-block-cart-items,
@@ -269,6 +265,16 @@ table.wc-block-cart-items {
 
 	.wc-block-radio-control__input {
 		left: 0;
+	}
+
+	.wc-block-sidebar {
+		> .wc-block-cart__totals-title,
+		.wc-block-cart__shipping-calculator,
+		.wc-block-shipping-totals__fieldset,
+		> .wc-block-cart__submit-container {
+			padding-left: $gap;
+			padding-right: $gap;
+		}
 	}
 }
 

assets/js/blocks/cart-checkout/checkout/style.scss
@@ -20,47 +20,31 @@
 }
 
 .wc-block-checkout__sidebar {
-	.wc-block-order-summary {
-		border: none;
-
-		&.is-opened {
-			padding-bottom: 0;
-		}
-
-		> .components-panel__body-title > .components-panel__body-toggle {
-			padding: $gap-small 0;
-		}
-
-		.components-panel__body-toggle {
-			padding: 0;
-		}
-	}
-
 	.wc-block-product-name {
 		color: inherit;
 	}
 
-	.wc-block-order-summary__button-icon {
-		vertical-align: middle;
+	.wc-block-order-summary {
+		border: 0;
 	}
 
-	.wc-block-order-summary__button-text {
-		margin-left: $gap;
-	}
-
-	.wc-block-order-summary__row {
+	.wc-block-order-summary__content {
 		display: table;
 		width: 100%;
 	}
 
 	.wc-block-order-summary-item {
 		display: table-row;
+		width: 100%;
+
 		> div {
 			border-bottom: 1px solid $core-grey-light-600;
 		}
+
 		&:last-child {
 			> div {
 				border-bottom: none;
+				padding-bottom: 0;
 			}
 		}
 	}
@@ -311,12 +295,6 @@
 }
 
 .is-large {
-	.wc-block-checkout__sidebar {
-		.wc-block-order-summary {
-			margin: -20px;
-		}
-	}
-
 	.wc-block-checkout__actions {
 		padding-right: 36px;
 	}
@@ -324,41 +302,6 @@
 	.wc-block-checkout__shipping-option {
 		.wc-block-radio-control__input {
 			margin-left: -8px;
-		}
-	}
-
-	.wc-block-checkout__sidebar {
-		.wc-block-totals-table-item {
-			padding-left: $gap;
-			padding-right: $gap;
-		}
-
-		.wc-block-coupon-code .components-panel__body-toggle {
-			padding-left: $gap;
-
-			.components-panel__arrow {
-				right: $gap-small;
-			}
-		}
-
-		.wc-block-coupon-code__row {
-			padding-left: $gap;
-			padding-right: $gap;
-
-		}
-
-		.wc-block-order-summary {
-			> .components-panel__body-title > .components-panel__body-toggle {
-				padding-left: $gap;
-
-				.components-panel__arrow {
-					right: $gap-small;
-				}
-			}
-		}
-
-		.wc-block-order-summary__row {
-			padding: 0 $gap;
 		}
 	}
 }

assets/js/base/components/cart-checkout/form-step/index.js
@@ -3,6 +3,7 @@
  */
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
+import Title from '@woocommerce/base-components/title';
 
 /**
  * Internal dependencies
@@ -11,9 +12,13 @@ import './style.scss';
 
 const StepHeading = ( { title, stepHeadingContent } ) => (
 	<div className="wc-block-checkout-step__heading">
-		<h2 aria-hidden="true" className="wc-block-checkout-step__title">
+		<Title
+			aria-hidden="true"
+			className="wc-block-checkout-step__title"
+			headingLevel="2"
+		>
 			{ title }
-		</h2>
+		</Title>
 		{ !! stepHeadingContent && (
 			<span className="wc-block-checkout-step__heading-content">
 				{ stepHeadingContent }

assets/js/base/components/cart-checkout/form-step/style.scss
@@ -27,20 +27,19 @@ $line-offset-from-circle-size: 8px;
 	align-content: center;
 	flex-wrap: wrap;
 	margin-bottom: $gap-smaller;
+	position: relative;
 
 	.wc-block-checkout-step__title {
-		@include font-size(regular);
 		line-height: 1.5;
-		color: $gray-80;
-		font-weight: 400;
 		margin: 0 $gap-small 0 0;
 	}
 }
 
 .wc-block-checkout-step__heading-content {
 	@include font-size(smaller);
-	line-height: 2;
 	color: $gray-80;
+	position: absolute;
+	right: 0;
 
 	a {
 		font-weight: bold;

assets/js/base/components/cart-checkout/shipping-rates-control/style.scss
@@ -1,5 +1,7 @@
-.wc-block-shipping-rates-control__package-title {
-	font-weight: bold;
+.wc-block-shipping-rates-control__package {
+	.wc-block-shipping-rates-control__package-title {
+		margin: 0;
+	}
 }
 
 .wc-block-shipping-rates-control__package-items {
@@ -7,7 +9,7 @@
 	color: $core-grey-dark-400;
 	display: block;
 	list-style: none;
-	margin: 0 0 $gap-small;
+	margin: 0;
 	padding: 0;
 }
 
@@ -29,7 +31,7 @@
 }
 
 .components-notice.wc-block-shipping-rates-control__no-results-notice {
-	margin-bottom: $gap;
+	margin-bottom: 0;
 }
 
 // Resets when it's inside a panel.
@@ -40,19 +42,6 @@
 		&,
 		&.is-opened {
 			padding-bottom: 0;
-		}
-
-		.components-panel__body-title {
-			margin-bottom: 0;
-		}
-
-		.components-panel__body-toggle {
-			padding-bottom: 14px;
-			padding-top: 14px;
-		}
-
-		.wc-block-shipping-rates-control__package-title {
-			font-weight: normal;
 		}
 
 		.wc-block-shipping-rates-control__package-items {

commit cc913e068de34c03a108d8a3f7babd834e14ccf0

Fix styling issues introduced in #2597 and #2619 (#2652)
* Move order summary styles to component style.scss

* Fix wrong class name in Order summary

* Move express payment methods a little bit higher

* Fix shipping options panel misaligned in the Cart sidebar

* Add right padding to panel button so text doesn't overlap the arrow

* Fix wrong class name in Order summary (II)

assets/js/base/components/cart-checkout/order-summary/index.js
@@ -10,6 +10,7 @@ import Panel from '@woocommerce/base-components/panel';
  * Internal dependencies
  */
 import OrderSummaryItem from './order-summary-item.js';
+import './style.scss';
 
 const OrderSummary = ( { cartItems = [] } ) => {
 	const { isLarge, hasContainerWidth } = useContainerWidthContext();
@@ -29,7 +30,7 @@ const OrderSummary = ( { cartItems = [] } ) => {
 			}
 			titleTag="h2"
 		>
-			<div className="wc-block-order-components-summary__content">
+			<div className="wc-block-components-order-summary__content">
 				{ cartItems.map( ( cartItem ) => {
 					return (
 						<OrderSummaryItem

assets/js/base/components/cart-checkout/order-summary/style.scss
@@ -1,0 +1,83 @@
+.wc-block-components-order-summary {
+	border: 0;
+}
+
+.wc-block-components-order-summary__content {
+	display: table;
+	width: 100%;
+}
+
+.wc-block-components-order-summary-item {
+	display: table-row;
+	width: 100%;
+
+	> div {
+		border-bottom: 1px solid $core-grey-light-600;
+	}
+
+	&:last-child {
+		> div {
+			border-bottom: none;
+			padding-bottom: 0;
+		}
+	}
+}
+
+.wc-block-components-order-summary-item__image,
+.wc-block-components-order-summary-item__description {
+	display: table-cell;
+	vertical-align: top;
+}
+
+.wc-block-components-order-summary-item__image {
+	width: #{$gap-large * 2};
+	padding-top: $gap;
+	padding-bottom: $gap;
+	position: relative;
+
+	> img {
+		width: #{$gap-large * 2};
+		max-width: #{$gap-large * 2};
+	}
+}
+
+.wc-block-components-order-summary-item__quantity {
+	@include font-size(smaller);
+	align-items: center;
+	background: #fff;
+	border: 2px solid;
+	border-radius: 1em;
+	box-shadow: 0 0 0 2px #fff;
+	color: #000;
+	display: flex;
+	line-height: 1;
+	min-height: 20px;
+	padding: 0 0.4em;
+	position: absolute;
+	justify-content: center;
+	min-width: 20px;
+	right: 0;
+	top: $gap;
+	transform: translate(50%, -50%);
+	white-space: nowrap;
+	z-index: 1;
+}
+
+.wc-block-components-order-summary-item__description {
+	padding-left: $gap-large;
+	padding-top: $gap;
+	padding-bottom: $gap;
+	line-height: 1.375;
+
+	p,
+	.wc-block-product-metadata {
+		line-height: 1.375;
+		margin-top: #{ ( $gap-large - $gap ) / 2 };
+	}
+}
+
+.wc-block-components-order-summary-item__header {
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+}

assets/js/base/components/panel/style.scss
@@ -12,6 +12,7 @@
 		margin-bottom: em(6px);
 		margin-top: em(6px);
 		padding-bottom: em($gap-small - 6px);
+		padding-right: #{24px + $gap-smaller};
 		padding-top: em($gap-small - 6px);
 		position: relative;
 		text-align: left;

assets/js/base/components/payment-methods/style.scss
@@ -15,6 +15,7 @@
 		transform: translateY(-50%);
 		position: absolute;
 		top: 0;
+		margin-top: -2px; // Same as container border width.
 	}
 
 	.wc-block-components-express-checkout__content {

assets/js/base/components/sidebar-layout/style.scss
@@ -29,12 +29,6 @@
 			@include reset-box();
 		}
 	}
-
-	.wc-blocks-components-panel__button {
-		> svg {
-			padding-right: $gap;
-		}
-	}
 }
 
 .is-medium,
@@ -58,8 +52,7 @@
 .is-large {
 	.wc-block-sidebar {
 		.wc-block-totals-table-item,
-		.wc-blocks-components-panel__button,
-		.wc-blocks-components-panel__content {
+		.wc-blocks-components-panel {
 			padding-left: $gap;
 			padding-right: $gap;
 		}

assets/js/blocks/cart-checkout/cart/full-cart/style.scss
@@ -266,7 +266,7 @@ table.wc-block-cart-items {
 	.wc-block-sidebar {
 		> .wc-block-cart__totals-title,
 		.wc-block-cart__shipping-calculator,
-		.wc-block-shipping-totals__fieldset,
+		.wc-block-shipping-rates-control__package:not(.wc-blocks-components-panel),
 		> .wc-block-cart__submit-container {
 			padding-left: $gap;
 			padding-right: $gap;

assets/js/blocks/cart-checkout/checkout/style.scss
@@ -28,90 +28,6 @@
 	}
 }
 
-.wc-block-components-order-summary {
-	border: 0;
-}
-
-.wc-block-components-order-summary__content {
-	display: table;
-	width: 100%;
-}
-
-.wc-block-components-order-summary-item {
-	display: table-row;
-	width: 100%;
-
-	> div {
-		border-bottom: 1px solid $core-grey-light-600;
-	}
-
-	&:last-child {
-		> div {
-			border-bottom: none;
-			padding-bottom: 0;
-		}
-	}
-}
-
-.wc-block-components-order-summary-item__image,
-.wc-block-components-order-summary-item__description {
-	display: table-cell;
-	vertical-align: top;
-}
-
-.wc-block-components-order-summary-item__image {
-	width: #{$gap-large * 2};
-	padding-top: $gap;
-	padding-bottom: $gap;
-	position: relative;
-
-	> img {
-		width: #{$gap-large * 2};
-		max-width: #{$gap-large * 2};
-	}
-}
-
-.wc-block-components-order-summary-item__quantity {
-	@include font-size(smaller);
-	align-items: center;
-	background: #fff;
-	border: 2px solid;
-	border-radius: 1em;
-	box-shadow: 0 0 0 2px #fff;
-	color: #000;
-	display: flex;
-	line-height: 1;
-	min-height: 20px;
-	padding: 0 0.4em;
-	position: absolute;
-	justify-content: center;
-	min-width: 20px;
-	right: 0;
-	top: $gap;
-	transform: translate(50%, -50%);
-	white-space: nowrap;
-	z-index: 1;
-}
-
-.wc-block-components-order-summary-item__description {
-	padding-left: $gap-large;
-	padding-top: $gap;
-	padding-bottom: $gap;
-	line-height: 1.375;
-
-	p,
-	.wc-block-product-metadata {
-		line-height: 1.375;
-		margin-top: #{ ( $gap-large - $gap ) / 2 };
-	}
-}
-
-.wc-block-components-order-summary-item__header {
-	display: flex;
-	flex-wrap: wrap;
-	justify-content: space-between;
-}
-
 .wc-block-components-express-checkout-continue-rule {
 	display: flex;
 	align-items: center;