62ca7e80de96ba2db4391a26f8c3ad38c09d4047
Seghir Nadir <nadir.seghir@gmail.com>
2020-06-15 14:14:51 +0100

    add docs

--- tests/utils/visit-block-page.js
+++ tests/utils/visit-block-page.js
@@ -11,6 +11,12 @@ import { outputFile } from 'fs-extra';
 import { dirname } from 'path';
 import kebabCase from 'lodash/kebabCase';
 
+/**
+ *
+ * @param {string} link the page or post you want to visit.
+ *
+ * This will visit a GB page or post, and will hide the welcome guide.
+ */
 async function visitPage( link ) {
 	await page.goto( link );
 	const isWelcomeGuideActive = await page.evaluate( () =>
@@ -23,16 +29,28 @@ async function visitPage( link ) {
 		);
 	}
 }
+
+/**
+ *
+ * @param {string} title the page title, written as `BLOCK_NAME block`
+ *
+ * This function will attempt to search for a page with the `title`
+ * if that block is found, it will open it, if it's not found, it will open
+ * a new page, insert the block, save the page content and title as a fixture file.
+ * In both cases, this page will end up with a page open with the block inserted.
+ */
 export async function visitBlockPage( title ) {
 	let link = '';
 	// Visit Import Products page.
 	await visitAdminPage( 'edit.php', 'post_type=page' );
-	// search for the page.
+	// If the website has no pages, `#post-search-input` will not render.
 	if ( await page.$( '#post-search-input' ) ) {
+		// search for the page.
 		await page.type( '#post-search-input', title );
 		await page.click( '#search-submit', { waitUntil: 'domcontentloaded' } );
 		const pageLink = await page.$x( `//a[contains(text(), '${ title }')]` );
 		if ( ( await pageLink.length ) > 0 ) {
+			// clicking the link directly caused racing issues, so I used goto.
 			link = await page.evaluate(
 				( a ) => a.getAttribute( 'href' ),
 				pageLink[ 0 ]
@@ -47,6 +65,8 @@ export async function visitBlockPage( title ) {
 		const pageContent = await getEditedPostContent();
 		await outputFile(
 			`${ dirname(
+				// we want to fetch the path of the test file who triggered this function
+				// this could be two levels up, or one level up, depending on how you launch the test.
 				module.parent.parent.filename ||
 					module.parent.filename ||
 					module.filename
