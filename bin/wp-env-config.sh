43eacd638362e73c16a1754c3ddfeab98cf55798
Darren Ethier <darren@roughsmootheng.in>
2020-06-17 11:12:28 -0400

    move things around and add pre-configuration as files so all wp commands run at once

---
+++ bin/wp-env-config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+## Check if user already exists
+wp user get customer 2> /dev/null
+
+## if 0 is the exit code then we can leave otherwise we'll try creating the user
+if [ $? -eq 0 ]
+then
+	EXIT_CODE=0
+else
+  wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=customer
+  EXIT_CODE=$?
+fi
+
+## set permalinks for easier wp-json
+wp rewrite structure '/%postname%/'
+wp plugin list
+wp option get 'siteurl'
+wp eval 'echo rest_url();'
+
+exit $EXIT_CODE
--- bin/wp-env-pre-config.sh
+++ bin/wp-env-pre-config.sh
@@ -1,20 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-## Check if user already exists
-npm run wp-env run tests-cli wp user get customer 2> /dev/null
-
-## if 0 is the exit code then we can leave otherwise we'll try creating the user
-if [ $? -eq 0 ]
-then
-	EXIT_CODE=0
-else
-  npm run wp-env run tests-cli wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=customer
-  EXIT_CODE=$?
-fi
-
-## set permalinks for easier wp-json
-npm run wp-env run tests-cli wp rewrite structure '/%postname%/'
-npm run wp-env run tests-cli wp rewrite flush -- --hard
-npm run wp-env run tests-cli wp plugin list
-npm run wp-env run tests-cli wp option get 'siteurl'
-exit $EXIT_CODE
+## get parent directory
+npm run wp-env run tests-cli './wp-content/plugins/$(basename `pwd`)/bin/wp-env-config.sh'
