8eec3d41da9490929c6b99a3eb5f909d73930906
Darren Ethier <darren@roughsmootheng.in>
2020-06-17 07:35:06 -0400

    looks liek we have to make sure wp db is up to date?!?
    - also moves pre-configuration stuff all into one file for easier maintenance.

--- bin/wp-env-pre-config.sh
+++ bin/wp-env-pre-config.sh
@@ -6,8 +6,15 @@ npm run wp-env run tests-cli wp user get customer 2> /dev/null
 ## if 0 is the exit code then we can leave otherwise we'll try creating the user
 if [ $? -eq 0 ]
 then
-	exit 0
+	EXIT_CODE=0
 else
   npm run wp-env run tests-cli wp user create customer customer@woocommercecoree2etestsuite.com --user_pass=password --role=customer
-  exit $?
+  EXIT_CODE=$?
 fi
+
+## set permalinks for easier wp-json
+npm run wp-env run tests-cli wp rewrite structure '/%postname%/'
+## make sure db is updated
+npm run wp-env run tests-cli wp core update-db
+
+exit $EXIT_CODE
