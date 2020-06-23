8e6fa60c86bd64a786fe1b6de3a869bb648843bb
Mike Jolley <mike.jolley@me.com>
2020-06-23 13:14:02 +0100

    Update query to get differences and merge product data

--- assets/js/shared/context/product-data-query.js
+++ assets/js/shared/context/product-data-query.js
@@ -3,28 +3,49 @@
  */
 import { addQueryArgs } from '@wordpress/url';
 import apiFetch from '@wordpress/api-fetch';
-import { useEffect } from '@wordpress/element';
+import { useEffect, useState, useRef } from '@wordpress/element';
+import { difference, concat } from 'lodash';
 
 /**
  * Rendered by Product Data Context to query data for registered fields once children are rendered.
  */
 const ProductDataQuery = ( { productId, fields, setProduct } ) => {
+	const [ productData, setProductData ] = useState( {} );
+	const retrievedFields = useRef( [] );
+
+	// If the product ID changes, reset fields.
+	useEffect( () => {
+		retrievedFields.current = [];
+		setProductData( {} );
+	}, [ productId ] );
+
+	// When product data changes, update the product context.
+	useEffect( () => {
+		setProduct( productData );
+	}, [ productData, setProduct ] );
+
+	// If the fields change, fetch new data.
 	useEffect( () => {
 		if ( ! productId || fields === undefined ) {
 			return;
 		}
+		const fieldsToFetch = difference( fields, retrievedFields.current );
+
+		if ( fieldsToFetch.length === 0 ) {
+			return;
+		}
+
 		apiFetch( {
 			path: addQueryArgs( `/wc/store/products/${ productId }`, {
-				_fields: [ ...fields, 'id' ],
+				_fields: [ ...fieldsToFetch, 'id' ],
 			} ),
-		} )
-			.then( ( theProduct ) => {
-				setProduct( theProduct );
-			} )
-			.catch( async () => {
-				setProduct( null );
+		} ).then( ( theProduct ) => {
+			setProductData( ( prevState ) => {
+				return { ...prevState, ...theProduct };
 			} );
-	}, [ productId, fields, setProduct ] );
+			retrievedFields.current = concat( retrievedFields.current, fields );
+		} );
+	}, [ productId, fields ] );
 
 	return null;
 };
