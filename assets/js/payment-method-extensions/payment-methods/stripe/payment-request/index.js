05e63deea8d430cbd9f08628d49d21360223885c
Rua Haszard <rua.haszard@automattic.com>
2020-07-11 14:15:36 +1200

    cache test payment request to avoid unnecessary stripe API calls: - in the canMakePayment callback there's a test payment to determine if   chrome pay/apple pay is set up and available - canMakePayment is now called multiple times as checkout state changes - now the results of the test payment are stored in variable, and   returned on subsequent calls

--- assets/js/payment-method-extensions/payment-methods/stripe/payment-request/index.js
+++ assets/js/payment-method-extensions/payment-methods/stripe/payment-request/index.js
@@ -16,30 +16,47 @@ const ApplePayPreview = () => <img src={ applePayImage } alt="" />;
 const canPayStripePromise = loadStripe();
 const componentStripePromise = loadStripe();
 
+let apiInitialized = false,
+	canPay = false;
+
+// Initialise stripe API client and determine if payment method can be used
+// in current environment (e.g. geo + shopper has payment settings configured).
+function paymentRequestAvailable( currencyCode ) {
+	// If we've already initialised, return the cached results.
+	if ( apiInitialized ) {
+		return canPay;
+	}
+
+	return canPayStripePromise.then( ( stripe ) => {
+		if ( stripe === null ) {
+			return false;
+		}
+		// Do a test payment to confirm if payment method is available.
+		const paymentRequest = stripe.paymentRequest( {
+			total: {
+				label: 'Test total',
+				amount: 1000,
+			},
+			country: getSetting( 'baseLocation', {} )?.country,
+			currency: currencyCode,
+		} );
+		return paymentRequest.canMakePayment().then( ( result ) => {
+			canPay = !! result;
+			apiInitialized = true;
+			return canPay;
+		} );
+	} );
+}
+
 const PaymentRequestPaymentMethod = {
 	name: PAYMENT_METHOD_NAME,
 	content: <PaymentRequestExpress stripe={ componentStripePromise } />,
 	edit: <ApplePayPreview />,
 	canMakePayment: ( cartData ) =>
-		canPayStripePromise.then( ( stripe ) => {
-			if ( stripe === null ) {
-				return false;
-			}
-			// do a test payment request to check if payment request payment can be
-			// done.
-			const paymentRequest = stripe.paymentRequest( {
-				total: {
-					label: 'Test total',
-					amount: 1000,
-				},
-				country: getSetting( 'baseLocation', {} )?.country,
-				// eslint-disable-next-line camelcase
-				currency: cartData?.cartTotals?.currency_code?.toLowerCase(),
-			} );
-			return paymentRequest
-				.canMakePayment()
-				.then( ( result ) => !! result );
-		} ),
+		paymentRequestAvailable(
+			// eslint-disable-next-line camelcase
+			cartData?.cartTotals?.currency_code?.toLowerCase()
+		),
 	paymentMethodId: 'stripe',
 };
 
