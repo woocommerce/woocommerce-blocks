b8761dac6596bb372c73e5ecb063e6cb8575cc7c
Darren Ethier <darren@roughsmootheng.in>
2020-07-16 12:03:19 -0400

    add tests for error handling.

--- tests/php/Domain/Services/DeleteDraftOrders.php
+++ tests/php/Domain/Services/DeleteDraftOrders.php
@@ -16,6 +16,7 @@ use Automattic\WooCommerce\Blocks\Domain\Package;
 class DeleteDraftOrders extends TestCase {
 
 	private $draft_orders_instance;
+	private $caught_exception;
 
 	/**
 	 * During setup create some draft orders.
@@ -61,7 +62,7 @@ class DeleteDraftOrders extends TestCase {
 
 		// set a non-draft order to make sure it's unaffected
 		$order = new WC_Order();
-		$order->set_status( 'wc-on-hold' );
+		$order->set_status( 'on-hold' );
 		$order->save();
 		$wpdb->update(
 			$wpdb->posts,
@@ -73,10 +74,16 @@ class DeleteDraftOrders extends TestCase {
 				'ID' => $order->get_id()
 			)
 		);
+
+		// set listening for exceptions
+		add_action( 'woocommerce_caught_exception', function($exception_object){
+			$this->caught_exception = $exception_object;
+		});
 	}
 
 	public function tearDown() {
 		$this->draft_orders_instance = null;
+		remove_all_actions( 'woocommerce_caught_exception' );
 	}
 
 	/**
@@ -99,5 +106,46 @@ class DeleteDraftOrders extends TestCase {
 
 		// The non-draft order should still be present
 		$this->assertEquals( 1, (int) $wpdb->get_var( "SELECT COUNT(ID) from $wpdb->posts posts WHERE posts.post_status = 'wc-on-hold'" ) );
+	}
+
+	public function test_greater_than_batch_results_error() {
+		$sample_results = function() {
+			return array_fill( 0, 21, ( new WC_Order ) );
+
+		};
+		$this->mock_results_for_wc_query($sample_results);
+		$this->draft_orders_instance->delete_expired_draft_orders();
+		$this->assertContains( 'unexpected number of results', $this->caught_exception->getMessage() );
+		$this->unset_mock_results_for_wc_query( $sample_results );
+	}
+
+	public function test_order_not_instance_of_wc_order_error() {
+		$sample_results = function() {
+			return [ 10 ];
+		};
+		$this->mock_results_for_wc_query( $sample_results );
+		$this->draft_orders_instance->delete_expired_draft_orders();
+		$this->assertContains( 'value that is not a WC_Order', $this->caught_exception->getMessage() );
+		$this->unset_mock_results_for_wc_query( $sample_results );
+	}
+
+	public function test_order_incorrect_status_error() {
+		$sample_results = function() {
+			$test_order = new WC_Order();
+			$test_order->set_status( 'on-hold' );
+			return [ $test_order ];
+		};
+		$this->mock_results_for_wc_query( $sample_results );
+		$this->draft_orders_instance->delete_expired_draft_orders();
+		$this->assertContains( 'order that is not a `wc-checkout-draft`', $this->caught_exception->getMessage() );
+		$this->unset_mock_results_for_wc_query( $sample_results );
+	}
+
+	private function mock_results_for_wc_query( $mock_callback ) {
+		add_filter( 'woocommerce_order_query', $mock_callback );
+	}
+
+	private function unset_mock_results_for_wc_query( $mock_callback ) {
+		remove_filter( 'woocommerce_order_query', $mock_callback );
 	}
 }
