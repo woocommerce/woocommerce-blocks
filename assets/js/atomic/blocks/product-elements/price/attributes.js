0889eded0b99814e33c5092446bac4763c0d32ee
Seghir Nadir <nadir.seghir@gmail.com>
2020-07-21 16:10:45 +0100

    include price PR

--- assets/js/atomic/blocks/product-elements/price/attributes.js
+++ assets/js/atomic/blocks/product-elements/price/attributes.js
@@ -1,8 +1,45 @@
-export const blockAttributes = {
+/**
+ * External dependencies
+ */
+import { isFeaturePluginBuild } from '@woocommerce/block-settings';
+
+let blockAttributes = {
 	productId: {
 		type: 'number',
 		default: 0,
 	},
 };
 
+if ( isFeaturePluginBuild() ) {
+	blockAttributes = {
+		...blockAttributes,
+		align: {
+			type: 'string',
+		},
+		fontSize: {
+			type: 'string',
+		},
+		customFontSize: {
+			type: 'number',
+		},
+		saleFontSize: {
+			type: 'string',
+		},
+		customSaleFontSize: {
+			type: 'number',
+		},
+		color: {
+			type: 'string',
+		},
+		saleColor: {
+			type: 'string',
+		},
+		customColor: {
+			type: 'string',
+		},
+		customSaleColor: {
+			type: 'string',
+		},
+	};
+}
 export default blockAttributes;
