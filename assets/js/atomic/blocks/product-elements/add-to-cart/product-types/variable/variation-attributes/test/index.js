24433766a61d7a7bd6a0dbfdbaef203144373036
Mike Jolley <mike.jolley@me.com>
2020-07-28 11:54:57 +0100

    move mock data to test file

--- assets/js/atomic/blocks/product-elements/add-to-cart/product-types/variable/variation-attributes/test/index.js
+++ assets/js/atomic/blocks/product-elements/add-to-cart/product-types/variable/variation-attributes/test/index.js
@@ -8,7 +8,123 @@ import {
 	getVariationMatchingSelectedAttributes,
 	getActiveSelectControlOptions,
 } from '../utils';
-import { rawVariations, rawAttributeData } from './mock-data';
+
+const rawAttributeData = [
+	{
+		id: 1,
+		name: 'Color',
+		taxonomy: 'pa_color',
+		has_variations: true,
+		terms: [
+			{
+				id: 22,
+				name: 'Blue',
+				slug: 'blue',
+			},
+			{
+				id: 23,
+				name: 'Green',
+				slug: 'green',
+			},
+			{
+				id: 24,
+				name: 'Red',
+				slug: 'red',
+			},
+		],
+	},
+	{
+		id: 0,
+		name: 'Logo',
+		taxonomy: null,
+		has_variations: true,
+		terms: [
+			{
+				id: 0,
+				name: 'Yes',
+				slug: 'Yes',
+			},
+			{
+				id: 0,
+				name: 'No',
+				slug: 'No',
+			},
+		],
+	},
+	{
+		id: 0,
+		name: 'Non-variable attribute',
+		taxonomy: null,
+		has_variations: false,
+		terms: [
+			{
+				id: 0,
+				name: 'Test',
+				slug: 'Test',
+			},
+			{
+				id: 0,
+				name: 'Test 2',
+				slug: 'Test 2',
+			},
+		],
+	},
+];
+
+const rawVariations = [
+	{
+		id: 35,
+		attributes: [
+			{
+				name: 'Color',
+				value: 'blue',
+			},
+			{
+				name: 'Logo',
+				value: 'Yes',
+			},
+		],
+	},
+	{
+		id: 28,
+		attributes: [
+			{
+				name: 'Color',
+				value: 'red',
+			},
+			{
+				name: 'Logo',
+				value: 'No',
+			},
+		],
+	},
+	{
+		id: 29,
+		attributes: [
+			{
+				name: 'Color',
+				value: 'green',
+			},
+			{
+				name: 'Logo',
+				value: 'No',
+			},
+		],
+	},
+	{
+		id: 30,
+		attributes: [
+			{
+				name: 'Color',
+				value: 'blue',
+			},
+			{
+				name: 'Logo',
+				value: 'No',
+			},
+		],
+	},
+];
 
 describe( 'Testing utils', () => {
 	describe( 'Testing getAttributes()', () => {
