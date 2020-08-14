7ab1a0914289c667d42aab3a24bff5d64291fa96
Darren Ethier <darren@roughsmootheng.in>
2020-08-14 16:53:05 -0400

    add todo for followup issue to actually remove the function

--- assets/js/settings/shared/set-setting.js
+++ assets/js/settings/shared/set-setting.js
@@ -14,10 +14,16 @@ import deprecated from '@wordpress/deprecated';
  * @export
  * @param {string}   name                        The setting property key for the
  *                                               setting being mutated.
- * @param {*}    value                       The value to set.
+ * @param {*}        value                       The value to set.
  * @param {Function} [filter=( val ) => val]     Allows for providing a callback
  *                                               to sanitize the setting (eg.
  *                                               ensure it's a number)
+ *
+ * @todo  Remove setSetting function from `@woocommerce/settings`
+ *
+ *  The `wc.wcSettings.setSetting` function was deprecated beginning with WooCommerce Blocks 3.2.0
+ *  and can be removed completely with WooCommerce Blocks 3.8.0 (that gives 6 versions of the
+ *  feature plugin for warning and 2 versions of WooCommerce core).
  */
 export function setSetting( name, value, filter = ( val ) => val ) {
 	deprecated( 'setSetting', {
