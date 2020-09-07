4f347673f43ece37023c58451b56b3b29f8a3afb
Darren Ethier <darren@roughsmootheng.in>
2020-09-06 23:07:10 -0400

    fix config and rename e2e-tests to e2e
    - fixes the failing product-search test
    - tests/e2e-tests was redundant, I changed to `tests/e2e` (this follows a file pattern change made in woocommerce core as well).

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -9,20 +9,20 @@ module.exports = {
 
 	moduleNameMapper: {
 		'@woocommerce/e2e-tests/(.*)':
-			'<rootDir>/node_modules/woocommerce/tests/e2e-tests/$1',
+			'<rootDir>/node_modules/woocommerce/tests/e2e/$1',
 		'@woocommerce/blocks-test-utils': '<rootDir>/tests/utils',
 	},
 
 	// Where to look for test files
-	roots: [ '<rootDir>/tests/e2e-tests/specs' ],
-	globalSetup: '<rootDir>/tests/e2e-tests/config/setup.js',
-	globalTeardown: '<rootDir>/tests/e2e-tests/config/teardown.js',
+	roots: [ '<rootDir>/tests/e2e/specs' ],
+	globalSetup: '<rootDir>/tests/e2e/config/setup.js',
+	globalTeardown: '<rootDir>/tests/e2e/config/teardown.js',
 	setupFiles: [],
 	// A list of paths to modules that run some code to configure or set up the testing framework
 	// before each test
 	setupFilesAfterEnv: [
-		'<rootDir>/tests/e2e-tests/config/custom-matchers/index.js',
-		'<rootDir>/tests/e2e-tests/config/jest.setup.js',
+		'<rootDir>/tests/e2e/config/custom-matchers/index.js',
+		'<rootDir>/tests/e2e/config/jest.setup.js',
 		'expect-puppeteer',
 	],
 
