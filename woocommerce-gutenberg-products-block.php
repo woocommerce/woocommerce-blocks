e03d6757fe2d07587ed364daee2326292b60fda2
Darren Ethier <darren@roughsmootheng.in>
2020-09-14 14:43:51 -0400

    change tested up to for WooCommerce core

--- woocommerce-gutenberg-products-block.php
+++ woocommerce-gutenberg-products-block.php
@@ -10,7 +10,7 @@
  * Requires at least: 5.3
  * Requires PHP: 5.6
  * WC requires at least: 4.2
- * WC tested up to: 4.4
+ * WC tested up to: 4.5
  *
  * @package WooCommerce\Blocks
  * @internal This file is only used when running as a feature plugin.
