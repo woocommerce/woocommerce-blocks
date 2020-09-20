765d0b723f881f320e4fde95c6f5933f07730928
Darren Ethier <darren@roughsmootheng.in>
2020-09-20 14:35:19 -0400

    re-enable dependency grouping linting and fix errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -54,6 +54,5 @@ module.exports = {
 		'@wordpress/valid-sprintf': 'off',
 		'@wordpress/no-unused-vars-before-return': 'off',
 		'testing-library/no-await-sync-query': 'off',
-		'@woocommerce/dependency-group': 'off',
 	},
 };
