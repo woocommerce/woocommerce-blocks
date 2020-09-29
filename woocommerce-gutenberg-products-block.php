ab6e9fc5aca36859acfb541166c74f8349326c03
Seghir Nadir <nadir.seghir@gmail.com>
2020-09-29 14:32:52 +0100

    update php and wc versions

--- woocommerce-gutenberg-products-block.php
+++ woocommerce-gutenberg-products-block.php
@@ -8,8 +8,8 @@
  * Author URI: https://woocommerce.com
  * Text Domain:  woo-gutenberg-products-block
  * Requires at least: 5.3
- * Requires PHP: 5.6
- * WC requires at least: 4.2
+ * Requires PHP: 7.0
+ * WC requires at least: 4.3
  * WC tested up to: 4.5
  *
  * @package WooCommerce\Blocks
