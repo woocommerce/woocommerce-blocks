woocommerce/woocommerce-blocks

commit f3b2b53435ffbc9ac49215846bad08ce74c7124b

Refactor payment methods components (#3226)
* Move 'renderedTabs' to its own component

* Move getRenderedTab to its own component

* Remove 'getPaymentMethod' function

* Fix wrong typedef

* Remove unnecessary useRef

* Simplify NewPaymentMethodTab

* Drop 'New' prefix from components name

* Add JSDocs to new components

* Add propTypes to new components

assets/js/base/components/payment-methods/payment-method-tab.js
@@ -1,0 +1,61 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import {
+	useCheckoutContext,
+	useEditorContext,
+	usePaymentMethodDataContext,
+} from '@woocommerce/base-context';
+import CheckboxControl from '@woocommerce/base-components/checkbox-control';
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import PaymentMethodErrorBoundary from './payment-method-error-boundary';
+
+/**
+ * Component used to render the contents of a payment method tab.
+ *
+ * @param {Object}  props              Incoming props for the component.
+ * @param {boolean} props.allowsSaving Whether that payment method allows saving
+ *                                     the data for future purchases.
+ * @param {Object}  props.children     Content of the payment method tab.
+ *
+ * @return {*} The rendered component.
+ */
+const PaymentMethodTab = ( { children, allowsSaving } ) => {
+	const { isEditor } = useEditorContext();
+	const {
+		shouldSavePayment,
+		setShouldSavePayment,
+	} = usePaymentMethodDataContext();
+	const { customerId } = useCheckoutContext();
+
+	return (
+		<PaymentMethodErrorBoundary isEditor={ isEditor }>
+			{ children }
+			{ customerId > 0 && allowsSaving && (
+				<CheckboxControl
+					className="wc-block-components-payment-methods__save-card-info"
+					label={ __(
+						'Save payment information to my account for future purchases.',
+						'woo-gutenberg-products-block'
+					) }
+					checked={ shouldSavePayment }
+					onChange={ () =>
+						setShouldSavePayment( ! shouldSavePayment )
+					}
+				/>
+			) }
+		</PaymentMethodErrorBoundary>
+	);
+};
+
+PaymentMethodTab.propTypes = {
+	allowsSaving: PropTypes.string,
+	children: PropTypes.node,
+};
+
+export default PaymentMethodTab;

assets/js/base/components/payment-methods/payment-method-options.js
@@ -1,0 +1,87 @@
+/**
+ * External dependencies
+ */
+import {
+	usePaymentMethods,
+	usePaymentMethodInterface,
+	useStoreNotices,
+	useEmitResponse,
+} from '@woocommerce/base-hooks';
+import { cloneElement } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import {
+	useEditorContext,
+	usePaymentMethodDataContext,
+} from '@woocommerce/base-context';
+
+/**
+ * Internal dependencies
+ */
+import Tabs from '../tabs';
+import PaymentMethodTab from './payment-method-tab';
+
+/**
+ * Component used to render all non-saved payment method options.
+ *
+ * @return {*} The rendered component.
+ */
+const PaymentMethodOptions = () => {
+	const { setActivePaymentMethod } = usePaymentMethodDataContext();
+	const { paymentMethods } = usePaymentMethods();
+	const {
+		activePaymentMethod,
+		...paymentMethodInterface
+	} = usePaymentMethodInterface();
+	const { noticeContexts } = useEmitResponse();
+	const { removeNotice } = useStoreNotices();
+	const { isEditor } = useEditorContext();
+
+	return (
+		<Tabs
+			className="wc-block-components-checkout-payment-methods"
+			onSelect={ ( tabName ) => {
+				setActivePaymentMethod( tabName );
+				removeNotice( 'wc-payment-error', noticeContexts.PAYMENTS );
+			} }
+			tabs={ Object.keys( paymentMethods ).map( ( name ) => {
+				const {
+					ariaLabel,
+					edit,
+					content,
+					label,
+					supports,
+				} = paymentMethods[ name ];
+				const component = isEditor ? edit : content;
+				return {
+					name,
+					title:
+						typeof label === 'string'
+							? label
+							: cloneElement( label, {
+									components:
+										paymentMethodInterface.components,
+							  } ),
+					ariaLabel,
+					content: (
+						<PaymentMethodTab
+							allowsSaving={ supports.savePaymentInfo }
+						>
+							{ cloneElement( component, {
+								activePaymentMethod,
+								...paymentMethodInterface,
+							} ) }
+						</PaymentMethodTab>
+					),
+				};
+			} ) }
+			initialTabName={ activePaymentMethod }
+			ariaLabel={ __(
+				'Payment Methods',
+				'woo-gutenberg-products-block'
+			) }
+			id="wc-block-payment-methods"
+		/>
+	);
+};
+
+export default PaymentMethodOptions;

assets/js/base/components/payment-methods/payment-methods.js
@@ -1,50 +1,15 @@
 /**
  * External dependencies
  */
-import {
-	usePaymentMethods,
-	usePaymentMethodInterface,
-	useStoreNotices,
-	useEmitResponse,
-} from '@woocommerce/base-hooks';
-import {
-	cloneElement,
-	useRef,
-	useEffect,
-	useCallback,
-} from '@wordpress/element';
-import { __ } from '@wordpress/i18n';
-import {
-	useCheckoutContext,
-	useEditorContext,
-	usePaymentMethodDataContext,
-} from '@woocommerce/base-context';
-import CheckboxControl from '@woocommerce/base-components/checkbox-control';
+import { usePaymentMethods } from '@woocommerce/base-hooks';
+import { usePaymentMethodDataContext } from '@woocommerce/base-context';
 
 /**
  * Internal dependencies
  */
-import Tabs from '../tabs';
 import NoPaymentMethods from './no-payment-methods';
+import PaymentMethodOptions from './payment-method-options';
 import SavedPaymentMethodOptions from './saved-payment-method-options';
-import PaymentMethodErrorBoundary from './payment-method-error-boundary';
-
-/**
- * Returns a payment method for the given context.
- *
- * @param {string} name The payment method slug to return.
- * @param {Object} paymentMethods The current registered payment methods
- * @param {boolean} isEditor Whether in the editor context (true) or not (false).
- *
- * @return {Object} The payment method matching the name for the given context.
- */
-const getPaymentMethod = ( name, paymentMethods, isEditor ) => {
-	let paymentMethod = paymentMethods[ name ] || null;
-	if ( paymentMethod ) {
-		paymentMethod = isEditor ? paymentMethod.edit : paymentMethod.content;
-	}
-	return paymentMethod;
-};
 
 /**
  * PaymentMethods component.
@@ -52,124 +17,25 @@ const getPaymentMethod = ( name, paymentMethods, isEditor ) => {
  * @return {*} The rendered component.
  */
 const PaymentMethods = () => {
-	const { isEditor } = useEditorContext();
 	const {
 		customerPaymentMethods = {},
 		paymentMethodData,
-		setActivePaymentMethod,
-		shouldSavePayment,
-		setShouldSavePayment,
 	} = usePaymentMethodDataContext();
 	const { isInitialized, paymentMethods } = usePaymentMethods();
-	const currentPaymentMethods = useRef( paymentMethods );
-	const {
-		activePaymentMethod,
-		...paymentMethodInterface
-	} = usePaymentMethodInterface();
-	const currentPaymentMethodInterface = useRef( paymentMethodInterface );
-	const { noticeContexts } = useEmitResponse();
-	const { removeNotice } = useStoreNotices();
-	const { customerId } = useCheckoutContext();
 
-	// update ref on change.
-	useEffect( () => {
-		currentPaymentMethods.current = paymentMethods;
-		currentPaymentMethodInterface.current = paymentMethodInterface;
-	}, [ paymentMethods, paymentMethodInterface, activePaymentMethod ] );
-
-	const getRenderedTab = useCallback(
-		( selectedTab ) => {
-			const paymentMethod = getPaymentMethod(
-				selectedTab,
-				currentPaymentMethods.current,
-				isEditor
-			);
-			const { supports = {} } =
-				paymentMethod &&
-				currentPaymentMethods.current[ activePaymentMethod ]
-					? currentPaymentMethods.current[ activePaymentMethod ]
-					: {};
-			return paymentMethod && activePaymentMethod ? (
-				<PaymentMethodErrorBoundary isEditor={ isEditor }>
-					{ cloneElement( paymentMethod, {
-						activePaymentMethod,
-						...currentPaymentMethodInterface.current,
-					} ) }
-					{ customerId > 0 && supports.savePaymentInfo && (
-						<CheckboxControl
-							className="wc-block-components-payment-methods__save-card-info"
-							label={ __(
-								'Save payment information to my account for future purchases.',
-								'woo-gutenberg-products-block'
-							) }
-							checked={ shouldSavePayment }
-							onChange={ () =>
-								setShouldSavePayment( ! shouldSavePayment )
-							}
-						/>
-					) }
-				</PaymentMethodErrorBoundary>
-			) : null;
-		},
-		[
-			isEditor,
-			activePaymentMethod,
-			shouldSavePayment,
-			setShouldSavePayment,
-			customerId,
-		]
-	);
-	if (
-		isInitialized &&
-		Object.keys( currentPaymentMethods.current ).length === 0
-	) {
+	if ( isInitialized && Object.keys( paymentMethods ).length === 0 ) {
 		return <NoPaymentMethods />;
 	}
-	const renderedTabs = (
-		<Tabs
-			className="wc-block-components-checkout-payment-methods"
-			onSelect={ ( tabName ) => {
-				setActivePaymentMethod( tabName );
-				removeNotice( 'wc-payment-error', noticeContexts.PAYMENTS );
-			} }
-			tabs={ Object.keys( paymentMethods ).map( ( name ) => {
-				const { label, ariaLabel } = paymentMethods[ name ];
-				return {
-					name,
-					title:
-						typeof label === 'string'
-							? label
-							: cloneElement( label, {
-									components:
-										currentPaymentMethodInterface.current
-											.components,
-							  } ),
-					ariaLabel,
-					content: getRenderedTab( name ),
-				};
-			} ) }
-			initialTabName={ activePaymentMethod }
-			ariaLabel={ __(
-				'Payment Methods',
-				'woo-gutenberg-products-block'
-			) }
-			id="wc-block-payment-methods"
-		/>
-	);
-
-	const renderedSavedPaymentOptions = <SavedPaymentMethodOptions />;
-
-	const renderedTabsAndSavedPaymentOptions = (
-		<>
-			{ renderedSavedPaymentOptions }
-			{ renderedTabs }
-		</>
-	);
 
 	return Object.keys( customerPaymentMethods ).length > 0 &&
-		paymentMethodData.isSavedToken
-		? renderedSavedPaymentOptions
-		: renderedTabsAndSavedPaymentOptions;
+		paymentMethodData.isSavedToken ? (
+		<SavedPaymentMethodOptions />
+	) : (
+		<>
+			<SavedPaymentMethodOptions />
+			<PaymentMethodOptions />
+		</>
+	);
 };
 
 export default PaymentMethods;

assets/js/type-defs/registered-payment-method-props.js
@@ -199,7 +199,7 @@
  *                                                                 observer response objects.
  * @property {Function}                   [onSubmit]               Used to trigger checkout
  *                                                                 processing.
- * @property {string}                     [activePaymentMethod]    Indicates what the active payment
+ * @property {string}                     activePaymentMethod      Indicates what the active payment
  *                                                                 method is.
  * @property {ComponentProps}             components               Components exposed to payment
  *                                                                 methods for use.

commit 2d2c6b1df9513f61183d416119d361053ccaf514

Fix wrong propType introduced in #3226

assets/js/base/components/payment-methods/payment-method-tab.js
@@ -54,7 +54,7 @@ const PaymentMethodTab = ( { children, allowsSaving } ) => {
 };
 
 PaymentMethodTab.propTypes = {
-	allowsSaving: PropTypes.string,
+	allowsSaving: PropTypes.bool,
 	children: PropTypes.node,
 };
 