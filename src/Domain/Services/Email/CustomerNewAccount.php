256fed1fde099ceb96edacc1c05b29bef04cc355
Rua Haszard <rua.haszard@automattic.com>
2020-10-07 15:54:59 +1300

    use my-account/lost-password for setting password in checkout signup: - this has been available forever - no need for a version check - page will show `Lost password`; looking at options for overriding that

--- src/Domain/Services/Email/CustomerNewAccount.php
+++ src/Domain/Services/Email/CustomerNewAccount.php
@@ -3,7 +3,6 @@ namespace Automattic\WooCommerce\Blocks\Domain\Services\Email;
 
 use \WP_User;
 use \WC_Email;
-use \WC_Logger;
 use Automattic\WooCommerce\Blocks\Domain\Package;
 
 /**
@@ -97,24 +96,13 @@ class CustomerNewAccount extends \WC_Email {
 	public function trigger( $user_id, $user_pass = '', $password_generated = false ) {
 		$this->setup_locale();
 
-		$log = new WC_Logger();
-
 		if ( $user_id ) {
 			$this->object = new \WP_User( $user_id );
 
 			// Generate a magic link so user can set initial password.
 			$key = get_password_reset_key( $this->object );
 			if ( ! is_wp_error( $key ) ) {
-				if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '4.7', '<' ) ) {
-					// Fall back to wp-admin/unbranded reset password route/handler.
-					$this->set_password_url = network_site_url(
-						"wp-login.php?action=rp&key=$key&login=" . rawurlencode( $this->object->user_login ),
-						'login'
-					);
-				} else {
-					// From v4.7 (TBC) WooCommerce core has a themed set-password endpoint we can use.
-					$this->set_password_url = wc_get_account_endpoint_url( 'set-password' ) . "?action=rp&key=$key&login=" . rawurlencode( $this->object->user_login );
-				}
+				$this->set_password_url = wc_get_account_endpoint_url( 'lost-password' ) . "?action=rp&key=$key&login=" . rawurlencode( $this->object->user_login );
 			}
 
 			$this->user_login = stripslashes( $this->object->user_login );
