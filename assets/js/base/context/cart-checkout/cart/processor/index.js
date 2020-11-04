77140321c9f6ef8b1bc7a8388fa7860b601adbef
Mike Jolley <mike.jolley@me.com>
2020-11-04 15:51:24 +0000

    Cart does not have billing

--- assets/js/base/context/cart-checkout/cart/processor/index.js
+++ assets/js/base/context/cart-checkout/cart/processor/index.js
@@ -5,10 +5,7 @@ import { useDispatch } from '@wordpress/data';
 import { useEffect, useRef } from '@wordpress/element';
 import { useStoreNotices } from '@woocommerce/base-hooks';
 import { CART_STORE_KEY as storeKey } from '@woocommerce/block-data';
-import {
-	useShippingDataContext,
-	useBillingDataContext,
-} from '@woocommerce/base-context';
+import { useShippingDataContext } from '@woocommerce/base-context';
 import { useDebounce } from 'use-debounce';
 
 /**
@@ -21,12 +18,9 @@ import { shouldUpdateAddressStore } from './utils';
  */
 const CartProcessor = () => {
 	const { addErrorNotice } = useStoreNotices();
-	const { billingData } = useBillingDataContext();
 	const { shippingAddress } = useShippingDataContext();
 	const { updateCustomerAddress } = useDispatch( storeKey );
-	const previousBillingData = useRef( billingData );
 	const previousShippingAddress = useRef( shippingAddress );
-	const [ debouncedBillingData ] = useDebounce( billingData, 400 );
 	const [ debouncedShippingAddress ] = useDebounce( shippingAddress, 400 );
 
 	// When the billing or shipping address changes we need to push the changes to the server to
@@ -35,24 +29,14 @@ const CartProcessor = () => {
 	useEffect( () => {
 		if (
 			shouldUpdateAddressStore(
-				previousBillingData.current,
-				billingData
-			) ||
-			shouldUpdateAddressStore(
 				previousShippingAddress.current,
 				shippingAddress
 			)
 		) {
-			previousBillingData.current = billingData;
 			previousShippingAddress.current = shippingAddress;
 
 			updateCustomerAddress( {
 				shipping_address: shippingAddress,
-				billing_address: {
-					...billingData,
-					phone: billingData.phone || null,
-					email: billingData.email || null,
-				},
 			} ).catch( ( error ) => {
 				addErrorNotice( error.message, {
 					id: 'checkout',
@@ -60,11 +44,9 @@ const CartProcessor = () => {
 			} );
 		}
 	}, [
-		debouncedBillingData,
 		debouncedShippingAddress,
 		addErrorNotice,
 		updateCustomerAddress,
-		billingData,
 		shippingAddress,
 	] );
 
