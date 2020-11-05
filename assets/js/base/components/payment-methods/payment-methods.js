bd5343ad501b346429e1f05ac665c4274cf0da5e
budzanowski <bartosz.budzanowski@gmail.com>
2020-11-05 15:34:31 +0100

    Add label for not saved payment methods option.

--- assets/js/base/components/payment-methods/payment-methods.js
+++ assets/js/base/components/payment-methods/payment-methods.js
@@ -3,6 +3,8 @@
  */
 import { usePaymentMethods } from '@woocommerce/base-hooks';
 import { useCallback, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import Label from '@woocommerce/base-components/label';
 
 /**
  * Internal dependencies
@@ -35,6 +37,20 @@ const PaymentMethods = () => {
 	return (
 		<>
 			<SavedPaymentMethodOptions onChange={ onChange } />
+			<Label
+				label={ __(
+					'Use another payment method',
+					'woo-gutenberg-products-block'
+				) }
+				screenReaderLabel={ __(
+					'Other available payment methods',
+					'woo-gutenberg-products-block'
+				) }
+				wrapperElement="p"
+				wrapperProps={ {
+					className: 'wc-block-components-checkout-step__description',
+				} }
+			/>
 			{ showNewPaymentMethods && <PaymentMethodOptions /> }
 		</>
 	);
