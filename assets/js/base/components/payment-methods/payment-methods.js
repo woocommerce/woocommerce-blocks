3f5ebbcfeb7a14495fca7be14078374a8add7132
budzanowski <bartosz.budzanowski@gmail.com>
2020-11-07 13:17:39 +0100

    Hide label if we don't have saved methods.

--- assets/js/base/components/payment-methods/payment-methods.js
+++ assets/js/base/components/payment-methods/payment-methods.js
@@ -5,6 +5,7 @@ import { usePaymentMethods } from '@woocommerce/base-hooks';
 import { useCallback, useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import Label from '@woocommerce/base-components/label';
+import { usePaymentMethodDataContext } from '@woocommerce/base-context';
 
 /**
  * Internal dependencies
@@ -22,6 +23,7 @@ const PaymentMethods = () => {
 	const { isInitialized, paymentMethods } = usePaymentMethods();
 	const [ savedMethodsToken, setSavedMethodsToken ] = useState( '' );
 	const [ paymentMethodsName, setPaymentMethodsName ] = useState( '' );
+	const { customerPaymentMethods } = usePaymentMethodDataContext()
 
 	const onChangeSavedMethods = useCallback(
 		( token ) => {
@@ -49,20 +51,22 @@ const PaymentMethods = () => {
 				selectedToken={ savedMethodsToken }
 				onChange={ onChangeSavedMethods }
 			/>
-			<Label
-				label={ __(
-					'Use another payment method',
-					'woo-gutenberg-products-block'
-				) }
-				screenReaderLabel={ __(
-					'Other available payment methods',
-					'woo-gutenberg-products-block'
-				) }
-				wrapperElement="p"
-				wrapperProps={ {
-					className: 'wc-block-components-checkout-step__description',
-				} }
-			/>
+			{ Object.keys( customerPaymentMethods).length > 0 && (
+				<Label
+					label={ __(
+						'Use another payment method',
+						'woo-gutenberg-products-block'
+					) }
+					screenReaderLabel={ __(
+						'Other available payment methods',
+						'woo-gutenberg-products-block'
+					) }
+					wrapperElement="p"
+					wrapperProps={ {
+						className: 'wc-block-components-checkout-step__description',
+					} }
+				/>
+			) }
 			<PaymentMethodOptions
 				selectedMethod={ paymentMethodsName }
 				onChange={ onChangePaymentMethods }
