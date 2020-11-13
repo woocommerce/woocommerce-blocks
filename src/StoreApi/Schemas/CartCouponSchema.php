14a89d077f45837b3b5d34cfc0cdb36dea4e1467
Rua Haszard <rua.haszard@automattic.com>
2020-11-13 14:41:21 +1300

    add comment documenting when discount_type appeared in API response

--- src/StoreApi/Schemas/CartCouponSchema.php
+++ src/StoreApi/Schemas/CartCouponSchema.php
@@ -8,6 +8,7 @@ use Automattic\WooCommerce\Blocks\StoreApi\Utilities\CartController;
  *
  * @internal This API is used internally by Blocks--it is still in flux and may be subject to revisions.
  * @since 2.5.0
+ * @since $VID:$ Coupon type (`discount_type`) added.
  */
 class CartCouponSchema extends AbstractSchema {
 	/**
