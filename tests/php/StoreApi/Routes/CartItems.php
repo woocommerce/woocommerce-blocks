073666cbcdb57b4521dbfbc75c443f557f6a2134
Thomas Roberts <thomas.roberts@automattic.com>
2020-11-17 14:50:27 +0000

    Add catalog_visibility to CartItemSchema.php
    This is used to get whether the product is visible in the catalogue, visible in the shop only, visible in search results only, or visible everywhere. We need to know this so we can pass it to the ProductImage and ProductName components.

--- tests/php/StoreApi/Routes/CartItems.php
+++ tests/php/StoreApi/Routes/CartItems.php
@@ -244,6 +244,7 @@ class CartItems extends TestCase {
 		$this->assertArrayHasKey( 'backorders_allowed', $data );
 		$this->assertArrayHasKey( 'show_backorder_badge', $data );
 		$this->assertArrayHasKey( 'short_description', $data );
+		$this->assertArrayHasKey( 'catalog_visibility', $data );
 	}
 
 	/**
