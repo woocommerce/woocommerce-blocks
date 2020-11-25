78f44a5f809f050d2774fdf12f8870408490e2b1
Mike Jolley <mike.jolley@me.com>
2020-11-25 16:42:05 +0000

    --no-keep-empty

--- bin/fix-package-lock.sh
+++ bin/fix-package-lock.sh
@@ -36,7 +36,7 @@ fi
 
 status "Rebasing branch with trunk...";
 
-if ! git rebase -Xours origin/trunk
+if ! git rebase -Xours origin/trunk --no-keep-empty
 then
 	error "Unable to rebase branch";
 else
