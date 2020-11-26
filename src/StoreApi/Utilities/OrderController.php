3dbf98e683336a6fccb93bbc07dbf91a7bff676e
Rua Haszard <rua.haszard@automattic.com>
2020-11-27 11:07:57 +1300

    fix MIA order validation/errors (due to rebase): - reinstate thrown exception when validating order - return exception was an experiment, now solved in #3454

--- src/StoreApi/Utilities/OrderController.php
+++ src/StoreApi/Utilities/OrderController.php
@@ -139,7 +139,7 @@ class OrderController {
 			$this->update_order_from_cart( $order );
 
 			// Return exception so customer can review before payment.
-			return new RouteException(
+			throw new RouteException(
 				'woocommerce_rest_cart_coupon_errors',
 				sprintf(
 					// Translators: %s Coupon codes.
