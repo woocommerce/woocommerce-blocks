0b39a56f83b8ad0159d131b82d7dca4f9222fa7d
Thomas Roberts <thomas.roberts@automattic.com>
2020-11-27 17:04:36 +0000

    Improve test for insufficient (but still in stock) product stock case

--- tests/php/StoreApi/Utilities/OrderController.php
+++ tests/php/StoreApi/Utilities/OrderController.php
@@ -15,13 +15,28 @@ class OrderControllerTests extends TestCase {
 
 	public function test_validate_order_before_payment() {
 		$class = new OrderController();
-		$product = ProductHelper::create_simple_product();
+
+		// This product will simply be in/out of stock.
+		$unmanaged_product = ProductHelper::create_simple_product();
+
+		//This product will have exact levels of stock known.
+		$managed_product = ProductHelper::create_simple_product();
+		$managed_product->set_manage_stock( true );
+		$managed_product->set_stock_quantity( 2 );
+
 		wc()->cart->empty_cart();
-		wc()->cart->add_to_cart( $product->get_id() );
+		wc()->cart->add_to_cart( $unmanaged_product->get_id() );
+		wc()->cart->add_to_cart( $managed_product->get_id(), 2 );
 		$order = $class->create_order_from_cart();
-		$product->set_stock_status( 'outofstock' );
-		$product->save_meta_data();
-		$product->save();
+
+		$unmanaged_product->set_stock_status( 'outofstock' );
+		$unmanaged_product->save_meta_data();
+		$unmanaged_product->save();
+
+		$managed_product->set_stock_quantity( 1 );
+		$managed_product->save_meta_data();
+		$managed_product->save();
+
 		$this->expectException( RouteException::class );
 		$class->validate_order_before_payment( $order );
 	}
