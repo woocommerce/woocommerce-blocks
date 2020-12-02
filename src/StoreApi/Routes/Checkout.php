e790dde9938076490ba7720f48138123db750ee8
Mike Jolley <mike.jolley@me.com>
2020-12-02 10:54:37 +0000

    Remove root slash

--- src/StoreApi/Routes/Checkout.php
+++ src/StoreApi/Routes/Checkout.php
@@ -111,11 +111,11 @@ class Checkout extends AbstractRoute {
 	/**
 	 * Prepare a single item for response. Handles setting the status based on the payment result.
 	 *
-	 * @param mixed            $item Item to format to schema.
-	 * @param \WP_REST_Request $request Request object.
-	 * @return \WP_REST_Response $response Response data.
+	 * @param mixed           $item Item to format to schema.
+	 * @param WP_REST_Request $request Request object.
+	 * @return WP_REST_Response $response Response data.
 	 */
-	public function prepare_item_for_response( $item, \WP_REST_Request $request ) {
+	public function prepare_item_for_response( $item, WP_REST_Request $request ) {
 		$response = parent::prepare_item_for_response( $item, $request );
 
 		if ( isset( $item->payment_result ) && $item->payment_result instanceof PaymentResult ) {
