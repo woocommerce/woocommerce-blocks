6e4c15d474ba0f4fddee2fc7158c57076cf0bf7a
Seghir Nadir <nadir.seghir@gmail.com>
2020-12-04 12:38:02 +0100

    wip

--- assets/js/base/components/sidebar-layout/sidebar.js
+++ assets/js/base/components/sidebar-layout/sidebar.js
@@ -3,12 +3,14 @@
  */
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+import { ExperimentalOrderMeta } from '@woocommerce/checkout';
 
 const Sidebar = ( { children, className } ) => {
 	return (
 		<div
 			className={ classNames( 'wc-block-components-sidebar', className ) }
 		>
+			<ExperimentalOrderMeta>Render me</ExperimentalOrderMeta>
 			{ children }
 		</div>
 	);
