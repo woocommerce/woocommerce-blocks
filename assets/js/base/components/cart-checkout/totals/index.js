ba03e2331e5d694373b52a3f43c76569a32d9ad0
Albert Juhé Lluveras <contact@albertjuhe.com>
2020-12-07 12:27:56 +0100

    Rename TotalsFooterItem folder

--- assets/js/base/components/cart-checkout/totals/index.js
+++ assets/js/base/components/cart-checkout/totals/index.js
@@ -2,7 +2,7 @@ export { default as Subtotal } from './subtotal';
 export { default as TotalsCoupon } from './coupon';
 export { default as TotalsDiscount } from './discount';
 export { default as TotalsFees } from './fees';
-export { default as TotalsFooterItem } from './totals-footer-item';
+export { default as TotalsFooterItem } from './footer-item';
 export { default as TotalsItem } from './totals-item';
 export { default as TotalsShippingItem } from './totals-shipping-item';
 export { default as TotalsTaxesItem } from './totals-taxes-item';
