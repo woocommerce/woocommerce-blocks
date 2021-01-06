9b6af7178cbef05589779ef7f5a750d53e7fc8cf
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-01-06 15:16:51 +0100

    Remove example code

--- assets/js/base/components/sidebar-layout/sidebar.js
+++ assets/js/base/components/sidebar-layout/sidebar.js
@@ -3,19 +3,12 @@
  */
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
-import {
-	ExperimentalOrderMeta,
-	TotalsItem,
-} from '@woocommerce/blocks-checkout';
 
 const Sidebar = ( { children, className } ) => {
 	return (
 		<div
 			className={ classNames( 'wc-block-components-sidebar', className ) }
 		>
-			<ExperimentalOrderMeta>
-				<TotalsItem label="My extension" value={ <span>0</span> } />
-			</ExperimentalOrderMeta>
 			{ children }
 		</div>
 	);
--- packages/checkout/index.js
+++ packages/checkout/index.js
@@ -1,4 +1,3 @@
-export { default as ExperimentalOrderMeta } from './order-meta';
 export * from './totals';
 
 export { SlotFillProvider } from 'wordpress-components';
