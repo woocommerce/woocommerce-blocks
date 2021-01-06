05619f6d5f2331593b87fcd5d9467c67a47f3393
Thomas Roberts <thomas.roberts@automattic.com>
2021-01-06 13:00:06 +0000

    Catch new out of stock exceptions when checking the cart for errors
    This is so we can get the cart sent back to the client, if we don't catch these, then the route will just return a 500 error and crash.

--- src/StoreApi/Utilities/CartController.php
+++ src/StoreApi/Utilities/CartController.php
@@ -344,6 +344,12 @@ class CartController {
 				$this->validate_cart_item( $cart_item );
 			} catch ( RouteException $error ) {
 				$errors[] = new \WP_Error( $error->getErrorCode(), $error->getMessage() );
+			} catch ( NotPurchasableException $error ) {
+				$errors[] = new \WP_Error( $error->getErrorCode(), $error->getMessage() );
+			} catch ( OutOfStockException $error ) {
+				$errors[] = new \WP_Error( $error->getErrorCode(), $error->getMessage() );
+			} catch ( PartialOutOfStockException $error ) {
+				$errors[] = new \WP_Error( $error->getErrorCode(), $error->getMessage() );
 			}
 		}
 
