b12a82cc799bc16c52189efca4df1137abfb2988
Mike Jolley <mike.jolley@me.com>
2021-01-08 11:34:37 +0000

    Use new helper in address form

--- assets/js/base/components/cart-checkout/address-form/prepare-address-fields.js
+++ assets/js/base/components/cart-checkout/address-form/prepare-address-fields.js
@@ -14,7 +14,7 @@ import countryAddressFields from './country-address-fields';
 /**
  * Combines address fields, including fields from the locale, and sorts them by index.
  *
- * This uses lodash merge rather than teh spread operator because we want to merge address field data rather than overwrite it, if overrides are used.
+ * This uses lodash merge rather than the spread operator because we want to merge address field data rather than overwrite it, if overrides are used.
  *
  * @param {Object} fieldConfig Field config contains field specific overrides at block level which may, for example, hide a field.
  * @param {string} addressCountry Address country code. If unknown, locale fields will not be merged.
