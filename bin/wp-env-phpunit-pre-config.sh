4fac160c3c59ce29bb6fba408bb2da3db58a13fe
Darren Ethier <darren@roughsmootheng.in>
2021-01-16 20:28:12 -0500

    change how woo tests files are pulled in
    - /tmp is not accessible in gh actions.

--- bin/wp-env-phpunit-pre-config.sh
+++ bin/wp-env-phpunit-pre-config.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 BASENAME=$(basename "`pwd`")
-TMPDIR=${TMPDIR-/tmp}
-WOO_DIR="./bin/woocommerce"
-# Grab Woo test helpers and move into directory (only if needed)
+WOO_DIR=$(PWD)
+WOO_DIR="$WOO_DIR/bin/woocommerce"
+echo $WOO_DIR
+# Clone Woo Core so tests helpers are available (the `tests` dir is mapped in
+# the `.wp-env.json` file)
 # TODO: This is always bringing in the latest test helpers, eventually we'll
 #       need to bring in the helpers for the version of Woo being tested.
-if [ ! -d '${WOO_DIR}/tests' ]; then
-	git clone --depth 1 "https://github.com/woocommerce/woocommerce.git" "$TMPDIR/woocommerce-git"
-	mv "$TMPDIR/woocommerce-git/tests" "$WOO_DIR"
-	## cleanup
-	rm -rf "$TMPDIR/woocommerce-git"
+# TODO: Local installs won't refresh this automatically. For now I wanted to
+#       avoid excessive unnecessary cloning, but we probably should have this
+#       refreshed somehow (maybe based on age of directory?).
+if [ ! -d $WOO_DIR ]; then
+	git clone --depth 1 "https://github.com/woocommerce/woocommerce.git" "$WOO_DIR"
 fi
 npm run wp-env run phpunit 'php -v'
 npm run wp-env run phpunit 'phpunit -c /var/www/html/wp-content/plugins/'$BASENAME'/phpunit.xml.dist --verbose'
