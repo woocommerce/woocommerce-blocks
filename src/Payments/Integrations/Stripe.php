5219744be8cb94becfb3b2c100ff1088a687fc96
Thomas Roberts <thomas.roberts@automattic.com>
2021-01-21 12:41:57 +0000

    Add requiresSaving option to Stripe payment method data
    This is informed by the saved_cards option and the result of the wc_stripe_display_save_payment_method_checkbox filter.

--- src/Payments/Integrations/Stripe.php
+++ src/Payments/Integrations/Stripe.php
@@ -81,21 +81,21 @@ final class Stripe extends AbstractPaymentMethodType {
 	 */
 	public function get_payment_method_data() {
 		return [
-			'stripeTotalLabel'                 => $this->get_total_label(),
-			'publicKey'                        => $this->get_publishable_key(),
-			'allowPrepaidCard'                 => $this->get_allow_prepaid_card(),
-			'title'                            => $this->get_title(),
-			'button'                           => [
+			'stripeTotalLabel'    => $this->get_total_label(),
+			'publicKey'           => $this->get_publishable_key(),
+			'allowPrepaidCard'    => $this->get_allow_prepaid_card(),
+			'title'               => $this->get_title(),
+			'button'              => [
 				'type'   => $this->get_button_type(),
 				'theme'  => $this->get_button_theme(),
 				'height' => $this->get_button_height(),
 				'locale' => $this->get_button_locale(),
 			],
-			'inline_cc_form'                   => $this->get_inline_cc_form(),
-			'icons'                            => $this->get_icons(),
-			'allowSavedCards'                  => $this->get_allow_saved_cards(),
-			'allowPaymentRequest'              => $this->get_allow_payment_request(),
-			'displaySavePaymentMethodCheckbox' => $this->get_display_save_payment_method_checkbox(),
+			'inline_cc_form'      => $this->get_inline_cc_form(),
+			'icons'               => $this->get_icons(),
+			'allowSavedCards'     => $this->get_allow_saved_cards(),
+			'allowPaymentRequest' => $this->get_allow_payment_request(),
+			'requiresSaving'      => $this->get_requires_saving(),
 		];
 	}
 
@@ -109,17 +109,18 @@ final class Stripe extends AbstractPaymentMethodType {
 	}
 
 	/**
-	 * Determine if the save payment method checkbox should be displayed.
+	 * Determine if the payment method requires saving. If saved_cards is set to yes and filters hide the save
+	 * payment method checkbox, we can assume that this method requires saving.
 	 *
 	 * @return bool True if the save payment checkbox should be displayed to the user.
 	 */
-	private function get_display_save_payment_method_checkbox() {
+	private function get_requires_saving() {
 		$saved_cards = $this->get_allow_saved_cards();
 		// This assumes that Stripe supports `tokenization` - currently this is true, based on
 		// https://github.com/woocommerce/woocommerce-gateway-stripe/blob/master/includes/class-wc-gateway-stripe.php#L95 .
 		// See https://github.com/woocommerce/woocommerce-gateway-stripe/blob/ad19168b63df86176cbe35c3e95203a245687640/includes/class-wc-gateway-stripe.php#L271 and
 		// https://github.com/woocommerce/woocommerce/wiki/Payment-Token-API .
-		return apply_filters( 'wc_stripe_display_save_payment_method_checkbox', filter_var( $saved_cards, FILTER_VALIDATE_BOOLEAN ) );
+		return ! apply_filters( 'wc_stripe_display_save_payment_method_checkbox', filter_var( $saved_cards, FILTER_VALIDATE_BOOLEAN ) );
 	}
 
 	/**
