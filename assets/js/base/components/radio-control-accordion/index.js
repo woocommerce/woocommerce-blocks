60a047daf32e40b11464129692f3c2c82ec6d2ef
budzanowski <bartosz.budzanowski@gmail.com>
2021-01-25 05:52:47 +0100

    Typecheck an option.

--- assets/js/base/components/radio-control-accordion/index.js
+++ assets/js/base/components/radio-control-accordion/index.js
@@ -28,7 +28,7 @@ const RadioControlAccordion = ( {
 				) }
 			>
 				{ options.map( ( option ) => {
-					const hasOptionContent = 'content' in option;
+					const hasOptionContent = ( typeof option === 'object' ) && 'content' in option;
 					const checked = option.value === selected;
 					return (
 						<div
