f653ded2cf1ad47aeace1476854df97513e61301
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-01-26 17:16:28 +0100

    Remove code targeting WC Subscriptions

--- packages/checkout/index.js
+++ packages/checkout/index.js
@@ -10,18 +10,3 @@ export const __EXPERIMENTAL_TOTAL_LABEL_FILTER =
 
 export const __EXPERIMENTAL_PRICE_FORMAT_FILTER =
 	'wcBlocks.__experimental_price_format_filter';
-
-// @todo move to WC Subscriptions
-import { __, sprintf } from '@wordpress/i18n';
-import { addFilter } from '@wordpress/hooks';
-
-addFilter(
-	__EXPERIMENTAL_PRICE_FORMAT_FILTER,
-	'woocommerce-subscriptions/set-totals-label',
-	( placeholder ) =>
-		sprintf(
-			/* translators: %s is the subscription price to pay immediately (ie: $10). */
-			__( 'Due today %s', 'woocommerce-subscriptions' ),
-			placeholder
-		)
-);
