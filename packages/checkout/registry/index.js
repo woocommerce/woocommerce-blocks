95103f8cc48e9255d4df1ba371fc87a2c713ff68
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-02-01 19:06:54 +0100

    Add docs comments

--- packages/checkout/registry/index.js
+++ packages/checkout/registry/index.js
@@ -1,4 +1,12 @@
 let checkoutFilters = {};
+
+/**
+ * Register filters for a specific extension.
+ *
+ * @param {string} namespace Name of the extension namespace.
+ * @param {Object} filters   Object of filters for that namespace. Each key of
+ *                           the object is the name of a filter.
+ */
 export const __experimentalRegisterCheckoutFilters = ( namespace, filters ) => {
 	checkoutFilters = {
 		...checkoutFilters,
@@ -6,6 +14,11 @@ export const __experimentalRegisterCheckoutFilters = ( namespace, filters ) => {
 	};
 };
 
+/**
+ * Get all filters with a specific name.
+ *
+ * @param {string} filterName   Name of the filter to search for.
+ */
 const getCheckoutFilters = ( filterName ) => {
 	const namespaces = Object.keys( checkoutFilters );
 	const filters = namespaces
@@ -14,6 +27,16 @@ const getCheckoutFilters = ( filterName ) => {
 	return filters;
 };
 
+/**
+ * Register a regular payment method.
+ *
+ * @param {string} filterName   Name of the filter to apply.
+ * @param {any}    defaultValue Default value to filter.
+ * @param {any}    args         Arguments to pass to registered functions.
+ * @param {any}    [validate]   Function that needs to return true when the
+ *                              filtered value is passed in order for the filter
+ *                              to be applied.
+ */
 export const __experimentalApplyCheckoutFilter = (
 	filterName,
 	defaultValue,
