e5f266ec129c955db1399cbdb88a2276baf6d7ba
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-02-02 14:14:53 +0100

    Add validation function

--- packages/checkout/index.js
+++ packages/checkout/index.js
@@ -2,6 +2,7 @@ export * from './totals';
 export * from './shipping';
 export * from './slot';
 export * from './registry';
+export * from './registry/validations.js';
 export { default as ExperimentalOrderMeta } from './order-meta';
 export { default as ExperimentalOrderShippingPackages } from './order-shipping-packages';
 export { default as Panel } from './panel';
