43c91a25778da50efcb3d127c864632920354ee7
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-02-10 11:13:06 +0100

    Use extendibility API instead of filters

--- packages/checkout/index.js
+++ packages/checkout/index.js
@@ -6,6 +6,3 @@ export { default as ExperimentalOrderMeta } from './order-meta';
 export { default as ExperimentalOrderShippingPackages } from './order-shipping-packages';
 export { default as Panel } from './panel';
 export { SlotFillProvider } from 'wordpress-components';
-
-export const __EXPERIMENTAL_CART_ITEM_PRICE_FILTER =
-	'wcBlocks.__experimental_cart_item_price_filter';
