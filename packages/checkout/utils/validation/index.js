e7291488b639ec3870ec9902d94d9822db04079f
Seghir Nadir <nadir.seghir@gmail.com>
2021-02-12 00:32:59 +0100

    add jsdoc

--- packages/checkout/utils/validation/index.js
+++ packages/checkout/utils/validation/index.js
@@ -3,6 +3,13 @@
  */
 import { __, sprintf } from '@wordpress/i18n';
 
+/**
+ * Checks if value passed is a string, throws an error if not.
+ *
+ * @param {string} value Value to be validated.
+ *
+ * @return {Error|true} Error if value is not string, true otherwise.
+ */
 export const mustBeString = ( value ) => {
 	if ( typeof value !== 'string' ) {
 		throw Error(
@@ -19,6 +26,14 @@ export const mustBeString = ( value ) => {
 	return true;
 };
 
+/**
+ * Checks if value passed contain passed label
+ *
+ * @param {string} value Value to be validated.
+ * @param {string} label Label to be searched for.
+ *
+ * @return {Error|true} Error if value contains label, true otherwise.
+ */
 export const mustContain = ( value, label ) => {
 	if ( ! value.includes( label ) ) {
 		throw Error(
