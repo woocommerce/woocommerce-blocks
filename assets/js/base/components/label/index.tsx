8899541827e5ab78e5f2213a9b5f12ef930c1e81
Darren Ethier <darren@roughsmootheng.in>
2021-02-25 12:49:27 -0500

    Add interface for LabelProps and implement.
    Also:
    - import `Fragment` from @wordpress/element.
    - import `HTMLAttributes` explicitly as a type from react (@types/wordpress__element doesn’t export this interface).

--- assets/js/base/components/label/index.tsx
+++ assets/js/base/components/label/index.tsx
@@ -1,31 +1,29 @@
 /**
  * External dependencies
  */
-import { Fragment, HTMLAttributes } from 'react';
+import { Fragment } from '@wordpress/element';
 import classNames from 'classnames';
+import type { HTMLAttributes } from 'react';
+
+interface LabelProps {
+	label?: string;
+	screenReaderLabel?: string;
+	wrapperElement?: string;
+	wrapperProps?: HTMLAttributes< HTMLElement >;
+}
 
 /**
  * Component used to render an accessible text given a label and/or a
  * screenReaderLabel. The wrapper element and wrapper props can also be
  * specified via props.
  *
- * @param {Object} props Incoming props for the component.
- * @param {string} [props.label] Label content.
- * @param {string} [props.screenReaderLabel] Content for screen readers.
- * @param {string} [props.wrapperElement] What element is used to wrap the label.
- * @param {Object} [props.wrapperProps] Props passed into wrapper element.
  */
 const Label = ( {
 	label,
 	screenReaderLabel,
 	wrapperElement,
 	wrapperProps = {},
-}: {
-	label?: string;
-	screenReaderLabel?: string;
-	wrapperElement?: string;
-	wrapperProps?: HTMLAttributes< HTMLElement >;
-} ) => {
+}: LabelProps ): JSX.Element => {
 	let Wrapper;
 
 	const hasLabel = typeof label !== 'undefined' && label !== null;
