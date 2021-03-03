d4afec1454d187dfd93c8cb04ae3e92425c40d6b
Seghir Nadir <nadir.seghir@gmail.com>
2021-03-03 11:25:12 +0100

    type useStoreCart

--- assets/js/type-defs/hooks.ts
+++ assets/js/type-defs/hooks.ts
@@ -4,8 +4,15 @@
 import type {
 	CartResponseErrorItem,
 	CartResponseCouponItem,
+	CartResponseItem,
+	CartResponseFeeItem,
+	CartResponseTotals,
+	CartResponseShippingAddress,
+	CartResponseBillingAddress,
+	CartResponseShippingRateItem,
+	CartResponse,
 } from './cart-response';
-
+import type { ResponseError } from '../data/types';
 export interface StoreCartItemQuantity {
 	isPendingDelete: boolean;
 	quantity: number;
@@ -21,4 +28,26 @@ export interface StoreCartCoupon {
 	removeCoupon: ( coupon: string ) => void;
 	isApplyingCoupon: boolean;
 	isRemovingCoupon: boolean;
+}
+
+export interface StoreCart {
+	cartCoupons: Array< CartResponseCouponItem >;
+	cartItems: Array< CartResponseItem >;
+	cartFees: Array< CartResponseFeeItem >;
+	cartItemsCount: number;
+	cartItemsWeight: number;
+	cartNeedsPayment: boolean;
+	cartNeedsShipping: boolean;
+	cartItemErrors: Array< CartResponseErrorItem >;
+	cartTotals: CartResponseTotals;
+	cartIsLoading: boolean;
+	cartErrors: Array< ResponseError >;
+	billingAddress: CartResponseBillingAddress;
+	shippingAddress: CartResponseShippingAddress;
+	shippingRates: Array< CartResponseShippingRateItem >;
+	extensions: Record< string, unknown >;
+	shippingRatesLoading: boolean;
+	cartHasCalculatedShipping: boolean;
+	paymentRequirements: Array< string >;
+	receiveCart: ( cart: CartResponse ) => void;
 }
