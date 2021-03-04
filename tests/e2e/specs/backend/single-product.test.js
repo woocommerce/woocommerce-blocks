ce59c7e75eb93cc0a65f60b138a33aeb577672cd
Seghir Nadir <nadir.seghir@gmail.com>
2021-03-04 16:58:49 +0100

    try clickOnBlockSettingsMenuRemoveBlockButton function

--- tests/e2e/specs/backend/single-product.test.js
+++ tests/e2e/specs/backend/single-product.test.js
@@ -8,7 +8,41 @@ import {
 } from '@wordpress/e2e-test-utils';
 
 import { visitBlockPage } from '@woocommerce/blocks-test-utils';
+/**
+ * Due to an issue with the Popover component not being scrollable
+ * under certain conditions, Pupeteer cannot "see" the "Remove Block"
+ * button. This is a workaround until that issue is resolved.
+ *
+ * see: https://github.com/WordPress/gutenberg/pull/14908#discussion_r284725956
+ */
+const clickOnBlockSettingsMenuRemoveBlockButton = async () => {
+	await clickBlockToolbarButton( 'Options' );
 
+	let isRemoveButton = false;
+
+	let numButtons = await page.$$eval(
+		'.block-editor-block-settings-menu__content button',
+		( btns ) => btns.length
+	);
+
+	// Limit by the number of buttons available
+	while ( --numButtons ) {
+		await page.keyboard.press( 'Tab' );
+
+		isRemoveButton = await page.evaluate( () => {
+			return document.activeElement.innerText.includes( 'Remove block' );
+		} );
+
+		// Stop looping once we find the button
+		if ( isRemoveButton ) {
+			await pressKeyTimes( 'Enter', 1 );
+			break;
+		}
+	}
+
+	// Makes failures more explicit
+	await expect( isRemoveButton ).toBe( true );
+};
 if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 3 )
 	// eslint-disable-next-line jest/no-focused-tests
 	test.only( 'skipping all other things', () => {} );
@@ -28,7 +62,7 @@ describe( `${ block.name } Block`, () => {
 	it( 'can be inserted more than once', async () => {
 		await insertBlock( block.name );
 		expect( await getAllBlocks() ).toHaveLength( 2 );
-		await page.keyboard.press( 'Backspace' );
+		await clickOnBlockSettingsMenuRemoveBlockButton();
 		expect( await getAllBlocks() ).toHaveLength( 1 );
 	} );
 
