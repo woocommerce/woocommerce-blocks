woocommerce/woocommerce-blocks

commit 0881cad9b2154fd0eef9c68dffe804cf6008f667

Google Analytics Integration - Action Hooks and initial tracking events (#3953)
* Remove item and change quantity doAction hooks

* Cart event

* Event/Action system for cart events

* GA implementation

* Revert some unrelated commits

* revert change

* Listing impression config for all products

* Category support

* remove unrelated change

* Remove listype and block import

* wrap action in try catch

* Tidy

* Only load analytics scripts in experimental builds

* Made console log more specific

* Wrap script reg in experimental check

assets/js/base/hooks/cart/use-store-cart-item-quantity.ts
@@ -8,21 +8,20 @@ import { useDebounce } from 'use-debounce';
 import { useCheckoutContext } from '@woocommerce/base-context';
 import { triggerFragmentRefresh } from '@woocommerce/base-utils';
 import type { CartItem, StoreCartItemQuantity } from '@woocommerce/types';
+
 /**
  * Internal dependencies
  */
 import { useStoreCart } from './use-store-cart';
 import { usePrevious } from '../use-previous';
+
 /**
- * This is a custom hook for loading the Store API /cart/ endpoint and
- * actions for removing or changing item quantity.
+ * This is a custom hook for loading the Store API /cart/ endpoint and actions for removing or changing item quantity.
  *
  * @see https://github.com/woocommerce/woocommerce-gutenberg-products-block/tree/trunk/src/RestApi/StoreApi
  *
- * @param {CartItem} cartItem      The cartItem to get quantity info from and
- *                                 will have quantity updated on.
- * @return {StoreCartItemQuantity} An object exposing data and actions relating
- *                                 to cart items.
+ * @param {CartItem} cartItem      The cartItem to get quantity info from and will have quantity updated on.
+ * @return {StoreCartItemQuantity} An object exposing data and actions relating to cart items.
  */
 export const useStoreCartItemQuantity = (
 	cartItem: CartItem
@@ -31,56 +30,45 @@ export const useStoreCartItemQuantity = (
 	const { cartErrors } = useStoreCart();
 	const { dispatchActions } = useCheckoutContext();
 
-	// Store quantity in hook state. This is used to keep the UI
-	// updated while server request is updated.
-	const [ quantity, changeQuantity ] = useState< number >( cartItemQuantity );
+	// Store quantity in hook state. This is used to keep the UI updated while server request is updated.
+	const [ quantity, setQuantity ] = useState< number >( cartItemQuantity );
 	const [ debouncedQuantity ] = useDebounce< number >( quantity, 400 );
 	const previousDebouncedQuantity = usePrevious( debouncedQuantity );
 	const { removeItemFromCart, changeCartItemQuantity } = useDispatch(
 		storeKey
 	);
 
-	const isPendingQuantity = useSelect(
+	// Track when things are already pending updates.
+	const isPending = useSelect(
 		( select ) => {
 			if ( ! cartItemKey ) {
-				return false;
+				return {
+					quantity: false,
+					delete: false,
+				};
 			}
-
 			const store = select( storeKey );
-			return store.isItemPendingQuantity( cartItemKey );
+			return {
+				quantity: store.isItemPendingQuantity( cartItemKey ),
+				delete: store.isItemPendingDelete( cartItemKey ),
+			};
 		},
 		[ cartItemKey ]
 	);
-	const previousIsPendingQuantity = usePrevious( isPendingQuantity );
-
-	const isPendingDelete = useSelect(
-		( select ) => {
-			if ( ! cartItemKey ) {
-				return false;
-			}
-
-			const store = select( storeKey );
-			return store.isItemPendingDelete( cartItemKey );
-		},
-		[ cartItemKey ]
-	);
-	const previousIsPendingDelete = usePrevious( isPendingDelete );
+	const previousIsPending = usePrevious( isPending );
 
 	const removeItem = () => {
 		return cartItemKey
 			? removeItemFromCart( cartItemKey ).then( () => {
 					triggerFragmentRefresh();
+					return true;
 			  } )
-			: false;
+			: Promise.resolve( false );
 	};
 
 	// Observe debounced quantity value, fire action to update server on change.
 	useEffect( () => {
-		if (
-			cartItemKey &&
-			Number.isFinite( previousDebouncedQuantity ) &&
-			previousDebouncedQuantity !== debouncedQuantity
-		) {
+		if ( cartItemKey && previousDebouncedQuantity !== debouncedQuantity ) {
 			changeCartItemQuantity( cartItemKey, debouncedQuantity ).then(
 				triggerFragmentRefresh
 			);
@@ -93,34 +81,37 @@ export const useStoreCartItemQuantity = (
 	] );
 
 	useEffect( () => {
-		if ( previousIsPendingQuantity !== isPendingQuantity ) {
-			if ( isPendingQuantity ) {
+		if ( typeof previousIsPending === 'undefined' ) {
+			return;
+		}
+		if ( previousIsPending.quantity !== isPending.quantity ) {
+			if ( isPending.quantity ) {
 				dispatchActions.incrementCalculating();
 			} else {
 				dispatchActions.decrementCalculating();
 			}
 		}
-	}, [ dispatchActions, isPendingQuantity, previousIsPendingQuantity ] );
-
-	useEffect( () => {
-		if ( previousIsPendingDelete !== isPendingDelete ) {
-			if ( isPendingDelete ) {
+		if ( previousIsPending.delete !== isPending.delete ) {
+			if ( isPending.delete ) {
 				dispatchActions.incrementCalculating();
 			} else {
 				dispatchActions.decrementCalculating();
 			}
 		}
 		return () => {
-			if ( isPendingDelete ) {
+			if ( isPending.quantity ) {
+				dispatchActions.decrementCalculating();
+			}
+			if ( isPending.delete ) {
 				dispatchActions.decrementCalculating();
 			}
 		};
-	}, [ dispatchActions, isPendingDelete, previousIsPendingDelete ] );
+	}, [ dispatchActions, isPending, previousIsPending ] );
 
 	return {
-		isPendingDelete,
+		isPendingDelete: isPending.delete,
 		quantity,
-		changeQuantity,
+		setItemQuantity: setQuantity,
 		removeItem,
 		cartItemQuantityErrors: cartErrors,
 	};

assets/js/type-defs/hooks.ts
@@ -16,8 +16,8 @@ import type { ResponseError } from '../data/types';
 export interface StoreCartItemQuantity {
 	isPendingDelete: boolean;
 	quantity: number;
-	changeQuantity: ( quantity: number ) => void;
-	removeItem: () => Promise< void > | false;
+	setItemQuantity: ( quantity: number ) => Promise< boolean >;
+	removeItem: () => Promise< boolean >;
 	cartItemQuantityErrors: Array< CartResponseErrorItem >;
 }
 

assets/js/atomic/blocks/product-elements/button/block.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { __, _n, sprintf } from '@wordpress/i18n';
 import { useEffect, useRef } from '@wordpress/element';
-import { useStoreAddToCart } from '@woocommerce/base-hooks';
+import { useStoreAddToCart, useStoreEvents } from '@woocommerce/base-hooks';
 import { decodeEntities } from '@wordpress/html-entities';
 import { triggerFragmentRefresh } from '@woocommerce/base-utils';
 import {
@@ -52,7 +52,6 @@ const Block = ( { className } ) => {
 
 const AddToCartButton = ( { product } ) => {
 	const firstMount = useRef( true );
-
 	const {
 		id,
 		permalink,
@@ -61,7 +60,7 @@ const AddToCartButton = ( { product } ) => {
 		is_purchasable: isPurchasable,
 		is_in_stock: isInStock,
 	} = product;
-
+	const { dispatchStoreEvent } = useStoreEvents();
 	const { cartQuantity, addingToCart, addToCart } = useStoreAddToCart( id );
 
 	useEffect( () => {
@@ -103,6 +102,9 @@ const AddToCartButton = ( { product } ) => {
 	} else {
 		buttonProps.onClick = () => {
 			addToCart();
+			dispatchStoreEvent( 'add-cart-item', {
+				product,
+			} );
 		};
 	}
 

assets/js/base/components/product-list/product-list.js
@@ -12,6 +12,7 @@ import {
 	useStoreProducts,
 	useSynchronizedQueryState,
 	useQueryStateByKey,
+	useStoreEvents,
 } from '@woocommerce/base-hooks';
 import withScrollToTop from '@woocommerce/base-hocs/with-scroll-to-top';
 import { useInnerBlockLayoutContext } from '@woocommerce/shared-context';
@@ -126,8 +127,9 @@ const ProductList = ( {
 	const { products, totalProducts, productsLoading } = useStoreProducts(
 		queryState
 	);
-	const { parentClassName } = useInnerBlockLayoutContext();
+	const { parentClassName, parentName } = useInnerBlockLayoutContext();
 	const totalQuery = extractPaginationAndSortAttributes( queryState );
+	const { dispatchStoreEvent } = useStoreEvents();
 
 	// These are possible filters.
 	const [ productAttributes, setProductAttributes ] = useQueryStateByKey(
@@ -137,15 +139,23 @@ const ProductList = ( {
 	const [ minPrice, setMinPrice ] = useQueryStateByKey( 'min_price' );
 	const [ maxPrice, setMaxPrice ] = useQueryStateByKey( 'max_price' );
 
-	// Only update previous query totals if the query is different and
-	// the total number of products is a finite number.
+	// Only update previous query totals if the query is different and the total number of products is a finite number.
 	const previousQueryTotals = usePrevious(
 		{ totalQuery, totalProducts },
 		areQueryTotalsDifferent
 	);
 
-	// If query state (excluding pagination/sorting attributes) changed,
-	// reset pagination to the first page.
+	// If the product list changes, trigger an event.
+	useEffect( () => {
+		if ( products.length > 0 ) {
+			dispatchStoreEvent( 'list-products', {
+				products,
+				listName: parentName,
+			} );
+		}
+	}, [ products, parentName, dispatchStoreEvent ] );
+
+	// If query state (excluding pagination/sorting attributes) changed, reset pagination to the first page.
 	useEffect( () => {
 		if ( isEqual( totalQuery, previousQueryTotals?.totalQuery ) ) {
 			return;

assets/js/base/hooks/index.js
@@ -12,6 +12,7 @@ export * from './use-previous';
 export * from './use-shallow-equal';
 export * from './use-store-products';
 export * from './use-store-add-to-cart';
+export * from './use-store-events';
 export * from './use-store-notices';
 export * from './use-query-state';
 export * from './use-throw-error';

assets/js/base/hooks/use-store-add-to-cart.ts
@@ -20,7 +20,7 @@ interface StoreAddToCart {
 	cartQuantity: number;
 	addingToCart: boolean;
 	cartIsLoading: boolean;
-	addToCart: ( quantity: number ) => void;
+	addToCart: ( quantity?: number ) => Promise< boolean >;
 }
 /**
  * Get the quantity of a product in the cart.
@@ -60,11 +60,9 @@ export const useStoreAddToCart = ( productId: number ): StoreAddToCart => {
 
 	const addToCart = ( quantity = 1 ) => {
 		setAddingToCart( true );
-		addItemToCart( productId, quantity )
-			.then( ( result ) => {
-				if ( result === true ) {
-					removeNotice( 'add-to-cart' );
-				}
+		return addItemToCart( productId, quantity )
+			.then( () => {
+				removeNotice( 'add-to-cart' );
 			} )
 			.catch( ( error ) => {
 				addErrorNotice( decodeEntities( error.message ), {

assets/js/base/hooks/use-store-events.ts
@@ -1,0 +1,43 @@
+/**
+ * External dependencies
+ */
+import { doAction } from '@wordpress/hooks';
+import { useCallback } from '@wordpress/element';
+
+/**
+ * Abstraction on top of @wordpress/hooks for dispatching events via doAction for 3rd parties to hook into.
+ */
+export const useStoreEvents = (
+	namespace = 'experimental__woocommerce_blocks'
+): {
+	dispatchStoreEvent: (
+		eventName: string,
+		eventParams: Partial< Record< string, unknown > >
+	) => void;
+} => {
+	const dispatchStoreEvent = useCallback(
+		(
+			eventName: string,
+			eventParams: Partial< Record< string, unknown > >
+		) => {
+			// eslint-disable-next-line no-console
+			console.log( {
+				event: `${ namespace }-${ eventName }`,
+				eventParams,
+			} );
+
+			try {
+				doAction( `${ namespace }-${ eventName }`, eventParams );
+			} catch ( e ) {
+				// We don't handle thrown errors but just console.log for troubleshooting.
+				// eslint-disable-next-line no-console
+				console.error( e );
+			}
+		},
+		[ namespace ]
+	);
+
+	return {
+		dispatchStoreEvent,
+	};
+};

assets/js/blocks/cart-checkout/cart/full-cart/cart-line-item-row.js
@@ -7,7 +7,10 @@ import PropTypes from 'prop-types';
 import QuantitySelector from '@woocommerce/base-components/quantity-selector';
 import ProductPrice from '@woocommerce/base-components/product-price';
 import ProductName from '@woocommerce/base-components/product-name';
-import { useStoreCartItemQuantity } from '@woocommerce/base-hooks';
+import {
+	useStoreCartItemQuantity,
+	useStoreEvents,
+} from '@woocommerce/base-hooks';
 import {
 	ProductBackorderBadge,
 	ProductImage,
@@ -94,10 +97,11 @@ const CartLineItemRow = ( { lineItem = {} } ) => {
 
 	const {
 		quantity,
-		changeQuantity,
+		setItemQuantity,
 		removeItem,
 		isPendingDelete,
 	} = useStoreCartItemQuantity( lineItem );
+	const { dispatchStoreEvent } = useStoreEvents();
 
 	const productPriceValidation = useCallback(
 		( value ) => mustBeString( value ) && mustContain( value, '<price/>' ),
@@ -243,12 +247,24 @@ const CartLineItemRow = ( { lineItem = {} } ) => {
 						disabled={ isPendingDelete }
 						quantity={ quantity }
 						maximum={ quantityLimit }
-						onChange={ changeQuantity }
+						onChange={ ( newQuantity ) => {
+							setItemQuantity( newQuantity );
+							dispatchStoreEvent( 'set-cart-item-quantity', {
+								product: lineItem,
+								quantity: newQuantity,
+							} );
+						} }
 						itemName={ name }
 					/>
 					<button
 						className="wc-block-cart-item__remove-link"
-						onClick={ removeItem }
+						onClick={ () => {
+							removeItem();
+							dispatchStoreEvent( 'remove-cart-item', {
+								product: lineItem,
+								quantity,
+							} );
+						} }
 						disabled={ isPendingDelete }
 					>
 						{ __( 'Remove item', 'woo-gutenberg-products-block' ) }

commit 4e608507091d727058e367519f4766a29b526631

Tests: Rename changeQuantity to setItemQuantity
This was broken in https://github.com/woocommerce/woocommerce-gutenberg-products-block/commit/0881cad9b2154fd0eef9c68dffe804cf6008f667

assets/js/base/hooks/cart/use-store-cart-item-quantity.ts
@@ -68,7 +68,11 @@ export const useStoreCartItemQuantity = (
 
 	// Observe debounced quantity value, fire action to update server on change.
 	useEffect( () => {
-		if ( cartItemKey && previousDebouncedQuantity !== debouncedQuantity ) {
+		if (
+			cartItemKey &&
+			Number.isFinite( previousDebouncedQuantity ) &&
+			previousDebouncedQuantity !== debouncedQuantity
+		) {
 			changeCartItemQuantity( cartItemKey, debouncedQuantity ).then(
 				triggerFragmentRefresh
 			);

assets/js/type-defs/hooks.ts
@@ -16,7 +16,7 @@ import type { ResponseError } from '../data/types';
 export interface StoreCartItemQuantity {
 	isPendingDelete: boolean;
 	quantity: number;
-	setItemQuantity: ( quantity: number ) => Promise< boolean >;
+	setItemQuantity: React.Dispatch< React.SetStateAction< number > >;
 	removeItem: () => Promise< boolean >;
 	cartItemQuantityErrors: Array< CartResponseErrorItem >;
 }

assets/js/base/hooks/cart/test/use-store-cart-item-quantity.js
@@ -95,14 +95,14 @@ describe( 'useStoreCartItemQuantity', () => {
 				);
 			} );
 
-			const { changeQuantity, quantity } = renderer.root.findByType(
+			const { setItemQuantity, quantity } = renderer.root.findByType(
 				'div'
 			).props;
 
 			expect( quantity ).toBe( 1 );
 
 			act( () => {
-				changeQuantity( 2 );
+				setItemQuantity( 2 );
 			} );
 
 			const { quantity: newQuantity } = renderer.root.findByType(
@@ -133,7 +133,7 @@ describe( 'useStoreCartItemQuantity', () => {
 			expect( mockRemoveItemFromCart ).toHaveBeenCalledWith( '123' );
 		} );
 
-		it( 'changeQuantity should call the dispatch action', () => {
+		it( 'setItemQuantity should call the dispatch action', () => {
 			const TestComponent = getTestComponent( {
 				key: '123',
 				quantity: 1,
@@ -145,10 +145,10 @@ describe( 'useStoreCartItemQuantity', () => {
 				);
 			} );
 
-			const { changeQuantity } = renderer.root.findByType( 'div' ).props;
+			const { setItemQuantity } = renderer.root.findByType( 'div' ).props;
 
 			act( () => {
-				changeQuantity( 2 );
+				setItemQuantity( 2 );
 			} );
 
 			expect( mockChangeCartItemQuantity.mock.calls ).toEqual( [