f45690cad8929058d7526f6766fc82acaaffc421
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-03-26 18:27:53 +0100

    Add initial e2e tests to Checkout block frontend

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -1,4 +1,5 @@
 export { findLabelWithText } from './find-label-with-text';
-export { visitBlockPage } from './visit-block-page';
+export { visitBlockPage, visitPostOfType } from './visit-block-page';
 export { getBlockPagePermalink } from './get-block-page-permalink';
 export { getNormalPagePermalink } from './get-normal-page-permalink';
+export { shopper } from './shopper';
