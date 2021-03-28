7fddfdb9e9bb45c236576518b972d15828c0e1a7
Thomas Roberts <thomas.roberts@automattic.com>
2021-03-28 14:52:33 +0100

    Test function directly, rather than through validate_cart_items

--- tests/php/StoreApi/Utilities/NoticeHandler.php
+++ tests/php/StoreApi/Utilities/NoticeHandler.php
@@ -7,21 +7,16 @@ namespace Automattic\WooCommerce\Blocks\Tests\StoreApi\Utilities;
 
 use Automattic\WooCommerce\Blocks\StoreApi\Routes\RouteException;
 use Automattic\WooCommerce\Blocks\StoreApi\Utilities\CartController;
+use Automattic\WooCommerce\Blocks\StoreApi\Utilities\NoticeHandler;
 use PHPUnit\Framework\TestCase;
 use \WC_Helper_Product as ProductHelper;
 
 class NoticeHandlerTests extends TestCase {
 
-	public function test_api_does_not_return_html() {
-		// We expect validate_cart_items to throw
+	public function test_convert_notices_to_exceptions() {
 		$this->expectException(RouteException::class);
 		$this->expectExceptionMessage('This is an error message with Some HTML in it.');
-		$class = new CartController();
-		add_action( 'woocommerce_check_cart_items', function() {
-			wc_add_notice( '<strong>This is an error message with <a href="#">Some HTML in it</a>.', 'error' );
-		} );
-		$product = ProductHelper::create_simple_product();
-		wc()->cart->add_to_cart( $product->get_id(), 2 );
-		$class->validate_cart_items();
+		wc_add_notice( '<strong>This is an error message with <a href="#">Some HTML in it</a>.', 'error' );
+		$errors = NoticeHandler::convert_notices_to_exceptions( 'test_error' );
 	}
 }
