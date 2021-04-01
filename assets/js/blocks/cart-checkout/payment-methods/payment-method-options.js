b2db554726177b47b25954b886712659d707b497
Mike Jolley <mike.jolley@me.com>
2021-04-01 14:30:14 +0100

    Fix base context hook import

--- assets/js/blocks/cart-checkout/payment-methods/payment-method-options.js
+++ assets/js/blocks/cart-checkout/payment-methods/payment-method-options.js
@@ -5,8 +5,8 @@ import {
 	usePaymentMethods,
 	usePaymentMethodInterface,
 	useEmitResponse,
-} from '@woocommerce/base-hooks';
-import { useStoreNotices } from '@woocommerce/base-context/hooks';
+	useStoreNotices,
+} from '@woocommerce/base-context/hooks';
 import { cloneElement } from '@wordpress/element';
 import {
 	useEditorContext,
