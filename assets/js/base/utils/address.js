d6212951d52a7488d4c4a4323489b0dda86a18a7
Thomas Roberts <thomas.roberts@automattic.com>
2021-04-06 15:42:34 +0100

    Add emptyHiddenAddressFields function

--- assets/js/base/utils/address.js
+++ assets/js/base/utils/address.js
@@ -1,4 +1,10 @@
 /**
+ * External dependencies
+ */
+import defaultAddressFields from '@woocommerce/base-components/cart-checkout/address-form/default-address-fields';
+import prepareAddressFields from '@woocommerce/base-components/cart-checkout/address-form/prepare-address-fields';
+
+/**
  * pluckAddress takes a full address object and returns relevant fields for calculating
  * shipping, so we can track when one of them change to update rates.
  *
@@ -21,3 +27,22 @@ export const pluckAddress = ( {
 	city: city.trim(),
 	postcode: postcode ? postcode.replace( ' ', '' ).toUpperCase() : '',
 } );
+
+/**
+ * Removes fields from an address if they are hidden by the field config from core.
+ *
+ * @param {Object} address The address to remove fields from.
+ * @return {Object} The address with hidden fields removed.
+ */
+export const emptyHiddenAddressFields = ( address ) => {
+	const fields = Object.keys( defaultAddressFields );
+	const addressFields = prepareAddressFields( fields, {}, address.country );
+	const newAddress = {};
+	addressFields
+		.filter( ( field ) => ! field.hidden )
+		.forEach( ( field ) => {
+			newAddress[ field.key ] = address[ field.key ];
+		} );
+
+	return newAddress;
+};
