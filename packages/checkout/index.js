69aa9e3835e1561975176ae54938481700a4c50b
Nadir Seghir <nadir.seghir@gmail.com>
2021-04-20 15:40:02 +0100

    remove wordpress-components from checkout

--- packages/checkout/index.js
+++ packages/checkout/index.js
@@ -5,4 +5,4 @@ export * from './registry';
 export { default as ExperimentalOrderMeta } from './order-meta';
 export { default as ExperimentalOrderShippingPackages } from './order-shipping-packages';
 export { default as Panel } from './panel';
-export { SlotFillProvider } from 'wordpress-components';
+export { SlotFillProvider } from '@wordpress/components';
