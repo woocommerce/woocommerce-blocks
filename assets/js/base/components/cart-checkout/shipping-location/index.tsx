43c788c8c9bb4719c2ace38bfdf06d1fef12945c
Thomas Roberts <thomas.roberts@automattic.com>
2021-04-23 13:47:43 +0100

    Convert ShippingLocation to TypeScript

--- assets/js/base/components/cart-checkout/shipping-location/index.tsx
+++ assets/js/base/components/cart-checkout/shipping-location/index.tsx
@@ -1,10 +1,14 @@
 /**
  * External dependencies
  */
-import PropTypes from 'prop-types';
 import { __, sprintf } from '@wordpress/i18n';
 import { getSetting } from '@woocommerce/settings';
 import { decodeEntities } from '@wordpress/html-entities';
+import { Address } from '@woocommerce/type-defs/customer';
+
+interface ShippingLocationProps {
+	address: Address;
+}
 
 /**
  * Shows a formatted shipping location.
@@ -12,13 +16,19 @@ import { decodeEntities } from '@wordpress/html-entities';
  * @param {Object} props Incoming props for the component.
  * @param {Object} props.address Incoming address information.
  */
-const ShippingLocation = ( { address } ) => {
+const ShippingLocation = ( { address }: ShippingLocationProps ) => {
 	// we bail early if we don't have an address.
 	if ( Object.values( address ).length === 0 ) {
 		return null;
 	}
-	const shippingCountries = getSetting( 'shippingCountries', {} );
-	const shippingStates = getSetting( 'shippingStates', {} );
+	const shippingCountries = getSetting( 'shippingCountries', {} ) as Record<
+		string,
+		string
+	>;
+	const shippingStates = getSetting( 'shippingStates', {} ) as Record<
+		string,
+		Record< string, string >
+	>;
 	const formattedCountry =
 		typeof shippingCountries[ address.country ] === 'string'
 			? decodeEntities( shippingCountries[ address.country ] )
@@ -54,15 +64,6 @@ const ShippingLocation = ( { address } ) => {
 			) + ' ' }
 		</span>
 	);
-};
-
-ShippingLocation.propTypes = {
-	address: PropTypes.shape( {
-		city: PropTypes.string,
-		state: PropTypes.string,
-		postcode: PropTypes.string,
-		country: PropTypes.string,
-	} ),
 };
 
 export default ShippingLocation;
