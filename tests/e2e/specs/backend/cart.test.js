4629bccd918eb45fef6f4fe35c07e7dfb10a2304
Raluca Stan <ralucastn@gmail.com>
2021-04-29 16:17:28 +0200

    Fix backend cart e2e test

--- tests/e2e/specs/backend/cart.test.js
+++ tests/e2e/specs/backend/cart.test.js
@@ -3,9 +3,9 @@
  */
 import {
 	clickButton,
-	getAllBlocks,
 	openDocumentSettingsSidebar,
 	switchUserToAdmin,
+	getAllBlocks,
 } from '@wordpress/e2e-test-utils';
 import {
 	findLabelWithText,
@@ -23,13 +23,89 @@ const block = {
 	class: '.wc-block-cart',
 };
 
-if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 2 )
+if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 2 ) {
 	// eslint-disable-next-line jest/no-focused-tests
 	test.only( `skipping ${ block.name } tests`, () => {} );
+}
 
 describe( `${ block.name } Block`, () => {
 	beforeAll( async () => {
+		await page.evaluate( () => {
+			localStorage.removeItem(
+				'wc-blocks_dismissed_compatibility_notices'
+			);
+		} );
+		await page.evaluate( () => {
+			localStorage.setItem(
+				'wc-blocks_dismissed_compatibility_notices',
+				'["cart"]'
+			);
+		} );
 		await switchUserToAdmin();
+		await visitBlockPage( `${ block.name } Block` );
+	} );
+
+	it( 'can only be inserted once', async () => {
+		await insertBlockDontWaitForInsertClose( block.name );
+		expect( await getAllBlocks() ).toHaveLength( 1 );
+	} );
+
+	it( 'renders without crashing', async () => {
+		await expect( page ).toRenderBlock( block );
+	} );
+
+	it( 'shows empty cart when changing the view', async () => {
+		await page.waitForSelector( block.class ).catch( () => {
+			throw new Error(
+				`Could not find an element with class ${ block.class } - the block probably did not load correctly.`
+			);
+		} );
+		await page.click( block.class );
+		await expect( page ).toMatchElement(
+			'[hidden] .wc-block-cart__empty-cart__title'
+		);
+		await clickButton( 'Empty Cart' );
+		await expect( page ).not.toMatchElement(
+			'[hidden] .wc-block-cart__empty-cart__title'
+		);
+		// Simulate user scrolling up so the block toolbar doesn't cover
+		// the `Full Cart` button.
+		await page.evaluate( () => {
+			document
+				.querySelector( '.wc-block-view-switch-control' )
+				.scrollIntoView( { block: 'center', inline: 'center' } );
+		} );
+		await clickButton( 'Full Cart' );
+		await expect( page ).toMatchElement(
+			'[hidden] .wc-block-cart__empty-cart__title'
+		);
+	} );
+
+	describe( 'attributes', () => {
+		beforeEach( async () => {
+			await openDocumentSettingsSidebar();
+			await page.click( block.class );
+		} );
+
+		it( 'can toggle Shipping calculator', async () => {
+			const selector = `${ block.class } .wc-block-components-totals-shipping__change-address-button`;
+			const toggleLabel = await findLabelWithText(
+				'Shipping calculator'
+			);
+			await expect( toggleLabel ).toToggleElement( selector );
+		} );
+	} );
+} );
+
+describe( 'before compatibility notice is dismissed', () => {
+	beforeEach( async () => {
+		await page.evaluate( () => {
+			localStorage.setItem(
+				'wc-blocks_dismissed_compatibility_notices',
+				'[]'
+			);
+		} );
+		await visitBlockPage( `${ block.name } Block` );
 	} );
 
 	afterEach( async () => {
@@ -40,86 +116,10 @@ describe( `${ block.name } Block`, () => {
 		} );
 	} );
 
-	describe( 'before compatibility notice is dismissed', () => {
-		beforeEach( async () => {
-			await page.evaluate( () => {
-				localStorage.setItem(
-					'wc-blocks_dismissed_compatibility_notices',
-					'[]'
-				);
-			} );
-			await visitBlockPage( `${ block.name } Block` );
-		} );
-
-		it( 'shows compatibility notice', async () => {
-			const compatibilityNoticeTitle = await page.$x(
-				`//h1[contains(text(), 'Compatibility notice')]`
-			);
-			expect( compatibilityNoticeTitle.length ).toBe( 1 );
-		} );
-	} );
-
-	describe( 'once compatibility notice is dismissed', () => {
-		beforeEach( async () => {
-			await page.evaluate( () => {
-				localStorage.setItem(
-					'wc-blocks_dismissed_compatibility_notices',
-					'["cart"]'
-				);
-			} );
-			await visitBlockPage( `${ block.name } Block` );
-		} );
-
-		it( 'can only be inserted once', async () => {
-			await insertBlockDontWaitForInsertClose( block.name );
-			await closeInserter();
-			expect( await getAllBlocks() ).toHaveLength( 1 );
-		} );
-
-		it( 'renders without crashing', async () => {
-			await expect( page ).toRenderBlock( block );
-		} );
-
-		describe( 'attributes', () => {
-			beforeEach( async () => {
-				await openDocumentSettingsSidebar();
-				await page.click( block.class );
-			} );
-
-			it( 'can toggle Shipping calculator', async () => {
-				const selector = `${ block.class } .wc-block-components-totals-shipping__change-address-button`;
-				const toggleLabel = await findLabelWithText(
-					'Shipping calculator'
-				);
-				await expect( toggleLabel ).toToggleElement( selector );
-			} );
-		} );
-
-		it( 'shows empty cart when changing the view', async () => {
-			await page.waitForSelector( block.class ).catch( () => {
-				throw new Error(
-					`Could not find an element with class ${ block.class } - the block probably did not load correctly.`
-				);
-			} );
-			await page.click( block.class );
-			await expect( page ).toMatchElement(
-				'[hidden] .wc-block-cart__empty-cart__title'
-			);
-			await clickButton( 'Empty Cart' );
-			await expect( page ).not.toMatchElement(
-				'[hidden] .wc-block-cart__empty-cart__title'
-			);
-			// Simulate user scrolling up so the block toolbar doesn't cover
-			// the `Full Cart` button.
-			await page.evaluate( () => {
-				document
-					.querySelector( '.wc-block-view-switch-control' )
-					.scrollIntoView( { block: 'center', inline: 'center' } );
-			} );
-			await clickButton( 'Full Cart' );
-			await expect( page ).toMatchElement(
-				'[hidden] .wc-block-cart__empty-cart__title'
-			);
-		} );
+	it( 'shows compatibility notice', async () => {
+		const compatibilityNoticeTitle = await page.$x(
+			`//h1[contains(text(), 'Compatibility notice')]`
+		);
+		expect( compatibilityNoticeTitle.length ).toBe( 1 );
 	} );
 } );
