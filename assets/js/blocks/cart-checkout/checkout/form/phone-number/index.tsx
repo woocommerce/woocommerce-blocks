50e30c40751616eeb836d697d56f1ed94dafaeb5
Thomas Roberts <thomas.roberts@automattic.com>
2021-05-12 10:10:50 +0100

    Convert Phone number input to TS

--- assets/js/blocks/cart-checkout/checkout/form/phone-number/index.tsx
+++ assets/js/blocks/cart-checkout/checkout/form/phone-number/index.tsx
@@ -4,28 +4,29 @@
 import { __ } from '@wordpress/i18n';
 import { ValidatedTextInput } from '@woocommerce/base-components/text-input';
 
+interface PhoneNumberProps {
+	isRequired?: boolean;
+	value?: string;
+	onChange: ( value: string ) => void;
+	onBlur?: ( value: string ) => void;
+}
 /**
  * Renders a phone number input.
- *
- * @param {Object} props Component props.
- * @param {boolean} props.isRequired Is the phone number required or optional.
- * @param {Function} props.onChange Event fired when the input changes.
- * @param {string} props.value Value of the input.
- * @param {Function} props.onBlur Event fired when the input is blurred.
- * @return {*} The component.
  */
 const PhoneNumber = ( {
 	isRequired = false,
 	value = '',
 	onChange,
-	onBlur = () => {},
-} ) => {
+	onBlur = () => {
+		/* Do nothing */
+	},
+}: PhoneNumberProps ) => {
 	return (
 		<ValidatedTextInput
 			id="phone"
 			type="tel"
 			autoComplete="tel"
-			onBlur={ ( nextValue ) => onBlur( nextValue ) }
+			onBlur={ ( nextValue: string ) => onBlur( nextValue ) }
 			required={ isRequired }
 			label={
 				isRequired
