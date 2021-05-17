69f3424f4da13c405a910acf58025143a4971372
Raluca Stan <ralucastn@gmail.com>
2021-05-17 14:19:09 +0200

    test checkout page on ci

--- tests/e2e/specs/frontend/checkout.test.js
+++ tests/e2e/specs/frontend/checkout.test.js
@@ -127,6 +127,8 @@ describe( `${ block.name } Block (frontend)`, () => {
 
 	it( 'should display an empty cart message when cart is empty', async () => {
 		await shopper.goToCheckoutBlock();
+		const html = await page.content();
+		console.log( html );
 		await page.waitForSelector( 'h1', { text: 'Checkout block' } );
 		await page.waitForSelector( 'strong', { text: 'Your cart is empty!' } );
 	} );
