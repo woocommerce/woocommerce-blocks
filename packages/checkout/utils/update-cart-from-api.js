9738b25417fb5f1994ba5265f8e7a16ecbcbe893
Thomas Roberts <thomas.roberts@automattic.com>
2021-05-20 19:01:50 +0100

    Add comment to updateCartFromApi

--- packages/checkout/utils/update-cart-from-api.js
+++ packages/checkout/utils/update-cart-from-api.js
@@ -4,6 +4,11 @@
 import { CART_STORE_KEY } from '@woocommerce/block-data';
 import { select, dispatch } from '@wordpress/data';
 
+/**
+ * When executed, this will invalidate the getCartData selector, causing a request to be made
+ * to the API. This is in place to allow extensions to signal that they have modified the cart,
+ * and that it needs to be reloaded in the client.
+ */
 export const updateCartFromApi = () => {
 	const { getCartData } = select( CART_STORE_KEY );
 	const { invalidateResolutionForStoreSelector, receiveCart } = dispatch(
