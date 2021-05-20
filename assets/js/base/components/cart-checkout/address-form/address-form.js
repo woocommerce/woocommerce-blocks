55a9db39777b1e9d4d5013208442f71bd2a221f8
Thomas Roberts <thomas.roberts@automattic.com>
2021-05-20 16:15:56 +0100

    Don't pass errorMessage to ValidatedTextInput

--- assets/js/base/components/cart-checkout/address-form/address-form.js
+++ assets/js/base/components/cart-checkout/address-form/address-form.js
@@ -214,7 +214,6 @@ const AddressForm = ( {
 								[ field.key ]: newValue,
 							} )
 						}
-						errorMessage={ field.errorMessage }
 						required={ field.required }
 						inputErrorComponent={ ValidationInputError }
 						{ ...textInputValidationFunctions }
