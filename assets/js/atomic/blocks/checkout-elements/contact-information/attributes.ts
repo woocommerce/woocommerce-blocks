42060774f9da4a387d458059eb64d382eacafafe
Mike Jolley <mike.jolley@me.com>
2021-05-24 14:05:00 +0100

    Contact information block

---
+++ assets/js/atomic/blocks/checkout-elements/contact-information/attributes.ts
@@ -0,0 +1,8 @@
+export const blockAttributes = {
+	allowCreateAccount: {
+		type: 'boolean',
+		default: false,
+	},
+};
+
+export default blockAttributes;
---
+++ assets/js/atomic/blocks/checkout-elements/contact-information/index.ts
@@ -0,0 +1,30 @@
+/**
+ * External dependencies
+ */
+import { registerExperimentalBlockType } from '@woocommerce/block-settings';
+
+/**
+ * Internal dependencies
+ */
+import attributes from './attributes';
+import edit from './edit';
+import {
+	BLOCK_TITLE as title,
+	BLOCK_ICON as icon,
+	BLOCK_DESCRIPTION as description,
+} from './constants';
+
+const blockConfig = {
+	title,
+	description,
+	icon: {
+		src: icon,
+		foreground: '#874FB9',
+	},
+	attributes,
+	edit,
+};
+
+registerExperimentalBlockType( 'woocommerce/checkout-contact-information', {
+	...blockConfig,
+} );
