a84a5d6ffc9ca247d01f0efb731105f758bf3ca4
Mike Jolley <mike.jolley@me.com>
2021-05-26 13:31:22 +0100

    Add order summary

---
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-order-summary/block.tsx
@@ -0,0 +1,97 @@
+/**
+ * External dependencies
+ */
+import {
+	OrderSummary,
+	TotalsCoupon,
+	TotalsDiscount,
+	TotalsFooterItem,
+	TotalsShipping,
+} from '@woocommerce/base-components/cart-checkout';
+import {
+	Subtotal,
+	TotalsFees,
+	TotalsTaxes,
+	ExperimentalOrderMeta,
+} from '@woocommerce/blocks-checkout';
+
+import { getCurrencyFromPriceResponse } from '@woocommerce/price-format';
+import { useShippingDataContext } from '@woocommerce/base-context';
+import {
+	useStoreCartCoupons,
+	useStoreCart,
+} from '@woocommerce/base-context/hooks';
+import { getSetting } from '@woocommerce/settings';
+
+/**
+ * Internal dependencies
+ */
+
+const Block = (): JSX.Element => {
+	// @todo move to attributes.
+	const showRateAfterTaxName = true;
+
+	const { cartItems, cartTotals, cartCoupons, cartFees } = useStoreCart();
+	const {
+		applyCoupon,
+		removeCoupon,
+		isApplyingCoupon,
+		isRemovingCoupon,
+	} = useStoreCartCoupons();
+
+	const { needsShipping } = useShippingDataContext();
+	const totalsCurrency = getCurrencyFromPriceResponse( cartTotals );
+
+	// Prepare props to pass to the ExperimentalOrderMeta slot fill.
+	// We need to pluck out receiveCart.
+	// eslint-disable-next-line no-unused-vars
+	const { extensions, receiveCart, ...cart } = useStoreCart();
+	const slotFillProps = {
+		extensions,
+		cart,
+	};
+
+	return (
+		<>
+			<OrderSummary cartItems={ cartItems } />
+			<Subtotal currency={ totalsCurrency } values={ cartTotals } />
+			<TotalsFees currency={ totalsCurrency } cartFees={ cartFees } />
+			<TotalsDiscount
+				cartCoupons={ cartCoupons }
+				currency={ totalsCurrency }
+				isRemovingCoupon={ isRemovingCoupon }
+				removeCoupon={ removeCoupon }
+				values={ cartTotals }
+			/>
+			{ getSetting( 'couponsEnabled', true ) && (
+				<TotalsCoupon
+					onSubmit={ applyCoupon }
+					initialOpen={ false }
+					isLoading={ isApplyingCoupon }
+				/>
+			) }
+			{ needsShipping && (
+				<TotalsShipping
+					showCalculator={ false }
+					showRateSelector={ false }
+					values={ cartTotals }
+					currency={ totalsCurrency }
+				/>
+			) }
+			{ ! getSetting( 'displayCartPricesIncludingTax', false ) && (
+				<TotalsTaxes
+					currency={ totalsCurrency }
+					showRateAfterTaxName={ showRateAfterTaxName }
+					values={ cartTotals }
+				/>
+			) }
+			<TotalsFooterItem
+				currency={ totalsCurrency }
+				values={ cartTotals }
+			/>
+			<ExperimentalOrderMeta.Slot { ...slotFillProps } />
+		</>
+	);
+};
+
+export default Block;
