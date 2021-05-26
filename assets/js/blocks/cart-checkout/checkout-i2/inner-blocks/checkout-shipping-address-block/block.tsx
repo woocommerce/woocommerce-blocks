2cf4b9b5b3916fa45c9f2f879c6e88bb5316b8c2
Mike Jolley <mike.jolley@me.com>
2021-05-26 17:08:59 +0100

    Add inspector controls for address fields

--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-shipping-address-block/block.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-shipping-address-block/block.tsx
@@ -2,18 +2,51 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { useMemo } from '@wordpress/element';
 import { AddressForm } from '@woocommerce/base-components/cart-checkout';
-import { useCheckoutAddress } from '@woocommerce/base-context';
+import { useCheckoutAddress, useStoreEvents } from '@woocommerce/base-context';
 import CheckboxControl from '@woocommerce/base-components/checkbox-control';
 
-const Block = (): JSX.Element => {
+/**
+ * Internal dependencies
+ */
+import PhoneNumber from '../../phone-number';
+
+const Block = ( {
+	showCompanyField = false,
+	showApartmentField = false,
+	showPhoneField = false,
+	requireCompanyField = false,
+	requirePhoneField = false,
+}: {
+	showCompanyField: boolean;
+	showApartmentField: boolean;
+	showPhoneField: boolean;
+	requireCompanyField: boolean;
+	requirePhoneField: boolean;
+} ): JSX.Element => {
 	const {
 		defaultAddressFields,
 		setShippingFields,
 		shippingFields,
 		setShippingAsBilling,
 		shippingAsBilling,
+		billingFields,
+		setPhone,
 	} = useCheckoutAddress();
+	const { dispatchCheckoutEvent } = useStoreEvents();
+
+	const addressFieldsConfig = useMemo( () => {
+		return {
+			company: {
+				hidden: ! showCompanyField,
+				required: requireCompanyField,
+			},
+			address_2: {
+				hidden: ! showApartmentField,
+			},
+		};
+	}, [ showCompanyField, requireCompanyField, showApartmentField ] );
 
 	return (
 		<>
@@ -23,8 +56,20 @@ const Block = (): JSX.Element => {
 				onChange={ setShippingFields }
 				values={ shippingFields }
 				fields={ Object.keys( defaultAddressFields ) }
-				fieldConfig={ {} }
+				fieldConfig={ addressFieldsConfig }
 			/>
+			{ showPhoneField && (
+				<PhoneNumber
+					isRequired={ requirePhoneField }
+					value={ billingFields.phone }
+					onChange={ ( value ) => {
+						setPhone( value );
+						dispatchCheckoutEvent( 'set-phone-number', {
+							step: 'shipping',
+						} );
+					} }
+				/>
+			) }
 			<CheckboxControl
 				className="wc-block-checkout__use-address-for-billing"
 				label={ __(
--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-shipping-address-block/index.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-shipping-address-block/index.tsx
@@ -33,6 +33,30 @@ registerFeaturePluginBlockType( 'woocommerce/checkout-shipping-address-block', {
 				'woo-gutenberg-products-block'
 			),
 		} ),
+		showCompanyField: {
+			type: 'boolean',
+			default: false,
+		},
+		requireCompanyField: {
+			type: 'boolean',
+			default: false,
+		},
+		allowCreateAccount: {
+			type: 'boolean',
+			default: false,
+		},
+		showApartmentField: {
+			type: 'boolean',
+			default: true,
+		},
+		showPhoneField: {
+			type: 'boolean',
+			default: true,
+		},
+		requirePhoneField: {
+			type: 'boolean',
+			default: false,
+		},
 	},
 	apiVersion: 2,
 	edit: Edit,
