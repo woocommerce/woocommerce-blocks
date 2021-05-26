32bfc8413cf0fc560f40d32248560c5e74d7dfc9
Mike Jolley <mike.jolley@me.com>
2021-05-26 17:19:08 +0100

    wrap shipping with form step

--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-shipping-address-block/frontend.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-shipping-address-block/frontend.tsx
@@ -2,6 +2,8 @@
  * External dependencies
  */
 import withFilteredAttributes from '@woocommerce/base-hocs/with-filtered-attributes';
+import { FormStep } from '@woocommerce/base-components/cart-checkout';
+import { useCheckoutContext } from '@woocommerce/base-context';
 
 /**
  * Internal dependencies
@@ -9,4 +11,45 @@ import withFilteredAttributes from '@woocommerce/base-hocs/with-filtered-attribu
 import Block from './block';
 import attributes from './attributes';
 
-export default withFilteredAttributes( attributes )( Block );
+const FrontendBlock = ( {
+	title,
+	description,
+	requireCompanyField,
+	requirePhoneField,
+	showApartmentField,
+	showCompanyField,
+	showPhoneField,
+	showStepNumber,
+}: {
+	title: string;
+	description: string;
+	requireCompanyField: boolean;
+	requirePhoneField: boolean;
+	showApartmentField: boolean;
+	showCompanyField: boolean;
+	showPhoneField: boolean;
+	showStepNumber: boolean;
+} ) => {
+	const { isProcessing: checkoutIsProcessing } = useCheckoutContext();
+
+	return (
+		<FormStep
+			id="shipping-fields"
+			disabled={ checkoutIsProcessing }
+			className="wc-block-checkout__shipping-fields"
+			title={ title }
+			description={ description }
+			showStepNumber={ showStepNumber }
+		>
+			<Block
+				requireCompanyField={ requireCompanyField }
+				requirePhoneField={ requirePhoneField }
+				showApartmentField={ showApartmentField }
+				showCompanyField={ showCompanyField }
+				showPhoneField={ showPhoneField }
+			/>
+		</FormStep>
+	);
+};
+
+export default withFilteredAttributes( attributes )( FrontendBlock );
