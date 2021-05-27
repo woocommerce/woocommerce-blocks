261583b72e865d520e18dd8f059b8b0f1597c0e9
Nadir Seghir <nadir.seghir@gmail.com>
2021-05-27 11:23:18 +0100

    move payment logic to frontend

--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-payment-block/block.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-payment-block/block.tsx
@@ -1,27 +1,10 @@
 /**
- * External dependencies
- */
-import { StoreNoticesProvider } from '@woocommerce/base-context';
-import { useStoreCart, useEmitResponse } from '@woocommerce/base-context/hooks';
-
-/**
  * Internal dependencies
  */
 import { PaymentMethods } from '../../../payment-methods';
 
 const Block = (): JSX.Element | null => {
-	const { cartNeedsPayment } = useStoreCart();
-	const { noticeContexts } = useEmitResponse();
-
-	if ( ! cartNeedsPayment ) {
-		return null;
-	}
-
-	return (
-		<StoreNoticesProvider context={ noticeContexts.PAYMENTS }>
-			<PaymentMethods />
-		</StoreNoticesProvider>
-	);
+	return <PaymentMethods />;
 };
 
 export default Block;
