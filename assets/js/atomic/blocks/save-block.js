b14c1686097772b122b6e4646770468569709a3e
Mike Jolley <mike.jolley@me.com>
2021-06-01 13:50:13 +0100

    Remove atomic components (unused)

--- assets/js/atomic/blocks/save-block.js
+++ assets/js/atomic/blocks/save-block.js
@@ -2,19 +2,10 @@
  * External dependencies
  */
 import classnames from 'classnames';
-import { InnerBlocks } from '@wordpress/block-editor';
 
 export const save = ( { attributes } ) => {
 	return (
 		<div className={ classnames( 'is-loading', attributes.className ) } />
-	);
-};
-
-export const saveInnerblocks = ( { attributes } ) => {
-	return (
-		<div className={ classnames( 'is-loading', attributes.className ) }>
-			<InnerBlocks.Content />
-		</div>
 	);
 };
 
