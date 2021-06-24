4ec99e1ff96517f6ed1796f2915a9d465fb94827
Mike Jolley <mike.jolley@me.com>
2021-06-24 18:31:05 +0100

    remove export

--- assets/js/atomic/utils/index.js
+++ assets/js/atomic/utils/index.js
@@ -3,4 +3,3 @@ export * from './create-blocks-from-template';
 export * from './render-parent-block';
 export * from './block-styling';
 export * from './render-standalone-blocks';
-export * from './lazy-translations';
