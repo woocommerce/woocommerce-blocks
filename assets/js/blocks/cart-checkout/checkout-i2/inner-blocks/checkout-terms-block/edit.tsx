979031a69aaf19a8032574501d81d9af8e54799b
Mike Jolley <mike.jolley@me.com>
2021-07-14 15:53:41 +0100

    Add notice if text is missing links

--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-terms-block/edit.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-terms-block/edit.tsx
@@ -1,12 +1,15 @@
 /**
  * External dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 import CheckboxControl from '@woocommerce/base-components/checkbox-control';
+import { Notice } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
+import './editor.scss';
 import { termsConsentDefaultText, termsCheckboxDefaultText } from './constants';
 
 export const Edit = ( {
@@ -16,26 +19,63 @@ export const Edit = ( {
 	attributes: { text: string; checkbox: boolean };
 	setAttributes: ( attributes: Record< string, unknown > ) => void;
 } ): JSX.Element => {
+	const currentText = text || termsCheckboxDefaultText;
+
 	return (
-		<div className="wc-block-checkout__terms">
-			{ checkbox ? (
-				<>
-					<CheckboxControl id="terms-condition" checked={ false } />
+		<>
+			<div className="wc-block-checkout__terms">
+				{ checkbox ? (
+					<>
+						<CheckboxControl
+							id="terms-condition"
+							checked={ false }
+						/>
+						<RichText
+							value={ currentText }
+							onChange={ ( value ) =>
+								setAttributes( { text: value } )
+							}
+						/>
+					</>
+				) : (
 					<RichText
-						value={ text || termsCheckboxDefaultText }
+						tagName="span"
+						value={ text || termsConsentDefaultText }
 						onChange={ ( value ) =>
 							setAttributes( { text: value } )
 						}
 					/>
-				</>
-			) : (
-				<RichText
-					tagName="span"
-					value={ text || termsConsentDefaultText }
-					onChange={ ( value ) => setAttributes( { text: value } ) }
-				/>
+				) }
+			</div>
+			{ ! currentText.includes( '<a ' ) && (
+				<Notice
+					className="wc-block-checkout__terms_notice"
+					status="warning"
+					isDismissible={ false }
+					actions={
+						termsConsentDefaultText !== text
+							? [
+									{
+										label: __(
+											'Restore default text',
+											'woo-gutenberg-products-block'
+										),
+										onClick: () =>
+											setAttributes( { text: '' } ),
+									},
+							  ]
+							: []
+					}
+				>
+					<p>
+						{ __(
+							'Ensure you add links to your policy pages in this section.',
+							'woo-gutenberg-products-block'
+						) }
+					</p>
+				</Notice>
 			) }
-		</div>
+		</>
 	);
 };
 
