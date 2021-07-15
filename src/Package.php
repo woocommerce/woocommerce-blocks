eb4bf677e94def44686c8fdb3ba1362f19743053
Thomas Roberts <thomas.roberts@automattic.com>
2021-07-15 16:50:10 +0100

    Bump version numbers

--- src/Package.php
+++ src/Package.php
@@ -95,7 +95,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '3.6.0';
+					$version = '3.6.1';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ )
