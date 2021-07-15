ddb2e700d90442028e55da4a241f02a3e4418d43
Thomas Roberts <thomas.roberts@automattic.com>
2021-07-15 17:04:47 +0100

    Bump version numbers

--- vendor/composer/jetpack_autoload_psr4.php
+++ vendor/composer/jetpack_autoload_psr4.php
@@ -11,7 +11,7 @@ return array(
 		'path'    => array( $vendorDir . '/composer/installers/src/Composer/Installers' )
 	),
 	'Automattic\\WooCommerce\\Blocks\\' => array(
-		'version' => '4.4.3.0',
+		'version' => '4.4.4.0',
 		'path'    => array( $baseDir . '/src' )
 	),
 	'Automattic\\Jetpack\\Autoloader\\' => array(
