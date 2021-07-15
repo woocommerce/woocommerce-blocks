247643d08bbf2624ffff80574ce626538bd6916d
Mike Jolley <mike.jolley@me.com>
2021-07-15 16:54:38 +0100

    Add inline docs to block registry

---
+++ packages/checkout/blocks-registry/README.md
@@ -0,0 +1,3 @@
+# WooCommerce Blocks - Blocks Registry
+
+This directory contains the Checkout Blocks Registry - functions to register custom blocks that can be inserted into various areas within the Checkout.
--- packages/checkout/blocks-registry/index.ts
+++ packages/checkout/blocks-registry/index.ts
@@ -3,18 +3,45 @@
  */
 import { registerBlockType, BlockConfiguration } from '@wordpress/blocks';
 import { registerBlockComponent } from '@woocommerce/blocks-registry';
-type RegistredBlocks = { totals: Array< string >; fields: Array< string > };
 
-let registeredBlocks: RegistredBlocks = {
-	totals: [],
-	fields: [],
+/**
+ * List of block areas where blocks can be registered for use. Keyed by area name.
+ */
+type RegisteredBlocks = {
+	totals: Array< string >;
+	fields: Array< string >;
+	contactInformation: Array< string >;
+	shippingAddress: Array< string >;
+	billingAddress: Array< string >;
+	shippingMethods: Array< string >;
+	paymentMethods: Array< string >;
 };
 
-export const addToCheckoutArea = (
-	area: keyof RegistredBlocks,
+let registeredBlocks: RegisteredBlocks = {
+	totals: [],
+	fields: [],
+	contactInformation: [],
+	shippingAddress: [],
+	billingAddress: [],
+	shippingMethods: [],
+	paymentMethods: [],
+};
+
+/**
+ * Validation to ensure an area exists.
+ */
+const isValidArea = ( area: string ): boolean => {
+	return registeredBlocks.hasOwnProperty( area );
+};
+
+/**
+ * Adds a block (block name) to an area, if the area exists. If the area does not exist, an error is thrown.
+ */
+const registerBlockForArea = (
+	area: keyof RegisteredBlocks,
 	blockName: string
 ): void | Error => {
-	if ( ! registeredBlocks.hasOwnProperty( area ) ) {
+	if ( ! isValidArea( area ) ) {
 		throw new Error(
 			`Incorrect value for the "area" argument when registering the checkout block. It was a ${ area }, but must be one of ${ Object.keys(
 				registeredBlocks
@@ -27,31 +54,52 @@ export const addToCheckoutArea = (
 	};
 };
 
+/**
+ * Get a list of blocks available within a specific area.
+ */
 export const getRegisteredBlocks = (
-	area: keyof RegistredBlocks
+	area: keyof RegisteredBlocks
 ): Array< string > => {
+	if ( ! isValidArea( area ) ) {
+		throw new Error(
+			`Incorrect value for the "area" argument. It was a ${ area }, but must be one of ${ Object.keys(
+				registeredBlocks
+			).join( ', ' ) }.`
+		);
+	}
 	return registeredBlocks[ area ];
 };
 
+/**
+ * Main API for registering a new checkout block within areas.
+ */
 export const registerCheckoutBlock = (
+	// Name of the block e.g. woocommerce/sample-block
 	blockName: string,
-	{
-		block: Block,
-		area,
-		...config
-	}: {
-		block: JSX.Element;
-		area: keyof RegistredBlocks;
-		config: BlockConfiguration;
-	}
+	// This is a component to render on the frontend in place of this block, when used.
+	blockComponent: JSX.Element,
+	// Area(s) to add the block to. This can be a single area (string) or an array of areas.
+	blockAreas: keyof RegisteredBlocks | Array< keyof RegisteredBlocks >,
+	// Standard block configuration object. If not passed, the block will not be registered with WordPress and must be done manually.
+	blockConfiguration?: BlockConfiguration
 ): void => {
-	registerBlockType( blockName, {
-		...config,
-		category: 'woocommerce',
-	} );
-	addToCheckoutArea( area, blockName );
+	if ( blockConfiguration ) {
+		registerBlockType( blockName, {
+			...blockConfiguration,
+			category: 'woocommerce',
+		} );
+	}
+
+	if ( Array.isArray( blockAreas ) ) {
+		blockAreas.forEach( ( area ) =>
+			registerBlockForArea( area, blockName )
+		);
+	} else {
+		registerBlockForArea( blockAreas, blockName );
+	}
+
 	registerBlockComponent( {
 		blockName,
-		component: Block,
+		component: blockComponent,
 	} );
 };
