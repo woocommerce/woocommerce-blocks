e65b23c98e8de8dd30287d401af12d5758b570ec
Mike Jolley <mike.jolley@me.com>
2021-07-22 14:03:48 +0100

    Add notice if terms is unchecked when processing checkout

--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-terms-block/frontend.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-terms-block/frontend.tsx
@@ -1,8 +1,10 @@
 /**
  * External dependencies
  */
+import { __ } from '@wordpress/i18n';
+import { useState, useEffect } from '@wordpress/element';
 import CheckboxControl from '@woocommerce/base-components/checkbox-control';
-import { useState } from '@wordpress/element';
+import { useCheckoutContext, useStoreNotices } from '@woocommerce/base-context';
 
 /**
  * Internal dependencies
@@ -18,6 +20,34 @@ const FrontendBlock = ( {
 	checkbox: boolean;
 } ): JSX.Element => {
 	const [ checked, setChecked ] = useState( false );
+	const {
+		dispatchActions,
+		onCheckoutValidationBeforeProcessing,
+	} = useCheckoutContext();
+	const { addErrorNotice, removeNotice } = useStoreNotices();
+
+	useEffect( () => {
+		const unsubscribe = onCheckoutValidationBeforeProcessing( () => {
+			if ( ! checked ) {
+				return {
+					errorMessage: __(
+						'Please read and accept the terms and conditions to proceed with your order.',
+						'woo-gutenberg-products-block'
+					),
+				};
+			}
+		} );
+		return () => {
+			unsubscribe();
+		};
+	}, [
+		onCheckoutValidationBeforeProcessing,
+		addErrorNotice,
+		removeNotice,
+		checked,
+		dispatchActions,
+	] );
+
 	return (
 		<div className="wc-block-checkout__terms">
 			{ checkbox ? (
