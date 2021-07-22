ce965b4adfb30a61138d995538bb3961ac6f0fac
Mike Jolley <mike.jolley@me.com>
2021-07-22 16:10:09 +0100

    update todo

--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-terms-block/frontend.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-terms-block/frontend.tsx
@@ -26,7 +26,7 @@ const FrontendBlock = ( {
 } ): JSX.Element => {
 	const [ checked, setChecked ] = useState( false );
 
-	// @todo Checkout i2: Pass validation context to Inner Blocks to avoid exporting in a public package.
+	// @todo Checkout i2 - Pass validation context to Inner Blocks to avoid exporting in a public package.
 	const { isDisabled } = useCheckoutSubmit();
 	const {
 		getValidationError,
