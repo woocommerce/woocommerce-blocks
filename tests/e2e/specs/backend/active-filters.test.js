e65988c5cee517799db3a30bec4d8c812044fd67
Thomas Roberts <thomas.roberts@automattic.com>
2021-08-11 09:47:52 +0100

    Add tests for active filters block

---
+++ tests/e2e/specs/backend/active-filters.test.js
@@ -0,0 +1,77 @@
+/**
+ * External dependencies
+ */
+import {
+	insertBlock,
+	getEditedPostContent,
+	getAllBlocks,
+	switchUserToAdmin,
+	openDocumentSettingsSidebar,
+} from '@wordpress/e2e-test-utils';
+
+import {
+	visitBlockPage,
+	clearAndFillInput,
+} from '@woocommerce/blocks-test-utils';
+import { insertBlockDontWaitForInsertClose } from '../../utils';
+
+const block = {
+	name: 'Active Product Filters',
+	slug: 'woocommerce/active-filters',
+	class: '.wc-block-active-filters',
+};
+
+if ( process.env.WP_VERSION < 5.3 )
+	// eslint-disable-next-line jest/no-focused-tests
+	test.only( 'skipping all other things', () => {} );
+
+describe( `${ block.name } Block`, () => {
+	beforeAll( async () => {
+		await switchUserToAdmin();
+		await visitBlockPage( `${ block.name } Block` );
+	} );
+
+	it( 'can only be inserted once', async () => {
+		await insertBlockDontWaitForInsertClose( block.name );
+		expect( await getAllBlocks() ).toHaveLength( 1 );
+	} );
+
+	it( 'renders without crashing', async () => {
+		await expect( page ).toRenderBlock( block );
+	} );
+
+	it( 'allows title can be manipulated', async () => {
+		await openDocumentSettingsSidebar();
+		await clearAndFillInput(
+			'.wp-block[data-type="woocommerce/active-filters"] textarea.wc-block-editor-components-title',
+			'New Title'
+		);
+		await page.click(
+			'.components-toolbar button[aria-label="Heading 6"]'
+		);
+		await expect(
+			page
+		).toMatchElement(
+			'.wp-block[data-type="woocommerce/active-filters"] h6 textarea',
+			{ text: 'New Title' }
+		);
+
+		expect( await getEditedPostContent() ).toMatchSnapshot();
+		await clearAndFillInput(
+			'.wp-block[data-type="woocommerce/active-filters"] textarea.wc-block-editor-components-title',
+			'Active filters'
+		);
+		await page.click(
+			'.components-toolbar button[aria-label="Heading 3"]'
+		);
+	} );
+
+	it( 'Display style to change', async () => {
+		await openDocumentSettingsSidebar();
+		await page.click( 'button[aria-label="Display Style: Chips"]' );
+		await expect( page ).toMatchElement(
+			'.wc-block-active-filters__list.wc-block-active-filters__list--chips'
+		);
+		await page.click( 'button[aria-label="Display Style: List"]' );
+	} );
+} );
