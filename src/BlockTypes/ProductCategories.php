ee41dc3dbe535d60c12bf78d8e0d380963ad2b4b
Thomas Roberts <thomas.roberts@automattic.com>
2021-08-16 17:40:43 +0100

    Remove spaces from around title

--- src/BlockTypes/ProductCategories.php
+++ src/BlockTypes/ProductCategories.php
@@ -304,9 +304,7 @@ class ProductCategories extends AbstractDynamicBlock {
 		foreach ( $categories as $category ) {
 			$output .= '
 				<li class="wc-block-product-categories-list-item">
-					<a href="' . esc_attr( get_term_link( $category->term_id, 'product_cat' ) ) . '">
-						' . $this->get_image_html( $category, $attributes ) . esc_html( $category->name ) . '
-					</a>
+					<a href="' . esc_attr( get_term_link( $category->term_id, 'product_cat' ) ) . '">' . $this->get_image_html( $category, $attributes ) . esc_html( $category->name ) . '</a>
 					(' . $this->getCount( $category, $attributes ) . ')
 					' . ( ! empty( $category->children ) ? $this->renderList( $category->children, $attributes, $uid, $depth + 1 ) : '' ) . '
 				</li>
