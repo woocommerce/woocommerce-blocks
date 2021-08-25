b10cb4924fa7e49d79376f500f4ae914890e130b
Albert Juhé Lluveras <contact@albertjuhe.com>
2021-08-25 16:00:55 +0200

    Add function to check if script tag is already in the DOM

--- assets/js/base/utils/lazy-load-script.ts
+++ assets/js/base/utils/lazy-load-script.ts
@@ -8,6 +8,17 @@ interface lazyLoadScriptParams {
 }
 
 /**
+ * In WP, registered scripts are loaded into the page with an element like this:
+ * `<script src='...' id='[SCRIPT_ID]'></script>`
+ * This function checks whether an element matching that selector exists.
+ * Useful to know if a script has already been appended to the page.
+ */
+const isScriptTagInDOM = ( scriptId: string ): boolean => {
+	const scriptElements = document.querySelectorAll( `script#${ scriptId }` );
+	return scriptElements.length > 0;
+};
+
+/**
  * Appends a `<script>` tag to the document body based on the src and handle
  * parameters. In addition, it appends additional script tags to load the code
  * needed for translations and any before and after inline scripts. See these
@@ -26,55 +37,40 @@ const lazyLoadScript = ( {
 }: lazyLoadScriptParams ): Promise< void > => {
 	return new Promise( ( resolve, reject ) => {
 		// Append script translations if they doesn't exist yet in the page.
-		if ( translations ) {
-			const handleTranslationsElements = document.querySelectorAll(
-				`#${ handle }-js-translations`
-			);
-			if ( handleTranslationsElements.length === 0 ) {
-				const handleTranslations = document.createElement( 'script' );
-				handleTranslations.innerHTML = translations;
-				handleTranslations.id = `${ handle }-js-translations`;
-				document.body.appendChild( handleTranslations );
-			}
+		if (
+			translations &&
+			! isScriptTagInDOM( `${ handle }-js-translations` )
+		) {
+			const handleTranslations = document.createElement( 'script' );
+			handleTranslations.innerHTML = translations;
+			handleTranslations.id = `${ handle }-js-translations`;
+			document.body.appendChild( handleTranslations );
 		}
 		// Append before inline script if it doesn't exist yet in the page.
-		if ( before ) {
-			const handleBeforeScriptElements = document.querySelectorAll(
-				`#${ handle }-js-before`
-			);
-			if ( handleBeforeScriptElements.length === 0 ) {
-				const handleBeforeScript = document.createElement( 'script' );
-				handleBeforeScript.innerHTML = before;
-				handleBeforeScript.id = `${ handle }-js-before`;
-				document.body.appendChild( handleBeforeScript );
-			}
+		if ( before && ! isScriptTagInDOM( `${ handle }-js-before` ) ) {
+			const handleBeforeScript = document.createElement( 'script' );
+			handleBeforeScript.innerHTML = before;
+			handleBeforeScript.id = `${ handle }-js-before`;
+			document.body.appendChild( handleBeforeScript );
 		}
 
-		// Append script.
-		const handleScriptElements = document.querySelectorAll(
-			`#${ handle }-js`
-		);
-		if ( handleScriptElements.length > 0 ) {
+		if ( isScriptTagInDOM( `${ handle }-js` ) ) {
 			resolve();
 		} else {
+			// Append script.
 			const handleScript = document.createElement( 'script' );
 			handleScript.src = version ? `${ src }?${ version }` : src;
 			handleScript.id = `${ handle }-js`;
 			handleScript.onerror = reject;
 			handleScript.onload = () => {
 				// Append after inline script if it doesn't exist yet in the page.
-				if ( after ) {
-					const handleAfterScriptElements = document.querySelectorAll(
-						`#${ handle }-js-after`
+				if ( after && ! isScriptTagInDOM( `${ handle }-js-after` ) ) {
+					const handleAfterScript = document.createElement(
+						'script'
 					);
-					if ( handleAfterScriptElements.length === 0 ) {
-						const handleAfterScript = document.createElement(
-							'script'
-						);
-						handleAfterScript.innerHTML = after;
-						handleAfterScript.id = `${ handle }-js-after`;
-						document.body.appendChild( handleAfterScript );
-					}
+					handleAfterScript.innerHTML = after;
+					handleAfterScript.id = `${ handle }-js-after`;
+					document.body.appendChild( handleAfterScript );
 				}
 				resolve();
 			};
