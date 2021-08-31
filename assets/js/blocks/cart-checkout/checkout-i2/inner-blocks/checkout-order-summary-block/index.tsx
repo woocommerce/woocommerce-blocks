2b9f877485550981c0eecbe436fdfb4a791e7e40
Mike Jolley <mike.jolley@me.com>
2021-08-31 10:57:04 +0100

    Order summary - totals icon

--- assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-order-summary-block/index.tsx
+++ assets/js/blocks/cart-checkout/checkout-i2/inner-blocks/checkout-order-summary-block/index.tsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { Icon, totals } from '@woocommerce/icons';
 import { registerFeaturePluginBlockType } from '@woocommerce/block-settings';
 
 /**
@@ -17,6 +18,10 @@ registerFeaturePluginBlockType( 'woocommerce/checkout-order-summary-block', {
 		'Displays the order summary and totals.',
 		'woo-gutenberg-products-block'
 	),
+	icon: {
+		src: <Icon srcElement={ totals } />,
+		foreground: '#874FB9',
+	},
 	supports: {
 		align: false,
 		html: false,
--- assets/js/icons/index.js
+++ assets/js/icons/index.js
@@ -47,6 +47,7 @@ export { default as tag } from './library/tag';
 export { default as tags } from './library/tags';
 export { default as thumbUp } from './library/thumb-up';
 export { default as toggle } from './library/toggle';
+export { default as totals } from './library/totals';
 export { default as truck } from './library/truck';
 export { default as widgets } from './library/widgets';
 export { default as woo } from './library/woo';
