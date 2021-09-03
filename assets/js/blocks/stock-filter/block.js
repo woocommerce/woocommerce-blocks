94eefe870e5040b4e034d24936520dd10b6ca093
Thomas Roberts <thomas.roberts@automattic.com>
2021-09-03 15:35:50 +0100

    Move initial stock options and the values they are derived from into useState

--- assets/js/blocks/stock-filter/block.js
+++ assets/js/blocks/stock-filter/block.js
@@ -34,24 +34,29 @@ const StockStatusFilterBlock = ( {
 	attributes: blockAttributes,
 	isEditor = false,
 } ) => {
-	const hideOutOfStockItems = getSetting( 'hideOutOfStockItems', false );
-	const { outofstock, ...otherStockStatusOptions } = getSetting(
-		'stockStatusOptions',
-		{}
+	const [ hideOutOfStockItems ] = useState(
+		getSetting( 'hideOutOfStockItems', false )
 	);
-	const STOCK_STATUS_OPTIONS = hideOutOfStockItems
-		? otherStockStatusOptions
-		: { outofstock, ...otherStockStatusOptions };
+	const [ { outofstock, ...otherStockStatusOptions } ] = useState(
+		getSetting( 'stockStatusOptions', {} )
+	);
+	const [ STOCK_STATUS_OPTIONS ] = useState(
+		hideOutOfStockItems
+			? otherStockStatusOptions
+			: { outofstock, ...otherStockStatusOptions }
+	);
 
 	const [ checked, setChecked ] = useState( [] );
 	const [ displayedOptions, setDisplayedOptions ] = useState(
 		blockAttributes.isPreview ? previewOptions : []
 	);
 	// Filter added to handle if there are slugs without a corresponding name defined.
-	const initialOptions = Object.entries( STOCK_STATUS_OPTIONS )
-		.map( ( [ slug, name ] ) => ( { slug, name } ) )
-		.filter( ( status ) => !! status.name )
-		.sort( ( a, b ) => a.slug.localeCompare( b.slug ) );
+	const [ initialOptions ] = useState(
+		Object.entries( STOCK_STATUS_OPTIONS )
+			.map( ( [ slug, name ] ) => ( { slug, name } ) )
+			.filter( ( status ) => !! status.name )
+			.sort( ( a, b ) => a.slug.localeCompare( b.slug ) )
+	);
 
 	const [ queryState ] = useQueryStateByContext();
 	const [
