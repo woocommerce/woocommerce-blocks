8fe65fdd66280ce8379bce4e3d417db9b19efd7f
Raluca Stan <ralucastn@gmail.com>
2021-09-09 16:03:41 +0200

    Type assertions and add them back to PaymentMethodConfig

--- assets/js/blocks-registry/payment-methods/payment-method-config.ts
+++ assets/js/blocks-registry/payment-methods/payment-method-config.ts
@@ -37,6 +37,14 @@ export interface PaymentMethodConfigClass {
 	savedTokenComponent?: ReactNode;
 	canMakePaymentFromConfig: CanMakePayment;
 }
+/**
+ * Internal dependencies
+ */
+import {
+	assertConfigHasProperties,
+	assertValidElement,
+	assertValidElementOrString,
+} from './assertions';
 
 const NullComponent = () => {
 	return null;
@@ -101,6 +109,66 @@ export default class PaymentMethodConfig implements PaymentMethodConfigClass {
 	}
 
 	static assertValidConfig = ( config: PaymentMethodConfigType ): void => {
+		assertConfigHasProperties( config, [
+			'name',
+			'label',
+			'ariaLabel',
+			'content',
+			'edit',
+			'canMakePayment',
+		] );
+		if ( typeof config.name !== 'string' ) {
+			throw new Error(
+				'The name property for the payment method must be a string'
+			);
+		}
+		if (
+			typeof config.icons !== 'undefined' &&
+			! Array.isArray( config.icons ) &&
+			config.icons !== null
+		) {
+			throw new Error(
+				'The icons property for the payment method must be an array or null.'
+			);
+		}
+		if (
+			typeof config.paymentMethodId !== 'string' &&
+			typeof config.paymentMethodId !== 'undefined'
+		) {
+			throw new Error(
+				'The paymentMethodId property for the payment method must be a string or undefined (in which case it will be the value of the name property).'
+			);
+		}
+		if (
+			typeof config.placeOrderButtonLabel !== 'string' &&
+			typeof config.placeOrderButtonLabel !== 'undefined'
+		) {
+			throw new TypeError(
+				'The placeOrderButtonLabel property for the payment method must be a string'
+			);
+		}
+		assertValidElementOrString( config.label, 'label' );
+		assertValidElement( config.content, 'content' );
+		assertValidElement( config.edit, 'edit' );
+		assertValidElement( config.savedTokenComponent, 'savedTokenComponent' );
+		if ( typeof config.ariaLabel !== 'string' ) {
+			throw new TypeError(
+				'The ariaLabel property for the payment method must be a string'
+			);
+		}
+		if ( typeof config.canMakePayment !== 'function' ) {
+			throw new TypeError(
+				'The canMakePayment property for the payment method must be a function.'
+			);
+		}
+		if (
+			typeof config.supports?.showSavedCards !== 'undefined' &&
+			typeof config.supports?.showSavedCards !== 'boolean'
+		) {
+			throw new TypeError(
+				'If the payment method includes the `supports.showSavedCards` property, it must be a boolean'
+			);
+		}
 		if ( typeof config.supports?.savePaymentInfo !== 'undefined' ) {
 			deprecated(
 				'Passing savePaymentInfo when registering a payment method.',
@@ -110,6 +178,22 @@ export default class PaymentMethodConfig implements PaymentMethodConfigClass {
 					link:
 						'https://github.com/woocommerce/woocommerce-gutenberg-products-block/pull/3686',
 				}
+			);
+		}
+		if (
+			typeof config.supports?.features !== 'undefined' &&
+			! Array.isArray( config.supports?.features )
+		) {
+			throw new Error(
+				'The features property for the payment method must be an array or undefined.'
+			);
+		}
+		if (
+			typeof config.supports?.showSaveOption !== 'undefined' &&
+			typeof config.supports?.showSaveOption !== 'boolean'
+		) {
+			throw new TypeError(
+				'If the payment method includes the `supports.showSaveOption` property, it must be a boolean'
 			);
 		}
 	};
