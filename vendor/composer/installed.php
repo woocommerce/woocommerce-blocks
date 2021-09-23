ac5c4219ac1848f1425befe980d25dcf64f064a9
Mike Jolley <mike.jolley@me.com>
2021-09-23 14:58:57 +0100

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-trunk',
+    'version' => 'dev-trunk',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1036ccd738dee45f2f8556e3b55ba0709ffe7d05',
+    'name' => 'woocommerce/woocommerce-blocks',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.10.3',
+      'version' => '2.10.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aab966d6f2c8fd6669d6f5b1378d8ced5fd665b4',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.11.0',
+      'version' => '1.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ae03311f45dfe194412081526be2e003960df74b',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'woocommerce/woocommerce-blocks' => 
+    array (
+      'pretty_version' => 'dev-trunk',
+      'version' => 'dev-trunk',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1036ccd738dee45f2f8556e3b55ba0709ffe7d05',
+    ),
+  ),
+);
