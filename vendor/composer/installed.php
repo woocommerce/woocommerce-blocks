053375d242726341e854d1ec2941fc991e64bb5a
Nadir Seghir <nadir.seghir@gmail.com>
2021-09-29 09:25:13 +0100

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-trunk',
+    'version' => 'dev-trunk',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '71146c1aa2eb9fd2d898187ba52bca6151a05b30',
+    'name' => 'woocommerce/woocommerce-blocks',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.10.4',
+      'version' => '2.10.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '70cb300a7a215ae87c671f600f77093518f87bac',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.11.0',
+      'version' => '1.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ae03311f45dfe194412081526be2e003960df74b',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'woocommerce/woocommerce-blocks' => 
+    array (
+      'pretty_version' => 'dev-trunk',
+      'version' => 'dev-trunk',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '71146c1aa2eb9fd2d898187ba52bca6151a05b30',
+    ),
+  ),
+);
