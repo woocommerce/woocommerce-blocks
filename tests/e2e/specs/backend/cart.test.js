c57f2c5ab1d7b60d8824d854c1cb9a8281a71c4b
Thomas Roberts <thomas.roberts@automattic.com>
2021-10-05 15:20:44 +0100

    Force cart test to break
    Revert this!

--- tests/e2e/specs/backend/cart.test.js
+++ tests/e2e/specs/backend/cart.test.js
@@ -44,7 +44,8 @@ describe( `${ block.name } Block`, () => {
 			const compatibilityNoticeTitle = await page.$x(
 				`//h1[contains(text(), 'Compatibility notice')]`
 			);
-			expect( compatibilityNoticeTitle.length ).toBe( 1 );
+			expect( compatibilityNoticeTitle.length ).not.toBe( 1 );
+			expect( compatibilityNoticeTitle.length ).not.toBe( 1 );
 		} );
 	} );
 
