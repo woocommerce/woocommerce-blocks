6e33ec9dbc76bea1c33b77a0253fdb1bbfad6324
Tung Du <dinhtungdu@gmail.com>
2021-10-06 17:54:54 +0700

    style the button in the editor

--- assets/js/blocks/cart-checkout/mini-cart/edit.tsx
+++ assets/js/blocks/cart-checkout/mini-cart/edit.tsx
@@ -4,6 +4,12 @@
 import { _n, sprintf } from '@wordpress/i18n';
 import { useBlockProps } from '@wordpress/block-editor';
 import type { ReactElement } from 'react';
+import { formatPrice } from '@woocommerce/price-format';
+
+/**
+ * Internal dependencies
+ */
+import QuantityBadge from './quantity-badge';
 
 const MiniCartBlock = (): ReactElement => {
 	const blockProps = useBlockProps( {
@@ -11,20 +17,25 @@ const MiniCartBlock = (): ReactElement => {
 	} );
 
 	const productCount = 0;
+	const productTotal = 0;
 
 	return (
 		<div { ...blockProps }>
 			<button className="wc-block-mini-cart__button">
-				{ sprintf(
-					/* translators: %d is the number of products in the cart. */
-					_n(
-						'%d product',
-						'%d products',
-						productCount,
-						'woo-gutenberg-products-block'
-					),
-					productCount
-				) }
+				<span className="amount">{ formatPrice( productTotal ) }</span>
+				<span className="count">
+					{ sprintf(
+						/* translators: %d is the number of products in the cart. */
+						_n(
+							'%d product',
+							'%d products',
+							productCount,
+							'woo-gutenberg-products-block'
+						),
+						productCount
+					) }
+				</span>
+				<QuantityBadge count={ productCount } />
 			</button>
 		</div>
 	);
