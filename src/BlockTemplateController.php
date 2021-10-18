ff69c61675fd1097d2dedf68909ce28e912b6a72
Tom Cafferkey <tjcafferkey@gmail.com>
2021-10-18 08:58:16 +0100

    BlockTemplateController

--- src/BlockTemplateController.php
+++ src/BlockTemplateController.php
@@ -13,9 +13,7 @@ final class BlockTemplateController {
 	 * Initialization method.
 	 */
 	protected function init() {
-		if ( ! current_theme_supports('block-templates') ) {
-			return;
-		}
+		// Check if the current template is a block template.
 
 		add_action( 'init', array($this, 'create_template') );
 	}
@@ -23,10 +21,26 @@ final class BlockTemplateController {
 	/**
 	 * Automatically create template for product post types which uses the legacy-single-product block.
 	 */
-	public function create_single_product_template() {
-		$post_type_object = get_post_type_object('product');
-		$post_type_object->template = array(
-			array('woocommerce/legacy-single-product'),
-		);
+	public function create_template() {
+		add_filter('get_block_templates', function(  $query_result, $query, $template_type ) {
+//			file_put_contents("query.txt", json_encode($query));
+//			file_put_contents("query_result.txt", json_encode($query_result));
+			if ( ! $query ) {
+				$query_result[] = (object) array(
+					"type" => "wp_template",
+					"theme" => "theme-experiments/tt1-blocks",
+					"slug" => "archive-product",
+					"id" => "theme-experiments/tt1-blocks//archive-product",
+					"title" => "Archive Product",
+					"content" => '<!-- wp:template-part {"slug":"header" "tagName": "header"} /--><!-- wp:woocommerce/legacy-archive-product /--><!-- wp:template-part {"slug":"footer","className":"site-footer-container"} /-->',
+					"description" => "Template used to display a product archive",
+					"source" => "woocommerce-gutenberg-products-block",
+					"wp_id" => null,
+					"status" => "publish",
+					"has_theme_file" => false
+				);
+			}
+			return $query_result;
+		}, 10, 3);
 	}
 }
