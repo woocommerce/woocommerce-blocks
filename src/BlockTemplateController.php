05b317b437f0483b409e70de19fbd2bbe9c36d1e
Tom Cafferkey <tjcafferkey@gmail.com>
2021-10-19 14:07:51 +0100

    Overwrite block templates

--- src/BlockTemplateController.php
+++ src/BlockTemplateController.php
@@ -2,6 +2,8 @@
 namespace Automattic\WooCommerce\Blocks;
 
 final class BlockTemplateController {
+	private $template_dir;
+
 	const TEMPLATE_BASE_PATH = 'block-templates';
 
 	/**
@@ -9,13 +11,17 @@ final class BlockTemplateController {
 	 */
 	public function __construct() {
 		$this->init();
+		$this->template_dir = plugin_dir_path( __FILE__ ) . '../templates/' . self::TEMPLATE_BASE_PATH;
 	}
 
 	/**
 	 * Initialization method.
 	 */
 	protected function init() {
-		add_action( 'init', array($this, 'add_block_templates') );
+		if ( gutenberg_supports_block_templates() ) {
+			add_action( 'init', array($this, 'add_block_templates') );
+			add_action( 'wp', array($this, 'load_template') );
+		}
 	}
 
 	/**
@@ -23,11 +29,9 @@ final class BlockTemplateController {
 	 */
 	public function add_block_templates() {
 		add_filter('get_block_templates', function(  $query_result, $query ) {
-			if ( ! $query ) {
-				$template_files = $this->get_block_template_files();
-				foreach($template_files as $template_file) {
-					$query_result[] = _gutenberg_build_template_result_from_file( $template_file, 'wp_template' );
-				}
+			$template_files = $this->get_block_template_files();
+			foreach($template_files as $template_file) {
+				$query_result[] = _gutenberg_build_template_result_from_file( $template_file, 'wp_template' );
 			}
 			return $query_result;
 		}, 10, 3);
@@ -39,8 +43,7 @@ final class BlockTemplateController {
 	 * @return array;
 	 */
 	public function get_block_template_files() {
-		$theme_dir = plugin_dir_path( __FILE__ ) . '../templates/' . self::TEMPLATE_BASE_PATH;
-		$template_files = _gutenberg_get_template_paths( $theme_dir );
+		$template_files = _gutenberg_get_template_paths( $this->template_dir );
 		$templates = array();
 
 		foreach($template_files as $template_file) {
@@ -63,5 +66,34 @@ final class BlockTemplateController {
 		}
 
 		return $templates;
+	}
+
+	/**
+	 * Checks whether a block template with that name exists.
+	 *
+	 * @param string $template_name Template to check.
+	 * @return boolean
+	 */
+	public function has_block_template( $template_name ) {
+		if ( ! $template_name ) {
+			return false;
+		}
+
+		return is_readable(
+			$this->template_dir . '/' . $template_name . '.html'
+		);
+	}
+
+	public function load_template() {
+		if ( is_embed() ) {
+			return;
+		}
+
+		if (
+			is_singular( 'product' ) &&
+			$this->has_block_template( 'single-product' )
+		) {
+			add_filter( 'wc_has_block_template', '__return_true', 10, 0 );
+		}
 	}
 }
