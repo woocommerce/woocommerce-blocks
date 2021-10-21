982442f701c359cdc9e882376ed437ad13753d13
Tom Cafferkey <tjcafferkey@gmail.com>
2021-10-21 14:49:20 +0100

    BlockTemplateController

---
+++ src/BlockTemplatesController.php
@@ -0,0 +1,85 @@
+<?php
+namespace Automattic\WooCommerce\Blocks;
+
+/**
+ * BlockTypesController class.
+ *
+ * @internal
+ */
+class BlockTemplatesController {
+
+	/**
+	 * Holds the path for the directory where the block templates will be kept.
+	 *
+	 * @var string
+	 */
+	private $templates_directory;
+
+	/**
+	 * Directory name of the block template directory.
+	 *
+	 * @var string
+	 */
+	const TEMPLATES_DIR_NAME = 'block-templates';
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->templates_directory = plugin_dir_path( __FILE__ ) . '../templates/' . self::TEMPLATES_DIR_NAME;
+		$this->init();
+	}
+
+	/**
+	 * Initialization method.
+	 */
+	protected function init() {
+		if ( gutenberg_supports_block_templates() ) {
+			add_filter( 'get_block_templates', array( $this, 'add_block_templates' ), 10, 3 );
+		}
+	}
+
+	/**
+	 * Add the block template objects to be used.
+	 *
+	 * @param array $query_result Array of template objects.
+	 * @return array
+	 */
+	protected function add_block_templates( $query_result ) {
+		$template_files = $this->get_block_templates();
+
+		foreach ( $template_files as $template_file ) {
+			$query_result[] = _gutenberg_build_template_result_from_file( $template_file, 'wp_template' );
+		}
+
+		return $query_result;
+	}
+
+	/**
+	 * Get and build the block template objects from the block template files.
+	 *
+	 * @return array
+	 */
+	protected function get_block_templates() {
+		$template_files = _gutenberg_get_template_paths( $this->templates_directory );
+		$templates      = array();
+
+		foreach ( $template_files as $template_file ) {
+			$template_slug     = substr(
+				$template_file,
+				strpos( $template_file, self::TEMPLATES_DIR_NAME . DIRECTORY_SEPARATOR ) + 1 + strlen( self::TEMPLATES_DIR_NAME ),
+				-5
+			);
+			$new_template_item = array(
+				'title' => ucwords( str_replace( '-', ' ', $template_slug ) ),
+				'slug'  => $template_slug,
+				'path'  => $template_file,
+				'theme' => get_template_directory(),
+				'type'  => 'wp_template',
+			);
+			$templates[]       = $new_template_item;
+		}
+
+		return $templates;
+	}
+}
