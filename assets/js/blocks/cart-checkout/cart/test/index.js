b3f800ba7ef8233a7b90082205d679f37169498a
Nadir Seghir <nadir.seghir@gmail.com>
2021-10-21 11:18:41 +0100

    move blocks to their own file

--- assets/js/blocks/cart-checkout/cart/test/index.js
+++ assets/js/blocks/cart-checkout/cart/test/index.js
@@ -9,52 +9,11 @@ import { default as fetchMock } from 'jest-fetch-mock';
 /**
  * Internal dependencies
  */
-import Cart from '../block';
-
-import FilledCart from '../inner-blocks/filled-cart-block/frontend';
-import EmptyCart from '../inner-blocks/empty-cart-block/frontend';
-
-import ItemsBlock from '../inner-blocks/cart-items-block/frontend';
-import TotalsBlock from '../inner-blocks/cart-totals-block/frontend';
-
-import LineItemsBlock from '../inner-blocks/cart-line-items-block/block.tsx';
-import OrderSummaryBlock from '../inner-blocks/cart-order-summary-block/block.tsx';
-import ExpressPaymentBlock from '../inner-blocks/cart-express-payment-block/block.tsx';
-import ProceedToCheckoutBlock from '../inner-blocks/proceed-to-checkout-block/block.tsx';
-import AcceptedPaymentMethodsIcons from '../inner-blocks/cart-accepted-payment-methods-block/block.tsx';
+import CartBlock from './block';
 
 import { defaultCartState } from '../../../../data/default-states';
 import { allSettings } from '../../../../settings/shared/settings-init';
 
-const CartBlock = ( { attributes = {} } ) => {
-	const {
-		showRateAfterTaxName = false,
-		isShippingCalculatorEnabled = false,
-	} = attributes;
-	return (
-		<Cart attributes={ attributes }>
-			<FilledCart>
-				<ItemsBlock>
-					<LineItemsBlock />
-				</ItemsBlock>
-				<TotalsBlock>
-					<OrderSummaryBlock
-						showRateAfterTaxName={ showRateAfterTaxName }
-						isShippingCalculatorEnabled={
-							isShippingCalculatorEnabled
-						}
-					/>
-					<ExpressPaymentBlock />
-					<ProceedToCheckoutBlock />
-					<AcceptedPaymentMethodsIcons />
-				</TotalsBlock>
-			</FilledCart>
-			<EmptyCart>
-				<p>Empty Cart</p>
-			</EmptyCart>
-		</Cart>
-	);
-};
 describe( 'Testing cart', () => {
 	beforeEach( async () => {
 		fetchMock.mockResponse( ( req ) => {
