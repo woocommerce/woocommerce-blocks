851ae14ebcf23cd1e8731c70b9278e2b4587bc0c
Nadir Seghir <nadir.seghir@gmail.com>
2021-10-21 11:57:40 +0100

    remove .tsx and update jest config

--- assets/js/blocks/cart-checkout/cart/test/cart-block.js
+++ assets/js/blocks/cart-checkout/cart/test/cart-block.js
@@ -9,16 +9,23 @@ import EmptyCart from '../inner-blocks/empty-cart-block/frontend';
 import ItemsBlock from '../inner-blocks/cart-items-block/frontend';
 import TotalsBlock from '../inner-blocks/cart-totals-block/frontend';
 
-import LineItemsBlock from '../inner-blocks/cart-line-items-block/block.tsx';
-import OrderSummaryBlock from '../inner-blocks/cart-order-summary-block/block.tsx';
-import ExpressPaymentBlock from '../inner-blocks/cart-express-payment-block/block.tsx';
-import ProceedToCheckoutBlock from '../inner-blocks/proceed-to-checkout-block/block.tsx';
-import AcceptedPaymentMethodsIcons from '../inner-blocks/cart-accepted-payment-methods-block/block.tsx';
+import LineItemsBlock from '../inner-blocks/cart-line-items-block/block';
+import OrderSummaryBlock from '../inner-blocks/cart-order-summary-block/block';
+import ExpressPaymentBlock from '../inner-blocks/cart-express-payment-block/block';
+import ProceedToCheckoutBlock from '../inner-blocks/proceed-to-checkout-block/block';
+import AcceptedPaymentMethodsIcons from '../inner-blocks/cart-accepted-payment-methods-block/block';
 
-export default function CartBlock( { attributes = {} } ) {
+export default function CartBlock( {
+	attributes = {
+		showRateAfterTaxName: false,
+		isShippingCalculatorEnabled: false,
+		checkoutPageId: 0,
+	},
+} ) {
 	const {
-		showRateAfterTaxName = false,
-		isShippingCalculatorEnabled = false,
+		showRateAfterTaxName,
+		isShippingCalculatorEnabled,
+		checkoutPageId,
 	} = attributes;
 	return (
 		<Cart attributes={ attributes }>
@@ -34,7 +41,7 @@ export default function CartBlock( { attributes = {} } ) {
 						}
 					/>
 					<ExpressPaymentBlock />
-					<ProceedToCheckoutBlock />
+					<ProceedToCheckoutBlock checkoutPageId={ checkoutPageId } />
 					<AcceptedPaymentMethodsIcons />
 				</TotalsBlock>
 			</FilledCart>
