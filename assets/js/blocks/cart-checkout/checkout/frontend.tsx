7c6c97175e48d1b5b90cfc793309c95b9da161d7
Mike Jolley <mike.jolley@me.com>
2021-10-27 19:39:59 +0100

    Handle cart hydration using createPreloadingMiddleware which removes the need for HOCs

--- assets/js/blocks/cart-checkout/checkout/frontend.tsx
+++ assets/js/blocks/cart-checkout/checkout/frontend.tsx
@@ -9,7 +9,6 @@ import {
 	useValidation,
 } from '@woocommerce/base-context/hooks';
 import { getRegisteredBlockComponents } from '@woocommerce/blocks-registry';
-import { withStoreCartApiHydration } from '@woocommerce/block-hocs';
 import { renderParentBlock } from '@woocommerce/atomic-utils';
 
 /**
@@ -54,7 +53,7 @@ const Wrapper = ( {
 };
 
 renderParentBlock( {
-	Block: withStoreCartApiHydration( Block ),
+	Block,
 	blockName,
 	selector: '.wp-block-woocommerce-checkout',
 	getProps,
