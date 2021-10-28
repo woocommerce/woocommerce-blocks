b88d4c4f4fa710c6740c03568589f3ee50a68a4e
Niels Lange <info@nielslange.de>
2021-10-28 10:46:50 +0700

    Correct textdomain

--- assets/js/payment-method-extensions/payment-methods/stripe/stripe-utils/utils.js
+++ assets/js/payment-method-extensions/payment-methods/stripe/stripe-utils/utils.js
@@ -233,12 +233,12 @@ const getErrorMessageForTypeAndCode = ( type, code = '' ) => {
 		case errorTypes.INVALID_EMAIL:
 			return __(
 				'Invalid email address, please correct and try again.',
-				'woo-gutenberg-product-blocks'
+				'woo-gutenberg-products-blocks'
 			);
 		case isNonFriendlyError( type ):
 			return __(
 				'Unable to process this payment, please try again or use alternative method.',
-				'woo-gutenberg-product-blocks'
+				'woo-gutenberg-products-blocks'
 			);
 		case errorTypes.CARD_ERROR:
 			return getErrorMessageForCode( code );
