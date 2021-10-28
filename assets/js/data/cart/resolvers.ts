de0ec69804a37353119fdc1f7f8cebfaa636eb2e
Mike Jolley <mike.jolley@me.com>
2021-10-28 07:49:14 +0100

    Remove withStoreCartApiHydration and timestamp checking

--- assets/js/data/cart/resolvers.ts
+++ assets/js/data/cart/resolvers.ts
@@ -1,18 +1,14 @@
 /**
  * External dependencies
  */
-import { select, dispatch, apiFetch } from '@wordpress/data-controls';
+import { select, apiFetch } from '@wordpress/data-controls';
 import { CartResponse, Cart } from '@woocommerce/types';
 
 /**
  * Internal dependencies
  */
 import { receiveCart, receiveError } from './actions';
-import {
-	STORE_KEY,
-	CART_API_ERROR,
-	LAST_CART_UPDATE_TIMESTAMP_KEY,
-} from './constants';
+import { STORE_KEY, CART_API_ERROR } from './constants';
 
 /**
  * Resolver for retrieving all cart data.
@@ -27,26 +23,6 @@ export function* getCartData(): Generator< unknown, void, CartResponse > {
 	if ( ! cartData ) {
 		yield receiveError( CART_API_ERROR );
 		return;
-	}
-
-	const lastCartUpdateRaw = window.localStorage.getItem(
-		LAST_CART_UPDATE_TIMESTAMP_KEY
-	);
-
-	if ( lastCartUpdateRaw ) {
-		const lastCartUpdate = parseFloat( lastCartUpdateRaw );
-		const cartGeneratedTimestamp = parseFloat(
-			cartData.generated_timestamp
-		);
-		const isStale =
-			! isNaN( cartGeneratedTimestamp ) &&
-			! isNaN( lastCartUpdate ) &&
-			lastCartUpdate > cartGeneratedTimestamp;
-
-		if ( isStale ) {
-			// Do any invalidation before the collection is received to prevent this query running again.
-			yield dispatch( STORE_KEY, 'invalidateResolutionForStore' );
-		}
 	}
 
 	yield receiveCart( cartData );
