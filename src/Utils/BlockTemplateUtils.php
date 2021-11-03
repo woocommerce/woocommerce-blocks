5acf2b65382b81d63946a2af4db223661e4ab06b
Thomas Roberts <thomas.roberts@automattic.com>
2021-11-03 08:08:57 +0000

    Add Gutenberg utils for processing templates based on a post from the db
    When a template is saved it gets saved to the database, we need to handle processing these WooCommerce templates that have been saved in the db and we need to use the gutenberg utils that are private, this is why they've been copied over.

--- src/Utils/BlockTemplateUtils.php
+++ src/Utils/BlockTemplateUtils.php
@@ -74,6 +74,143 @@ class BlockTemplateUtils {
 		return $template_content;
 	}
 
+
+	/**
+	 * Attempts to add custom template information to the template item.
+	 *
+	 * @param array $template_item Template to add information to (requires 'slug' field).
+	 * @return array Template
+	 */
+	public static function gutenberg_add_template_info( $template_item ) {
+		if ( ! \WP_Theme_JSON_Resolver_Gutenberg::theme_has_support() ) {
+			return $template_item;
+		}
+
+		$theme_data = \WP_Theme_JSON_Resolver_Gutenberg::get_theme_data()->get_custom_templates();
+		if ( isset( $theme_data[ $template_item['slug'] ] ) ) {
+			$template_item['title']     = $theme_data[ $template_item['slug'] ]['title'];
+			$template_item['postTypes'] = $theme_data[ $template_item['slug'] ]['postTypes'];
+		}
+
+		return $template_item;
+	}
+
+	/**
+	 * Retrieves the template file from the theme for a given slug.
+	 *
+	 * @internal
+	 *
+	 * @param string $template_type wp_template or wp_template_part.
+	 * @param string $slug template slug.
+	 *
+	 * @return array|null Template.
+	 */
+	public static function gutenberg_get_template_file( $template_type, $slug ) {
+		$template_base_paths = array(
+			'wp_template'      => 'block-templates',
+			'wp_template_part' => 'block-template-parts',
+		);
+		$themes              = array(
+			get_stylesheet() => get_stylesheet_directory(),
+			get_template()   => get_template_directory(),
+		);
+		foreach ( $themes as $theme_slug => $theme_dir ) {
+			$file_path = $theme_dir . '/' . $template_base_paths[ $template_type ] . '/' . $slug . '.html';
+			if ( file_exists( $file_path ) ) {
+				$new_template_item = array(
+					'slug'  => $slug,
+					'path'  => $file_path,
+					'theme' => $theme_slug,
+					'type'  => $template_type,
+				);
+
+				if ( 'wp_template_part' === $template_type ) {
+					return self::gutenberg_add_template_part_area_info( $new_template_item );
+				}
+
+				if ( 'wp_template' === $template_type ) {
+					return self::gutenberg_add_template_info( $new_template_item );
+				}
+
+				return $new_template_item;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Attempts to add the template part's area information to the input template.
+	 *
+	 * @param array $template_info Template to add information to (requires 'type' and 'slug' fields).
+	 *
+	 * @return array Template.
+	 */
+	public static function gutenberg_add_template_part_area_info( $template_info ) {
+		if ( \WP_Theme_JSON_Resolver_Gutenberg::theme_has_support() ) {
+			$theme_data = \WP_Theme_JSON_Resolver_Gutenberg::get_theme_data()->get_template_parts();
+		}
+
+		if ( isset( $theme_data[ $template_info['slug'] ]['area'] ) ) {
+			$template_info['title'] = $theme_data[ $template_info['slug'] ]['title'];
+			$template_info['area']  = gutenberg_filter_template_part_area( $theme_data[ $template_info['slug'] ]['area'] );
+		} else {
+			$template_info['area'] = WP_TEMPLATE_PART_AREA_UNCATEGORIZED;
+		}
+
+		return $template_info;
+	}
+
+	/**
+	 * Build a unified template object based a post Object.
+	 *
+	 * @param \WP_Post $post Template post.
+	 *
+	 * @return \WP_Block_Template|\WP_Error Template.
+	 */
+	public static function gutenberg_build_template_result_from_post( $post ) {
+		$default_template_types = gutenberg_get_default_template_types();
+		$terms                  = get_the_terms( $post, 'wp_theme' );
+
+		if ( is_wp_error( $terms ) ) {
+			return $terms;
+		}
+
+		if ( ! $terms ) {
+			return new \WP_Error( 'template_missing_theme', __( 'No theme is defined for this template.', 'woo-gutenberg-products-block' ) );
+		}
+
+		$theme          = $terms[0]->name;
+		$has_theme_file = true;
+
+		$template                 = new \WP_Block_Template();
+		$template->wp_id          = $post->ID;
+		$template->id             = $theme . '//' . $post->post_name;
+		$template->theme          = $theme;
+		$template->content        = $post->post_content;
+		$template->slug           = $post->post_name;
+		$template->source         = 'custom';
+		$template->type           = $post->post_type;
+		$template->description    = $post->post_excerpt;
+		$template->title          = $post->post_title;
+		$template->status         = $post->post_status;
+		$template->has_theme_file = $has_theme_file;
+		$template->is_custom      = true;
+
+		if ( 'wp_template' === $post->post_type && isset( $default_template_types[ $template->slug ] ) ) {
+			$template->is_custom = false;
+		}
+
+		if ( 'wp_template_part' === $post->post_type ) {
+			$type_terms = get_the_terms( $post, 'wp_template_part_area' );
+			if ( ! is_wp_error( $type_terms ) && false !== $type_terms ) {
+				$template->area = $type_terms[0]->name;
+			}
+		}
+
+		return $template;
+	}
+
 	/**
 	 * Build a unified template object based on a theme file.
 	 *
@@ -83,7 +220,7 @@ class BlockTemplateUtils {
 	 * @return \WP_Block_Template Template.
 	 */
 	public static function gutenberg_build_template_result_from_file( $template_file, $template_type ) {
-		$template_file = (object) $template_file;
+		$template_file          = (object) $template_file;
 		$default_template_types = function_exists( 'gutenberg_get_default_template_types' ) ? gutenberg_get_default_template_types() : array();
 		// phpcs:ignore WordPress.WP.AlternativeFunctions.file_get_contents_file_get_contents
 		$template_content = file_get_contents( $template_file->path );
