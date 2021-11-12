8024da973d6845e05639ab8582540c558a795817
Niels Lange <info@nielslange.de>
2021-11-12 17:43:13 +0700

    Add additional unit test cases

--- packages/prices/utils/test/price.js
+++ packages/prices/utils/test/price.js
@@ -1,7 +1,68 @@
 /**
  * Internal dependencies
  */
-import { formatPrice, getCurrency } from '../price';
+import {
+	getIntegerValue,
+	getDecimalValue,
+	formatPrice,
+	getCurrency,
+} from '../price';
+
+describe( 'testInteger', () => {
+	test.each`
+		value           | thousandSeparator | minorUnit | expected
+		${ 1 }          | ${ ',' }          | ${ 2 }    | ${ '0' }
+		${ 12 }         | ${ ',' }          | ${ 2 }    | ${ '0' }
+		${ 123 }        | ${ ',' }          | ${ 2 }    | ${ '1' }
+		${ 1000 }       | ${ ',' }          | ${ 2 }    | ${ '10' }
+		${ 1234 }       | ${ ',' }          | ${ 2 }    | ${ '12' }
+		${ 12345 }      | ${ ',' }          | ${ 2 }    | ${ '123' }
+		${ 123456 }     | ${ ',' }          | ${ 2 }    | ${ '1,234' }
+		${ 1234567 }    | ${ ',' }          | ${ 2 }    | ${ '12,345' }
+		${ 12345678 }   | ${ ',' }          | ${ 2 }    | ${ '123,456' }
+		${ 123456789 }  | ${ ',' }          | ${ 2 }    | ${ '1,234,567' }
+		${ 123456789 }  | ${ ',' }          | ${ 3 }    | ${ '123,456' }
+		${ 123456789 }  | ${ ',' }          | ${ 4 }    | ${ '12,345' }
+		${ 123456789 }  | ${ ',' }          | ${ 5 }    | ${ '1,234' }
+		${ 1234567890 } | ${ ',' }          | ${ 0 }    | ${ '1,234,567,890' }
+		${ 1234567890 } | ${ '.' }          | ${ 0 }    | ${ '1.234.567.890' }
+	`(
+		'correctly returns integer value given "$value", "$thousandSeparator" thousandSeparator and "$minorUnit" minorUnit',
+		( { value, thousandSeparator, minorUnit, expected } ) => {
+			const formattedPrice = getIntegerValue(
+				value,
+				thousandSeparator,
+				minorUnit
+			);
+
+			expect( formattedPrice ).toEqual( expected );
+		}
+	);
+} );
+
+describe( 'testDecimal', () => {
+	test.each`
+		value          | minorUnit | expected
+		${ 0 }         | ${ 0 }    | ${ '' }
+		${ 0 }         | ${ 1 }    | ${ '0' }
+		${ 0 }         | ${ 2 }    | ${ '00' }
+		${ 0 }         | ${ 3 }    | ${ '000' }
+		${ 123456789 } | ${ 0 }    | ${ '' }
+		${ 123456789 } | ${ 1 }    | ${ '9' }
+		${ 123456789 } | ${ 2 }    | ${ '89' }
+		${ 123456789 } | ${ 3 }    | ${ '789' }
+		${ 123456789 } | ${ 4 }    | ${ '6789' }
+		${ 123456789 } | ${ 5 }    | ${ '56789' }
+		${ 123456789 } | ${ 6 }    | ${ '456789' }
+	`(
+		'correctly returns integer value given "$value" and "$minorUnit" minorUnit',
+		( { value, minorUnit, expected } ) => {
+			const formattedPrice = getDecimalValue( value, minorUnit );
+
+			expect( formattedPrice ).toEqual( expected );
+		}
+	);
+} );
 
 describe( 'formatPrice', () => {
 	test.each`
