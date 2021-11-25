3214aa0682a1f1e0828237794c2e092d1b43c940
Luigi <gigitux@gmail.com>
2021-11-25 16:50:06 +0100

    add generics

--- assets/js/base/utils/render-frontend.tsx
+++ assets/js/base/utils/render-frontend.tsx
@@ -10,18 +10,33 @@ import BlockErrorBoundary from '@woocommerce/base-components/block-error-boundar
 // the event `wc-blocks_render_blocks_frontend` to render its inner blocks.
 const selectorsToSkipOnLoad = [ '.wp-block-woocommerce-cart' ];
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type BlockType = ( props: any ) => JSX.Element;
+type BlockProps<
+	TProps extends Record< string, unknown >,
+	TAttribute extends Record< string, unknown >
+> = TProps & {
+	attributes: TAttribute;
+};
 
-interface RenderBlockParams {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type BlockType<
+	TProps extends Record< string, unknown >,
+	TAttribute extends Record< string, unknown >
+> = (
+	props: Partial< BlockProps< TProps, TAttribute > >
+) => JSX.Element | null;
+
+interface RenderBlockParams<
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+> {
 	// React component to use as a replacement.
-	Block: BlockType;
+	Block: BlockType< TProps, TAttributes > | null;
 	// Container to replace with the Block component.
 	container: HTMLElement;
 	// Attributes object for the block.
-	attributes?: Record< string, unknown >;
+	attributes: TAttributes;
 	// Props object for the block.
-	props?: Record< string, unknown >;
+	props: Partial< BlockProps< TProps, TAttributes > >;
 	// Props object for the error boundary.
 	errorBoundaryProps?: Record< string, unknown >;
 }
@@ -29,35 +44,39 @@ interface RenderBlockParams {
 /**
  * Renders a block component in a single `container` node.
  */
-export const renderBlock = ( {
+export const renderBlock = <
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+>( {
 	Block,
 	container,
-	attributes = {},
-	props = {},
+	attributes = {} as TAttributes,
+	props = {} as BlockProps< TProps, TAttributes >,
 	errorBoundaryProps = {},
-}: RenderBlockParams ): void => {
+}: RenderBlockParams< TProps, TAttributes > ): void => {
 	render(
 		<BlockErrorBoundary { ...errorBoundaryProps }>
 			<Suspense fallback={ <div className="wc-block-placeholder" /> }>
-				<Block { ...props } attributes={ attributes } />
+				{ Block && <Block { ...props } attributes={ attributes } /> }
 			</Suspense>
 		</BlockErrorBoundary>,
 		container
 	);
 };
 
-interface PropsWithAttributes {
-	attributes?: Record< string, unknown >;
-	[ key: string ]: unknown;
-}
-
-interface RenderBlockInContainersParams {
+interface RenderBlockInContainersParams<
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+> {
 	// React component to use as a replacement.
-	Block: BlockType;
+	Block: BlockType< TProps, TAttributes > | null;
 	// Containers to replace with the Block component.
 	containers: NodeListOf< Element >;
 	// Function to generate the props object for the block.
-	getProps?: ( el: HTMLElement, i: number ) => PropsWithAttributes;
+	getProps?: (
+		el: HTMLElement,
+		i: number
+	) => Partial< BlockProps< TProps, TAttributes > >;
 	// Function to generate the props object for the error boundary.
 	getErrorBoundaryProps?: (
 		el: HTMLElement,
@@ -68,12 +87,15 @@ interface RenderBlockInContainersParams {
 /**
  * Renders a block component in each `containers` node.
  */
-const renderBlockInContainers = ( {
+const renderBlockInContainers = <
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+>( {
 	Block,
 	containers,
-	getProps = () => ( {} ),
+	getProps = () => ( {} as BlockProps< TProps, TAttributes > ),
 	getErrorBoundaryProps = () => ( {} ),
-}: RenderBlockInContainersParams ): void => {
+}: RenderBlockInContainersParams< TProps, TAttributes > ): void => {
 	if ( containers.length === 0 ) {
 		return;
 	}
@@ -81,6 +103,7 @@ const renderBlockInContainers = ( {
 	// Use Array.forEach for IE11 compatibility.
 	Array.prototype.forEach.call( containers, ( el, i ) => {
 		const props = getProps( el, i );
+
 		const errorBoundaryProps = getErrorBoundaryProps( el, i );
 		const attributes = {
 			...el.dataset,
@@ -110,7 +133,10 @@ const isElementInsideWrappers = (
 	);
 };
 
-interface RenderBlockOutsideWrappersParams extends RenderFrontendParams {
+interface RenderBlockOutsideWrappersParams<
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+> extends RenderFrontendParams< TProps, TAttributes > {
 	// All elements matched by the selector which are inside the wrapper will be ignored.
 	wrappers: NodeListOf< Element >;
 }
@@ -119,13 +145,16 @@ interface RenderBlockOutsideWrappersParams extends RenderFrontendParams {
  * Renders the block frontend in the elements matched by the selector which are
  * outside the wrapper elements.
  */
-const renderBlockOutsideWrappers = ( {
+const renderBlockOutsideWrappers = <
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+>( {
 	Block,
 	getProps,
 	getErrorBoundaryProps,
 	selector,
 	wrappers,
-}: RenderBlockOutsideWrappersParams ): void => {
+}: RenderBlockOutsideWrappersParams< TProps, TAttributes > ): void => {
 	const containers = document.body.querySelectorAll( selector );
 	// Filter out blocks inside the wrappers.
 	if ( wrappers.length > 0 ) {
@@ -141,7 +170,10 @@ const renderBlockOutsideWrappers = ( {
 	} );
 };
 
-interface RenderBlockInsideWrapperParams extends RenderFrontendParams {
+interface RenderBlockInsideWrapperParams<
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+> extends RenderFrontendParams< TProps, TAttributes > {
 	// Wrapper element to query the selector inside.
 	wrapper: HTMLElement;
 }
@@ -150,13 +182,16 @@ interface RenderBlockInsideWrapperParams extends RenderFrontendParams {
  * Renders the block frontend in the elements matched by the selector inside the
  * wrapper element.
  */
-const renderBlockInsideWrapper = ( {
+const renderBlockInsideWrapper = <
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+>( {
 	Block,
 	getProps,
 	getErrorBoundaryProps,
 	selector,
 	wrapper,
-}: RenderBlockInsideWrapperParams ): void => {
+}: RenderBlockInsideWrapperParams< TProps, TAttributes > ): void => {
 	const containers = wrapper.querySelectorAll( selector );
 	renderBlockInContainers( {
 		Block,
@@ -166,13 +201,19 @@ const renderBlockInsideWrapper = ( {
 	} );
 };
 
-interface RenderFrontendParams {
+interface RenderFrontendParams<
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+> {
 	// React component to use as a replacement.
-	Block: BlockType;
+	Block: BlockType< TProps, TAttributes > | null;
 	// CSS selector to match the elements to replace.
 	selector: string;
 	// Function to generate the props object for the block.
-	getProps?: ( el: HTMLElement, i: number ) => PropsWithAttributes;
+	getProps?: (
+		el: HTMLElement,
+		i: number
+	) => Partial< BlockProps< TProps, TAttributes > >;
 	// Function to generate the props object for the error boundary.
 	getErrorBoundaryProps?: (
 		el: HTMLElement,
@@ -186,12 +227,25 @@ interface RenderFrontendParams {
  * appropriate event listeners to render the block when the
  * `wc-blocks_render_blocks_frontend` event is triggered.
  */
-export const renderFrontend = ( props: RenderFrontendParams ): void => {
+export const renderFrontend = <
+	TProps extends Record< string, unknown >,
+	TAttributes extends Record< string, unknown >
+>(
+	props:
+		| RenderBlockOutsideWrappersParams< TProps, TAttributes >
+		| RenderBlockInsideWrapperParams< TProps, TAttributes >
+): void => {
 	const wrappersToSkipOnLoad = document.body.querySelectorAll(
 		selectorsToSkipOnLoad.join( ',' )
 	);
+
+	const { Block, getProps, getErrorBoundaryProps, selector } = props;
+
 	renderBlockOutsideWrappers( {
-		...props,
+		Block,
+		getProps,
+		getErrorBoundaryProps,
+		selector,
 		wrappers: wrappersToSkipOnLoad,
 	} );
 	// For each wrapper, add an event listener to render the inner blocks when
