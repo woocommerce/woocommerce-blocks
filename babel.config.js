3331016bc505808997daac544d828d202622d112
Lucio Giannotta <lucio.giannotta@a8c.com>
2021-11-26 20:08:23 +0100

    Migrate Storybook to 6.4.0 and enable auto-detection of controls (#5249)
    Previously our Babel configuration conflicted with the one provided by
    default by Storybook, making us unable to leverage the automatic
    detection of properties from both JS and TS components to create
    controls (formerly known as knobs).
    
    Upgrading to 6.4.0 fixes a bug that makes it easier to work with
    Babel configurations. Also done here:
    
    * Removed a few deprecated addons in favor of the replacement ones
    bundled within Storybook Essentials.
    * Add auto-detection of certain control types depending on the property names
    * Add support for TS stories
    
    More context: https://github.com/storybookjs/storybook/issues/12292

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+	plugins: [
+		[
+			'react-docgen',
+			{ DOC_GEN_COLLECTION_NAME: 'STORYBOOK_REACT_CLASSES' },
+		],
+		[ '@babel/plugin-syntax-jsx' ],
+	],
 	presets: [
 		[
 			'@wordpress/babel-preset-default',
