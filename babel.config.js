59e1573137822d0e417ef0935e58743d9290eb7d
Lucio Giannotta <lucio.giannotta@a8c.com>
2021-11-30 21:01:28 +0100

    Use `react-docgen` only in development environments
    This should reduce production bundle size dramatically. The only
    problem here is that Storybook runs its `build` command with the
    `NODE_ENV` set to `production`. For this reason, we change the
    script to provide `BABEL_ENV` set to `development` to still make sure
    the correct configuration is loaded, while trying to reduce any
    side-effects on changing the whole `NODE_ENV`.

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-	plugins: [
-		[
-			'react-docgen',
-			{ DOC_GEN_COLLECTION_NAME: 'STORYBOOK_REACT_CLASSES' },
-		],
-		[ '@babel/plugin-syntax-jsx' ],
-	],
+	env: {
+		development: {
+			plugins: [
+				[
+					'react-docgen',
+					{ DOC_GEN_COLLECTION_NAME: 'STORYBOOK_REACT_CLASSES' },
+				],
+				[ '@babel/plugin-syntax-jsx' ],
+			],
+		},
+	},
 	presets: [
 		[
 			'@wordpress/babel-preset-default',
