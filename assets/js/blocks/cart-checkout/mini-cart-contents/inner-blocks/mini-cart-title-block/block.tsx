3894b2edad63225dbc15bd5d9ce6f7b762be5979
Tung Du <dinhtungdu@gmail.com>
2021-12-15 12:57:10 +0700

    front end style

--- assets/js/blocks/cart-checkout/mini-cart-contents/inner-blocks/mini-cart-title-block/block.tsx
+++ assets/js/blocks/cart-checkout/mini-cart-contents/inner-blocks/mini-cart-title-block/block.tsx
@@ -4,10 +4,17 @@
 import { sprintf, _n, __ } from '@wordpress/i18n';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
 
+/**
+ * Internal dependencies
+ */
+
 const Block = (): JSX.Element => {
 	const { cartItemsCount, cartIsLoading } = useStoreCart();
+	/**
+	 * @todo Allow the user to edit the title of the mini cart.
+	 */
 	return (
-		<h2 className="mini-cart-title-block">
+		<h2 className="wc-block-mini-cart__title">
 			{ cartIsLoading
 				? __( 'Your cart', 'woo-gutenberg-products-block' )
 				: sprintf(
