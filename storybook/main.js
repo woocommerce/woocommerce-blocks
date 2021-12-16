d27140b156fccda6bdc6e07021c3b98c50ef7f3b
Lucio Giannotta <lucio.giannotta@a8c.com>
2021-12-17 00:26:30 +0100

    Remove Storybook knobs
    They are now fully replaced by controls

--- storybook/main.js
+++ storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
 	addons: [
 		'@storybook/addon-essentials',
 		'@storybook/addon-a11y',
-		'@storybook/addon-knobs',
 		'@storybook/addon-links',
 		'storybook-addon-react-docgen',
 	],
