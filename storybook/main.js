cfa0bedcbb63e21474b143b9154c5eccaee793fb
Lucio Giannotta <lucio.giannotta@a8c.com>
2021-12-17 18:42:02 +0100

    Convert stories from checkout packages and include them in Storybook

--- storybook/main.js
+++ storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
 	stories: [
 		// WooCommerce Blocks stuff (anywhere in repo!)
 		'../assets/js/**/stories/*.@(js|jsx|ts|tsx)',
+		'../packages/**/stories/*.@(js|jsx|ts|tsx)',
 	],
 	addons: [
 		'@storybook/addon-essentials',
