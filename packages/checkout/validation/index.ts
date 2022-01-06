29f3ee3be627b99a52be022b6a6308636ca11969
Thomas Roberts <thomas.roberts@automattic.com>
2022-01-06 15:04:31 +0000

    Move validation store to checkout package

--- packages/checkout/validation/index.ts
+++ packages/checkout/validation/index.ts
@@ -1,12 +1,32 @@
 /**
  * External dependencies
  */
-import { select } from '@wordpress/data';
+import { registerStore, select } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
-import { VALIDATION_STORE_KEY } from '../../../assets/js/data/validation';
-window.package_key = VALIDATION_STORE_KEY;
+import reducer, { State } from './reducers';
+import { STORE_KEY } from './constants';
+import * as actions from './actions';
+import * as selectors from './selectors';
 
-export const { getValidationError } = select( VALIDATION_STORE_KEY );
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+registerStore< State >( STORE_KEY, {
+	reducer,
+	actions,
+	selectors,
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	// controls: { ...dataControls, ...sharedControls, ...controls } as any,
+} );
+
+export const VALIDATION_STORE_KEY = STORE_KEY;
+
+export const exposedSelectors = {
+	getValidationError( propertyName: string ) {
+		return select( VALIDATION_STORE_KEY ).getValidationError(
+			propertyName
+		);
+	},
+};
