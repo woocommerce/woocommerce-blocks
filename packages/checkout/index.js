cb72301ce94d4fe6b7f3dc5b9ba16e0193aff771
Thomas Roberts <thomas.roberts@automattic.com>
2022-01-06 15:19:28 +0000

    Only export "exposedSelectors" from validation

--- packages/checkout/index.js
+++ packages/checkout/index.js
@@ -1,6 +1,6 @@
 export * from './components';
 export * from './utils';
-export * from './validation';
+export { exposedSelectors } from './validation';
 export * from './slot';
 export * from './filter-registry';
 export * from './blocks-registry';
