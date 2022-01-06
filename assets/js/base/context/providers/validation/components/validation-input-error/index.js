f76d4da2d37c63cca6d24d8ca51da9acfcca2d50
Thomas Roberts <thomas.roberts@automattic.com>
2022-01-06 09:07:53 +0000

    Use the validation wp-data store for showing error messages

--- assets/js/base/context/providers/validation/components/validation-input-error/index.js
+++ assets/js/base/context/providers/validation/components/validation-input-error/index.js
@@ -2,22 +2,30 @@
  * External dependencies
  */
 import PropTypes from 'prop-types';
+import { useSelect } from '@wordpress/data';
+import { VALIDATION_STORE_KEY } from '@woocommerce/block-data';
 
 /**
  * Internal dependencies
  */
-import { useValidationContext } from '../../context';
 import './style.scss';
+//import { useValidationContext } from '../../context';
 
 export const ValidationInputError = ( {
 	errorMessage = '',
 	propertyName = '',
 	elementId = '',
 } ) => {
-	const { getValidationError, getValidationErrorId } = useValidationContext();
+	//const { getValidationError, getValidationErrorId } = useValidationContext();
+	/* Note that I've short-circuited the function below to avoid needing the useValidationContext */
+	const getValidationErrorId = ( _elementId ) =>
+		`error- ${ _elementId }-${ propertyName }`;
+	const validationError = useSelect( ( select ) =>
+		select( VALIDATION_STORE_KEY ).getValidationError( propertyName )
+	);
 
 	if ( ! errorMessage || typeof errorMessage !== 'string' ) {
-		const error = getValidationError( propertyName ) || {};
+		const error = validationError || {};
 		if ( error.message && ! error.hidden ) {
 			errorMessage = error.message;
 		} else {
