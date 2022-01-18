908f34d248d788025553ba742c99b9b2af74a950
Michael P. Pfeiffer <michael@cssconf.eu>
2022-01-18 07:45:38 +0100

    Update version to 6.7.2-dev

--- src/Package.php
+++ src/Package.php
@@ -106,7 +106,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '6.7.1';
+					$version = '6.7.2-dev';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
