b5db0cf934f074ef45a943f71c1cc12ce743c96f
Mike Jolley <mike.jolley@me.com>
2022-01-19 14:43:41 +0000

    add to cart button use button icon

--- assets/js/atomic/blocks/product-elements/button/constants.js
+++ assets/js/atomic/blocks/product-elements/button/constants.js
@@ -2,15 +2,14 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { cart } from '@woocommerce/icons';
-import { Icon } from '@wordpress/icons';
+import { Icon, button } from '@wordpress/icons';
 
 export const BLOCK_TITLE = __(
 	'Add to Cart Button',
 	'woo-gutenberg-products-block'
 );
 export const BLOCK_ICON = (
-	<Icon icon={ cart } className="wc-block-editor-components-block-icon" />
+	<Icon icon={ button } className="wc-block-editor-components-block-icon" />
 );
 export const BLOCK_DESCRIPTION = __(
 	'Display a call to action button which either adds the product to the cart, or links to the product page.',
