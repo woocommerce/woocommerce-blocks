6dddcf00ed7fbbfccdcf2b89f0107baa971ff3f6
Thomas Roberts <thomas.roberts@automattic.com>
2022-01-26 20:06:06 +0000

    Add textContentMatcherAccrossSiblings helper
    This is needed to match text when it's spread over a few elements

--- assets/js/blocks/cart-checkout/checkout/inner-blocks/checkout-order-summary-block/test/block.js
+++ assets/js/blocks/cart-checkout/checkout/inner-blocks/checkout-order-summary-block/test/block.js
@@ -29,6 +29,43 @@ const textContentMatcher = ( text ) => {
 	};
 };
 
+/**
+ * This will check if the text is present an the container, it can be within
+ * multiple elements, for example:
+ * <div>
+ *     <span>Text</span>
+ *     <span>is</span>
+ *     <span>present</span>
+ * </div>
+ *
+ * @param {string} text The text to find
+ * @return {function(*, ?)} the matcher function for RTL to use.
+ */
+const textContentMatcherAcrossSiblings = ( text ) => {
+	return function ( _content, node ) {
+		/*
+		If the element in question is not the first child, then skip, as we
+		will have already run this check for its siblings (when we ran it on the
+		first child).
+		*/
+		const siblings =
+			node?.parentElement?.children[ 0 ] === node
+				? node?.parentElement?.children
+				: [];
+		let siblingText = '';
+
+		// Get the text of all siblings and put it into a single string.
+		if ( siblings?.length > 0 ) {
+			siblingText = Array.from( siblings )
+				.map( ( child ) => child.textContent )
+				.filter( Boolean )
+				.join( ' ' )
+				.trim();
+		}
+		return siblingText !== '' && siblingText === text;
+	};
+};
+
 jest.mock( '@woocommerce/settings', () => {
 	const originalModule = jest.requireActual( '@woocommerce/settings' );
 
