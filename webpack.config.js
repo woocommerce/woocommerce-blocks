woocommerce/woocommerce-blocks

commit 6cf9c4cb19821036765f56dc77aec063b9a94019

Implement `@wordpress/scripts` analyze script (#5589)
* Add analyze-bundles command, replacing explore

* Replace source map explorer from webpack in favour of BundleAnalyzerPlugin

* Update deps

* update locks

webpack.config.js
@@ -2,7 +2,7 @@
  * Internal dependencies
  */
 const FallbackModuleDirectoryPlugin = require( './bin/fallback-module-directory-webpack-plugin' );
-const { NODE_ENV, FORCE_MAP, getAlias } = require( './bin/webpack-helpers.js' );
+const { NODE_ENV, getAlias } = require( './bin/webpack-helpers.js' );
 const {
 	getCoreConfig,
 	getMainConfig,
@@ -30,7 +30,7 @@ const sharedConfig = {
 	watchOptions: {
 		ignored: /node_modules/,
 	},
-	devtool: NODE_ENV === 'development' || FORCE_MAP ? 'source-map' : false,
+	devtool: NODE_ENV === 'development',
 };
 
 // Core config for shared libraries.

bin/webpack-configs.js
@@ -11,6 +11,7 @@ const WebpackRTLPlugin = require( 'webpack-rtl-plugin' );
 const TerserPlugin = require( 'terser-webpack-plugin' );
 const CreateFileWebpack = require( 'create-file-webpack' );
 const CircularDependencyPlugin = require( 'circular-dependency-plugin' );
+const { BundleAnalyzerPlugin } = require( 'webpack-bundle-analyzer' );
 
 /**
  * Internal dependencies
@@ -18,7 +19,6 @@ const CircularDependencyPlugin = require( 'circular-dependency-plugin' );
 const { getEntryConfig } = require( './webpack-entries' );
 const {
 	NODE_ENV,
-	FORCE_MAP,
 	CHECK_CIRCULAR_DEPS,
 	requestToExternal,
 	requestToHandle,
@@ -39,6 +39,9 @@ const sharedPlugins = [
 				failOnError: 'warn',
 		  } )
 		: false,
+	// The WP_BUNDLE_ANALYZER global variable enables a utility that represents bundle
+	// content as a convenient interactive zoomable treemap.
+	process.env.WP_BUNDLE_ANALYZER && new BundleAnalyzerPlugin(),
 	new DependencyExtractionWebpackPlugin( {
 		injectPolyfill: true,
 		requestToExternal,
@@ -112,6 +115,9 @@ woocommerce_blocks_env = ${ NODE_ENV }
 			} ),
 		],
 		optimization: {
+			// Only concatenate modules in production, when not analyzing bundles.
+			concatenateModules:
+				isProduction && ! process.env.WP_BUNDLE_ANALYZER,
 			splitChunks: {
 				automaticNameDelimiter: '--',
 			},
@@ -119,7 +125,6 @@ woocommerce_blocks_env = ${ NODE_ENV }
 				new TerserPlugin( {
 					cache: true,
 					parallel: true,
-					sourceMap: !! FORCE_MAP || ! isProduction,
 					terserOptions: {
 						output: {
 							comments: /translators:/i,
@@ -210,6 +215,8 @@ const getMainConfig = ( options = {} ) => {
 			],
 		},
 		optimization: {
+			concatenateModules:
+				isProduction && ! process.env.WP_BUNDLE_ANALYZER,
 			splitChunks: {
 				minSize: 0,
 				automaticNameDelimiter: '--',
@@ -226,7 +233,6 @@ const getMainConfig = ( options = {} ) => {
 				new TerserPlugin( {
 					cache: true,
 					parallel: true,
-					sourceMap: !! FORCE_MAP || ! isProduction,
 					terserOptions: {
 						output: {
 							comments: /translators:/i,
@@ -333,6 +339,8 @@ const getFrontConfig = ( options = {} ) => {
 			],
 		},
 		optimization: {
+			concatenateModules:
+				isProduction && ! process.env.WP_BUNDLE_ANALYZER,
 			splitChunks: {
 				automaticNameDelimiter: '--',
 			},
@@ -340,7 +348,6 @@ const getFrontConfig = ( options = {} ) => {
 				new TerserPlugin( {
 					cache: true,
 					parallel: true,
-					sourceMap: !! FORCE_MAP || ! isProduction,
 					terserOptions: {
 						output: {
 							comments: /translators:/i,
@@ -435,6 +442,8 @@ const getPaymentsConfig = ( options = {} ) => {
 			],
 		},
 		optimization: {
+			concatenateModules:
+				isProduction && ! process.env.WP_BUNDLE_ANALYZER,
 			splitChunks: {
 				automaticNameDelimiter: '--',
 			},
@@ -442,7 +451,6 @@ const getPaymentsConfig = ( options = {} ) => {
 				new TerserPlugin( {
 					cache: true,
 					parallel: true,
-					sourceMap: !! FORCE_MAP || ! isProduction,
 					terserOptions: {
 						output: {
 							comments: /translators:/i,
@@ -531,6 +539,8 @@ const getExtensionsConfig = ( options = {} ) => {
 			],
 		},
 		optimization: {
+			concatenateModules:
+				isProduction && ! process.env.WP_BUNDLE_ANALYZER,
 			splitChunks: {
 				automaticNameDelimiter: '--',
 			},
@@ -538,7 +548,6 @@ const getExtensionsConfig = ( options = {} ) => {
 				new TerserPlugin( {
 					cache: true,
 					parallel: true,
-					sourceMap: !! FORCE_MAP || ! isProduction,
 					terserOptions: {
 						output: {
 							comments: /translators:/i,

bin/webpack-helpers.js
@@ -5,7 +5,6 @@
 const path = require( 'path' );
 const chalk = require( 'chalk' );
 const NODE_ENV = process.env.NODE_ENV || 'development';
-const FORCE_MAP = process.env.FORCE_MAP || false;
 const CHECK_CIRCULAR_DEPS = process.env.CHECK_CIRCULAR_DEPS || false;
 
 // If a package is not available, or missing functionality, in an old but __supported__ version of WordPress, it should be listed here.
@@ -139,7 +138,6 @@ const getProgressBarPluginConfig = ( name, fileSuffix ) => {
 
 module.exports = {
 	NODE_ENV,
-	FORCE_MAP,
 	CHECK_CIRCULAR_DEPS,
 	getAlias,
 	findModuleMatch,

commit 75796010c54865a01313800d9f83ee08b1f39d97

Fix devtool config broken in #5589

webpack.config.js
@@ -30,7 +30,7 @@ const sharedConfig = {
 	watchOptions: {
 		ignored: /node_modules/,
 	},
-	devtool: NODE_ENV === 'development',
+	devtool: NODE_ENV === 'development' ? 'source-map' : false,
 };
 
 // Core config for shared libraries.