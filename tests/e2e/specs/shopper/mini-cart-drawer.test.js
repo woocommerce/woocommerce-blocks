0a6909f7f08fe916f118d5ecdbc7cc0bdf5fa48b
Tung Du <dinhtungdu@gmail.com>
2022-02-18 21:56:57 +0700

    e2e test open and close the drawer

---
+++ tests/e2e/specs/shopper/mini-cart-drawer.test.js
@@ -0,0 +1,78 @@
+/**
+ * Internal dependencies
+ */
+import { getBlockPagePermalink } from '../../../utils';
+
+const block = {
+	name: 'Mini Cart Block',
+};
+
+if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 3 )
+	// eslint-disable-next-line jest/no-focused-tests
+	test.only( `skipping ${ block.name } tests`, () => {} );
+
+describe( 'Shopper → Mini Cart → Can open/close the drawer', () => {
+	it( 'The drawer opens when shopper clicks on the mini cart icon', async () => {
+		await page.goto( await getBlockPagePermalink( block.name ), {
+			waitUntil: 'networkidle0',
+		} );
+		await expect( page ).toMatchElement( 'h1', { text: block.name } );
+
+		await expect( page ).toClick( '.wc-block-mini-cart__button' );
+
+		await expect( page ).toMatchElement( '.wc-block-mini-cart__drawer', {
+			text: 'Start shopping',
+			visible: true,
+		} );
+	} );
+
+	it( 'The drawer closes when shopper clicks on the drawer close button', async () => {
+		await page.goto( await getBlockPagePermalink( block.name ), {
+			waitUntil: 'networkidle0',
+		} );
+		await expect( page ).toMatchElement( 'h1', { text: block.name } );
+
+		await expect( page ).toClick( '.wc-block-mini-cart__button' );
+
+		await expect( page ).toMatchElement( '.wc-block-mini-cart__drawer', {
+			text: 'Start shopping',
+			visible: true,
+		} );
+
+		await expect( page ).toClick(
+			'.wc-block-mini-cart__drawer .components-modal__header button'
+		);
+
+		await expect( page ).not.toMatchElement(
+			'.wc-block-mini-cart__drawer',
+			{
+				text: 'Start shopping',
+				visible: true,
+			}
+		);
+	} );
+
+	it( 'The drawer closes when shopper clicks outside the drawer', async () => {
+		await page.goto( await getBlockPagePermalink( block.name ), {
+			waitUntil: 'networkidle0',
+		} );
+		await expect( page ).toMatchElement( 'h1', { text: block.name } );
+
+		await expect( page ).toClick( '.wc-block-mini-cart__button' );
+
+		await expect( page ).toMatchElement( '.wc-block-mini-cart__drawer', {
+			text: 'Start shopping',
+			visible: true,
+		} );
+
+		await page.mouse.click( 100, 100 );
+
+		await expect( page ).not.toMatchElement(
+			'.wc-block-mini-cart__drawer',
+			{
+				text: 'Start shopping',
+				visible: true,
+			}
+		);
+	} );
+} );
