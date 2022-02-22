21082ada9df959e3298d9cb6b41c5b17fab9e2dc
Tung Du <dinhtungdu@gmail.com>
2022-02-22 15:40:56 +0700

    switch to use puppeteer click method

--- tests/e2e/specs/shopper/mini-cart-drawer.test.js
+++ tests/e2e/specs/shopper/mini-cart-drawer.test.js
@@ -15,11 +15,9 @@ describe( 'Shopper → Mini Cart → Can open/close the drawer', () => {
 	it( 'The drawer opens when shopper clicks on the mini cart icon', async () => {
 		await shopper.goToBlockPage( block.name );
 
-		await page.hover( '.wc-block-mini-cart__button' );
+		await page.click( '.wc-block-mini-cart__button' );
 
-		await expect( page ).toClick( '.wc-block-mini-cart__button' );
-
-		await page.waitFor( 500 ); // Mini Cart Drawer takes 300ms to open.
+		await page.waitForTimeout( 500 ); // Mini Cart Drawer takes 300ms to open.
 
 		await expect( page ).toMatchElement( '.wc-block-mini-cart__drawer', {
 			text: 'Start shopping',
@@ -29,11 +27,9 @@ describe( 'Shopper → Mini Cart → Can open/close the drawer', () => {
 	it( 'The drawer closes when shopper clicks on the drawer close button', async () => {
 		await shopper.goToBlockPage( block.name );
 
-		await page.hover( '.wc-block-mini-cart__button' );
+		await page.click( '.wc-block-mini-cart__button' );
 
-		await expect( page ).toClick( '.wc-block-mini-cart__button' );
-
-		await page.waitFor( 500 ); // Mini Cart Drawer takes 300ms to open.
+		await page.waitForTimeout( 500 ); // Mini Cart Drawer takes 300ms to open.
 
 		await expect( page ).toMatchElement( '.wc-block-mini-cart__drawer', {
 			text: 'Start shopping',
@@ -54,17 +50,15 @@ describe( 'Shopper → Mini Cart → Can open/close the drawer', () => {
 	it( 'The drawer closes when shopper clicks outside the drawer', async () => {
 		await shopper.goToBlockPage( block.name );
 
-		await page.hover( '.wc-block-mini-cart__button' );
+		await page.click( '.wc-block-mini-cart__button' );
 
-		await expect( page ).toClick( '.wc-block-mini-cart__button' );
-
-		await page.waitFor( 500 ); // Mini Cart Drawer takes 300ms to open.
+		await page.waitForTimeout( 500 ); // Mini Cart Drawer takes 300ms to open.
 
 		await expect( page ).toMatchElement( '.wc-block-mini-cart__drawer', {
 			text: 'Start shopping',
 		} );
 
-		await page.mouse.click( 100, 100 );
+		await page.mouse.click( 50, 200 );
 
 		await expect( page ).not.toMatchElement(
 			'.wc-block-mini-cart__drawer',
