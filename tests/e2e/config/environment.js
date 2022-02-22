be037e43daea8991fe5a0d04fc1da571af34ff81
Thomas Roberts <thomas.roberts@automattic.com>
2022-02-22 20:21:30 +0000

    Console log for testing

--- tests/e2e/config/environment.js
+++ tests/e2e/config/environment.js
@@ -6,7 +6,10 @@ const { addAttach } = require( 'jest-html-reporters/helper' );
 
 class E2EEnvironment extends PuppeteerEnvironment {
 	async handleTestEvent( event ) {
-		if ( event.name === 'test_fn_failure' ) {
+		if (
+			event.name === 'test_fn_failure' ||
+			event.asyncError !== undefined
+		) {
 			const attach = await this.global.page.screenshot();
 			console.log( 'taking screenshot' );
 			await addAttach( {
