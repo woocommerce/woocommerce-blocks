a7165f7fa1586e406d92517c8d1b3277817681a1
Lucio Giannotta <lucio.giannotta@a8c.com>
2022-02-22 22:28:45 +0100

    Add `useTheme` hook

--- tests/e2e/page-utils.js
+++ tests/e2e/page-utils.js
@@ -21,6 +21,21 @@ export async function elementExists( selector, root = page ) {
 }
 
 /**
+ * Gets data from an HTML element
+ *
+ * @param {string} selector The selector for the desired element
+ * @param {string} key The key in the element data to look for
+ * @param {Page | ElementHandle} root The root from which to search for the selector
+ *
+ * @return {Promise<string | undefined>} The data of that element if it exists
+ */
+export async function getElementData( selector, key, root = page ) {
+	return root.$eval( selector, ( $element ) => {
+		return $element.dataset[ key ];
+	} );
+}
+
+/**
  * Gets the text value of an element
  *
  * If the element is an `input` it will get the `value`, otherwise,
