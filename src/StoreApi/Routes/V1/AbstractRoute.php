5982e4dcdc640544e2a1610c42a47e85ffcddcc0
Mike Jolley <mike.jolley@me.com>
2022-02-22 17:59:19 +0000

    Correct namespace

--- src/StoreApi/Routes/V1/AbstractRoute.php
+++ src/StoreApi/Routes/V1/AbstractRoute.php
@@ -5,7 +5,7 @@ use Automattic\WooCommerce\Blocks\StoreApi\SchemaController;
 use Automattic\WooCommerce\Blocks\StoreApi\Routes\RouteInterface;
 use Automattic\WooCommerce\Blocks\StoreApi\Exceptions\RouteException;
 use Automattic\WooCommerce\Blocks\StoreApi\Exceptions\InvalidStockLevelsInCartException;
-use Automattic\WooCommerce\Blocks\StoreApi\Schemas\AbstractSchema;
+use Automattic\WooCommerce\Blocks\StoreApi\Schemas\v1\AbstractSchema;
 use WP_Error;
 
 /**
