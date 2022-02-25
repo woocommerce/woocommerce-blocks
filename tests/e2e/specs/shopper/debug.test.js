be19ad96ffbcc728ab4e667c7dddb6dd43be3d64
Alex Florisca <alex.florisca@automattic.com>
2022-02-25 12:21:08 +0000

    debug taxes

--- tests/e2e/specs/shopper/debug.test.js
+++ tests/e2e/specs/shopper/debug.test.js
@@ -1,4 +1,6 @@
-import { sleep } from '../../utils';
+/**
+ * Internal dependencies
+ */
 import { shopper } from '../../../utils';
 import { Taxes, Products } from '../../fixtures/fixture-data';
 import {
@@ -13,27 +15,61 @@ const productWooSingle1 = Products().find(
 	( prod ) => prod.name === 'Woo Single #1'
 );
 
-describe( 'debug', () => {
-	it( 'debuging tests', async () => {
-		await showTaxes( false );
-		await shopper.goToShop();
-		await shopper.searchForProduct( productWooSingle1.name );
-		await shopper.addToCart();
-		await shopper.goToCartBlock();
+describe( 'Tax', () => {
+	beforeEach( async () => {
+		await shopper.emptyCart();
+	} );
 
-		const cartTaxes = await getTaxesFromCurrentPage();
-		expect( cartTaxes ).toEqual( [] );
+	describe( '"Enable tax rate calculations" is unchecked in WC settings -> general', () => {
+		it( 'Tax is not displayed', async () => {
+			await showTaxes( false );
+			await shopper.goToShop();
+			await shopper.searchForProduct( productWooSingle1.name );
+			await shopper.addToCart();
+			await shopper.goToCartBlock();
 
-		await shopper.goToCheckoutBlock();
-		const checkoutTaxes = await getTaxesFromCurrentPage();
-		expect( checkoutTaxes ).toEqual( [] );
+			const cartTaxes = await getTaxesFromCurrentPage();
+			expect( cartTaxes ).toEqual( [] );
 
-		await shopper.fillInCheckoutWithTestData();
-		await shopper.placeOrder();
-		await page.waitForSelector( 'h1.entry-title' );
-		const orderSummaryTaxes = await getTaxesFromOrderSummaryPage(
-			taxRates.filter( ( taxRate ) => taxRate.country === 'US' )
-		);
-		expect( orderSummaryTaxes ).toEqual( [] );
+			await shopper.goToCheckoutBlock();
+			const checkoutTaxes = await getTaxesFromCurrentPage();
+			expect( checkoutTaxes ).toEqual( [] );
+
+			await shopper.fillInCheckoutWithTestData();
+			await shopper.placeOrder();
+			await page.waitForSelector( 'h1.entry-title' );
+			const orderSummaryTaxes = await getTaxesFromOrderSummaryPage(
+				taxRates.filter( ( taxRate ) => taxRate.country === 'US' )
+			);
+			expect( orderSummaryTaxes ).toEqual( [] );
+		} );
+	} );
+
+	describe( '"Enable tax rate calculations" is checked in WC settings -> general', () => {
+		it( 'Tax is displayed correctly on Cart & Checkout ', async () => {
+			await showTaxes( true );
+			await shopper.goToShop();
+			await shopper.searchForProduct( productWooSingle1.name );
+			await shopper.addToCart();
+			await shopper.goToCartBlock();
+
+			const expectedTaxes = getExpectedTaxes( taxRates, 'US', [
+				productWooSingle1,
+			] );
+			const cartTaxes = await getTaxesFromCurrentPage();
+			expect( cartTaxes.sort() ).toEqual( expectedTaxes.sort() );
+
+			await shopper.goToCheckoutBlock();
+			const checkoutTaxes = await getTaxesFromCurrentPage();
+			expect( checkoutTaxes.sort() ).toEqual( expectedTaxes.sort() );
+
+			await shopper.fillInCheckoutWithTestData();
+			await shopper.placeOrder();
+			await page.waitForSelector( 'h1.entry-title' );
+			const orderSummaryTaxes = await getTaxesFromOrderSummaryPage(
+				taxRates.filter( ( taxRate ) => taxRate.country === 'US' )
+			);
+			expect( orderSummaryTaxes.sort() ).toEqual( expectedTaxes.sort() );
+		} );
 	} );
 } );
