6f2d1f9126491004992a8542a469d0f233d11aee
Alex Florisca <alex.florisca@automattic.com>
2022-02-25 09:51:25 +0000

    use waitforSelector

--- tests/utils/shopper.js
+++ tests/utils/shopper.js
@@ -62,10 +62,11 @@ export const shopper = {
 		We can delete this function once the PR is merged
 		*/
 	searchForProduct: async ( productname ) => {
-		const searchFieldSelector = 'input.wp-block-search__input';
-		await expect( page ).toMatchElement( searchFieldSelector, {
-			timeout: 5000,
-		} );
+		const searchFieldSelector = '.wp-block-search__input';
+		// await expect( page ).toMatchElement( searchFieldSelector, {
+		// 	timeout: 5000,
+		// } );
+		await page.waitForSelector( searchFieldSelector, { timeout: 5000 } );
 		await expect( page ).toFill( searchFieldSelector, productname );
 		await expect( page ).toClick( '.wp-block-search__button' );
 		// Single search results may go directly to product page
