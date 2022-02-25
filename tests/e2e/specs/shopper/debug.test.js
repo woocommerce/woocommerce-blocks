faabfd63564133d106d549fad69f2e976ab50bce
Alex Florisca <alex.florisca@automattic.com>
2022-02-25 12:00:18 +0000

    Run debug test only

--- tests/e2e/specs/shopper/debug.test.js
+++ tests/e2e/specs/shopper/debug.test.js
@@ -1,7 +1,11 @@
 import { sleep } from '../../utils';
+import { showTaxes } from '../../../utils/taxes';
+import { shopper } from '../../../utils/shopper';
 
 describe( 'debug', () => {
 	it( 'debuging tests', async () => {
+		await showTaxes( false );
+		await shopper.goToShop();
 		await sleep( 60 );
 	} );
 } );
