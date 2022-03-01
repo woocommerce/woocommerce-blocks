aa2011e5541b6a0da723e9fcb33b7afcc718cb77
Mike Jolley <mike.jolley@me.com>
2022-03-01 14:45:06 +0000

    Rate limit helper

---
+++ src/StoreApi/Utilities/RateLimits.php
@@ -0,0 +1,112 @@
+<?php
+namespace Automattic\WooCommerce\Blocks\StoreApi\Utilities;
+
+use WC_Rate_Limiter;
+use WC_Cache_Helper;
+
+class RateLimits extends WC_Rate_Limiter {
+	/**
+	 * Gets a cache prefix.
+	 *
+	 * @param string $action_id Identifier of the action.
+	 * @return string
+	 */
+	protected static function get_cache_key( $action_id ) {
+		return WC_Cache_Helper::get_cache_prefix( 'store_api_rate_limit' . $action_id );
+	}
+
+	/**
+	 * Get current rate limit row from DB and normalize types. This query is not cached.
+	 *
+	 * @param string $action_id Identifier of the action.
+	 * @return object Object containing reset and remaining.
+	 */
+	protected static function get_rate_limit_row( $action_id ) {
+		global $wpdb;
+
+		$row = $wpdb->get_row(
+			$wpdb->prepare(
+				"
+					SELECT rate_limit_expiry as reset, rate_limit_remaining as remaining
+					FROM {$wpdb->prefix}wc_rate_limits
+					WHERE rate_limit_key = %s
+				",
+				$action_id
+			),
+			'OBJECT'
+		);
+
+		return (object) [
+			'reset'     => (int) $row->reset,
+			'remaining' => (int) $row->remaining,
+		];
+	}
+
+	/**
+	 * Returns true if the rate limit has been exceeded within the current rate period.
+	 *
+	 * @param string $action_id Identifier of the action.
+	 * @return bool
+	 */
+	public static function is_rate_limit_exceeded( $action_id ) {
+		global $wpdb;
+
+		$current_limit = self::get_cached( $action_id );
+
+		if ( false === $current_limit ) {
+			$current_limit = self::get_rate_limit_row( $action_id );
+			self::set_cache( $action_id, $current_limit );
+		}
+
+		// No record of action running, so action is allowed to run.
+		if ( null === $current_limit ) {
+			return false;
+		}
+
+		// Before the next run is allowed, retry forbidden.
+		if ( time() <= $current_limit->reset && $current_limit->remaining <= 0 ) {
+			return true;
+		}
+
+		// After the next run is allowed, retry allowed.
+		return false;
+	}
+
+	/**
+	 * Sets the rate limit delay in seconds for action with identifier $id.
+	 *
+	 * @param string $action_id Identifier of the action.
+	 * @param int    $reset Reset delay in seconds.
+	 * @param int    $limit Number of requests allowed before current rate limit is reset.
+	 * @return object Current rate limits.
+	 */
+	public static function update_rate_limit( $action_id, $reset, $limit = 1 ) {
+		global $wpdb;
+
+		$rate_limit_expiry = time() + $reset;
+
+		$wpdb->query(
+			$wpdb->prepare(
+				"INSERT INTO {$wpdb->prefix}wc_rate_limits
+					(`rate_limit_key`, `rate_limit_expiry`, `rate_limit_remaining`)
+				VALUES
+					(%s, %d, %d)
+				ON DUPLICATE KEY UPDATE
+					`rate_limit_remaining` = IF(`rate_limit_expiry` < %d, VALUES(`rate_limit_remaining`), GREATEST(`rate_limit_remaining` - 1, 0)),
+					`rate_limit_expiry` = IF(`rate_limit_expiry` < %d, VALUES(`rate_limit_expiry`), `rate_limit_expiry`);
+				",
+				$action_id,
+				$rate_limit_expiry,
+				$limit - 1,
+				time(),
+				time()
+			)
+		);
+
+		$current_limit = self::get_rate_limit_row( $action_id );
+
+		self::set_cache( $action_id, $current_limit );
+
+		return $current_limit;
+	}
+}
