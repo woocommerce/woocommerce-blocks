0db0e4f277809087119042f982ce7b0248f91a5f
“Chris <xristos3490@gmail.com>
2022-03-01 13:53:02 +0200

    Add docs to tests to bypass precommit scripts

--- tests/php/StoreApi/Utilities/NoticeHandler.php
+++ tests/php/StoreApi/Utilities/NoticeHandler.php
@@ -15,7 +15,17 @@ use Yoast\PHPUnitPolyfills\TestCases\TestCase;
 class NoticeHandlerTests extends TestCase {
 
 	/**
-	 * Tests for NoticeHandler.
+	 * Test convert_notices_to_exceptions.
+	 */
+	public function test_convert_notices_to_exceptions() {
+		$this->expectException( RouteException::class );
+		$this->expectExceptionMessage( 'This is an error message with Some HTML in it.' );
+		wc_add_notice( '<strong>This is an error message with <a href="#">Some HTML in it</a>.', 'error' );
+		$errors = NoticeHandler::convert_notices_to_exceptions( 'test_error' );
+	}
+
+	/**
+	 * Test convert_notices_to_wp_errors.
 	 */
 	public function test_convert_notices_to_wp_errors() {
 		wc_add_notice( '<strong>This is an error message with <a href="#">Some HTML in it</a>.', 'error' );
