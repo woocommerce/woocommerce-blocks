f531bfd2cd58e4c1f2dc80eddfefe8d0e2e0ec80
Mike Jolley <mike.jolley@me.com>
2022-03-01 14:49:12 +0000

    Usused dep

--- src/Installer.php
+++ src/Installer.php
@@ -1,8 +1,6 @@
 <?php
 namespace Automattic\WooCommerce\Blocks;
 
-use Automattic\WooCommerce\Internal\Utilities\DatabaseUtil;
-
 /**
  * Installer class.
  * Handles installation of Blocks plugin dependencies.
