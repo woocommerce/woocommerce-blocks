c14182c9de02969179c79fc5ea0289358f89719a
Thomas Roberts <thomas.roberts@automattic.com>
2022-03-02 15:44:56 +0000

    Move selectedRates, selectShippingRate and isSelectingRate

--- assets/js/base/context/hooks/use-shipping-data.ts
+++ assets/js/base/context/hooks/use-shipping-data.ts
@@ -4,11 +4,24 @@
 import { CART_STORE_KEY as storeKey } from '@woocommerce/block-data';
 import { useSelect } from '@wordpress/data';
 import { Cart } from '@woocommerce/type-defs/cart';
+import { SelectShippingRateType } from '@woocommerce/type-defs/shipping';
+import { useEffect, useRef } from '@wordpress/element';
+import { deriveSelectedShippingRates } from '@woocommerce/base-utils';
+import isShallowEqual from '@wordpress/is-shallow-equal';
+import { isObject } from '@woocommerce/types';
+
+/**
+ * Internal dependencies
+ */
+import { useSelectShippingRate } from './shipping';
 
 type ShippingData = Pick<
 	Cart,
 	'shippingRates' | 'needsShipping' | 'hasCalculatedShipping'
-> & { shippingRatesLoading: boolean };
+> & {
+	shippingRatesLoading: boolean;
+	selectedRates: Record< string, string | unknown >;
+} & SelectShippingRateType;
 
 export const useShippingData = (): ShippingData => {
 	const {
@@ -25,7 +38,26 @@ export const useShippingData = (): ShippingData => {
 			shippingRatesLoading: store.isShippingRateBeingSelected(),
 		};
 	} );
+	const { isSelectingRate, selectShippingRate } = useSelectShippingRate();
+
+	// set selected rates on ref so it's always current.
+	const selectedRates = useRef< Record< string, unknown > >( {} );
+	useEffect( () => {
+		const derivedSelectedRates = deriveSelectedShippingRates(
+			shippingRates
+		);
+		if (
+			isObject( derivedSelectedRates ) &&
+			! isShallowEqual( selectedRates.current, derivedSelectedRates )
+		) {
+			selectedRates.current = derivedSelectedRates;
+		}
+	}, [ shippingRates ] );
+
 	return {
+		isSelectingRate,
+		selectedRates: selectedRates.current,
+		selectShippingRate,
 		shippingRates,
 		needsShipping,
 		hasCalculatedShipping,
