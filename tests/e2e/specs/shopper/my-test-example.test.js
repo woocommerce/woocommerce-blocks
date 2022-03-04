29d99a7997edd8cb5a81552d7804316de0ae674c
Saad Tarhi <saad.tarhi@automattic.com>
2022-03-04 15:50:19 +0100

    Create a minimal test example
    This test file is used as an example, we will add later a script in
    package.json to call only this file

---
+++ tests/e2e/specs/shopper/my-test-example.test.js
@@ -0,0 +1,33 @@
+/**
+ * Internal dependencies
+ */
+import { shopper } from '../../../utils';
+
+const block = {
+	name: 'Cart',
+};
+
+if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 2 )
+	// eslint-disable-next-line jest/no-focused-tests
+	test.only( `skipping ${ block.name } tests`, () => {} );
+
+const simpleProductName = 'Woo Single #1';
+
+describe( 'Shopper → Checkout → Can see warnings when form is incomplete', () => {
+	beforeAll( async () => {
+		await shopper.emptyCart();
+	} );
+
+	afterAll( async () => {
+		await shopper.emptyCart();
+	} );
+
+	it( 'allows customer to proceed to checkout', async () => {
+		await shopper.goToShop();
+		await shopper.addToCartFromShopPage( simpleProductName );
+		await shopper.goToCheckout();
+
+		// Verify that you see the Checkout Block page
+		await expect( page ).toMatchElement( 'h1', { text: 'Checkout' } );
+	} );
+} );
