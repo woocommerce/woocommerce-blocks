b1e64cfa3eb2a40578e7a443cff9ac1a19c4c4d0
Mike Jolley <mike.jolley@me.com>
2022-03-07 15:28:14 +0000

    Remove line break

--- src/StoreApi/Exceptions/NotPurchasableException.php
+++ src/StoreApi/Exceptions/NotPurchasableException.php
@@ -4,7 +4,6 @@ namespace Automattic\WooCommerce\StoreApi\Exceptions;
 /**
  * NotPurchasableException class.
  *
- * This exception is thrown when an item in the cart is not able to be
- * purchased.
+ * This exception is thrown when an item in the cart is not able to be purchased.
  */
 class NotPurchasableException extends StockAvailabilityException {}
