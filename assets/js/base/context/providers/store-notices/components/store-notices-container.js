7cac80bcafc0c47729b6090d7476d09592282169
Panos (Panagiotis) Synetos <panos.synetos@automattic.com>
2022-03-08 10:55:06 +0200

    ⏪ REVERT: Revert decode entities directly on the store notices container - #5869

--- assets/js/base/context/providers/store-notices/components/store-notices-container.js
+++ assets/js/base/context/providers/store-notices/components/store-notices-container.js
@@ -4,7 +4,6 @@
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import { Notice } from 'wordpress-components';
-import { decodeEntities } from '@wordpress/html-entities';
 
 /**
  * Internal dependencies
