b31a55b2a19969cf13657b5a1d97226f895d9f5b
Tomasz Tunik <tomasztunik@gmail.com>
2022-03-08 14:10:09 +0100

    Unify all atomic blocks to register on php side

---
+++ src/BlockTypes/ProductSKU.php
@@ -0,0 +1,22 @@
+<?php
+namespace Automattic\WooCommerce\Blocks\BlockTypes;
+
+/**
+ * ProductSKU class.
+ */
+class ProductSKU extends AbstractBlock {
+
+	/**
+	 * Block name.
+	 *
+	 * @var string
+	 */
+	protected $block_name = 'product-sku';
+
+	/**
+	 * API version name.
+	 *
+	 * @var string
+	 */
+	protected $api_version = '2';
+}
