21de8f1c128d3961e10b54c78a955652c536a458
Niels Lange <info@nielslange.de>
2022-03-09 17:31:07 +0100

    Shopper can have different shipping and billing addresses

--- tests/utils/shopper.js
+++ tests/utils/shopper.js
@@ -200,11 +200,11 @@ export const shopper = {
 		fillBillingDetails: async ( customerBillingDetails ) => {
 			await expect( page ).toFill( '#billing-first_name', customerBillingDetails.firstname );
 			await expect( page ).toFill( '#billing-last_name', customerBillingDetails.lastname );
-			await expect( page ).toFill( '#components-form-token-input-0', customerBillingDetails.country );
+			await expect( page ).toFill( '#shipping-country input', customerBillingDetails.country );
 			await expect( page ).toFill( '#billing-address_1', customerBillingDetails.addressfirstline );
 			await expect( page ).toFill( '#billing-address_2', customerBillingDetails.addresssecondline );
 			await expect( page ).toFill( '#billing-city', customerBillingDetails.city );
-			await expect( page ).toFill( '#components-form-token-input-1', customerBillingDetails.state );
+			await expect( page ).toFill( '#shipping-state input', customerBillingDetails.state );
 			await expect( page ).toFill( '#billing-postcode', customerBillingDetails.postcode );
 			await expect( page ).toFill( '#phone', customerBillingDetails.phone );
 			await expect( page ).toFill( '#email', customerBillingDetails.email );
@@ -214,11 +214,11 @@ export const shopper = {
 		fillShippingDetails: async ( customerShippingDetails ) => {
 			await expect( page ).toFill( '#shipping-first_name', customerShippingDetails.firstname );
 			await expect( page ).toFill( '#shipping-last_name', customerShippingDetails.lastname );
-			await expect( page ).toFill( '#components-form-token-input-2', customerShippingDetails.country );
+			await expect( page ).toFill( '#shipping-country input', customerShippingDetails.country );
 			await expect( page ).toFill( '#shipping-address_1', customerShippingDetails.addressfirstline );
 			await expect( page ).toFill( '#shipping-address_2', customerShippingDetails.addresssecondline );
 			await expect( page ).toFill( '#shipping-city', customerShippingDetails.city );
-			await expect( page ).toFill( '#components-form-token-input-3', customerShippingDetails.state );
+			await expect( page ).toFill( '#shipping-state input', customerShippingDetails.state );
 			await expect( page ).toFill( '#shipping-postcode', customerShippingDetails.postcode );
 			await expect( page ).toFill( '#shipping-phone', customerShippingDetails.phone );
 		},
