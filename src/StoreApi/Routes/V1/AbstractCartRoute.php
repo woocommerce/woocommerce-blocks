702d1b5e9096e1f604d675885b5279efeec38d92
Mike Jolley <mike.jolley@me.com>
2022-03-10 14:45:56 +0000

    typo

--- src/StoreApi/Routes/V1/AbstractCartRoute.php
+++ src/StoreApi/Routes/V1/AbstractCartRoute.php
@@ -177,7 +177,7 @@ abstract class AbstractCartRoute extends AbstractRoute {
 	 */
 	protected function check_nonce( \WP_REST_Request $request ) {
 		if ( $request->get_header( 'X-WC-Store-API-Nonce' ) ) {
-			// @todo Remove handling and sendinf of deprecated X-WC-Store-API-Nonce Header (Blocks 7.5.0)
+			// @todo Remove handling and sending of deprecated X-WC-Store-API-Nonce Header (Blocks 7.5.0)
 			wc_deprecated_argument( 'X-WC-Store-API-Nonce', '7.2.0', 'Use the "Nonce" Header instead. This header will be removed after Blocks release 7.5' );
 			rest_handle_deprecated_argument( 'X-WC-Store-API-Nonce', 'Use the "Nonce" Header instead. This header will be removed after Blocks release 7.5', '7.2.0' );
 			$nonce = $request->get_header( 'X-WC-Store-API-Nonce' );
