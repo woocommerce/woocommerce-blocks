d27e625ffb2b5b82544b6c7a63dd18465912124a
Tarun Vijwani <tarun.vijwani@automattic.com>
2022-03-11 22:27:46 +0400

    Added test case for Critical flows: Shopper → Checkout → Can see redirect to empty Cart when Checkout is empty

--- tests/e2e/specs/shopper/checkout-redirect.test.js
+++ tests/e2e/specs/shopper/checkout-redirect.test.js
@@ -2,3 +2,27 @@
  * Internal dependencies
  */
 import { shopper } from '../../../utils';
+import { BASE_URL } from '../../utils';
+
+if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 2 )
+	// eslint-disable-next-line jest/no-focused-tests
+	test.only( `skipping ${ block.name } tests`, () => {} );
+
+describe( 'Shopper → Checkout → Can see redirect to empty Cart when Checkout is empty', () => {
+	beforeAll( async () => {
+		await shopper.block.emptyCart();
+	} );
+
+	it( 'Can see redirect to empty Cart', async () => {
+		const slug = 'checkout';
+		const url = BASE_URL + slug;
+		await page.goto( url, {
+			waitUntil: 'networkidle0',
+		} );
+
+		// Verify cart is empty'
+		await expect( page ).toMatchElement( 'div.woocommerce-info', {
+			text: 'Checkout is not available whilst your cart is empty.',
+		} );
+	} );
+} );
