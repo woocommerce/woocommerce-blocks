f4fdfc0b66ca0e1d597202c51d2762263bc4cdc5
Mike Jolley <mike.jolley@me.com>
2022-03-18 16:53:34 +0000

    Todo for Consider deprecating OrderMetaSlotFill and DiscountSlotFill in favour of inner block areas.

--- assets/js/blocks/checkout/inner-blocks/checkout-order-summary-block/slotfills.tsx
+++ assets/js/blocks/checkout/inner-blocks/checkout-order-summary-block/slotfills.tsx
@@ -4,6 +4,7 @@
 import { ExperimentalOrderMeta } from '@woocommerce/blocks-checkout';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
 
+// @todo Consider deprecating OrderMetaSlotFill and DiscountSlotFill in favour of inner block areas.
 export const OrderMetaSlotFill = (): JSX.Element => {
 	// Prepare props to pass to the ExperimentalOrderMeta slot fill. We need to pluck out receiveCart.
 	// eslint-disable-next-line no-unused-vars
