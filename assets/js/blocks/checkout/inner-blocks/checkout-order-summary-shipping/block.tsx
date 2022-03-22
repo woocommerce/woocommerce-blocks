7c9af1bacc4a0096898fa4a0c2de17d00ce30066
Mike Jolley <mike.jolley@me.com>
2022-03-22 10:44:26 +0000

    Optional classname

--- assets/js/blocks/checkout/inner-blocks/checkout-order-summary-shipping/block.tsx
+++ assets/js/blocks/checkout/inner-blocks/checkout-order-summary-shipping/block.tsx
@@ -5,7 +5,11 @@ import { TotalsShipping } from '@woocommerce/base-components/cart-checkout';
 import { getCurrencyFromPriceResponse } from '@woocommerce/price-format';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
 
-const Block = ( { className }: { className: string } ): JSX.Element | null => {
+const Block = ( {
+	className = '',
+}: {
+	className?: string;
+} ): JSX.Element | null => {
 	const { cartTotals, cartNeedsShipping } = useStoreCart();
 
 	if ( ! cartNeedsShipping ) {
