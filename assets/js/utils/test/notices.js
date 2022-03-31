a5e4f455b69215625679441acd2f14ac25b3e5d5
Thomas Roberts <thomas.roberts@automattic.com>
2022-03-31 10:02:45 +0100

    Add function to remove notices by status

--- assets/js/utils/test/notices.js
+++ assets/js/utils/test/notices.js
@@ -1,16 +1,19 @@
 /**
  * External dependencies
  */
-import { select } from '@wordpress/data';
+import { select, dispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
-import { hasNoticesOfType } from '../notices';
+import { hasNoticesOfType, removeNoticesByStatus } from '../notices';
 
 jest.mock( '@wordpress/data' );
 
 describe( 'Notice utils', () => {
+	beforeEach( () => {
+		jest.resetAllMocks();
+	} );
 	describe( 'hasNoticesOfType', () => {
 		it( 'Correctly returns if there are notices of a given type in the core data store', () => {
 			select.mockReturnValue( {
@@ -45,6 +48,73 @@ describe( 'Notice utils', () => {
 			} );
 			const hasDefaultNotices = hasNoticesOfType( 'wc/cart', 'default' );
 			expect( hasDefaultNotices ).toBe( false );
+		} );
+	} );
+	describe( 'removeNoticesByStatus', () => {
+		it( 'Correctly removes notices of a given status', () => {
+			select.mockReturnValue( {
+				getNotices: jest.fn().mockReturnValue( [
+					{
+						id: 'coupon-form',
+						status: 'error',
+						content:
+							'Coupon cannot be removed because it is not already applied to the cart.',
+						spokenMessage:
+							'Coupon cannot be removed because it is not already applied to the cart.',
+						isDismissible: true,
+						actions: [],
+						type: 'default',
+						icon: null,
+						explicitDismiss: false,
+					},
+					{
+						id: 'address-form',
+						status: 'error',
+						content: 'Address invalid',
+						spokenMessage: 'Address invalid',
+						isDismissible: true,
+						actions: [],
+						type: 'default',
+						icon: null,
+						explicitDismiss: false,
+					},
+					{
+						id: 'some-warning',
+						status: 'warning',
+						content: 'Warning notice.',
+						spokenMessage: 'Warning notice.',
+						isDismissible: true,
+						actions: [],
+						type: 'default',
+						icon: null,
+						explicitDismiss: false,
+					},
+				] ),
+			} );
+			dispatch.mockReturnValue( {
+				removeNotice: jest.fn(),
+			} );
+			removeNoticesByStatus( 'error' );
+			expect( dispatch().removeNotice ).toHaveBeenNthCalledWith(
+				1,
+				'coupon-form'
+			);
+			expect( dispatch().removeNotice ).toHaveBeenNthCalledWith(
+				2,
+				'address-form'
+			);
+		} );
+
+		it( 'Handles notices being empty', () => {
+			select.mockReturnValue( {
+				getNotices: jest.fn().mockReturnValue( [] ),
+			} );
+
+			dispatch.mockReturnValue( {
+				removeNotice: jest.fn(),
+			} );
+			removeNoticesByStatus( 'empty' );
+			expect( dispatch().removeNotice ).not.toBeCalled();
 		} );
 	} );
 } );
