0c71b3c680343a1aa1d5606dd875185c72357783
Thomas Roberts <thomas.roberts@automattic.com>
2022-04-05 14:22:56 +0100

    Export payment method data store key

--- assets/js/data/index.ts
+++ assets/js/data/index.ts
@@ -9,5 +9,6 @@ import '@wordpress/notices';
 export { SCHEMA_STORE_KEY } from './schema';
 export { COLLECTIONS_STORE_KEY } from './collections';
 export { CART_STORE_KEY } from './cart';
+export { PAYMENT_METHOD_DATA_STORE_KEY } from './payment-method-data';
 export { QUERY_STATE_STORE_KEY } from './query-state';
 export * from './constants';
