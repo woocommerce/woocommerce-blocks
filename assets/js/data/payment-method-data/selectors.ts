fc0f1ed8444afd4132c27562ff2792464026d62f
Thomas Roberts <thomas.roberts@automattic.com>
2022-04-05 14:23:39 +0100

    Add shouldSavePaymentMethod selector

--- assets/js/data/payment-method-data/selectors.ts
+++ assets/js/data/payment-method-data/selectors.ts
@@ -1,12 +1,8 @@
 /**
- * External dependencies
- */
-import { getExpressPaymentMethods } from '@woocommerce/blocks-registry';
-
-/**
  * Internal dependencies
  */
 import { PaymentMethodDataState } from '../default-states';
+import { getExpressPaymentMethods } from '../../blocks-registry';
 
 export const isExpressPaymentMethodActive = (
 	state: PaymentMethodDataState
@@ -16,14 +12,14 @@ export const isExpressPaymentMethodActive = (
 	);
 };
 
-export const getPaymentMethods = () => {
-	return 'lala';
-};
-
 export const getActiveSavedToken = ( state: PaymentMethodDataState ) => {
 	return state.activeSavedToken;
 };
 
 export const getActivePaymentMethod = ( state: PaymentMethodDataState ) => {
 	return state.activePaymentMethod;
+};
+
+export const shouldSavePaymentMethod = ( state: PaymentMethodDataState ) => {
+	return state.shouldSavePaymentMethod;
 };
