3ff9fe9882bb47a0f0ff6b02ed529742ea441cc4
Thomas Roberts <thomas.roberts@automattic.com>
2022-04-06 16:34:30 +0100

    Create new setDefaultPaymentMethod function
    This will set the payment method when the cart loads.

---
+++ assets/js/data/payment-methods/set-default-payment-method.ts
@@ -0,0 +1,72 @@
+/**
+ * External dependencies
+ */
+import { select, dispatch } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { STORE_KEY as PAYMENT_METHOD_DATA_STORE_KEY } from './constants';
+import { STATUS } from '../../base/context/providers/cart-checkout/payment-methods/constants';
+import { getCustomerPaymentMethods } from '../../base/context/providers/cart-checkout/payment-methods/utils';
+
+export const setDefaultPaymentMethod = async ( methods ) => {
+	const paymentMethodKeys = methods;
+
+	const expressPaymentMethodKeys = select(
+		PAYMENT_METHOD_DATA_STORE_KEY
+	).getAvailableExpressPaymentMethods();
+	const activePaymentMethod = select(
+		PAYMENT_METHOD_DATA_STORE_KEY
+	).getActivePaymentMethod();
+
+	const allPaymentMethodKeys = [
+		...paymentMethodKeys,
+		expressPaymentMethodKeys,
+	];
+
+	// Return if current method is valid.
+	if (
+		activePaymentMethod &&
+		allPaymentMethodKeys.includes( activePaymentMethod )
+	) {
+		return;
+	}
+
+	dispatch( PAYMENT_METHOD_DATA_STORE_KEY ).setPaymentStatus(
+		STATUS.PRISTINE
+	);
+	const objectOfPaymentMethods = allPaymentMethodKeys.reduce( ( a, curr ) => {
+		a[ curr ] = null;
+		return a;
+	}, {} );
+	const customerPaymentMethods = getCustomerPaymentMethods(
+		objectOfPaymentMethods
+	);
+	const customerPaymentMethod =
+		Object.keys( customerPaymentMethods ).flatMap(
+			( type ) => customerPaymentMethods[ type ]
+		)[ 0 ] || undefined;
+
+	if ( customerPaymentMethod ) {
+		const token = customerPaymentMethod.tokenId.toString();
+		const paymentMethodSlug = customerPaymentMethod.method.gateway;
+
+		const savedTokenKey = `wc-${ paymentMethodSlug }-payment-token`;
+
+		dispatch( PAYMENT_METHOD_DATA_STORE_KEY ).setActivePaymentMethod(
+			paymentMethodSlug,
+			{
+				token,
+				payment_method: paymentMethodSlug,
+				[ savedTokenKey ]: token,
+				isSavedToken: true,
+			}
+		);
+		return;
+	}
+
+	dispatch( PAYMENT_METHOD_DATA_STORE_KEY ).setActivePaymentMethod(
+		paymentMethodKeys[ 0 ]
+	);
+};
