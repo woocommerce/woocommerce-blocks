1b11e91383b9bb0c7eafe95f202d81fb30149e64
Saad Tarhi <saad.trh@gmail.com>
2022-04-08 08:54:00 +0000

    Fix PHP coding standards

--- src/StoreApi/Utilities/QuantityLimits.php
+++ src/StoreApi/Utilities/QuantityLimits.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Automattic\WooCommerce\StoreApi\Utilities;
 
 use Automattic\WooCommerce\Checkout\Helpers\ReserveStock;
@@ -10,8 +9,7 @@ use Automattic\WooCommerce\StoreApi\Utilities\DraftOrderTrait;
  *
  * Returns limits for products and cart items when using the StoreAPI and supporting classes.
  */
-final class QuantityLimits
-{
+final class QuantityLimits {
 	use DraftOrderTrait;
 
 	/**
@@ -20,11 +18,10 @@ final class QuantityLimits
 	 * @param array $cart_item A cart item array.
 	 * @return array
 	 */
-	public function get_cart_item_quantity_limits($cart_item)
-	{
+	public function get_cart_item_quantity_limits( $cart_item ) {
 		$product = $cart_item['data'] ?? false;
 
-		if (!$product instanceof \WC_Product) {
+		if ( ! $product instanceof \WC_Product ) {
 			return [
 				'minimum'     => 1,
 				'maximum'     => null,
@@ -33,14 +30,14 @@ final class QuantityLimits
 			];
 		}
 
-		$multiple_of = (int) $this->filter_value(1, 'multiple_of', $cart_item);
-		$minimum     = (int) $this->filter_value(1, 'minimum', $cart_item);
-		$maximum     = (int) $this->filter_value($this->get_product_quantity_limit($product), 'maximum', $cart_item);
-		$editable    = (bool) $this->filter_value(!$product->is_sold_individually(), 'editable', $cart_item);
+		$multiple_of = (int) $this->filter_value( 1, 'multiple_of', $cart_item );
+		$minimum     = (int) $this->filter_value( 1, 'minimum', $cart_item );
+		$maximum     = (int) $this->filter_value( $this->get_product_quantity_limit( $product ), 'maximum', $cart_item );
+		$editable    = (bool) $this->filter_value( ! $product->is_sold_individually(), 'editable', $cart_item );
 
 		return [
-			'minimum'     => $this->limit_to_multiple($minimum, $multiple_of, 'ceil'),
-			'maximum'     => $this->limit_to_multiple($maximum, $multiple_of, 'floor'),
+			'minimum'     => $this->limit_to_multiple( $minimum, $multiple_of, 'ceil' ),
+			'maximum'     => $this->limit_to_multiple( $maximum, $multiple_of, 'floor' ),
 			'multiple_of' => $multiple_of,
 			'editable'    => $editable,
 		];
@@ -52,15 +49,14 @@ final class QuantityLimits
 	 * @param \WC_Product $product Product instance.
 	 * @return array
 	 */
-	public function get_add_to_cart_limits(\WC_Product $product)
-	{
-		$multiple_of = $this->filter_value(1, 'multiple_of', $product);
-		$minimum     = $this->filter_value(1, 'minimum', $product);
-		$maximum     = $this->filter_value($this->get_product_quantity_limit($product), 'maximum', $product);
+	public function get_add_to_cart_limits( \WC_Product $product ) {
+		$multiple_of = $this->filter_value( 1, 'multiple_of', $product );
+		$minimum     = $this->filter_value( 1, 'minimum', $product );
+		$maximum     = $this->filter_value( $this->get_product_quantity_limit( $product ), 'maximum', $product );
 
 		return [
-			'minimum'     => $this->limit_to_multiple($minimum, $multiple_of, 'ceil'),
-			'maximum'     => $this->limit_to_multiple($maximum, $multiple_of, 'floor'),
+			'minimum'     => $this->limit_to_multiple( $minimum, $multiple_of, 'ceil' ),
+			'maximum'     => $this->limit_to_multiple( $maximum, $multiple_of, 'floor' ),
 			'multiple_of' => $multiple_of,
 		];
 	}
@@ -73,13 +69,12 @@ final class QuantityLimits
 	 * @param string $rounding_function ceil, floor, or round.
 	 * @return int
 	 */
-	public function limit_to_multiple(int $number, int $multiple_of, string $rounding_function = 'round')
-	{
-		if ($multiple_of <= 1) {
+	public function limit_to_multiple( int $number, int $multiple_of, string $rounding_function = 'round' ) {
+		if ( $multiple_of <= 1 ) {
 			return $number;
 		}
-		$rounding_function = in_array($rounding_function, ['ceil', 'floor', 'round'], true) ? $rounding_function : 'round';
-		return $rounding_function($number / $multiple_of) * $multiple_of;
+		$rounding_function = in_array( $rounding_function, [ 'ceil', 'floor', 'round' ], true ) ? $rounding_function : 'round';
+		return $rounding_function( $number / $multiple_of ) * $multiple_of;
 	}
 
 	/**
@@ -89,45 +84,44 @@ final class QuantityLimits
 	 * @param \WC_Product|array $cart_item Cart item.
 	 * @return \WP_Error|true
 	 */
-	public function validate_cart_item_quantity($quantity, $cart_item)
-	{
-		$limits = $this->get_cart_item_quantity_limits($cart_item);
+	public function validate_cart_item_quantity( $quantity, $cart_item ) {
+		$limits = $this->get_cart_item_quantity_limits( $cart_item );
 
-		if (!$limits['editable']) {
+		if ( ! $limits['editable'] ) {
 			return new \WP_Error(
 				'readonly_quantity',
-				__('This item is already in the cart and its quantity cannot be edited', 'woo-gutenberg-products-block')
+				__( 'This item is already in the cart and its quantity cannot be edited', 'woo-gutenberg-products-block' )
 			);
 		}
 
-		if ($quantity < $limits['minimum']) {
+		if ( $quantity < $limits['minimum'] ) {
 			return new \WP_Error(
 				'invalid_quantity',
 				sprintf(
 					// Translators: %s amount.
-					__('The minimum quantity that can be added to the cart is %s', 'woo-gutenberg-products-block'),
+					__( 'The minimum quantity that can be added to the cart is %s', 'woo-gutenberg-products-block' ),
 					$limits['minimum']
 				)
 			);
 		}
 
-		if ($quantity > $limits['maximum']) {
+		if ( $quantity > $limits['maximum'] ) {
 			return new \WP_Error(
 				'invalid_quantity',
 				sprintf(
 					// Translators: %s amount.
-					__('The maximum quantity that can be added to the cart is %s', 'woo-gutenberg-products-block'),
+					__( 'The maximum quantity that can be added to the cart is %s', 'woo-gutenberg-products-block' ),
 					$limits['maximum']
 				)
 			);
 		}
 
-		if ($quantity % $limits['multiple_of']) {
+		if ( $quantity % $limits['multiple_of'] ) {
 			return new \WP_Error(
 				'invalid_quantity',
 				sprintf(
 					// Translators: %s amount.
-					__('The quantity added to the cart must be a multiple of %s', 'woo-gutenberg-products-block'),
+					__( 'The quantity added to the cart must be a multiple of %s', 'woo-gutenberg-products-block' ),
 					$limits['multiple_of']
 				)
 			);
@@ -145,14 +139,13 @@ final class QuantityLimits
 	 * @param \WC_Product $product Product instance.
 	 * @return int
 	 */
-	protected function get_product_quantity_limit(\WC_Product $product)
-	{
-		$limits = [9999];
+	protected function get_product_quantity_limit( \WC_Product $product ) {
+		$limits = [ 9999 ];
 
-		if ($product->is_sold_individually()) {
+		if ( $product->is_sold_individually() ) {
 			$limits[] = 1;
-		} elseif (!$product->backorders_allowed()) {
-			$limits[] = $this->get_remaining_stock($product);
+		} elseif ( ! $product->backorders_allowed() ) {
+			$limits[] = $this->get_remaining_stock( $product );
 		}
 
 		/**
@@ -164,7 +157,7 @@ final class QuantityLimits
 		 * @param \WC_Product $product Product instance.
 		 * @return integer
 		 */
-		return apply_filters('woocommerce_store_api_product_quantity_limit', max(min(array_filter($limits)), 1), $product);
+		return apply_filters( 'woocommerce_store_api_product_quantity_limit', max( min( array_filter( $limits ) ), 1 ), $product );
 	}
 
 	/**
@@ -175,14 +168,13 @@ final class QuantityLimits
 	 * @param \WC_Product $product Product instance.
 	 * @return integer|null
 	 */
-	protected function get_remaining_stock(\WC_Product $product)
-	{
-		if (is_null($product->get_stock_quantity())) {
+	protected function get_remaining_stock( \WC_Product $product ) {
+		if ( is_null( $product->get_stock_quantity() ) ) {
 			return null;
 		}
 
 		$reserve_stock  = new ReserveStock();
-		$reserved_stock = $reserve_stock->get_reserved_stock($product, $this->get_draft_order_id());
+		$reserved_stock = $reserve_stock->get_reserved_stock( $product, $this->get_draft_order_id() );
 
 		return $product->get_stock_quantity() - $reserved_stock;
 	}
@@ -195,8 +187,7 @@ final class QuantityLimits
 	 * @param \WC_Product|array $cart_item_or_product Either a cart item or a product instance.
 	 * @return mixed
 	 */
-	protected function filter_value($value, string $value_type, $cart_item_or_product)
-	{
+	protected function filter_value( $value, string $value_type, $cart_item_or_product ) {
 		$is_product = $cart_item_or_product instanceof \WC_Product;
 		$product    = $is_product ? $cart_item_or_product : $cart_item_or_product['data'];
 		$cart_item  = $is_product ? null : $cart_item_or_product;
@@ -212,6 +203,6 @@ final class QuantityLimits
 		 * @param array|null $cart_item The cart item if the product exists in the cart, or null.
 		 * @return mixed
 		 */
-		return apply_filters("woocommerce_store_api_product_quantity_{$value_type}", $value, $product, $cart_item);
+		return apply_filters( "woocommerce_store_api_product_quantity_{$value_type}", $value, $product, $cart_item );
 	}
 }
