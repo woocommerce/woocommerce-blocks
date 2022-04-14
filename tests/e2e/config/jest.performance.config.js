27d40651a5f4b97effe98451f81f43f36fc57955
Niels Lange <info@nielslange.de>
2022-04-14 15:54:07 +0200

    Simplify jest.performance.config.js

--- tests/e2e/config/jest.performance.config.js
+++ tests/e2e/config/jest.performance.config.js
@@ -1,33 +1,13 @@
 module.exports = {
-	...require( '@wordpress/scripts/config/jest-e2e.config' ),
-	rootDir: '../../../',
-	// Automatically clear mock calls and instances between every test
-	clearMocks: true,
-
-	moduleNameMapper: {
-		'@woocommerce/blocks-test-utils': '<rootDir>/tests/utils',
-	},
+	...require( './jest.config' ),
 
 	reporters: [
 		'default',
 		'<rootDir>/tests/e2e/config/performance-reporter.js',
 	],
 
-	testEnvironment: '<rootDir>/tests/e2e/config/environment.js',
-	testRunner: 'jest-circus/runner',
-
 	// Where to look for test files
 	roots: [ '<rootDir>/tests/e2e/specs/performance' ],
-	globalSetup: '<rootDir>/tests/e2e/config/setup.js',
-	globalTeardown: '<rootDir>/tests/e2e/config/teardown.js',
-	setupFiles: [],
-	// A list of paths to modules that run some code to configure or set up the testing framework
-	// before each test
-	setupFilesAfterEnv: [
-		'<rootDir>/tests/e2e/config/custom-matchers/index.js',
-		'<rootDir>/tests/e2e/config/jest.setup.js',
-		'expect-puppeteer',
-	],
 
-	transformIgnorePatterns: [ 'node_modules/(?!(woocommerce)/)' ],
+	testPathIgnorePatterns: [],
 };
