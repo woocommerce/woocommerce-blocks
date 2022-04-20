f07ff09138bb945d526f2b9507fcc6106362b68a
Tarun Vijwani <tarun.vijwani@automattic.com>
2022-04-20 13:41:06 +0400

    Added comments and changed constant for email to variable

--- tests/e2e/specs/shopper/cart-checkout/account.test.js
+++ tests/e2e/specs/shopper/cart-checkout/account.test.js
@@ -1,33 +1,46 @@
+/**
+ * Internal dependencies
+ */
 import { shopper } from '../../../../utils';
 import { SIMPLE_PHYSICAL_PRODUCT_NAME } from '.../../../../utils/constants';
+
+/**
+ * External dependencies
+ */
 import {
 	visitBlockPage,
 	selectBlockByName,
 	saveOrPublish,
 } from '@woocommerce/blocks-test-utils';
-
 import {
 	merchant,
 	setCheckbox,
-	unsetCheckbox,
 	openDocumentSettingsSidebar,
 } from '@woocommerce/e2e-utils';
 import { switchUserToAdmin, visitAdminPage } from '@wordpress/e2e-test-utils';
+
+// Defining block details
 const block = {
 	name: 'Checkout',
 	slug: 'woocommerce/checkout',
 	class: '.wp-block-woocommerce-checkout',
 };
+
 if ( process.env.WOOCOMMERCE_BLOCKS_PHASE < 2 ) {
+	// eslint-disable-next-line jest/no-focused-tests
 	test.only( 'Skipping Cart & Checkout tests', () => {} );
 }
+
 describe( 'Shopper → Checkout → Account', () => {
 	beforeAll( async () => {
 		await merchant.openSettings( 'account' );
+		//Enable create an account at checkout option
 		await setCheckbox( '#woocommerce_enable_checkout_login_reminder' );
+		//Enable login at checkout option
 		await setCheckbox(
 			'#woocommerce_enable_signup_and_login_from_checkout'
 		);
+		//Enable guest checkout option
 		await setCheckbox( '#woocommerce_enable_guest_checkout' );
 		await page.click( 'button[name="save"]' );
 		await page.waitForNavigation( { waitUntil: 'networkidle0' } );
@@ -37,6 +50,7 @@ describe( 'Shopper → Checkout → Account', () => {
 		await selectBlockByName(
 			'woocommerce/checkout-contact-information-block'
 		);
+		//Enable shoppers to sign up at checkout option
 		await expect( page ).toClick( 'label', {
 			text:
 				'Allow shoppers to sign up for a user account during checkout',
@@ -47,28 +61,33 @@ describe( 'Shopper → Checkout → Account', () => {
 		await shopper.addToCartFromShopPage( SIMPLE_PHYSICAL_PRODUCT_NAME );
 		await shopper.block.goToCheckout();
 	} );
+
 	it( 'user can login to existing account', async () => {
+		//Get the login link from checkout page
 		const loginLink = await page.$eval(
 			'span.wc-block-components-checkout-step__heading-content a',
 			( el ) => el.href
 		);
-
+		//Confirm login link is correct
 		await expect( loginLink ).toContain(
 			`${ process.env.WORDPRESS_BASE_URL }/my-account/?redirect_to`
 		);
 		await expect( loginLink ).toContain( `checkout` );
 	} );
+
 	it( 'user can can create an account', async () => {
 		await expect( page ).toClick( 'span', {
 			text: 'Create an account?',
 		} );
-		const TEST_EMAIL = 'test' + Math.random() * 10 + '@example.com';
-		await expect( page ).toFill( `#email`, TEST_EMAIL );
+		//Create random email to place an order
+		let testEmail = `test${ Math.random() * 10 }@example.com`;
+		await expect( page ).toFill( `#email`, testEmail );
 		await shopper.block.fillInCheckoutWithTestData();
 		await shopper.block.placeOrder();
 		await expect( page ).toMatch( 'Order received' );
 		await switchUserToAdmin();
 		await visitAdminPage( 'users.php' );
-		await expect( page ).toMatch( TEST_EMAIL );
+		//Confirm account is being created with the email
+		await expect( page ).toMatch( testEmail );
 	} );
 } );
