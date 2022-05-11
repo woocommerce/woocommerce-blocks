90fc97dc3747766cec0adb152f785c2f8475ae35
Tarun Vijwani <tarun.vijwani@automattic.com>
2022-05-11 15:35:40 +0400

    Convert validation context to data store

--- packages/checkout/validation/actions.ts
+++ packages/checkout/validation/actions.ts
@@ -13,6 +13,30 @@ export const setValidationErrors = (
 		errors,
 	} as const );
 
+export const clearAllValidationErrors = () => ( {
+	type: types.CLEAR_ALL_VALIDATION_ERRORS,
+} );
+
+export const clearValidationError = ( error: string ) => ( {
+	type: types.CLEAR_VALIDATION_ERROR,
+	error,
+} );
+export const hideValidationError = ( error: string ) => ( {
+	type: types.HIDE_VALIDATION_ERROR,
+	error,
+} );
+export const showValidationError = ( error: string ) => ( {
+	type: types.SHOW_VALIDATION_ERROR,
+	error,
+} );
+export const showAllValidationErrors = () => ( {
+	type: types.SHOW_ALL_VALIDATION_ERRORS,
+} );
 export type ValidationAction = ReturnOrGeneratorYieldUnion<
-	typeof setValidationErrors
+	| typeof setValidationErrors
+	| typeof clearAllValidationErrors
+	| typeof clearValidationError
+	| typeof hideValidationError
+	| typeof showValidationError
+	| typeof showAllValidationErrors
 >;
