de46831f40aca2ae6db5570d13f4e11928bf8539
Lucio Giannotta <lucio.giannotta@a8c.com>
2022-05-13 05:22:19 +0200

    Remove duplicate utils

--- assets/js/blocks/featured-items/featured-category/utils.js
+++ assets/js/blocks/featured-items/featured-category/utils.js
@@ -29,34 +29,4 @@ function getCategoryImageId( category ) {
 	return 0;
 }
 
-/**
- * Generate a style object given either a product category image from the API or URL to an image.
- *
- * @param {string} url An image URL.
- * @return {Object} A style object with a backgroundImage set (if a valid image is provided).
- */
-function getBackgroundImageStyles( url ) {
-	if ( url ) {
-		return { backgroundImage: `url(${ url })` };
-	}
-	return {};
-}
-
-/**
- * Convert the selected ratio to the correct background class.
- *
- * @param {number} ratio Selected opacity from 0 to 100.
- * @return {string} The class name, if applicable (not used for ratio 0 or 50).
- */
-function dimRatioToClass( ratio ) {
-	return ratio === 0 || ratio === 50
-		? null
-		: `has-background-dim-${ 10 * Math.round( ratio / 10 ) }`;
-}
-
-export {
-	getCategoryImageSrc,
-	getCategoryImageId,
-	getBackgroundImageStyles,
-	dimRatioToClass,
-};
+export { getCategoryImageSrc, getCategoryImageId };
