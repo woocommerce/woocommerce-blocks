4d0a6120b94660bc4e7d7b4c363ee13d1055882a
Alba Rincón <alba.rincon@automattic.com>
2022-05-20 10:27:34 +0200

    Refactor: move the edit related code to the edit.jsx file

--- assets/js/blocks/products-by-attribute/block.tsx
+++ assets/js/blocks/products-by-attribute/block.tsx
@@ -1,65 +1,23 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { BlockControls } from '@wordpress/block-editor';
 import ServerSideRender from '@wordpress/server-side-render';
-import {
-	Disabled,
-	ToolbarGroup,
-	withSpokenMessages,
-} from '@wordpress/components';
+import { withSpokenMessages } from '@wordpress/components';
 import { gridBlockPreview } from '@woocommerce/resource-previews';
 
 /**
  * Internal dependencies
  */
 import { Props } from './types';
-import { ProductsByAttributeInspectorControls } from './inspector-controls';
-import { ProductsByAttributeEditMode } from './edit-mode';
 
-/**
- * Component to handle edit mode of "Products by Attribute".
- */
-const ProductsByAttributeBlock = ( props: Props ): JSX.Element => {
-	const { attributes, name, setAttributes } = props;
-	const { editMode } = attributes;
+const Block = ( props: Props ): JSX.Element => {
+	const { attributes, name } = props;
 
 	if ( attributes.isPreview ) {
 		return gridBlockPreview;
 	}
 
-	return (
-		<>
-			<BlockControls>
-				<ToolbarGroup
-					controls={ [
-						{
-							icon: 'edit',
-							title: __(
-								'Edit selected attribute',
-								'woo-gutenberg-products-block'
-							),
-							onClick: () =>
-								setAttributes( { editMode: ! editMode } ),
-							isActive: editMode,
-						},
-					] }
-				/>
-			</BlockControls>
-			<ProductsByAttributeInspectorControls { ...props } />
-			{ editMode ? (
-				<ProductsByAttributeEditMode { ...props } />
-			) : (
-				<Disabled>
-					<ServerSideRender
-						block={ name }
-						attributes={ attributes }
-					/>
-				</Disabled>
-			) }
-		</>
-	);
+	return <ServerSideRender block={ name } attributes={ attributes } />;
 };
 
-export default withSpokenMessages( ProductsByAttributeBlock );
+export const ProductsByAttributeBlock = withSpokenMessages( Block );
--- assets/js/blocks/products-by-attribute/edit.tsx
+++ assets/js/blocks/products-by-attribute/edit.tsx
@@ -1,20 +1,51 @@
 /**
  * External dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { BlockControls, useBlockProps } from '@wordpress/block-editor';
+import { Disabled, ToolbarGroup } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
-import Block from './block';
 import './editor.scss';
+import { Props } from './types';
+import { ProductsByAttributeInspectorControls } from './inspector-controls';
+import { ProductsByAttributeEditMode } from './edit-mode';
+import { ProductsByAttributeBlock } from './block';
 
-export const Edit = ( props: unknown ): JSX.Element => {
+export const Edit = ( props: Props ): JSX.Element => {
 	const blockProps = useBlockProps();
+
+	const { attributes, setAttributes } = props;
+	const { editMode } = attributes;
 
 	return (
 		<div { ...blockProps }>
-			<Block { ...props } />
+			<BlockControls>
+				<ToolbarGroup
+					controls={ [
+						{
+							icon: 'edit',
+							title: __(
+								'Edit selected attribute',
+								'woo-gutenberg-products-block'
+							),
+							onClick: () =>
+								setAttributes( { editMode: ! editMode } ),
+							isActive: editMode,
+						},
+					] }
+				/>
+			</BlockControls>
+			<ProductsByAttributeInspectorControls { ...props } />
+			{ editMode ? (
+				<ProductsByAttributeEditMode { ...props } />
+			) : (
+				<Disabled>
+					<ProductsByAttributeBlock { ...props } />
+				</Disabled>
+			) }
 		</div>
 	);
 };
