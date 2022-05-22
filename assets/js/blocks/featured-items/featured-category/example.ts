0626338654d78ad06533a68a5ca144eea60c4bb6
Lucio Giannotta <lucio.giannotta@a8c.com>
2022-05-23 00:51:44 +0300

    Add types to block examples

--- assets/js/blocks/featured-items/featured-category/example.ts
+++ assets/js/blocks/featured-items/featured-category/example.ts
@@ -2,10 +2,11 @@
  * External dependencies
  */
 import { previewCategories } from '@woocommerce/resource-previews';
+import { Block } from '@wordpress/blocks';
 
-export const example = {
+export const example: Block[ 'example' ] = {
 	attributes: {
 		categoryId: 'preview',
 		previewCategory: previewCategories[ 0 ],
 	},
-};
+} as const;
--- assets/js/blocks/featured-items/featured-product/example.ts
+++ assets/js/blocks/featured-items/featured-product/example.ts
@@ -2,10 +2,11 @@
  * External dependencies
  */
 import { previewProducts } from '@woocommerce/resource-previews';
+import { Block } from '@wordpress/blocks';
 
-export const example = {
+export const example: Block[ 'example' ] = {
 	attributes: {
 		productId: 'preview',
 		previewProduct: previewProducts[ 0 ],
 	},
-};
+} as const;
