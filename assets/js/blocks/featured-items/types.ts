c46794776ff32c69400cc933bd1ebe5cbfc92172
Lucio Giannotta <lucio.giannotta@a8c.com>
2022-05-23 00:30:18 +0300

    Add types to `withEditMode` 

--- assets/js/blocks/featured-items/types.ts
+++ assets/js/blocks/featured-items/types.ts
@@ -1,6 +1,18 @@
+/**
+ * External dependencies
+ */
+import { Block, BlockEditProps } from '@wordpress/blocks';
+
+export type EditorBlock< T > = Block< T > & BlockEditProps< T >;
+
 export interface Coordinates {
 	x: number;
 	y: number;
+}
+
+export interface GenericBlockUIConfig {
+	icon: JSX.Element;
+	label: string;
 }
 
 export type ImageFit = 'cover' | 'none';
