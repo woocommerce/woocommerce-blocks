08d538de1dd40637bbc476d5c200853ad77f458f
Nadir Seghir <nadir.seghir@gmail.com>
2022-05-25 16:40:14 +0100

    bail early if there is no build folder

--- src/BlockTypes/AbstractBlock.php
+++ src/BlockTypes/AbstractBlock.php
@@ -180,6 +180,9 @@ abstract class AbstractBlock {
 	protected function get_chunks_paths( $chunks_folder ) {
 		$build_path = \Automattic\WooCommerce\Blocks\Package::get_path() . 'build/';
 		$blocks     = [];
+		if ( ! is_dir( $build_path ) ) {
+			return [];
+		}
 		foreach ( new \RecursiveIteratorIterator( new \RecursiveDirectoryIterator( $build_path . $chunks_folder ) ) as $block_name ) {
 			$blocks[] = str_replace( $build_path, '', $block_name );
 		}
