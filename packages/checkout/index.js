f84faa038b54963a0c8cae16e088748d98afaa08
Thomas Roberts <thomas.roberts@automattic.com>
2022-06-06 16:56:01 +0100

    Removed unused `exposedSelectors` variable

--- packages/checkout/index.js
+++ packages/checkout/index.js
@@ -1,6 +1,5 @@
 export * from './components';
 export * from './utils';
-export { exposedSelectors } from './validation';
 export * from './slot';
 export * from './filter-registry';
 export * from './blocks-registry';
