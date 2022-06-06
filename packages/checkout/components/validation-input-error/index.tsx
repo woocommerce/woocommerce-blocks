e258a156355fd83a78720df3e98eb42c4103c644
Thomas Roberts <thomas.roberts@automattic.com>
2022-06-06 17:19:45 +0100

    convert ValidationInputError to TS and get correct id/error from store

--- packages/checkout/components/validation-input-error/index.tsx
+++ packages/checkout/components/validation-input-error/index.tsx
@@ -14,14 +14,17 @@ export const ValidationInputError = ( {
 	errorMessage = '',
 	propertyName = '',
 	elementId = '',
-} ) => {
-	//const { getValidationError, getValidationErrorId } = useValidationContext();
-	/* Note that I've short-circuited the function below to avoid needing the useValidationContext */
-	const getValidationErrorId = ( _elementId ) =>
-		`error- ${ _elementId }-${ propertyName }`;
-	const validationError = useSelect( ( select ) =>
-		select( VALIDATION_STORE_KEY ).getValidationError( propertyName )
+} ): JSX.Element | null => {
+	const { getValidationError, getValidationErrorId } = useSelect(
+		( select ) => {
+			const store = select( VALIDATION_STORE_KEY );
+			return {
+				getValidationError: store.getValidationError(),
+				getValidationErrorId: store.getValidationErrorId(),
+			};
+		}
 	);
+	const validationError = getValidationError( propertyName );
 
 	if ( ! errorMessage || typeof errorMessage !== 'string' ) {
 		const error = validationError || {};
