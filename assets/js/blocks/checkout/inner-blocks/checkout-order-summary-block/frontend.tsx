fcb17d2046c1bedd2a383ae144154abbfdd58562
Alex Florisca <alex.florisca@automattic.com>
2022-06-10 17:53:14 +0300

    remove temp buttons

--- assets/js/blocks/checkout/inner-blocks/checkout-order-summary-block/frontend.tsx
+++ assets/js/blocks/checkout/inner-blocks/checkout-order-summary-block/frontend.tsx
@@ -4,9 +4,6 @@
 import { TotalsFooterItem } from '@woocommerce/base-components/cart-checkout';
 import { getCurrencyFromPriceResponse } from '@woocommerce/price-format';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
-import { useCheckoutEventsContext } from '@woocommerce/base-context';
-import { dispatch } from '@wordpress/data';
-import { CHECKOUT_STORE_KEY } from '@woocommerce/block-data';
 
 /**
  * Internal dependencies
@@ -22,11 +19,6 @@ const FrontendBlock = ( {
 } ): JSX.Element | null => {
 	const { cartTotals } = useStoreCart();
 	const totalsCurrency = getCurrencyFromPriceResponse( cartTotals );
-	const {
-		onCheckoutValidationBeforeProcessing,
-		onCheckoutAfterProcessingWithSuccess,
-		onCheckoutAfterProcessingWithError,
-	} = useCheckoutEventsContext();
 
 	return (
 		<div className={ className }>
@@ -36,88 +28,6 @@ const FrontendBlock = ( {
 					currency={ totalsCurrency }
 					values={ cartTotals }
 				/>
-			</div>
-			<div>
-				<button
-					onClick={ () => {
-						onCheckoutValidationBeforeProcessing( () => ( {
-							type: 'error',
-							errorMessage:
-								'onCheckoutValidationBeforeProcessing error',
-							validationErrors: 'nothing',
-						} ) );
-					} }
-				>
-					Register an validation observer
-				</button>
-				<button
-					onClick={ () => {
-						onCheckoutAfterProcessingWithSuccess( () => ( {
-							type: 'success',
-						} ) );
-					} }
-				>
-					Register success observer for
-					CHECKOUT_AFTER_PROCESSING_WITH_SUCCESS
-				</button>
-
-				<button
-					onClick={ () => {
-						onCheckoutAfterProcessingWithSuccess( () => ( {
-							type: 'error',
-							message:
-								'onCheckoutAfterProcessingWithSuccess error',
-							messageContext: 'wc/checkout',
-						} ) );
-					} }
-				>
-					Register error observer for
-					CHECKOUT_AFTER_PROCESSING_WITH_SUCCESS
-				</button>
-
-				<button
-					onClick={ () => {
-						onCheckoutAfterProcessingWithSuccess( () => ( {
-							type: 'error',
-						} ) );
-					} }
-				>
-					Register error observer without message for
-					CHECKOUT_AFTER_PROCESSING_WITH_SUCCESS
-				</button>
-
-				<button
-					onClick={ () => {
-						onCheckoutAfterProcessingWithError( () => ( {
-							type: 'error',
-							message: 'onCheckoutAfterProcessingWithError error',
-							messageContext: 'wc/checkout',
-						} ) );
-					} }
-				>
-					Register error observer for
-					CHECKOUT_AFTER_PROCESSING_WITH_ERROR
-				</button>
-
-				<button
-					onClick={ () => {
-						onCheckoutAfterProcessingWithError( () => ( {
-							type: 'success',
-						} ) );
-					} }
-				>
-					Register success observer for
-					CHECKOUT_AFTER_PROCESSING_WITH_ERROR
-				</button>
-
-				<button
-					onClick={ () => {
-						const { setHasError } = dispatch( CHECKOUT_STORE_KEY );
-						setHasError( true );
-					} }
-				>
-					Set state error
-				</button>
 			</div>
 			<OrderMetaSlotFill />
 		</div>
