f214d347477606233450d6f0a81e4dde78037423
Niels Lange <info@nielslange.de>
2022-06-13 14:37:05 +0200

    Create Cross-Sells and Cross-Sells Products inner blocks

---
+++ assets/js/blocks/cart/inner-blocks/cart-cross-sells-block/edit.tsx
@@ -0,0 +1,45 @@
+/**
+ * External dependencies
+ */
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import type { TemplateArray } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+export const Edit = (): JSX.Element => {
+	const blockProps = useBlockProps( {
+		className: 'wc-block-cart__cross-sells',
+	} );
+
+	const defaultTemplate = [
+		[
+			'core/heading',
+			{
+				content: __(
+					'You may be interested in…',
+					'woo-gutenberg-products-block'
+				),
+				level: 3,
+			},
+			,
+			[],
+		],
+		[ 'woocommerce/cart-cross-sells-products-block', {}, [] ],
+	] as TemplateArray;
+
+	return (
+		<div { ...blockProps }>
+			<InnerBlocks template={ defaultTemplate } templateLock={ false } />
+		</div>
+	);
+};
+
+export const Save = (): JSX.Element => {
+	return (
+		<div { ...useBlockProps.save() }>
+			<InnerBlocks.Content />
+		</div>
+	);
+};
---
+++ assets/js/blocks/cart/inner-blocks/cart-cross-sells-products/block.tsx
@@ -0,0 +1,10 @@
+const Block = (): JSX.Element => {
+	const divStyle = {
+		border: '1px solid',
+		padding: '5px 10px',
+		textAlign: 'center',
+	};
+	return <div style={ divStyle }>CROSS SELLS PRODUCTS</div>;
+};
+
+export default Block;
