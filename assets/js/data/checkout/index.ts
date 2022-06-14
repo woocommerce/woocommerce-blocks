e1a0a9c3833be849089e15f5f66e2f0be7eb3a0f
Thomas Roberts <thomas.roberts@automattic.com>
2022-06-14 14:52:02 +0100

    Augment @wordpress/data package with our new store types

--- assets/js/data/checkout/index.ts
+++ assets/js/data/checkout/index.ts
@@ -10,14 +10,23 @@ import { STORE_KEY } from './constants';
 import * as selectors from './selectors';
 import * as actions from './actions';
 import reducer from './reducers';
+import { DispatchFromMap, SelectFromMap } from '../mapped-types';
 
-export const config = {
+registerStore( STORE_KEY, {
 	reducer,
 	selectors,
 	actions,
 	__experimentalUseThunks: true,
-};
-
-registerStore( STORE_KEY, config );
+} );
 
 export const CHECKOUT_STORE_KEY = STORE_KEY;
+declare module '@wordpress/data' {
+	function dispatch(
+		key: typeof CHECKOUT_STORE_KEY
+	): DispatchFromMap< typeof actions >;
+	function select(
+		key: typeof CHECKOUT_STORE_KEY
+	): SelectFromMap< typeof selectors > & {
+		hasFinishedResolution: ( selector: string ) => boolean;
+	};
+}
