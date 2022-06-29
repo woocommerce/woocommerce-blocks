371a300ce5199e64432854f583159ade26d255cd
Luigi <gigitux@gmail.com>
2022-06-29 11:45:36 +0200

    improve conf

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	...require( '@wordpress/scripts/config/jest-e2e.config' ),
 	rootDir: '../../../',
+	maxConcurrency: 1,
+	maxWorkers: 1,
 	// Automatically clear mock calls and instances between every test
 	clearMocks: true,
 
