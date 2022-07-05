47379270586de9d409200885883ec035dd4aceed
Niels Lange <info@nielslange.de>
2022-07-05 17:54:39 +0200

    Adding /vendor directory to release

---
+++ vendor/composer/installed.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-trunk',
+    'version' => 'dev-trunk',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5cc9182a7c1e8991839ea1600607c242e6738991',
+    'name' => 'woocommerce/woocommerce-blocks',
+  ),
+  'versions' => 
+  array (
+    'automattic/jetpack-autoloader' => 
+    array (
+      'pretty_version' => 'v2.10.7',
+      'version' => '2.10.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e3dfa1a2679e4b8e380bc37b722081875c84a381',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.12.0',
+      'version' => '1.12.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd20a64ed3c94748397ff5973488761b22f6d3f19',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'woocommerce/woocommerce-blocks' => 
+    array (
+      'pretty_version' => 'dev-trunk',
+      'version' => 'dev-trunk',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5cc9182a7c1e8991839ea1600607c242e6738991',
+    ),
+  ),
+);
