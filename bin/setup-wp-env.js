793d40304ed2e889cd2cfd511e7274bd030be5a7
Luigi <gigitux@gmail.com>
2022-07-15 12:06:20 +0200

    try now

--- bin/setup-wp-env.js
+++ bin/setup-wp-env.js
@@ -5,24 +5,19 @@ const wpEnvRaw = fs.readFileSync(
 	path.join( __dirname, '..', '.wp-env.json' )
 );
 
+const wpEnv = JSON.parse( wpEnvRaw );
+
 if ( process.env.GUTENBERG_EDITOR_CONTEXT ) {
-	const wpEnv = JSON.parse( wpEnvRaw );
 	wpEnv.plugins.push(
 		'https://downloads.wordpress.org/plugin/gutenberg.latest-stable.zip'
-	);
-	// We write the new file to .wp-env.override.json (https://developer.wordpress.org/block-editor/reference-guides/packages/packages-env/#wp-env-override-json)
-	fs.writeFileSync(
-		path.join( __dirname, '..', '.wp-env.override.json' ),
-		JSON.stringify( wpEnv )
 	);
 }
 
 if ( process.env.WORDPRESS_VERSION ) {
-	const wpEnv = JSON.parse( wpEnvRaw );
 	wpEnv.core = `WordPress/WordPress#${ process.env.WORDPRESS_VERSION }`;
-	// We write the new file to .wp-env.override.json (https://developer.wordpress.org/block-editor/reference-guides/packages/packages-env/#wp-env-override-json)
-	fs.writeFileSync(
-		path.join( __dirname, '..', '.wp-env.override.json' ),
-		JSON.stringify( wpEnv )
-	);
 }
+
+fs.writeFileSync(
+	path.join( __dirname, '..', '.wp-env.override.json' ),
+	JSON.stringify( wpEnv )
+);
