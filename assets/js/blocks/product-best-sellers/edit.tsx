e054b3bf6f25ba5273e1e15489d1e4ff0730a974
Alba Rincón <alba.rincon@automattic.com>
2022-07-19 14:57:53 +0200

    Import props

--- assets/js/blocks/product-best-sellers/edit.tsx
+++ assets/js/blocks/product-best-sellers/edit.tsx
@@ -7,6 +7,7 @@ import { useBlockProps } from '@wordpress/block-editor';
  * Internal dependencies
  */
 import { ProductBestSellersBlock } from './block';
+import { Props } from './types';
 
 export const Edit = ( props: unknown & Props ): JSX.Element => {
 	const blockProps = useBlockProps();
