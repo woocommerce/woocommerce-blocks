713b83564c6a86d33fb590e0181d89e008184d37
Saad Tarhi <saad.trh@gmail.com>
2022-07-22 19:47:04 +0100

    Fix registering the cart taxes inner block issue

--- assets/js/blocks/cart/inner-blocks/cart-order-summary-taxes/index.tsx
+++ assets/js/blocks/cart/inner-blocks/cart-order-summary-taxes/index.tsx
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { totals } from '@woocommerce/icons';
 import { Icon } from '@wordpress/icons';
 import { registerFeaturePluginBlockType } from '@woocommerce/block-settings';
@@ -12,7 +13,9 @@ import { Edit, Save } from './edit';
 import attributes from './attributes';
 import metadata from './block.json';
 
-registerFeaturePluginBlockType( metadata, {
+registerFeaturePluginBlockType( 'woocommerce/cart-order-summary-taxes-block', {
+	// When a block is registered server side, we should add the block metadata in the settings.
+	...metadata,
 	icon: {
 		src: (
 			<Icon
@@ -21,7 +24,20 @@ registerFeaturePluginBlockType( metadata, {
 			/>
 		),
 	},
-	attributes,
+	attributes: {
+		...metadata.attributes,
+		...attributes,
+	},
+	title: __( 'Taxes', 'woo-gutenberg-products-block' ),
+	description: __(
+		'Shows the cart taxes row.',
+		'woo-gutenberg-products-block'
+	),
+	// description: _x(
+	// 	'Shows the cart taxes row.',
+	// 	'block description',
+	// 	'woo-gutenberg-products-block'
+	// ),
 	edit: Edit,
 	save: Save,
 } );
--- bin/webpack-entries.js
+++ bin/webpack-entries.js
@@ -45,6 +45,9 @@ const blocks = {
 	'stock-filter': {},
 	'active-filters': {},
 	cart: {},
+	'cart-order-summary-taxes-block': {
+		customDir: 'cart/inner-blocks/cart-order-summary-taxes',
+	},
 	checkout: {},
 	'mini-cart': {},
 	'mini-cart-contents': {
