1c4e08fa5f9fc717da2ff11a65c8c55602af97b7
Thomas Roberts <thomas.roberts@automattic.com>
2022-08-04 15:30:27 +0100

    Include CartCheckoutSidebarCompatibilityNotice in checkout terms block

--- assets/js/blocks/checkout/inner-blocks/checkout-terms-block/edit.tsx
+++ assets/js/blocks/checkout/inner-blocks/checkout-terms-block/edit.tsx
@@ -12,6 +12,7 @@ import { PanelBody, ToggleControl, Notice } from '@wordpress/components';
 import { PRIVACY_URL, TERMS_URL } from '@woocommerce/block-settings';
 import { ADMIN_URL } from '@woocommerce/settings';
 import { Icon, external } from '@wordpress/icons';
+import CartCheckoutSidebarCompatibilityNotice from '@woocommerce/editor-components/sidebar-compatibility-notice';
 
 /**
  * Internal dependencies
@@ -55,6 +56,7 @@ export const Edit = ( {
 						}
 					/>
 				</PanelBody>
+				<CartCheckoutSidebarCompatibilityNotice />
 			</InspectorControls>
 			<div className="wc-block-checkout__terms">
 				{ checkbox ? (
