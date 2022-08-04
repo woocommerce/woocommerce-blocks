dd5b5aef47f125a9d7d32ab42450d659f4f86661
Thomas Roberts <thomas.roberts@automattic.com>
2022-08-04 17:11:21 +0100

    Add CartSidebarCompatibilityNotice to cart coupons block

--- assets/js/blocks/cart/inner-blocks/cart-order-summary-coupon-form/edit.tsx
+++ assets/js/blocks/cart/inner-blocks/cart-order-summary-coupon-form/edit.tsx
@@ -1,8 +1,9 @@
 /**
  * External dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import Noninteractive from '@woocommerce/base-components/noninteractive';
+import { CartSidebarCompatibilityNotice } from '@woocommerce/editor-components/sidebar-compatibility-notice';
 
 /**
  * Internal dependencies
@@ -21,6 +22,9 @@ export const Edit = ( {
 	const blockProps = useBlockProps();
 	return (
 		<div { ...blockProps }>
+			<InspectorControls>
+				<CartSidebarCompatibilityNotice />
+			</InspectorControls>
 			<Noninteractive>
 				<Block className={ className } />
 			</Noninteractive>
