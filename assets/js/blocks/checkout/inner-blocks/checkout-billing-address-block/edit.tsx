16215db08b439147f4c1488053728141609ffc4a
Thomas Roberts <thomas.roberts@automattic.com>
2022-08-05 14:39:37 +0100

    Include CheckoutSidebarCompatibilityNotice in billing address

--- assets/js/blocks/checkout/inner-blocks/checkout-billing-address-block/edit.tsx
+++ assets/js/blocks/checkout/inner-blocks/checkout-billing-address-block/edit.tsx
@@ -2,9 +2,10 @@
  * External dependencies
  */
 import classnames from 'classnames';
-import { useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { useCheckoutAddress } from '@woocommerce/base-context/hooks';
 import { innerBlockAreas } from '@woocommerce/blocks-checkout';
+import { CheckoutSidebarCompatibilityNotice } from '@woocommerce/editor-components/sidebar-compatibility-notice';
 
 /**
  * Internal dependencies
@@ -44,7 +45,11 @@ export const Edit = ( {
 	const { showBillingFields } = useCheckoutAddress();
 
 	if ( ! showBillingFields ) {
-		return null;
+		return (
+			<InspectorControls>
+				<CheckoutSidebarCompatibilityNotice />
+			</InspectorControls>
+		);
 	}
 
 	return (
