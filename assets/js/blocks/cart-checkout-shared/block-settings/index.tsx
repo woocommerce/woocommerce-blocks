3be49fcc2d069092a04d97f4e143397904bbe62f
Thomas Roberts <thomas.roberts@automattic.com>
2022-08-08 16:17:53 +0100

    Create sidebar compatibility notice hoc

---
+++ assets/js/blocks/cart-checkout-shared/block-settings/index.tsx
@@ -0,0 +1,76 @@
+/**
+ * External dependencies
+ */
+import { InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, ToggleControl } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { CartCheckoutFeedbackPrompt } from '@woocommerce/editor-components/feedback-prompt';
+import { addFilter, hasFilter } from '@wordpress/hooks';
+import { createHigherOrderComponent } from '@wordpress/compose';
+
+/**
+ * Internal dependencies
+ */
+import { DefaultNotice } from '../default-notice';
+
+const BlockSettings = ( { attributes, setAttributes } ) => {
+	const { hasDarkControls } = attributes;
+	return (
+		<InspectorControls>
+			<DefaultNotice page="cart" />
+			<PanelBody title={ __( 'Style', 'woo-gutenberg-products-block' ) }>
+				<ToggleControl
+					label={ __(
+						'Dark mode inputs',
+						'woo-gutenberg-products-block'
+					) }
+					help={ __(
+						'Inputs styled specifically for use on dark background colors.',
+						'woo-gutenberg-products-block'
+					) }
+					checked={ hasDarkControls }
+					onChange={ () =>
+						setAttributes( {
+							hasDarkControls: ! hasDarkControls,
+						} )
+					}
+				/>
+			</PanelBody>
+			<CartCheckoutFeedbackPrompt />
+		</InspectorControls>
+	);
+};
+
+const withBlockSettings = createHigherOrderComponent(
+	( BlockEdit ) => ( props ) => {
+		const isCartOrCheckout =
+			props.name === 'woocommerce/checkout' ||
+			props.name === '@woocommerce/cart';
+		return (
+			<>
+				{ isCartOrCheckout && (
+					<InspectorControls>
+						<BlockSettings { ...props } />
+					</InspectorControls>
+				) }
+
+				<BlockEdit { ...props } />
+			</>
+		);
+	},
+	'withBlockSettings'
+);
+
+if (
+	! hasFilter(
+		'editor.BlockEdit',
+		'woocommerce/add/cart-checkout-block-settings'
+	)
+) {
+	addFilter(
+		'editor.BlockEdit',
+		'woocommerce/add/cart-checkout-block-settings',
+		withBlockSettings,
+		20
+	);
+}
