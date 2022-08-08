6cb78b0031d02a9d2fa827bb5e30eb56102b719e
Tung Du <dinhtungdu@gmail.com>
2022-08-08 13:00:58 +0700

    only test against wp latest

--- tests/e2e/specs/backend/product-search.test.js
+++ tests/e2e/specs/backend/product-search.test.js
@@ -10,6 +10,7 @@ import { clearAndFillInput } from '@woocommerce/e2e-utils';
 import {
 	findLabelWithText,
 	visitBlockPage,
+	GUTENBERG_EDITOR_CONTEXT,
 } from '@woocommerce/blocks-test-utils';
 
 const block = {
@@ -18,46 +19,51 @@ const block = {
 	class: '.wc-block-product-search',
 };
 
-describe( `${ block.name } Block`, () => {
-	beforeAll( async () => {
-		await switchUserToAdmin();
-		await visitBlockPage( `${ block.name } Block` );
+if ( GUTENBERG_EDITOR_CONTEXT === 'gutenberg' ) {
+	// eslint-disable-next-line jest/no-focused-tests, jest/expect-expect
+	test.only( `Skipping ${ block.name } tests`, () => {} );
+} else {
+	describe( `${ block.name } Block`, () => {
+		beforeAll( async () => {
+			await switchUserToAdmin();
+			await visitBlockPage( `${ block.name } Block` );
+		} );
+
+		it( 'renders without crashing', async () => {
+			await expect( page ).toRenderBlock( block );
+		} );
+
+		it( 'can toggle field label', async () => {
+			await openDocumentSettingsSidebar();
+			await page.click( block.class );
+			const selector = `${ block.class } .wc-block-product-search__label`;
+			const toggleLabel = await findLabelWithText(
+				'Show search field label'
+			);
+			await expect( toggleLabel ).toToggleElement( selector );
+		} );
+
+		it( 'can change field labels in editor', async () => {
+			await expect( page ).toFill(
+				'textarea.wc-block-product-search__label',
+				'I am a new label'
+			);
+
+			await expect( page ).toFill(
+				'.wc-block-product-search__field input',
+				'I am a new placeholder'
+			);
+
+			await clearAndFillInput(
+				'textarea.wc-block-product-search__label',
+				'The Label'
+			);
+			await clearAndFillInput(
+				'.wc-block-product-search__field input',
+				'The Placeholder'
+			);
+
+			expect( await getEditedPostContent() ).toMatchSnapshot();
+		} );
 	} );
-
-	it( 'renders without crashing', async () => {
-		await expect( page ).toRenderBlock( block );
-	} );
-
-	it( 'can toggle field label', async () => {
-		await openDocumentSettingsSidebar();
-		await page.click( block.class );
-		const selector = `${ block.class } .wc-block-product-search__label`;
-		const toggleLabel = await findLabelWithText(
-			'Show search field label'
-		);
-		await expect( toggleLabel ).toToggleElement( selector );
-	} );
-
-	it( 'can change field labels in editor', async () => {
-		await expect( page ).toFill(
-			'textarea.wc-block-product-search__label',
-			'I am a new label'
-		);
-
-		await expect( page ).toFill(
-			'.wc-block-product-search__field input',
-			'I am a new placeholder'
-		);
-
-		await clearAndFillInput(
-			'textarea.wc-block-product-search__label',
-			'The Label'
-		);
-		await clearAndFillInput(
-			'.wc-block-product-search__field input',
-			'The Placeholder'
-		);
-
-		expect( await getEditedPostContent() ).toMatchSnapshot();
-	} );
-} );
+}
