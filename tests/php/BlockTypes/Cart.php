fcd89373dbb81089b1d4b0d7ec7d4190672ecba8
Darren Ethier <darren@roughsmootheng.in>
2022-08-12 13:31:17 -0400

    fix tests

--- tests/php/BlockTypes/Cart.php
+++ tests/php/BlockTypes/Cart.php
@@ -16,17 +16,17 @@ class Cart extends \WP_UnitTestCase {
 	}
 
 	public function test_deep_sort_with_accents() {
-		$test_array_1 = [
+		$test_array_1 = array(
 			'0',
 			'1',
-			[ '2', '3' ]
-		];
-		$test_array_2 = [
-			[ '0', '1' ],
+			array( '2', '3' ),
+		);
+		$test_array_2 = array(
+			array( '0', '1' ),
 			'2',
 			'3',
-		];
-		$this->assertEquals( $test_array_1, $this->cart_block_instance->deep_sort_test( $test_array_1 ) );
-		$this->assertEquals( $test_array_2, $this->cart_block_instance->deep_sort_test( $test_array_2 ) );
+		);
+		$this->assertEquals( $test_array_1, $this->cart_block_instance->deep_sort_test( $test_array_1 ), '' );
+		$this->assertEquals( $test_array_2, $this->cart_block_instance->deep_sort_test( $test_array_2 ), '' );
 	}
 }
--- tests/php/mocks/CartMock.php
+++ tests/php/mocks/CartMock.php
@@ -2,8 +2,20 @@
 namespace Automattic\WooCommerce\Blocks\Tests\Mocks;
 
 use Automattic\WooCommerce\Blocks\BlockTypes\Cart;
+use Automattic\WooCommerce\Blocks\Package;
+use Automattic\WooCommerce\Blocks\Assets\Api;
+use Automattic\WooCommerce\Blocks\Assets\AssetDataRegistry;
+use Automattic\WooCommerce\Blocks\Integrations\IntegrationRegistry
 
 class CartMock extends Cart {
+
+	public function __construct() {
+		parent::__construct(
+			Package::container()->get( API::class ),
+			Package::container()->get( AssetDataRegistry::class ),
+			Package::container()->get( IntegrationRegistry::class ),
+		);
+	}
 
 	/**
 	 * For now don't need to initialize anything in tests so let's
