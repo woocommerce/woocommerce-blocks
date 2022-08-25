907ef8ae544c6ee4bcfc4da388c90639cf989e2a
Daniel Dudzic <daniel.dudzic@automattic.com>
2022-08-25 16:16:40 +0200

    Fix block with the latest repo changes

--- assets/js/blocks/rating-filter/edit.tsx
+++ assets/js/blocks/rating-filter/edit.tsx
@@ -3,8 +3,14 @@
  */
 import { __ } from '@wordpress/i18n';
 import classnames from 'classnames';
-import { useBlockProps } from '@wordpress/block-editor';
-import { Disabled, withSpokenMessages } from '@wordpress/components';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import {
+	Disabled,
+	PanelBody,
+	ToggleControl,
+	withSpokenMessages,
+} from '@wordpress/components';
+import HeadingToolbar from '@woocommerce/editor-components/heading-toolbar';
 import BlockTitle from '@woocommerce/editor-components/block-title';
 import type { BlockEditProps } from '@wordpress/blocks';
 
@@ -19,19 +25,53 @@ const Edit = ( {
 	attributes,
 	setAttributes,
 }: BlockEditProps< Attributes > ) => {
-	const { className, heading, headingLevel, showCounts, showFilterButton } =
-		attributes;
+	const { className, heading, headingLevel, showCounts } = attributes;
 
 	const blockProps = useBlockProps( {
-		className: classnames( 'wc-block-rating-filter', className ),
+		className: classnames( 'wc-block-stock-filter', className ),
 	} );
+
+	const getInspectorControls = () => {
+		return (
+			<InspectorControls key="inspector">
+				<PanelBody
+					title={ __( 'Content', 'woo-gutenberg-products-block' ) }
+				>
+					<ToggleControl
+						label={ __(
+							'Product count',
+							'woo-gutenberg-products-block'
+						) }
+						help={
+							showCounts
+								? __(
+										'Product count is visible.',
+										'woo-gutenberg-products-block'
+								  )
+								: __(
+										'Product count is hidden.',
+										'woo-gutenberg-products-block'
+								  )
+						}
+						checked={ showCounts }
+						onChange={ () =>
+							setAttributes( {
+								showCounts: ! showCounts,
+							} )
+						}
+					/>
+				</PanelBody>
+			</InspectorControls>
+		);
+	};
 
 	return (
 		<>
+			{ getInspectorControls() }
 			{
 				<div { ...blockProps }>
 					<BlockTitle
-						className="wc-block-rating-filter__title"
+						className="wc-block-stock-filter__title"
 						headingLevel={ headingLevel }
 						heading={ heading }
 						onChange={ ( value: string ) =>
