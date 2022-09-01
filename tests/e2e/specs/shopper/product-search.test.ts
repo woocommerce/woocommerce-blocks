9154dd6557e66168985292a8fa8ce51411a99a69
Tung Du <dinhtungdu@gmail.com>
2022-09-01 17:29:53 +0700

    prevent flaky test caused by expect extend

--- tests/e2e/specs/shopper/product-search.test.ts
+++ tests/e2e/specs/shopper/product-search.test.ts
@@ -3,43 +3,41 @@
  */
 import { GUTENBERG_EDITOR_CONTEXT, describeOrSkip } from '../../utils';
 import { shopper } from '../../../utils';
-
-const block = {
-	name: 'Product Search',
-	slug: 'core/search',
-	class: '.wp-block-search',
-};
+import { getTextContent } from '../../page-utils';
 
 describeOrSkip( GUTENBERG_EDITOR_CONTEXT === 'gutenberg' )(
 	`Shopper → Product Search`,
 	() => {
 		beforeEach( async () => {
-			await shopper.block.goToBlockPage( block.name );
+			await shopper.block.goToBlockPage( 'Product Search' );
+			await page.waitForSelector( '.wp-block-search' );
 		} );
 
 		it( 'should render product variation', async () => {
-			await expect( page ).toMatchElement(
-				'input[name="post_type"][value="product"]'
+			const [ postType ] = await getTextContent(
+				'.wp-block-search input[name="post_type"]'
 			);
+			await expect( postType ).toBe( 'product' );
 		} );
 
 		it( 'should be able to search for products', async () => {
 			await page.type( '.wp-block-search input[name="s"]', 'Stick' );
 
 			await Promise.all( [
-				page.keyboard.press( 'Enter' ),
 				page.waitForNavigation(),
+				page.keyboard.press( 'Enter' ),
 			] );
-
-			await expect( page ).toMatchElement( 'ul.products.columns-3' );
 
 			const products = await page.$$( 'ul.products.columns-3 > li' );
 
 			expect( products ).toHaveLength( 2 );
 
-			expect( page ).toMatchElement( 'ul.products.columns-3 > li', {
-				text: '128GB USB Stick',
-			} );
+			const productTitles = await getTextContent(
+				'ul.products.columns-3 .woocommerce-loop-product__title'
+			);
+
+			expect( productTitles ).toContain( '32GB USB Stick' );
+			expect( productTitles ).toContain( '128GB USB Stick' );
 		} );
 	}
 );
