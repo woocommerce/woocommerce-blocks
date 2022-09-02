bb6973ab3b0c6e97d4341f3f1c1e9abd4e56a5a5
Tung Du <dinhtungdu@gmail.com>
2022-09-02 15:07:50 +0700

    update pattern

--- patterns/filters.php
+++ patterns/filters.php
@@ -35,6 +35,14 @@
 <div class="wp-block-woocommerce-attribute-filter is-loading" data-attribute-id="0" data-show-counts="true" data-query-type="or" data-heading="Filter by attribute" data-heading-level="3"><span aria-hidden="true" class="wc-block-product-attribute-filter__placeholder"></span></div>
 <!-- /wp:woocommerce/attribute-filter -->
 
-<!-- wp:woocommerce/stock-filter -->
-<div class="wp-block-woocommerce-stock-filter is-loading" data-show-counts="true" data-heading="Filter by stock status" data-heading-level="3"><span aria-hidden="true" class="wc-block-product-stock-filter__placeholder"></span></div>
+<!-- wp:woocommerce/filter-wrapper {"filterType":"stock-filter","heading":""} -->
+<div class="wp-block-woocommerce-filter-wrapper">
+<!-- wp:heading {"level":3} -->
+<h3>Filter by Stock</h3>
+<!-- /wp:heading -->
+
+<!-- wp:woocommerce/stock-filter {"heading":""} -->
+<div class="wp-block-woocommerce-stock-filter is-loading" data-show-counts="true" data-heading="" data-heading-level="3"><span aria-hidden="true" class="wc-block-product-stock-filter__placeholder"></span></div>
 <!-- /wp:woocommerce/stock-filter -->
+</div>
+<!-- /wp:woocommerce/filter-wrapper -->
