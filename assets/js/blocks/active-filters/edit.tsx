3ba7237ea898250340f69976811a750bf2036114
Tung Du <dinhtungdu@gmail.com>
2022-09-02 11:30:35 +0700

    try: upgrade button

--- assets/js/blocks/active-filters/edit.tsx
+++ assets/js/blocks/active-filters/edit.tsx
@@ -2,13 +2,20 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import {
+	InspectorControls,
+	useBlockProps,
+	BlockControls,
+} from '@wordpress/block-editor';
+import { BlockEditProps, createBlock } from '@wordpress/blocks';
+import { useDispatch } from '@wordpress/data';
 import BlockTitle from '@woocommerce/editor-components/block-title';
-import type { BlockEditProps } from '@wordpress/blocks';
 import {
 	Disabled,
 	PanelBody,
 	withSpokenMessages,
+	ToolbarGroup,
+	ToolbarButton,
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
 	__experimentalToggleGroupControl as ToggleGroupControl,
 	// eslint-disable-next-line @wordpress/no-unsafe-wp-apis
@@ -25,6 +32,7 @@ import './editor.scss';
 const Edit = ( {
 	attributes,
 	setAttributes,
+	clientId,
 }: BlockEditProps< Attributes > ) => {
 	const { className, displayStyle, heading, headingLevel } = attributes;
 
@@ -32,45 +40,83 @@ const Edit = ( {
 		className,
 	} );
 
+	const { replaceBlock } = useDispatch( 'core/block-editor' );
+
 	const getInspectorControls = () => {
 		return (
-			<InspectorControls key="inspector">
-				<PanelBody
-					title={ __(
-						'Display Settings',
-						'woo-gutenberg-products-block'
-					) }
-				>
-					<ToggleGroupControl
-						label={ __(
-							'Display Style',
+			<>
+				<InspectorControls key="inspector">
+					<PanelBody
+						title={ __(
+							'Display Settings',
 							'woo-gutenberg-products-block'
 						) }
-						value={ displayStyle }
-						onChange={ ( value: Attributes[ 'displayStyle' ] ) =>
-							setAttributes( {
-								displayStyle: value,
-							} )
-						}
-						className="wc-block-active-filter__style-toggle"
 					>
-						<ToggleGroupControlOption
-							value="list"
+						<ToggleGroupControl
 							label={ __(
-								'List',
+								'Display Style',
 								'woo-gutenberg-products-block'
 							) }
-						/>
-						<ToggleGroupControlOption
-							value="chips"
-							label={ __(
-								'Chips',
-								'woo-gutenberg-products-block'
-							) }
-						/>
-					</ToggleGroupControl>
-				</PanelBody>
-			</InspectorControls>
+							value={ displayStyle }
+							onChange={ (
+								value: Attributes[ 'displayStyle' ]
+							) =>
+								setAttributes( {
+									displayStyle: value,
+								} )
+							}
+							className="wc-block-active-filter__style-toggle"
+						>
+							<ToggleGroupControlOption
+								value="list"
+								label={ __(
+									'List',
+									'woo-gutenberg-products-block'
+								) }
+							/>
+							<ToggleGroupControlOption
+								value="chips"
+								label={ __(
+									'Chips',
+									'woo-gutenberg-products-block'
+								) }
+							/>
+						</ToggleGroupControl>
+					</PanelBody>
+				</InspectorControls>
+				{ heading && (
+					<BlockControls>
+						<ToolbarGroup>
+							<ToolbarButton
+								text={ __(
+									'Upgrade block',
+									'woo-gutenberg-products-block'
+								) }
+								showTooltip={ true }
+								label={ __(
+									'We have improved this block to make the styling easier. Upgrade to the new block to get started.',
+									'woo-gutenberg-products-block'
+								) }
+								onClick={ () => {
+									replaceBlock(
+										clientId,
+										createBlock(
+											'woocommerce/filter-wrapper',
+											{
+												heading,
+												filterType: 'active-filters',
+											}
+										)
+									);
+									setAttributes( {
+										heading: '',
+									} );
+								} }
+							/>
+						</ToolbarGroup>
+					</BlockControls>
+				) }
+			</>
 		);
 	};
 
