89e93c6fada9758a38e56f37f1dbefdf8aa38273
Daniel Dudzic <daniel.dudzic@automattic.com>
2022-09-08 00:32:36 +0200

    Add an experimental build gate and update block title and description

--- assets/js/blocks/rating-filter/index.tsx
+++ assets/js/blocks/rating-filter/index.tsx
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import { isExperimentalBuild } from '@woocommerce/block-settings';
 import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
 import { Icon, starEmpty } from '@wordpress/icons';
@@ -15,44 +16,46 @@ import metadata from './block.json';
 import { blockAttributes } from './attributes';
 import type { Attributes } from './types';
 
-registerBlockType( metadata, {
-	title: __( 'Filter Products by Rating', 'woo-gutenberg-products-block' ),
-	description: __(
-		'Allow customers to filter the grid by products rating. Works in combination with the All Products block.',
-		'woo-gutenberg-products-block'
-	),
-	icon: {
-		src: (
-			<Icon
-				icon={ starEmpty }
-				className="wc-block-editor-components-block-icon"
-			/>
+if ( isExperimentalBuild() ) {
+	registerBlockType( metadata, {
+		title: __( 'Filter by Rating', 'woo-gutenberg-products-block' ),
+		description: __(
+			'Enable customers to filter the product grid by rating.',
+			'woo-gutenberg-products-block'
 		),
-	},
-	attributes: {
-		...metadata.attributes,
-		...blockAttributes,
-	},
-	edit,
-	// Save the props to post content.
-	save( { attributes }: { attributes: Attributes } ) {
-		const { className, heading, headingLevel } = attributes;
-		const data: Record< string, unknown > = {
-			'data-heading': heading,
-			'data-heading-level': headingLevel,
-		};
-		return (
-			<div
-				{ ...useBlockProps.save( {
-					className: classNames( 'is-loading', className ),
-				} ) }
-				{ ...data }
-			>
-				<span
-					aria-hidden
-					className="wc-block-product-rating-filter__placeholder"
+		icon: {
+			src: (
+				<Icon
+					icon={ starEmpty }
+					className="wc-block-editor-components-block-icon"
 				/>
-			</div>
-		);
-	},
-} );
+			),
+		},
+		attributes: {
+			...metadata.attributes,
+			...blockAttributes,
+		},
+		edit,
+		// Save the props to post content.
+		save( { attributes }: { attributes: Attributes } ) {
+			const { className, heading, headingLevel } = attributes;
+			const data: Record< string, unknown > = {
+				'data-heading': heading,
+				'data-heading-level': headingLevel,
+			};
+			return (
+				<div
+					{ ...useBlockProps.save( {
+						className: classNames( 'is-loading', className ),
+					} ) }
+					{ ...data }
+				>
+					<span
+						aria-hidden
+						className="wc-block-product-rating-filter__placeholder"
+					/>
+				</div>
+			);
+		},
+	} );
+}
