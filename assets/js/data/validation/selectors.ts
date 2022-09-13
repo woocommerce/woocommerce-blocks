4ef55143835ca64c30d0501f5af8cede5c7327fa
Thomas Roberts <thomas.roberts@automattic.com>
2022-09-13 12:20:24 -0700

    Refactor selectors to not return functions anymore
    This is a poor DevEx and naming did not reflect what the selector was returning.

--- assets/js/data/validation/selectors.ts
+++ assets/js/data/validation/selectors.ts
@@ -3,17 +3,33 @@
  */
 import type { State } from './reducers';
 
-export const getValidationError = ( state: State ) => {
-	return ( errorId: string ) => state[ errorId ];
+/**
+ * Gets a validation error by ID.
+ *
+ * @param  state   The current state.
+ * @param  errorId { string } The error ID.
+ */
+export const getValidationError = ( state: State, errorId: string ) =>
+	state[ errorId ];
+
+/**
+ * Gets a validation error ID for use in HTML which can be used as a CSS selector, or to reference an error message.
+ *
+ * @param  state   The current state.
+ * @param  errorId { string } The error ID.
+ */
+export const getValidationErrorId = ( state: State, errorId: string ) => {
+	if ( ! state.hasOwnProperty( errorId ) || state[ errorId ].hidden ) {
+		return;
+	}
+	return `validate-error-${ errorId }`;
 };
-export const getValidationErrorId = ( state: State ) => {
-	return ( errorId: string ) => {
-		if ( ! state.hasOwnProperty( errorId ) || state[ errorId ].hidden ) {
-			return;
-		}
-		return `validate-error-${ errorId }`;
-	};
-};
+
+/**
+ * Whether the store has validation errors.
+ *
+ * @param  state The current state.
+ */
 export const hasValidationErrors = ( state: State ) => {
 	return Object.keys( state ).length > 0;
 };
