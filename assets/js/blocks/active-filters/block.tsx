32436c8d20b6a7783902d97278145feb15067b93
Daniel Dudzic <daniel.dudzic@automattic.com>
2022-09-20 15:58:56 +0200

    Active Filters: Fix the Clear All button for Ratings. Closes ##7172

--- assets/js/blocks/active-filters/block.tsx
+++ assets/js/blocks/active-filters/block.tsx
@@ -295,6 +295,7 @@ const ActiveFiltersBlock = ( {
 							setMaxPrice( undefined );
 							setProductAttributes( [] );
 							setProductStockStatus( [] );
+							setProductRatings( [] );
 						}
 					} }
 				>
