f0982d149e1ba52e8d43e66af277bae034319535
Nadir Seghir <nadir.seghir@gmail.com>
2022-09-30 12:02:20 +0100

    setup skeleton of collection method block

---
+++ assets/js/blocks/checkout/inner-blocks/checkout-collection-method/frontend.tsx
@@ -0,0 +1,52 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+import { withFilteredAttributes } from '@woocommerce/shared-hocs';
+import { FormStep } from '@woocommerce/base-components/cart-checkout';
+import { useCheckoutContext } from '@woocommerce/base-context';
+import { useState } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import Block from './block';
+import attributes from './attributes';
+
+const FrontendBlock = ( {
+	title,
+	description,
+	showStepNumber,
+	children,
+	className,
+}: {
+	title: string;
+	description: string;
+	showStepNumber: boolean;
+	children: JSX.Element;
+	className?: string;
+} ) => {
+	const { isProcessing: checkoutIsProcessing } = useCheckoutContext();
+	const [ collectionMethod, setCollectionMethod ] = useState( 'shipping' );
+	return (
+		<FormStep
+			id="collection-method"
+			disabled={ checkoutIsProcessing }
+			className={ classnames(
+				'wc-block-checkout__collection-method',
+				className
+			) }
+			title={ title }
+			description={ description }
+			showStepNumber={ showStepNumber }
+		>
+			<Block
+				checked={ collectionMethod }
+				onChange={ setCollectionMethod }
+			/>
+			{ children }
+		</FormStep>
+	);
+};
+
+export default withFilteredAttributes( attributes )( FrontendBlock );
