67dc1d3d6d9cbc562ef2ab93ba286430c92b6c64
Alex Florisca <alex.florisca@automattic.com>
2022-10-06 11:49:06 +0100

    Rename unsubscribeInitializePaymentMethodDataStore

--- assets/js/data/cart/index.ts
+++ assets/js/data/cart/index.ts
@@ -43,7 +43,7 @@ registeredStore.subscribe( async () => {
 	await checkPaymentMethodsCanPay( true );
 } );
 
-const unsubscribeInitializePaymentMethodDataStore = registeredStore.subscribe(
+const unsubscribeInitializePaymentStore = registeredStore.subscribe(
 	async () => {
 		const cartLoaded =
 			wpDataSelect( STORE_KEY ).hasFinishedResolution( 'getCartTotals' );
@@ -51,7 +51,7 @@ const unsubscribeInitializePaymentMethodDataStore = registeredStore.subscribe(
 			wpDataDispatch(
 				'wc/store/payment'
 			).__internalInitializePaymentStore();
-			unsubscribeInitializePaymentMethodDataStore();
+			unsubscribeInitializePaymentStore();
 		}
 	}
 );
