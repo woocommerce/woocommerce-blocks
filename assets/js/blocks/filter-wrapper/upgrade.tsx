4e9122684bba6f68e6d82ffc5acbe8e785998847
Tung Du <dinhtungdu@gmail.com>
2022-10-06 21:57:40 +0700

    fix: heading level when upgrading the block

--- assets/js/blocks/filter-wrapper/upgrade.tsx
+++ assets/js/blocks/filter-wrapper/upgrade.tsx
@@ -49,7 +49,7 @@ export const UpgradeNotice = ( {
 			filterWrapperInnerBlocks.unshift(
 				createBlock( 'core/heading', {
 					content: heading,
-					headingLevel: headingLevel ?? 2,
+					level: headingLevel ?? 2,
 				} )
 			);
 		}
