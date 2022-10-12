110420849afd442644c5bac59e011dc25f927008
Daniel W. Robert <danielwrobert@gmail.com>
2022-10-11 22:06:50 -0400

    Add PQ Context interface to shared type defs.

--- assets/js/types/type-defs/contexts.ts
+++ assets/js/types/type-defs/contexts.ts
@@ -13,6 +13,12 @@ export type ValidationData = {
 	) => void;
 };
 
+export type ProductQueryContext = {
+	query: object;
+	queryId: number;
+	productId: number;
+};
+
 export enum SHIPPING_ERROR_TYPES {
 	NONE = 'none',
 	INVALID_ADDRESS = 'invalid_address',
