85bade555f1e79c25951f46d105d739374e41358
Tung Du <dinhtungdu@gmail.com>
2022-10-13 10:15:57 +0700

    add backend test for product query

---
+++ tests/e2e/specs/backend/product-query.test.ts
@@ -0,0 +1,39 @@
+/**
+ * External dependencies
+ */
+import { getAllBlocks, switchUserToAdmin } from '@wordpress/e2e-test-utils';
+import { visitBlockPage } from '@woocommerce/blocks-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import {
+	insertBlockDontWaitForInsertClose,
+	GUTENBERG_EDITOR_CONTEXT,
+	describeOrSkip,
+} from '../../utils';
+
+const block = {
+	name: 'Product Query',
+	slug: 'woocommerce/product-query',
+	class: '.wp-block-query',
+};
+
+describeOrSkip( GUTENBERG_EDITOR_CONTEXT === 'gutenberg' )(
+	`${ block.name } Block`,
+	() => {
+		beforeAll( async () => {
+			await switchUserToAdmin();
+			await visitBlockPage( `${ block.name } Block` );
+		} );
+
+		it( 'can be inserted more than once', async () => {
+			await insertBlockDontWaitForInsertClose( block.name );
+			expect( await getAllBlocks() ).toHaveLength( 2 );
+		} );
+
+		it( 'renders without crashing', async () => {
+			await expect( page ).toRenderBlock( block );
+		} );
+	}
+);
