619aed0778f2c5eb8da278a198082970ecf6a8a1
Tung Du <dinhtungdu@gmail.com>
2022-10-13 13:23:28 +0700

    test editor preview shows only on sale products

--- tests/e2e/specs/backend/product-query.test.ts
+++ tests/e2e/specs/backend/product-query.test.ts
@@ -1,7 +1,13 @@
 /**
  * External dependencies
  */
-import { getAllBlocks, switchUserToAdmin } from '@wordpress/e2e-test-utils';
+import {
+	getAllBlocks,
+	switchUserToAdmin,
+	canvas,
+	openDocumentSettingsSidebar,
+	openListView,
+} from '@wordpress/e2e-test-utils';
 import { visitBlockPage } from '@woocommerce/blocks-test-utils';
 
 /**
@@ -34,6 +40,28 @@ describeOrSkip( GUTENBERG_EDITOR_CONTEXT === 'gutenberg' )(
 
 		it( 'renders without crashing', async () => {
 			await expect( page ).toRenderBlock( block );
+		} );
+
+		it( 'Editor preview shows only on sale products after enabling `Show only products on sale`', async () => {
+			await visitBlockPage( `${ block.name } Block` );
+			const canvasEl = canvas();
+			await openDocumentSettingsSidebar();
+			await openListView();
+			await page.click(
+				'.block-editor-list-view-block__contents-container a.components-button'
+			);
+			const [ onSaleToggle ] = await page.$x(
+				'//label[text()="Show only products on sale"]'
+			);
+			await onSaleToggle.click();
+			await canvasEl.waitForSelector( `${ block.class } > p` );
+			await canvasEl.waitForSelector(
+				`${ block.class } > ul.wp-block-post-template`
+			);
+			const products = await canvasEl.$$(
+				`${ block.class } ul.wp-block-post-template > li.block-editor-block-preview__live-content`
+			);
+			expect( products ).toHaveLength( 1 );
 		} );
 	}
 );
