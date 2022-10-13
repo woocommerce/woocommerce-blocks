e45572e27892c98704c4acf2c6db8bd163db9017
Alexandre Lara <allexandrelara@gmail.com>
2022-10-13 10:04:25 -0300

    Merge test utils methods with attribute filter block
    Removing test utils file and merging the methods with the Attribute Filter block to prevent tests from failing due to Jest configuration that only allow test files to exists inside a 'test' folder

--- assets/js/blocks/attribute-filter/test/block.tsx
+++ assets/js/blocks/attribute-filter/test/block.tsx
@@ -8,11 +8,6 @@ import userEvent from '@testing-library/user-event';
 /**
  * Internal dependencies
  */
-import {
-	setWindowUrl,
-	stubProductsAttributesTerms,
-	stubCollectionData,
-} from './test-utils';
 import AttributeFilterBlock from '../block';
 import { BlockAttributes } from '../types';
 
@@ -20,6 +15,63 @@ jest.mock( '@woocommerce/base-context/hooks', () => ( {
 	__esModule: true,
 	...jest.requireActual( '@woocommerce/base-context/hooks' ),
 } ) );
+
+const setWindowUrl = ( { url }: SetWindowUrlParams ) => {
+	global.window = Object.create( window );
+	Object.defineProperty( window, 'location', {
+		value: {
+			href: url,
+		},
+		writable: true,
+	} );
+};
+
+const stubProductsAttributesTerms = () => [
+	{
+		id: 25,
+		name: 'Large',
+		slug: 'large',
+		description: '',
+		parent: 0,
+		count: 1,
+	},
+	{
+		id: 26,
+		name: 'Medium',
+		slug: 'medium',
+		description: '',
+		parent: 0,
+		count: 1,
+	},
+	{
+		id: 27,
+		name: 'Small',
+		slug: 'small',
+		description: '',
+		parent: 0,
+		count: 1,
+	},
+];
+
+const stubCollectionData = () => ( {
+	price_range: null,
+	attribute_counts: [
+		{
+			term: 25,
+			count: 1,
+		},
+		{
+			term: 26,
+			count: 1,
+		},
+		{
+			term: 27,
+			count: 1,
+		},
+	],
+	rating_counts: null,
+	stock_status_counts: null,
+} );
 
 interface SetupParams {
 	initialUrl: string;
