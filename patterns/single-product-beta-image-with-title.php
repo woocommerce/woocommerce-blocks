ca27a7300949f09985c439b5d0765085061a022d
Alexandre Lara <allexandrelara@gmail.com>
2022-10-22 11:57:43 +0200

    Add Single Product (Beta) patterns
    Add two patterns for the Single Product (Beta) block: the first pattern contains an image with a title, while the other one has a title with an Add to Cart button.

---
+++ patterns/single-product-beta-image-with-title.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Single Product Beta | Image With Title
+ * Slug: woocommerce-blocks/single-product-beta-image-with-title
+ * Categories: WooCommerce
+ * Block Types: woocommerce/single-product-beta, core/query/woocommerce/single-product-beta
+ */
+?>
+
+<!-- wp:query {"queryId":3,"query":{"perPage":6,"pages":0,"offset":0,"postType":"product","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"__woocommerceStockStatus":["instock","onbackorder"],"include":[16]},"displayLayout":{"type":"list"},"namespace":"woocommerce/single-product-beta"} -->
+<div class="wp-block-query"><!-- wp:post-template -->
+<!-- wp:woocommerce/product-image {"isDescendentOfQueryLoop":true} /-->
+
+<!-- wp:post-title {"level":3,"fontSize":"large"} /-->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
---
+++ patterns/single-product-beta.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Single Product Beta
+ * Slug: woocommerce-blocks/single-product-beta
+ * Categories: WooCommerce
+ * Block Types: woocommerce/single-product-beta, core/query/woocommerce/single-product-beta
+ */
+?>
+
+<!-- wp:query {"queryId":1,"query":{"perPage":6,"pages":0,"offset":0,"postType":"product","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"__woocommerceStockStatus":["instock","onbackorder"],"include":[16]},"displayLayout":{"type":"list"},"namespace":"woocommerce/single-product-beta"} -->
+<div class="wp-block-query"><!-- wp:post-template -->
+<!-- wp:post-title {"level":3,"fontSize":"large"} /-->
+
+<!-- wp:woocommerce/product-button {"isDescendentOfQueryLoop":true} /-->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
