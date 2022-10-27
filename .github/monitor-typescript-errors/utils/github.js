28e816d8c62c53c083427cac92a1799b8ed3dc78
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2022-10-27 19:08:12 +0100

    testing typescript error monitors

--- .github/monitor-typescript-errors/utils/github.js
+++ .github/monitor-typescript-errors/utils/github.js
@@ -9,6 +9,7 @@ exports.getFileContent = async ( {
 		return await octokit.rest.repos.getContent( {
 			owner,
 			repo,
+			ref: 'try/typescript-monitor',
 			path: fileName,
 			mediaType: {
 				format: 'raw',
