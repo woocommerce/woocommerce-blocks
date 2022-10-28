9ca5aef78b79a5550e729b4163c37abaad3e8f8b
Manish Menaria <the.manish.menaria@gmail.com>
2022-10-28 14:20:43 +0530

    Fix storybook build failing issues
    Storybook build was failing because of 2 issues:
    1. Can't import the named export 'Fragment' from non EcmaScript module (only default export is available)
    I used solution provided in following comment:
    https://github.com/storybookjs/storybook/issues/16690#issuecomment-971579785
    
    This issue comes from the framer-motion library.
    
    2. SassError: Can't find stylesheet to import.
    To fixe this issue I have updated the path in import statements.
    **Before**
    ```
    @import "@wordpress/base-styles/breakpoints";
    @import "@wordpress/base-styles/mixins";
    @import "wordpress-components/src/popover/style";
    @import "wordpress-components/src/tooltip/style";
    ```
    **After**
    ```
    @import "node_modules/@wordpress/base-styles/breakpoints";
    @import "node_modules/@wordpress/base-styles/mixins";
    @import "node_modules/wordpress-components/src/popover/style";
    @import "node_modules/wordpress-components/src/tooltip/style";
    ```

--- storybook/main.js
+++ storybook/main.js
@@ -14,4 +14,12 @@ module.exports = {
 		babelModeV7: true,
 		emotionAlias: false,
 	},
+	webpackFinal: async ( config ) => {
+		config.module.rules.push( {
+			test: /\.mjs$/,
+			include: /node_modules/,
+			type: 'javascript/auto',
+		} );
+		return config;
+	},
 };
