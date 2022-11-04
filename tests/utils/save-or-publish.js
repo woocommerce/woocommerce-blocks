88f3e2da3cb89e766b0674f056b12afa3796483d
Tung Du <dinhtungdu@gmail.com>
2022-11-05 01:03:04 +0700

    try: hybrid timeout

--- tests/utils/save-or-publish.js
+++ tests/utils/save-or-publish.js
@@ -3,28 +3,23 @@
  */
 import { publishPost } from '@wordpress/e2e-test-utils';
 
-export async function saveOrPublish() {
+export async function saveOrPublish( retry = 0 ) {
 	const link = await page.evaluate( () =>
 		wp.data.select( 'core/editor' ).getPermalink()
 	);
 	if ( link.match( 'auto-draft' ) ) {
 		await publishPost();
 	} else {
-		const publishButton = await page.waitForSelector(
+		const publishButton = await page.$(
 			'.editor-post-publish-button.editor-post-publish-button__button:not([aria-disabled="true"])'
 		);
 		if ( publishButton ) {
 			await publishButton.click();
 			// A success notice should show up
-			try {
-				await page.waitForSelector( '.components-snackbar' );
-			} catch ( e ) {
-				// If the notice doesn't show up, it's probably because there's a race condition
-				// with the publish button being disabled at the time clicking it. In that case,
-				// we click the publich button again.
-				await publishButton.click();
-				await page.waitForSelector( '.components-snackbar' );
-			}
+			await page.waitForSelector( '.components-snackbar' );
+		} else if ( retry < 5 ) {
+			await page.waitForTimeout( 1000 );
+			await saveOrPublish( retry + 1 );
 		}
 	}
 }
