b814f62a56be01154edc98c722d96f21ca081a5b
Tung Du <dinhtungdu@gmail.com>
2022-11-04 12:37:53 +0700

    clarify the design decision

--- tests/utils/insert-inner-block.ts
+++ tests/utils/insert-inner-block.ts
@@ -7,6 +7,9 @@ import { selectBlockByName } from './select-block-by-name';
  * Inserts an inner block into the currently selected block. If a parent block
  * is provided, it will be selected before inserting the inner block.
  *
+ * We make the parentBlockName optional to add more flexibility to the function,
+ * enabling tests to have more control over the selection of the parent block.
+ *
  * @param  blockTitle      Block title, such as "Add to Cart Button".
  * @param  parentBlockName Parent block name, such as core/group.
  */
