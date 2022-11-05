f256ee18c9fa1efa3613d3d9b5c757067a5c5c1b
Tung Du <dinhtungdu@gmail.com>
2022-11-06 00:25:57 +0700

    Revert "try: hybrid timeout"
    This reverts commit 88f3e2da3cb89e766b0674f056b12afa3796483d.

--- tests/utils/save-or-publish.js
+++ tests/utils/save-or-publish.js
@@ -3,23 +3,28 @@
  */
 import { publishPost } from '@wordpress/e2e-test-utils';
 
-export async function saveOrPublish( retry = 0 ) {
+export async function saveOrPublish() {
 	const link = await page.evaluate( () =>
 		wp.data.select( 'core/editor' ).getPermalink()
 	);
 	if ( link.match( 'auto-draft' ) ) {
 		await publishPost();
 	} else {
-		const publishButton = await page.$(
+		const publishButton = await page.waitForSelector(
 			'.editor-post-publish-button.editor-post-publish-button__button:not([aria-disabled="true"])'
 		);
 		if ( publishButton ) {
 			await publishButton.click();
 			// A success notice should show up
-			await page.waitForSelector( '.components-snackbar' );
-		} else if ( retry < 5 ) {
-			await page.waitForTimeout( 1000 );
-			await saveOrPublish( retry + 1 );
+			try {
+				await page.waitForSelector( '.components-snackbar' );
+			} catch ( e ) {
+				// If the notice doesn't show up, it's probably because there's a race condition
+				// with the publish button being disabled at the time clicking it. In that case,
+				// we click the publich button again.
+				await publishButton.click();
+				await page.waitForSelector( '.components-snackbar' );
+			}
 		}
 	}
 }
