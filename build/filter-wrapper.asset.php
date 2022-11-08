11693ed600ecfc2b5a00a1f128eeb9373f68cb6d
Alba Rincón <alba.rincon@automattic.com>
2022-11-08 12:37:31 +0100

    Adding /build directory to release

---
+++ build/filter-wrapper.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '0b552ae9b07c5cbf04c5de0508c925f8');
---
+++ build/stock-filter.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wc-blocks-checkout', 'wc-blocks-data-store', 'wc-settings', 'wp-a11y', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => 'd32fff24558126303520d61ad08b892e');
