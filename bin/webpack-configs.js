b620012c986cb4cc96eb59234a0bae8fe89d7119
Mike Jolley <mike.jolley@me.com>
2022-11-11 16:17:49 +0000

    React Based Local Pickup Settings Screen (#7581)
    * Move payment methods
    
    * Setup entrypoints
    
    * Sortable implementation
    
    * Sortable
    
    * Basic UI in place for settings
    
    * Hydrate real settings
    
    * Form updates values
    
    * Styling and save button placement
    
    * useSettings hook
    
    * Prepare for save
    
    * delete
    
    * Add location button
    
    * Remove className
    
    * Conditional display of taxes
    
    * Save via API
    
    * Update general settings to designs
    
    * Modal styles
    
    * Style table
    
    * Border colors and radius
    
    * Added e2e tests
    
    * use node 16
    
    * Enqueue states in admin
    
    * Use render from wordpress/element
    
    * Missing handle style
    
    * Enable translations
    
    * Remove curried function
    
    * Todo for inline settings
    
    Co-authored-by: Nadir Seghir <nadir.seghir@gmail.com>

--- bin/webpack-configs.js
+++ bin/webpack-configs.js
@@ -566,6 +566,12 @@ const getExtensionsConfig = ( options = {} ) => {
 						},
 					},
 				},
+				{
+					test: /\.s[c|a]ss$/,
+					use: {
+						loader: 'ignore-loader',
+					},
+				},
 			],
 		},
 		optimization: {
