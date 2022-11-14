14acb5e42a9f630f4017137fdc2b76a90df3fa35
Tung Du <dinhtungdu@gmail.com>
2022-11-14 23:34:50 +0700

    test removed filter. continue refactoring local utilities. add docs explaining scoping for selectors but not label

--- tests/e2e/specs/backend/product-query/product-filters.test.ts
+++ tests/e2e/specs/backend/product-query/product-filters.test.ts
@@ -8,6 +8,7 @@ import {
 	selectBlockByName,
 	findToolsPanelWithTitle,
 } from '@woocommerce/blocks-test-utils';
+import { ElementHandle } from 'puppeteer';
 
 /**
  * Internal dependencies
@@ -25,26 +26,43 @@ const block = {
 	class: '.wp-block-query',
 };
 
+/**
+ * Selectors used for interacting with the block in the editor. These selectors
+ * can be changed upstream in Gutenberg, so we scope them here for
+ * maintainability.
+ *
+ * There are also some labels that are used repeatedly, but we don't scope them
+ * in favor of readability. Unlike selectors, those label are visible to end
+ * users, so it's easier to understand what's going on if we don't scope them.
+ * Those labels can get upated in the future, but the tests will fail and we'll
+ * know to update them.
+ */
 const SELECTORS = {
-	productFiltersDropdownButton:
-		'.components-tools-panel-header .components-dropdown-menu button',
+	productFiltersDropdownButton: {
+		default:
+			'.components-tools-panel-header .components-dropdown-menu button[aria-expanded="false"]',
+		expanded:
+			'.components-tools-panel-header .components-dropdown-menu button[aria-expanded="true"]',
+	},
 	productFiltersDropdown:
 		'.components-dropdown-menu__menu[aria-label="Product filters options"]',
 	productFiltersDropdownItem: '.components-menu-item__button',
 };
 
-const addProductFilter = async ( filterName: string ) => {
+const toggleProductFilter = async ( filterName: string ) => {
 	const productFiltersPanel = await findToolsPanelWithTitle(
 		'Product filters'
 	);
-	const button = await productFiltersPanel.$(
-		SELECTORS.productFiltersDropdownButton
+	await expect( productFiltersPanel ).toClick(
+		SELECTORS.productFiltersDropdownButton.default
 	);
-	await button.click();
 	await canvas().waitForSelector( SELECTORS.productFiltersDropdown );
 	await expect( canvas() ).toClick( SELECTORS.productFiltersDropdownItem, {
 		text: filterName,
 	} );
+	await expect( productFiltersPanel ).toClick(
+		SELECTORS.productFiltersDropdownButton.expanded
+	);
 };
 
 const resetProductQueryBlockPage = async () => {
@@ -58,40 +76,50 @@ const resetProductQueryBlockPage = async () => {
 describeOrSkip( GUTENBERG_EDITOR_CONTEXT === 'gutenberg' )(
 	'Product Query > Products Filters',
 	() => {
+		let productFiltersPanel: ElementHandle< Node > | null;
 		beforeEach( async () => {
-			await visitBlockPage( `${ block.name } Block` );
-			await waitForCanvas();
+			/**
+			 * Reset the block page before each test to ensure the block is
+			 * inserted in a known state. This is also needed to ensure each
+			 * test can be run individually.
+			 */
+			await resetProductQueryBlockPage();
 			await openBlockEditorSettings( { isFSEEditor: false } );
 			await selectBlockByName( block.slug );
+			productFiltersPanel = await findToolsPanelWithTitle(
+				'Product filters'
+			);
 		} );
 
 		/**
-		 * Reset the content of Product Query Block page before and after this
-		 * test suite to avoide breaking other tests.
+		 * Reset the content of Product Query Block page after this test suite
+		 * to avoid breaking other tests.
 		 */
-		beforeAll( async () => {
-			await resetProductQueryBlockPage();
-		} );
 		afterAll( async () => {
 			await resetProductQueryBlockPage();
 		} );
 
 		describe( 'Sale Status', () => {
 			it( 'Sale status is disabled by default', async () => {
-				const productFiltersPanel = await findToolsPanelWithTitle(
-					'Product filters'
-				);
 				await expect( productFiltersPanel ).not.toMatch(
 					'Show only products on sale'
 				);
 			} );
 
 			it( 'Can add Sale Status filter', async () => {
-				await addProductFilter( 'Sale status' );
-				const productFiltersPanel = await findToolsPanelWithTitle(
-					'Product filters'
-				);
+				await toggleProductFilter( 'Sale status' );
 				await expect( productFiltersPanel ).toMatch(
+					'Show only products on sale'
+				);
+			} );
+
+			it( 'Can remove Sale Status filter', async () => {
+				await toggleProductFilter( 'Sale status' );
+				await expect( productFiltersPanel ).toMatch(
+					'Show only products on sale'
+				);
+				await toggleProductFilter( 'Sale status' );
+				await expect( productFiltersPanel ).not.toMatch(
 					'Show only products on sale'
 				);
 			} );
