089fa97c1eecd27fda9227f7e6f25983ed04a241
Alba Rincón <alba.rincon@automattic.com>
2022-11-14 09:56:26 +0100

    Adding /build directory to release

---
+++ build/price-filter-frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wc-blocks-data-store', 'wc-price-format', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-url'), 'version' => 'e113b779dc4dca6f04868453638db2e5');
---
+++ build/product-categories.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives', 'wp-server-side-render'), 'version' => '1124599dfe88e7a2f436374d23d2a4a8');
