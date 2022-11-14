c7d77b555931c8f9ed8b3390955715bcde5d80d4
Tung Du <dinhtungdu@gmail.com>
2022-11-14 12:10:20 +0700

    instead of retry after an arbitrary time, we retry until the timeout is reached. we also throw an error instead of return the id which is possibly detected from the page.

--- tests/utils/get-toggle-id-by-label.ts
+++ tests/utils/get-toggle-id-by-label.ts
@@ -1,4 +1,9 @@
 /**
+ * Internal dependencies
+ */
+import { DEFAULT_TIMEOUT } from './constants';
+
+/**
  * Get the ID of the setting toogle so test can manipulate the toggle using
  * unsetCheckbox and setCheckbox utilities.
  *
@@ -7,12 +12,13 @@
  * check if the node still attached to the document before returning its
  * ID. If the node is detached, it means that the toggle is rendered, then
  * we retry by calling this function again with increased retry argument. We
- * will retry maximum 5 times before returning whatever ID we get.
+ * will retry until the timeout is reached.
  */
 export const getToggleIdByLabel = async (
 	label: string,
 	retry = 0
 ): Promise< string > => {
+	const delay = 1000;
 	const labelElement = await page.waitForXPath(
 		`//label[contains(text(), "${ label }") and contains(@class, "components-toggle-control__label")]`,
 		{ visible: true }
@@ -22,10 +28,13 @@ export const getToggleIdByLabel = async (
 		labelElement
 	);
 	// Wait a bit for toggle to finish rerendering.
-	await page.waitForTimeout( 1000 );
+	await page.waitForTimeout( delay );
 	const checkbox = await page.$( checkboxId );
-	if ( ! checkbox && retry < 5 ) {
-		return await getToggleIdByLabel( label, retry + 1 );
+	if ( ! checkbox ) {
+		if ( retry * delay < DEFAULT_TIMEOUT ) {
+			return await getToggleIdByLabel( label, retry + 1 );
+		}
+		throw new Error( `Can't find toggle with label ${ label }` );
 	}
 	return checkboxId;
 };
