aa3704a103c6a734d220f79d7f968fa46dba337c
Thomas Roberts <thomas.roberts@automattic.com>
2022-11-16 11:44:48 +0000

    Wrap Cart in CartEventsProvider

--- assets/js/base/context/providers/cart-checkout/index.js
+++ assets/js/base/context/providers/cart-checkout/index.js
@@ -2,6 +2,7 @@ export * from './payment-events';
 export * from './shipping';
 export * from './customer';
 export * from './checkout-events';
+export * from './cart-events';
 export * from './cart';
 export * from './checkout-processor';
 export * from './checkout-provider';
