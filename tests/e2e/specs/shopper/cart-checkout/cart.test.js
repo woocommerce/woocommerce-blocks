3f040f0fc415b712d08dc93611f02d9b2b0b1998
Niels Lange <info@nielslange.de>
2022-11-17 22:52:00 +0700

    Remove obsolete eslint-disable comment

--- tests/e2e/specs/shopper/cart-checkout/cart.test.js
+++ tests/e2e/specs/shopper/cart-checkout/cart.test.js
@@ -105,7 +105,6 @@ describe( 'Shopper → Cart', () => {
 		await shopper.block.productIsInCart( SIMPLE_VIRTUAL_PRODUCT_NAME, 4 );
 	} );
 
-	// eslint-disable-next-line jest/expect-expect
 	it( 'User can see Cross-Sells products block', async () => {
 		await shopper.block.emptyCart();
 		await shopper.block.goToShop();
