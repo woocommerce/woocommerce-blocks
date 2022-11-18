1b9a2056fa2eb162c20b2ec87c6a7680d85e2786
Mike Jolley <mike.jolley@me.com>
2022-11-18 09:27:46 +0000

    Remove context provider

--- assets/js/base/context/providers/store-notices/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './store-notice-context';
-export { default as StoreNoticesContainer } from './store-notices-container';
-export { default as SnackbarNoticesContainer } from './snackbar-notices-container';
