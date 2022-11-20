ded24df1e0aa7afec208def6957315144ff7321a
Lucio Giannotta <lucio.giannotta@a8c.com>
2022-11-20 19:03:01 +0100

    Product Query: Add support for sorting by rating

--- src/BlockTypes/ProductQuery.php
+++ src/BlockTypes/ProductQuery.php
@@ -28,7 +28,7 @@ class ProductQuery extends AbstractBlock {
 	 *
 	 * @var array
 	 */
-	protected $custom_order_opts = array( 'popularity' );
+	protected $custom_order_opts = array( 'popularity', 'rating' );
 
 	/**
 	 * All the query args related to the filter by attributes block.
@@ -238,6 +238,7 @@ class ProductQuery extends AbstractBlock {
 
 		$meta_keys = array(
 			'popularity' => 'total_sales',
+			'rating'     => '_wc_average_rating',
 		);
 
 		return array(
