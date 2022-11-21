c5969cf11c9b853cfa73d4705736416bc348ce2c
Mike Jolley <mike.jolley@me.com>
2022-11-21 10:53:40 +0000

    Update assets/js/base/context/event-emit/utils.ts
    Co-authored-by: Thomas Roberts <5656702+opr@users.noreply.github.com>

--- assets/js/base/context/event-emit/utils.ts
+++ assets/js/base/context/event-emit/utils.ts
@@ -31,7 +31,7 @@ export enum noticeContexts {
 	CHECKOUT = 'wc/checkout',
 	PAYMENTS = 'wc/checkout/payments',
 	EXPRESS_PAYMENTS = 'wc/checkout/express-payments',
-	CONTACT_INFO = 'wc/checkout/contact-info',
+	CONTACT_INFORMATION = 'wc/checkout/contact-information',
 	SHIPPING_ADDRESS = 'wc/checkout/shipping-address',
 	BILLING_ADDRESS = 'wc/checkout/billing-address',
 	SHIPPING_METHODS = 'wc/checkout/shipping-methods',
