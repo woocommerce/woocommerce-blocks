fc953718846528d123865558d9f159b13a1bf8b6
Mike Jolley <mike.jolley@me.com>
2022-11-21 10:19:16 +0000

    Optional props are undefined

--- packages/checkout/components/store-notices-container/types.ts
+++ packages/checkout/components/store-notices-container/types.ts
@@ -7,8 +7,8 @@ import type {
 } from '@wordpress/notices';
 
 export interface StoreNoticesContainerProps {
-	className?: string;
-	context?: string;
+	className?: string | undefined;
+	context?: string | undefined;
 	// If true, notices under the `wc/global` context will be shown as well as notices in the given context.
 	showGlobal: boolean;
 	// List of additional notices that were added inline and not stored in the `core/notices` store.
