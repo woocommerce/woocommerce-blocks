3c5a87dbc6663f5a31ccfa84be0f87d068c85fa3
Tung Du <dinhtungdu@gmail.com>
2022-11-21 14:45:26 +0700

    delete out of date tests

--- tests/e2e/specs/backend/product-query.test.ts
+++ tests/e2e/specs/backend/product-query.test.ts
@@ -1,15 +1,7 @@
 /**
  * External dependencies
  */
-import {
-	getAllBlocks,
-	switchUserToAdmin,
-	canvas,
-	openDocumentSettingsSidebar,
-	openListView,
-	setPostContent,
-	insertBlock,
-} from '@wordpress/e2e-test-utils';
+import { getAllBlocks, switchUserToAdmin } from '@wordpress/e2e-test-utils';
 import { visitBlockPage } from '@woocommerce/blocks-test-utils';
 
 /**
@@ -42,71 +34,6 @@ describeOrSkip( GUTENBERG_EDITOR_CONTEXT === 'gutenberg' )(
 
 		it( 'renders without crashing', async () => {
 			await expect( page ).toRenderBlock( block );
-		} );
-
-		/**
-		 * We changed the “Show only products on sale” from a top-level toggle
-		 * setting to a product filter, but tests for them haven't been updated
-		 * yet. We will fix these tests in a follow-up PR.
-		 */
-		it.skip( 'Editor preview shows only on sale products after enabling `Show only products on sale`', async () => {
-			await visitBlockPage( `${ block.name } Block` );
-			const canvasEl = canvas();
-			await openDocumentSettingsSidebar();
-			await openListView();
-			await page.click(
-				'.block-editor-list-view-block__contents-container a.components-button'
-			);
-			const [ onSaleToggle ] = await page.$x(
-				'//label[text()="Show only products on sale"]'
-			);
-			await onSaleToggle.click();
-			await canvasEl.waitForSelector( `${ block.class } > p` );
-			await canvasEl.waitForSelector(
-				`${ block.class } > ul.wp-block-post-template`
-			);
-			const products = await canvasEl.$$(
-				`${ block.class } ul.wp-block-post-template > li.block-editor-block-preview__live-content`
-			);
-			expect( products ).toHaveLength( 1 );
-		} );
-
-		describe( 'On Sale variation', () => {
-			beforeAll( async () => {
-				await visitBlockPage( `${ block.name } Block` );
-				await setPostContent( '' );
-				await insertBlock( 'Products on Sale' );
-			} );
-
-			it( 'Show only on sale products', async () => {
-				const canvasEl = canvas();
-				await canvasEl.waitForSelector(
-					`${ block.class } > ul.wp-block-post-template`
-				);
-				const products = await canvasEl.$$(
-					`${ block.class } ul.wp-block-post-template > li.block-editor-block-preview__live-content`
-				);
-				expect( products ).toHaveLength( 1 );
-			} );
-
-			/**
-			 * We changed the “Show only products on sale” from a top-level toggle
-			 * setting to a product filter, but tests for them haven't been updated
-			 * yet. We will fix these tests in a follow-up PR.
-			 */
-			it.skip( 'Does not have on sale toggle', async () => {
-				await openDocumentSettingsSidebar();
-				await openListView();
-				await page.click(
-					'.block-editor-list-view-block__contents-container a.components-button'
-				);
-				await expect( page ).not.toMatchElement(
-					'.block-editor-block-inspector',
-					{
-						text: 'Show only products on sale',
-					}
-				);
-			} );
 		} );
 	}
 );
