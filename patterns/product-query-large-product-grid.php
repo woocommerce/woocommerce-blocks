9a55e40389332f7257c95217628ea96033d83a44
Daniel Dudzic <daniel.dudzic@automattic.com>
2022-11-23 09:16:21 +0100

    Product Query: First attempt at adding patterns

---
+++ patterns/product-query-large-product-grid.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: WooCommerce Large Product Grid
+ * Slug: woocommerce-blocks/product-query-large-product-grid
+ * Categories: WooCommerce
+ * Block Types: core/query/woocommerce/product-query
+ */
+?>
+
