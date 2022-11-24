e4de7a7c9e03c7a075fb27087e368cb187d97529
Mike Jolley <mike.jolley@me.com>
2022-11-24 13:33:35 +0000

    text descriptions

--- assets/js/blocks/order-received/order-summary/block.tsx
+++ assets/js/blocks/order-received/order-summary/block.tsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
+import { createInterpolateElement } from '@wordpress/element';
 import { Icon, currencyDollar } from '@wordpress/icons';
 import { objectHasProp } from '@woocommerce/types';
 
@@ -52,11 +53,32 @@ const Block = ( props: {
 		__( 'Your order is %s', 'woo-gutenberg-products-block' ),
 		orderStatusText
 	);
+	let orderStatusDescription = '';
 
 	switch ( orderStatus ) {
+		case 'pending':
+			orderStatusDescription = __(
+				"We've received your order and will start processing it once payment has been verified.",
+				'woo-gutenberg-products-block'
+			);
+			break;
+		case 'on-hold':
+			orderStatusHeading = __(
+				'Your order is pending approval',
+				'woo-gutenberg-products-block'
+			);
+			orderStatusDescription = __(
+				"We've received your order and will start getting it ready once approved.",
+				'woo-gutenberg-products-block'
+			);
+			break;
 		case 'processing':
 			orderStatusHeading = __(
 				'Your order is confirmed',
+				'woo-gutenberg-products-block'
+			);
+			orderStatusDescription = __(
+				"We've received your order and we're getting it ready.",
 				'woo-gutenberg-products-block'
 			);
 			break;
@@ -84,6 +106,20 @@ const Block = ( props: {
 			</div>
 			<div className="wc-block-components-order-summary-box">
 				<h3>{ orderStatusHeading }</h3>
+				<p>
+					{ orderStatusDescription
+						? orderStatusDescription + ' '
+						: '' }
+					{ createInterpolateElement(
+						__(
+							'A confirmation email has been sent to <email/>. Come back to this page to see updates regarding your order status.',
+							'woo-gutenberg-products-block'
+						),
+						{
+							email: <strong>{ orderEmail }</strong>,
+						}
+					) }
+				</p>
 				<ul className="wc-block-components-order-summary-items">
 					<li className="wc-block-components-order-summary__order-number">
 						{ __(
