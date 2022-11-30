c4744dba3a1ced0e880fdf18ce7841f3e9f8f8be
Nadir Seghir <nadir.seghir@gmail.com>
2022-11-30 16:12:24 +0100

    fix errors

--- assets/js/base/components/cart-checkout/totals/shipping/index.tsx
+++ assets/js/base/components/cart-checkout/totals/shipping/index.tsx
@@ -7,8 +7,6 @@ import { useState } from '@wordpress/element';
 import { useStoreCart } from '@woocommerce/base-context/hooks';
 import { TotalsItem } from '@woocommerce/blocks-checkout';
 import type { Currency } from '@woocommerce/price-format';
-import type { ReactElement } from 'react';
-import { ShippingAddress as ShippingAddressType } from '@woocommerce/settings';
 import { ShippingVia } from '@woocommerce/base-components/cart-checkout/totals/shipping/shipping-via';
 import { useSelect } from '@wordpress/data';
 import { CHECKOUT_STORE_KEY } from '@woocommerce/block-data';
@@ -17,102 +15,11 @@ import { CHECKOUT_STORE_KEY } from '@woocommerce/block-data';
  * Internal dependencies
  */
 import ShippingCalculator from '../../shipping-calculator';
-import ShippingRateSelector from './shipping-rate-selector';
 import { hasShippingRate, getTotalShippingValue } from './utils';
 import ShippingPlaceholder from './shipping-placeholder';
 import ShippingAddress from './shipping-address';
+import ShippingRateSelector from './shipping-rate-selector';
 import './style.scss';
-
-interface CalculatorButtonProps {
-	label?: string;
-	isShippingCalculatorOpen: boolean;
-	setIsShippingCalculatorOpen: ( isShippingCalculatorOpen: boolean ) => void;
-}
-
-const CalculatorButton = ( {
-	label = __( 'Calculate', 'woo-gutenberg-products-block' ),
-	isShippingCalculatorOpen,
-	setIsShippingCalculatorOpen,
-}: CalculatorButtonProps ): ReactElement => {
-	return (
-		<button
-			className="wc-block-components-totals-shipping__change-address-button"
-			onClick={ () => {
-				setIsShippingCalculatorOpen( ! isShippingCalculatorOpen );
-			} }
-			aria-expanded={ isShippingCalculatorOpen }
-		>
-			{ label }
-		</button>
-	);
-};
-
-interface ShippingAddressProps {
-	showCalculator: boolean;
-	isShippingCalculatorOpen: boolean;
-	setIsShippingCalculatorOpen: CalculatorButtonProps[ 'setIsShippingCalculatorOpen' ];
-	shippingAddress: ShippingAddressType;
-}
-
-const ShippingAddress = ( {
-	showCalculator,
-	isShippingCalculatorOpen,
-	setIsShippingCalculatorOpen,
-	shippingAddress,
-}: ShippingAddressProps ): ReactElement | null => {
-	return (
-		<>
-			<ShippingLocation address={ shippingAddress } />
-			{ showCalculator && (
-				<CalculatorButton
-					label={ __(
-						'(change address)',
-						'woo-gutenberg-products-block'
-					) }
-					isShippingCalculatorOpen={ isShippingCalculatorOpen }
-					setIsShippingCalculatorOpen={ setIsShippingCalculatorOpen }
-				/>
-			) }
-		</>
-	);
-};
-
-interface NoShippingPlaceholderProps {
-	showCalculator: boolean;
-	isShippingCalculatorOpen: boolean;
-	isCheckout?: boolean;
-	setIsShippingCalculatorOpen: CalculatorButtonProps[ 'setIsShippingCalculatorOpen' ];
-}
-
-const NoShippingPlaceholder = ( {
-	showCalculator,
-	isShippingCalculatorOpen,
-	setIsShippingCalculatorOpen,
-	isCheckout = false,
-}: NoShippingPlaceholderProps ): ReactElement => {
-	if ( ! showCalculator ) {
-		return (
-			<em>
-				{ isCheckout
-					? __(
-							'No shipping options available',
-							'woo-gutenberg-products-block'
-					  )
-					: __(
-							'Calculated during checkout',
-							'woo-gutenberg-products-block'
-					  ) }
-			</em>
-		);
-	}
-
-	return (
-		<CalculatorButton
-			isShippingCalculatorOpen={ isShippingCalculatorOpen }
-			setIsShippingCalculatorOpen={ setIsShippingCalculatorOpen }
-		/>
-	);
-};
 
 export interface TotalShippingProps {
 	currency: Currency;
