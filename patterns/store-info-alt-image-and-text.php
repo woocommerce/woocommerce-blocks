b7efcd80b8ce9d86019d10ad210504e23770d9f5
Darren Ethier <darren@roughsmootheng.in>
2022-12-02 08:40:55 -0500

    fix pattern title

--- patterns/store-info-alt-image-and-text.php
+++ patterns/store-info-alt-image-and-text.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Title: WooCommerce Alternate Image and Text
+ * Title: WooCommerce Alternating Image and Text
  * Slug: woocommerce-blocks/alt-image-and-text
  * Categories: WooCommerce
  */
