69dc020e58d7269374137470d59f2249466e6ac7
Niels Lange <info@nielslange.de>
2022-12-02 17:47:07 +0700

    Ensure to keep the existing styles

--- assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-footer-block/edit.tsx
+++ assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-footer-block/edit.tsx
@@ -1,8 +1,19 @@
 /**
  * External dependencies
  */
-import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+import { TotalsItem } from '@woocommerce/blocks-checkout';
 import Button from '@woocommerce/base-components/button';
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+import { getCurrencyFromPriceResponse } from '@woocommerce/price-format';
+import {
+	usePaymentMethods,
+	useStoreCart,
+} from '@woocommerce/base-context/hooks';
+import PaymentMethodIcons from '@woocommerce/base-components/cart-checkout/payment-method-icons';
+import { getIconsFromPaymentMethods } from '@woocommerce/base-utils';
+import { getSetting } from '@woocommerce/settings';
+import { PaymentEventsProvider } from '@woocommerce/base-context';
 
 /**
  * Internal dependencies
@@ -11,6 +22,15 @@ import {
 	defaultCartButtonLabel,
 	defaultCheckoutButtonLabel,
 } from './constants';
+
+const PaymentMethodIconsElement = (): JSX.Element => {
+	const { paymentMethods } = usePaymentMethods();
+	return (
+		<PaymentMethodIcons
+			icons={ getIconsFromPaymentMethods( paymentMethods ) }
+		/>
+	);
+};
 
 export const Edit = ( {
 	attributes,
@@ -24,35 +44,60 @@ export const Edit = ( {
 } ): JSX.Element => {
 	const blockProps = useBlockProps();
 	const { cartButtonLabel, checkoutButtonLabel } = attributes;
+	const { cartTotals } = useStoreCart();
+	const subTotal = getSetting( 'displayCartPricesIncludingTax', false )
+		? parseInt( cartTotals.total_items, 10 ) +
+		  parseInt( cartTotals.total_items_tax, 10 )
+		: parseInt( cartTotals.total_items, 10 );
 
 	return (
 		<div { ...blockProps }>
-			<Button className="wc-block-mini-cart__footer-cart">
-				<RichText
-					multiline={ false }
-					allowedFormats={ [] }
-					value={ cartButtonLabel }
-					placeholder={ defaultCartButtonLabel }
-					onChange={ ( content ) => {
-						setAttributes( {
-							cartButtonLabel: content,
-						} );
-					} }
+			<div className="wc-block-mini-cart__footer">
+				<TotalsItem
+					className="wc-block-mini-cart__footer-subtotal"
+					currency={ getCurrencyFromPriceResponse( cartTotals ) }
+					label={ __( 'Subtotal', 'woo-gutenberg-products-block' ) }
+					value={ subTotal }
+					description={ __(
+						'Shipping, taxes, and discounts calculated at checkout.',
+						'woo-gutenberg-products-block'
+					) }
 				/>
-			</Button>
-			<Button className="wc-block-mini-cart__footer-checkout">
-				<RichText
-					multiline={ false }
-					allowedFormats={ [] }
-					value={ checkoutButtonLabel }
-					placeholder={ defaultCheckoutButtonLabel }
-					onChange={ ( content ) => {
-						setAttributes( {
-							checkoutButtonLabel: content,
-						} );
-					} }
-				/>
-			</Button>
+				<div className="wc-block-mini-cart__footer-actions">
+					<Button
+						className="wc-block-mini-cart__footer-cart"
+						variant="outlined"
+					>
+						<RichText
+							multiline={ false }
+							allowedFormats={ [] }
+							value={ cartButtonLabel }
+							placeholder={ defaultCartButtonLabel }
+							onChange={ ( content ) => {
+								setAttributes( {
+									cartButtonLabel: content,
+								} );
+							} }
+						/>
+					</Button>
+					<Button className="wc-block-mini-cart__footer-checkout">
+						<RichText
+							multiline={ false }
+							allowedFormats={ [] }
+							value={ checkoutButtonLabel }
+							placeholder={ defaultCheckoutButtonLabel }
+							onChange={ ( content ) => {
+								setAttributes( {
+									checkoutButtonLabel: content,
+								} );
+							} }
+						/>
+					</Button>
+				</div>
+				<PaymentEventsProvider>
+					<PaymentMethodIconsElement />
+				</PaymentEventsProvider>
+			</div>
 		</div>
 	);
 };
