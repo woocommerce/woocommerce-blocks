1d1f889f7e090356ce47594c8aa582b673a1940f
Niels Lange <info@nielslange.de>
2022-12-02 11:22:45 +0700

    Make Cart and Checkout buttons editable

--- assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-footer-block/edit.tsx
+++ assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-footer-block/edit.tsx
@@ -1,22 +1,57 @@
 /**
  * External dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
-import Noninteractive from '@woocommerce/base-components/noninteractive';
-
+import { useBlockProps, RichText } from '@wordpress/block-editor';
+import Button from '@woocommerce/base-components/button';
 /**
  * Internal dependencies
  */
-import Block from './block';
+import {
+	defaultCartButtonLabel,
+	defaultCheckoutButtonLabel,
+} from './constants';
 
-export const Edit = (): JSX.Element => {
+export const Edit = ( {
+	attributes,
+	setAttributes,
+}: {
+	attributes: {
+		cartButtonLabel: string;
+		checkoutButtonLabel: string;
+	};
+	setAttributes: ( attributes: Record< string, unknown > ) => void;
+} ): JSX.Element => {
 	const blockProps = useBlockProps();
+	const { cartButtonLabel, checkoutButtonLabel } = attributes;
 
 	return (
 		<div { ...blockProps }>
-			<Noninteractive>
-				<Block />
-			</Noninteractive>
+			<Button className="wc-block-mini-cart__footer-cart">
+				<RichText
+					multiline={ false }
+					allowedFormats={ [] }
+					value={ cartButtonLabel }
+					placeholder={ defaultCartButtonLabel }
+					onChange={ ( content ) => {
+						setAttributes( {
+							cartButtonLabel: content,
+						} );
+					} }
+				/>
+			</Button>
+			<Button className="wc-block-mini-cart__footer-checkout">
+				<RichText
+					multiline={ false }
+					allowedFormats={ [] }
+					value={ checkoutButtonLabel }
+					placeholder={ defaultCheckoutButtonLabel }
+					onChange={ ( content ) => {
+						setAttributes( {
+							checkoutButtonLabel: content,
+						} );
+					} }
+				/>
+			</Button>
 		</div>
 	);
 };
