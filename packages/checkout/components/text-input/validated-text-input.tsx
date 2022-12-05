569c0d9018300dd37892c3e9ae02891936acd1a0
Mike Jolley <mike.jolley@me.com>
2022-12-05 12:38:40 +0000

    Update packages/checkout/components/text-input/validated-text-input.tsx
    Co-authored-by: Thomas Roberts <5656702+opr@users.noreply.github.com>

--- packages/checkout/components/text-input/validated-text-input.tsx
+++ packages/checkout/components/text-input/validated-text-input.tsx
@@ -169,6 +169,7 @@ const ValidatedTextInput = ( {
 			onChange={ ( val ) => {
 				// Hide errors while typing.
 				hideValidationError( errorIdString );
+
 				// Revalidate on user input so we know if the value is valid.
 				validateInput( true );
 				// Push the changes up to the parent component if the value is valid.
