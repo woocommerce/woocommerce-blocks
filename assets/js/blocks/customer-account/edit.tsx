6be4e79d80a59efd0fb5e898dac66c3eeb2f0bab
Alexandre Lara <allexandrelara@gmail.com>
2022-12-07 17:24:04 -0300

    Add Block Settings to Customer Account block
    Add Typography and Color settings to the Customer Account block that can be used during Edit mode.

--- assets/js/blocks/customer-account/edit.tsx
+++ assets/js/blocks/customer-account/edit.tsx
@@ -1,18 +1,26 @@
 /**
  * External dependencies
  */
+import classNames from 'classnames';
+import { useBlockProps } from '@wordpress/block-editor';
 import { Disabled, withSpokenMessages } from '@wordpress/components';
+import type { BlockEditProps } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
  */
 import Block from './block';
+import { Attributes } from './types';
 
-const Edit = () => {
+const Edit = ( { attributes }: BlockEditProps< Attributes > ) => {
+	const { className } = attributes;
+	const blockProps = useBlockProps( {
+		className: classNames( 'wc-block-customer-account', className ),
+	} );
 	return (
 		<>
 			{
-				<div>
+				<div { ...blockProps }>
 					<Disabled>
 						<Block />
 					</Disabled>
