7ee9d1640745810ed1b718b9c5c3010e798afd81
Mike Jolley <mike.jolley@me.com>
2022-12-09 14:26:47 +0000

    Spacing

--- assets/js/base/context/hooks/use-checkout-address.ts
+++ assets/js/base/context/hooks/use-checkout-address.ts
@@ -39,11 +39,14 @@ interface CheckoutAddress {
  */
 export const useCheckoutAddress = (): CheckoutAddress => {
 	const { needsShipping } = useShippingData();
+
 	const useShippingAsBilling = useSelect( ( select ) =>
 		select( CHECKOUT_STORE_KEY ).getUseShippingAsBilling()
 	);
+
 	const { __internalSetUseShippingAsBilling } =
 		useDispatch( CHECKOUT_STORE_KEY );
+
 	const {
 		billingAddress,
 		setBillingAddress,
