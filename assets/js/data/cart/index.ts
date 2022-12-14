48de29adddeeb4174fb50abbbcbe2c1c4f3cd243
Thomas Roberts <thomas.roberts@automattic.com>
2022-12-14 00:22:14 +0000

    Remove controls from cart data store
    Not needed anymore since the exported value from the shared-controls file was empty.

--- assets/js/data/cart/index.ts
+++ assets/js/data/cart/index.ts
@@ -12,7 +12,6 @@ import * as selectors from './selectors';
 import * as actions from './actions';
 import * as resolvers from './resolvers';
 import reducer, { State } from './reducers';
-import { controls as sharedControls } from '../shared-controls';
 import { controls } from './controls';
 import type { SelectFromMap, DispatchFromMap } from '../mapped-types';
 import { pushChanges } from './push-changes';
@@ -28,7 +27,7 @@ const registeredStore = registerStore< State >( STORE_KEY, {
 	reducer,
 	actions,
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	controls: { ...dataControls, ...sharedControls, ...controls } as any,
+	controls: { ...dataControls, ...controls } as any,
 	selectors,
 	resolvers,
 } );
