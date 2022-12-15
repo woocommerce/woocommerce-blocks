d7369d4b6ff5e594a02ffe2c250663a4787b907b
Karol Manijak <karol.manijak@automattic.com>
2022-12-15 17:11:40 +0100

    Make Add review link NOT clickable in editor

--- assets/js/atomic/blocks/product-elements/rating/edit.tsx
+++ assets/js/atomic/blocks/product-elements/rating/edit.tsx
@@ -18,6 +18,7 @@ import Block from './block';
 import withProductSelector from '../shared/with-product-selector';
 import { BLOCK_TITLE, BLOCK_ICON } from './constants';
 import { Attributes } from './types';
+import './editor.scss';
 
 const Edit = ( {
 	attributes,
