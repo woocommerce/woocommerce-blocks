341135f8f7083d86031e0ba7fec8399f9cf4d64b
Mike Jolley <mike.jolley@me.com>
2022-12-19 13:57:24 +0000

    fix notice tests

--- assets/js/utils/notices.ts
+++ assets/js/utils/notices.ts
@@ -5,8 +5,8 @@ import { dispatch, select } from '@wordpress/data';
 import type { Notice } from '@wordpress/notices';
 
 export const hasNoticesOfType = (
-	context = '',
-	type: 'default' | 'snackbar'
+	type: 'default' | 'snackbar',
+	context?: string | undefined
 ): boolean => {
 	const notices: Notice[] = select( 'core/notices' ).getNotices( context );
 	return notices.some( ( notice: Notice ) => notice.type === type );
@@ -15,12 +15,12 @@ export const hasNoticesOfType = (
 // Note, if context is blank, the default context is used.
 export const removeNoticesByStatus = (
 	status: string,
-	context?: string
+	context?: string | undefined
 ): void => {
 	const notices = select( 'core/notices' ).getNotices( context );
 	const { removeNotice } = dispatch( 'core/notices' );
-	const noticesOfType = notices.filter( ( notice ) => {
-		return notice.status === status;
-	} );
+	const noticesOfType = notices.filter(
+		( notice ) => notice.status === status
+	);
 	noticesOfType.forEach( ( notice ) => removeNotice( notice.id, context ) );
 };
