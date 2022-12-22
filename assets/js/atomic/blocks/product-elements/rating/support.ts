42158f37e87e4f3d861d19588d4d97e36feaf9c5
Mike Jolley <mike.jolley@me.com>
2022-12-22 14:20:57 +0000

    Update assets/js/atomic/blocks/product-elements/rating/support.ts
    Co-authored-by: Saad Tarhi <saad.trh@gmail.com>

--- assets/js/atomic/blocks/product-elements/rating/support.ts
+++ assets/js/atomic/blocks/product-elements/rating/support.ts
@@ -17,7 +17,7 @@ export const supports = {
 			fontSize: true,
 			__experimentalSkipSerialization: true,
 		},
-		...( __experimentalGetSpacingClassesAndStyles === 'function' && {
+		...( typeof __experimentalGetSpacingClassesAndStyles === 'function' && {
 			spacing: {
 				margin: true,
 				__experimentalSkipSerialization: true,
