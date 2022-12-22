028767a1b477d84beaaa3e803700f268a0ce9686
Alba Rincón <alba.rincon@automattic.com>
2022-12-22 10:58:03 +0100

    Update library to latest version

---
+++ patterns/test.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Title: WooCommerce Test Pattern
+ * Slug: woocommerce-blocks/test-pattern
+ * Categories: WooCommerce
+ */
+?>
+<!-- wp:group {"backgroundColor":"tertiary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background">
+	<!-- wp:heading -->
+	<h2>Test pattern</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:paragraph {"backgroundColor":"pale-cyan-blue","textColor":"secondary"} -->
+	<p class="has-secondary-color has-pale-cyan-blue-background-color has-text-color has-background">
+		<strong>I am a test pattern with a heading and paragraph</strong>
+	</p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
+
