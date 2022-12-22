5ad7efe68166febdc0debf2e82b41f8d7b518e5f
Mike Jolley <mike.jolley@me.com>
2022-12-22 15:37:38 +0000

    revised error ids to reference in validation class

--- assets/js/base/components/cart-checkout/address-form/address-form.tsx
+++ assets/js/base/components/cart-checkout/address-form/address-form.tsx
@@ -42,7 +42,7 @@ const validateShippingCountry = (
 	clearValidationError: ( error: string ) => void,
 	hasValidationError: boolean
 ): void => {
-	const validationErrorId = 'shipping-missing-country';
+	const validationErrorId = 'shipping_country';
 	if (
 		! hasValidationError &&
 		! values.country &&
@@ -94,7 +94,7 @@ const AddressForm = ( {
 	type = 'shipping',
 	values,
 }: AddressFormProps ): JSX.Element => {
-	const validationErrorId = 'shipping-missing-country';
+	const validationErrorId = 'shipping_country';
 	const { setValidationErrors, clearValidationError } =
 		useDispatch( VALIDATION_STORE_KEY );
 
@@ -153,6 +153,10 @@ const AddressForm = ( {
 					return null;
 				}
 
+				// Create a consistent error ID based on the field key and type to reference later on when checking
+				// validation errors.
+				const errorId = `${ type }_${ field.key }`;
+
 				if ( field.key === 'country' ) {
 					const Tag =
 						type === 'shipping'
@@ -162,6 +166,7 @@ const AddressForm = ( {
 						<Tag
 							key={ field.key }
 							id={ `${ id }-${ field.key }` }
+							errorId={ errorId }
 							label={
 								field.required
 									? field.label
@@ -175,11 +180,6 @@ const AddressForm = ( {
 									country: newValue,
 									state: '',
 								} )
-							}
-							errorId={
-								type === 'shipping'
-									? 'shipping-missing-country'
-									: null
 							}
 							errorMessage={ field.errorMessage }
 							required={ field.required }
@@ -196,6 +196,7 @@ const AddressForm = ( {
 						<Tag
 							key={ field.key }
 							id={ `${ id }-${ field.key }` }
+							errorId={ errorId }
 							country={ values.country }
 							label={
 								field.required
@@ -220,6 +221,7 @@ const AddressForm = ( {
 					<ValidatedTextInput
 						key={ field.key }
 						id={ `${ id }-${ field.key }` }
+						errorId={ errorId }
 						className={ `wc-block-components-address-form__${ field.key }` }
 						label={
 							field.required ? field.label : field.optionalLabel
