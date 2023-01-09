55361f83de3a0a50e5a85df10111e4f3f5498eda
Alba Rincón <alba.rincon@automattic.com>
2023-01-09 14:28:17 +0100

    Simplify select

--- assets/js/blocks/catalog-sorting/block.tsx
+++ assets/js/blocks/catalog-sorting/block.tsx
@@ -1,12 +1,8 @@
 const CatalogSorting = () => {
 	return (
-		<form className="woocommerce-ordering" method="get">
-			<select name="orderby" className="orderby" aria-label="Shop order">
-				<option value="menu_order" selected="selected">
-					Default sorting
-				</option>
-			</select>
-		</form>
+		<select>
+			<option>Default sorting</option>
+		</select>
 	);
 };
 
