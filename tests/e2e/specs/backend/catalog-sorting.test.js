02801438514bcdec423dc551ef79675ac6539b2c
Alba Rincón <alba.rincon@automattic.com>
2023-01-10 14:53:13 +0100

    Rename test file and add new tests for catalog-sorting block

---
+++ tests/e2e/specs/backend/catalog-sorting.test.js
@@ -0,0 +1,33 @@
+/**
+ * External dependencies
+ */
+import { getAllBlocks, switchUserToAdmin } from '@wordpress/e2e-test-utils';
+
+import { visitBlockPage } from '@woocommerce/blocks-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import { insertBlockDontWaitForInsertClose } from '../../utils.js';
+
+const block = {
+	name: 'Catalog Sorting',
+	slug: 'woocommerce/catalog-sorting',
+	class: '.wc-block-catalog-sorting',
+};
+
+describe( `${ block.name } Block`, () => {
+	beforeAll( async () => {
+		await switchUserToAdmin();
+		await visitBlockPage( `${ block.name } Block` );
+	} );
+
+	it( 'renders without crashing', async () => {
+		await expect( page ).toRenderBlock( block );
+	} );
+
+	it( 'can be inserted more than once', async () => {
+		await insertBlockDontWaitForInsertClose( block.name );
+		expect( await getAllBlocks() ).toHaveLength( 2 );
+	} );
+} );
