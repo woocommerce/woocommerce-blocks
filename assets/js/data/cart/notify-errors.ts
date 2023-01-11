a9ac8bcbbf389e29e5d05056151ef20ee1454619
Mike Jolley <mike.jolley@me.com>
2023-01-11 16:13:13 +0000

    Update DEFAULT_ERROR_MESSAGE

--- assets/js/data/cart/notify-errors.ts
+++ assets/js/data/cart/notify-errors.ts
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { ApiErrorResponse, isApiErrorResponse } from '@woocommerce/types';
-import { createNotice } from '@woocommerce/base-utils';
+import { createNotice, DEFAULT_ERROR_MESSAGE } from '@woocommerce/base-utils';
 
 /**
  * This function is used to notify the user of cart errors.
@@ -11,7 +11,7 @@ export const notifyErrors = ( errors: ApiErrorResponse | null = null ) => {
 	if ( errors === null || ! isApiErrorResponse( errors ) ) {
 		return;
 	}
-	createNotice( 'error', errors.message || 'An error occurred', {
+	createNotice( 'error', errors.message || DEFAULT_ERROR_MESSAGE, {
 		id: errors.code,
 		context: 'wc/cart',
 	} );
