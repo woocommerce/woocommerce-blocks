de6febf071653c72c5bec16f0a0ab0834bd233b5
Karol Manijak <karol.manijak@automattic.com>
2023-01-13 11:33:53 +0100

    Fix TS issue in Product Results Count

--- assets/js/blocks/product-results-count/block.tsx
+++ assets/js/blocks/product-results-count/block.tsx
@@ -2,15 +2,29 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import classNames from 'classnames';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 
-const ProductResultsCount = () => {
+interface Props {
+	attributes: {
+		className?: string;
+	};
+}
+
+const ProductResultsCount = ( { attributes }: Props ): JSX.Element => {
+	const { className } = attributes;
+
 	return (
-		<div className=".wc-block-product-results-count">
+		<div
+			className={ classNames(
+				'.wc-block-product-results-count',
+				className
+			) }
+		>
 			{ __( 'Showing 1-X of X results', 'woo-gutenberg-products-block' ) }
 		</div>
 	);
