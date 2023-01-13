678e682edf853580051fd9f609c8b4c165126071
Karol Manijak <karol.manijak@automattic.com>
2023-01-13 11:02:00 +0100

    Make sure the Product Results Count is available inly in Product Catalog template

--- assets/js/blocks/product-results-count/block.tsx
+++ assets/js/blocks/product-results-count/block.tsx
@@ -10,7 +10,7 @@ import './style.scss';
 
 const ProductResultsCount = () => {
 	return (
-		<div className="woocommerce-notice">
+		<div className=".wc-block-product-results-count">
 			{ __( 'Showing 1-X of X results', 'woo-gutenberg-products-block' ) }
 		</div>
 	);
