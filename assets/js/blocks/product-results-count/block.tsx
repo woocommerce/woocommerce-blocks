cf2506be389818be1fb1d81d3f6ad0b5ea46b037
Karol Manijak <karol.manijak@automattic.com>
2023-01-13 10:10:01 +0100

    Improve the way Product Results Count is rendered in the editor

--- assets/js/blocks/product-results-count/block.tsx
+++ assets/js/blocks/product-results-count/block.tsx
@@ -1,10 +1,19 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import './style.scss';
 
-const ProductResultsCount = ( props ) => {
-	return <p>Results { JSON.stringify( props ) }</p>;
+const ProductResultsCount = () => {
+	return (
+		<div className="woocommerce-notice">
+			{ __( 'Showing 1-X of X results', 'woo-gutenberg-products-block' ) }
+		</div>
+	);
 };
 
 export default ProductResultsCount;
