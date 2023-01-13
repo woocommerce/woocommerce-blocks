3cf2cb57a6e60340d3b72df5703b888e5e00f5ab
Alba Rincón <albarin@users.noreply.github.com>
2023-01-13 13:44:37 +0100

    Override the `enqueue_assets`
    Co-authored-by: Tung Du <dinhtungdu@gmail.com>

--- src/BlockTypes/CatalogSorting.php
+++ src/BlockTypes/CatalogSorting.php
@@ -38,4 +38,13 @@ class CatalogSorting extends AbstractBlock {
 
 		return "<div class='wc-block-catalog-sorting " . esc_attr( $classes_and_styles['classes'] ) . "' style='" . esc_attr( $classes_and_styles['styles'] ) . "'>" . $catalog_sorting . '</div>';
 	}
+
+	/**
+	 * Get the frontend script handle for this block type.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 */
+	protected function get_block_type_script( $key = null ) {
+		return null;
+	}
 }
