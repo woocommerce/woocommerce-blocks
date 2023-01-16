ee47a82b5e4cd6cb01194e3860d022d7988dafac
Alba Rincón <alba.rincon@automattic.com>
2023-01-16 12:40:27 +0100

    Fix copy

--- assets/js/blocks/store-notices/edit.tsx
+++ assets/js/blocks/store-notices/edit.tsx
@@ -15,7 +15,7 @@ const StoreNotices = () => {
 	return (
 		<div className="wc-block-woocommerce-notice">
 			{ __(
-				'This is an example notice. Notices added WooCommerce or extensions will show up here.',
+				'This is an example notice. Notices added by WooCommerce or extensions will show up here.',
 				'woo-gutenberg-products-block'
 			) }
 		</div>
