842b06ccc767ec2bff83ebdefbd0502d6b22dd4b
Alba Rincón <alba.rincon@automattic.com>
2023-01-16 11:37:52 +0100

    Improve class name

--- assets/js/blocks/store-notices/block.tsx
+++ assets/js/blocks/store-notices/block.tsx
@@ -5,7 +5,7 @@ import { __ } from '@wordpress/i18n';
 
 const StoreNotices = () => {
 	return (
-		<div className="woocommerce-notice">
+		<div className="wc-block-woocommerce-notice">
 			{ __(
 				'This is an example notice. Notices added WooCommerce or extensions will show up here.',
 				'woo-gutenberg-products-block'
