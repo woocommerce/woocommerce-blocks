e2dedbbbc7c7163f6884b6f9ef034c52f31bde22
Alba Rincón <alba.rincon@automattic.com>
2023-01-17 11:06:06 +0100

    Simplify edit component

--- assets/js/blocks/product-results-count/edit.tsx
+++ assets/js/blocks/product-results-count/edit.tsx
@@ -1,33 +1,27 @@
 /**
  * External dependencies
  */
-import classNames from 'classnames';
 import { useBlockProps } from '@wordpress/block-editor';
-import type { BlockEditProps } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
 
 export interface Attributes {
 	className?: string;
 }
 
-const ProductResultsCount = (): JSX.Element => {
-	return (
-		<div>
-			{ __( 'Showing 1-X of X results', 'woo-gutenberg-products-block' ) }
-		</div>
-	);
-};
-
-const Edit = ( { attributes }: BlockEditProps< Attributes > ) => {
-	const { className } = attributes;
+const Edit = () => {
 	const blockProps = useBlockProps( {
-		className: classNames( 'wc-block-product-results-count', className ),
+		className: 'wc-block-product-results-count',
 	} );
 
 	return (
 		<>
 			<div { ...blockProps }>
-				<ProductResultsCount />
+				<div>
+					{ __(
+						'Showing 1-X of X results',
+						'woo-gutenberg-products-block'
+					) }
+				</div>
 			</div>
 		</>
 	);
