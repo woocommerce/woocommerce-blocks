1315906a0643a7d0638731cff61ba7b838782dff
Tung Du <dinhtungdu@gmail.com>
2023-01-18 01:40:47 +0700

    refactor the data structure

--- src/Templates/BlockTemplatesCompatibility.php
+++ src/Templates/BlockTemplatesCompatibility.php
@@ -95,118 +95,189 @@ class BlockTemplatesCompatibility {
 
 		$hook_data = $this->get_hook_data();
 
-		if ( ! in_array( $block_name, array_keys( $hook_data ), true ) ) {
+		$supported_blocks = array_map(
+			function( $hook ) {
+				return $hook['block_name'];
+			},
+			array_values( $hook_data )
+		);
+
+		if ( ! in_array( $block_name, $supported_blocks, true ) ) {
 			return $block_content;
 		}
 
 		/**
-		 * If the block is empty, we don't need to inject hooks.
+		 * `core/query-no-result` is a special case because it can return two
+		 * different content depending on the context. We need to check if the
+		 * block content is empty to determine if we need to inject hooks.
 		 */
-		if ( empty( $block_content ) ) {
+		if (
+			'core/query-no-results' === $block_name &&
+			empty( trim( $block_content ) )
+		) {
 			return $block_content;
 		}
 
-		$hooks = $hook_data[ $block_name ];
+		$block_hooks = array_filter(
+			$hook_data,
+			function( $hook ) use ( $block_name ) {
+				return $hook['block_name'] === $block_name;
+			}
+		);
 
 		return sprintf(
 			'%1$s%2$s%3$s',
-			$this->get_hooks_buffer( $hooks, 'before' ),
+			$this->get_hooks_buffer( $block_hooks, 'before' ),
 			$block_content,
-			$this->get_hooks_buffer( $hooks, 'after' )
+			$this->get_hooks_buffer( $block_hooks, 'after' )
 		);
 	}
 
 	/**
-	 * Hoding the supported hooks, their positions and the hooked functions.
+	 * The hook data to inject to the rendered content of blocks. This also
+	 * contains hooked functions that will be removed by remove_default_hooks.
+	 *
+	 * The array format:
+	 * [
+	 *   <hook-name> => [
+	 *     block_name => <block-name>,
+	 *     position => before|after,
+	 *     hooked => [
+	 *       <function-name> => <priority>,
+	 *        ...
+	 *     ],
+	 *  ],
+	 * ]
+	 * Where:
+	 * - hook-name is the name of the hook that will be replaced.
+	 * - block-name is the name of the block that will replace the hook.
+	 * - position is the position of the block relative to the hook.
+	 * - hooked is an array of functions hooked to the hook that will be
+	 *   replaced. The key is the function name and the value is the
+	 *   priority.
 	 *
 	 * @return array Hook data.
 	 */
 	protected function get_hook_data() {
 		$hook_data = array(
-			'core/query'            => array(
-				'woocommerce_before_main_content' => array(
-					'position' => 'before',
-					'hooked'   => array(
-						'woocommerce_output_content_wrapper' => 10,
-						'woocommerce_breadcrumb' => 20,
-					),
-				),
-				'woocommerce_after_main_content'  => array(
-					'position' => 'after',
-					'hooked'   => array(
-						'woocommerce_output_content_wrapper_end' => 10,
-					),
+			'woocommerce_before_main_content'         => array(
+				'block_name' => 'core/query',
+				'position'   => 'before',
+				'hooked'     => array(
+					'woocommerce_output_content_wrapper' => 10,
+					'woocommerce_breadcrumb'             => 20,
 				),
 			),
-			'core/post-title'       => array(
-				'woocommerce_before_shop_loop_item_title' => array(
-					'position' => 'before',
-					'hooked'   => array(
-						'woocommerce_show_product_loop_sale_flash' => 10,
-						'woocommerce_template_loop_product_thumbnail' => 10,
-					),
-				),
-				'woocommerce_shop_loop_item_title'        => array(
-					'position' => 'after',
-					'hooked'   => array(
-						'woocommerce_template_loop_product_title' => 10,
-					),
-				),
-				'woocommerce_after_shop_loop_item_title'  => array(
-					'position' => 'before',
-					'hooked'   => array(
-						'woocommerce_template_loop_rating' => 5,
-						'woocommerce_template_loop_price'  => 10,
-					),
+			'woocommerce_after_main_content'          => array(
+				'block_name' => 'core/query',
+				'position'   => 'after',
+				'hooked'     => array(
+					'woocommerce_output_content_wrapper_end' => 10,
 				),
 			),
-			self::LOOP_ITEM_ID      => array(
-				'woocommerce_before_shop_loop_item' => array(
-					'position' => 'before',
-					'hooked'   => array(
-						'woocommerce_template_loop_product_link_open' => 10,
-					),
-				),
-				'woocommerce_after_shop_loop_item'  => array(
-					'position' => 'after',
-					'hooked'   => array(
-						'woocommerce_template_loop_product_link_close' => 5,
-						'woocommerce_template_loop_add_to_cart' => 10,
-					),
+			'woocommerce_before_shop_loop_item_title' => array(
+				'block_name' => 'core/post-title',
+				'position'   => 'before',
+				'hooked'     => array(
+					'woocommerce_show_product_loop_sale_flash' => 10,
+					'woocommerce_template_loop_product_thumbnail' => 10,
 				),
 			),
-			'core/post-template'    => array(
-				'woocommerce_before_shop_loop' => array(
-					'position' => 'before',
-					'hooked'   => array(
-						'woocommerce_output_all_notices' => 10,
-						'woocommerce_result_count'       => 20,
-						'woocommerce_catalog_ordering'   => 30,
-					),
-				),
-				'woocommerce_after_shop_loop'  => array(
-					'position' => 'after',
-					'hooked'   => array(
-						'woocommerce_pagination' => 10,
-					),
+			'woocommerce_shop_loop_item_title'        => array(
+				'block_name' => 'core/post-title',
+				'position'   => 'after',
+				'hooked'     => array(
+					'woocommerce_template_loop_product_title' => 10,
 				),
 			),
-			'core/query-no-results' => array(
-				'woocommerce_no_products_found' => array(
-					'position' => 'before',
-					'hooked'   => array(
-						'wc_no_products_found' => 10,
-					),
+			'woocommerce_after_shop_loop_item_title'  => array(
+				'block_name' => 'core/post-title',
+				'position'   => 'before',
+				'hooked'     => array(
+					'woocommerce_template_loop_rating' => 5,
+					'woocommerce_template_loop_price'  => 10,
+				),
+			),
+			'woocommerce_before_shop_loop_item'       => array(
+				'block_name' => self::LOOP_ITEM_ID,
+				'position'   => 'before',
+				'hooked'     => array(
+					'woocommerce_template_loop_product_link_open' => 10,
+				),
+			),
+			'woocommerce_after_shop_loop_item'        => array(
+				'block_name' => self::LOOP_ITEM_ID,
+				'position'   => 'after',
+				'hooked'     => array(
+					'woocommerce_template_loop_product_link_close' => 5,
+					'woocommerce_template_loop_add_to_cart' => 10,
+				),
+			),
+			'woocommerce_before_shop_loop'            => array(
+				'block_name' => 'core/post-template',
+				'position'   => 'before',
+				'hooked'     => array(
+					'woocommerce_output_all_notices' => 10,
+					'woocommerce_result_count'       => 20,
+					'woocommerce_catalog_ordering'   => 30,
+				),
+			),
+			'woocommerce_after_shop_loop'             => array(
+				'block_name' => 'core/post-template',
+				'position'   => 'after',
+				'hooked'     => array(
+					'woocommerce_pagination' => 10,
+				),
+			),
+			'woocommerce_no_products_found'           => array(
+				'block_name' => 'core/query-no-results',
+				'position'   => 'before',
+				'hooked'     => array(
+					'wc_no_products_found' => 10,
 				),
 			),
 		);
 
 		/**
-		 * Filter the hook data used to replace the default WooCommerce hooks.
+		 * When extentions implement their equivalent blocks of the template
+		 * hook functions, they can use this filter to register their old hooked
+		 * data here, so in the blockified template, the old hooked functions
+		 * can be removed in favor of the new blocks while keeping the old
+		 * hooked functions working in classic templates.
 		 *
-		 * @param array $hook_data Hook data.
+		 * Accepts an array of hooked data. The array should be in the following
+		 * format:
+		 * [
+		 *   [
+		 *     hook => <hook-name>,
+		 *     function => <function-name>,
+		 *     priority => <priority>,
+		 *  ],
+		 *  ...
+		 * ]
+		 * Where:
+		 * - hook-name is the name of the hook that have the functions hooked to.
+		 * - function-name is the hooked function name.
+		 * - priority is the priority of the hooked function.
+		 *
+		 * @param array $data Additional hooked data. Default to empty
 		 */
-		return apply_filters( 'woocommerce_blocks_hooks_compatibility_data', $hook_data );
+		$additional_hook_data = apply_filters( 'woocommerce_blocks_hook_compatibility_additional_hook_data', array() );
+
+		foreach ( $additional_hook_data as $data ) {
+			if (
+				! is_string( $data['hook'] ) ||
+				! is_int( $data['priority'] ) ||
+				! in_array( $data['hook'], array_keys( $hook_data ), true ) ||
+				isset( $hook_data[ $data['hook'] ]['hooked'][ $data['function'] ] )
+			) {
+				continue;
+			}
+
+			$hook_data[ $data['hook'] ]['hooked'][ $data['function'] ] = $data['priority'];
+		}
+
+		return $hook_data;
 	}
 
 	/**
@@ -222,8 +293,7 @@ class BlockTemplatesCompatibility {
 	 * content.
 	 */
 	protected function remove_default_hooks() {
-		$hooks = array_merge( ...array_values( $this->get_hook_data() ) );
-		foreach ( $hooks as $hook => $data ) {
+		foreach ( $this->get_hook_data() as $hook => $data ) {
 			if ( ! isset( $data['hooked'] ) ) {
 				continue;
 			}
@@ -243,8 +313,8 @@ class BlockTemplatesCompatibility {
 	 */
 	protected function get_hooks_buffer( $hooks, $position ) {
 		ob_start();
-		foreach ( array_keys( $hooks ) as $hook ) {
-			if ( $hooks[ $hook ]['position'] === $position ) {
+		foreach ( $hooks as $hook => $data ) {
+			if ( $data['position'] === $position ) {
 				do_action( $hook );
 			}
 		}
