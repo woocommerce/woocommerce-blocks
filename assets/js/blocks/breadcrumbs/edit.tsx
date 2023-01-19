0400541215e3d4238816e193d84dcc638e598ce0
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-01-19 23:19:49 +0100

    Update the breadcrumbs block details.

--- assets/js/blocks/breadcrumbs/edit.tsx
+++ assets/js/blocks/breadcrumbs/edit.tsx
@@ -2,26 +2,24 @@
  * External dependencies
  */
 import { useBlockProps } from '@wordpress/block-editor';
-import { Disabled } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
 
-/**
- * Internal dependencies
- */
-import Block from './block';
+export interface Attributes {
+	className?: string;
+}
 
 const Edit = () => {
 	const blockProps = useBlockProps( {
-		className: 'wc-block-breadcrumbs',
+		className: 'woocommerce wc-block-breadcrumbs',
 	} );
 
 	return (
-		<>
-			<div { ...blockProps }>
-				<Disabled>
-					<Block />
-				</Disabled>
-			</div>
-		</>
+		<div { ...blockProps }>
+			{ __(
+				'Breadcrumbs / Navigation / Path',
+				'woo-gutenberg-products-block'
+			) }
+		</div>
 	);
 };
 
