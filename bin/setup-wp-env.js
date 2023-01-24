b4c9b78e558bc0e7a95d6365f07cdd1a59391825
Luigi <gigitux@gmail.com>
2023-01-24 11:34:58 +0100

    Run E2E tests on old WordPress versions

--- bin/setup-wp-env.js
+++ bin/setup-wp-env.js
@@ -4,11 +4,19 @@ const path = require( 'path' );
 const wpEnvRaw = fs.readFileSync(
 	path.join( __dirname, '..', '.wp-env.json' )
 );
+
 const wpEnv = JSON.parse( wpEnvRaw );
-wpEnv.plugins.push(
-	'https://downloads.wordpress.org/plugin/gutenberg.latest-stable.zip'
-);
-// We write the new file to .wp-env.override.json (https://developer.wordpress.org/block-editor/reference-guides/packages/packages-env/#wp-env-override-json)
+
+if ( process.env.GUTENBERG_EDITOR_CONTEXT ) {
+	wpEnv.plugins.push(
+		'https://downloads.wordpress.org/plugin/gutenberg.latest-stable.zip'
+	);
+}
+
+if ( process.env.WORDPRESS_VERSION ) {
+	wpEnv.core = `WordPress/WordPress#${ process.env.WORDPRESS_VERSION }`;
+}
+
 fs.writeFileSync(
 	path.join( __dirname, '..', '.wp-env.override.json' ),
 	JSON.stringify( wpEnv )
