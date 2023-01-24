c3bbc70ea247dd448d6fd42330b86220d5be5c5a
Karol Manijak <karol.manijak@automattic.com>
2023-01-24 10:54:52 +0100

    Add a Row block wrapping Product Results Count and Catalog Sorting blocks

--- templates/templates/blockified/archive-product.ts
+++ templates/templates/blockified/archive-product.ts
@@ -2,8 +2,10 @@
  * External dependencies
  */
 import {
+	getBlockType,
 	createBlock,
 	createBlocksFromInnerBlocksTemplate,
+	type BlockInstance,
 } from '@wordpress/blocks';
 
 /**
@@ -12,12 +14,28 @@ import {
 import {
 	INNER_BLOCKS_TEMPLATE as productsInnerBlocksTemplate,
 	QUERY_DEFAULT_ATTRIBUTES as productsQueryDefaultAttributes,
-} from '../../../assets/js/blocks/product-query/constants';
-import { VARIATION_NAME as productsVariationName } from '../../../assets/js/blocks/product-query/variations/product-query';
+} from './constants';
+import { VARIATION_NAME as productsVariationName } from './variations/product-query';
 
-const productArchiveClassName = 'woo-blocks-product-archive-template';
-const commonAttributes = {
-	className: productArchiveClassName,
+const createRowBlock = ( innerBlocks: Array< BlockInstance > ) => {
+	const rowVariationName = `group-row`;
+	const groupBlockVariations = getBlockType( 'core/group' )?.variations || [];
+	const rowVariation = groupBlockVariations.find(
+		( { name }: { name: string } ) => name === rowVariationName
+	);
+
+	if ( ! rowVariation ) return null;
+
+	const { attributes } = rowVariation;
+	const extendedAttributes = {
+		...attributes,
+		layout: {
+			...attributes.layout,
+			justifyContent: 'space-between',
+		},
+	};
+
+	return createBlock( `core/group`, extendedAttributes, innerBlocks );
 };
 
 const createProductsBlock = () =>
@@ -25,15 +43,17 @@ const createProductsBlock = () =>
 		'core/query',
 		{
 			...productsQueryDefaultAttributes,
-			...commonAttributes,
 			namespace: productsVariationName,
 		},
 		createBlocksFromInnerBlocksTemplate( productsInnerBlocksTemplate )
 	);
 
-export const getProductArchiveTemplate = () => [
-	createBlock( 'woocommerce/store-notices', commonAttributes ),
-	createBlock( 'woocommerce/product-results-count', commonAttributes ),
-	createBlock( 'woocommerce/catalog-sorting', commonAttributes ),
-	createProductsBlock(),
-];
+export const getProductArchiveTemplate = () =>
+	[
+		createBlock( 'woocommerce/store-notices' ),
+		createRowBlock( [
+			createBlock( 'woocommerce/product-results-count' ),
+			createBlock( 'woocommerce/catalog-sorting' ),
+		] ),
+		createProductsBlock(),
+	].filter( Boolean );
