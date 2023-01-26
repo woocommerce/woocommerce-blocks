b934a0e0ee2b37ebfdfcb770ca0de6b7a76b54d5
Thomas Roberts <thomas.roberts@automattic.com>
2023-01-26 11:54:11 +0000

    Add pickup location component

---
+++ assets/js/base/components/cart-checkout/pickup-location/index.tsx
@@ -0,0 +1,83 @@
+/**
+ * External dependencies
+ */
+import { __, sprintf } from '@wordpress/i18n';
+import { getSetting } from '@woocommerce/settings';
+import { useSelect } from '@wordpress/data';
+import { isObject, objectHasProp } from '@woocommerce/types';
+
+/**
+ * Shows a formatted pickup location.
+ */
+const PickupLocation = (): JSX.Element | null => {
+	const collectibleMethodIds = [
+		...getSetting< string[] >( 'collectibleMethodIds', [] ),
+		'pickup_location',
+	];
+
+	const { pickupAddress, pickupMethod } = useSelect( ( select ) => {
+		const cartShippingRates = select( 'wc/store/cart' ).getShippingRates();
+
+		const flattenedRates = cartShippingRates.flatMap(
+			( cartShippingRate ) => cartShippingRate.shipping_rates
+		);
+		const selectedCollectibleRate = flattenedRates.find(
+			( rate ) =>
+				rate.selected && collectibleMethodIds.includes( rate.method_id )
+		);
+
+		// If the rate has an address specified in its metadata.
+		if (
+			isObject( selectedCollectibleRate ) &&
+			objectHasProp( selectedCollectibleRate, 'meta_data' )
+		) {
+			const selectedRateMetaData = selectedCollectibleRate.meta_data.find(
+				( meta ) => meta.key === 'pickup_address'
+			);
+			if (
+				isObject( selectedRateMetaData ) &&
+				objectHasProp( selectedRateMetaData, 'value' )
+			) {
+				const selectedRatePickupAddress = selectedRateMetaData.value;
+				return {
+					pickupAddress: selectedRatePickupAddress,
+					pickupMethod: selectedCollectibleRate.name,
+				};
+			}
+		}
+
+		if ( isObject( selectedCollectibleRate ) ) {
+			return {
+				pickupAddress: undefined,
+				pickupMethod: selectedCollectibleRate.name,
+			};
+		}
+		return {
+			pickupAddress: undefined,
+			pickupMethod: undefined,
+		};
+	} );
+
+	// If the method does not contain an address, or the method supporting collection was not found, return early.
+	if (
+		typeof pickupAddress === 'undefined' &&
+		typeof pickupMethod === 'undefined'
+	) {
+		return null;
+	}
+
+	// Show the pickup method's name if we don't have an address to show.
+	return (
+		<span className="wc-block-components-shipping-address">
+			{ sprintf(
+				/* translators: %s: shipping method name, e.g. "Amazon Locker" */
+				__( 'Collection from %s', 'woo-gutenberg-products-block' ),
+				typeof pickupAddress === 'undefined'
+					? pickupMethod
+					: pickupAddress
+			) + ' ' }
+		</span>
+	);
+};
+
+export default PickupLocation;
