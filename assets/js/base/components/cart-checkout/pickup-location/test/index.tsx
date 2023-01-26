a40523566b81dd4d4f9fe72e8065b287e6856825
Thomas Roberts <thomas.roberts@automattic.com>
2023-01-26 15:02:12 +0000

    Move PickupLocation specific tests to new file

--- assets/js/base/components/cart-checkout/pickup-location/test/index.tsx
+++ assets/js/base/components/cart-checkout/pickup-location/test/index.tsx
@@ -2,10 +2,10 @@
  * External dependencies
  */
 import { render, screen } from '@testing-library/react';
-import ShippingAddress from '@woocommerce/base-components/cart-checkout/totals/shipping/shipping-address';
 import { CART_STORE_KEY, CHECKOUT_STORE_KEY } from '@woocommerce/block-data';
 import { dispatch } from '@wordpress/data';
 import { previewCart } from '@woocommerce/resource-previews';
+import PickupLocation from '@woocommerce/base-components/cart-checkout/pickup-location';
 
 jest.mock( '@woocommerce/settings', () => {
 	const originalModule = jest.requireActual( '@woocommerce/settings' );
@@ -25,35 +25,8 @@ jest.mock( '@woocommerce/settings', () => {
 		},
 	};
 } );
-describe( 'ShippingAddress', () => {
-	const testShippingAddress = {
-		first_name: 'John',
-		last_name: 'Doe',
-		company: 'Automattic',
-		address_1: '123 Main St',
-		address_2: '',
-		city: 'San Francisco',
-		state: 'CA',
-		postcode: '94107',
-		country: 'US',
-		phone: '555-555-5555',
-	};
-
-	it( 'renders ShippingLocation if user does not prefer collection', () => {
-		render(
-			<ShippingAddress
-				showCalculator={ false }
-				isShippingCalculatorOpen={ false }
-				setIsShippingCalculatorOpen={ jest.fn() }
-				shippingAddress={ testShippingAddress }
-			/>
-		);
-		expect( screen.getByText( /Shipping to 94107/ ) ).toBeInTheDocument();
-		expect(
-			screen.queryByText( /Collection from/ )
-		).not.toBeInTheDocument();
-	} );
-	it( 'renders PickupLocation if shopper prefers collection', async () => {
+describe( 'PickupLocation', () => {
+	it( `renders an address if one is set in the method's metadata`, async () => {
 		dispatch( CHECKOUT_STORE_KEY ).setPrefersCollection( true );
 
 		// Deselect the default selected rate and select pickup_location:1 rate.
@@ -74,18 +47,47 @@ describe( 'ShippingAddress', () => {
 
 		dispatch( CART_STORE_KEY ).receiveCart( previewCart );
 
-		render(
-			<ShippingAddress
-				showCalculator={ false }
-				isShippingCalculatorOpen={ false }
-				setIsShippingCalculatorOpen={ jest.fn() }
-				shippingAddress={ testShippingAddress }
-			/>
-		);
+		render( <PickupLocation /> );
 		expect(
 			screen.getByText(
 				/Collection from 123 Easy Street, New York, 12345/
 			)
+		).toBeInTheDocument();
+	} );
+	it( 'renders the method name if address is not in metadata', async () => {
+		dispatch( CHECKOUT_STORE_KEY ).setPrefersCollection( true );
+
+		// Deselect the default selected rate and select pickup_location:1 rate.
+		const currentlySelectedIndex =
+			previewCart.shipping_rates[ 0 ].shipping_rates.findIndex(
+				( rate ) => rate.selected
+			);
+		previewCart.shipping_rates[ 0 ].shipping_rates[
+			currentlySelectedIndex
+		].selected = false;
+		const pickupRateIndex =
+			previewCart.shipping_rates[ 0 ].shipping_rates.findIndex(
+				( rate ) => rate.rate_id === 'pickup_location:2'
+			);
+		previewCart.shipping_rates[ 0 ].shipping_rates[
+			pickupRateIndex
+		].selected = true;
+
+		// Set the pickup_location metadata value to an empty string in the selected pickup rate.
+		const addressKeyIndex = previewCart.shipping_rates[ 0 ].shipping_rates[
+			pickupRateIndex
+		].meta_data.findIndex(
+			( metaData ) => metaData.key === 'pickup_address'
+		);
+		previewCart.shipping_rates[ 0 ].shipping_rates[
+			pickupRateIndex
+		].meta_data[ addressKeyIndex ].value = '';
+
+		dispatch( CART_STORE_KEY ).receiveCart( previewCart );
+
+		render( <PickupLocation /> );
+		expect(
+			screen.getByText( /Collection from Local pickup/ )
 		).toBeInTheDocument();
 	} );
 } );
