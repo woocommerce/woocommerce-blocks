d07b9b5559a2ef1b7a8c6f143ee08906b52b0ae7
Alexandre Lara <allexandrelara@gmail.com>
2023-01-27 12:59:11 -0300

    Render tabs title with empty content

--- src/BlockTypes/SingleProductDetails.php
+++ src/BlockTypes/SingleProductDetails.php
@@ -25,89 +25,73 @@ class SingleProductDetails extends AbstractBlock {
 	 * @return string Rendered block output.
 	 */
 	protected function render( $attributes, $content, $block ) {
-		ob_start();
-
-		while ( have_posts() ) {
-			global $product;
-			the_post();
-			the_content();
-		}
-
-		$description = ob_get_clean();
-
-		ob_start();
-
-		while ( have_posts() ) {
-			global $product;
-			the_post();
-			wc_display_product_attributes($product);
-		}
-
-		$product_attributes = ob_get_clean();
-
-		while ( have_posts() ) {
-			global $product;
-			the_post();
-			wp_list_comments( array( 'post_id' => the_ID() ) );
-		}
-
-		$comments = ob_get_clean();
-
-		if ( ! $product ) {
-			return 'hello';
-		}
+		$tabs = $this->render_tabs();
 
 		$classes_and_styles = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes );
 
 		return sprintf(
 			'<div class="wp-block-woocommerce-single-product-details %1$s" style="%2$s">
 				<div class="woocommerce-tabs wc-tabs-wrapper">
-					<ul class="tabs" role="tablist">
-						<li>
-							<a href="#tab-description">
-								Description
-							</a>
-						</li>
-						<li>
-							<a href="#tab-additional-information">
-								Additional Information
-							</a>
-						</li>
-						<li>
-							<a href="#reviews">
-								Reviews
-							</a>
-						</li>
-					</ul>
-
-					<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--description panel entry-content wc-tab" id="tab-description" role="tabpanel" aria-labelledby="tab-title-description">
-						<h2>Description</h2>
-						<div class="woocommerce-product-details__short-description">
-							%3$s
-						</div>
-					</div>
-
-					<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--additional-information panel entry-content wc-tab" id="tab-additional-information" role="tabpanel" aria-labelledby="tab-title-additional-information">
-						<h2>Additional Information</h2>
-						<div class="woocommerce-product-details__short-additional-information">
-							%4$s
-						</div>
-					</div>
-
-					<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--reviews panel entry-content wc-tab" id="tab-reviews" role="tabpanel" aria-labelledby="tab-title-reviews">
-						<h2>Reviews</h2>
-						<div class="woocommerce-product-details__short-reviews">
-							%5$s
-						</div>
-					</div>
-
+					%3$s
 				</div>
 			</div>',
 			esc_attr( $classes_and_styles['classes'] ),
 			esc_attr( $classes_and_styles['styles'] ),
-			$description,
-			$product_attributes,
-			$comments
+			$tabs,
 		);
+	}
+
+	protected function render_tabs(){
+		ob_start();
+
+		while ( have_posts() ) {
+			global $product, $post;
+			the_post();
+			$product_tabs = apply_filters( 'woocommerce_product_tabs', array() );
+			$tabs_title = '';
+			$tabs_content = '';
+			if ( ! empty( $product_tabs ) ) {
+				foreach ( $product_tabs as $key => $product_tab  ) {
+					$tabs_title .= sprintf('
+						<li id="tab-title-%1$s" role="tab" aria-controls="tab-%1$s">
+							<a href="#tab-%1$s">
+								%2$s
+							</a>
+						</li>
+						',
+						esc_attr( $key ),
+						esc_attr( $product_tab['title'] ),
+					);
+
+					$tabs_content .= sprintf('
+						<div class="woocommerce-Tabs-panel woocommerce-Tabs-panel--%1$s panel entry-content wc-tab" id="tab-%1$s" role="tabpanel" aria-labelledby="tab-title-%1$s">
+							<h2>%2$s</h2>
+							<div class="woocommerce-product-details__short-%1$s">
+
+							</div>
+						</div>
+						',
+						esc_attr( $key ),
+						esc_attr( $product_tab['title'] ),
+					);
+				}
+			}
+			$tabs = sprintf('
+				<ul class="tabs" role="tablist">
+					%1$s
+				</ul>
+
+				%2$s
+
+				',
+				$tabs_title,
+				$tabs_content
+			);
+			echo $tabs;
+		}
+
+		$tabs = ob_get_clean();
+
+		return $tabs;
 	}
 }
