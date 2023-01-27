70bc51b1fb434728a6f001d51cbad4212b40e850
David Arenas <david.arenas@automattic.com>
2023-01-27 18:54:00 +0100

    Use `data-woo-` prefixed directives

--- src/BlockTypes/SimplePriceFilter.php
+++ src/BlockTypes/SimplePriceFilter.php
@@ -11,22 +11,28 @@ class SimplePriceFilter extends AbstractBlock {
 	 *
 	 * @var string
 	 */
-	protected $block_name = 'simple-price-filter';
+	protected $block_name     = 'simple-price-filter';
 	const MIN_PRICE_QUERY_VAR = 'min_price';
 	const MAX_PRICE_QUERY_VAR = 'max_price';
 
 	/**
-	 * Render function.
+	 * Render the block.
+	 *
+	 * @param array    $attributes Block attributes.
+	 * @param string   $content Block content.
+	 * @param WP_Block $block Block instance.
+	 *
+	 * @return string Rendered block output.
 	 */
 	public function render( $attributes = [], $content = '', $block = null ) {
 		$wrapper_attributes = get_block_wrapper_attributes();
-		$max_range = 90; // TODO: get this value from DB.
-		$min_price = get_query_var( self::MIN_PRICE_QUERY_VAR, 0 );
-		$max_price = get_query_var( self::MAX_PRICE_QUERY_VAR, $max_range );
+		$max_range          = 90; // TODO: get this value from DB.
+		$min_price          = get_query_var( self::MIN_PRICE_QUERY_VAR, 0 );
+		$max_price          = get_query_var( self::MAX_PRICE_QUERY_VAR, $max_range );
 
 		// CSS variables for the range bar style.
-		$__low = 100 * $min_price / $max_range;
-		$__high = 100 * $max_price / $max_range;
+		$__low       = 100 * $min_price / $max_range;
+		$__high      = 100 * $max_price / $max_range;
 		$range_style = "--low: $__low%; --high: $__high%";
 
 		return "
@@ -35,8 +41,8 @@ class SimplePriceFilter extends AbstractBlock {
 			<div
 				class='range'
 				style='$range_style'
-				wp-bind:style='derived.filters.rangeStyle'
-				wp-on:mousemove='actions.filters.updateActiveHandle'
+				data-woo-bind:style='derived.filters.rangeStyle'
+				data-woo-on:mousemove='actions.filters.updateActiveHandle'
 			>
 				<div class='range-bar'></div>
 				<input
@@ -45,10 +51,10 @@ class SimplePriceFilter extends AbstractBlock {
 					value='$min_price'
 					min='0'
 					max='$max_range'
-					wp-bind:value='state.filters.minPrice'
-					wp-class:active='state.filters.isMinActive'
-					wp-on:input='actions.filters.setMinPrice'
-					wp-on:change='actions.filters.updateProducts'
+					data-woo-bind:value='state.filters.minPrice'
+					data-woo-class:active='state.filters.isMinActive'
+					data-woo-on:input='actions.filters.setMinPrice'
+					data-woo-on:change='actions.filters.updateProducts'
 				>
 				<input
 					class=''
@@ -56,41 +62,45 @@ class SimplePriceFilter extends AbstractBlock {
 					value='$max_price'
 					min='0'
 					max='$max_range'
-					wp-bind:value='state.filters.maxPrice'
-					wp-class:active='state.filters.isMaxActive'
-					wp-on:input='actions.filters.setMaxPrice'
-					wp-on:change='actions.filters.updateProducts'
+					data-woo-bind:value='state.filters.maxPrice'
+					data-woo-class:active='state.filters.isMaxActive'
+					data-woo-on:input='actions.filters.setMaxPrice'
+					data-woo-on:change='actions.filters.updateProducts'
 				>
 			</div>
 			<div class='text'>
 				<input
 					type='text'
 					value='$min_price'
-					wp-bind:value='state.filters.minPrice'
-					wp-on:input='actions.filters.setMinPrice'
-					wp-on:change='actions.filters.updateProducts'
+					data-woo-bind:value='state.filters.minPrice'
+					data-woo-on:input='actions.filters.setMinPrice'
+					data-woo-on:change='actions.filters.updateProducts'
 				>
 				<input
 					type='text'
 					value='$max_price'
-					wp-bind:value='state.filters.maxPrice'
-					wp-on:input='actions.filters.setMaxPrice'
-					wp-on:change='actions.filters.updateProducts'
+					data-woo-bind:value='state.filters.maxPrice'
+					data-woo-on:input='actions.filters.setMaxPrice'
+					data-woo-on:change='actions.filters.updateProducts'
 				>
 			</div>
-			<button wp-on:click='actions.filters.reset'>Reset</button>
+			<button data-woo-on:click='actions.filters.reset'>Reset</button>
 		</div>
 		";
 	}
 
 	/**
-	 * Block type script definition.
+	 * Get the frontend script handle for this block type.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 *
+	 * @return null
 	 */
 	public function get_block_type_script( $key = null ) {
 		$script = [
 			'handle'       => 'simple-price-filter-view',
-			'path'         => 'build/wp-directives-simple-price-filter.js',
-			'dependencies' => [ 'wp-directives-runtime' ],
+			'path'         => 'build/woo-directives-simple-price-filter.js',
+			'dependencies' => [ 'woo-directives-runtime' ],
 		];
 		return $key ? $script[ $key ] : $script;
 	}
