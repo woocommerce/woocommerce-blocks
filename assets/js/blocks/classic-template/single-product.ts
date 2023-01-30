61fd14ab23fdddfd1f980eb5f3e28ce3f6a81ec8
Alba Rincón <alba.rincon@automattic.com>
2023-01-30 11:46:46 +0100

    Show conversion button with label and description

--- assets/js/blocks/classic-template/single-product.ts
+++ assets/js/blocks/classic-template/single-product.ts
@@ -2,14 +2,27 @@
  * External dependencies
  */
 import { __, sprintf } from '@wordpress/i18n';
+import { isWpVersion } from '@woocommerce/settings';
 
 // TODO: Provide the blockified template for the single product page and adjust
 // other functions accordingly.
 const getBlockifiedTemplate = () => [];
 
-const isConversionPossible = () => false;
+const isConversionPossible = () => {
+	// Blockification is possible for the WP version 6.1 and above,
+	// which are the versions the Products block supports.
+	return isWpVersion( '6.1', '>=' );
+};
 
-const getDescriptionAllowingConversion = () => '';
+const getDescriptionAllowingConversion = ( templateTitle: string ) =>
+	sprintf(
+		/* translators: %s is the template title */
+		__(
+			"This block serves as a placeholder for your %s. We recommend upgrading to the Single Products block for more features to edit your products visually. Don't worry, you can always revert back.",
+			'woo-gutenberg-products-block'
+		),
+		templateTitle
+	);
 
 const getDescriptionDisallowingConversion = ( templateTitle: string ) =>
 	sprintf(
@@ -23,13 +36,14 @@ const getDescriptionDisallowingConversion = ( templateTitle: string ) =>
 
 const getDescription = ( templateTitle: string, canConvert: boolean ) => {
 	if ( canConvert ) {
-		return getDescriptionAllowingConversion();
+		return getDescriptionAllowingConversion( templateTitle );
 	}
 
 	return getDescriptionDisallowingConversion( templateTitle );
 };
 
-const getButtonLabel = () => '';
+const getButtonLabel = () =>
+	__( 'Upgrade to Single Product block', 'woo-gutenberg-products-block' );
 
 export {
 	getBlockifiedTemplate,
