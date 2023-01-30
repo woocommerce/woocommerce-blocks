4b48504b2809f7533285a14dfda18e7e156db160
Tung Du <dinhtungdu@gmail.com>
2023-01-30 18:09:10 +0700

    new default templates, all blocks are Products block inner blocks

---
+++ patterns/product-search-form.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Title: Product Search
+ * Slug: woocommerce/product-search-form
+ * Inserter: no
+ */
+?>
+<!-- wp:search {"label":"<?php echo esc_html_x( 'Search', 'label', 'woo-gutenberg-products-block' ); ?>","showLabel":false,"placeholder":"<?php echo esc_attr_x( 'Search products…', 'placeholder for search field', 'woo-gutenberg-products-block' ); ?>","buttonText":"<?php esc_attr_e( 'Search', 'woo-gutenberg-products-block' ); ?>","query":{"post_type":"product"}} /-->
