50850d1d0ec680fe08be10bc9a5b6cf66adbdf9e
Saad Tarhi <saad.trh@gmail.com>
2023-02-01 13:49:47 +0100

    Fix the alert icon's name

--- assets/js/icons/library/alert.tsx
+++ assets/js/icons/library/alert.tsx
@@ -3,7 +3,7 @@
  */
 import { SVG } from '@wordpress/primitives';
 
-const cart = (
+const alert = (
 	<SVG xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
 		<path
 			d="M12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20Z"
@@ -15,4 +15,4 @@ const cart = (
 	</SVG>
 );
 
-export default cart;
+export default alert;
