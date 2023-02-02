70ff5eb434c5693ca2307d253694aefa57b9d95b
Paulo Arromba <17236129+wavvves@users.noreply.github.com>
2023-02-02 17:29:07 +0000

    Added Cart and Checkout to the template hierarchies

--- src/Templates/CartTemplate.php
+++ src/Templates/CartTemplate.php
@@ -10,4 +10,31 @@ class CartTemplate {
 
 	const SLUG = 'cart';
 
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->init();
+	}
+
+	/**
+	 * Initialization method.
+	 */
+	protected function init() {
+		add_filter( 'page_template_hierarchy', array( $this, 'update_page_template_hierarchy' ), 10, 3 );
+	}
+
+	/**
+	 * When the page is displaying the cart and a block theme is active, render the Cart Template.
+	 *
+	 * @param array $templates Templates that match the pages_template_hierarchy.
+	 */
+	public function update_page_template_hierarchy( $templates ) {
+		if ( is_cart() && wc_current_theme_is_fse_theme() ) {
+			array_unshift( $templates, self::SLUG );
+		}
+
+		return $templates;
+	}
+
 }
