508f3fb2c4806b76e9104513aab72eaba2d00fab
Alex Florisca <alex.florisca@automattic.com>
2023-02-03 16:43:10 +0000

    Update assets/js/base/context/providers/cart-checkout/payment-events/index.tsx
    Co-authored-by: Mike Jolley <mike.jolley@me.com>

--- assets/js/base/context/providers/cart-checkout/payment-events/index.tsx
+++ assets/js/base/context/providers/cart-checkout/payment-events/index.tsx
@@ -86,7 +86,7 @@ export const PaymentEventsProvider = ( {
 		__internalEmitPaymentProcessingEvent,
 	} = useDispatch( PAYMENT_STORE_KEY );
 
-	// flip payment to processing if checkout processing is complete, there are no errors
+	// flip payment to processing if checkout processing is complete and there are no errors
 	useEffect( () => {
 		if (
 			checkoutIsProcessing &&
