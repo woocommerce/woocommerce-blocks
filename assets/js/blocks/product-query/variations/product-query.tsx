213bc7bfadf7fdd8ac736e2b70972f9544ad76c3
Alba Rincón <alba.rincon@automattic.com>
2023-02-03 10:13:38 +0100

    Extract the products registration to a separate function

--- assets/js/blocks/product-query/variations/product-query.tsx
+++ assets/js/blocks/product-query/variations/product-query.tsx
@@ -7,6 +7,7 @@ import { __ } from '@wordpress/i18n';
 import { stacks } from '@woocommerce/icons';
 import { isWpVersion } from '@woocommerce/settings';
 import { select, subscribe } from '@wordpress/data';
+import { QueryBlockAttributes } from '@woocommerce/blocks/product-query/types';
 
 /**
  * Internal dependencies
@@ -28,6 +29,34 @@ const ARCHIVE_PRODUCT_TEMPLATES = [
 	'woocommerce/woocommerce//product-search-results',
 ];
 
+const registerProductsBlock = ( attributes: QueryBlockAttributes ) => {
+	registerBlockVariation( QUERY_LOOP_ID, {
+		description: __(
+			'A block that displays a selection of products in your store.',
+			'woo-gutenberg-products-block'
+		),
+		name: VARIATION_NAME,
+		/* translators: “Products“ is the name of the block. */
+		title: __( 'Products (Beta)', 'woo-gutenberg-products-block' ),
+		isActive: ( blockAttributes ) =>
+			blockAttributes.namespace === VARIATION_NAME,
+		icon: (
+			<Icon
+				icon={ stacks }
+				className="wc-block-editor-components-block-icon wc-block-editor-components-block-icon--stacks"
+			/>
+		),
+		attributes,
+		// Gutenberg doesn't support this type yet, discussion here:
+		// https://github.com/WordPress/gutenberg/pull/43632
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		allowedControls: DEFAULT_ALLOWED_CONTROLS,
+		innerBlocks: INNER_BLOCKS_TEMPLATE,
+		scope: [ 'inserter' ],
+	} );
+};
+
 if ( isWpVersion( '6.1', '>=' ) ) {
 	let currentTemplateId: string | undefined;
 
@@ -41,40 +70,14 @@ if ( isWpVersion( '6.1', '>=' ) ) {
 		}
 
 		const queryAttributes = {
-		    ...QUERY_DEFAULT_ATTRIBUTES,
-		    query: {
-		        ...QUERY_DEFAULT_ATTRIBUTES.query,
-		        inherit: ARCHIVE_PRODUCT_TEMPLATES.includes( currentTemplateId );
-		    },
-		}	
-
-		registerBlockVariation( QUERY_LOOP_ID, {
-			description: __(
-				'A block that displays a selection of products in your store.',
-				'woo-gutenberg-products-block'
-			),
-			name: VARIATION_NAME,
-			/* translators: “Products“ is the name of the block. */
-			title: __( 'Products (Beta)', 'woo-gutenberg-products-block' ),
-			isActive: ( blockAttributes ) =>
-				blockAttributes.namespace === VARIATION_NAME,
-			icon: (
-				<Icon
-					icon={ stacks }
-					className="wc-block-editor-components-block-icon wc-block-editor-components-block-icon--stacks"
-				/>
-			),
-			attributes: {
-				...QUERY_DEFAULT_ATTRIBUTES,
-				namespace: VARIATION_NAME,
+			...QUERY_DEFAULT_ATTRIBUTES,
+			query: {
+				...QUERY_DEFAULT_ATTRIBUTES.query,
+				inherit:
+					ARCHIVE_PRODUCT_TEMPLATES.includes( currentTemplateId ),
 			},
-			// Gutenberg doesn't support this type yet, discussion here:
-			// https://github.com/WordPress/gutenberg/pull/43632
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			allowedControls: DEFAULT_ALLOWED_CONTROLS,
-			innerBlocks: INNER_BLOCKS_TEMPLATE,
-			scope: [ 'inserter' ],
-		} );
+		};
+
+		registerProductsBlock( queryAttributes );
 	} );
 }
