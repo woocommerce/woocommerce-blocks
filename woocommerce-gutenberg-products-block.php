85244951aa4802c443151d3f33df77c177b2b6f4
David Arenas <david.arenas@automattic.com>
2023-02-14 15:39:37 +0100

    Remove duplicated csn tag

--- woocommerce-gutenberg-products-block.php
+++ woocommerce-gutenberg-products-block.php
@@ -289,17 +289,3 @@ add_action( 'admin_notices', 'woocommerce_blocks_plugin_outdated_notice' );
 
 // Include the Interactivity API.
 require_once __DIR__ . '/src/Interactivity/woo-directives.php';
-
-/**
- * Insert the required meta tag for client-side navigation.
- */
-function add_cst_meta_tag() {
-	echo '<meta itemprop="woo-client-side-navigation" content="active">';
-	add_filter(
-		'client_side_navigation',
-		true
-	);
-}
-
-add_action( 'wp_head', 'add_cst_meta_tag' );
-
