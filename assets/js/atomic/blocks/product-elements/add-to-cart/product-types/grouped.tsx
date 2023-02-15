3376850b02639c85a0f61f75929949aee27389dc
Karol Manijak <karol.manijak@automattic.com>
2023-02-15 13:01:02 +0100

    Simplify unused Add to Cart button form placeholder for grouped product

---
+++ assets/js/atomic/blocks/product-elements/add-to-cart/product-types/grouped.tsx
@@ -0,0 +1,12 @@
+/**
+ * Grouped Product Add To Cart Form
+ */
+const Grouped = () => {
+	return (
+		<p className="wc-block-components-product-add-to-cart-grouped">
+			This is a placeholder for the grouped products form element.
+		</p>
+	);
+};
+
+export default Grouped;
