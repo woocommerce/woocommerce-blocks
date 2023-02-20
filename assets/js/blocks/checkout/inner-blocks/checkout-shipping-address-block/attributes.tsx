858585521080dc8a5834abf46244810d51cf59fa
Mike Jolley <mike.jolley@me.com>
2023-02-20 14:49:18 +0000

    Remove shipping defaultDescription

--- assets/js/blocks/checkout/inner-blocks/checkout-shipping-address-block/attributes.tsx
+++ assets/js/blocks/checkout/inner-blocks/checkout-shipping-address-block/attributes.tsx
@@ -11,10 +11,7 @@ import formStepAttributes from '../../form-step/attributes';
 export default {
 	...formStepAttributes( {
 		defaultTitle: __( 'Shipping address', 'woo-gutenberg-products-block' ),
-		defaultDescription: __(
-			'Enter the address where you want your order delivered.',
-			'woo-gutenberg-products-block'
-		),
+		defaultDescription: '',
 	} ),
 	className: {
 		type: 'string',
