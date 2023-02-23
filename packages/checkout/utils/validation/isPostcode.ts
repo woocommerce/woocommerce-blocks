8028cce88b9d19c90fe1f88c06f0a37a08c17512
Niels Lange <info@nielslange.de>
2023-02-23 14:53:15 +0700

    Convert function to static variable set

--- packages/checkout/utils/validation/isPostcode.ts
+++ packages/checkout/utils/validation/isPostcode.ts
@@ -3,29 +3,32 @@
  */
 import { POSTCODE_REGEXES } from 'postcode-validator/lib/cjs/postcode-regexes.js';
 
-const getCustomRegexes = () => {
-	POSTCODE_REGEXES.set( 'BA', /^([7-8]{1})([0-9]{4})$/ );
-	POSTCODE_REGEXES.set(
+const CUSTOM_REGEXES = new Map< string, RegExp >( [
+	[ 'BA', /^([7-8]{1})([0-9]{4})$/ ],
+	[
 		'GB',
-		/^([A-Z]){1}([0-9]{1,2}|[A-Z][0-9][A-Z]|[A-Z][0-9]{2}|[A-Z][0-9]|[0-9][A-Z]){1}([ ])?([0-9][A-z]{2}){1}|BFPO(?:\s)?([0-9]{1,4})$|BFPO(c\/o[0-9]{1,3})$/i
-	);
-	POSTCODE_REGEXES.set( 'IN', /^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/ );
-	POSTCODE_REGEXES.set( 'JP', /^([0-9]{3})([-]?)([0-9]{4})$/ );
-	POSTCODE_REGEXES.set( 'LI', /^(94[8-9][0-9])$/ );
-	POSTCODE_REGEXES.set( 'NL', /^([1-9][0-9]{3})(\s?)(?!SA|SD|SS)[A-Z]{2}$/i );
-	POSTCODE_REGEXES.set( 'SI', /^([1-9][0-9]{3})$/ );
+		/^([A-Z]){1}([0-9]{1,2}|[A-Z][0-9][A-Z]|[A-Z][0-9]{2}|[A-Z][0-9]|[0-9][A-Z]){1}([ ])?([0-9][A-z]{2}){1}|BFPO(?:\s)?([0-9]{1,4})$|BFPO(c\/o[0-9]{1,3})$/i,
+	],
+	[ 'IN', /^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/ ],
+	[ 'JP', /^([0-9]{3})([-]?)([0-9]{4})$/ ],
+	[ 'LI', /^(94[8-9][0-9])$/ ],
+	[ 'NL', /^([1-9][0-9]{3})(\s?)(?!SA|SD|SS)[A-Z]{2}$/i ],
+	[ 'SI', /^([1-9][0-9]{3})$/ ],
+] );
 
-	return POSTCODE_REGEXES;
-};
+const DEFAULT_REGEXES = new Map< string, RegExp >( [
+	...POSTCODE_REGEXES,
+	...CUSTOM_REGEXES,
+] );
+
 export interface IsPostcodeProps {
 	postcode: string;
 	country: string;
 }
 
 const isPostcode = ( { postcode, country }: IsPostcodeProps ): boolean => {
-	const CUSTOM_POSTCODE_REGEXES = getCustomRegexes();
 	// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-	return CUSTOM_POSTCODE_REGEXES.get( country )!.test( postcode );
+	return DEFAULT_REGEXES.get( country )!.test( postcode );
 };
 
 export default isPostcode;
