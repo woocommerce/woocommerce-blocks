92c1dae9fa61e72ac45ff1b7443522e6d73199a9
Luigi <gigitux@gmail.com>
2023-02-28 12:43:38 +0100

    Enable Product Image Gallery only for experimental build

--- assets/js/atomic/blocks/product-elements/product-image-gallery/index.ts
+++ assets/js/atomic/blocks/product-elements/product-image-gallery/index.ts
@@ -4,6 +4,7 @@
 import { gallery as icon } from '@wordpress/icons';
 import { registerBlockType, unregisterBlockType } from '@wordpress/blocks';
 import { registerBlockSingleProductTemplate } from '@woocommerce/atomic-utils';
+import { isExperimentalBuild } from '@woocommerce/block-settings';
 
 /**
  * Internal dependencies
@@ -11,16 +12,18 @@ import { registerBlockSingleProductTemplate } from '@woocommerce/atomic-utils';
 import edit from './edit';
 import metadata from './block.json';
 
-registerBlockSingleProductTemplate( {
-	registerBlockFn: () => {
-		// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
-		registerBlockType( metadata, {
-			icon,
-			edit,
-		} );
-	},
-	unregisterBlockFn: () => {
-		unregisterBlockType( metadata.name );
-	},
-	blockName: metadata.name,
-} );
+if ( isExperimentalBuild() ) {
+	registerBlockSingleProductTemplate( {
+		registerBlockFn: () => {
+			// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
+			registerBlockType( metadata, {
+				icon,
+				edit,
+			} );
+		},
+		unregisterBlockFn: () => {
+			unregisterBlockType( metadata.name );
+		},
+		blockName: metadata.name,
+	} );
+}
