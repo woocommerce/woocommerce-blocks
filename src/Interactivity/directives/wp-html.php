de6c42db8dca696737e925cafb49005b242deacf
David Arenas <david.arenas@automattic.com>
2023-03-01 19:12:10 +0100

    Remove `wp-html.php` file

--- src/Interactivity/directives/wp-html.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if ( ! class_exists( 'WP_HTML_Tag_Processor' ) ) {
-	require __DIR__ . '/../../../../gutenberg/lib/compat/wordpress-6.2/html-api/class-wp-html-attribute-token.php';
-	require __DIR__ . '/../../../../gutenberg/lib/compat/wordpress-6.2/html-api/class-wp-html-span.php';
-	require __DIR__ . '/../../../../gutenberg/lib/compat/wordpress-6.2/html-api/class-wp-html-text-replacement.php';
-	require __DIR__ . '/../../../../gutenberg/lib/compat/wordpress-6.2/html-api/class-wp-html-tag-processor.php';
-}
