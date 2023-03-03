3d3a57c2518e5ba304d43e391791a583573a5087
Alba Rincón <alba.rincon@automattic.com>
2023-03-03 11:22:41 +0100

    Add variation explanation

--- assets/js/blocks/customer-account/index.tsx
+++ assets/js/blocks/customer-account/index.tsx
@@ -30,6 +30,11 @@ registerBlockType( metadata, {
 	},
 } );
 
+// We needed to change the size of the icon without affecting already existing blocks.
+// This is why we are registering a new variation with a different icon class instead of changing directly the icon
+// size in the css. By giving it the same name and making it default we are making sure that new blocks will use the
+// new icon size and existing blocks will keep using the old one after updating the plugin.
+// For more context, see https://github.com/woocommerce/woocommerce-blocks/pull/8594
 registerBlockVariation( 'woocommerce/customer-account', {
 	name: 'woocommerce/customer-account',
 	title: __( 'Customer account', 'woo-gutenberg-products-block' ),
