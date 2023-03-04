558da338ff992fe24ddf21dc3231522d90887a94
Alexandre Lara <allexandrelara@gmail.com>
2023-03-03 21:03:37 -0300

    Add InnerBlocks template to Editor for Single Product Block

--- assets/js/blocks/single-product-server-side/edit.tsx
+++ assets/js/blocks/single-product-server-side/edit.tsx
@@ -1,15 +1,23 @@
 /**
  * External dependencies
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
 import { Disabled } from '@wordpress/components';
-import type { BlockEditProps } from '@wordpress/blocks';
+import { compose } from '@wordpress/compose';
+import { withProduct } from '@woocommerce/block-hocs';
+import { withProductDataContext } from '@woocommerce/shared-hocs';
+import type { BlockEditProps, InnerBlockTemplate } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+import { InnerBlockLayoutContextProvider } from '@woocommerce/shared-context';
 
 /**
  * Internal dependencies
  */
-import Block from './block';
+import withProductSelector from '../../atomic/blocks/product-elements/shared/with-product-selector';
 import { Attributes } from './types';
+import { BLOCK_ICON } from './constants';
+import metadata from './block.json';
+// import { useSelect } from '@wordpress/data';
 
 const Edit = ( { attributes }: BlockEditProps< Attributes > ) => {
 	const { className } = attributes;
@@ -17,15 +25,74 @@ const Edit = ( { attributes }: BlockEditProps< Attributes > ) => {
 		className,
 	} );
 
+	// const isDescendentOfSingleProductTemplate = useSelect( ( select ) => {
+	// 	const store = select( 'core/edit-site' );
+	// 	const postId = store?.getEditedPostId();
+	// 	const PRODUCT_META_POST_ID = 'woocommerce/woocommerce//product-meta';
+	// 	const SINGLE_PRODUCT_POST_ID =
+	// 		'woocommerce/woocommerce//single-product';
+
+	// 	return (
+	// 		postId === PRODUCT_META_POST_ID || postId === SINGLE_PRODUCT_POST_ID
+	// 	);
+	// }, [] );
+
+	const TEMPLATE: InnerBlockTemplate[] = [
+		[
+			'core/group',
+			{ layout: { type: 'constrained', contentSize: '1000px' } },
+			[
+				[ 'woocommerce/breadcrumbs' ],
+				[
+					'core/columns',
+					{},
+					[
+						[
+							'core/column',
+							{},
+							[ [ 'woocommerce/product-image-gallery' ] ],
+						],
+						[
+							'core/column',
+							{},
+							[
+								[ 'woocommerce/product-title' ],
+								[ 'woocommerce/product-price' ],
+								[ 'woocommerce/product-summary' ],
+								[ 'woocommerce/add-to-cart-form' ],
+							],
+						],
+					],
+				],
+				[ 'core/heading', { placeholder: 'Related Products' } ],
+				[ 'woocommerce/related-products' ],
+			],
+		],
+	];
+
 	return (
-		<>
+		<InnerBlockLayoutContextProvider
+			parentName={ metadata.name }
+			parentClassName={ className }
+		>
 			<div { ...blockProps }>
 				<Disabled>
-					<Block />
+					<InnerBlocks template={ TEMPLATE } />
 				</Disabled>
 			</div>
-		</>
+		</InnerBlockLayoutContextProvider>
 	);
 };
 
-export default Edit;
+export default compose( [
+	withProduct,
+	withProductDataContext,
+	withProductSelector( {
+		icon: BLOCK_ICON,
+		label: metadata.name,
+		description: __(
+			'Choose a product to display its categories.',
+			'woo-gutenberg-products-block'
+		),
+	} ),
+] )( Edit );
--- assets/js/blocks/single-product-server-side/index.tsx
+++ assets/js/blocks/single-product-server-side/index.tsx
@@ -3,25 +3,26 @@
  */
 import { registerBlockSingleProductTemplate } from '@woocommerce/atomic-utils';
 import { registerBlockType, unregisterBlockType } from '@wordpress/blocks';
-import { Icon, mediaAndText } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import { BLOCK_ICON } from './constants';
 
 /**
  * Internal dependencies
  */
 import metadata from './block.json';
 import edit from './edit';
+import save from './save';
 
 registerBlockSingleProductTemplate( {
 	registerBlockFn: () => {
 		// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
 		registerBlockType( metadata, {
-			icon: (
-				<Icon
-					icon={ mediaAndText }
-					className="wc-block-editor-components-block-icon"
-				/>
-			),
+			icon: BLOCK_ICON,
 			edit,
+			save,
 		} );
 	},
 	unregisterBlockFn: () => {
