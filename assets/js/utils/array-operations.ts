2d6b30b985bc1781dee1aa5fb5a6bd1e5eb639d9
Lucio Giannotta <lucio.giannotta@a8c.com>
2023-03-06 15:25:40 +0100

    Fix array operations functions

--- assets/js/utils/array-operations.ts
+++ assets/js/utils/array-operations.ts
@@ -1,11 +1,17 @@
+/**
+ * Returns the difference between two arrays (A - B)
+ */
 export function arrayDifferenceBy< T >( a: T[], b: T[], key: keyof T ) {
-	const keys = new Set( a.map( ( item ) => item[ key ] ) );
+	const keys = new Set( b.map( ( item ) => item[ key ] ) );
 
-	return b.filter( ( item ) => ! keys.has( item[ key ] ) );
+	return a.filter( ( item ) => ! keys.has( item[ key ] ) );
 }
 
+/**
+ * Returns the union of two arrays (A ∪ B)
+ */
 export function arrayUnionBy< T >( a: T[], b: T[], key: keyof T ) {
-	const difference = arrayDifferenceBy( a, b, key );
+	const difference = arrayDifferenceBy( b, a, key );
 
 	return [ ...a, ...difference ];
 }
