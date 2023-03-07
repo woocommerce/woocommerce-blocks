909d5cc84b6c2bedf54c48faf6d7210665a4d425
Thomas Roberts <thomas.roberts@automattic.com>
2023-03-07 12:33:41 -0800

    Check if the address is required before showing rates

--- assets/js/blocks/checkout/inner-blocks/checkout-shipping-methods-block/block.tsx
+++ assets/js/blocks/checkout/inner-blocks/checkout-shipping-methods-block/block.tsx
@@ -2,11 +2,15 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useShippingData } from '@woocommerce/base-context/hooks';
+import {
+	useCustomerData,
+	useShippingData,
+} from '@woocommerce/base-context/hooks';
 import { ShippingRatesControl } from '@woocommerce/base-components/cart-checkout';
 import {
 	getShippingRatesPackageCount,
 	hasCollectableRate,
+	isAddressComplete,
 } from '@woocommerce/base-utils';
 import { getCurrencyFromPriceResponse } from '@woocommerce/price-format';
 import FormattedMonetaryAmount from '@woocommerce/base-components/formatted-monetary-amount';
@@ -66,6 +70,8 @@ const Block = ( {
 		isCollectable,
 	} = useShippingData();
 
+	const { shippingAddress } = useCustomerData();
+
 	const filteredShippingRates = isCollectable
 		? shippingRates.map( ( shippingRatesPackage ) => {
 				return {
@@ -87,9 +93,18 @@ const Block = ( {
 	const shippingRatesPackageCount =
 		getShippingRatesPackageCount( shippingRates );
 
+	const shippingCostRequiresAddress = getSetting< boolean >(
+		'shippingCostRequiresAddress',
+		false
+	);
+
+	const addressComplete = isAddressComplete( shippingAddress );
+
 	if (
-		( ! hasCalculatedShipping && ! shippingRatesPackageCount ) ||
-		( shippingCostRequiresAddress && isEditor )
+		( ! isEditor &&
+			! hasCalculatedShipping &&
+			! shippingRatesPackageCount ) ||
+		( shippingCostRequiresAddress && ! addressComplete )
 	) {
 		return (
 			<p>
