44f8d48198ff56a983d6251249aa2c6139c835a7
Alexandre Lara <allexandrelara@gmail.com>
2023-03-09 19:08:07 -0300

    Add renderOnServerSide attribute to Product Title block

--- assets/js/atomic/blocks/product-elements/title/attributes.ts
+++ assets/js/atomic/blocks/product-elements/title/attributes.ts
@@ -20,6 +20,10 @@ let blockAttributes: BlockAttributes = {
 		type: 'number',
 		default: 0,
 	},
+	renderOnServerSide: {
+		type: 'boolean',
+		default: false,
+	},
 };
 
 if ( isFeaturePluginBuild() ) {
