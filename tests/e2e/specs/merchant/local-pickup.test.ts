c0b5fb9bfab24111db41f88664242f9aabda402d
Tarun Vijwani <tarun.vijwani@automattic.com>
2023-03-15 10:43:13 +0400

    Add inline comment about same field for Country and State fields
    - We merged the Country and State fields into the same field in the local pickup modal. Add inline comment in e2e test to avoid confusion.

--- tests/e2e/specs/merchant/local-pickup.test.ts
+++ tests/e2e/specs/merchant/local-pickup.test.ts
@@ -253,6 +253,7 @@ describe( `Local Pickup Settings`, () => {
 				'.components-modal__content input[name="location_city"]',
 				'New City'
 			);
+			// We merged the Country and State fields into the same field in the local pickup modal.
 			await page.select(
 				'.components-modal__content select[name="location_country_state"]',
 				'United Kingdom (UK)'
