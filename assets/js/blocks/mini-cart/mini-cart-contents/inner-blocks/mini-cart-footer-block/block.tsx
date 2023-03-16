10c4147fac79323b78a86a5f63157c734b624a76
Alba Rincón <alba.rincon@automattic.com>
2023-03-16 16:26:06 +0100

    Render inner buttons on the frontend

--- assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-footer-block/block.tsx
+++ assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-footer-block/block.tsx
@@ -11,18 +11,8 @@ import {
 import PaymentMethodIcons from '@woocommerce/base-components/cart-checkout/payment-method-icons';
 import { getIconsFromPaymentMethods } from '@woocommerce/base-utils';
 import { getSetting } from '@woocommerce/settings';
-import { CART_URL, CHECKOUT_URL } from '@woocommerce/block-settings';
-import Button from '@woocommerce/base-components/button';
 import { PaymentEventsProvider } from '@woocommerce/base-context';
 import classNames from 'classnames';
-
-/**
- * Internal dependencies
- */
-import {
-	defaultCartButtonLabel,
-	defaultCheckoutButtonLabel,
-} from './constants';
 
 const PaymentMethodIconsElement = (): JSX.Element => {
 	const { paymentMethods } = usePaymentMethods();
@@ -34,16 +24,11 @@ const PaymentMethodIconsElement = (): JSX.Element => {
 };
 
 interface Props {
+	children: JSX.Element | JSX.Element[];
 	className?: string;
-	cartButtonLabel: string;
-	checkoutButtonLabel: string;
 }
 
-const Block = ( {
-	className,
-	cartButtonLabel,
-	checkoutButtonLabel,
-}: Props ): JSX.Element => {
+const Block = ( { children, className }: Props ): JSX.Element => {
 	const { cartTotals } = useStoreCart();
 	const subTotal = getSetting( 'displayCartPricesIncludingTax', false )
 		? parseInt( cartTotals.total_items, 10 ) +
@@ -65,23 +50,7 @@ const Block = ( {
 				) }
 			/>
 			<div className="wc-block-mini-cart__footer-actions">
-				{ CART_URL && (
-					<Button
-						className="wc-block-mini-cart__footer-cart"
-						href={ CART_URL }
-						variant="outlined"
-					>
-						{ cartButtonLabel || defaultCartButtonLabel }
-					</Button>
-				) }
-				{ CHECKOUT_URL && (
-					<Button
-						className="wc-block-mini-cart__footer-checkout"
-						href={ CHECKOUT_URL }
-					>
-						{ checkoutButtonLabel || defaultCheckoutButtonLabel }
-					</Button>
-				) }
+				{ children }
 			</div>
 			<PaymentEventsProvider>
 				<PaymentMethodIconsElement />
--- assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-footer-block/index.tsx
+++ assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-footer-block/index.tsx
@@ -8,7 +8,6 @@ import { registerBlockType } from '@wordpress/blocks';
  * Internal dependencies
  */
 import { Edit, Save } from './edit';
-import attributes from './attributes';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore -- TypeScript expects some required properties which we already
@@ -22,7 +21,6 @@ registerBlockType( 'woocommerce/mini-cart-footer-block', {
 			/>
 		),
 	},
-	attributes,
 	edit: Edit,
 	save: Save,
 } );
