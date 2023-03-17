2c2b34b229eb626f9f5917b1fc55a0153ac1b04c
Alexandre Lara <allexandrelara@gmail.com>
2023-03-17 18:15:23 -0300

    Replace global $post only for Single Product inner blocks

--- src/BlockTypes/SingleProduct.php
+++ src/BlockTypes/SingleProduct.php
@@ -12,6 +12,7 @@ class SingleProduct extends AbstractBlock {
 	 */
 	protected $block_name = 'single-product';
 	protected $product_id = 0;
+	protected $single_product_inner_blocks_names = [];
 
 	/**
 	 * API version name.
@@ -57,17 +58,47 @@ class SingleProduct extends AbstractBlock {
 	 * @return array Updated block context.
 	 */
 	public function update_context( $context, $block, $parent_block ) {
+		// var_dump($block['blockName']);
+
 		if( $block['blockName'] === 'woocommerce/single-product'
 			&& isset( $block['attrs']['productId'] ) ) {
 				$this->product_id = $block['attrs']['productId'];
-		}
-		if( $this->product_id > 0 ){
-			global $post;
-			$post = get_post( $this->product_id );
-			setup_postdata( $post );
-			$context['postId'] = $this->product_id;
+
+				$this->single_product_inner_blocks_names = array_reverse($this->extract_single_product_inner_block_names( $block ));
 		}
 
+		$this->replace_post_for_single_product_inner_block( $block, $context );
+
 		return $context;
+	}
+
+	protected function extract_single_product_inner_block_names($block, &$result = []) {
+		if(isset($block['blockName'])){
+			$result[] = $block['blockName'];
+		}
+
+		if(isset($block['innerBlocks'])){
+			foreach ($block['innerBlocks'] as $inner_block) {
+				$this->extract_single_product_inner_block_names($inner_block, $result);
+			}
+		}
+		return $result;
+	}
+
+	protected function replace_post_for_single_product_inner_block( $block, &$context ) {
+		if( $this->single_product_inner_blocks_names ){
+			$block_name = array_pop( $this->single_product_inner_blocks_names);
+
+			if( $block_name === $block['blockName']){
+				global $post;
+				$post = get_post( $this->product_id );
+				setup_postdata( $post );
+				$context['postId'] = $this->product_id;
+			}
+
+			if ( ! $this->single_product_inner_blocks_names ) {
+				wp_reset_postdata();
+			}
+		}
 	}
 }
