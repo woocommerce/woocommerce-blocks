37031213933c3deeed6c4c4113c69a93ef0e05ab
Manish Menaria <the.manish.menaria@gmail.com>
2023-03-21 12:50:43 +0530

    Fix width setting not working in Single product block

--- assets/js/atomic/blocks/product-elements/button/index.ts
+++ assets/js/atomic/blocks/product-elements/button/index.ts
@@ -11,6 +11,7 @@ import { supports } from './supports';
 import attributes from './attributes';
 import sharedConfig from '../shared/config';
 import edit from './edit';
+import save from './save';
 import {
 	BLOCK_TITLE as title,
 	BLOCK_ICON as icon,
@@ -32,6 +33,7 @@ const blockConfig: BlockConfiguration = {
 	attributes,
 	supports,
 	edit,
+	save,
 	styles: [
 		{ name: 'fill', label: 'Fill', isDefault: true },
 		{ name: 'outline', label: 'Outline' },
