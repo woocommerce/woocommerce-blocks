45f710882a913eab8f24e7f6a58e180d9e87131a
Saad Tarhi <saad.trh@gmail.com>
2023-03-27 07:14:39 +0000

    Fix duplicate "Shipping method" Block on the front-end
    - Issue: When local pickup is enabled and the shipping method's
    description is edited, the "Shipping method" is duplicated on the
    front-end
    
    - Solution: The previous regex didn't match some special characters such
    as ".", "!", etc. Because of that, it considers the Local Pickup toggle
    template to be missing and duplicate it.

--- src/BlockTypes/Checkout.php
+++ src/BlockTypes/Checkout.php
@@ -155,11 +155,11 @@ class Checkout extends AbstractBlock {
 		 * Add the Local Pickup toggle to checkouts missing this forced template.
 		 */
 		$local_pickup_inner_blocks = '<div data-block-name="woocommerce/checkout-shipping-method-block" class="wp-block-woocommerce-checkout-shipping-method-block"></div>' . PHP_EOL . PHP_EOL . '<div data-block-name="woocommerce/checkout-pickup-options-block" class="wp-block-woocommerce-checkout-pickup-options-block"></div>' . PHP_EOL . PHP_EOL . '$0';
-		$has_local_pickup_regex    = '/<div[\n\r\s\ta-zA-Z0-9_\-=\'"]*data-block-name="woocommerce\/checkout-shipping-method-block"[\n\r\s\ta-zA-Z0-9_\-=\'"]*>/mi';
+		$has_local_pickup_regex    = '/<div[^<]*?data-block-name="woocommerce\/checkout-shipping-method-block"[^>]*?>/mi';
 		$has_local_pickup          = preg_match( $has_local_pickup_regex, $content );
 
 		if ( ! $has_local_pickup ) {
-			$shipping_address_block_regex = '/<div[\n\r\s\ta-zA-Z0-9_\-=\'"]*data-block-name="woocommerce\/checkout-shipping-address-block" class="wp-block-woocommerce-checkout-shipping-address-block"[\n\r\s\ta-zA-Z0-9_\-=\'"]*><\/div>/mi';
+			$shipping_address_block_regex = '/<div[^<]*?data-block-name="woocommerce\/checkout-shipping-address-block" class="wp-block-woocommerce-checkout-shipping-address-block"[^>]*?><\/div>/mi';
 			$content                      = preg_replace( $shipping_address_block_regex, $local_pickup_inner_blocks, $content );
 		}
 
