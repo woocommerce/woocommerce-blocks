f77eceed6fbc58edcc9e4d91a2b2a4ea06754b08
Alexandre Lara <allexandrelara@gmail.com>
2023-03-28 18:30:15 -0300

    Fix unsupported block error when using inner blocks outside the Single Product template

--- assets/js/atomic/blocks/product-elements/add-to-cart-form/index.tsx
+++ assets/js/atomic/blocks/product-elements/add-to-cart-form/index.tsx
@@ -2,12 +2,6 @@
  * External dependencies
  */
 import { registerBlockSingleProductTemplate } from '@woocommerce/atomic-utils';
-import {
-	getBlockType,
-	registerBlockType,
-	unregisterBlockType,
-} from '@wordpress/blocks';
-import { subscribe, select } from '@wordpress/data';
 import { Icon, button } from '@wordpress/icons';
 
 /**
@@ -16,63 +10,24 @@ import { Icon, button } from '@wordpress/icons';
 import metadata from './block.json';
 import edit from './edit';
 
-// When the post/page editor loads, register the block with the ancestor key.
-subscribe( () => {
-	const block = getBlockType( metadata.name );
+const blockSettings = {
+	edit,
+	icon: {
+		src: (
+			<Icon
+				icon={ button }
+				className="wc-block-editor-components-block-icon"
+			/>
+		),
+	},
+	ancestor: [ 'woocommerce/single-product' ],
+	save() {
+		return null;
+	},
+};
 
-	const store = select( 'core/edit-post' );
-
-	if ( block === undefined && store !== undefined ) {
-		// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
-		registerBlockType( metadata, {
-			edit,
-			icon: {
-				src: (
-					<Icon
-						icon={ button }
-						className="wc-block-editor-components-block-icon"
-					/>
-				),
-			},
-			ancestor: [ 'woocommerce/single-product' ],
-		} );
-	}
-}, 'core/edit-post' );
-
-// When the site editor loads, register the block as a global block on the single product template, otherwise register it as block with the ancestor key.
 registerBlockSingleProductTemplate( {
-	registerBlockFn: () => {
-		// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
-		registerBlockType( metadata, {
-			icon: {
-				src: (
-					<Icon
-						icon={ button }
-						className="wc-block-editor-components-block-icon"
-					/>
-				),
-			},
-			edit,
-			save() {
-				return null;
-			},
-		} );
-	},
-	unregisterBlockFn: () => {
-		unregisterBlockType( metadata.name );
-		// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
-		registerBlockType( metadata, {
-			edit,
-			icon: {
-				src: (
-					<Icon
-						icon={ button }
-						className="wc-block-editor-components-block-icon"
-					/>
-				),
-			},
-			ancestor: [ 'woocommerce/single-product' ],
-		} );
-	},
 	blockName: metadata.name,
+	blockMetadata: metadata,
+	blockSettings,
 } );
--- assets/js/atomic/blocks/product-elements/product-meta/index.tsx
+++ assets/js/atomic/blocks/product-elements/product-meta/index.tsx
@@ -2,13 +2,7 @@
  * External dependencies
  */
 import { box as icon } from '@wordpress/icons';
-import {
-	getBlockType,
-	registerBlockType,
-	unregisterBlockType,
-} from '@wordpress/blocks';
 import { registerBlockSingleProductTemplate } from '@woocommerce/atomic-utils';
-import { subscribe, select } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -16,39 +10,13 @@ import { subscribe, select } from '@wordpress/data';
 import edit from './edit';
 import metadata from './block.json';
 
-// When the post/page editor loads, register the block with the ancestor key.
-subscribe( () => {
-	const block = getBlockType( metadata.name );
-
-	const store = select( 'core/edit-post' );
-
-	if ( block === undefined && store !== undefined ) {
-		// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
-		registerBlockType( metadata, {
-			edit,
-			icon,
-			ancestor: [ 'woocommerce/single-product' ],
-		} );
-	}
-}, 'core/edit-post' );
-
-// When the site editor loads, register the block as a global block on the single product template, otherwise register it as block with the ancestor key.
 registerBlockSingleProductTemplate( {
-	registerBlockFn: () => {
-		// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
-		registerBlockType( metadata, {
-			icon,
-			edit,
-		} );
-	},
-	unregisterBlockFn: () => {
-		unregisterBlockType( metadata.name );
-		// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
-		registerBlockType( metadata, {
-			edit,
-			icon,
-			ancestor: [ 'woocommerce/single-product' ],
-		} );
-	},
 	blockName: metadata.name,
+	// @ts-expect-error: `metadata` currently does not have a type definition in WordPress core
+	blockMetadata: metadata,
+	blockSettings: {
+		edit,
+		icon,
+		ancestor: [ 'woocommerce/single-product' ],
+	},
 } );
