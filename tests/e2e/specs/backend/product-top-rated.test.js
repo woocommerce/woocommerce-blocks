48b1eefc0448b156e5f3e3349fd2bb3d413b8857
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-03-30 12:00:17 +0200

    Replace insertBlockDontWaitForInsertClose with insertBlockUsingQuickInserter in Top Rated Products tests

--- tests/e2e/specs/backend/product-top-rated.test.js
+++ tests/e2e/specs/backend/product-top-rated.test.js
@@ -2,16 +2,15 @@
  * External dependencies
  */
 import { switchUserToAdmin } from '@wordpress/e2e-test-utils';
-import { visitBlockPage } from '@woocommerce/blocks-test-utils';
+import {
+	insertBlockUsingQuickInserter,
+	visitBlockPage,
+} from '@woocommerce/blocks-test-utils';
 
 /**
  * Internal dependencies
  */
-import {
-	removeAllBlocks,
-	getBlocksBySlug,
-	insertBlockDontWaitForInsertClose,
-} from '../../utils.js';
+import { removeAllBlocks, getBlocksBySlug } from '../../utils.js';
 
 const block = {
 	name: 'Top Rated Products',
@@ -30,12 +29,13 @@ describe( `${ block.name } Block`, () => {
 	} );
 
 	it( 'renders without crashing', async () => {
-		await insertBlockDontWaitForInsertClose( block.name );
+		await insertBlockUsingQuickInserter( block.name );
 		await expect( page ).toRenderBlock( block );
 	} );
 
 	it( 'can be inserted more than once', async () => {
-		await insertBlockDontWaitForInsertClose( block.name );
+		await insertBlockUsingQuickInserter( block.name );
+		await insertBlockUsingQuickInserter( block.name );
 		expect( await getBlocksBySlug( block.slug ) ).toHaveLength( 2 );
 	} );
 } );
