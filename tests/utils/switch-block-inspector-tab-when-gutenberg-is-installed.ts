712c243cf385c5dae69d93a36adaa8a4c8a8dec2
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-03-31 13:03:52 +0200

    Make sure sidebar is open when calling switchBlockInspectorTab()

--- tests/utils/switch-block-inspector-tab-when-gutenberg-is-installed.ts
+++ tests/utils/switch-block-inspector-tab-when-gutenberg-is-installed.ts
@@ -3,8 +3,17 @@
  */
 import { switchBlockInspectorTab } from '@wordpress/e2e-test-utils';
 
+/**
+ * Internal dependencies
+ */
+import { openSettingsSidebar } from '../e2e/utils.js';
+
 export const switchBlockInspectorTabWhenGutenbergIsInstalled = async (
 	tabName: string
 ) => {
+	// Open the sidebar in case it was closed.
+	await openSettingsSidebar();
+
+	// Switch to the tab.
 	await switchBlockInspectorTab( tabName );
 };
