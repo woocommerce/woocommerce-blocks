06660431240aa56ee7279616ef7a1e00ca8ccac7
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-03-31 15:14:35 +0200

    Select paragraph block before inserting another block using slash

--- tests/utils/insert-block-using-slash.ts
+++ tests/utils/insert-block-using-slash.ts
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { canvas, insertBlock } from '@wordpress/e2e-test-utils';
+import { selectBlockByName } from '@woocommerce/blocks-test-utils';
 
 /**
  * Internal dependencies
@@ -10,6 +11,7 @@ import SELECTORS from './selectors';
 
 export const insertBlockUsingSlash = async ( blockTitle: string ) => {
 	await insertBlock( 'Paragraph' );
+	await selectBlockByName( 'core/paragraph' );
 	await canvas().keyboard.type( `/${ blockTitle }` );
 	await canvas().waitForSelector( SELECTORS.popover );
 	await canvas().keyboard.press( 'Enter' );
