18f88b70d16d606457c99a4f99e08bb3c22c133c
Karol Manijak <karol.manijak@automattic.com>
2023-04-03 16:59:27 +0200

    Comment out the integration with Airtable

--- .github/monitor-typescript-errors/index.js
+++ .github/monitor-typescript-errors/index.js
@@ -1,9 +1,8 @@
 const fs = require( 'fs' );
 const { getOctokit, context } = require( '@actions/github' );
-const { setFailed, getInput, setOutput } = require( '@actions/core' );
+const { getInput, setOutput } = require( '@actions/core' );
 const { parseXml, getFilesWithNewErrors } = require( './utils/xml' );
 const { generateMarkdownMessage } = require( './utils/markdown' );
-const { addRecord } = require( './utils/airtable' );
 const { addComment } = require( './utils/github' );
 
 const runner = async () => {
@@ -58,13 +57,16 @@ const runner = async () => {
 		}
 	}
 
-	if ( process.env[ 'CURRENT_BRANCH' ] === 'trunk' ) {
-		try {
-			await addRecord( currentCheckStyleFileContentParsed.totalErrors );
-		} catch ( error ) {
-			setFailed( error );
-		}
-	}
+	/**
+	 * TODO: Airtable integration is failing auth, so we're disabling it for now.
+	 */
+	// if ( process.env[ 'CURRENT_BRANCH' ] === 'trunk' ) {
+	// 	try {
+	// 		await addRecord( currentCheckStyleFileContentParsed.totalErrors );
+	// 	} catch ( error ) {
+	// 		setFailed( error );
+	// 	}
+	// }
 };
 
 runner();
