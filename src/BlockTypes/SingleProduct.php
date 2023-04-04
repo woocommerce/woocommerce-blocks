e39d65e11115a710eaae15957a8617d690afd791
Alexandre Lara <allexandrelara@gmail.com>
2023-04-04 11:57:23 -0300

    Fix single-product-frontend.js 404 error

--- src/BlockTypes/SingleProduct.php
+++ src/BlockTypes/SingleProduct.php
@@ -138,4 +138,15 @@ class SingleProduct extends AbstractBlock {
 			}
 		}
 	}
+
+	/**
+	 * Get the frontend script handle for this block type.
+	 *
+	 * @param string $key Data to get, or default to everything.
+	 *
+	 * @return null This block has no frontend script.
+	 */
+	protected function get_block_type_script( $key = null ) {
+		return null;
+	}
 }
