41c2aa7a0f8ad56cd829f277561cc7b4770e3d52
Luigi <gigitux@gmail.com>
2023-04-04 20:00:54 +0200

    add tooltip

--- assets/js/atomic/blocks/product-elements/add-to-cart-form/edit.tsx
+++ assets/js/atomic/blocks/product-elements/add-to-cart-form/edit.tsx
@@ -3,7 +3,7 @@
  */
 import { useBlockProps } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
-import { Button, Disabled } from '@wordpress/components';
+import { Button, Disabled, Tooltip } from '@wordpress/components';
 import { Skeleton } from '@woocommerce/base-components/skeleton';
 
 /**
@@ -21,20 +21,34 @@ const Edit = () => {
 
 	return (
 		<div { ...blockProps }>
-			<Skeleton numberOfLines={ 3 } />
-			<Disabled>
-				<input
-					type={ 'number' }
-					value={ '1' }
-					className={ 'wc-block-add-to-cart-form__quantity' }
-				/>
-				<Button
-					variant={ 'primary' }
-					className={ 'wc-block-add-to-cart-form__button' }
-				>
-					{ __( 'Add to cart', 'woo-gutenberg-products-block' ) }
-				</Button>
-			</Disabled>
+			<Tooltip
+				text="Customer will see product add-to-cart options in this space, dependend on the product type. "
+				position="bottom right"
+			>
+				<div className="wc-block-editor-container">
+					<Skeleton numberOfLines={ 3 } />
+					<Disabled>
+						<input
+							type={ 'number' }
+							value={ '1' }
+							className={
+								'wc-block-editor-add-to-cart-form__quantity'
+							}
+						/>
+						<Button
+							variant={ 'primary' }
+							className={
+								'wc-block-editor-add-to-cart-form__button'
+							}
+						>
+							{ __(
+								'Add to cart',
+								'woo-gutenberg-products-block'
+							) }
+						</Button>
+					</Disabled>
+				</div>
+			</Tooltip>
 		</div>
 	);
 };
