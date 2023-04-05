31506f348d19905f265a318c98634f048fc9f8c1
hsingyuc <hsingyuc.7@gmail.com>
2023-04-04 23:07:20 -0700

    Move validation check to order controller

--- src/StoreApi/Routes/V1/Order.php
+++ src/StoreApi/Routes/V1/Order.php
@@ -80,93 +80,15 @@ class Order extends AbstractRoute {
 	public function is_authorized( \WP_REST_Request $request ) {
 		$order_id  = absint( $request['id'] );
 		$order_key = wc_clean( wp_unslash( $request->get_param( 'key' ) ) );
-		$order     = $this->order_controller->get_order( $order_id );
 
-		if ( ! $order || $order->get_id() !== $order_id || ! hash_equals( $order->get_order_key(), $order_key ) ) {
-			return new \WP_Error( 'invalid_order', __( 'Sorry, this order is invalid and cannot be paid for.', 'woo-gutenberg-products-block' ), array( 'status' => 401 ) );
-		}
-
-		// Logged out customer does not have permission to pay for this order.
-		if ( ! current_user_can( 'pay_for_order', $order_id ) && ! is_user_logged_in() ) {
-			return new \WP_Error( 'invalid_user', __( 'Please log in to your account below to continue to the payment form.', 'woo-gutenberg-products-block' ), array( 'status' => 403 ) );
-		}
-
-		// Logged in customer trying to pay for someone else's order.
-		if ( ! current_user_can( 'pay_for_order', $order_id ) ) {
-			return new \WP_Error( 'invalid_user', __( 'This order cannot be paid for. Please contact us if you need assistance.', 'woo-gutenberg-products-block' ), array( 'status' => 403 ) );
-		}
-
-		// Does not need payment.
-		if ( ! $order->needs_payment() ) {
-			/* translators: %s: order status */
-			return new \WP_Error( '@Todo', sprintf( __( 'This order&rsquo;s status is &ldquo;%s&rdquo;&mdash;it cannot be paid for. Please contact us if you need assistance.', 'woo-gutenberg-products-block' ), wc_get_order_status_name( $order->get_status() ) ), array( 'status' => 403 ) );
-		}
-
-		// Ensure order items are still stocked if paying for a failed order. Pending orders do not need this check because stock is held.
-		if ( ! $order->has_status( wc_get_is_pending_statuses() ) ) {
-			$quantities = array();
-
-			foreach ( $order->get_items() as $item_key => $item ) {
-				if ( $item && is_callable( array( $item, 'get_product' ) ) ) {
-					$product = $item->get_product();
-
-					if ( ! $product ) {
-						continue;
-					}
-
-					$quantities[ $product->get_stock_managed_by_id() ] = isset( $quantities[ $product->get_stock_managed_by_id() ] ) ? $quantities[ $product->get_stock_managed_by_id() ] + $item->get_quantity() : $item->get_quantity();
-				}
-			}
-
-			// Stock levels may already have been adjusted for this order (in which case we don't need to worry about checking for low stock).
-			if ( ! $order->get_data_store()->get_stock_reduced( $order->get_id() ) ) {
-				foreach ( $order->get_items() as $item_key => $item ) {
-					if ( $item && is_callable( array( $item, 'get_product' ) ) ) {
-						$product = $item->get_product();
-
-						if ( ! $product ) {
-							continue;
-						}
-
-						/**
-						 * Filters whether or not the product is in stock for this pay for order.
-						 *
-						 * @param boolean True if in stock.
-						 * @param \WC_Product $product Product.
-						 * @param \WC_Order $order Order.
-						 *
-						 * @since 9.8.0-dev
-						 */
-						if ( ! apply_filters( 'woocommerce_pay_order_product_in_stock', $product->is_in_stock(), $product, $order ) ) {
-							/* translators: %s: product name */
-							return new \WP_Error( '@Todo', sprintf( __( 'Sorry, "%s" is no longer in stock so this order cannot be paid for. We apologize for any inconvenience caused.', 'woo-gutenberg-products-block' ), $product->get_name() ), array( 'status' => 403 ) );
-						}
-
-						// We only need to check products managing stock, with a limited stock qty.
-						if ( ! $product->managing_stock() || $product->backorders_allowed() ) {
-							continue;
-						}
-
-						// Check stock based on all items in the cart and consider any held stock within pending orders.
-						$held_stock     = wc_get_held_stock_quantity( $product, $order->get_id() );
-						$required_stock = $quantities[ $product->get_stock_managed_by_id() ];
-
-						/**
-						 * Filters whether or not the product has enough stock.
-						 *
-						 * @param boolean True if has enough stock.
-						 * @param \WC_Product $product Product.
-						 * @param \WC_Order $order Order.
-						 *
-						 * @since 9.8.0-dev
-						 */
-						if ( ! apply_filters( 'woocommerce_pay_order_product_has_enough_stock', ( $product->get_stock_quantity() >= ( $held_stock + $required_stock ) ), $product, $order ) ) {
-							/* translators: 1: product name 2: quantity in stock */
-							return new \WP_Error( '@Todo', sprintf( __( 'Sorry, we do not have enough "%1$s" in stock to fulfill your order (%2$s available). We apologize for any inconvenience caused.', 'woo-gutenberg-products-block' ), $product->get_name(), wc_format_stock_quantity_for_display( $product->get_stock_quantity() - $held_stock, $product ) ), array( 'status' => 403 ) );
-						}
-					}
-				}
-			}
+		try {
+			$this->order_controller->validate_order_key( $order_id, $order_key );
+		} catch ( RouteException $error ) {
+			return new \WP_Error(
+				$error->getErrorCode(),
+				$error->getMessage(),
+				array( 'status' => $error->getCode() )
+			);
 		}
 
 		return true;
