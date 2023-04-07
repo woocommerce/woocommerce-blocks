2920a56009cc75b806cdeccf2a962bf7232be280
Thomas Roberts <thomas.roberts@automattic.com>
2023-04-07 13:40:17 +0100

    Decide whether to select a rate for all packages if rate is collectable

--- assets/js/base/context/hooks/shipping/use-shipping-data.ts
+++ assets/js/base/context/hooks/shipping/use-shipping-data.ts
@@ -99,10 +99,8 @@ export const useShippingData = (): ShippingData => {
 			 *
 			 * Forces pickup location to be selected for all packages since we don't allow a mix of shipping and pickup.
 			 */
-			if (
+			if ( hasCollectableRate( newShippingRateId.split( ':' )[ 0 ] ) ) {
 				// Do not pass a packageId because we want this pickup rate to apply to all packages.
-				hasSelectedLocalPickup
-			) {
 				selectPromise = dispatchSelectShippingRate( newShippingRateId );
 			} else {
 				selectPromise = dispatchSelectShippingRate(
