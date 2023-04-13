8a2f449812a30df739a450687f534fa86eb83fe0
roykho <roykho77@gmail.com>
2023-04-13 11:09:12 -0700

    Test with timeouts removed

--- tests/e2e/specs/shopper/filter-products-by-attribute.test.ts
+++ tests/e2e/specs/shopper/filter-products-by-attribute.test.ts
@@ -259,7 +259,6 @@ describe( `${ block.name } Block`, () => {
 		} );
 
 		it( 'should render products', async () => {
-			page.setDefaultTimeout( 200000 );
 			const products = await page.$$(
 				selectors.frontend.queryProductsList
 			);
@@ -268,7 +267,6 @@ describe( `${ block.name } Block`, () => {
 		} );
 
 		it( 'should show only products that match the filter', async () => {
-			page.setDefaultTimeout( 200000 );
 			const isRefreshed = jest.fn( () => void 0 );
 			page.on( 'load', isRefreshed );
 
@@ -301,7 +299,6 @@ describe( `${ block.name } Block`, () => {
 		} );
 
 		it( 'should refresh the page only if the user clicks on button', async () => {
-			page.setDefaultTimeout( 200000 );
 			await page.goto( editorPageUrl );
 
 			await waitForCanvas();
