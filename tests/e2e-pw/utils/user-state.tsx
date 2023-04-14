34fe967c2149db19bac23e84cd5f6fe980c6cadf
Thomas Roberts <thomas.roberts@automattic.com>
2023-04-14 14:54:51 +0100

    Add getShopperPage and getLoggedOutShopperPage functions

--- tests/e2e-pw/utils/user-state.tsx
+++ tests/e2e-pw/utils/user-state.tsx
@@ -14,3 +14,27 @@ export const getMerchantPage = async (
 	} );
 	return await context.newPage();
 };
+
+/**
+ * Gets a page logged in with a user session. This is a shopper who has logged into their account.
+ */
+export const getShopperPage = async (
+	browser: PlaywrightWorkerArgs[ 'browser' ]
+) => {
+	const context = await browser.newContext( {
+		storageState: process.env.CUSTOMERSTATE,
+	} );
+	return await context.newPage();
+};
+
+/**
+ * Gets a page not logged in with any session. This will be a shopper who has not logged into their account yet.
+ */
+export const getLoggedOutShopperPage = async (
+	browser: PlaywrightWorkerArgs[ 'browser' ]
+) => {
+	const context = await browser.newContext( {
+		storageState: null,
+	} );
+	return await context.newPage();
+};
