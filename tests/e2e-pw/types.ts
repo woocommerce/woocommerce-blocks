923836099aa0cd0e181129f775e9d85761e3c12b
Thomas Roberts <thomas.roberts@automattic.com>
2023-04-14 14:49:45 +0100

    Add BlockTestingProperties type
    Contains information relating to a block, used when selecting it in the editor/front-end

---
+++ tests/e2e-pw/types.ts
@@ -0,0 +1,8 @@
+/**
+ * Interface that describes the properties of a block, used to help with finding/editing the block in E2E tests.
+ */
+export interface BlockTestingProperties {
+	title: string;
+	selector: string;
+	slug: string;
+}
