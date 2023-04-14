455673ac21d7054e1a9cf2bf0114e10f181ee77d
Thomas Roberts <thomas.roberts@automattic.com>
2023-04-14 19:49:09 +0100

    Update Cart test to use correct selector

--- tests/e2e-pw/tests/blocks/cart/merchant.spec.tsx
+++ tests/e2e-pw/tests/blocks/cart/merchant.spec.tsx
@@ -12,14 +12,19 @@ import { BlockTestingProperties } from '../../../types';
 const blockProperties: BlockTestingProperties = {
 	title: 'Cart',
 	slug: 'woocommerce/cart',
-	selector: '.wp-block-woocommerce-cart',
+	selectors: {
+		block: '.wp-block-woocommerce-cart',
+		insertButton: "//button//span[text()='Cart']",
+	},
 };
 
 test.describe( 'Merchant → Cart', () => {
 	test.use( { storageState: process.env.ADMINSTATE } );
 	test( 'it renders without crashing', async ( { page } ) => {
 		await editBlockPage( page, blockProperties );
-		const blockPresence = page.locator( blockProperties.selector );
+		const blockPresence = page.locator(
+			blockProperties.selectors.className
+		);
 		expect( blockPresence ).toBeTruthy();
 	} );
 } );
