c480759c6a74684254d592d92ccc87c4c1b330a7
Alba Rincón <alba.rincon@automattic.com>
2023-04-17 13:50:58 +0200

    Add new `Product features: rigid grid` pattern

---
+++ patterns/product-feature-rigid-grid.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Title: WooCommerce Product Features Rigid Grid
+ * Slug: woocommerce-blocks/product-features-rigid-grid
+ * Categories: WooCommerce
+ */
+?>
+
+<!-- wp:group {"align":"wide","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide">
+	<!-- wp:heading {"textAlign":"left","level":4,"align":"wide"} -->
+	<h4 class="wp-block-heading alignwide has-text-align-left">
+		<strong>What makes this product unique?</strong>
+	</h4>
+	<!-- /wp:heading -->
+
+	<!-- wp:heading {"level":6,"align":"wide","style":{"typography":{"textTransform":"none"}}} -->
+	<h6 class="wp-block-heading alignwide" style="text-transform:none">
+		At dui sed egestas suspendisse. Consectetur tellus mattis aliquam semper posuere aliquam. Magna eu vulputate et blandit mi placerat enim.
+	</h6>
+	<!-- /wp:heading -->
+
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|40"}}}} -->
+	<div class="wp-block-columns alignwide" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--40)">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size">
+				At dui sed egestas suspendisse. Consectetur tellus mattis aliquam semper posuere aliquam. Magna eu vulputate et blandit mi placerat enim.
+			</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size">
+				At dui sed egestas suspendisse. Consectetur tellus mattis aliquam semper posuere aliquam. Magna eu vulputate et blandit mi placerat enim.
+			</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:columns {"align":"wide","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|50"}}}} -->
+	<div class="wp-block-columns alignwide" style="margin-bottom:var(--wp--preset--spacing--50)">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size">
+				At dui sed egestas suspendisse. Consectetur tellus mattis aliquam semper posuere aliquam. Magna eu vulputate et blandit mi placerat enim.
+			</p>
+			<!-- /wp:paragraph --></div>
+		<!-- /wp:column -->
+
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image -->
+			<figure class="wp-block-image"><img alt="" /></figure>
+			<!-- /wp:image -->
+
+			<!-- wp:paragraph {"fontSize":"small"} -->
+			<p class="has-small-font-size">
+				At dui sed egestas suspendisse. Consectetur tellus mattis aliquam semper posuere aliquam. Magna eu vulputate et blandit mi placerat enim.
+			</p>
+			<!-- /wp:paragraph -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+	<div class="wp-block-buttons alignwide">
+		<!-- wp:button -->
+		<div class="wp-block-button">
+			<a class="wp-block-button__link wp-element-button">Add To Cart</a>
+		</div>
+		<!-- /wp:button -->
+	</div>
+	<!-- /wp:buttons -->
+</div>
+<!-- /wp:group -->
