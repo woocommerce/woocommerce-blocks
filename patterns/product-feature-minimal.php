957d5ef7b36b725fc71838533ae2a512774ca2cb
Alba Rincón <alba.rincon@automattic.com>
2023-04-17 14:20:21 +0200

    Add new `Featured products: minimal 2` pattern

---
+++ patterns/product-feature-minimal.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Title: WooCommerce Product Features Minimal
+ * Slug: woocommerce-blocks/product-features-minimal
+ * Categories: WooCommerce
+ */
+?>
+
+<!-- wp:columns {"verticalAlignment":null,"align":"wide"} -->
+<div class="wp-block-columns alignwide">
+	<!-- wp:column {"verticalAlignment":"center","width":"66.66%"} -->
+	<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:66.66%">
+		<!-- wp:heading {"textAlign":"left","level":4,"align":"wide"} -->
+		<h4 class="wp-block-heading alignwide has-text-align-left"><strong>The timeless trench</strong></h4>
+		<!-- /wp:heading -->
+
+		<!-- wp:paragraph -->
+		<p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna
+			aliqua. Ut enim ad minim veniam, quis nostrum.</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph -->
+		<p><strong style="text-decoration: underline;">Discover more</strong></p>
+		<!-- /wp:paragraph --></div>
+	<!-- /wp:column -->
+
+	<!-- wp:column {"width":"33.33%"} -->
+	<div class="wp-block-column" style="flex-basis:33.33%">
+		<!-- wp:image -->
+		<figure class="wp-block-image size-full"><img /></figure>
+		<!-- /wp:image --></div>
+	<!-- /wp:column --></div>
+<!-- /wp:columns -->
