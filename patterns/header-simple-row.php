0ea624b82f4941b5fee5287b3e7ac5ea234bc4ae
Alba Rincón <alba.rincon@automattic.com>
2023-04-18 09:29:23 +0200

    Add title, spacers and update customer account icon

--- patterns/header-simple-row.php
+++ patterns/header-simple-row.php
@@ -1,25 +1,23 @@
 <?php
 /**
- * Title: WooCommerce Header 2
- * Slug: woocommerce-blocks/header-2
+ * Title: WooCommerce Header Simple Row
+ * Slug: woocommerce-blocks/header-simple-row
  * Categories: WooCommerce
  * Block Types: core/template-part/header
  */
 ?>
-
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"8px","right":"8px","bottom":"8px","left":"8px"},"margin":{"top":"0px","bottom":"0px"}},"elements":{"link":{"color":{"text":"var:preset|color|background"}}}},"backgroundColor":"contrast","textColor":"base","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="margin-top:0px;margin-bottom:0px;padding-top:8px;padding-right:8px;padding-bottom:8px;padding-left:8px">
-	<!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"14px"}}} -->
-	<p class="has-text-align-center" style="font-size:14px">Free shipping on all orders over $100.</p>
-	<!-- /wp:paragraph -->
-</div>
-<!-- /wp:group -->
 
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"2%","bottom":"16px","left":"2%","top":"16px"},"margin":{"top":"0px","bottom":"0px"}}},"className":"sticky-header","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignfull sticky-header" style="margin-top:0px;margin-bottom:0px;padding-top:16px;padding-right:2%;padding-bottom:16px;padding-left:2%">
 	<!-- wp:group {"style":{"spacing":{"blockGap":"40px"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
 	<div class="wp-block-group">
 		<!-- wp:site-logo {"shouldSyncIcon":false} /-->
+		<!-- wp:site-title /-->
+
+		<!-- wp:spacer -->
+		<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- /wp:spacer -->
+
 		<!-- wp:navigation /-->
 	</div>
 	<!-- /wp:group -->
@@ -29,12 +27,16 @@
 		<!-- wp:group {"style":{"spacing":{"blockGap":"16px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 		<div class="wp-block-group">
 			<!-- wp:separator {"className":"is-style-wide"} -->
-			<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+			<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" />
 			<!-- /wp:separator -->
 		</div>
 		<!-- /wp:group -->
 
-		<!-- wp:woocommerce/customer-account {"displayStyle":"icon_only","iconClass":"wc-block-customer-account__account-icon"} /-->
+		<!-- wp:woocommerce/customer-account {"displayStyle":"icon_only","iconStyle":"alt","iconClass":"wc-block-customer-account__account-icon"} /-->
+
+		<!-- wp:spacer -->
+		<div style="height:100px" aria-hidden="true" class="wp-block-spacer"></div>
+		<!-- /wp:spacer -->
 
 		<!-- wp:woocommerce/mini-cart {"style":{"typography":{"fontSize":"14px"}}} /-->
 	</div>
