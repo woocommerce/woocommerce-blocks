c26f9f6e857d639908300221c65b0516fbbf7d76
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-04-19 10:04:13 +0200

    Adding /build directory to release

---
+++ build/active-filters-frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wc-blocks-data-store', 'wc-price-format', 'wc-settings', 'wp-data', 'wp-element', 'wp-html-entities', 'wp-i18n', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-primitives', 'wp-url'), 'version' => '3fc95e57a36710c6ce28c27f60ded227');
