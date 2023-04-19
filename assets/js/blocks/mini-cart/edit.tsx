016e05dcb0470863b457596b8d2638a1879b5ad7
Karol Manijak <karol.manijak@automattic.com>
2023-04-19 16:53:24 +0200

    Fix linting in assets/js/mini-cart/edit.tsx file

--- assets/js/blocks/mini-cart/edit.tsx
+++ assets/js/blocks/mini-cart/edit.tsx
@@ -72,11 +72,11 @@ const Edit = ( { attributes, setAttributes }: Props ): ReactElement => {
 								'woo-gutenberg-products-block'
 							) }
 							onChange={ ( value ) => {
-								setAttributes(
-									{
-										addToCartBehaviour: value ? 'open_drawer' : 'none'
-									}
-								);
+								setAttributes( {
+									addToCartBehaviour: value
+										? 'open_drawer'
+										: 'none',
+								} );
 							} }
 							help={ __(
 								'Select what happens when a customer adds a product to the cart.',
