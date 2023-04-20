1acf6ecadea0aa7b75c8a1129e2e314499483ebb
Alba Rincón <alba.rincon@automattic.com>
2023-04-20 10:43:58 +0200

    Updates after review

--- patterns/header-centered-search-pattern.php
+++ patterns/header-centered-search-pattern.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Title: Header Centered Search
- * Slug: woocommerce-blocks/header-centered-search
+ * Title: Header Centered Menu with Search
+ * Slug: woocommerce-blocks/header-centered-menu-with-search
  * Categories: WooCommerce
  * Block Types: core/template-part/header
  */
@@ -11,16 +11,15 @@
 <div class="wp-block-group alignfull">
 	<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"2%","bottom":"16px","left":"2%","top":"16px"},"margin":{"top":"0px","bottom":"0px"}}},"className":"sticky-header","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
 	<div class="wp-block-group alignfull sticky-header" style="margin-top:0px;margin-bottom:0px;padding-top:16px;padding-right:2%;padding-bottom:16px;padding-left:2%">
-		<!-- wp:group {"style":{"spacing":{"blockGap":"40px"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
+		<!-- wp:group {"style":{"spacing":{"blockGap":"20px"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
 		<div class="wp-block-group">
 			<!-- wp:site-logo {"shouldSyncIcon":false} /-->
 			<!-- wp:site-title /-->
 		</div>
 		<!-- /wp:group -->
-
 		<!-- wp:group {"style":{"spacing":{"blockGap":"8px"},"typography":{"fontSize":"13px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
 		<div class="wp-block-group" style="font-size:13px">
-			<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search for products or brands","width":350,"widthUnit":"px","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"color":{"background":"#ffffff00"}},"textColor":"foreground"} /-->
+			<!-- wp:search {"label":"Search","showLabel":false,"placeholder":"Search our store","width":350,"widthUnit":"px","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"style":{"color":{"background":"#ffffff00"}},"textColor":"foreground"} /-->
 
 			<!-- wp:woocommerce/customer-account {"displayStyle":"icon_only","iconClass":"wc-block-customer-account__account-icon","style":{"spacing":{"margin":{"left":"10px"}}}} /-->
 
@@ -35,7 +34,8 @@
 	<!-- /wp:separator -->
 
 	<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-	<div class="wp-block-group"><!-- wp:navigation /-->
+	<div class="wp-block-group">
+		<!-- wp:navigation {"ref":1855,"style":{"typography":{"fontStyle":"normal","fontWeight":"400"}}} /-->
 	</div>
 	<!-- /wp:group -->
 
@@ -44,3 +44,4 @@
 	<!-- /wp:separator -->
 </div>
 <!-- /wp:group -->
+<style>.wp-block-search__input {border:none!important;}</style>
