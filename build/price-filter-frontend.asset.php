13feb21563d31a7c6a278dadcd91e64774f84b02
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-04-21 10:42:57 +0200

    Adding /build directory to release

---
+++ build/price-filter-frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wc-blocks-data-store', 'wc-price-format', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n', 'wp-is-shallow-equal', 'wp-polyfill', 'wp-url'), 'version' => '8a7dfdf537dd82379fed71daf2cd30c5');
