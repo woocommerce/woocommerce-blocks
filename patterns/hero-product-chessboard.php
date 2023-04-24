e739e67a5b7c407337b17c79d1c591e49140b9e1
Alba Rincón <alba.rincon@automattic.com>
2023-04-24 15:03:24 +0200

    Add new `Hero product chessboard` pattern

---
+++ patterns/hero-product-chessboard.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Title: Hero Product Chessboard
+ * Slug: woocommerce-blocks/hero-product-chessboard
+ * Categories: WooCommerce
+ */
+?>
+
+<!-- wp:group {"align":"full","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull">
+	<!-- wp:columns {"verticalAlignment":null,"align":"wide","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":{"top":"0","left":"0"}}}} -->
+	<div class="wp-block-columns alignwide" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+		<!-- wp:column -->
+		<div class="wp-block-column">
+			<!-- wp:image {"id":3356,"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full">
+				<img src="https://store.local/wp-content/uploads/2023/04/unsplash_qJQqAq1CTDM.png" alt="" class="wp-image-3356" />
+			</figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"constrained"}} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+			<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical","verticalAlignment":"center","justifyContent":"stretch"}} -->
+			<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+				<!-- wp:heading {"textAlign":"left","level":3,"style":{"typography":{"fontSize":"40px","fontStyle":"normal","fontWeight":"600"}}} -->
+				<h3 class="wp-block-heading has-text-align-left" style="font-size:40px;font-style:normal;font-weight:600">Unisex Leather Winter Jacket</h3>
+				<!-- /wp:heading -->
+
+				<!-- wp:paragraph {"style":{"typography":{"fontSize":"18px"},"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
+				<p style="margin-bottom:var(--wp--preset--spacing--40);font-size:18px">
+					With high-quality materials and expert craftsmanship, our products are built to last and exceed your expectations.
+				</p>
+				<!-- /wp:paragraph -->
+
+				<!-- wp:buttons -->
+				<div class="wp-block-buttons">
+					<!-- wp:button {"textAlign":"left","style":{"typography":{"fontSize":"16px"}}} -->
+					<div class="wp-block-button has-custom-font-size" style="font-size:16px">
+						<a class="wp-block-button__link has-text-align-left wp-element-button">Shop now</a>
+					</div>
+					<!-- /wp:button -->
+				</div>
+				<!-- /wp:buttons -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+
+	<!-- wp:columns {"verticalAlignment":"center","align":"wide","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":{"top":"0","left":"0"},"margin":{"top":"0","bottom":"0"}}}} -->
+	<div class="wp-block-columns alignwide are-vertically-aligned-center" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+		<!-- wp:column {"verticalAlignment":"center","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
+		<div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
+			<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"var:preset|spacing|40"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+			<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--40)">
+				<!-- wp:columns -->
+				<div class="wp-block-columns">
+					<!-- wp:column -->
+					<div class="wp-block-column">
+						<!-- wp:heading {"level":5,"style":{"typography":{"textTransform":"capitalize"}}} -->
+						<h5 class="wp-block-heading" style="text-transform:capitalize">Quality Materials</h5>
+						<!-- /wp:heading -->
+
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
+						<p style="font-size:16px">
+							We use only the highest-quality materials in our products, ensuring that they look great and last for years to come.
+						</p>
+						<!-- /wp:paragraph -->
+					</div>
+					<!-- /wp:column -->
+
+					<!-- wp:column -->
+					<div class="wp-block-column">
+						<!-- wp:heading {"level":5,"style":{"typography":{"textTransform":"capitalize"}}} -->
+						<h5 class="wp-block-heading" style="text-transform:capitalize">Expert Craftsmanship</h5>
+						<!-- /wp:heading -->
+
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
+						<p style="font-size:16px">
+							Our products are made with expert craftsmanship and attention to detail, ensuring that every stitch and seam is perfect.
+						</p>
+						<!-- /wp:paragraph -->
+					</div>
+					<!-- /wp:column -->
+				</div>
+				<!-- /wp:columns -->
+
+				<!-- wp:columns -->
+				<div class="wp-block-columns">
+					<!-- wp:column -->
+					<div class="wp-block-column">
+						<!-- wp:heading {"level":5,"style":{"typography":{"textTransform":"capitalize"}}} -->
+						<h5 class="wp-block-heading" style="text-transform:capitalize">Unique Design</h5>
+						<!-- /wp:heading -->
+
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
+						<p style="font-size:16px">
+							From bold prints and colors to intricate details and textures, our products are a perfect combination of style and function.
+						</p>
+						<!-- /wp:paragraph -->
+					</div>
+					<!-- /wp:column -->
+
+					<!-- wp:column -->
+					<div class="wp-block-column">
+						<!-- wp:heading {"level":5,"style":{"typography":{"textTransform":"capitalize"}}} -->
+						<h5 class="wp-block-heading" style="text-transform:capitalize">Customer Satisfaction</h5>
+						<!-- /wp:heading -->
+
+						<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
+						<p style="font-size:16px">
+							Our top priority is customer satisfaction, and we stand behind our products 100%.
+						</p>
+						<!-- /wp:paragraph -->
+					</div>
+					<!-- /wp:column -->
+				</div>
+				<!-- /wp:columns -->
+			</div>
+			<!-- /wp:group -->
+		</div>
+		<!-- /wp:column -->
+
+		<!-- wp:column {"verticalAlignment":"center"} -->
+		<div class="wp-block-column is-vertically-aligned-center">
+			<!-- wp:image {"id":3358,"sizeSlug":"full","linkDestination":"none"} -->
+			<figure class="wp-block-image size-full">
+				<img src="https://store.local/wp-content/uploads/2023/04/unsplash_qJQqAq1CTDM-1.png" alt="" class="wp-image-3358" />
+			</figure>
+			<!-- /wp:image -->
+		</div>
+		<!-- /wp:column -->
+	</div>
+	<!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
