5710999788aa95b727c26997a40821154db4bad2
Danae Millan <danae.millan@automattic.com>
2023-04-24 18:54:51 -0300

    Add interface to the custom field frontend block

--- assets/js/blocks/checkout/inner-blocks/checkout-custom-field-block/frontend.tsx
+++ assets/js/blocks/checkout/inner-blocks/checkout-custom-field-block/frontend.tsx
@@ -9,10 +9,19 @@ import { withFilteredAttributes } from '@woocommerce/shared-hocs';
 import attributes from './attributes';
 import Block from './block';
 
+interface FieldProps {
+	name: string;
+	label: string;
+	required: boolean;
+	type: string;
+	size?: string | undefined;
+}
+
 const FrontendBlock = ( {
 	field,
 	section,
 }: {
+	field: FieldProps;
 	section: string;
 } ): JSX.Element | null => {
 	return (
