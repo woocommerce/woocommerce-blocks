cca6a42bc7b4677a3bf2573cc0405ad416d636b0
Alba Rincón <alba.rincon@automattic.com>
2023-05-02 13:38:41 +0200

    Improve colors and margins

--- patterns/hero-product-3-split.php
+++ patterns/hero-product-3-split.php
@@ -6,12 +6,12 @@
  */
 ?>
 
-<!-- wp:columns {"verticalAlignment":null,"align":"wide","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":{"top":"0","left":"0"}}}} -->
+<!-- wp:columns {"align":"wide","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":{"top":"0","left":"0"}}}} -->
 <div class="wp-block-columns alignwide" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
 	<!-- wp:column {"width":"66.66%"} -->
 	<div class="wp-block-column" style="flex-basis:66.66%">
-		<!-- wp:media-text {"mediaPosition":"right","mediaId":3800,"mediaLink":"https://store.local/p/image/","mediaType":"image","backgroundColor":"contrast","textColor":"base"} -->
-		<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile has-base-color has-contrast-background-color has-text-color has-background">
+		<!-- wp:media-text {"mediaPosition":"right","mediaId":3800,"mediaLink":"https://store.local/p/image/","mediaType":"image","style":{"color":{"background":"#000000","text":"#ffffff"}}} -->
+		<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile has-text-color has-background" style="color:#ffffff;background-color:#000000">
 			<div class="wp-block-media-text__content">
 				<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","left":"20px","right":"20px"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
 				<div class="wp-block-group" style="padding-top:0;padding-right:20px;padding-left:20px">
@@ -25,9 +25,9 @@
 
 					<!-- wp:buttons -->
 					<div class="wp-block-buttons">
-						<!-- wp:button -->
+						<!-- wp:button {"backgroundColor":"background","textColor":"foreground"} -->
 						<div class="wp-block-button">
-							<a class="wp-block-button__link wp-element-button">Shop now</a>
+							<a class="wp-block-button__link has-foreground-color has-background-background-color has-text-color has-background wp-element-button">Shop now</a>
 						</div>
 						<!-- /wp:button -->
 					</div>
@@ -35,17 +35,16 @@
 				</div>
 				<!-- /wp:group -->
 			</div>
-
 			<figure class="wp-block-media-text__media">
-				<img src="http://store.local/wp-content/uploads/2023/04/Image.png" alt="" class="wp-image-3800 size-full" />
+				<img src="<?php echo esc_url( plugins_url() ); ?>/woocommerce-blocks/images/pattern-placeholders/product-apparel-4.jpg" alt="" class="wp-image-3800 size-full" />
 			</figure>
 		</div>
 		<!-- /wp:media-text -->
 	</div>
 	<!-- /wp:column -->
 
-	<!-- wp:column {"verticalAlignment":"center","width":"25%","style":{"spacing":{"padding":{"top":"60px","left":"60px","right":"60px","bottom":"60px"}}}} -->
-	<div class="wp-block-column is-vertically-aligned-center" style="padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px;flex-basis:25%">
+	<!-- wp:column {"verticalAlignment":"center","width":"25%","style":{"spacing":{"padding":{"top":"40px","right":"40px","bottom":"40px","left":"40px"}}}} -->
+	<div class="wp-block-column is-vertically-aligned-center" style="padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px;flex-basis:25%">
 		<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"15px"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
 		<h2 class="wp-block-heading" style="margin-top:0;margin-right:0;margin-bottom:0;margin-left:0;font-size:15px;font-style:normal;font-weight:700">Waterproof Membrane</h2>
 		<!-- /wp:heading -->
@@ -54,8 +53,8 @@
 		<p style="margin-top:10px;margin-right:0;margin-bottom:0;margin-left:0;font-size:12px">Never worry about the weather again. Keep yourself dry, warm, and looking stylish.</p>
 		<!-- /wp:paragraph -->
 
-		<!-- wp:separator {"style":{"spacing":{"margin":{"top":"30px","bottom":"30px"}}},"className":"is-style-default"} -->
-		<hr class="wp-block-separator has-alpha-channel-opacity is-style-default" style="margin-top:30px;margin-bottom:30px" />
+		<!-- wp:separator {"style":{"spacing":{"margin":{"top":"20px","bottom":"20px"}}},"className":"is-style-wide"} -->
+		<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" style="margin-top:20px;margin-bottom:20px" />
 		<!-- /wp:separator -->
 
 		<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"15px"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
@@ -66,8 +65,8 @@
 		<p style="margin-top:10px;margin-right:0;margin-bottom:0;margin-left:0;font-size:12px">Our products are made with expert craftsmanship and attention to detail, ensuring that every stitch and seam is perfect.</p>
 		<!-- /wp:paragraph -->
 
-		<!-- wp:separator {"style":{"spacing":{"margin":{"top":"30px","bottom":"30px"}}}} -->
-		<hr class="wp-block-separator has-alpha-channel-opacity" style="margin-top:30px;margin-bottom:30px" />
+		<!-- wp:separator {"style":{"spacing":{"margin":{"top":"20px","bottom":"20px"}}},"className":"is-style-wide"} -->
+		<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" style="margin-top:20px;margin-bottom:20px" />
 		<!-- /wp:separator -->
 
 		<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"15px"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
@@ -78,8 +77,8 @@
 		<p style="margin-top:10px;margin-right:0;margin-bottom:0;margin-left:0;font-size:12px">We use only the highest-quality materials in our products, ensuring that they look great and last for years to come.</p>
 		<!-- /wp:paragraph -->
 
-		<!-- wp:separator {"style":{"spacing":{"margin":{"top":"30px","bottom":"30px"}}}} -->
-		<hr class="wp-block-separator has-alpha-channel-opacity" style="margin-top:30px;margin-bottom:30px" />
+		<!-- wp:separator {"style":{"spacing":{"margin":{"top":"20px","bottom":"20px"}}},"className":"is-style-wide"} -->
+		<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" style="margin-top:20px;margin-bottom:20px" />
 		<!-- /wp:separator -->
 
 		<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","fontSize":"15px"},"spacing":{"margin":{"top":"0","right":"0","bottom":"0","left":"0"}}}} -->
