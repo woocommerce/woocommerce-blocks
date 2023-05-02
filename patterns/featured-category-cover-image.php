39ef235ed6cd62085213f1333f1571f26479a427
roykho <roykho77@gmail.com>
2023-05-02 07:21:32 -0700

    Use WP core blocks

--- patterns/featured-category-cover-image.php
+++ patterns/featured-category-cover-image.php
@@ -4,31 +4,22 @@
  * Slug: woocommerce-blocks/featured-category-cover-image
  * Categories: WooCommerce
  */
-
-// Below query is temporary work around to get patterns previews to work.
-$transient_name = 'wc_blocks_pattern_featured_category_cover_image';
-$categories     = get_transient( $transient_name );
-
-if ( ( false === $categories ) || ( defined( 'WP_DEBUG' ) && WP_DEBUG ) ) {
-	global $wpdb;
-
-	$categories = $wpdb->get_results(
-		"SELECT tt.term_id FROM {$wpdb->prefix}term_taxonomy AS tt
-		LEFT JOIN {$wpdb->prefix}terms AS t ON tt.term_id = t.term_id
-		WHERE tt.taxonomy = 'product_cat' AND tt.count > 0 AND tt.parent = 0 AND t.slug != 'uncategorized'
-		LIMIT 1",
-		ARRAY_A
-	);
-
-	set_transient( $transient_name, $categories, DAY_IN_SECONDS * 14 );
-}
-
-$category = $categories[0]['term_id'] ? $categories[0]['term_id'] : 0;
 ?>
-<?php echo '<!-- wp:woocommerce/featured-category {"dimRatio":0,"editMode":false,"focalPoint":{"x":0.52,"y":0.5},"imageFit":"cover","categoryId":' . esc_attr( $category ) . ',"overlayColor":"#F6F6F6","align":"full","textColor":"foreground"} -->'; ?>
-<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-<div class="wp-block-buttons"><!-- wp:button -->
-	<div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="<?php echo esc_url( get_category_link( $category ) ); ?>"><?php esc_html_e( 'Shop now', 'woo-gutenberg-products-block' ); ?></a></div>
-	<!-- /wp:button --></div>
-<!-- /wp:buttons -->
-<!-- /wp:woocommerce/featured-category -->
+<?php echo '<!-- wp:cover {"url":"' . esc_url( plugins_url() ) . '/woocommerce-blocks/images/pattern-placeholders/product-apparel-1.png","id":1,"dimRatio":0,"contentPosition":"top left","align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|70","right":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->'; ?>
+<div class="wp-block-cover alignwide has-custom-content-position is-position-top-left" style="padding-top:var(--wp--preset--spacing--70);padding-right:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)">
+	<span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+	<img class="wp-block-cover__image-background wp-image-1" alt="" src="<?php echo esc_url( plugins_url() ) . '/woocommerce-blocks/images/pattern-placeholders/product-apparel-1.png'; ?>" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"typography":{"lineHeight":"0"}},"fontSize":"large"} -->
+	<p class="has-text-align-left has-x-large-font-size" style="line-height:0"><strong>100% natural denim</strong></p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:paragraph {"style":{"typography":{"lineHeight":"3"}}} -->
+	<p style="line-height:3">Only the finest goes into our products. You deserve it.</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:buttons -->
+	<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"background","textColor":"foreground","style":{"border":{"width":"0px","style":"none"}},"className":"is-style-fill"} -->
+		<div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-foreground-color has-background-background-color has-text-color has-background wp-element-button" style="border-style:none;border-width:0px">Shop jeans</a></div>
+		<!-- /wp:button --></div>
+	<!-- /wp:buttons --></div>
+</div>
+<!-- /wp:cover -->
