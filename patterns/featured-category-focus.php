d4ed7f33a3d4d16161ed5b671e41c114226b097c
roykho <roykho77@gmail.com>
2023-05-02 06:13:47 -0700

    Add category focus pattern

---
+++ patterns/featured-category-focus.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Title: Featured Category Focus
+ * Slug: woocommerce-blocks/featured-category-focus
+ * Categories: WooCommerce
+ */
+?>
+<!-- wp:group {"align":"full","style":{"color":{"background":"#84bfe1"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center","flexWrap":"wrap"}} -->
+<div class="wp-block-group alignfull has-background" style="background-color:#84bfe1">
+	<!-- wp:image {"id":1,"sizeSlug":"full","linkDestination":"none"} -->
+	<figure class="wp-block-image size-full"><img src="<?php echo esc_url( plugins_url() ); ?>/woocommerce-blocks/images/pattern-placeholders/product-apparel-2.png" alt="" class="wp-image-1"/></figure>
+	<!-- /wp:image -->
+
+	<!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+	<p class="has-text-align-center has-large-font-size">adidas x Gucci women’s Gazelle sneaker</p>
+	<!-- /wp:paragraph -->
+
+	<!-- wp:buttons -->
+	<div class="wp-block-buttons"><!-- wp:button -->
+		<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Shop shoes</a></div>
+		<!-- /wp:button --></div>
+	<!-- /wp:buttons --></div>
+<!-- /wp:group -->
