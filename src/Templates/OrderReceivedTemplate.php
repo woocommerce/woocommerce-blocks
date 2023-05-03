236bed167a822669ee64ebc9fa0b0198ee3d5dc9
Mike Jolley <mike.jolley@me.com>
2023-05-03 13:39:46 +0100

    page_template_hierarchy priority to 1 (#9323)
    Co-authored-by: Paulo Arromba <17236129+wavvves@users.noreply.github.com>

--- src/Templates/OrderReceivedTemplate.php
+++ src/Templates/OrderReceivedTemplate.php
@@ -22,16 +22,19 @@ class OrderReceivedTemplate {
 	 * Initialization method.
 	 */
 	protected function init() {
-		add_filter( 'page_template_hierarchy', array( $this, 'update_page_template_hierarchy' ), 10, 3 );
+		add_filter( 'page_template_hierarchy', array( $this, 'update_page_template_hierarchy' ), 1 );
 	}
 
 	/**
 	 * When it's the Order Received page and a block theme is active, render the Order Received Template.
 	 *
+	 * This places the template name e.g. `order-received`, at the beginning of the template hierarchy array. The hook priority
+	 * is 1 to ensure it runs first; other consumers e.g. extensions, could therefore inject their own template instead
+	 * of this one when using the default priority of 10.
+	 *
 	 * @param array $templates Templates that match the pages_template_hierarchy.
 	 */
 	public function update_page_template_hierarchy( $templates ) {
-
 		if ( is_wc_endpoint_url( 'order-received' ) && wc_current_theme_is_fse_theme() ) {
 			array_unshift( $templates, self::SLUG );
 		}
