1ac72c775d633aaaa843af059fcad13140d4c6f0
Mike Jolley <mike.jolley@me.com>
2023-05-03 15:36:10 +0100

    Migrate cart and checkout page content to the template editor

--- src/Templates/CartTemplate.php
+++ src/Templates/CartTemplate.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Automattic\WooCommerce\Blocks\Templates;
 
 /**
@@ -23,6 +22,7 @@ class CartTemplate {
 	 */
 	protected function init() {
 		add_filter( 'page_template_hierarchy', array( $this, 'update_page_template_hierarchy' ), 1 );
+		add_filter( 'woocommerce_blocks_template_content', array( $this, 'maybe_migrate_page_content_to_template' ), 10, 3 );
 	}
 
 	/**
@@ -38,8 +38,40 @@ class CartTemplate {
 		if ( is_cart() && wc_current_theme_is_fse_theme() ) {
 			array_unshift( $templates, self::SLUG );
 		}
-
 		return $templates;
 	}
 
+	/**
+	 * Migrates an existing page using blocks to the block templates.
+	 *
+	 * @param string $template_content The content of the template.
+	 * @param object $template_file The template file object.
+	 * @param string $template_type The type of template.
+	 * @return string
+	 */
+	public function maybe_migrate_page_content_to_template( $template_content, $template_file, $template_type ) {
+		if ( self::SLUG !== $template_file->slug ) {
+			return $template_content;
+		}
+
+		$page_id = wc_get_page_id( 'cart' );
+		$page    = $page_id ? get_post( $page_id ) : false;
+
+		if ( $page && ! empty( $page->post_content ) ) {
+			$template_content = '
+				<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+				<!-- wp:group {"layout":{"inherit":true}} -->
+				<div class="wp-block-group">
+					<!-- wp:heading {"level":1} -->
+					<h1 class="wp-block-heading">' . wp_kses_post( $page->post_title ) . '</h1>
+					<!-- /wp:heading -->
+					' . wp_kses_post( $page->post_content ) . '
+				</div>
+				<!-- /wp:group -->
+				<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
+			';
+		}
+
+		return $template_content;
+	}
 }
