c2fb3dab1ca6ce17266137ece61e804c7edaba25
Mike Jolley <mike.jolley@me.com>
2023-05-03 11:03:48 +0100

    Update type based on feedback

--- assets/js/data/utils/update-state.ts
+++ assets/js/data/utils/update-state.ts
@@ -1,16 +1,16 @@
 /**
  * Utility for updating nested state in the path that changed.
  */
-function updateNested< Type >( // The state being updated
-	state: Type,
+function updateNested< T >( // The state being updated
+	state: T,
 	// The path being updated
 	path: string[],
 	// The value to update for the path
 	value: unknown,
 	// The current index in the path
-	index: number
-): Type {
-	const key = path[ index ] as keyof Type;
+	index = 0
+): T {
+	const key = path[ index ] as keyof T;
 	if ( index === path.length - 1 ) {
 		return { ...state, [ key ]: value };
 	}
@@ -19,19 +19,19 @@ function updateNested< Type >( // The state being updated
 	return {
 		...state,
 		[ key ]: updateNested( nextState, path, value, index + 1 ),
-	} as Type;
+	} as T;
 }
 
 /**
  * Utility for updating state and only cloning objects in the path that changed.
  */
-export default function updateState< Type >(
+export default function updateState< T >(
 	// The state being updated
-	state: Type,
+	state: T,
 	// The path being updated
 	path: string[],
 	// The value to update for the path
 	value: unknown
-): Type {
-	return updateNested( state, path, value, 0 );
+): T {
+	return updateNested( state, path, value );
 }
