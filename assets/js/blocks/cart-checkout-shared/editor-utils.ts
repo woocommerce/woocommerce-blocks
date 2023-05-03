92c7c7c9380d06a7b6f3cd0e384545832223ebcb
Danae Millan <danae.millan@automattic.com>
2023-05-02 22:09:35 -0300

    Allow adding the custom fields section in the checkout inner blocks

--- assets/js/blocks/cart-checkout-shared/editor-utils.ts
+++ assets/js/blocks/cart-checkout-shared/editor-utils.ts
@@ -7,7 +7,12 @@ import { CART_STORE_KEY } from '@woocommerce/block-data';
 import { select } from '@wordpress/data';
 
 // List of core block types to allow in inner block areas.
-const coreBlockTypes = [ 'core/paragraph', 'core/image', 'core/separator' ];
+const coreBlockTypes = [
+	'core/paragraph',
+	'core/image',
+	'core/separator',
+	'woocommerce/checkout-custom-fields-block',
+];
 
 /**
  * Gets a list of allowed blocks types under a specific parent block type.
