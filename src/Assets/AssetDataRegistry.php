801ca134371fd22a17c5e129fe2249d005f95f2b
Luigi <gigitux@gmail.com>
2023-05-04 12:35:51 +0200

    try now

--- src/Assets/AssetDataRegistry.php
+++ src/Assets/AssetDataRegistry.php
@@ -66,11 +66,12 @@ class AssetDataRegistry {
 	 * Hook into WP asset registration for enqueueing asset data.
 	 */
 	protected function init() {
-		// is_login() was introduced in 6.1 so lets check it exists before executing.
-		if ( ! is_admin() && function_exists( 'is_login' ) && ! is_login() ) {
-			add_action( 'init', array( $this, 'register_data_script' ) );
-		} else {
+		// phpcs:ignore WordPress.Security.NonceVerification
+		if ( isset( $_GET['page'] ) && 'gutenberg-edit-site' === $_GET['page'] ) {
 			add_action( 'enqueue_block_editor_assets', array( $this, 'register_data_script' ) );
+		} else {
+			add_action( 'init', array( $this, 'register_data_script' ) );
+
 		}
 
 		add_action( 'wp_print_footer_scripts', array( $this, 'enqueue_asset_data' ), 2 );
