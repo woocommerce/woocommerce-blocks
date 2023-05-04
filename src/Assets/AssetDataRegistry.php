ee15cdb69b230efb5c9bc3ddec0c3b0a35f90cc6
Luigi <gigitux@gmail.com>
2023-05-04 13:15:28 +0200

    try now

--- src/Assets/AssetDataRegistry.php
+++ src/Assets/AssetDataRegistry.php
@@ -66,27 +66,10 @@ class AssetDataRegistry {
 	 * Hook into WP asset registration for enqueueing asset data.
 	 */
 	protected function init() {
-		if ( $this->is_site_editor() ) {
-			add_action( 'enqueue_block_editor_assets', array( $this, 'register_data_script' ) );
-		} else {
-			add_action( 'init', array( $this, 'register_data_script' ) );
-		}
+		add_action( 'enqueue_block_assets', array( $this, 'register_data_script' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'register_data_script' ) );
 		add_action( 'wp_print_footer_scripts', array( $this, 'enqueue_asset_data' ), 2 );
 		add_action( 'admin_print_footer_scripts', array( $this, 'enqueue_asset_data' ), 2 );
-	}
-
-	/**
-	 * Checks if the current URL is the Site Editor.
-	 *
-	 * @return boolean
-	 */
-	protected function is_site_editor() {
-		// phpcs:ignore WordPress.Security.NonceVerification
-		if ( isset( $_GET['page'] ) && 'gutenberg-edit-site' === $_GET['page'] ) {
-			return true;
-		}
-
-		return false;
 	}
 
 	/**
