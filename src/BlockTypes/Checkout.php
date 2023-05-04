b3df3848286d99be793b3b1d3ed074aa932c667e
hsingyuc <hsingyuc.7@gmail.com>
2023-05-03 19:05:03 -0700

    Render cart block on pay for order page

--- src/BlockTypes/Checkout.php
+++ src/BlockTypes/Checkout.php
@@ -172,7 +172,7 @@ class Checkout extends AbstractBlock {
 	 * @return boolean
 	 */
 	protected function is_checkout_endpoint() {
-		return is_wc_endpoint_url( 'order-pay' ) || is_wc_endpoint_url( 'order-received' );
+		return is_wc_endpoint_url( 'order-received' );
 	}
 
 	/**
