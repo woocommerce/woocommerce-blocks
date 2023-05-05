946ae6a2c3d758a2255d211595bd7fb6fc1c0286
Mike Jolley <mike.jolley@me.com>
2023-05-05 13:17:08 +0100

    Migrate Cart and Checkout Pages to the Template Editor when using a FSE theme (#9339)
    * Introduce woocommerce_blocks_template_content hook
    
    * Migrate cart and checkout page content to the template editor
    
    * Add redirection from edit page to edit template
    
    ---------
    
    Co-authored-by: Paulo Arromba <17236129+wavvves@users.noreply.github.com>

--- src/Templates/CheckoutTemplate.php
+++ src/Templates/CheckoutTemplate.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Automattic\WooCommerce\Blocks\Templates;
 
 /**
@@ -13,8 +12,14 @@ class CheckoutTemplate {
 
 	/**
 	 * Constructor.
+	 *
+	 * Templates require FSE theme support, so this will only init if a FSE theme is active.
 	 */
 	public function __construct() {
+		// Templates require FSE theme support.
+		if ( ! wc_current_theme_is_fse_theme() ) {
+			return;
+		}
 		$this->init();
 	}
 
@@ -23,6 +28,8 @@ class CheckoutTemplate {
 	 */
 	protected function init() {
 		add_filter( 'page_template_hierarchy', array( $this, 'update_page_template_hierarchy' ), 1 );
+		add_action( 'current_screen', array( $this, 'template_editor_redirect' ) );
+		add_filter( 'woocommerce_blocks_template_content', array( $this, 'default_template_content' ), 10, 3 );
 	}
 
 	/**
@@ -35,11 +42,58 @@ class CheckoutTemplate {
 	 * @param array $templates Templates that match the pages_template_hierarchy.
 	 */
 	public function update_page_template_hierarchy( $templates ) {
-		if ( is_checkout() && wc_current_theme_is_fse_theme() ) {
+		if ( is_checkout() ) {
 			array_unshift( $templates, self::SLUG );
 		}
-
 		return $templates;
 	}
 
+	/**
+	 * Redirect a page to the template editor if it's the checkout page and a block theme is active.
+	 *
+	 * @param \WP_Screen $current_screen Current screen information.
+	 */
+	public function template_editor_redirect( \WP_Screen $current_screen ) {
+		$page_id      = wc_get_page_id( 'checkout' );
+		$edit_page_id = 'page' === $current_screen->id && ! empty( $_GET['post'] ) ? absint( $_GET['post'] ) : 0; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+
+		if ( $edit_page_id === $page_id ) {
+			wp_safe_redirect( admin_url( 'site-editor.php?postType=wp_template&postId=woocommerce%2Fwoocommerce%2F%2Fcheckout' ) );
+			exit;
+		}
+	}
+
+	/**
+	 * Migrates an existing page using blocks to the block templates.
+	 *
+	 * @param string $template_content The content of the template.
+	 * @param object $template_file The template file object.
+	 * @param string $template_type The type of template.
+	 * @return string
+	 */
+	public function default_template_content( $template_content, $template_file, $template_type ) {
+		if ( self::SLUG !== $template_file->slug ) {
+			return $template_content;
+		}
+
+		$page_id = wc_get_page_id( 'checkout' );
+		$page    = $page_id ? get_post( $page_id ) : false;
+
+		if ( $page && ! empty( $page->post_content ) ) {
+			$template_content = '
+				<!-- wp:template-part {"slug":"header","tagName":"header"} /-->
+				<!-- wp:group {"layout":{"inherit":true}} -->
+				<div class="wp-block-group">
+					<!-- wp:heading {"level":1} -->
+					<h1 class="wp-block-heading">' . wp_kses_post( $page->post_title ) . '</h1>
+					<!-- /wp:heading -->
+					' . wp_kses_post( $page->post_content ) . '
+				</div>
+				<!-- /wp:group -->
+				<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->
+			';
+		}
+
+		return $template_content;
+	}
 }
