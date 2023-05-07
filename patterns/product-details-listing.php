bbcd24c3235293464c1b3df9dce79a48534922fc
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-05-08 00:24:20 +0200

    Add the skeleton for the Product Details: product listing pattern

---
+++ patterns/product-details-listing.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Product Details: product listing
+ * Slug: woocommerce-blocks/product-details-listing
+ * Categories: WooCommerce
+ */
+?>
+
+<!-- wp:woocommerce/single-product {"productId":32,"align":"center"} -->
+<div class="wp-block-woocommerce-single-product aligncenter">
+	<!-- wp:column {"style":{"max-width":"50%"}} -->
+	<div class="wp-block-column">
+		<!-- wp:woocommerce/product-image {"showProductLink":false,"showSaleBadge":false,"isDescendentOfSingleProductBlock":true} /-->
+
+		<!-- wp:woocommerce/product-rating {"isDescendentOfSingleProductBlock":true,"textColor":"luminous-vivid-amber","style":{"typography":{"fontSize":"22px"}}} /-->
+
+		<!-- wp:post-title {"isLink":true,"style":{"typography":{"fontSize":"48px"}},"__woocommerceNamespace":"woocommerce/product-query/product-title"} /-->
+
+		<!-- wp:woocommerce/product-price {"isDescendentOfSingleProductBlock":true,"style":{"typography":{"fontSize":"32px","fontStyle":"normal","fontWeight":"700"}}} /-->
+
+		<!-- wp:woocommerce/product-button {"productId":32,"align":"center"}/-->
+	</div>
+	<!-- /wp:column -->
+</div>
+<!-- /wp:woocommerce/single-product -->
