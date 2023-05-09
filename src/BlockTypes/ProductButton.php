2a6897fdd9624e3c04066d9868ef7804dc027afc
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-05-09 18:13:17 +0200

    Update get_block_type_uses_context and register_block_type_assets

--- src/BlockTypes/ProductButton.php
+++ src/BlockTypes/ProductButton.php
@@ -16,6 +16,20 @@ class ProductButton extends AbstractBlock {
 	protected $block_name = 'product-button';
 
 	/**
+	 * It is necessary to register and enqueue assets during the render phase because we want to load assets only if the block has the content.
+	 */
+	protected function register_block_type_assets() {
+		return null;
+	}
+
+	/**
+	 * Register the context.
+	 */
+	protected function get_block_type_uses_context() {
+		return [ 'query', 'queryId', 'postId' ];
+	}
+
+	/**
 	 * Include and render the block.
 	 *
 	 * @param array    $attributes Block attributes. Default empty array.
