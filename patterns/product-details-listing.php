69549c51ec0183ab915b4b588d901cc5d7fba2bd
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-05-10 20:14:54 +0200

    Update the styles and spacing for ratings.

--- patterns/product-details-listing.php
+++ patterns/product-details-listing.php
@@ -14,18 +14,18 @@
 			<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/product-details-product-listing.jpg', dirname( __FILE__ ) ) ); ?>" alt="" />
 		</figure>
 		<!-- /wp:image -->
-		<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px","margin":{"top":"40px","bottom":"40px"}}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center"}} -->
-		<div class="wp-block-group" style="margin-top:40px;margin-bottom:40px;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
-			<!-- wp:paragraph {"textColor":"luminous-vivid-amber"} -->
-			<p class="has-luminous-vivid-amber-color has-text-color">★★★★</p>
+		<!-- wp:group {"style":{"spacing":{"padding":{"top":"0px","right":"0px","bottom":"0px","left":"0px"},"blockGap":"0px","margin":{"top":"40px","bottom":"40px","left":"32%"}}},"layout":{"type":"flex","flexWrap":"nowrap","verticalAlignment":"center"}} -->
+		<div class="wp-block-group is-content-justification-center" style="margin-top:40px;margin-bottom:40px;margin-left:32%;padding-top:0px;padding-right:0px;padding-bottom:0px;padding-left:0px">
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"24px"},"textColor":"luminous-vivid-amber"} -->
+			<p class="has-luminous-vivid-amber-color has-text-color" style="font-size:24px">★★★★</p>
 			<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph {"style":{"color":{"text":"#ffe8a4"},"spacing":{"margin":{"right":"5px"}}}} -->
-			<p class="has-text-color" style="color:#ffe8a4;margin-right:5px">★</p>
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"24px"}},"color":{"text":"#ffe8a4"},"spacing":{"margin":{"right":"5px"}}}} -->
+			<p class="has-text-color" style="color:#ffe8a4;margin-right:5px;font-size:24px">★</p>
 			<!-- /wp:paragraph -->
 
-			<!-- wp:paragraph {"style":{"typography":{"fontSize":"0.7em"}},"textColor":"foreground"} -->
-			<p class="has-foreground-color has-text-color" style="font-size:0.7em">
+			<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}},"textColor":"foreground"} -->
+			<p class="has-foreground-color has-text-color" style="font-size:16px">
 				<strong>4.2</strong>(1,079 reviews)
 			</p>
 			<!-- /wp:paragraph -->
