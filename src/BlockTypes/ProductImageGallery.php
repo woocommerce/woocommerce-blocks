f13174346439bec2f97080385a9bbe0a5455be5f
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-05-13 11:54:43 +0200

    Have an adapted version of the native image gallery templates available within the ProductImageGallery class for rendering the Product Image block.

--- src/BlockTypes/ProductImageGallery.php
+++ src/BlockTypes/ProductImageGallery.php
@@ -29,11 +29,110 @@ class ProductImageGallery extends AbstractBlock {
 	}
 
 	/**
+	 * Based on the single-product/sale-flash.php template from the core of WooCommerce.
+	 *
+	 * @param \WC_Product $product Product instance.
+	 * @param \WP_Post    $post    Post instance.
+	 */
+	private function sale_flash( $product, $post ) {
+		if ( ! $product->is_on_sale() ) {
+			return '';
+		}
+
+		ob_start();
+		echo sprintf( '<span class="onsale">%1$s</span>', esc_html__( 'Sale!', 'woo-gutenberg-products-block' ) );
+		return ob_get_clean();
+	}
+
+	/**
+	 * Based on the single-product/product-image.php template from the core of WooCommerce.
+	 *
+	 * @param \WC_Product $product Product instance.
+	 * @param \WP_Post    $post    Post instance.
+	 *
+	 * @return string
+	 */
+	private function product_image( $product, $post ) {
+		/**
+		 * The woocommerce_product_thumbnails_columns filter.
+		 *
+		 * Allow users to modify the number of columns displayed in the product gallery.
+		 *
+		 * @since TBD
+		 */
+		$columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
+		$post_thumbnail_id = $product->get_image_id();
+		/**
+		 * The woocommerce_single_product_image_gallery_classes filter.
+		 *
+		 * Allow users to modify the class names for the product image gallery.
+		 *
+		 * @since TBD
+		 */
+		$wrapper_classes = apply_filters(
+			'woocommerce_single_product_image_gallery_classes',
+			array(
+				'woocommerce-product-gallery',
+				'woocommerce-product-gallery--' . ( $post_thumbnail_id ? 'with-images' : 'without-images' ),
+				'woocommerce-product-gallery--columns-' . absint( $columns ),
+				'images',
+			)
+		);
+
+		if ( $post_thumbnail_id ) {
+			$product_image_gallery_placeholder = wc_get_gallery_image_html( $post_thumbnail_id, true );
+		} else {
+			$product_image_gallery_placeholder = sprintf(
+				'<div class="woocommerce-product-gallery__image--placeholder"><img src="%1$s" alt="%2$s" class="wp-post-image" /></div></div>',
+				esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ),
+				esc_html__( 'Awaiting product image', 'woo-gutenberg-products-block' )
+			);
+		}
+		/**
+		 * The woocommerce_single_product_image_thumbnail_html filter.
+		 *
+		 * Allow users to modify the product gallery thumbnail html.
+		 *
+		 * @since TBD
+		 */
+		$product_image_gallery_placeholder = apply_filters( 'woocommerce_single_product_image_thumbnail_html', $product_image_gallery_placeholder, $post_thumbnail_id );
+
+		return sprintf(
+			'<div class="%1$s" data-columns="%2$s"><div class="woocommerce-product-gallery__wrapper">%3$s %4$s</div></div>',
+			esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ),
+			esc_attr( $columns ),
+			$product_image_gallery_placeholder,
+			$this->product_thumbnails( $product )
+		);
+	}
+
+	/**
+	 * Based on the single-product/product-thumbnails.php template from the core of WooCommerce.
+	 *
+	 * @param  \WC_Product $product Product instance.
+	 *
+	 * @return false|string
+	 */
+	private function product_thumbnails( $product ) {
+		$attachment_ids = $product->get_gallery_image_ids();
+		if ( ! $attachment_ids || ! $product->get_image_id() ) {
+			return '';
+		}
+
+		ob_start();
+		foreach ( $attachment_ids as $attachment_id ) {
+			echo wp_kses_post( wc_get_gallery_image_html( $attachment_id ) );
+		}
+		return ob_get_clean();
+	}
+
+	/**
 	 * Include and render the block.
 	 *
-	 * @param array    $attributes Block attributes. Default empty array.
-	 * @param string   $content    Block content. Default empty string.
-	 * @param WP_Block $block      Block instance.
+	 * @param array     $attributes Block attributes. Default empty array.
+	 * @param string    $content    Block content. Default empty string.
+	 * @param \WP_Block $block      Block instance.
+	 *
 	 * @return string Rendered block type output.
 	 */
 	protected function render( $attributes, $content, $block ) {
@@ -43,7 +142,11 @@ class ProductImageGallery extends AbstractBlock {
 			return '';
 		}
 
-		$single_post    = get_post( $post_id );
+		$single_post = get_post( $post_id );
+		if ( ! $single_post instanceof \WP_Post ) {
+			return '';
+		}
+
 		$single_product = wc_get_product( $post_id );
 		if ( ! $single_product instanceof \WC_Product ) {
 			return '';
@@ -55,63 +158,12 @@ class ProductImageGallery extends AbstractBlock {
 		}
 
 		$classname = $attributes['className'] ?? '';
-		ob_start();
-		/**
-		 * Based on the single-product/sale-flash.php template from the core of WooCommerce.
-		 */
-		if ( $single_product->is_on_sale() ) {
-			echo apply_filters( 'woocommerce_sale_flash', '<span class="onsale">' . esc_html__( 'Sale!', 'woocommerce' ) . '</span>', $single_post, $single_product );
-		}
-
-		$sale_badge_html = ob_get_clean();
-
-		ob_start();
-		/**
-		 * Based on the single-product/product-image.php template from the core of WooCommerce.
-		 */
-		$columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
-		$post_thumbnail_id = $single_product->get_image_id();
-		$wrapper_classes   = apply_filters(
-			'woocommerce_single_product_image_gallery_classes',
-			array(
-				'woocommerce-product-gallery',
-				'woocommerce-product-gallery--' . ( $post_thumbnail_id ? 'with-images' : 'without-images' ),
-				'woocommerce-product-gallery--columns-' . absint( $columns ),
-				'images',
-			)
-		);
-		?>
-		<div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
-			<div class="woocommerce-product-gallery__wrapper">
-				<?php
-				if ( $post_thumbnail_id ) {
-					$html = wc_get_gallery_image_html( $post_thumbnail_id, true );
-				} else {
-					$html  = '<div class="woocommerce-product-gallery__image--placeholder">';
-					$html .= sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
-					$html .= '</div>';
-				}
-
-				echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', $html, $post_thumbnail_id );
-
-				$attachment_ids = $single_product->get_gallery_image_ids();
-
-				if ( $attachment_ids && $single_product->get_image_id() ) {
-					foreach ( $attachment_ids as $attachment_id ) {
-						echo apply_filters( 'woocommerce_single_product_image_thumbnail_html', wc_get_gallery_image_html( $attachment_id ), $attachment_id );
-					}
-				}
-				?>
-			</div>
-		</div>
-		<?php
-		$product_image_gallery_html = ob_get_clean();
 
 		return sprintf(
 			'<div class="wp-block-woocommerce-product-image-gallery %1$s">%2$s %3$s</div>',
 			esc_attr( $classname ),
-			$sale_badge_html,
-			$product_image_gallery_html
+			$this->sale_flash( $single_product, $single_post ),
+			$this->product_image( $single_product, $single_post )
 		);
 
 	}
