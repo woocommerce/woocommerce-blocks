91dbd9429fa5b20e878ef2f9ce1f94fae50962fe
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-05-15 21:21:02 +0200

    Rely on static as the post reset only happens when the loop of inner blocks ends and not right away.

--- src/BlockTypes/SingleProduct.php
+++ src/BlockTypes/SingleProduct.php
@@ -125,7 +125,7 @@ class SingleProduct extends AbstractBlock {
 		if ( $this->single_product_inner_blocks_names ) {
 			$block_name = array_pop( $this->single_product_inner_blocks_names );
 
-			$global_post_variable_changed = false;
+			static $global_post_variable_changed;
 
 			if ( $block_name === $block['blockName'] ) {
 				/**
