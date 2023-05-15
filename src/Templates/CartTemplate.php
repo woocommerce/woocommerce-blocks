ec4905f27086ee1621b22ca89e17a9abc078d22e
Mike Jolley <mike.jolley@me.com>
2023-05-15 16:35:46 +0100

    Permalink solution for the checkout endpoint/template (#9406)
    * Checkout endpoint work
    
    * Move setting field to util
    
    * Include link to edit the template
    
    * Remove todo
    
    * Refactor checkout templates to share logic (#9411)
    
    * Sync endpoints with pages (#9426)
    
    * Switch to page syncing
    
    * Update settings descriptions
    
    ---------
    
    Co-authored-by: Paulo Arromba <17236129+wavvves@users.noreply.github.com>

--- src/Templates/CartTemplate.php
+++ src/Templates/CartTemplate.php
@@ -6,78 +6,52 @@ namespace Automattic\WooCommerce\Blocks\Templates;
  *
  * @internal
  */
-class CartTemplate {
-
-	const SLUG = 'cart';
-
+class CartTemplate extends AbstractPageTemplate {
 	/**
-	 * Constructor.
+	 * Template slug.
 	 *
-	 * Templates require FSE theme support, so this will only init if a FSE theme is active.
+	 * @return string
 	 */
-	public function __construct() {
-		// Templates require FSE theme support.
-		if ( ! wc_current_theme_is_fse_theme() ) {
-			return;
-		}
-		$this->init();
+	public static function get_slug() {
+		return 'cart';
 	}
 
 	/**
-	 * Initialization method.
+	 * Returns the page object assigned to this template/page.
+	 *
+	 * @return \WP_Post|null Post object or null.
 	 */
-	protected function init() {
-		add_filter( 'page_template_hierarchy', array( $this, 'update_page_template_hierarchy' ), 1 );
-		add_action( 'current_screen', array( $this, 'template_editor_redirect' ) );
-		add_filter( 'woocommerce_blocks_template_content', array( $this, 'default_template_content' ), 10, 3 );
+	public static function get_placeholder_page() {
+		$page_id = wc_get_page_id( 'cart' );
+		return $page_id ? get_post( $page_id ) : null;
 	}
 
 	/**
-	 * When the page is displaying the cart and a block theme is active, render the Cart Template.
+	 * True when viewing the cart page or cart endpoint.
 	 *
-	 * This places the template name e.g. `cart`, at the beginning of the template hierarchy array. The hook priority
-	 * is 1 to ensure it runs first; other consumers e.g. extensions, could therefore inject their own template instead
-	 * of this one when using the default priority of 10.
-	 *
-	 * @param array $templates Templates that match the pages_template_hierarchy.
+	 * @return boolean
 	 */
-	public function update_page_template_hierarchy( $templates ) {
-		if ( is_cart() ) {
-			array_unshift( $templates, self::SLUG );
-		}
-		return $templates;
+	protected function is_active_template() {
+		return is_cart();
 	}
 
 	/**
-	 * Redirect a page to the template editor if it's the checkout page and a block theme is active.
+	 * Should return the title of the page.
 	 *
-	 * @param \WP_Screen $current_screen Current screen information.
+	 * @return string
 	 */
-	public function template_editor_redirect( \WP_Screen $current_screen ) {
-		$page_id      = wc_get_page_id( 'cart' ) ?: false;
-		$edit_page_id = 'page' === $current_screen->id && ! empty( $_GET['post'] ) ? absint( $_GET['post'] ) : 0; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-
-		if ( $edit_page_id === $page_id ) {
-			wp_safe_redirect( admin_url( 'site-editor.php?postType=wp_template&postId=woocommerce%2Fwoocommerce%2F%2Fcart' ) );
-			exit;
-		}
+	public static function get_template_title() {
+		return __( 'Cart', 'woo-gutenberg-products-block' );
 	}
 
 	/**
 	 * Migrates an existing page using blocks to the block templates.
 	 *
 	 * @param string $template_content The content of the template.
-	 * @param object $template_file The template file object.
-	 * @param string $template_type The type of template.
 	 * @return string
 	 */
-	public function default_template_content( $template_content, $template_file, $template_type ) {
-		if ( self::SLUG !== $template_file->slug ) {
-			return $template_content;
-		}
-
-		$page_id = wc_get_page_id( 'cart' );
-		$page    = $page_id ? get_post( $page_id ) : false;
+	public function get_default_template_content( $template_content ) {
+		$page = $this->get_placeholder_page();
 
 		if ( $page && ! empty( $page->post_content ) ) {
 			$template_content = '
