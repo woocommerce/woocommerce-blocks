bdb13b88d1b1ad7b0c606ee46a1556987339a296
Mike Jolley <mike.jolley@me.com>
2023-05-15 16:35:46 +0100

    Permalink solution for the checkout endpoint/template (#9406)
    * Checkout endpoint work
    
    * Move setting field to util
    
    * Include link to edit the template
    
    * Remove todo
    
    * Refactor checkout templates to share logic (#9411)
    
    * Sync endpoints with pages (#9426)
    
    * Switch to page syncing
    
    * Update settings descriptions
    
    ---------
    
    Co-authored-by: Paulo Arromba <17236129+wavvves@users.noreply.github.com>

--- src/Templates/OrderReceivedTemplate.php
+++ src/Templates/OrderReceivedTemplate.php
@@ -6,43 +6,40 @@ namespace Automattic\WooCommerce\Blocks\Templates;
  *
  * @internal
  */
-class OrderReceivedTemplate {
-
-	const SLUG = 'order-received';
-
+class OrderReceivedTemplate extends AbstractPageTemplate {
 	/**
-	 * Constructor.
+	 * Template slug.
 	 *
-	 * Templates require FSE theme support, so this will only init if a FSE theme is active.
+	 * @return string
 	 */
-	public function __construct() {
-		// Templates require FSE theme support.
-		if ( ! wc_current_theme_is_fse_theme() ) {
-			return;
-		}
-		$this->init();
+	public static function get_slug() {
+		return 'order-received';
 	}
 
 	/**
-	 * Initialization method.
+	 * Returns the page object assigned to this template/page.
+	 *
+	 * @return \WP_Post|null Post object or null.
 	 */
-	protected function init() {
-		add_filter( 'page_template_hierarchy', array( $this, 'update_page_template_hierarchy' ), 1 );
+	public static function get_placeholder_page() {
+		return null;
 	}
 
 	/**
-	 * When it's the Order Received page and a block theme is active, render the Order Received Template.
+	 * True when viewing the cart page or cart endpoint.
 	 *
-	 * This places the template name e.g. `order-received`, at the beginning of the template hierarchy array. The hook priority
-	 * is 1 to ensure it runs first; other consumers e.g. extensions, could therefore inject their own template instead
-	 * of this one when using the default priority of 10.
-	 *
-	 * @param array $templates Templates that match the pages_template_hierarchy.
+	 * @return boolean
 	 */
-	public function update_page_template_hierarchy( $templates ) {
-		if ( is_wc_endpoint_url( 'order-received' ) ) {
-			array_unshift( $templates, self::SLUG );
-		}
-		return $templates;
+	protected function is_active_template() {
+		return is_wc_endpoint_url( 'order-received' );
+	}
+
+	/**
+	 * Should return the title of the page.
+	 *
+	 * @return string
+	 */
+	public static function get_template_title() {
+		return __( 'Order Received', 'woo-gutenberg-products-block' );
 	}
 }
