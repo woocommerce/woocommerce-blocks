3fab8b7693ed31226f8ead179896c7ce2601456d
Luigi <gigitux@gmail.com>
2023-05-17 15:22:12 +0200

    improve code readability

--- src/Templates/ArchiveProductTemplatesCompatibility.php
+++ src/Templates/ArchiveProductTemplatesCompatibility.php
@@ -73,26 +73,20 @@ class ArchiveProductTemplatesCompatibility extends AbstractTemplateCompatibility
 
 		$block_name = $block['blockName'];
 
-		if ( $this->is_products_block_with_inherit_query( $block ) ) {
-			$before_shop_loop_hooks = array(
-				'before' => array(
-					'woocommerce_before_shop_loop' => $this->hook_data['woocommerce_before_shop_loop'],
-				),
-				'after'  => array(),
-			);
-			$after_shop_loop_hooks  = array(
-				'after'  => array(
-					'woocommerce_after_shop_loop' => $this->hook_data['woocommerce_after_shop_loop'],
-				),
-				'before' => array(),
-			);
+		$block_hooks = array_filter(
+			$this->hook_data,
+			function( $hook ) use ( $block_name ) {
+				return $hook['block_name'] === $block_name;
+			}
+		);
 
+		if ( 'core/post-template' === $block_name ) {
 			$this->restore_default_hooks();
 			$content = sprintf(
 				'%1$s%2$s%3$s',
-				$this->get_hooks_buffer( $before_shop_loop_hooks['before'], 'before' ),
+				$this->get_hooks_buffer( $block_hooks, 'before' ),
 				$block_content,
-				$this->get_hooks_buffer( $after_shop_loop_hooks['after'], 'after' )
+				$this->get_hooks_buffer( $block_hooks, 'after' )
 			);
 			$this->remove_default_hooks();
 			return $content;
@@ -121,13 +115,6 @@ class ArchiveProductTemplatesCompatibility extends AbstractTemplateCompatibility
 		if ( ! in_array( $block_name, $supported_blocks, true ) ) {
 			return $block_content;
 		}
-
-		$block_hooks = array_filter(
-			$this->hook_data,
-			function( $hook ) use ( $block_name ) {
-				return $hook['block_name'] === $block_name;
-			}
-		);
 
 		if (
 			'core/query-no-results' === $block_name
@@ -247,7 +234,7 @@ class ArchiveProductTemplatesCompatibility extends AbstractTemplateCompatibility
 				),
 			),
 			'woocommerce_before_shop_loop'            => array(
-				'block_name'                  => '',
+				'block_name'                  => 'core/post-template',
 				'position'                    => 'before',
 				'hooked'                      => array(
 					'woocommerce_output_all_notices' => 10,
@@ -261,7 +248,7 @@ class ArchiveProductTemplatesCompatibility extends AbstractTemplateCompatibility
 				),
 			),
 			'woocommerce_after_shop_loop'             => array(
-				'block_name'                  => '',
+				'block_name'                  => 'core/post-template',
 				'position'                    => 'after',
 				'hooked'                      => array(
 					'woocommerce_pagination' => 10,
