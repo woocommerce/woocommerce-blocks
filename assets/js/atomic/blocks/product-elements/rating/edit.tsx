949df2b2c6a0c63981d7285032a9bad1e0e7a942
Alexandre Lara <allexandrelara@gmail.com>
2023-05-17 16:18:40 -0300

    Add Single Product template support to Product Rating block

--- assets/js/atomic/blocks/product-elements/rating/edit.tsx
+++ assets/js/atomic/blocks/product-elements/rating/edit.tsx
@@ -10,6 +10,7 @@ import {
 import type { BlockEditProps } from '@wordpress/blocks';
 import { useEffect } from '@wordpress/element';
 import { ProductQueryContext as Context } from '@woocommerce/blocks/product-query/types';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -21,11 +22,10 @@ import { BlockAttributes } from './types';
 import './editor.scss';
 import { useIsDescendentOfSingleProductBlock } from '../shared/use-is-descendent-of-single-product-block';
 
-const Edit = ( {
-	attributes,
-	setAttributes,
-	context,
-}: BlockEditProps< BlockAttributes > & { context: Context } ): JSX.Element => {
+const Edit = (
+	props: BlockEditProps< BlockAttributes > & { context: Context }
+): JSX.Element => {
+	const { attributes, setAttributes, context } = props;
 	const blockProps = useBlockProps( {
 		className: 'wp-block-woocommerce-product-rating',
 	} );
@@ -38,14 +38,33 @@ const Edit = ( {
 		useIsDescendentOfSingleProductBlock( {
 			blockClientId: blockProps?.id,
 		} );
+	const { isDescendentOfSingleProductTemplate } = useSelect(
+		( select ) => {
+			const store = select( 'core/edit-site' );
+			const postId = store?.getEditedPostId< string | undefined >();
+
+			return {
+				isDescendentOfSingleProductTemplate: Boolean(
+					postId?.includes( '//single-product' ) &&
+						! isDescendentOfQueryLoop &&
+						! isDescendentOfSingleProductBlock
+				),
+			};
+		},
+		[ isDescendentOfQueryLoop, isDescendentOfSingleProductBlock ]
+	);
 
 	useEffect( () => {
-		setAttributes( { isDescendentOfQueryLoop } );
-		setAttributes( { isDescendentOfSingleProductBlock } );
+		setAttributes( {
+			isDescendentOfQueryLoop,
+			isDescendentOfSingleProductBlock,
+			isDescendentOfSingleProductTemplate,
+		} );
 	}, [
 		setAttributes,
 		isDescendentOfQueryLoop,
 		isDescendentOfSingleProductBlock,
+		isDescendentOfSingleProductTemplate,
 	] );
 
 	return (
@@ -64,11 +83,26 @@ const Edit = ( {
 		</>
 	);
 };
-export default withProductSelector( {
-	icon: BLOCK_ICON,
-	label: BLOCK_TITLE,
-	description: __(
-		'Choose a product to display its rating.',
-		'woo-gutenberg-products-block'
-	),
-} )( Edit );
+
+/**
+ * The Edit block can be used in several contexts.
+ *
+ * If the `isDescendentOfSingleProductTemplate` attribute is true, it returns the `Edit` component with the `props` passed in.
+ * If it's false, it wraps the `Edit` component with the `withProductSelector` higher-order component, which provides a product selector UI for the user to choose a product to display its rating.
+ *
+ */
+export default (
+	props: BlockEditProps< BlockAttributes > & { context: Context }
+) => {
+	if ( props.attributes.isDescendentOfSingleProductTemplate ) {
+		return <Edit { ...props } />;
+	}
+	return withProductSelector( {
+		icon: BLOCK_ICON,
+		label: BLOCK_TITLE,
+		description: __(
+			'Choose a product to display its rating.',
+			'woo-gutenberg-products-block'
+		),
+	} )( Edit )( props );
+};
