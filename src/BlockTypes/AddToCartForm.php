d8734335394a9cb381fe07b8f5d2b1c0aff1996d
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-05-17 14:27:21 +0200

    Ditch the 'woocommerce_single_variation' action and invoke the variation-add-to-cart-button directly.

--- src/BlockTypes/AddToCartForm.php
+++ src/BlockTypes/AddToCartForm.php
@@ -89,16 +89,27 @@ class AddToCartForm extends AbstractBlock {
 					 * Hook: woocommerce_before_single_variation.
 					 */
 					do_action( 'woocommerce_before_single_variation' );
+					?>
+					<div class="woocommerce-variation single_variation">
+						<div class="woocommerce-variation-add-to-cart variations_button">
+							<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+							<?php
+							do_action( 'woocommerce_before_add_to_cart_quantity' );
 
-					/**
-					 * Hook: woocommerce_single_variation. Used to output the cart button and placeholder for variation data.
-					 *
-					 * @since 2.4.0
-					 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
-					 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
-					 */
-					do_action( 'woocommerce_single_variation' );
+							$this->get_quantity_input( $product );
 
+							do_action( 'woocommerce_after_add_to_cart_quantity' );
+							?>
+							<button type="submit" class="single_add_to_cart_button button alt<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+
+							<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
+
+							<input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
+							<input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
+							<input type="hidden" name="variation_id" class="variation_id" value="0" />
+						</div>
+					</div>
+					<?php
 					/**
 					 * Hook: woocommerce_after_single_variation.
 					 */
