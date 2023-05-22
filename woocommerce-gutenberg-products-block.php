a8fc3c3dd1d70f098d2fb184ddcd69abd44b231f
Mike Jolley <mike.jolley@me.com>
2023-05-22 12:54:08 +0100

    required versions

--- woocommerce-gutenberg-products-block.php
+++ woocommerce-gutenberg-products-block.php
@@ -9,8 +9,8 @@
  * Text Domain:  woo-gutenberg-products-block
  * Requires at least: 6.2
  * Requires PHP: 7.3
- * WC requires at least: 7.5
- * WC tested up to: 7.6
+ * WC requires at least: 7.6
+ * WC tested up to: 7.7
  *
  * @package WooCommerce\Blocks
  * @internal This file is only used when running as a feature plugin.
