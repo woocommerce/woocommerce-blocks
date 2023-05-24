a4b97ebedde1acc557c2859dbbbb875f7c7ab06b
Luigi <gigitux@gmail.com>
2023-05-24 11:01:02 +0200

    fix command

--- tests/e2e-pw/bin/test-env-setup.sh
+++ tests/e2e-pw/bin/test-env-setup.sh
@@ -11,7 +11,7 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ###################################################################################################
 
 wp-env run tests-cli "wp site empty --yes"
-wp-env run tests-cli "wp plugin active woocommerce-blocks"
+wp-env run tests-cli "wp plugin activate woocommerce-blocks"
 
 ###################################################################################################
 # If no attributes exist, otherwiese create them
