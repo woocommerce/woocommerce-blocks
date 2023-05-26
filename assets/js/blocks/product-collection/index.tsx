3b1b6c6dd685f4adb438fc20b9e3095aea5fd07e
Manish Menaria <the.manish.menaria@gmail.com>
2023-05-26 17:56:59 +0530

    remove unused import of getDefaultStockStatuses

--- assets/js/blocks/product-collection/index.tsx
+++ assets/js/blocks/product-collection/index.tsx
@@ -12,7 +12,6 @@ import edit from './edit';
 import save from './save';
 import icon from './icon';
 import './variations';
-import { getDefaultStockStatuses } from './inspector-controls/constants';
 
 if ( isExperimentalBuild() ) {
 	registerBlockType( metadata, {
