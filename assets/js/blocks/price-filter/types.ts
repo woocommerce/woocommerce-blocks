c5aa13acc36fa946227261d81d63758f1e93a9a6
Hritik Chaudhary <33057454+hritikchaudhary@users.noreply.github.com>
2023-05-27 16:50:09 +0530

    removed proptypes from price filters

--- assets/js/blocks/price-filter/types.ts
+++ assets/js/blocks/price-filter/types.ts
@@ -1,8 +1,12 @@
-export interface Attributes {
+interface Attributes {
 	showFilterButton: boolean;
 	showInputFields: boolean;
 	inlineInput: boolean;
 	heading: string;
 	headingLevel: number;
 	className?: string;
+}
+export interface PriceFilterBlockProps {
+	attributes: Attributes;
+	isEditor: boolean;
 }
