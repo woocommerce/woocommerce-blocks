2587cc44f08f42fa9880026e5d063d0653a21b88
Thomas Roberts <thomas.roberts@automattic.com>
2023-05-31 15:07:17 +0100

    Add tests for mini cart onProceedToCheckout emitter

---
+++ assets/js/blocks/mini-cart/mini-cart-contents/inner-blocks/mini-cart-checkout-button-block/test/block.tsx
@@ -0,0 +1,44 @@
+/**
+ * External dependencies
+ */
+import {
+	CartEventsProvider,
+	useCartEventsContext,
+} from '@woocommerce/base-context';
+import { useEffect } from '@wordpress/element';
+import { render, screen, waitFor } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import Block from '../block';
+describe( 'Mini Cart Checkout Button Block', () => {
+	it( 'dispatches the onProceedToCheckout event when the button is clicked', async () => {
+		const mockObserver = jest.fn();
+		const MockObserverComponent = () => {
+			const { onProceedToCheckout } = useCartEventsContext();
+			useEffect( () => {
+				return onProceedToCheckout( mockObserver );
+			}, [ onProceedToCheckout ] );
+			return <div>Mock observer</div>;
+		};
+
+		render(
+			<CartEventsProvider>
+				<div>
+					<MockObserverComponent />
+					<Block
+						checkoutButtonLabel={ 'Proceed to Checkout' }
+						className="test-block"
+					/>
+				</div>
+			</CartEventsProvider>
+		);
+		expect( screen.getByText( 'Mock observer' ) ).toBeInTheDocument();
+		const button = screen.getByText( 'Proceed to Checkout' );
+		button.click();
+		await waitFor( () => {
+			expect( mockObserver ).toHaveBeenCalled();
+		} );
+	} );
+} );
