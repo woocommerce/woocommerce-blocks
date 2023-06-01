79bcf9ba19dd9f89bfb349e56e186658370e4c06
Thomas Roberts <thomas.roberts@automattic.com>
2023-06-01 17:13:19 +0100

    Add selector to get highlighted notices

--- assets/js/data/store-notices/selectors.ts
+++ assets/js/data/store-notices/selectors.ts
@@ -6,3 +6,7 @@ import { StoreNoticesState } from './default-state';
 export const getRegisteredContainers = (
 	state: StoreNoticesState
 ): StoreNoticesState[ 'containers' ] => state.containers;
+
+export const getHighlightedNotices = ( state: StoreNoticesState ) => {
+	return state.highlightedNotices;
+};
