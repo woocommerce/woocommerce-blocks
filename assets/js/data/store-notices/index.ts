342190abec8695681f16f1eecdbd68f8d7ab8874
Thomas Roberts <thomas.roberts@automattic.com>
2023-06-01 17:11:51 +0100

    Add thunks to store's actions

--- assets/js/data/store-notices/index.ts
+++ assets/js/data/store-notices/index.ts
@@ -6,6 +6,7 @@ import { createReduxStore, register } from '@wordpress/data';
 /**
  * Internal dependencies
  */
+import * as thunks from './thunks';
 import * as actions from './actions';
 import * as selectors from './selectors';
 import reducer from './reducers';
@@ -14,7 +15,7 @@ import { DispatchFromMap, SelectFromMap } from '../mapped-types';
 const STORE_KEY = 'wc/store/store-notices';
 const config = {
 	reducer,
-	actions,
+	actions: { ...actions, ...thunks },
 	selectors,
 };
 const store = createReduxStore( STORE_KEY, config );
@@ -25,7 +26,7 @@ export const STORE_NOTICES_STORE_KEY = STORE_KEY;
 declare module '@wordpress/data' {
 	function dispatch(
 		key: typeof STORE_KEY
-	): DispatchFromMap< typeof actions >;
+	): DispatchFromMap< typeof actions > & DispatchFromMap< typeof thunks >;
 	function select( key: typeof STORE_KEY ): SelectFromMap<
 		typeof selectors
 	> & {
