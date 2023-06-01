0a2f1ece2b17ff03d506958543cd5727dbf5eb86
Thomas Roberts <thomas.roberts@automattic.com>
2023-06-01 17:32:13 +0100

    Get isHighlighted from data store and set it on NoticeBanner

--- packages/checkout/components/store-notice/index.tsx
+++ packages/checkout/components/store-notice/index.tsx
@@ -5,6 +5,9 @@ import classnames from 'classnames';
 import NoticeBanner, {
 	NoticeBannerProps,
 } from '@woocommerce/base-components/notice-banner';
+import { useSelect } from '@wordpress/data';
+import { STORE_NOTICES_STORE_KEY } from '@woocommerce/block-data';
+import { NoticeType } from '@woocommerce/types';
 
 /**
  * Wrapper for NoticeBanner component.
@@ -13,12 +16,23 @@ const StoreNotice = ( {
 	className,
 	children,
 	status,
+	context,
+	id,
 	...props
-}: NoticeBannerProps ) => {
+}: NoticeBannerProps & NoticeType ) => {
+	const highlightedNotices = useSelect( ( select ) => {
+		return select( STORE_NOTICES_STORE_KEY ).getHighlightedNotices();
+	} );
+
+	const isHighlighted = highlightedNotices.some(
+		( notice ) => notice.id === id && notice.context === context
+	);
+
 	return (
 		<NoticeBanner
 			className={ classnames( 'wc-block-store-notice', className ) }
 			status={ status }
+			isHighlighted={ isHighlighted }
 			{ ...props }
 		>
 			{ children }
