19d6f86655a30566c073c62950d93ecda981bd39
Brian <brian@brianhaas.li>
2023-06-13 21:27:05 +0200

    Update footer-simple.php

--- patterns/footer-simple.php
+++ patterns/footer-simple.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Title: WooCommerce Simple Footer
+ * Title: WooCommerce Blocks - Simple Footer
  * Slug: woocommerce-blocks/footer-simple
  * Categories: WooCommerce
  * Block Types: core/template-part/footer
