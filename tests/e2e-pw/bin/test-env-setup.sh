f16ff4da4a704ba2d15e220d44c0f89c0d485d61
Luigi <gigitux@gmail.com>
2023-06-13 17:17:50 +0200

    try now

--- tests/e2e-pw/bin/test-env-setup.sh
+++ tests/e2e-pw/bin/test-env-setup.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-wp-env run tests-cli "wp db reset --yes"
-wp-env run tests-cli "wp db import woocommerce.sql"
+wp-env run tests-cli wp db reset --yes
+wp-env run tests-cli wp db import woocommerce.sql
+wp-env run tests-cli wp wc tool run regenerate_product_lookup_tables --user=1
+wp-env run tests-wordpress chmod -c ugo+w /var/www/html
+wp-env run tests-cli wp rewrite structure /%postname%/ --hard
+wp-env run tests-cli wp rewrite flush --hard
+wp-env run tests-cli wp option update blogname 'WooCommerce Blocks E2E Test Suite'
