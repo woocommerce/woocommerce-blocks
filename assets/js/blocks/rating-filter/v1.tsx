8c13f739a3a4cc14439dd89c7515205dc5d16f47
roykho <roykho77@gmail.com>
2023-06-14 10:10:35 -0700

    Default show product counts to false for filter by rating block

--- assets/js/blocks/rating-filter/v1.tsx
+++ assets/js/blocks/rating-filter/v1.tsx
@@ -1,35 +1,24 @@
 /**
  * External dependencies
  */
-import { registerBlockType } from '@wordpress/blocks';
-import { Icon, starEmpty } from '@wordpress/icons';
 import classNames from 'classnames';
 import { useBlockProps } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
  */
-import edit from './edit';
 import metadata from './block.json';
 import type { Attributes } from './types';
-import v1 from './v1';
 
-registerBlockType( metadata, {
-	deprecated: [ v1 ],
-	icon: {
-		src: (
-			<Icon
-				icon={ starEmpty }
-				className="wc-block-editor-components-block-icon"
-			/>
-		),
-	},
+const v1 = {
 	attributes: {
 		...metadata.attributes,
+		showCounts: {
+			type: 'boolean',
+			default: true,
+		},
 	},
-	edit,
-	// Save the props to post content.
-	save( { attributes }: { attributes: Attributes } ) {
+	save: ( { attributes }: { attributes: Attributes } ) => {
 		const { className, showCounts } = attributes;
 		const data: Record< string, unknown > = {
 			'data-show-counts': showCounts,
@@ -48,4 +37,6 @@ registerBlockType( metadata, {
 			</div>
 		);
 	},
-} );
+};
+
+export default v1;
