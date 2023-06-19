243f16cc02f6264ef34210abb38e120564e215f2
Karol Manijak <karol.manijak@automattic.com>
2023-06-19 13:37:31 +0200

    Fix block theme setup command

--- tests/e2e-pw/block-theme.setup.ts
+++ tests/e2e-pw/block-theme.setup.ts
@@ -3,4 +3,4 @@
  */
 import { BLOCK_THEME_SLUG, cli } from '@woocommerce/e2e-utils';
 
-cli( `npm run wp-env run tests-cli "wp theme activate ${ BLOCK_THEME_SLUG }` );
+cli( `npm run wp-env run tests-cli wp theme activate ${ BLOCK_THEME_SLUG }` );
