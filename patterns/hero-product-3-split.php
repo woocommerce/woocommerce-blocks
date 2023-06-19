3c40b8056d1fe5b7593280451eb9499dc214c7c3
Alba Rincón <alba.rincon@automattic.com>
2023-06-19 14:08:17 +0200

    Add padding top & bottom to the text, add button link to the shop

--- patterns/hero-product-3-split.php
+++ patterns/hero-product-3-split.php
@@ -13,8 +13,8 @@
 		<!-- wp:media-text {"mediaPosition":"right","mediaId":3800,"mediaLink":"https://store.local/p/image/","mediaType":"image","style":{"color":{"background":"#000000","text":"#ffffff"}}} -->
 		<div class="wp-block-media-text alignwide has-media-on-the-right is-stacked-on-mobile has-text-color has-background" style="color:#ffffff;background-color:#000000">
 			<div class="wp-block-media-text__content">
-				<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","left":"20px","right":"20px"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
-				<div class="wp-block-group" style="padding-top:0;padding-right:20px;padding-left:20px">
+				<!-- wp:group {"style":{"spacing":{"padding":{"top":"35px","left":"20px","right":"20px","bottom":"35px"}}},"layout":{"type":"flex","orientation":"vertical"}} -->
+				<div class="wp-block-group" style="padding-top:35px;padding-right:20px;padding-left:20px;padding-bottom:35px;">
 					<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"large"} -->
 					<h2 class="wp-block-heading has-large-font-size" style="font-style:normal;font-weight:700">Endless Tee's</h2>
 					<!-- /wp:heading -->
@@ -27,7 +27,7 @@
 					<div class="wp-block-buttons">
 						<!-- wp:button {"style":{"color":{"background":"#ffffff","text":"#000000"}}} -->
 						<div class="wp-block-button">
-							<a class="wp-block-button__link has-text-color has-background wp-element-button" style="color:#000000;background-color:#ffffff">Shop now</a>
+							<a href="<?php echo esc_url( get_permalink( wc_get_page_id( 'shop' ) ) ); ?>" class="wp-block-button__link has-text-color has-background wp-element-button" style="color:#000000;background-color:#ffffff">Shop now</a>
 						</div>
 						<!-- /wp:button -->
 					</div>
