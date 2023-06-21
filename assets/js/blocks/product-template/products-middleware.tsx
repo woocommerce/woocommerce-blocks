d4414da02a7b9a3325911bf19fb984fd5870bd37
Karol Manijak <karol.manijak@automattic.com>
2023-06-21 10:14:58 +0200

    Add data tarnsformation to product data middleware

--- assets/js/blocks/product-template/products-middleware.tsx
+++ assets/js/blocks/product-template/products-middleware.tsx
@@ -3,15 +3,31 @@
  */
 import apiFetch from '@wordpress/api-fetch';
 
+interface ProductsStoreAPIData {
+	id: number;
+}
+interface ProductsOutputData {
+	id: number;
+}
+
+const transformProductData = (
+	input: ProductsStoreAPIData
+): ProductsOutputData => {
+	return input;
+};
+
 export const productApiFetchMiddleware = () => {
-	apiFetch.use( ( options, next ) => {
+	apiFetch.use( async ( options, next ) => {
 		const regex = /^\/wp\/v2\/product.*isProductCollectionBlock=true/;
 
 		if ( options.path && regex.test( options?.path ) ) {
 			const from = '/wp/v2/product';
 			const to = '/wc/store/v1/products';
 			const amendedPath = options.path.replace( from, to );
-			return next( { ...options, path: amendedPath } );
+			const response = await next( { ...options, path: amendedPath } );
+			const transformedData = transformProductData( response );
+
+			return transformedData;
 		}
 
 		return next( options );
