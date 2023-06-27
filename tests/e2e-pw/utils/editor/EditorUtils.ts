1ae992649e6010587c3e55764c2ad5861cd053dc
Mike Jolley <mike.jolley@me.com>
2023-06-27 10:43:24 +0100

    Remove revertTemplate

--- tests/e2e-pw/utils/editor/EditorUtils.ts
+++ tests/e2e-pw/utils/editor/EditorUtils.ts
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { Editor } from '@wordpress/e2e-test-utils-playwright';
-import { Page } from '@playwright/test';
 
 export class EditorUtils {
 	editor: Editor;
@@ -12,26 +11,5 @@ export class EditorUtils {
 
 	async getBlockByName( name: string ) {
 		return this.editor.canvas.locator( `[data-type="${ name }"]` );
-	}
-
-	async revertTemplate( page: Page ) {
-		await this.editor.openDocumentSettingsSidebar();
-		const isTemplateTabVisible = await page
-			.locator(
-				'role=region[name="Editor settings"i] >> role=button[name="Template"i]'
-			)
-			.isVisible();
-		if ( isTemplateTabVisible ) {
-			await page.click(
-				'role=region[name="Editor settings"i] >> role=button[name="Template"i]'
-			);
-		}
-		await page.click(
-			'role=region[name="Editor settings"i] >> role=button[name="Actions"i]'
-		);
-		await page.click( 'role=menuitem[name=/Clear customizations/i]' );
-		await page.waitForSelector(
-			'role=button[name="Dismiss this notice"i] >> text="Template reverted."'
-		);
 	}
 }
