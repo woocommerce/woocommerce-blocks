5897a1436e76d104d2d56eaee1998177166d8a88
roykho <roykho77@gmail.com>
2023-06-28 05:48:54 -0700

    Remove unneeded classnames

--- assets/js/blocks/rating-filter/preview.tsx
+++ assets/js/blocks/rating-filter/preview.tsx
@@ -6,58 +6,23 @@ import Rating from '@woocommerce/base-components/product-rating';
 
 export const previewOptions = [
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 5 }
-				rating={ 5 }
-				ratedProductsCount={ null }
-			/>
-		),
+		label: <Rating key={ 5 } rating={ 5 } ratedProductsCount={ null } />,
 		value: '5',
 	},
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 4 }
-				rating={ 4 }
-				ratedProductsCount={ null }
-			/>
-		),
+		label: <Rating key={ 4 } rating={ 4 } ratedProductsCount={ null } />,
 		value: '4',
 	},
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 3 }
-				rating={ 3 }
-				ratedProductsCount={ null }
-			/>
-		),
+		label: <Rating key={ 3 } rating={ 3 } ratedProductsCount={ null } />,
 		value: '3',
 	},
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 2 }
-				rating={ 2 }
-				ratedProductsCount={ null }
-			/>
-		),
+		label: <Rating key={ 2 } rating={ 2 } ratedProductsCount={ null } />,
 		value: '2',
 	},
 	{
-		label: (
-			<Rating
-				className={ '' }
-				key={ 1 }
-				rating={ 1 }
-				ratedProductsCount={ null }
-			/>
-		),
+		label: <Rating key={ 1 } rating={ 1 } ratedProductsCount={ null } />,
 		value: '1',
 	},
 ];
