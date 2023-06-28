c596d9e25d0138d4abde4530f60c6ea8eda1f5a3
Manish Menaria <the.manish.menaria@gmail.com>
2023-06-28 18:19:59 +0530

    Refactor wp-env command usage across scripts
    The changes primarily involve:
    1. Removed escaping quotes when reading the post contents from text files. The script now directly reads the file content without using `sed` to escape double quotes.
    2. Changed the format of `wp-env run` command, moving `--` before `wp`.

--- tests/e2e-pw/classic-theme.setup.ts
+++ tests/e2e-pw/classic-theme.setup.ts
@@ -3,4 +3,6 @@
  */
 import { CLASSIC_THEME_SLUG, cli } from '@woocommerce/e2e-utils';
 
-cli( `npm run wp-env run tests-cli wp theme activate ${ CLASSIC_THEME_SLUG }` );
+cli(
+	`npm run wp-env run tests-cli -- wp theme activate ${ CLASSIC_THEME_SLUG }`
+);
