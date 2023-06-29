a6997e145b40fb4b42f9dfc13f1c204259883ec7
Hritik Chaudhary <33057454+hritikchaudhary@users.noreply.github.com>
2023-06-29 17:56:06 +0530

    Remove data attributes from being saved in html of All products block

--- assets/js/blocks/products/all-products/save.js
+++ assets/js/blocks/products/all-products/save.js
@@ -15,16 +15,12 @@ export default function save( { attributes } ) {
 		.forEach( ( key ) => {
 			dataAttributes[ key ] = attributes[ key ];
 		} );
-	const data = {
-		'data-attributes': JSON.stringify( dataAttributes ),
-	};
 	return (
 		<div
 			className={ getBlockClassName(
 				'wc-block-all-products',
 				attributes
 			) }
-			{ ...data }
 		>
 			<InnerBlocks.Content />
 		</div>
