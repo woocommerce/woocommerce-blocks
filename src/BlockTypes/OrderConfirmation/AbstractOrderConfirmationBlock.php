e113886aa7c38d1a328b720e205a0ea9a8a244e6
Mike Jolley <mike.jolley@me.com>
2023-06-29 16:25:52 +0100

    Initial blocks

--- src/BlockTypes/OrderConfirmation/AbstractOrderConfirmationBlock.php
+++ src/BlockTypes/OrderConfirmation/AbstractOrderConfirmationBlock.php
@@ -11,21 +11,40 @@ abstract class AbstractOrderConfirmationBlock extends AbstractBlock {
 	/**
 	 * Get the content from a hook and return it.
 	 *
-	 * @param string    $hook Hook name.
+	 * @param string $hook Hook name.
+	 * @param array  $args Array of args to pass to the hook.
+	 * @return string
+	 */
+	protected function get_hook_content( $hook, $args ) {
+		ob_start();
+		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+		do_action_ref_array( $hook, $args );
+		return ob_get_clean();
+	}
+
+	/**
+	 * This renders the content of the block within the wrapper.
+	 *
 	 * @param \WC_Order $order Order object.
 	 * @return string
 	 */
-	protected function get_hook_content( $hook, $order ) {
-		ob_start();
-		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
-		do_action( $hook, $order );
-		return ob_get_clean();
+	abstract protected function render_content( $order );
+
+	/**
+	 * This is what gets rendered when the order does not exist.
+	 *
+	 * @return string
+	 */
+	protected function render_content_fallback() {
+		return '';
 	}
 
 	/**
 	 * Get current order.
 	 *
-	 * @return WC_Order|null
+	 * @todo Check auth.
+	 *
+	 * @return \WC_Order|null
 	 */
 	protected function get_order() {
 		$order_id = absint( get_query_var( 'order-received' ) );
@@ -35,6 +54,29 @@ abstract class AbstractOrderConfirmationBlock extends AbstractBlock {
 		}
 
 		return null;
+	}
+
+	/**
+	 * See if the current user has access to sensitive order details.
+	 *
+	 * @param \WC_Order|null $order Order object.
+	 * @return boolean
+	 */
+	protected function is_current_customer_order( $order ) {
+		if ( ! $order ) {
+			return false;
+		}
+
+		if ( ! is_user_logged_in() || $order->get_user_id() !== get_current_user_id() ) {
+			return false;
+		}
+
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		if ( empty( $_GET['key'] ) || ! $order->key_is_valid( wc_clean( wp_unslash( $_GET['key'] ) ) ) ) {
+			return false;
+		}
+
+		return true;
 	}
 
 	/**
--- src/BlockTypesController.php
+++ src/BlockTypesController.php
@@ -217,6 +217,7 @@ final class BlockTypesController {
 			'ProductDetails',
 			'SingleProduct',
 			'StockFilter',
+			'OrderConfirmation\Status',
 			'OrderConfirmation\Summary',
 			'OrderConfirmation\Details',
 			'OrderConfirmation\BillingAddress',
