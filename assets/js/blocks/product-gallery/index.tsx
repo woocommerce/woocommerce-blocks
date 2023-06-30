b4762b7dde097a83df858b777afe7a2796de184a
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-06-30 13:47:47 +0200

    Product Gallery: Move the block from registerBlockType to registerBlockSingleProductTemplate

--- assets/js/blocks/product-gallery/index.tsx
+++ assets/js/blocks/product-gallery/index.tsx
@@ -1,18 +1,27 @@
 /**
  * External dependencies
  */
-import { registerBlockType } from '@wordpress/blocks';
+import { gallery as icon } from '@wordpress/icons';
+import { registerBlockSingleProductTemplate } from '@woocommerce/atomic-utils';
 import { isExperimentalBuild } from '@woocommerce/block-settings';
 
 /**
  * Internal dependencies
  */
+import { Edit, Save } from './edit';
 import metadata from './block.json';
-import icon from './icon';
 
 if ( isExperimentalBuild() ) {
-	// @ts-expect-error: `registerBlockType` is a function that is typed in WordPress core.
-	registerBlockType( metadata, {
-		icon,
+	registerBlockSingleProductTemplate( {
+		blockName: metadata.name,
+		// @ts-expect-error: `metadata` currently does not have a type definition in WordPress core
+		blockMetadata: metadata,
+		blockSettings: {
+			icon,
+			// @ts-expect-error `edit` can be extended to include other attributes
+			edit: Edit,
+			save: Save,
+			ancestor: [ 'woocommerce/single-product' ],
+		},
 	} );
 }
