3d393e62d33cc807bddac9165bf5a9685707f7b7
Mike Jolley <mike.jolley@me.com>
2023-06-30 16:06:20 +0100

    Update summary icon

--- assets/js/blocks/order-confirmation/summary/index.tsx
+++ assets/js/blocks/order-confirmation/summary/index.tsx
@@ -2,8 +2,7 @@
  * External dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-import { Icon } from '@wordpress/icons';
-import { totals } from '@woocommerce/icons';
+import { Icon, receipt } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -15,7 +14,7 @@ registerBlockType( metadata, {
 	icon: {
 		src: (
 			<Icon
-				icon={ totals }
+				icon={ receipt }
 				className="wc-block-editor-components-block-icon"
 			/>
 		),
