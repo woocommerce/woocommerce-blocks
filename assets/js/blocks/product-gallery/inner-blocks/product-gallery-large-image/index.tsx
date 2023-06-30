55377ca211851376bbbdd11de65d4d42ab9e06db
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-06-30 14:22:18 +0200

    Product Gallery Large Image: Add experimental mode check

--- assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image/index.tsx
+++ assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image/index.tsx
@@ -4,26 +4,29 @@
 import { Icon, image } from '@wordpress/icons';
 import { registerBlockType } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
+import { isExperimentalBuild } from '@woocommerce/block-settings';
 
 /**
  * Internal dependencies
  */
 import { Edit, Save } from './edit';
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore -- TypeScript expects some required properties which we already
-// registered in PHP.
-registerBlockType( 'woocommerce/product-gallery-large-image', {
-	title: __( 'Large Image', 'woo-gutenberg-products-block' ),
-	icon: {
-		src: (
-			<Icon
-				icon={ image }
-				className="wc-block-editor-components-block-icon"
-			/>
-		),
-	},
-	edit: Edit,
-	save: Save,
-	ancestor: [ 'woocommerce/product-gallery' ],
-} );
+if ( isExperimentalBuild() ) {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore -- TypeScript expects some required properties which we already
+	// registered in PHP.
+	registerBlockType( 'woocommerce/product-gallery-large-image', {
+		title: __( 'Large Image', 'woo-gutenberg-products-block' ),
+		icon: {
+			src: (
+				<Icon
+					icon={ image }
+					className="wc-block-editor-components-block-icon"
+				/>
+			),
+		},
+		edit: Edit,
+		save: Save,
+		ancestor: [ 'woocommerce/product-gallery' ],
+	} );
+}
