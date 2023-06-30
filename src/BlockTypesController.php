ce171a417dcac2af5154391f3164a6ce76143f2d
Mike Jolley <mike.jolley@me.com>
2023-06-30 14:49:07 +0100

    Make blocks experimental

--- src/BlockTypesController.php
+++ src/BlockTypesController.php
@@ -219,11 +219,6 @@ final class BlockTypesController {
 			'ProductDetails',
 			'SingleProduct',
 			'StockFilter',
-			'OrderConfirmation\Status',
-			'OrderConfirmation\Summary',
-			'OrderConfirmation\Details',
-			'OrderConfirmation\BillingAddress',
-			'OrderConfirmation\ShippingAddress',
 		];
 
 		$block_types = array_merge(
@@ -239,6 +234,11 @@ final class BlockTypesController {
 			$block_types[] = 'ProductGalleryLargeImageNextPrevious';
 			$block_types[] = 'ProductGalleryPager';
 			$block_types[] = 'ProductGalleryThumbnails';
+			$block_types[] = 'OrderConfirmation\Status';
+			$block_types[] = 'OrderConfirmation\Summary';
+			$block_types[] = 'OrderConfirmation\Details';
+			$block_types[] = 'OrderConfirmation\BillingAddress';
+			$block_types[] = 'OrderConfirmation\ShippingAddress';
 		}
 
 		/**
