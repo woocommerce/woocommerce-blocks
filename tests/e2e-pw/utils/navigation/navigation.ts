21cad9b08af148fe97f2e0de7a578a171af5a872
Thomas Roberts <thomas.roberts@automattic.com>
2023-07-03 11:31:22 +0100

    Update selector to use locator instead

--- tests/e2e-pw/utils/navigation/navigation.ts
+++ tests/e2e-pw/utils/navigation/navigation.ts
@@ -1,21 +1,17 @@
 /**
  * External dependencies
  */
-import { PlaywrightTestArgs } from '@playwright/test';
+import { Page, PlaywrightTestArgs } from '@playwright/test';
 import { BlockData } from '@woocommerce/e2e-types';
 
 /**
  * Closes any modals in the editor if they are open.
  */
-export const closeModalIfExists = async (
-	page: PlaywrightTestArgs[ 'page' ]
-) => {
+export const closeModalIfExists = async ( page: Page ) => {
 	// The modal close button can have different aria-labels, depending on the version of Gutenberg/WP.
 	// Newer versions (WP >=6.2) use `Close`, while older versions (WP <6.1) use `Close dialog`.
-	const closeButton = await page.$(
-		'.components-modal__header [aria-label="Close"], .components-modal__header [aria-label="Close dialog"]'
-	);
-	if ( closeButton ) {
+	const closeButton = await page.getByLabel( /^Close$|^Close dialog$/ );
+	if ( ( await closeButton.count() ) > 0 ) {
 		await closeButton.click();
 	}
 };
