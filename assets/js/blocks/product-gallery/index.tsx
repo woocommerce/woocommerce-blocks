fa21f08014459a5982f72015f7d7f2adcd85cb10
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-07-03 15:32:06 +0200

    Product Gallery Large Image: Update Large Image icon and description. Move metadata to the block.json file.

--- assets/js/blocks/product-gallery/index.tsx
+++ assets/js/blocks/product-gallery/index.tsx
@@ -15,14 +15,13 @@ import './inner-blocks/product-gallery-large-image';
 if ( isExperimentalBuild() ) {
 	registerBlockSingleProductTemplate( {
 		blockName: metadata.name,
-		// @ts-expect-error: `metadata` currently does not have a type definition in WordPress core
 		blockMetadata: metadata,
 		blockSettings: {
 			icon,
-			// @ts-expect-error `edit` can be extended to include other attributes
 			edit: Edit,
 			save: Save,
 			ancestor: [ 'woocommerce/single-product' ],
 		},
+		isAvailableOnPostEditor: true,
 	} );
 }
