953c133869f7e2078af7527bb53dc397fa5a9afa
Mike Jolley <mike.jolley@me.com>
2023-07-04 11:32:56 +0100

    Move code from templates into the details block

--- src/BlockTypes/OrderConfirmation/Details.php
+++ src/BlockTypes/OrderConfirmation/Details.php
@@ -86,6 +86,7 @@ class Details extends AbstractOrderConfirmationBlock {
 					</tbody>
 					<tfoot>
 						' . $this->render_order_details_table_totals( $order ) . '
+						' . $this->render_order_details_table_customer_note( $order ) . '
 					</tfoot>
 				</table>
 				' . $this->get_hook_content( 'woocommerce_order_details_after_order_table', [ $order ] ) . '
@@ -97,33 +98,92 @@ class Details extends AbstractOrderConfirmationBlock {
 	/**
 	 * Render order details table items.
 	 *
+	 * Loosely based on the templates order-details.php and order-details-item.php from core.
+	 *
 	 * @param \WC_Order $order Order object.
 	 * @return string
 	 */
 	protected function render_order_details_table_items( $order ) {
-		ob_start();
+		$return      = '';
+		$order_items = array_filter(
+			// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+			$order->get_items( apply_filters( 'woocommerce_purchase_order_item_types', 'line_item' ) ),
+			function( $item ) {
+				// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+				return apply_filters( 'woocommerce_order_item_visible', true, $item );
+			}
+		);
 
-		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
-		$show_purchase_note = $order->has_status( apply_filters( 'woocommerce_purchase_note_order_statuses', array( 'completed', 'processing' ) ) );
-
-		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
-		foreach ( $order->get_items( apply_filters( 'woocommerce_purchase_order_item_types', 'line_item' ) ) as $item_id => $item ) {
+		foreach ( $order_items as $item_id => $item ) {
 			$product = $item->get_product();
-
-			wc_get_template(
-				'order/order-details-item.php',
-				array(
-					'order'              => $order,
-					'item_id'            => $item_id,
-					'item'               => $item,
-					'show_purchase_note' => $show_purchase_note,
-					'purchase_note'      => $product ? $product->get_purchase_note() : '',
-					'product'            => $product,
-				)
-			);
+			$return .= $this->render_order_details_table_item( $order, $item_id, $item, $product );
 		}
 
-		return ob_get_clean();
+		return $return;
+	}
+
+	/**
+	 * Render an item in the order details table.
+	 *
+	 * @param \WC_Order         $order Order object.
+	 * @param integer           $item_id Item ID.
+	 * @param \WC_Order_Item    $item Item object.
+	 * @param \WC_Product|false $product Product object if it exists.
+	 * @return string
+	 */
+	protected function render_order_details_table_item( $order, $item_id, $item, $product ) {
+		$is_visible = $product && $product->is_visible();
+		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+		$row_class = apply_filters( 'woocommerce_order_item_class', 'woocommerce-table__line-item order_item', $item, $order );
+		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+		$product_permalink = apply_filters( 'woocommerce_order_item_permalink', $is_visible ? $product->get_permalink( $item ) : '', $item, $order );
+		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+		$item_name    = apply_filters(
+			'woocommerce_order_item_name',
+			$product_permalink ? sprintf( '<a href="%s">%s</a>', $product_permalink, $item->get_name() ) : $item->get_name(),
+			$item,
+			$is_visible
+		);
+		$qty          = $item->get_quantity();
+		$refunded_qty = $order->get_qty_refunded_for_item( $item_id );
+		$qty_display  = $refunded_qty ? '<del>' . esc_html( $qty ) . '</del> <ins>' . esc_html( $qty - ( $refunded_qty * -1 ) ) . '</ins>' : esc_html( $qty );
+		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+		$item_qty = apply_filters(
+			'woocommerce_order_item_quantity_html',
+			'<strong class="product-quantity">' . sprintf( '&times;&nbsp;%s', $qty_display ) . '</strong>',
+			$item
+		);
+
+		return '
+			<tr class="' . esc_attr( $row_class ) . '">
+				<td class="woocommerce-table__product-name product-name">
+					' . wp_kses_post( $item_name ) . '&nbsp;
+					' . wp_kses_post( $item_qty ) . '
+					' . $this->get_hook_content( 'woocommerce_order_item_meta_start', [ $item_id, $item, $order, false ] ) . '
+					' . wc_display_item_meta( $item, [ 'echo' => false ] ) . '
+					' . $this->get_hook_content( 'woocommerce_order_item_meta_end', [ $item_id, $item, $order, false ] ) . '
+					' . $this->render_order_details_table_item_purchase_note( $order, $product ) . '
+				</td>
+				<td class="woocommerce-table__product-total product-total">
+					' . wp_kses_post( $order->get_formatted_line_subtotal( $item ) ) . '
+				</td>
+			</tr>
+		';
+	}
+
+	/**
+	 * Render an item purchase note.
+	 *
+	 * @param \WC_Order         $order Order object.
+	 * @param \WC_Product|false $product Product object if it exists.
+	 * @return string
+	 */
+	protected function render_order_details_table_item_purchase_note( $order, $product ) {
+		// phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+		$show_purchase_note = $order->has_status( apply_filters( 'woocommerce_purchase_note_order_statuses', array( 'completed', 'processing' ) ) );
+		$purchase_note      = $product ? $product->get_purchase_note() : '';
+
+		return $show_purchase_note && $purchase_note ? '<div class="product-purchase-note">' . wp_kses_post( $purchase_note ) . '</div>' : '';
 	}
 
 	/**
@@ -133,26 +193,34 @@ class Details extends AbstractOrderConfirmationBlock {
 	 * @return string
 	 */
 	protected function render_order_details_table_totals( $order ) {
-		ob_start();
+		$return = '';
 
-		foreach ( $order->get_order_item_totals() as $key => $total ) {
-			?>
+		foreach ( $order->get_order_item_totals() as $total ) {
+			$return .= '
 				<tr>
-					<th scope="row"><?php echo esc_html( $total['label'] ); ?></th>
-					<td><?php echo wp_kses_post( $total['value'] ); ?></td>
+					<th scope="row">' . esc_html( $total['label'] ) . '</th>
+					<td>' . wp_kses_post( $total['value'] ) . '</td>
 				</tr>
-				<?php
+			';
 		}
 
-		if ( $order->get_customer_note() ) {
-			?>
-			<tr>
-				<th><?php esc_html_e( 'Note:', 'woo-gutenberg-products-block' ); ?></th>
-				<td><?php echo wp_kses_post( nl2br( wptexturize( $order->get_customer_note() ) ) ); ?></td>
-			</tr>
-			<?php
+		return $return;
+	}
+
+	/**
+	 * Render customer note.
+	 *
+	 * @param \WC_Order $order Order object.
+	 * @return string
+	 */
+	protected function render_order_details_table_customer_note( $order ) {
+		if ( ! $order->get_customer_note() ) {
+			return '';
 		}
 
-		return ob_get_clean();
+		return '<tr>
+			<th>' . esc_html__( 'Note:', 'woo-gutenberg-products-block' ) . '</th>
+			<td>' . wp_kses_post( nl2br( wptexturize( $order->get_customer_note() ) ) ) . '</td>
+		</tr>';
 	}
 }
