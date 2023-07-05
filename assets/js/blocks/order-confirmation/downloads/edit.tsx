3d117e1ac97e7f30afb8d60def7f4ff36ad71837
Mike Jolley <mike.jolley@me.com>
2023-07-05 17:13:41 +0100

    Order confirmation: Convert Order Details Templates to Blocks (#10095)
    * Move code from templates into the details block
    
    * Details -> Totals
    
    * Downloads block
    
    * Sample content for downloads block
    
    * Add block icon

--- assets/js/blocks/order-confirmation/downloads/edit.tsx
+++ assets/js/blocks/order-confirmation/downloads/edit.tsx
@@ -3,27 +3,39 @@
  */
 import ServerSideRender from '@wordpress/server-side-render';
 import { useBlockProps } from '@wordpress/block-editor';
+import { Disabled } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
 
+interface Props {
+	attributes: {
+		align: string;
+		className: string;
+		isPreview: boolean;
+	};
+	name: string;
+}
+
 const Edit = ( props: Props ): JSX.Element => {
 	const { attributes, name } = props;
 	const blockProps = useBlockProps( {
-		className: 'wc-block-order-confirmation-details',
+		className: 'wc-block-order-confirmation-downloads',
 	} );
 
 	return (
 		<div { ...blockProps }>
-			<ServerSideRender
-				block={ name }
-				attributes={ {
-					...attributes,
-					isPreview: true,
-				} }
-			/>
+			<Disabled>
+				<ServerSideRender
+					block={ name }
+					attributes={ {
+						...attributes,
+						isPreview: true,
+					} }
+				/>
+			</Disabled>
 		</div>
 	);
 };
