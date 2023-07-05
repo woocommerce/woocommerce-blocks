d80080aabac199097344ae931a424b6b6138515b
Mike Jolley <mike.jolley@me.com>
2023-07-05 17:13:41 +0100

    Order confirmation: Convert Order Details Templates to Blocks (#10095)
    * Move code from templates into the details block
    
    * Details -> Totals
    
    * Downloads block
    
    * Sample content for downloads block
    
    * Add block icon

---
+++ assets/js/blocks/order-confirmation/totals/edit.tsx
@@ -0,0 +1,40 @@
+/**
+ * External dependencies
+ */
+import ServerSideRender from '@wordpress/server-side-render';
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+interface Props {
+	attributes: {
+		align: string;
+		className: string;
+		isPreview: boolean;
+	};
+	name: string;
+}
+
+const Edit = ( props: Props ): JSX.Element => {
+	const { attributes, name } = props;
+	const blockProps = useBlockProps( {
+		className: 'wc-block-order-confirmation-totals',
+	} );
+
+	return (
+		<div { ...blockProps }>
+			<ServerSideRender
+				block={ name }
+				attributes={ {
+					...attributes,
+					isPreview: true,
+				} }
+			/>
+		</div>
+	);
+};
+
+export default Edit;
--- src/BlockTypesController.php
+++ src/BlockTypesController.php
@@ -231,7 +231,8 @@ final class BlockTypesController {
 			$block_types[] = 'ProductGallery';
 			$block_types[] = 'OrderConfirmation\Status';
 			$block_types[] = 'OrderConfirmation\Summary';
-			$block_types[] = 'OrderConfirmation\Details';
+			$block_types[] = 'OrderConfirmation\Totals';
+			$block_types[] = 'OrderConfirmation\Downloads';
 			$block_types[] = 'OrderConfirmation\BillingAddress';
 			$block_types[] = 'OrderConfirmation\ShippingAddress';
 		}
