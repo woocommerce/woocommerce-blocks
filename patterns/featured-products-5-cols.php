d1974a066bb149f51788b1f770317c7ba5993c8c
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-07-05 17:35:20 +0200

    Featured Products 5-Item Grid pattern: Ensure a placeholder image is displayed when the product doesn't have any

--- patterns/featured-products-5-cols.php
+++ patterns/featured-products-5-cols.php
@@ -12,34 +12,29 @@
 </h2>
 <!-- /wp:heading -->
 
-<!-- wp:query {"queryId":2,"query":{"perPage":"5","pages":0,"offset":0,"postType":"product","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"__woocommerceAttributes":[],"__woocommerceStockStatus":["instock","onbackorder"]},"displayLayout":{"type":"flex","columns":5},"namespace":"woocommerce/product-query","align":"wide","layout":{"type":"default"}} -->
-<div class="wp-block-query alignwide">
-	<!-- wp:post-template {"__woocommerceNamespace":"woocommerce/product-query/product-template"} -->
+<!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignfull">
+	<!-- wp:query {"query":{"perPage":"5","pages":0,"offset":0,"postType":"product","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"__woocommerceAttributes":[],"__woocommerceStockStatus":["instock","onbackorder"]},"displayLayout":{"type":"flex","columns":5},"namespace":"woocommerce/product-query"} -->
+	<div class="wp-block-query">
+		<!-- wp:post-template {"__woocommerceNamespace":"woocommerce/product-query/product-template"} -->
 
-		<!-- wp:cover {"useFeaturedImage":true,"dimRatio":0,"minHeight":190,"minHeightUnit":"px","contentPosition":"top right","isDark":false,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}}} -->
-		<div class="wp-block-cover is-light has-custom-content-position is-position-top-right" style="margin-bottom:var(--wp--preset--spacing--40);min-height:190px">
-			<span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
-			<div class="wp-block-cover__inner-container">
-				<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-				<div class="wp-block-group">
-					<!-- wp:woocommerce/product-sale-badge {"isDescendentOfQueryLoop":true,"style":{"border":{"width":"0px","style":"none","radius":"0px"},"typography":{"textTransform":"capitalize","fontSize":"12px","fontStyle":"normal","fontWeight":"400"}}} /-->
-				</div>
-				<!-- /wp:group -->
-			</div>
+		<!-- wp:group -->
+		<div class="wp-block-group has-contrast-color has-text-color" style="">
+			<!-- wp:woocommerce/product-image {"imageSizing":"thumbnail","isDescendentOfQueryLoop":true} /-->
+			<!-- wp:post-title {"level":3,"isLink":true,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0rem"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small","__woocommerceNamespace":"woocommerce/product-query/product-title"} /-->
+			<!-- wp:woocommerce/product-price {"isDescendentOfQueryLoop":true,"textAlign":"left","fontSize":"small","style":{"spacing":{"margin":{"bottom":"0","top":"var:preset|spacing|20"}}}} /-->
 		</div>
-		<!-- /wp:cover -->
+		<!-- /wp:group -->
 
-		<!-- wp:post-title {"level":3,"isLink":true,"style":{"spacing":{"margin":{"top":"0","right":"0","bottom":"0rem"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"small","__woocommerceNamespace":"woocommerce/product-query/product-title"} /-->
+		<!-- /wp:post-template -->
 
-		<!-- wp:woocommerce/product-price {"isDescendentOfQueryLoop":true,"textAlign":"left","fontSize":"small","style":{"spacing":{"margin":{"bottom":"0","top":"var:preset|spacing|20"}}}} /-->
-
-	<!-- /wp:post-template -->
-
-	<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
-	<p></p>
-	<!-- /wp:paragraph -->
+		<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
+		<p></p>
+		<!-- /wp:paragraph -->
+	</div>
+	<!-- /wp:query -->
 </div>
-<!-- /wp:query -->
+<!-- /wp:group -->
 
 <!-- wp:buttons {"align":"wide","layout":{"type":"flex","verticalAlignment":"center","justifyContent":"center"}} -->
 <div class="wp-block-buttons alignwide">
