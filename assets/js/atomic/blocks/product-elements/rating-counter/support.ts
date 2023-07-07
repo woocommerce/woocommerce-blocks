1328dc355db3071ef93d611668165f7a3ece6467
Alba Rincón <alba.rincon@automattic.com>
2023-07-07 15:16:38 +0200

    Remove condition

--- assets/js/atomic/blocks/product-elements/rating-counter/support.ts
+++ assets/js/atomic/blocks/product-elements/rating-counter/support.ts
@@ -3,7 +3,6 @@
  * External dependencies
  */
 import { isFeaturePluginBuild } from '@woocommerce/block-settings';
-import { __experimentalGetSpacingClassesAndStyles } from '@wordpress/block-editor';
 
 export const supports = {
 	...( isFeaturePluginBuild() && {
@@ -22,10 +21,4 @@ export const supports = {
 		},
 		__experimentalSelector: '.wc-block-components-product-rating-counter',
 	} ),
-	...( ! isFeaturePluginBuild() &&
-		typeof __experimentalGetSpacingClassesAndStyles === 'function' && {
-			spacing: {
-				margin: true,
-			},
-		} ),
 };
