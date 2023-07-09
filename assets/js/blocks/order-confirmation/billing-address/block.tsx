2d6885a7aa592999c3224bc9ea0d3f0bcfcd6427
Saad Tarhi <saad.trh@gmail.com>
2023-07-09 15:54:51 +0100

    Update the billing address displayed data
    - Display the full country name
    - Add checks and conditional rendering

--- assets/js/blocks/order-confirmation/billing-address/block.tsx
+++ assets/js/blocks/order-confirmation/billing-address/block.tsx
@@ -2,6 +2,8 @@
  * External dependencies
  */
 import { useCheckoutAddress } from '@woocommerce/base-context';
+import { getSetting } from '@woocommerce/settings';
+import { __ } from '@wordpress/i18n';
 
 const Block = (): JSX.Element => {
 	const { billingAddress } = useCheckoutAddress();
@@ -16,31 +18,45 @@ const Block = (): JSX.Element => {
 		state,
 		email,
 		phone,
-	} = billingAddress;
+	} = billingAddress || {};
+
+	// Contains country names.
+	const countries = getSetting< Record< string, string > >( 'countries', {} );
+	const fullCountryName = countries[ country ] || country;
+	const fallback = __(
+		'This order has no billing address.',
+		'woo-gutenberg-products-block'
+	);
 
 	return (
-		<address>
-			{ firstName } { lastName }
-			<br />
-			{ address1 }
-			<br />
-			{ address2 }
-			<br />
-			{ city }
-			<br />
-			{ state }
-			<br />
-			{ postcode }
-			<br />
-			{ country }
-			<br />
-			<br />
-			{ phone }
-			<br />
-			<br />
-			{ email }
-			<br />
-		</address>
+		<>
+			{ billingAddress && Object.keys( billingAddress ).length > 0 ? (
+				<address>
+					{ firstName } { lastName }
+					<br />
+					{ address1 }
+					<br />
+					{ address2 }
+					<br />
+					{ city }
+					<br />
+					{ state }
+					<br />
+					{ postcode }
+					<br />
+					{ fullCountryName }
+					<br />
+					<br />
+					{ phone }
+					<br />
+					<br />
+					{ email }
+					<br />
+				</address>
+			) : (
+				fallback
+			) }
+		</>
 	);
 };
 
