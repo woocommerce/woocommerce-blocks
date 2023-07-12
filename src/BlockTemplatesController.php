1843a9a35067ddf97b1e148af52ca411bab17420
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-07-12 14:35:33 +0200

    Fix supports_block_templates check to test correct template type

--- src/BlockTemplatesController.php
+++ src/BlockTemplatesController.php
@@ -295,7 +295,7 @@ class BlockTemplatesController {
 	 * @return array
 	 */
 	public function add_block_templates( $query_result, $query, $template_type ) {
-		if ( ! BlockTemplateUtils::supports_block_templates( 'wp_template_part' ) ) {
+		if ( ! BlockTemplateUtils::supports_block_templates( $template_type ) ) {
 			return $query_result;
 		}
 
