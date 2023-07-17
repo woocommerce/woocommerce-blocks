b76f51adc37b499cd31acc5daf431eab4d659ded
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-07-17 15:06:38 +0200

    Make strings in Featured Category Cover Image pattern translatable

--- patterns/featured-category-cover-image.php
+++ patterns/featured-category-cover-image.php
@@ -11,16 +11,27 @@
 	<img class="wp-block-cover__image-background wp-image-1" alt="<?php esc_attr_e( 'Placeholder image used to represent products being showcased in a featured category section.', 'woo-gutenberg-products-block' ); ?>" src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/wood-leather-fur-shop-jeans-shelf.png', dirname( __FILE__ ) ) ); ?>" style="object-position:0% 0%" data-object-fit="cover" data-object-position="0% 0%"/>
 	<div class="wp-block-cover__inner-container">
 		<!-- wp:heading {"textAlign":"center","style":{"color":{"text":"#ffffff"}}} -->
-		<h2 class="wp-block-heading has-text-align-center has-text-color" style="color:#ffffff;"><strong>100% natural denim</strong></h2>
+		<h2 class="wp-block-heading has-text-align-center has-text-color" style="color:#ffffff;"><strong>
+		<?php
+		esc_attr_e(
+			'100% natural denim',
+			'woo-gutenberg-products-block'
+		);
+		?>
+		</strong></h2>
 		<!-- /wp:heading -->
 
 		<!-- wp:paragraph {"align":"center","style":{"color":{"text":"#ffffff"},"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
-		<p class="has-text-align-center has-text-color" style="color:#ffffff;margin-top:0px;margin-bottom:0px">Only the finest goes into our products. You deserve it.</p>
+		<p class="has-text-align-center has-text-color" style="color:#ffffff;margin-top:0px;margin-bottom:0px">
+		<?php
+		esc_attr_e( 'Only the finest goes into our products. You deserve it.', 'woo-gutenberg-products-block' );
+		?>
+		</p>
 		<!-- /wp:paragraph -->
 		<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"30px"}}}} -->
 		<div class="wp-block-buttons" style="margin-top:30px">
 			<!-- wp:button {"textAlign":"center"} -->
-			<div class="wp-block-button"><a href="<?php echo esc_url( wc_get_page_permalink( 'shop' ) ); ?>" class="wp-block-button__link has-text-align-center wp-element-button">Shop jeans</a></div>
+			<div class="wp-block-button"><a href="<?php echo esc_url( wc_get_page_permalink( 'shop' ) ); ?>" class="wp-block-button__link has-text-align-center wp-element-button"><?php esc_attr_e( 'Shop jeans', 'woo-gutenberg-products-block' ); ?></a></div>
 			<!-- /wp:button --></div>
 		<!-- /wp:buttons --></div>
 </div>
