f21eebc651036db01e5234c6906fd4314c37d6e4
Alba Rincón <alba.rincon@automattic.com>
2023-07-18 09:27:23 +0200

    Avoid duplication of styles

--- assets/js/atomic/blocks/product-elements/average-rating/support.ts
+++ assets/js/atomic/blocks/product-elements/average-rating/support.ts
@@ -5,19 +5,22 @@
 import { isFeaturePluginBuild } from '@woocommerce/block-settings';
 
 export const supports = {
-	color: {
-		text: true,
-		background: true,
-	},
-	spacing: {
-		margin: true,
-		padding: true,
-	},
 	...( isFeaturePluginBuild() && {
+		color: {
+			text: true,
+			background: true,
+			__experimentalSkipSerialization: true,
+		},
+		spacing: {
+			margin: true,
+			padding: true,
+			__experimentalSkipSerialization: true,
+		},
 		typography: {
 			fontSize: true,
 			__experimentalFontWeight: true,
 			__experimentalSkipSerialization: true,
 		},
+		__experimentalSelector: '.wc-block-components-product-average-rating',
 	} ),
 };
