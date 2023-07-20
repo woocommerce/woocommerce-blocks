0058528c964296b3f811fc48654b028f4be3b0aa
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-07-20 10:33:42 +0200

    Change the way to bold text in Upgrade Notices so it's translatable

--- assets/js/blocks/product-query/inspector-controls/upgrade-notice.tsx
+++ assets/js/blocks/product-query/inspector-controls/upgrade-notice.tsx
@@ -3,24 +3,24 @@
  */
 import { __ } from '@wordpress/i18n';
 import { Notice, Button } from '@wordpress/components';
-
-const FormattedNotice = ( { notice }: { notice: string } ) => {
-	const strongText = 'Product Collection';
-	const [ before, after ] = notice.split( strongText );
-
-	return (
-		<>
-			{ before }
-			<strong>{ strongText }</strong>
-			{ after }
-		</>
-	);
-};
+import { createInterpolateElement } from '@wordpress/element';
 
 export const UpgradeNotice = ( props: { upgradeBlock: () => void } ) => {
-	const notice = __(
-		'Upgrade all Products (Beta) blocks on this page to Product Collection for more features!',
-		'woo-gutenberg-products-block'
+	const notice = createInterpolateElement(
+		__(
+			'Upgrade all Products (Beta) blocks on this page to <strongText /> for more features!',
+			'woo-gutenberg-products-block'
+		),
+		{
+			strongText: (
+				<strong>
+					{ __(
+						`Product Collection`,
+						'woo-gutenberg-products-block'
+					) }
+				</strong>
+			),
+		}
 	);
 
 	const buttonLabel = __(
@@ -34,7 +34,7 @@ export const UpgradeNotice = ( props: { upgradeBlock: () => void } ) => {
 
 	return (
 		<Notice isDismissible={ false }>
-			<FormattedNotice notice={ notice } />
+			<>{ notice }</>
 			<br />
 			<br />
 			<Button variant="link" onClick={ handleClick }>
