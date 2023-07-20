845526f424da1d4422bbe18eafe1e1bf7f1a7018
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-07-20 14:56:17 +0200

    Disable auto migration

--- assets/js/blocks/migration-products-to-product-collection/constants.ts
+++ assets/js/blocks/migration-products-to-product-collection/constants.ts
@@ -1,3 +1,3 @@
-export const REPLACE_PRODUCTS_WITH_PRODUCT_COLLECTION = true;
+export const REPLACE_PRODUCTS_WITH_PRODUCT_COLLECTION = false;
 export const MIGRATION_STATUS_LS_KEY =
 	'wc-blocks_upgraded-products-to-product-collection';
