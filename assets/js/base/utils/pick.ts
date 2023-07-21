63c0e858ee732b0cd8896a1a574e95e3de831f74
Mike Jolley <mike.jolley@me.com>
2023-07-21 16:39:40 +0100

    Push changes refactoring

---
+++ assets/js/base/utils/pick.ts
@@ -0,0 +1,8 @@
+export const pick = < Type >( object: Type, keys: string[] ): Type => {
+	return keys.reduce( ( obj, key ) => {
+		if ( object && object.hasOwnProperty( key ) ) {
+			obj[ key as keyof Type ] = object[ key as keyof Type ];
+		}
+		return obj;
+	}, {} as Type );
+};
