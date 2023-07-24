c5c54590958c99a80c0c5bb00ed369f0595a473d
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-07-24 13:12:53 +0200

    Introduce the new Featured Products: Fresh & Tasty pattern.

---
+++ patterns/featured-products-fresh-and-tasty.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Title: Featured Products: Fresh & Tasty
+ * Slug: woocommerce-blocks/featured-products-fresh-and-tasty
+ * Categories: WooCommerce
+ */
+?>
+<!-- wp:heading {"level":3,"align":"wide"} -->
+<h3 class="wp-block-heading alignwide">Fresh &amp; tasty goods</h3>
+<!-- /wp:heading -->
+
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":2267,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full">
+	<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/sweet-organic-lemons.png', dirname( __FILE__ ) ) ); ?>" alt="<?php esc_attr_e( 'Placeholder image used to represent a product being showcased as Sweet organic Lemons.', 'woo-gutenberg-products-block' ); ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"180px","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:180px"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><strong>Sweet Organic Lemons</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"94px","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:94px"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">from $1.99</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":2268,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full">
+	<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/fresh-organic-tomatoes.png', dirname( __FILE__ ) ) ); ?>" alt="<?php esc_attr_e( 'Placeholder image used to represent a product being showcased as Fresh Organic Tomatoes.', 'woo-gutenberg-products-block' ); ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"180px","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:180px"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><strong>Fresh Organic Tomatoes</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"94px","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:94px"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">from $2.99</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":2269,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full">
+	<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/fresh-lettuce-washed.png', dirname( __FILE__ ) ) ); ?>" alt="<?php esc_attr_e( 'Placeholder image used to represent a product being showcased as Fresh Lettuce Washed.', 'woo-gutenberg-products-block' ); ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"180px","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:180px"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><strong>Fresh Lettuce (Washed)</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"94px","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:94px"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">from $0.99</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"id":2270,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full">
+	<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/russet-organic-potatoes.png', dirname( __FILE__ ) ) ); ?>" alt="<?php esc_attr_e( 'Placeholder image used to represent a product being showcased as Russet Organic Potatoes.', 'woo-gutenberg-products-block' ); ?>" />
+</figure>
+<!-- /wp:image -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"width":"180px","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:180px"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size"><strong>Russet Organic Potatoes</strong></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"94px","layout":{"type":"default"}} -->
+<div class="wp-block-column" style="flex-basis:94px"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">from $1.49</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
