fc2bc64f33829c56437af723d23f451b029a5b7d
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-07-26 13:05:40 +0200

    Make title translatable. Removed hardcoded color for the icons. Update indentation.

--- patterns/social-follow-us-in-social-media.php
+++ patterns/social-follow-us-in-social-media.php
@@ -10,13 +10,13 @@
 	<!-- wp:column {"verticalAlignment":"bottom","width":"66.66%","layout":{"type":"constrained"}} -->
 	<div class="wp-block-column is-vertically-aligned-bottom" style="flex-basis:66.66%">
 		<!-- wp:heading {"level":3,"align":"wide"} -->
-		<h3 class="wp-block-heading alignwide">Follow us in social media</h3>
+		<h3 class="wp-block-heading alignwide"><?php esc_html_e( 'Follow us in social media', 'woo-gutenberg-products-block' ); ?></h3>
 		<!-- /wp:heading --></div>
 	<!-- /wp:column -->
 
 	<!-- wp:column {"verticalAlignment":"center","width":"33.33%"} -->
 	<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:33.33%">
-		<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#000000","openInNewTab":true,"style":{"spacing":{"blockGap":{"top":"0"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"right","orientation":"horizontal"}} -->
+		<!-- wp:social-links {"iconColor":"contrast","openInNewTab":true,"style":{"spacing":{"blockGap":{"top":"0"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"right","orientation":"horizontal"}} -->
 		<ul class="wp-block-social-links has-icon-color is-style-logos-only">
 			<!-- wp:social-link {"url":"<?php echo esc_url( 'https://twitter.com/' ); ?>","service":"twitter"} /-->
 
@@ -40,7 +40,8 @@
 		<figure class="wp-block-image size-large">
 			<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/office.png', dirname( __FILE__ ) ) ); ?>" alt="<?php esc_attr_e( 'Placeholder image used to represent products being showcased as furniture in an office.', 'woo-gutenberg-products-block' ); ?>" />
 		</figure>
-		<!-- /wp:image --></div>
+		<!-- /wp:image -->
+	</div>
 	<!-- /wp:column -->
 
 	<!-- wp:column -->
@@ -49,7 +50,8 @@
 		<figure class="wp-block-image size-large">
 			<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/living-room.png', dirname( __FILE__ ) ) ); ?>" alt="<?php esc_attr_e( 'Placeholder image used to represent products being showcased as furniture in a living room.', 'woo-gutenberg-products-block' ); ?>" />
 		</figure>
-		<!-- /wp:image --></div>
+		<!-- /wp:image -->
+	</div>
 	<!-- /wp:column -->
 
 	<!-- wp:column -->
@@ -58,7 +60,8 @@
 		<figure class="wp-block-image size-large">
 			<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/living-room-sofa.png', dirname( __FILE__ ) ) ); ?>" alt="<?php esc_attr_e( 'Placeholder image used to represent products being showcased as furniture in a living room with gray sofa.', 'woo-gutenberg-products-block' ); ?>" />
 		</figure>
-		<!-- /wp:image --></div>
+		<!-- /wp:image -->
+	</div>
 	<!-- /wp:column -->
 
 	<!-- wp:column -->
@@ -67,6 +70,8 @@
 		<figure class="wp-block-image size-large">
 			<img src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/dining-room.png', dirname( __FILE__ ) ) ); ?>" alt="<?php esc_attr_e( 'Placeholder image used to represent products being showcased as furniture in a dining room.', 'woo-gutenberg-products-block' ); ?>" />
 		</figure>
-		<!-- /wp:image --></div>
-	<!-- /wp:column --></div>
+		<!-- /wp:image -->
+	</div>
+	<!-- /wp:column -->
+</div>
 <!-- /wp:columns -->
