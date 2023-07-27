5528087fdb5b250b0b71b22088ae0cfc7065f7a1
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-07-27 20:31:19 +0200

    Product Gallery Thumbnails: Refactor Product Gallery edit code and move the logic to a utils file

---
+++ assets/js/blocks/product-gallery/utils.tsx
@@ -0,0 +1,175 @@
+/**
+ * External dependencies
+ */
+import { store as blockEditorStore } from '@wordpress/block-editor';
+import { BlockAttributes } from '@wordpress/blocks';
+import { select, dispatch } from '@wordpress/data';
+
+/**
+ * Generates layout attributes based on the position of thumbnails.
+ *
+ * @param {string} thumbnailsPosition - The position of thumbnails ('bottom' or other values).
+ * @return {Object} - An object representing layout attributes.
+ */
+export function getGroupLayoutAttributes( thumbnailsPosition: string ) {
+	switch ( thumbnailsPosition ) {
+		case 'bottom':
+			// Stack
+			return { type: 'flex', orientation: 'vertical' };
+		default:
+			// Row
+			return { type: 'flex', flexWrap: 'nowrap' };
+	}
+}
+
+/**
+ * Returns inner block lock attributes based on provided action.
+ *
+ * @param {string} action - The action to take on the inner blocks ('lock' or 'unlock').
+ * @return {Object} - An object representing lock attributes for inner blocks.
+ */
+export function getInnerBlocksLockAttributes( action: string ) {
+	switch ( action ) {
+		case 'lock':
+			return { lock: { move: true, remove: true } };
+		case 'unlock':
+			return { lock: {} };
+		default:
+			return { lock: {} };
+	}
+}
+
+/**
+ * Updates block attributes based on provided attributes.
+ *
+ * @param {BlockAttributes} attributesToUpdate - The new attributes to set on the block.
+ * @param {Object} block - The block object to update.
+ */
+export function updateBlockAttributes(
+	attributesToUpdate: BlockAttributes,
+	block: BlockAttributes | undefined
+) {
+	if ( block !== undefined ) {
+		const updatedBlock = {
+			...block,
+			attributes: {
+				...block.attributes,
+				...attributesToUpdate,
+			},
+		};
+
+		dispatch( 'core/block-editor' ).updateBlock(
+			block.clientId,
+			updatedBlock
+		);
+	}
+}
+
+/**
+ * Moves inner blocks to a position based on provided attributes.
+ *
+ * @param {BlockAttributes} attributes - The attributes of the parent block.
+ * @param {string} clientId - The clientId of the parent block.
+ */
+export function moveInnerBlocksToPosition(
+	attributes: BlockAttributes,
+	clientId: string
+) {
+	const parentBlock = select( 'core/block-editor' ).getBlock( clientId );
+
+	if ( parentBlock?.name === 'woocommerce/product-gallery' ) {
+		const groupBlock = parentBlock.innerBlocks.find(
+			( innerBlock ) => innerBlock.name === 'core/group'
+		);
+
+		if ( groupBlock ) {
+			const largeImageBlock = groupBlock.innerBlocks.find(
+				( innerBlock ) =>
+					innerBlock.name ===
+					'woocommerce/product-gallery-large-image'
+			);
+
+			const thumbnailsBlock = groupBlock.innerBlocks.find(
+				( innerBlock ) =>
+					innerBlock.name === 'woocommerce/product-gallery-thumbnails'
+			);
+
+			const thumbnailsIndex = groupBlock.innerBlocks.findIndex(
+				( innerBlock ) =>
+					innerBlock.name === 'woocommerce/product-gallery-thumbnails'
+			);
+
+			const largeImageIndex = groupBlock.innerBlocks.findIndex(
+				( innerBlock ) =>
+					innerBlock.name ===
+					'woocommerce/product-gallery-large-image'
+			);
+
+			if ( thumbnailsIndex !== -1 && largeImageIndex !== -1 ) {
+				updateBlockAttributes(
+					getInnerBlocksLockAttributes( 'unlock' ),
+					thumbnailsBlock
+				);
+				updateBlockAttributes(
+					getInnerBlocksLockAttributes( 'unlock' ),
+					largeImageBlock
+				);
+
+				const { thumbnailsPosition } = attributes;
+				const clientIdToMove =
+					groupBlock.innerBlocks[ thumbnailsIndex ].clientId;
+
+				if (
+					thumbnailsPosition === 'bottom' ||
+					thumbnailsPosition === 'right'
+				) {
+					// @ts-expect-error - Ignoring because `moveBlocksDown` is not yet in the type definitions.
+					dispatch( blockEditorStore ).moveBlocksDown(
+						[ clientIdToMove ],
+						groupBlock.clientId
+					);
+				} else {
+					// @ts-expect-error - Ignoring because `moveBlocksUp` is not yet in the type definitions.
+					dispatch( blockEditorStore ).moveBlocksUp(
+						[ clientIdToMove ],
+						groupBlock.clientId
+					);
+				}
+
+				updateBlockAttributes(
+					getInnerBlocksLockAttributes( 'lock' ),
+					thumbnailsBlock
+				);
+				updateBlockAttributes(
+					getInnerBlocksLockAttributes( 'lock' ),
+					largeImageBlock
+				);
+			}
+		}
+	}
+}
+
+/**
+ * Updates the type of group block based on provided attributes.
+ *
+ * @param {BlockAttributes} attributes - The attributes of the parent block.
+ * @param {string} clientId - The clientId of the parent block.
+ */
+export function updateGroupBlockType(
+	attributes: BlockAttributes,
+	clientId: string
+) {
+	const block = select( 'core/block-editor' ).getBlock( clientId );
+	block?.innerBlocks.forEach( ( innerBlock ) => {
+		if ( innerBlock.name === 'core/group' ) {
+			updateBlockAttributes(
+				{
+					layout: getGroupLayoutAttributes(
+						attributes.thumbnailsPosition
+					),
+				},
+				innerBlock
+			);
+		}
+	} );
+}
