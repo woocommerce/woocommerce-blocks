58b6c4fe7d264ee67b887c98964c70017cddfec8
Alexandre Lara <allexandrelara@gmail.com>
2023-07-28 17:10:20 -0300

    Fix the block icon color when selected

--- assets/js/blocks/product-gallery/inner-blocks/product-gallery-pager/icons.tsx
+++ assets/js/blocks/product-gallery/inner-blocks/product-gallery-pager/icons.tsx
@@ -15,7 +15,7 @@ export const ProductGalleryBlockIcon = () => (
 			fillRule="evenodd"
 			clipRule="evenodd"
 			d="M5.00018 11L7.00018 11L7.00018 13H5.00018V11ZM11.0002 11L13.0002 11V13H11.0002V11ZM17.0002 11L19.0002 11V13H17.0002V11Z"
-			fill="#1E1E1E"
+			fill="currentColor"
 		/>
 	</svg>
 );
