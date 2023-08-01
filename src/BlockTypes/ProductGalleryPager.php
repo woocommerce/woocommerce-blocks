25dd64dffe78b4ba801d71ff431ea7e21a0e2a51
Alexandre Lara <allexandrelara@gmail.com>
2023-08-01 15:03:20 -0300

    Fix php cs errors

--- src/BlockTypes/ProductGalleryPager.php
+++ src/BlockTypes/ProductGalleryPager.php
@@ -18,7 +18,7 @@ class ProductGalleryPager extends AbstractBlock {
 	protected function register_block_type_assets() {
 		return null;
 	}
-	
+
 	/**
 	 * Get the block's attributes.
 	 *
@@ -43,10 +43,10 @@ class ProductGalleryPager extends AbstractBlock {
 	 * @return string Rendered block type output.
 	 */
 	protected function render( $attributes, $content, $block ) {
-		$parsed_attributes = $this->parse_attributes( $attributes );
+		$parsed_attributes 	= $this->parse_attributes( $attributes );
 		$classname          = $attributes['className'] ?? '';
 		$wrapper_attributes = get_block_wrapper_attributes( array( 'class' => trim( sprintf( 'woocommerce %1$s', $classname ) ) ) );
-		$html = $this->render_pager( $parsed_attributes['pagerDisplayMode'] );
+		$html 				= $this->render_pager( $parsed_attributes['pagerDisplayMode'] );
 
 		return sprintf(
 			'<div %1$s>
