3a86d1a63f85c558edc3472489a9116bacb75403
Saad Tarhi <saad.trh@gmail.com>
2023-08-02 13:11:50 +0100

    Register the BillingWrapper Block server side

---
+++ src/BlockTypes/OrderConfirmation/BillingWrapper.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Automattic\WooCommerce\Blocks\BlockTypes\OrderConfirmation;
+
+use Automattic\WooCommerce\Blocks\Utils\StyleAttributesUtils;
+
+/**
+ * BillingWrapper class.
+ */
+class BillingWrapper extends AbstractOrderConfirmationBlock {
+
+	/**
+	 * Block name.
+	 *
+	 * @var string
+	 */
+	protected $block_name = 'order-confirmation-billing-wrapper';
+
+	/**
+	 * Render the block.
+	 *
+	 * @param array    $attributes Block attributes.
+	 * @param string   $content Block content.
+	 * @param WP_Block $block Block instance.
+	 *
+	 * @return string | void Rendered block output.
+	 */
+	protected function render( $attributes, $content, $block ) {
+		$order              = $this->get_order();
+		$content            = $order && $this->is_current_customer_order( $order ) ? $this->render_content( $order ) : '';
+		$classname          = $attributes['className'] ?? '';
+		$classes_and_styles = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes );
+
+		if ( isset( $attributes['align'] ) ) {
+			$classname .= " align{$attributes['align']}";
+		}
+
+		return sprintf(
+			'<div class="wc-block-%4$s %1$s %2$s">%3$s</div>',
+			esc_attr( $classes_and_styles['classes'] ),
+			esc_attr( $classname ),
+			$content,
+			esc_attr( $this->block_name )
+		);
+	}
+
+	/**
+	 * This renders the content of the billing wrapper.
+	 *
+	 * @return string
+	 */
+	protected function render_content() {
+		$heading = esc_html_( 'Billing address', 'woo-gutenberg-products-block' );
+
+		return '
+			<div class="woocommerce-column woocommerce-column--1 woocommerce-column--billing-address col-1">
+				<h2 class="woocommerce-order-details">
+					' . wp_kses_post( $heading ) . '
+				</h2>
+			</div>
+		';
+	}
+
+	/**
+	 * This is what gets rendered when the order does not exist.
+	 *
+	 * @return string
+	 */
+	protected function render_content_fallback() {
+		return '-';
+	}
+}
