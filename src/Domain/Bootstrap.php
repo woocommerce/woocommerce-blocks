2d342944a2ab9a18c2ca4e23bb8c534597957944
Mike Jolley <mike.jolley@me.com>
2023-08-04 12:39:33 +0100

    Comments

--- src/Domain/Bootstrap.php
+++ src/Domain/Bootstrap.php
@@ -142,7 +142,8 @@ class Bootstrap {
 
 		// Load assets unless this is a request specifically for the store API.
 		if ( ! $is_store_api_request ) {
-			// Template related functionality. These won't be loaded for store API requests, but may be loaded for regular rest requests to maintain compatibility with the store editor.
+			// Template related functionality. These won't be loaded for store API requests, but may be loaded for
+			// regular rest requests to maintain compatibility with the store editor.
 			$this->container->get( BlockPatterns::class );
 			$this->container->get( BlockTypesController::class );
 			$this->container->get( BlockTemplatesController::class );
