6e64837593a4e8b3bfac810658f857fd2c6e29ff
Mike Jolley <mike.jolley@me.com>
2023-08-04 13:27:41 +0100

    Remove ShippingAssets

--- src/Domain/Bootstrap.php
+++ src/Domain/Bootstrap.php
@@ -136,7 +136,6 @@ class Bootstrap {
 			$this->container->get( AssetsController::class );
 			$this->container->get( Installer::class )->init();
 			$this->container->get( GoogleAnalytics::class )->init();
-			$this->container->get( ShippingAssets::class )->init();
 		}
 
 		// Load assets unless this is a request specifically for the store API.
