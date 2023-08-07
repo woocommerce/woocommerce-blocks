26deb7268189ea5cb8e396b57e1da3b3618b6756
Alexandre Lara <allexandrelara@gmail.com>
2023-08-07 15:16:07 -0300

    Remove isExperimentalBuild from Product Gallery inner blocks

--- assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image/index.tsx
+++ assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image/index.tsx
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { registerBlockType } from '@wordpress/blocks';
-import { isExperimentalBuild } from '@woocommerce/block-settings';
 
 /**
  * Internal dependencies
@@ -12,11 +11,9 @@ import { Edit, Save } from './edit';
 import metadata from './block.json';
 import './style.scss';
 
-if ( isExperimentalBuild() ) {
-	// @ts-expect-error: `metadata` currently does not have a type definition in WordPress core
-	registerBlockType( metadata, {
-		icon,
-		edit: Edit,
-		save: Save,
-	} );
-}
+// @ts-expect-error: `metadata` currently does not have a type definition in WordPress core
+registerBlockType( metadata, {
+	icon,
+	edit: Edit,
+	save: Save,
+} );
