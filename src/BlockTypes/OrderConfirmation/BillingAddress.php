a3c57155639d6d8a164403678cb820c8e23d46d0
Mike Jolley <mike.jolley@me.com>
2023-08-07 11:11:34 +0100

    Unauthenticated views for Order Confirmation template (#10414)
    * Different views by permission
    
    * check user id matches when logged out

--- src/BlockTypes/OrderConfirmation/BillingAddress.php
+++ src/BlockTypes/OrderConfirmation/BillingAddress.php
@@ -17,58 +17,20 @@ class BillingAddress extends AbstractOrderConfirmationBlock {
 	protected $block_name = 'order-confirmation-billing-address';
 
 	/**
-	 * Render the block.
-	 *
-	 * @param array    $attributes Block attributes.
-	 * @param string   $content Block content.
-	 * @param WP_Block $block Block instance.
-	 *
-	 * @return string | void Rendered block output.
-	 */
-	protected function render( $attributes, $content, $block ) {
-		$order              = $this->get_order();
-		$content            = $order && $this->is_current_customer_order( $order ) ? $this->render_content( $order ) : $this->render_content_fallback();
-		$classname          = $attributes['className'] ?? '';
-		$classes_and_styles = StyleAttributesUtils::get_classes_and_styles_by_attributes( $attributes );
-
-		if ( isset( $attributes['align'] ) ) {
-			$classname .= " align{$attributes['align']}";
-		}
-
-		return sprintf(
-			'<div class="wc-block-%4$s %1$s %2$s">%3$s</div>',
-			esc_attr( $classes_and_styles['classes'] ),
-			esc_attr( $classname ),
-			$content,
-			esc_attr( $this->block_name )
-		);
-	}
-
-	/**
 	 * This renders the content of the block within the wrapper.
 	 *
 	 * @param \WC_Order $order Order object.
+	 * @param string    $permission Permission level for viewing order details.
+	 * @param array     $attributes Block attributes.
 	 * @return string
 	 */
-	protected function render_content( $order ) {
-		$fallback = esc_html__( 'This order has no billing address.', 'woo-gutenberg-products-block' );
-		$address  = $order->get_formatted_billing_address( $fallback );
-		$address  = $address . ( $order->get_billing_phone() ? '<p class="woocommerce-customer-details--phone">' . esc_html( $order->get_billing_phone() ) . '</p>' : '' );
-		$address  = $address . ( $order->get_billing_email() ? '<p class="woocommerce-customer-details--email">' . esc_html( $order->get_billing_email() ) . '</p>' : '' );
+	protected function render_content( $order, $permission = false, $attributes = [] ) {
+		if ( 'full' !== $permission ) {
+			return '';
+		}
+		$address = $order->get_formatted_billing_address( esc_html__( 'This order has no billing address.', 'woo-gutenberg-products-block' ) );
+		$address = $address . ( $order->get_billing_phone() ? '<br><span class="woocommerce-customer-details--phone">' . esc_html( $order->get_billing_phone() ) . '</span>' : '' );
 
-		return '
-			<address>
-				' . wp_kses_post( $address ) . '
-			</address>
-		';
-	}
-
-	/**
-	 * This is what gets rendered when the order does not exist.
-	 *
-	 * @return string
-	 */
-	protected function render_content_fallback() {
-		return '-';
+		return '<address>' . wp_kses_post( $address ) . '</address>';
 	}
 }
