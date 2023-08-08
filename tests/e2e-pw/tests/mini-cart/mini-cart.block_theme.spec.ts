807a140c7fd3af4fc4bfdc96ba04deb74aa6ac42
Luigi <gigitux@gmail.com>
2023-08-08 16:06:24 +0200

    try now

--- tests/e2e-pw/tests/mini-cart/mini-cart.block_theme.spec.ts
+++ tests/e2e-pw/tests/mini-cart/mini-cart.block_theme.spec.ts
@@ -1,28 +1,11 @@
 /**
  * External dependencies
  */
-import { BlockData } from '@woocommerce/e2e-types';
 import { test, expect } from '@woocommerce/e2e-playwright-utils';
 
-const blockData: BlockData = {
-	name: 'woocommerce/mini-cart',
-	mainClass: '.wc-block-mini-cart',
-	selectors: {
-		frontend: {
-			drawer: '.wc-block-mini-cart__drawer',
-			drawerCloseButton: 'button[aria-label="Close"]',
-		},
-		editor: {},
-	},
-};
-
-const getMiniCartButton = async ( { page } ) => {
-	return await page.getByLabel( '0 items in cart, total price of $0.00' );
-};
-
-test.describe( `${ blockData.name } Block`, () => {
-	test( `laky test`, ( { page }, testInfo ) => {
-		if ( testInfo.retry ) {
+test.describe( 'test', () => {
+	test( `flaky test`, ( {}, testInfo ) => {
+		if ( testInfo.retry > 0 ) {
 			expect( true ).toBe( true );
 		}
 		expect( true ).toBe( false );
