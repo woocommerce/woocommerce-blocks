783dc05b81d28390d14ab906695f050e428c899b
Thomas Roberts <thomas.roberts@automattic.com>
2023-08-08 15:40:18 +0100

    Re-add selectAndVerifyShippingOption to checkout block test

--- tests/e2e-pw/tests/cart-checkout/shopper/checkout-block.shopper.block_theme.spec.ts
+++ tests/e2e-pw/tests/cart-checkout/shopper/checkout-block.shopper.block_theme.spec.ts
@@ -3,6 +3,38 @@
  */
 import { test, expect } from '@woocommerce/e2e-playwright-utils';
 import { BASE_URL, FrontendUtils } from '@woocommerce/e2e-utils';
+import { Page } from '@playwright/test';
+
+const selectAndVerifyShippingOption = async (
+	shippingName: string,
+	shippingPrice: string,
+	page: Page
+) => {
+	const isShippingRateSelected = async () => {
+		const shippingLine = page.locator(
+			'.wc-block-components-totals-shipping'
+		);
+
+		const nameCount = await shippingLine.getByText( shippingName ).count();
+		const priceCount = await shippingLine
+			.getByText( shippingPrice )
+			.count();
+		return nameCount > 0 && priceCount > 0;
+	};
+
+	await page.waitForLoadState( 'networkidle' );
+	await page.waitForSelector( '.wc-block-components-radio-control__label' );
+
+	if ( ! ( await isShippingRateSelected() ) ) {
+		const shipping = await page.getByLabel( shippingName );
+		await shipping.click();
+		await page.waitForResponse( ( request ) => {
+			const url = request.url();
+			return url.includes( 'wc/store/v1/batch' );
+		} );
+	}
+	await expect( await isShippingRateSelected() ).toBe( true );
+};
 
 test.describe( 'Shopper → Checkout block', () => {
 	test.beforeAll( async ( { browser } ) => {
@@ -20,7 +52,6 @@ test.describe( 'Shopper → Checkout block', () => {
 
 	test.describe( 'Shipping', () => {
 		test( 'Shopper can switch between free shipping and flat rate shipping', async ( {
-			frontendUtils,
 			page,
 		} ) => {
 			await page.goto( '/checkout', {
@@ -31,13 +62,15 @@ test.describe( 'Shopper → Checkout block', () => {
 			const FLAT_RATE_SHIPPING_NAME = 'Flat rate shipping';
 			const FLAT_RATE_SHIPPING_PRICE = '$10.00';
 
-			await frontendUtils.selectAndVerifyShippingOption(
+			await selectAndVerifyShippingOption(
 				FREE_SHIPPING_NAME,
-				FREE_SHIPPING_PRICE
+				FREE_SHIPPING_PRICE,
+				page
 			);
-			await frontendUtils.selectAndVerifyShippingOption(
+			await selectAndVerifyShippingOption(
 				FLAT_RATE_SHIPPING_NAME,
-				FLAT_RATE_SHIPPING_PRICE
+				FLAT_RATE_SHIPPING_PRICE,
+				page
 			);
 		} );
 	} );
