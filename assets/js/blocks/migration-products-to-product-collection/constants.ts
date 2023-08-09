03ec500a6f3a46d5470c969d1714e88f4f80f027
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-08-09 16:18:17 +0200

    Update Upgrade Notice visibility conditions

--- assets/js/blocks/migration-products-to-product-collection/constants.ts
+++ assets/js/blocks/migration-products-to-product-collection/constants.ts
@@ -4,9 +4,9 @@
 import type { UpgradeNoticeStatus } from './types';
 
 export const AUTO_REPLACE_PRODUCTS_WITH_PRODUCT_COLLECTION = false;
-export const MANUAL_REPLACE_PRODUCTS_WITH_PRODUCT_COLLECTION = true;
-export const HOURS_TO_DISPLAY_UPGRADE_NOTICE = 24;
-export const UPGRADE_NOTICE_DISPLAY_COUNT_THRESHOLD = 5;
+export const MANUAL_REPLACE_PRODUCTS_WITH_PRODUCT_COLLECTION = false;
+export const HOURS_TO_DISPLAY_UPGRADE_NOTICE = 72;
+export const UPGRADE_NOTICE_DISPLAY_COUNT_THRESHOLD = 4;
 export const MIGRATION_STATUS_LS_KEY =
 	'wc-blocks_upgraded-products-to-product-collection';
 export const INITIAL_STATUS_LS_VALUE = 'notseen';
