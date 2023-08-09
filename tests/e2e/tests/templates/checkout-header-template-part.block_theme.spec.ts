2088b4a12b7f76e287d4e1220687d7504aa5b7d8
Tung Du <dinhtungdu@gmail.com>
2023-08-09 18:22:42 +0700

    udpate button label

--- tests/e2e/tests/templates/checkout-header-template-part.block_theme.spec.ts
+++ tests/e2e/tests/templates/checkout-header-template-part.block_theme.spec.ts
@@ -15,7 +15,7 @@ test.afterAll( async ( { requestUtils } ) => {
 test.describe( 'Test the checkout header template part', async () => {
 	test( 'Template can be opened in the site editor', async ( { page } ) => {
 		await page.goto( '/wp-admin/site-editor.php' );
-		await page.getByRole( 'button', { name: /Template Parts/i } ).click();
+		await page.getByRole( 'button', { name: /Patterns/i } ).click();
 		await page.getByRole( 'button', { name: /Checkout Header/i } ).click();
 
 		const editButton = page.getByRole( 'button', { name: /Edit/i } );
