4ba7e0f29954a3ac2d610da9f5fd9a8b58c1fe0f
Mike Jolley <mike.jolley@me.com>
2023-08-11 13:45:09 +0100

    Introduce shipping wrapper based on billing wrapper
    Closes #10053

--- assets/js/blocks/order-confirmation/shipping-address/index.tsx
+++ assets/js/blocks/order-confirmation/shipping-address/index.tsx
@@ -1,9 +1,8 @@
 /**
  * External dependencies
  */
-import { registerBlockType } from '@wordpress/blocks';
-import { Icon } from '@wordpress/icons';
-import { totals } from '@woocommerce/icons';
+import { registerBlockType, type BlockConfiguration } from '@wordpress/blocks';
+import { Icon, mapMarker } from '@wordpress/icons';
 
 /**
  * Internal dependencies
@@ -12,20 +11,23 @@ import metadata from './block.json';
 import edit from './edit';
 import './style.scss';
 
-registerBlockType( metadata, {
-	icon: {
-		src: (
-			<Icon
-				icon={ totals }
-				className="wc-block-editor-components-block-icon"
-			/>
-		),
-	},
-	attributes: {
-		...metadata.attributes,
-	},
-	edit,
-	save() {
-		return null;
-	},
-} );
+registerBlockType(
+	metadata as BlockConfiguration,
+	{
+		icon: {
+			src: (
+				<Icon
+					icon={ mapMarker }
+					className="wc-block-editor-components-block-icon"
+				/>
+			),
+		},
+		attributes: {
+			...metadata.attributes,
+		},
+		edit,
+		save() {
+			return null;
+		},
+	} as unknown as Partial< BlockConfiguration >
+);
