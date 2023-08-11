d1f10dac002e6f23a95d9497703b03bbba5be253
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-08-11 10:33:06 +0200

    Add E2E tests for Product Collection with Compatibility Layer

--- tests/e2e/tests/product-collection/product-collection.page.ts
+++ tests/e2e/tests/product-collection/product-collection.page.ts
@@ -2,8 +2,9 @@
  * External dependencies
  */
 import { Locator, Page } from '@playwright/test';
-import { TemplateApiUtils } from '@woocommerce/e2e-utils';
+import { TemplateApiUtils, EditorUtils } from '@woocommerce/e2e-utils';
 import { Editor, Admin } from '@wordpress/e2e-test-utils-playwright';
+import { BlockRepresentation } from '@wordpress/e2e-test-utils-playwright/build-types/editor/insert-block';
 
 export const SELECTORS = {
 	productTemplate: '.wc-block-product-template',
@@ -28,6 +29,8 @@ export const SELECTORS = {
 	onSaleControlLabel: 'Show only products on sale',
 	inheritQueryFromTemplateControl:
 		'.wc-block-product-collection__inherit-query-control',
+	productSearchLabel: 'Search',
+	productSearchButton: '.wp-block-search__button wp-element-button',
 };
 
 class ProductCollectionPage {
@@ -36,6 +39,7 @@ class ProductCollectionPage {
 	private admin: Admin;
 	private editor: Editor;
 	private templateApiUtils: TemplateApiUtils;
+	private editorUtils: EditorUtils;
 	productTemplate!: Locator;
 	products!: Locator;
 	productImages!: Locator;
@@ -49,20 +53,23 @@ class ProductCollectionPage {
 		admin,
 		editor,
 		templateApiUtils,
+		editorUtils,
 	}: {
 		page: Page;
 		admin: Admin;
 		editor: Editor;
 		templateApiUtils: TemplateApiUtils;
+		editorUtils: EditorUtils;
 	} ) {
 		this.page = page;
 		this.admin = admin;
 		this.editor = editor;
 		this.templateApiUtils = templateApiUtils;
+		this.editorUtils = editorUtils;
 	}
 
 	async createNewPostAndInsertBlock() {
-		await this.admin.createNewPost( { legacyCanvas: true } );
+		await this.admin.createNewPost();
 		await this.editor.insertBlock( {
 			name: this.BLOCK_NAME,
 		} );
@@ -73,11 +80,35 @@ class ProductCollectionPage {
 		await this.editor.publishPost();
 		const url = new URL( this.page.url() );
 		const postId = url.searchParams.get( 'post' );
-		await this.page.goto( `/?p=${ postId }`, { waitUntil: 'commit' } );
+		await this.page.goto( `/?p=${ postId }` );
 		await this.refreshLocators( 'frontend' );
 	}
 
-	async goToProductCatalogAndInsertBlock() {
+	async replaceProductsWithProductCollectionInTemplate( template: string ) {
+		await this.templateApiUtils.revertTemplate( template );
+		await this.admin.visitSiteEditor( {
+			postId: template,
+			postType: 'wp_template',
+		} );
+		await this.editorUtils.enterEditMode();
+
+		await this.editorUtils.replaceBlockByBlockName(
+			'core/query',
+			'woocommerce/product-collection'
+		);
+
+		await this.editor.saveSiteEditorEntities();
+	}
+
+	async goToProductCatalogFrontend() {
+		await this.page.goto( `/shop` );
+	}
+
+	async goToProductCatalogAndInsertBlock(
+		block: BlockRepresentation = {
+			name: this.BLOCK_NAME,
+		}
+	) {
 		await this.templateApiUtils.revertTemplate(
 			'woocommerce/woocommerce//archive-product'
 		);
@@ -89,10 +120,16 @@ class ProductCollectionPage {
 
 		await this.editor.canvas.click( 'body' );
 
-		await this.editor.insertBlock( {
-			name: this.BLOCK_NAME,
-		} );
+		await this.editor.insertBlock( block );
 		await this.editor.openDocumentSettingsSidebar();
+		await this.editor.saveSiteEditorEntities();
+	}
+
+	async searchProducts( phrase: string ) {
+		await this.page
+			.getByLabel( SELECTORS.productSearchLabel )
+			.fill( phrase );
+		await this.page.locator( SELECTORS.productSearchButton ).click();
 	}
 
 	async addFilter(
@@ -304,10 +341,14 @@ class ProductCollectionPage {
 	/**
 	 * Locators
 	 */
-	async locateSidebarSettings() {
+	locateSidebarSettings() {
 		return this.page.getByRole( 'region', {
 			name: 'Editor settings',
 		} );
+	}
+
+	locateByTestId( testId: string ) {
+		return this.page.getByTestId( testId );
 	}
 
 	/**
