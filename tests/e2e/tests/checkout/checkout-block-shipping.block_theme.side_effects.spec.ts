1139592f3d3f1efa182a58207a94f3a31164457c
Thomas Roberts <thomas.roberts@automattic.com>
2023-08-14 14:12:19 +0100

    Make test check for setting different addresses and shipping in same run

--- tests/e2e/tests/checkout/checkout-block-shipping.block_theme.side_effects.spec.ts
+++ tests/e2e/tests/checkout/checkout-block-shipping.block_theme.side_effects.spec.ts
@@ -18,23 +18,25 @@ const test = base.extend< { pageObject: CheckoutPage } >( {
 } );
 
 test.describe( 'Shopper → Checkout block → Shipping', () => {
-	test.beforeEach( async ( { frontendUtils } ) => {
-		await frontendUtils.emptyCart();
-		await frontendUtils.goToShop();
-		await frontendUtils.addToCart( 'Beanie' );
-		await frontendUtils.goToCheckout();
-	} );
-
 	const FREE_SHIPPING_NAME = 'Free shipping';
 	const FREE_SHIPPING_PRICE = '$0.00';
 	const FLAT_RATE_SHIPPING_NAME = 'Flat rate shipping';
 	const FLAT_RATE_SHIPPING_PRICE = '$10.00';
 
-	test( 'Shopper can choose free shipping', async ( {
+	test.use( {
+		storageState: process.env.CUSTOMERSTATE,
+	} );
+
+	test( 'Shopper can choose free shipping, flat rate shipping, and can have different billing and shipping addresses', async ( {
 		pageObject,
+		frontendUtils,
 		page,
 	} ) => {
-		expect(
+		await frontendUtils.emptyCart();
+		await frontendUtils.goToShop();
+		await frontendUtils.addToCart( 'Beanie' );
+		await frontendUtils.goToCheckout();
+		await expect(
 			await pageObject.selectAndVerifyShippingOption(
 				FREE_SHIPPING_NAME,
 				FREE_SHIPPING_PRICE
@@ -42,21 +44,40 @@ test.describe( 'Shopper → Checkout block → Shipping', () => {
 		).toBe( true );
 		await pageObject.fillInCheckoutWithTestData();
 		await pageObject.placeOrder();
+		await pageObject.verifyAddressDetails( 'billing' );
+		await pageObject.verifyAddressDetails( 'shipping' );
 		await expect( page.getByText( FREE_SHIPPING_NAME ) ).toBeVisible();
-	} );
 
-	test( 'Shopper can choose flat rate shipping', async ( {
-		pageObject,
-		page,
-	} ) => {
-		expect(
+		await frontendUtils.goToShop();
+		await frontendUtils.addToCart( 'Beanie' );
+		await frontendUtils.goToCheckout();
+		await expect(
 			await pageObject.selectAndVerifyShippingOption(
 				FLAT_RATE_SHIPPING_NAME,
 				FLAT_RATE_SHIPPING_PRICE
 			)
 		).toBe( true );
+		await page.getByLabel( 'Use same address for billing' ).uncheck();
 		await pageObject.fillInCheckoutWithTestData();
+		const overrideBillingDetails = {
+			firstname: 'Juan',
+			lastname: 'Perez',
+			addressfirstline: '123 Test Street',
+			addresssecondline: 'Apartment 6',
+			country: 'ES',
+			city: 'Madrid',
+			postcode: '08830',
+			state: 'M',
+			phone: '0987654321',
+			email: 'juan.perez@test.com',
+		};
+		await pageObject.fillBillingDetails( overrideBillingDetails );
 		await pageObject.placeOrder();
+		await pageObject.verifyAddressDetails(
+			'billing',
+			overrideBillingDetails
+		);
+		await pageObject.verifyAddressDetails( 'shipping' );
 		await expect( page.getByText( FLAT_RATE_SHIPPING_NAME ) ).toBeVisible();
 	} );
 } );
