b0146b37bf91feb7c3fe59ef1ffbf4cac245b1f8
Tung Du <dinhtungdu@gmail.com>
2023-08-16 12:36:25 +0700

    improve folder structure

--- tests/e2e/bin/scripts/attributes.sh
+++ tests/e2e/bin/scripts/attributes.sh
@@ -1,23 +1,11 @@
 #!/usr/bin/env bash
 
-###################################################################################################
-# If no attributes exist, otherwiese create them
-###################################################################################################
+wp wc product_attribute create \
+	--name=Color \
+	--slug=pa_color \
+	--user=1
 
-attributes=$(wp wc product_attribute list --format=json --user=1)
-
-if [ -z "$attributes" ] || [ "$attributes" == "[]" ]; then
-    pa_color=$(wp wc product_attribute create \
-        --name=Color \
-        --slug=pa_color \
-        --user=1 \
-        --porcelain \
-    )
-
-    pa_size=$(wp wc product_attribute create \
-        --name=Size \
-        --slug=pa_size \
-        --user=1 \
-        --porcelain \
-    )
-fi
+wp wc product_attribute create \
+	--name=Size \
+	--slug=pa_size \
+	--user=1
