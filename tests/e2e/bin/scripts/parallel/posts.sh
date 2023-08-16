d562e7cfaa3b1005b383112490e84d6cc45b5cdb
Tung Du <dinhtungdu@gmail.com>
2023-08-16 13:25:55 +0700

    fix test posts title and slug

--- tests/e2e/bin/scripts/parallel/posts.sh
+++ tests/e2e/bin/scripts/parallel/posts.sh
@@ -3,11 +3,11 @@
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../../posts/ && pwd)"
 
 find $script_dir -maxdepth 1 -type f -print0 | while read -d $'\0' file; do
-	name=$(basename -s .html $file)
-	title=$(IFS=- read -ra str <<<"$name"; printf '%s' "${str[*]^}")
+	name=$(basename -s .html $file) # Remove .html extension
+	title=$(IFS=- read -ra str <<<"$name"; printf '%s' "${str[*]^}") # Capitalize each word
 	wp post create \
 		--post_status=publish \
 		--post_author=1 \
-		--post_title="$title" \
+		--post_title="$title block" \
 		$file
 done
