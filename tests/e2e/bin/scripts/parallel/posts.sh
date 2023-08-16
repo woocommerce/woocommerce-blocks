6c59de420839fc324ea00bbeb9f5a709b0328368
Tung Du <dinhtungdu@gmail.com>
2023-08-16 17:01:39 +0700

    creating test posts in parallel too

--- tests/e2e/bin/scripts/parallel/posts.sh
+++ tests/e2e/bin/scripts/parallel/posts.sh
@@ -2,12 +2,16 @@
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ../../posts/ && pwd)"
 
-find $script_dir -maxdepth 1 -type f -print0 | while read -d $'\0' file; do
-	name=$(basename -s .html $file) # Remove .html extension
+function create_post {
+	name=$(basename -s .html $1) # Remove .html extension
 	title=$(IFS=- read -ra str <<<"$name"; printf '%s' "${str[*]^}") # Capitalize each word
 	wp post create \
 		--post_status=publish \
 		--post_author=1 \
 		--post_title="$title block" \
-		$file
-done
+		$1
+}
+export -f create_post
+
+find $script_dir -maxdepth 1 -type f | xargs -P5 -n1 bash -c 'create_post "$@"' _
+
