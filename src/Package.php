8140b561293a2d2aad2d25f846e4b02ee54ac70c
Niels Lange <info@nielslange.de>
2023-08-17 16:57:38 +0200

    Update version number

--- src/Package.php
+++ src/Package.php
@@ -109,7 +109,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '10.9.0';
+					$version = '10.9.1';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
