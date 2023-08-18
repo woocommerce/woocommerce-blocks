4af2ca891dd3780a3df59d26e6c22efa7e8d3a6b
Tung Du <dinhtungdu@gmail.com>
2023-08-18 15:00:41 +0700

    remove decorator

--- storybook/main.js
+++ storybook/main.js
@@ -1,9 +1,11 @@
+const { readFileSync } = require( 'fs' );
+const { loadCsf } = require( '@storybook/csf-tools' );
+
 module.exports = {
 	stories: [
 		// WooCommerce Blocks stuff (anywhere in repo!)
-		'../assets/js/base/components/button/stories/index.stories.tsx',
-		// '../assets/js/**/stories/*.@(js|jsx|ts|tsx)',
-		// '../packages/**/stories/*.@(js|jsx|ts|tsx)',
+		'../assets/js/**/stories/*.@(jsx|tsx)',
+		'../packages/**/stories/*.@(jsx|tsx)',
 	],
 	addons: [
 		'@storybook/addon-essentials',
@@ -22,6 +24,20 @@ module.exports = {
 	},
 	docs: {
 		autodocs: 'tag',
+	},
+	storyIndexers: ( indexers ) => {
+		const indexer = async ( fileName, opts ) => {
+			const code = readFileSync( fileName, { encoding: 'utf-8' } );
+			return loadCsf( code, { ...opts, fileName } ).parse();
+		};
+
+		return [
+			{
+				test: /stories\/(.+).tsx$/,
+				indexer,
+			},
+			...( indexers || [] ),
+		];
 	},
 	// webpackFinal field was added in following PR: https://github.com/woocommerce/woocommerce-blocks/pull/7514
 	// This fixes "storybook build issue" related to framer-motion library.
