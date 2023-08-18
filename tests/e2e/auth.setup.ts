f87427afdba1169ff8ae53365b4b6f9c3312bbcf
Saad Tarhi <saad.trh@gmail.com>
2023-08-18 15:32:52 +0100

    Update comments
    Co-authored-by: Thomas Roberts <5656702+opr@users.noreply.github.com>

--- tests/e2e/auth.setup.ts
+++ tests/e2e/auth.setup.ts
@@ -57,8 +57,6 @@ setup( 'authenticate as customer', async ( { page } ) => {
 			.fill( customer.username );
 		await page.getByLabel( 'Password' ).fill( customer.password );
 		await page.getByRole( 'button', { name: 'Log in' } ).click();
-		// Wait until the page receives the cookies.
-		//
 		// Sometimes login flow sets cookies in the process of several redirects.
 		// Wait for the final URL to ensure that the cookies are actually set.
 		await page.waitForURL( '/my-account/' );
