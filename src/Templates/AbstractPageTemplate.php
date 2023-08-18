aeb4ea0fd0db0b9d60fd64c9dae0cf077c833860
Mike Jolley <mike.jolley@me.com>
2023-08-18 11:54:55 +0100

    Remove redirect and custom title

--- src/Templates/AbstractPageTemplate.php
+++ src/Templates/AbstractPageTemplate.php
@@ -23,7 +23,6 @@ abstract class AbstractPageTemplate {
 	 */
 	protected function init() {
 		add_filter( 'page_template_hierarchy', array( $this, 'page_template_hierarchy' ), 1 );
-		add_action( 'current_screen', array( $this, 'page_template_editor_redirect' ) );
 		add_filter( 'pre_get_document_title', array( $this, 'page_template_title' ) );
 	}
 
@@ -102,7 +101,7 @@ abstract class AbstractPageTemplate {
 	 * @return string
 	 */
 	public function page_template_title( $title ) {
-		if ( $this->is_active_template() ) {
+		if ( $this->is_active_template() && $this->get_template_title() ) {
 			return $this->get_template_title();
 		}
 		return $title;
