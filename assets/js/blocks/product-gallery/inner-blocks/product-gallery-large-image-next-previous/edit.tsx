d93f7f4d7fcd78d5162cc99225b8adecb71d7749
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-08-21 23:29:00 +0200

    Product Gallery Next Previous: Fix block crashing in the editor when vertical alignment has not been customized yet

--- assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image-next-previous/edit.tsx
+++ assets/js/blocks/product-gallery/inner-blocks/product-gallery-large-image-next-previous/edit.tsx
@@ -38,7 +38,7 @@ export const Edit = ( {
 			width: '100%',
 			height: '100%',
 			alignItems: getAlignmentStyle(
-				attributes.layout.verticalAlignment
+				attributes.layout?.verticalAlignment
 			),
 		},
 	} );
