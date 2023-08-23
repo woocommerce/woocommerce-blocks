4bcccbf6b35e1fb32b84e6ace27c5630e0e288ec
Thomas Roberts <thomas.roberts@automattic.com>
2023-08-23 12:05:13 +0100

    Log out before each authentication setup step

--- tests/e2e/auth.setup.ts
+++ tests/e2e/auth.setup.ts
@@ -9,94 +9,78 @@ import { adminFile, customerFile, guestFile } from '@woocommerce/e2e-utils';
  */
 import { customer, admin } from './test-data/data/data';
 
-setup( 'authenticate as admin', async ( { page } ) => {
-	await page.goto( '/my-account' );
-	const isLoggedIn = await page
-		.getByLabel( 'Username or email address' )
-		.isHidden();
-
-	if ( isLoggedIn ) {
-		await page
+setup.describe( 'Authentication setup', () => {
+	// Check if logged in, then log out of the account before each test.
+	setup.beforeEach( async ( { page } ) => {
+		await page.goto( '/my-account' );
+		const logoutLinkVisible = await page
 			.getByRole( 'paragraph' )
 			.filter( { hasText: /Hello .* \(not .*? Log out\)/ } )
-			.getByRole( 'link', { name: 'Log out' } )
-			.click();
-	}
+			.isVisible();
 
-	await page.getByLabel( 'Username or email address' ).fill( admin.username );
-	await page.getByLabel( 'Password' ).fill( admin.password );
-	await page.getByRole( 'button', { name: 'Log in' } ).click();
-	// Sometimes login flow sets cookies in the process of several redirects.
-	// Wait for the final URL to ensure that the cookies are actually set.
-	await page.waitForURL( '/my-account/' );
+		if ( logoutLinkVisible ) {
+			await page
+				.getByRole( 'paragraph' )
+				.filter( { hasText: /Hello .* \(not .*? Log out\)/ } )
+				.getByRole( 'link', { name: 'Log out' } )
+				.click();
+		}
+		await page.goto( '/my-account' );
+	} );
 
-	await expect(
-		page
-			.getByRole( 'list' )
-			.filter( {
-				hasText:
-					'Dashboard Orders Downloads Addresses Account details Log out',
-			} )
-			.getByRole( 'link', { name: 'Log out' } )
-	).toBeVisible();
-
-	await page.context().storageState( { path: adminFile } );
-} );
-
-setup( 'authenticate as customer', async ( { page } ) => {
-	await page.goto( '/my-account' );
-	const isLoggedIn = await page
-		.getByLabel( 'Username or email address' )
-		.isHidden();
-
-	if ( isLoggedIn ) {
+	setup( 'authenticate as admin', async ( { page } ) => {
 		await page
-			.getByRole( 'paragraph' )
-			.filter( { hasText: /Hello .* \(not .*? Log out\)/ } )
-			.getByRole( 'link', { name: 'Log out' } )
-			.click();
-	}
-	await page
-		.getByLabel( 'Username or email address' )
-		.fill( customer.username );
-	await page.getByLabel( 'Password' ).fill( customer.password );
-	await page.getByRole( 'button', { name: 'Log in' } ).click();
-	// Sometimes login flow sets cookies in the process of several redirects.
-	// Wait for the final URL to ensure that the cookies are actually set.
-	await page.waitForURL( '/my-account/' );
+			.getByLabel( 'Username or email address' )
+			.fill( admin.username );
+		await page.getByLabel( 'Password' ).fill( admin.password );
+		await page.getByRole( 'button', { name: 'Log in' } ).click();
+		// Sometimes login flow sets cookies in the process of several redirects.
+		// Wait for the final URL to ensure that the cookies are actually set.
+		await page.waitForURL( '/my-account/' );
 
-	await expect(
-		page
-			.getByRole( 'list' )
-			.filter( {
-				hasText:
-					'Dashboard Orders Downloads Addresses Account details Log out',
-			} )
-			.getByRole( 'link', { name: 'Log out' } )
-	).toBeVisible();
+		await expect(
+			page
+				.getByRole( 'list' )
+				.filter( {
+					hasText:
+						'Dashboard Orders Downloads Addresses Account details Log out',
+				} )
+				.getByRole( 'link', { name: 'Log out' } )
+		).toBeVisible();
 
-	await page.context().storageState( { path: customerFile } );
-} );
+		await page.context().storageState( { path: adminFile } );
+	} );
 
-setup( 'authenticate as guest', async ( { page } ) => {
-	await page.goto( '/my-account' );
-	const isLoggedIn = await page
-		.getByLabel( 'Username or email address' )
-		.isHidden();
-
-	if ( isLoggedIn ) {
+	setup( 'authenticate as customer', async ( { page } ) => {
 		await page
-			.getByRole( 'paragraph' )
-			.filter( { hasText: /Hello .* \(not .*? Log out\)/ } )
-			.getByRole( 'link', { name: 'Log out' } )
-			.click();
-	}
+			.getByLabel( 'Username or email address' )
+			.fill( customer.username );
+		await page.getByLabel( 'Password' ).fill( customer.password );
+		await page.getByRole( 'button', { name: 'Log in' } ).click();
+		// Sometimes login flow sets cookies in the process of several redirects.
+		// Wait for the final URL to ensure that the cookies are actually set.
+		await page.waitForURL( '/my-account/' );
 
-	await expect(
-		page.getByLabel( 'Username or email address' )
-	).toBeVisible();
+		await expect(
+			page
+				.getByRole( 'list' )
+				.filter( {
+					hasText:
+						'Dashboard Orders Downloads Addresses Account details Log out',
+				} )
+				.getByRole( 'link', { name: 'Log out' } )
+		).toBeVisible();
 
-	// End of authentication steps.
+		await page.context().storageState( { path: customerFile } );
+	} );
 
-	await page.context().storageState( { path: guestFile } );
+	setup( 'authenticate as guest', async ( { page } ) => {
+		await expect(
+			page.getByLabel( 'Username or email address' )
+		).toBeVisible();
+
+		// End of authentication steps.
+
+		await page.context().storageState( { path: guestFile } );
+	} );
 } );
