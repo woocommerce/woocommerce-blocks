9229b0b457e9a134fee91adb274a662e7fd48719
Saad Tarhi <saad.trh@gmail.com>
2023-08-29 07:41:32 +0100

    Add E2E pw authentication util functions

--- tests/e2e/utils/frontend/frontend-utils.page.ts
+++ tests/e2e/utils/frontend/frontend-utils.page.ts
@@ -3,6 +3,12 @@
  */
 import { Page, Locator } from '@playwright/test';
 import { RequestUtils } from '@wordpress/e2e-test-utils-playwright';
+import { expect } from '@woocommerce/e2e-playwright-utils';
+
+/**
+ * Internal dependencies
+ */
+import { customer } from '../../test-data/data/data';
 
 export class FrontendUtils {
 	page: Page;
@@ -121,5 +127,60 @@ export class FrontendUtils {
 		await locator.click();
 		await locator.press( 'End' );
 		await locator.press( 'Shift+Home' );
+	}
+
+	async gotoMyAccount() {
+		await this.page.goto( '/my-account', {
+			waitUntil: 'commit',
+		} );
+	}
+
+	async isLoggedIn() {
+		await this.gotoMyAccount();
+		await expect(
+			this.page.getByRole( 'heading', { name: 'My account' } )
+		).toBeVisible();
+		const isLoginFormVisible = await this.page
+			.locator( 'form.woocommerce-form-login' )
+			.isVisible();
+
+		return ! isLoginFormVisible;
+	}
+
+	async login() {
+		const isLoggedIn = await this.isLoggedIn();
+		if ( isLoggedIn ) {
+			return;
+		}
+
+		await this.gotoMyAccount();
+		await expect(
+			this.page.getByRole( 'heading', { name: 'My account' } )
+		).toBeVisible();
+		await this.page
+			.locator( 'input[name="username"]' )
+			.fill( customer.username );
+		await this.page
+			.locator( 'input[name="password"]' )
+			.fill( customer.password );
+		await this.page.locator( 'text=Log In' ).click();
+		// eslint-disable-next-line playwright/no-networkidle
+		await this.page.waitForLoadState( 'networkidle' );
+	}
+
+	async logout() {
+		await this.gotoMyAccount();
+		await expect(
+			this.page.getByRole( 'heading', { name: 'My account' } )
+		).toBeVisible();
+		// await this.page.locator( 'text=Log out' ).click();
+		await this.page
+			.getByRole( 'list' )
+			.filter( {
+				hasText:
+					'Dashboard Orders Downloads Addresses Account details Log out',
+			} )
+			.getByRole( 'link', { name: 'Log out' } )
+			.click();
 	}
 }
