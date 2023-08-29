a54ed7e036cbde2201cb1b50b93a01e27cf04e56
Tung Du <dinhtungdu@gmail.com>
2023-08-29 18:06:31 +0700

    Fix: load blocks style separately for classic themes

--- src/BlockTypes/AbstractBlock.php
+++ src/BlockTypes/AbstractBlock.php
@@ -210,6 +210,35 @@ abstract class AbstractBlock {
 		}
 
 		$metadata_path = $this->asset_api->get_block_metadata_path( $this->block_name );
+
+		/**
+		 * We want to load blocks style separatedly, always, for every themes.
+		 * When the core assets are loaded separately, other blocks' styles get
+		 * enqueue separately too. Thus we only need to handle the remaining
+		 * case.
+		 */
+		if (
+			$block_settings['style'] &&
+			(
+				! function_exists( 'wp_should_load_separate_core_block_assets' ) ||
+				! wp_should_load_separate_core_block_assets()
+			)
+		) {
+			$style_handles           = $block_settings['style'];
+			$block_settings['style'] = null;
+			add_filter(
+				'render_block',
+				function( $html, $block ) use ( $block_name, $style_handles ) {
+					if ( $block['blockName'] === $this->get_block_type() ) {
+						array_map( 'wp_enqueue_style', $style_handles );
+					}
+					return $html;
+				},
+				10,
+				2
+			);
+		}
+
 		// Prefer to register with metadata if the path is set in the block's class.
 		if ( ! empty( $metadata_path ) ) {
 			register_block_type_from_metadata(
