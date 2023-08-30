f10424ac3231aeeab19d31b2bf09eee7b932a63b
Luigi <gigitux@gmail.com>
2023-08-30 09:47:37 +0200

    fix TS error

--- assets/js/atomic/blocks/product-elements/sale-badge/support.ts
+++ assets/js/atomic/blocks/product-elements/sale-badge/support.ts
@@ -7,7 +7,7 @@ import { __experimentalGetSpacingClassesAndStyles } from '@wordpress/block-edito
 
 export const supports = {
 	html: false,
-	align: [ 'left', 'right', 'center' ],
+	align: true,
 	...( isFeaturePluginBuild() && {
 		color: {
 			gradients: true,
