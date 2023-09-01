78f52f28b1d50698371d0a829ff16c96fad90974
Thomas Roberts <thomas.roberts@automattic.com>
2023-09-01 14:20:28 +0100

    Add checkout info to WC Settings

--- src/Domain/Services/JetpackWooCommerceAnalytics.php
+++ src/Domain/Services/JetpackWooCommerceAnalytics.php
@@ -79,6 +79,7 @@ class JetpackWooCommerceAnalytics {
 		}
 		$this->register_assets();
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_script_data' ) );
 	}
 
 	/**
@@ -101,5 +102,120 @@ class JetpackWooCommerceAnalytics {
 			return;
 		}
 		wp_enqueue_script( 'wc-blocks-jetpack-woocommerce-analytics' );
+	}
+
+	/**
+	 * Enqueue the Google Tag Manager script if prerequisites are met.
+	 */
+	public function register_script_data() {
+		$this->asset_data_registry->add( 'wc-blocks-jetpack-woocommerce-analytics_cart_checkout_info', $this->get_cart_checkout_info() );
+	}
+
+	/**
+	 * Get info about the cart & checkout pages, in particular
+	 * whether the store is using shortcodes or Gutenberg blocks.
+	 * This info is cached in a transient.
+	 *
+	 * Note: similar code is in a WooCommerce core PR:
+	 * https://github.com/woocommerce/woocommerce/pull/25932
+	 *
+	 * @return array
+	 */
+	public function get_cart_checkout_info() {
+		$transient_name = 'woocommerce_blocks_jetpack_woocommerce_analytics_cart_checkout_info_cache';
+
+		$info = get_transient( $transient_name );
+
+		// Return cached data early to prevent additional processing, the transient lasts for 1 day.
+		if ( false && false !== $info ) {
+			return $info;
+		}
+
+		$cart_template        = null;
+		$checkout_template    = null;
+		$cart_template_id     = null;
+		$checkout_template_id = null;
+		$templates            = $this->block_templates_controller->get_block_templates( array( 'cart', 'checkout' ) );
+
+		foreach ( $templates as $template ) {
+			if ( 'cart' === $template->slug ) {
+				$cart_template_id = ( $template->id );
+				continue;
+			}
+			if ( 'checkout' === $template->slug ) {
+				$checkout_template_id = ( $template->id );
+			}
+		}
+
+		// Get the template and its contents from the IDs we found above.
+		if ( function_exists( 'get_block_template' ) ) {
+			$cart_template     = get_block_template( $cart_template_id );
+			$checkout_template = get_block_template( $checkout_template_id );
+		}
+
+		if ( function_exists( 'gutenberg_get_block_template' ) ) {
+			$cart_template     = get_block_template( $cart_template_id );
+			$checkout_template = get_block_template( $checkout_template_id );
+		}
+
+		// Update the info transient with data we got from the templates, if the site isn't using WC Blocks we
+		// won't be doing this so no concern about overwriting.
+		$info = array(
+			'cart_page_contains_cart_block'             => str_contains( $cart_template->content, '<!-- wp:woocommerce/cart' ),
+			'cart_page_contains_cart_shortcode'         => str_contains( $cart_template->content, '[woocommerce_cart]' ),
+			'checkout_page_contains_checkout_block'     => str_contains( $checkout_template->content, '<!-- wp:woocommerce/checkout' ),
+			'checkout_page_contains_checkout_shortcode' => str_contains( $checkout_template->content, '[woocommerce_checkout]' ),
+			'additional_blocks_on_cart_page'            => $this->get_additional_blocks(
+				$cart_template->content,
+				array( 'woocommerce/cart' )
+			),
+			'additional_blocks_on_checkout_page'        => $this->get_additional_blocks(
+				$checkout_template->content,
+				array( 'woocommerce/checkout' )
+			),
+		);
+		set_transient( $transient_name, $info, DAY_IN_SECONDS );
+		return $info;
+	}
+
+	/**
+	 * Get the additional blocks used in a post or template.
+	 *
+	 * @param string $content The post content.
+	 * @param array  $exclude The blocks to exclude.
+	 *
+	 * @return array The additional blocks.
+	 */
+	private function get_additional_blocks( $content, $exclude = array() ) {
+		$parsed_blocks = parse_blocks( $content );
+		return $this->get_nested_blocks( $parsed_blocks, $exclude );
+	}
+
+	/**
+	 * Get the nested blocks from a block array.
+	 *
+	 * @param array    $blocks  The blocks array to find nested blocks inside.
+	 * @param string[] $exclude Blocks to exclude, won't find nested blocks within any of the supplied blocks.
+	 *
+	 * @return array
+	 */
+	private function get_nested_blocks( $blocks, $exclude = array() ) {
+		if ( ! is_array( $blocks ) ) {
+			return array();
+		}
+		$additional_blocks = array();
+		foreach ( $blocks as $block ) {
+			if ( ! isset( $block['blockName'] ) ) {
+				continue;
+			}
+			if ( in_array( $block['blockName'], $exclude, true ) ) {
+				continue;
+			}
+			if ( is_array( $block['innerBlocks'] ) ) {
+				$additional_blocks = array_merge( $additional_blocks, self::get_nested_blocks( $block['innerBlocks'], $exclude ) );
+			}
+			$additional_blocks[] = $block['blockName'];
+		}
+		return $additional_blocks;
 	}
 }
