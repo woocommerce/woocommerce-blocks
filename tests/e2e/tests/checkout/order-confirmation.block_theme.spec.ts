f8f1816534b1ab3cffb64a8ee6c721aaa0d2cded
Saad Tarhi <saad.trh@gmail.com>
2023-09-08 08:56:57 +0100

    Add tests for logged in user

--- tests/e2e/tests/checkout/order-confirmation.block_theme.spec.ts
+++ tests/e2e/tests/checkout/order-confirmation.block_theme.spec.ts
@@ -10,9 +10,32 @@ import {
 	FREE_SHIPPING_NAME,
 	FREE_SHIPPING_PRICE,
 	SIMPLE_PHYSICAL_PRODUCT_NAME,
-	SIMPLE_VIRTUAL_PRODUCT_NAME,
 } from './constants';
 import { CheckoutPage } from './checkout.page';
+
+const testData = {
+	firstname: 'John',
+	lastname: 'Doe',
+	addressfirstline: '123 Easy Street',
+	addresssecondline: 'Testville',
+	country: 'United States (US)',
+	city: 'New York',
+	state: 'New York',
+	postcode: '90210',
+	email: 'john.doe@test.com',
+	phone: '01234567890',
+};
+
+const {
+	firstname,
+	lastname,
+	addressfirstline,
+	addresssecondline,
+	city,
+	postcode,
+	email,
+	phone,
+} = testData;
 
 const test = base.extend< { pageObject: CheckoutPage } >( {
 	pageObject: async ( { page }, use ) => {
@@ -24,11 +47,23 @@ const test = base.extend< { pageObject: CheckoutPage } >( {
 } );
 
 test.describe( 'Shopper → Order Confirmation', () => {
-	test.use( {
-		storageState: process.env.CUSTOMERSTATE,
+	test.beforeAll( async ( { page, editorUtils } ) => {
+		// Set the site to use the order confirmation template.
+		await page.goto( '/wp-admin/site-editor.php' );
+		await page.getByRole( 'button', { name: /Templates/i } ).click();
+		await page
+			.getByRole( 'button', { name: /Order confirmation/i } )
+			.click();
+		await editorUtils.enterEditMode();
+		await editorUtils.closeWelcomeGuideModal();
+		await editorUtils.transformIntoBlocks();
 	} );
 
-	test.beforeAll( async ( { pageObject, frontendUtils } ) => {
+	test( 'should be rendered on the frontend side', async ( {
+		frontendUtils,
+		pageObject,
+		page,
+	} ) => {
 		await frontendUtils.emptyCart();
 		await frontendUtils.goToShop();
 		await frontendUtils.addToCart( SIMPLE_PHYSICAL_PRODUCT_NAME );
@@ -39,9 +74,30 @@ test.describe( 'Shopper → Order Confirmation', () => {
 				FREE_SHIPPING_PRICE
 			)
 		).toBe( true );
-		await pageObject.fillInCheckoutWithTestData();
+		await pageObject.fillInCheckoutWithTestData( testData );
 		await pageObject.placeOrder();
-		await pageObject.verifyOrderDetailsOnConfirmationPage();
+		await expect(
+			page.getByText( 'Thank you. Your order has been received.' )
+		).toBeVisible();
+		await expect( page.getByText( email ) ).toBeVisible();
+		await expect( page.getByText( FREE_SHIPPING_NAME ) ).toBeVisible();
+		await expect(
+			page.getByText( SIMPLE_PHYSICAL_PRODUCT_NAME )
+		).toBeVisible();
+		await expect(
+			page
+				.getByText(
+					`${ firstname } ${ lastname }${ addressfirstline }${ addresssecondline }${ city }, NY ${ postcode }${ phone }`
+				)
+				.first()
+		).toBeVisible();
+		await expect(
+			page
+				.getByText(
+					`${ firstname } ${ lastname }${ addressfirstline }${ addresssecondline }${ city }, NY ${ postcode }${ phone }`
+				)
+				.nth( 1 )
+		).toBeVisible();
 	} );
 } );
 
