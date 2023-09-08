fbe011371a51f90f82478f91b64d96c6e88de4c3
Tung Du <dinhtungdu@gmail.com>
2023-09-08 18:00:17 +0700

    remove directive from edit component

--- assets/js/blocks/collection-filters/inner-blocks/price-filter/block.tsx
+++ assets/js/blocks/collection-filters/inner-blocks/price-filter/block.tsx
@@ -8,42 +8,23 @@ const Block = ( {
 	maxRange = 0,
 	minPrice = 0,
 	maxPrice = 0,
-	isEditor = false,
 	showInputFields,
 	inlineInput,
 }: BlockProps ) => {
 	const priceMin = showInputFields ? (
-		<input
-			type="text"
-			value={ minPrice }
-			data-wc-bind--value="state.filters.minPrice"
-			data-wc-on--input="actions.filters.setMinPrice"
-			data-wc-on--change="actions.filters.updateProducts"
-			readOnly={ isEditor }
-		/>
+		<input type="text" value={ minPrice } readOnly={ true } />
 	) : (
-		<span data-wc-text="state.filters.minPrice">{ minPrice }</span>
+		<span>{ minPrice }</span>
 	);
 
 	const priceMax = showInputFields ? (
-		<input
-			type="text"
-			value={ maxPrice }
-			data-wc-bind--value="state.filters.maxPrice"
-			data-wc-on--input="actions.filters.setmaxPrice"
-			data-wc-on--change="actions.filters.updateProducts"
-			readOnly={ isEditor }
-		/>
+		<input type="text" value={ maxPrice } readOnly={ true } />
 	) : (
-		<span data-wc-text="state.filters.maxPrice">{ maxPrice }</span>
+		<span>{ maxPrice }</span>
 	);
 
 	const priceRange = (
-		<div
-			className="range"
-			data-wc-bind--style="state.filters.rangeStyle"
-			data-wc-on--mousemove="actions.filters.updateActiveHandle"
-		>
+		<div className="range">
 			<div className="range-bar"></div>
 			<input
 				type="range"
@@ -51,24 +32,14 @@ const Block = ( {
 				max={ maxRange }
 				value={ minPrice }
 				className="active"
-				data-wc-bind--max="state.filters.maxRange"
-				data-wc-bind--value="state.filters.minPrice"
-				data-wc-class--active="state.filters.isMinActive"
-				data-wc-on--input="actions.filters.setMinPrice"
-				data-wc-on--change="actions.filters.updateProducts"
-				readOnly={ isEditor }
+				readOnly={ true }
 			/>
 			<input
 				type="range"
 				min={ minRange }
 				max={ maxRange }
 				value={ maxPrice }
-				data-wc-bind--max="state.filters.maxRange"
-				data-wc-bind--value="state.filters.maxPrice"
-				data-wc-class--active="state.filters.isMaxActive"
-				data-wc-on--input="actions.filters.setMaxPrice"
-				data-wc-on--change="actions.filters.updateProducts"
-				readOnly={ isEditor }
+				readOnly={ true }
 			/>
 		</div>
 	);
@@ -81,7 +52,7 @@ const Block = ( {
 					{ priceRange }
 					{ priceMax }
 				</div>
-				<button data-wc-on--click="actions.filters.reset">Reset</button>
+				<button>Reset</button>
 			</>
 		);
 	}
@@ -92,7 +63,7 @@ const Block = ( {
 				{ priceMin }
 				{ priceMax }
 			</div>
-			<button data-wc-on--click="actions.filters.reset">Reset</button>
+			<button>Reset</button>
 		</>
 	);
 };
