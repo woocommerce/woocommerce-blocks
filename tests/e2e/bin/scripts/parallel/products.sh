fd37cf834b438a661b8259fe60f3707bdb5e670a
Luigi <gigitux@gmail.com>
2023-09-11 20:07:46 +0200

    reset script

--- tests/e2e/bin/scripts/parallel/products.sh
+++ tests/e2e/bin/scripts/parallel/products.sh
@@ -5,6 +5,3 @@
 ###################################################################################################
 wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
 wp wc tool run regenerate_product_lookup_tables --user=1
-# Currently, sometimes the thumbnails aren't visible after importing products via xml. We have to regenerate them. https://github.com/woocommerce/woocommerce/issues/31646.
-wp media regenerate --yes
-
