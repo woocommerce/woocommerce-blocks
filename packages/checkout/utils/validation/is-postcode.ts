6478e8f9f7b838eacc4edcd2f9a208e0dfeff6bb
Thomas Roberts <thomas.roberts@automattic.com>
2023-09-12 17:08:34 +0100

    Move Cambodian postcodes to correct alphabetic location

--- packages/checkout/utils/validation/is-postcode.ts
+++ packages/checkout/utils/validation/is-postcode.ts
@@ -11,11 +11,11 @@ const CUSTOM_REGEXES = new Map< string, RegExp >( [
 	],
 	[ 'IN', /^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/ ],
 	[ 'JP', /^([0-9]{3})([-]?)([0-9]{4})$/ ],
+	[ 'KH', /^[0-9]{6}$/ ], // Cambodia (6-digit postal code).
 	[ 'LI', /^(94[8-9][0-9])$/ ],
 	[ 'NL', /^([1-9][0-9]{3})(\s?)(?!SA|SD|SS)[A-Z]{2}$/i ],
 	[ 'SI', /^([1-9][0-9]{3})$/ ],
 	[ 'TW', /^\d{3}(\d{2,3})?$/ ], // Taiwanese postcodes can be a 5 digit or 6-digit number. (3+3 or 3+2).
-	[ 'KH', /^[0-9]{6}$/ ], // Cambodia (6-digit postal code)
 ] );
 
 const DEFAULT_REGEXES = new Map< string, RegExp >( [
