559ecf38777fe94d34819c17347825698f3fee84
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-09-12 20:28:48 +0200

    Update the Store API Route for Patterns.

--- src/StoreApi/Routes/V1/Patterns.php
+++ src/StoreApi/Routes/V1/Patterns.php
@@ -2,8 +2,8 @@
 
 namespace Automattic\WooCommerce\StoreApi\Routes\V1;
 
-use Automattic\WooCommerce\Blocks\AI\Connection;
-use Automattic\WooCommerce\StoreApi\Exceptions\RouteException;
+use Automattic\WooCommerce\Blocks\Patterns\PatternImages;
+use Automattic\WooCommerce\Blocks\Verticals\VerticalsSelector;
 
 /**
  * Patterns class.
@@ -29,7 +29,7 @@ class Patterns extends AbstractRoute {
 	 * @return string
 	 */
 	public function get_path() {
-		return '/patterns/';
+		return '/patterns(?P<id>[\d]+)';
 	}
 
 	/**
@@ -43,45 +43,61 @@ class Patterns extends AbstractRoute {
 				'methods'             => \WP_REST_Server::CREATABLE,
 				'callback'            => [ $this, 'get_response' ],
 				'permission_callback' => '__return_true',
-				'args'                => $this->get_collection_params(),
+				'args'                => [
+					'business_description' => [
+						'description' => __( 'The business description for a given store.', 'woo-gutenberg-products-block' ),
+						'type'        => 'string',
+					],
+				],
 			],
-			'schema' => [ $this->schema, 'get_public_item_schema' ],
+			'schema'      => [ $this->schema, 'get_public_item_schema' ],
+			'allow_batch' => [ 'v1' => true ],
 		];
 	}
 
 	/**
-	 * Get a single item.
+	 * Ensure the content and images in patterns are powered by AI.
 	 *
-	 * @throws RouteException On error.
-	 * @param \WP_REST_Request $request Request object.
-	 * @return \WP_REST_Response
+	 * @param  \WP_REST_Request $request Request object.
+	 *
+	 * @return bool|string|\WP_Error|\WP_REST_Response
 	 */
-	protected function get_route_response( \WP_REST_Request $request ) {
-		// @TODO: add the response.
+	protected function get_route_post_response( \WP_REST_Request $request ) {
+		$allow_ai_connection = get_option( 'woocommerce_blocks_allow_ai_connection' );
 
-		return rest_ensure_response( $this->schema->get_item_response( '' ) );
-	}
-
-	/**
-	 * Populate Patterns
-	 *
-	 * @param string $store_description Store description.
-	 *
-	 * @return bool
-	 */
-	public function populate_patterns( $store_description ) {
-		if ( empty( $store_description ) ) {
-			$store_description = get_option( 'woo_ai_describe_store_description' );
-
-			if ( ! is_string( $store_description ) ) {
-				return false;
-			}
+		if ( ! $allow_ai_connection ) {
+			return new \WP_Error(
+				'ai_connection_not_allowed',
+				__( 'AI content generation is not allowed on this store. Update your store settings if you want to enable this feature.', 'woo-gutenberg-products-block' )
+			);
 		}
 
-		// @TODO finish populating patterns.
+		$business_description = sanitize_text_field( wp_unslash( $request['business_description'] ) );
 
-		$ai_connection = new Connection();
+		if ( ! is_string( $business_description ) ) {
+			return new \WP_Error(
+				'missing_business_description',
+				__( 'The business description is required to generate the content for your site.', 'woo-gutenberg-products-block' )
+			);
+		}
 
-		return true;
+		$vertical_id = ( new VerticalsSelector() )->get_vertical_id( $business_description );
+
+		if ( is_wp_error( $vertical_id ) ) {
+			return $vertical_id;
+		}
+
+		$populate_images = ( new PatternImages() )->create_patterns_content( $vertical_id );
+
+		if ( is_wp_error( $populate_images ) ) {
+			return $populate_images;
+		}
+
+		return $this->prepare_item_for_response(
+			(object) [
+				'ai_content_generated' => true,
+			],
+			$request
+		);
 	}
 }
