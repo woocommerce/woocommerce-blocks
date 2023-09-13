dcb6c2d9075cd4fa46d4a4350e4b8ceb428ce837
Luigi <gigitux@gmail.com>
2023-09-13 12:03:29 +0200

    remove not necessary file

--- src/Domain/Bootstrap.php
+++ src/Domain/Bootstrap.php
@@ -36,7 +36,6 @@ use Automattic\WooCommerce\StoreApi\StoreApi;
 use Automattic\WooCommerce\Blocks\Shipping\ShippingController;
 use Automattic\WooCommerce\Blocks\Templates\SingleProductTemplateCompatibility;
 use Automattic\WooCommerce\Blocks\Templates\ArchiveProductTemplatesCompatibility;
-use Automattic\WooCommerce\Blocks\Templates\ProductGalleryTemplate;
 
 /**
  * Takes care of bootstrapping the plugin.
@@ -150,7 +149,6 @@ class Bootstrap {
 			$this->container->get( ProductSearchResultsTemplate::class );
 			$this->container->get( ProductAttributeTemplate::class );
 			$this->container->get( CartTemplate::class );
-			$this->container->get( ProductGalleryTemplate::class );
 			$this->container->get( CheckoutTemplate::class );
 			$this->container->get( CheckoutHeaderTemplate::class );
 			$this->container->get( OrderConfirmationTemplate::class );
@@ -305,12 +303,6 @@ class Bootstrap {
 			CartTemplate::class,
 			function () {
 				return new CartTemplate();
-			}
-		);
-		$this->container->register(
-			ProductGalleryTemplate::class,
-			function () {
-				return new ProductGalleryTemplate();
 			}
 		);
 		$this->container->register(
