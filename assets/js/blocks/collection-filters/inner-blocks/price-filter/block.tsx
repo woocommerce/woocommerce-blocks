ff1a6e14e37e732548c61c3dcb03ab43f9f2bf90
Tung Du <dinhtungdu@gmail.com>
2023-09-19 14:44:23 +0700

    prepare for multiple styles support

--- assets/js/blocks/collection-filters/inner-blocks/price-filter/block.tsx
+++ assets/js/blocks/collection-filters/inner-blocks/price-filter/block.tsx
@@ -1,70 +1,95 @@
 /**
+ * External dependencies
+ */
+import { Disabled } from '@wordpress/components';
+import classNames from 'classnames';
+
+/**
  * Internal dependencies
  */
 import { BlockProps } from './types';
 
-const Block = ( {
-	minRange = 0,
-	maxRange = 0,
-	minPrice = 0,
-	maxPrice = 0,
+const PriceSlider = ( {
+	minPrice,
+	maxPrice,
+	minRange,
+	maxRange,
 	showInputFields,
 	inlineInput,
 }: BlockProps ) => {
+	const onChange = () => null;
 	const priceMin = showInputFields ? (
-		<input type="text" value={ minPrice } readOnly={ true } />
+		<input
+			className="min"
+			type="text"
+			value={ minPrice }
+			onChange={ onChange }
+		/>
 	) : (
 		<span>{ minPrice }</span>
 	);
 
 	const priceMax = showInputFields ? (
-		<input type="text" value={ maxPrice } readOnly={ true } />
+		<input
+			className="max"
+			type="text"
+			value={ maxPrice }
+			onChange={ onChange }
+		/>
 	) : (
 		<span>{ maxPrice }</span>
 	);
-
-	const priceRange = (
-		<div className="range">
-			<div className="range-bar"></div>
-			<input
-				type="range"
-				min={ minRange }
-				max={ maxRange }
-				value={ minPrice }
-				className="active"
-				readOnly={ true }
-			/>
-			<input
-				type="range"
-				min={ minRange }
-				max={ maxRange }
-				value={ maxPrice }
-				readOnly={ true }
-			/>
-		</div>
-	);
-
-	if ( showInputFields && inlineInput ) {
-		return (
-			<>
-				<div className="text">
-					{ priceMin }
-					{ priceRange }
-					{ priceMax }
-				</div>
-				<button>Reset</button>
-			</>
-		);
-	}
 	return (
-		<>
-			{ priceRange }
+		<div
+			className={ classNames( 'price-slider', {
+				'inline-input': inlineInput,
+			} ) }
+		>
+			<div className="range">
+				<div className="range-bar"></div>
+				<input
+					type="range"
+					min={ minRange }
+					max={ maxRange }
+					value={ minPrice }
+					className="active"
+					onChange={ onChange }
+				/>
+				<input
+					type="range"
+					min={ minRange }
+					max={ maxRange }
+					value={ maxPrice }
+					onChange={ onChange }
+				/>
+			</div>
 			<div className="text">
 				{ priceMin }
 				{ priceMax }
 			</div>
-			<button>Reset</button>
-		</>
+		</div>
+	);
+};
+
+const Block = ( props: BlockProps ) => {
+	props = Object.assign(
+		{
+			minPrice: 0,
+			maxPrice: 0,
+			minRange: 0,
+			maxRange: 0,
+		},
+		props
+	);
+	return (
+		<Disabled>
+			<div className="controls">
+				<PriceSlider { ...props } />
+			</div>
+			<div className="actions">
+				<button className="reset">Reset</button>
+			</div>
+		</Disabled>
 	);
 };
 
--- src/BlockTypes/CollectionPriceFilter.php
+++ src/BlockTypes/CollectionPriceFilter.php
@@ -26,8 +26,6 @@ final class CollectionPriceFilter extends AbstractBlock {
 	 * @return string Rendered block type output.
 	 */
 	protected function render( $attributes, $content, $block ) {
-		list ( 'showInputFields' => $show_input_fields, 'inlineInput' => $inline_input ) = $attributes;
-
 		// This should be available in $block->context['collectionData']['price_range'].
 		$price_range                              = new stdClass();
 		$price_range->min_price                   = 0;
@@ -40,13 +38,11 @@ final class CollectionPriceFilter extends AbstractBlock {
 		$price_range->currency_prefix             = '$';
 		$price_range->currency_suffix             = '';
 
-		$wrapper_attributes  = get_block_wrapper_attributes();
-		$min_range           = $price_range->min_price / 10 ** $price_range->currency_minor_unit;
-		$max_range           = $price_range->max_price / 10 ** $price_range->currency_minor_unit;
-		$min_price           = intval( get_query_var( self::MIN_PRICE_QUERY_VAR, $min_range ) );
-		$max_price           = intval( get_query_var( self::MAX_PRICE_QUERY_VAR, $max_range ) );
-		$formatted_min_price = wc_price( $min_price, array( 'decimals' => 0 ) );
-		$formatted_max_price = wc_price( $max_price, array( 'decimals' => 0 ) );
+		$wrapper_attributes = get_block_wrapper_attributes();
+		$min_range          = $price_range->min_price / 10 ** $price_range->currency_minor_unit;
+		$max_range          = $price_range->max_price / 10 ** $price_range->currency_minor_unit;
+		$min_price          = intval( get_query_var( self::MIN_PRICE_QUERY_VAR, $min_range ) );
+		$max_price          = intval( get_query_var( self::MAX_PRICE_QUERY_VAR, $max_range ) );
 
 		// CSS variables for the range bar style.
 		$__low       = 100 * $min_price / $max_range;
@@ -71,8 +67,47 @@ final class CollectionPriceFilter extends AbstractBlock {
 			)
 		);
 
+		$price_slider_html = $this->get_price_slider( $data, $attributes );
+
+		return <<<HTML
+		<div $wrapper_attributes>
+			<div class="controls">
+				$price_slider_html
+			</div>
+			<div class="actions">
+				<button data-wc-on--click='actions.filters.reset'>Reset</button>
+			</div>
+		</div>
+		HTML;
+	}
+
+	/**
+	 * Get the price slider HTML.
+	 *
+	 * @param array $store_data The data passing to Interactivity Store.
+	 * @param array $attributes Block attributes.
+	 */
+	private function get_price_slider( $store_data, $attributes ) {
+		list (
+			'showInputFields' => $show_input_fields,
+			'inlineInput' => $inline_input
+		) = $attributes;
+		list (
+			'minPrice' => $min_price,
+			'maxPrice' => $max_price,
+			'minRange' => $min_range,
+			'maxRange' => $max_range,
+			'rangeStyle' => $range_style,
+		) = $store_data;
+
+		$formatted_min_price = wc_price( $min_price, array( 'decimals' => 0 ) );
+		$formatted_max_price = wc_price( $max_price, array( 'decimals' => 0 ) );
+
+		$classes = $show_input_fields && $inline_input ? 'price-slider inline-input' : 'price-slider';
+
 		$price_min = $show_input_fields ? <<<HTML
 			<input
+				class="min"
 				type='text'
 				value='$min_price'
 				data-wc-bind--value='state.filters.minPrice'
@@ -85,6 +120,7 @@ final class CollectionPriceFilter extends AbstractBlock {
 
 		$price_max = $show_input_fields ? <<<HTML
 			<input
+				class="max"
 				type='text'
 				value='$max_price'
 				data-wc-bind--value='state.filters.maxPrice'
@@ -95,62 +131,44 @@ final class CollectionPriceFilter extends AbstractBlock {
 			<span data-wc-text="state.filters.formattedMaxPrice">$formatted_max_price</span>
 		HTML;
 
-		$price_range = <<<HTML
-			<div
-				class='range'
-				style='$range_style'
-				data-wc-bind--style='state.filters.rangeStyle'
-				data-wc-on--mousemove='actions.filters.updateActiveHandle'
-			>
-				<div class='range-bar'></div>
-				<input
-					type='range'
-					min='0'
-					max='$max_range'
-					value='$min_price'
-					class='active'
-					data-wc-bind--max='state.filters.maxRange'
-					data-wc-bind--value='state.filters.minPrice'
-					data-wc-class--active='state.filters.isMinActive'
-					data-wc-on--input='actions.filters.setMinPrice'
-					data-wc-on--change='actions.filters.updateProducts'
+		return <<<HTML
+			<div class='$classes'>
+				<div
+					class='range'
+					style='$range_style'
+					data-wc-bind--style='state.filters.rangeStyle'
+					data-wc-on--mousemove='actions.filters.updateActiveHandle'
 				>
-				<input
-					type='range'
-					min='0'
-					max='$max_range'
-					value='$max_price'
-					data-wc-bind--max='state.filters.maxRange'
-					data-wc-bind--value='state.filters.maxPrice'
-					data-wc-class--active='state.filters.isMaxActive'
-					data-wc-on--input='actions.filters.setMaxPrice'
-					data-wc-on--change='actions.filters.updateProducts'
-				>
-			</div>
-		HTML;
-
-		if ( $show_input_fields & $inline_input ) {
-			return <<<HTML
-			<div $wrapper_attributes>
+					<div class='range-bar'></div>
+					<input
+						type='range'
+						min='$min_range'
+						max='$max_range'
+						value='$min_price'
+						class='active'
+						data-wc-bind--max='state.filters.maxRange'
+						data-wc-bind--value='state.filters.minPrice'
+						data-wc-class--active='state.filters.isMinActive'
+						data-wc-on--input='actions.filters.setMinPrice'
+						data-wc-on--change='actions.filters.updateProducts'
+					>
+					<input
+						type='range'
+						min='0'
+						max='$max_range'
+						value='$max_price'
+						data-wc-bind--max='state.filters.maxRange'
+						data-wc-bind--value='state.filters.maxPrice'
+						data-wc-class--active='state.filters.isMaxActive'
+						data-wc-on--input='actions.filters.setMaxPrice'
+						data-wc-on--change='actions.filters.updateProducts'
+					>
+				</div>
 				<div class='text'>
 					$price_min
-					$price_range
 					$price_max
 				</div>
-				<button data-wc-on--click='actions.filters.reset'>Reset</button>
 			</div>
-			HTML;
-		}
-
-		return <<<HTML
-		<div $wrapper_attributes>
-			$price_range
-			<div class='text'>
-				$price_min
-				$price_max
-			</div>
-			<button data-wc-on--click='actions.filters.reset'>Reset</button>
-		</div>
-HTML;
+		HTML;
 	}
 }
