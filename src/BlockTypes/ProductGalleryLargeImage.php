009beb00dd4f3a04e95bba9300becbd2a9b10236
Luigi <gigitux@gmail.com>
2023-09-20 15:06:17 +0200

    fix zoom

--- src/BlockTypes/ProductGalleryLargeImage.php
+++ src/BlockTypes/ProductGalleryLargeImage.php
@@ -14,12 +14,6 @@ class ProductGalleryLargeImage extends AbstractBlock {
 	 */
 	protected $block_name = 'product-gallery-large-image';
 
-	/**
-	 * It isn't necessary register block assets because it is a server side block.
-	 */
-	protected function register_block_type_assets() {
-		return null;
-	}
 
 	/**
 	 * Get the frontend style handle for this block type.
