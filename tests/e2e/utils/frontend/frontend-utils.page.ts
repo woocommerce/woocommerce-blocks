15b443986581ce3a68bef389bd7981e8db0fd1ee
Saad Tarhi <saad.trh@gmail.com>
2023-09-20 08:27:19 +0100

    Remove unused util functions

--- tests/e2e/utils/frontend/frontend-utils.page.ts
+++ tests/e2e/utils/frontend/frontend-utils.page.ts
@@ -3,12 +3,6 @@
  */
 import { Page, Locator } from '@playwright/test';
 import { RequestUtils } from '@wordpress/e2e-test-utils-playwright';
-import { expect } from '@woocommerce/e2e-playwright-utils';
-
-/**
- * Internal dependencies
- */
-import { customer, admin } from '../../test-data/data/data';
 
 export class FrontendUtils {
 	page: Page;
@@ -176,62 +170,5 @@ export class FrontendUtils {
 		await locator.click();
 		await locator.press( 'End' );
 		await locator.press( 'Shift+Home' );
-	}
-
-	async gotoMyAccount() {
-		await this.page.goto( '/my-account', {
-			waitUntil: 'commit',
-		} );
-	}
-
-	async isLoggedIn() {
-		await this.gotoMyAccount();
-		await expect(
-			this.page.getByRole( 'heading', { name: 'My account' } )
-		).toBeVisible();
-		const isLoginFormVisible = await this.page
-			.locator( 'form.woocommerce-form-login' )
-			.isVisible();
-
-		return ! isLoginFormVisible;
-	}
-
-	async login( asAdmin = false ) {
-		const isLoggedIn = await this.isLoggedIn();
-		if ( isLoggedIn ) {
-			return;
-		}
-
-		let { username, password } = customer;
-
-		if ( asAdmin ) {
-			username = admin.username;
-			password = admin.password;
-		}
-
-		await this.gotoMyAccount();
-		await expect(
-			this.page.getByRole( 'heading', { name: 'My account' } )
-		).toBeVisible();
-		await this.page.locator( 'input[name="username"]' ).fill( username );
-		await this.page.locator( 'input[name="password"]' ).fill( password );
-		await this.page.locator( 'text=Log In' ).click();
-		// eslint-disable-next-line playwright/no-networkidle
-		await this.page.waitForLoadState( 'networkidle' );
-	}
-
-	async logout() {
-		await this.gotoMyAccount();
-		await expect(
-			this.page.getByRole( 'heading', { name: 'My account' } )
-		).toBeVisible();
-		await this.page
-			.getByRole( 'list' )
-			.filter( {
-				hasText:
-					'Dashboard Orders Downloads Addresses Account details Log out',
-			} )
-			.getByRole( 'link', { name: 'Log out' } )
-			.click();
 	}
 }
