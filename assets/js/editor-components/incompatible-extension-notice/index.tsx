7e4d513e33ba84e836a601283d5814874f0339b0
Niels Lange <info@nielslange.de>
2023-09-29 17:04:11 +0700

    Show single incompatible extension within notice instead of as list

--- assets/js/editor-components/incompatible-extension-notice/index.tsx
+++ assets/js/editor-components/incompatible-extension-notice/index.tsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { _n } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 import { Notice, ExternalLink } from '@wordpress/components';
 import { createInterpolateElement, useEffect } from '@wordpress/element';
 import { Alert } from '@woocommerce/icons';
@@ -36,20 +36,39 @@ export function IncompatibleExtensionsNotice( {
 		return null;
 	}
 
-	const noticeContent = createInterpolateElement(
-		_n(
-			'The following extension may be incompatible with the block-based checkout. <a>Learn more</a>',
-			'The following extensions may be incompatible with the block-based checkout. <a>Learn more</a>',
-			numberOfIncompatiblePaymentMethods,
-			'woo-gutenberg-products-block'
-		),
-		{
-			a: (
-				// Suppress the warning as this <a> will be interpolated into the string with content.
-				// eslint-disable-next-line jsx-a11y/anchor-has-content
-				<ExternalLink href="https://woocommerce.com/document/cart-checkout-blocks-support-status/" />
-			),
-		}
+	// console.log( incompatiblePaymentMethods );
+
+	const noticeContent = (
+		<>
+			{ numberOfIncompatiblePaymentMethods > 1
+				? createInterpolateElement(
+						__(
+							'The following extensions may be incompatible with the block-based checkout. <a>Learn more</a>',
+							'woo-gutenberg-products-block'
+						),
+						{
+							a: (
+								<ExternalLink href="https://woocommerce.com/document/cart-checkout-blocks-support-status/" />
+							),
+						}
+				  )
+				: createInterpolateElement(
+						sprintf(
+							// translators: %s is the name of the extension that is incompatible with the block-based checkout.
+							__(
+								'<strong>%s</strong> may be incompatible with the block-based checkout. <a>Learn more</a>',
+								'woo-gutenberg-products-block'
+							),
+							Object.values( incompatiblePaymentMethods )[ 0 ]
+						),
+						{
+							strong: <strong />,
+							a: (
+								<ExternalLink href="https://woocommerce.com/document/cart-checkout-blocks-support-status/" />
+							),
+						}
+				  ) }
+		</>
 	);
 
 	return (
@@ -66,18 +85,20 @@ export function IncompatibleExtensionsNotice( {
 				/>
 				<div>
 					<p>{ noticeContent }</p>
-					<ul>
-						{ Object.entries( incompatiblePaymentMethods ).map(
-							( [ id, title ] ) => (
-								<li
-									key={ id }
-									className="wc-blocks-incompatible-extensions-notice__element"
-								>
-									{ title }
-								</li>
-							)
-						) }
-					</ul>
+					{ numberOfIncompatiblePaymentMethods > 1 && (
+						<ul>
+							{ Object.entries( incompatiblePaymentMethods ).map(
+								( [ id, title ] ) => (
+									<li
+										key={ id }
+										className="wc-blocks-incompatible-extensions-notice__element"
+									>
+										{ title }
+									</li>
+								)
+							) }
+						</ul>
+					) }
 				</div>
 			</div>
 		</Notice>
