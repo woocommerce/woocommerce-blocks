626964433c0eaad912bac707cce3c8edbbbb7f68
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-10-03 15:09:44 +0200

    Update styles for the pattern to match the new ones.

--- patterns/featured-category-cover-image.php
+++ patterns/featured-category-cover-image.php
@@ -11,27 +11,30 @@ $images  = PatternsHelper::get_pattern_images( 'woocommerce-blocks/featured-cate
 
 $image1 = PatternsHelper::get_image_url( $images, 0, 'images/pattern-placeholders/shop-jeans.png' );
 ?>
-<!-- wp:cover {"url":"<?php echo esc_url( $image1 ); ?>","id":1,"dimRatio":30,"customOverlayColor":"#000000","focalPoint":{"x":0,"y":0},"contentPosition":"center","align":"wide","style":{"spacing":{"padding":{"top":"2em","right":"2.25em","bottom":"2.25em","left":"2.25em"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignwide has-custom-content-position" style="padding-top:2em;padding-right:2.25em;padding-bottom:2.25em;padding-left:2.25em">
-	<span aria-hidden="true" class="wp-block-cover__background has-background-dim-30 has-background-dim" style="background-color:#000000"></span>
-	<img class="wp-block-cover__image-background wp-image-1" alt="<?php esc_attr_e( 'Placeholder image used to represent products being showcased in a featured category section.', 'woo-gutenberg-products-block' ); ?>" src="<?php echo esc_url( plugins_url( 'images/pattern-placeholders/wood-leather-fur-shop-jeans-shelf.png', dirname( __FILE__ ) ) ); ?>" style="object-position:0% 0%" data-object-fit="cover" data-object-position="0% 0%"/>
+<!-- wp:cover {"url":"<?php echo esc_url( $image1 ); ?>","dimRatio":50,"align":"wide","style":{"spacing":{"padding":{"top":"3%","right":"0%","bottom":"10%","left":"3%"},"margin":{"top":"0","bottom":"0"},"blockGap":"1%"}},"layout":{"type":"default"}} -->
+<div class="wp-block-cover alignwide" style="margin-top:0;margin-bottom:0;padding-top:3%;padding-right:0%;padding-bottom:10%;padding-left:3%">
+	<span aria-hidden="true" class="wp-block-cover__background has-background-dim"></span>
+	<img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( $image1 ); ?>" data-object-fit="cover"/>
 	<div class="wp-block-cover__inner-container">
-		<!-- wp:heading {"textAlign":"center","style":{"color":{"text":"#ffffff"}}} -->
-		<h2 class="wp-block-heading has-text-align-center has-text-color" style="color:#ffffff;">
-			<?php echo esc_html( $content['titles'][0]['default'] ); ?>
-		</h2>
+		<!-- wp:heading {"level":3} -->
+		<h3 class="wp-block-heading"><strong><?php echo esc_html( $content['titles'][0]['default'] ); ?></strong></h3>
 		<!-- /wp:heading -->
 
-		<!-- wp:paragraph {"align":"center","style":{"color":{"text":"#ffffff"},"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
-		<p class="has-text-align-center has-text-color" style="color:#ffffff;margin-top:0px;margin-bottom:0px">
-			<?php echo esc_html( $content['descriptions'][0]['default'] ); ?>
-		</p>
+		<!-- wp:paragraph {"fontSize":"medium"} -->
+		<p class="has-medium-font-size"><?php echo esc_html( $content['descriptions'][0]['default'] ); ?></p>
 		<!-- /wp:paragraph -->
-		<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"30px"}}}} -->
-		<div class="wp-block-buttons" style="margin-top:30px">
-			<!-- wp:button {"textAlign":"center"} -->
-			<div class="wp-block-button"><a href="<?php echo esc_url( wc_get_page_permalink( 'shop' ) ); ?>" class="wp-block-button__link has-text-align-center wp-element-button"><?php echo esc_html( $content['buttons'][0]['default'] ); ?></a></div>
-			<!-- /wp:button --></div>
-		<!-- /wp:buttons --></div>
+
+		<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} -->
+		<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30)">
+			<!-- wp:button {"fontSize":"small"} -->
+			<div class="wp-block-button has-custom-font-size has-small-font-size">
+				<a href="<?php echo esc_url( wc_get_page_permalink( 'shop' ) ); ?>" class="wp-block-button__link wp-element-button">
+					<?php echo esc_html( $content['buttons'][0]['default'] ); ?>
+				</a>
+			</div>
+			<!-- /wp:button -->
+		</div>
+		<!-- /wp:buttons -->
+	</div>
 </div>
 <!-- /wp:cover -->
