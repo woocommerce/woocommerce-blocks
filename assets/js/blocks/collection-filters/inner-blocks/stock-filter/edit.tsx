c0555e65643f9050aaf844781a04cd40de252142
roykho <roykho77@gmail.com>
2023-10-04 10:31:27 -0700

    =WIP

--- assets/js/blocks/collection-filters/inner-blocks/stock-filter/edit.tsx
+++ assets/js/blocks/collection-filters/inner-blocks/stock-filter/edit.tsx
@@ -5,12 +5,16 @@ import classnames from 'classnames';
 import { useBlockProps } from '@wordpress/block-editor';
 import type { BlockEditProps } from '@wordpress/blocks';
 import { ProductQueryContext as Context } from '@woocommerce/blocks/product-query/types';
-import { Disabled, withSpokenMessages } from '@wordpress/components';
+import { Disabled } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { Icon, chevronDown } from '@wordpress/icons';
+import CheckboxList from '@woocommerce/base-components/checkbox-list';
+import Label from '@woocommerce/base-components/filter-element-label';
+import FormTokenField from '@woocommerce/base-components/form-token-field';
 
 /**
  * Internal dependencies
  */
-import Block from './block';
 import './editor.scss';
 import { BlockProps } from './types';
 import { Inspector } from './inspector';
@@ -23,13 +27,83 @@ const Edit = ( props: BlockEditProps< BlockProps > & { context: Context } ) => {
 		),
 	} );
 
+	const { showCounts, displayStyle } = props.attributes;
+	const listOptions = [
+		{
+			value: 'instock',
+			name: __( 'In stock', 'woo-gutenberg-products-block' ),
+			textLabel: __( 'In stock (18)', 'woo-gutenberg-products-block' ),
+			label: (
+				<Label name={ 'In stock' } count={ showCounts ? 18 : null } />
+			),
+		},
+		{
+			value: 'outofstock',
+			name: __( 'Out of stock', 'woo-gutenberg-products-block' ),
+			textLabel: __( 'Out of stock (2)', 'woo-gutenberg-products-block' ),
+			label: (
+				<Label
+					name={ 'Out of stock' }
+					count={ showCounts ? 2 : null }
+				/>
+			),
+		},
+		{
+			value: 'onbackorder',
+			name: __( 'On backorder', 'woo-gutenberg-products-block' ),
+			textLabel: __( 'On backorder (5)', 'woo-gutenberg-products-block' ),
+			label: (
+				<Label
+					name={ 'On backorder' }
+					count={ showCounts ? 5 : null }
+				/>
+			),
+		},
+	];
+
 	return (
 		<>
 			{
 				<div { ...blockProps }>
 					<Inspector { ...props } />
 					<Disabled>
-						<Block { ...props.attributes } isEditor={ true } />
+						<div
+							className={ classnames(
+								'wc-block-stock-filter',
+								`style-${ displayStyle }`,
+								{
+									'is-loading': false,
+								}
+							) }
+						>
+							{ displayStyle === 'dropdown' ? (
+								<>
+									<FormTokenField
+										className={ classnames( {
+											'single-selection': true,
+											'is-loading': false,
+										} ) }
+										suggestions={ [] }
+										placeholder={ __(
+											'Select stock status',
+											'woo-gutenberg-products-block'
+										) }
+										onChange={ () => null }
+										value={ [] }
+									/>
+									<Icon icon={ chevronDown } size={ 30 } />
+								</>
+							) : (
+								<CheckboxList
+									className={ 'wc-block-stock-filter-list' }
+									options={ listOptions }
+									checked={ [] }
+									onChange={ () => null }
+									isLoading={ false }
+									isDisabled={ true }
+								/>
+							) }
+						</div>
 					</Disabled>
 				</div>
 			}
@@ -37,4 +111,4 @@ const Edit = ( props: BlockEditProps< BlockProps > & { context: Context } ) => {
 	);
 };
 
-export default withSpokenMessages( Edit );
+export default Edit;
--- src/BlockTypes/CollectionStockFilter.php
+++ src/BlockTypes/CollectionStockFilter.php
@@ -4,7 +4,7 @@ namespace Automattic\WooCommerce\Blocks\BlockTypes;
 /**
  * CollectionStockFilter class.
  */
-class CollectionStockFilter extends AbstractBlock {
+final class CollectionStockFilter extends AbstractBlock {
 
 	/**
 	 * Block name.
@@ -29,29 +29,6 @@ class CollectionStockFilter extends AbstractBlock {
 	}
 
 	/**
-	 * Register the context.
-	 */
-	protected function get_block_type_uses_context() {
-		return [ 'query', 'queryId', 'postId' ];
-	}
-
-	/**
-	 * Get Stock status query variables values.
-	 */
-	public static function get_stock_status_query_var_values() {
-		return array_keys( wc_get_product_stock_status_options() );
-	}
-
-	/**
-	 * Get the frontend style handle for this block type.
-	 *
-	 * @return null
-	 */
-	protected function get_block_type_style() {
-		return array_merge( parent::get_block_type_style(), [ 'wc-blocks-packages-style' ] );
-	}
-
-	/**
 	 * Include and render the block.
 	 *
 	 * @param array    $attributes Block attributes. Default empty array.
@@ -60,24 +37,65 @@ class CollectionStockFilter extends AbstractBlock {
 	 * @return string Rendered block type output.
 	 */
 	protected function render( $attributes, $content, $block ) {
-		$data = array( 'stockStatus' => 'instock' );
+		$stock_status_counts = $block->context['collectionData']['stock_status_counts'] ?? [];
+		$wrapper_attributes  = get_block_wrapper_attributes();
 
 		wc_store(
 			array(
 				'state' => array(
-					'filters' => $data,
+					'filters' => $stock_status_counts,
 				),
 			)
 		);
 
-		return '<select name="stock_status" data-wc-on--change="actions.filters.updateProducts"><option value="instock">In Stock</option><option value="outofstock">Out of Stock</option></select>';
+		return sprintf(
+			'<div %1$s>
+				<div class="wc-block-stock-filter__controls">%2$s</div>
+				<div class="wc-block-stock-filter__actions"></div>
+			</div>',
+			$wrapper_attributes,
+			$this->get_stock_filter_html( $stock_status_counts, $attributes, $block ),
+		);
 	}
 
 	/**
 	 * Stock filter HTML
 	 *
+	 * @param array    $datat	  Block data. Default empty array.
+	 * @param array    $attributes Block attributes. Default empty array.
+	 * @param WP_Block $block      Block instance.
+	 * @return string Rendered block type output.
 	 */
-	private function get_stock_filter_html( $data, $attributes ) {
-<div class="wp-block-woocommerce-stock-filter"><div class="wc-block-stock-filter style-list"><ul class="wc-block-checkbox-list wc-block-components-checkbox-list wc-block-stock-filter-list"><li><div class="wc-block-components-checkbox wc-block-checkbox-list__checkbox"><label for="instock"><input id="instock" class="wc-block-components-checkbox__input" type="checkbox" aria-invalid="false"><svg class="wc-block-components-checkbox__mark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg><span class="wc-block-components-checkbox__label">In stock<span class="wc-filter-element-label-list-count"><span aria-hidden="true">18</span><span class="screen-reader-text">18 products</span></span></span></label></div></li><li><div class="wc-block-components-checkbox wc-block-checkbox-list__checkbox"><label for="outofstock"><input id="outofstock" class="wc-block-components-checkbox__input" type="checkbox" aria-invalid="false"><svg class="wc-block-components-checkbox__mark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg><span class="wc-block-components-checkbox__label">Out of stock<span class="wc-filter-element-label-list-count"><span aria-hidden="true">1</span><span class="screen-reader-text">1 product</span></span></span></label></div></li></ul></div><div class="wc-block-stock-filter__actions"></div></div>
+	private function get_stock_filter_html( $data, $attributes, $block ) {
+		$display_style  = $attributes['displayStyle'] ?? 'list';
+		$select_type    = $attributes['selectType'] ?? 'multiple';
+		$stock_statuses = wc_get_product_stock_status_options();
+		ob_start();
+	?>
+			<div class="wc-block-stock-filter style-<?php echo esc_attr( $display_style ); ?>">
+				<?php if ( 'list' === $display_style ) : ?>
+					<ul class="wc-block-checkbox-list wc-block-components-checkbox-list wc-block-stock-filter-list">
+						<?php foreach ( $data as $obj ) { ?>
+							<li>
+								<div class="wc-block-components-checkbox wc-block-checkbox-list__checkbox">
+									<label for="<?php echo esc_attr( $obj->status ); ?>">
+										<input id="<?php echo esc_attr( $obj->status ); ?>" class="wc-block-components-checkbox__input" type="checkbox" aria-invalid="false" data-wc-on--change="actions.filters.updateProducts" value="<?php echo esc_attr( $obj->status ); ?>">
+											<svg class="wc-block-components-checkbox__mark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>
+											<span class="wc-block-components-checkbox__label"><?php echo esc_html( $stock_statuses[ $obj->status ] ); ?>
+												<span class="wc-filter-element-label-list-count"><span aria-hidden="true"><?php echo esc_html( $obj->count ); ?></span><span class="screen-reader-text"><?php printf( _n( '%s product', '%s products', $obj->count, 'woo-gutenberg-products-block' ), number_format_i18n( $obj->count ) ); ?></span></span>
+											</span>
+									</label>
+								</div>
+							</li>
+						<?php } ?>
+					</ul>
+				<?php endif; ?>
+
+				<?php if ( 'dropdown' === $display_style ) : ?>
+					<div class="wc-blocks-components-form-token-field-wrapper single-selection"><div class="components-form-token-field" tabindex="-1"><label for="components-form-token-input-1" class="components-form-token-field__label"></label><div class="components-form-token-field__input-container" tabindex="-1"><input id="components-form-token-input-1" type="text" autocomplete="off" placeholder="Select stock status" class="components-form-token-field__input" role="combobox" aria-expanded="false" aria-autocomplete="list" aria-describedby="components-form-token-suggestions-howto-1" value=""></div></div></div><svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="30" height="30" aria-hidden="true" focusable="false"><path d="M17.5 11.6L12 16l-5.5-4.4.9-1.2L12 14l4.5-3.6 1 1.2z"></path></svg>
+				<?php endif; ?>
+			</div>
+	<?php
+		return ob_get_clean();
 	}
 }
