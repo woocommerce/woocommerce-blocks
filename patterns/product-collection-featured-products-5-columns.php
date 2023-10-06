595a563390b6f20d0a054c95d5390f5aadcfbd84
Patricia Hillebrandt <patriciahillebrandt@gmail.com>
2023-10-06 15:26:13 +0200

    Adjust the width for the hero-product-3-split and remove unnecessary placeholder text from product-collection-featured-products-5-columns pattern.

--- patterns/product-collection-featured-products-5-columns.php
+++ patterns/product-collection-featured-products-5-columns.php
@@ -29,7 +29,6 @@ $content = PatternsHelper::get_pattern_content( 'woocommerce-blocks/product-coll
 	<!-- /wp:woocommerce/product-template -->
 
 	<!-- wp:query-no-results -->
-	<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when a query returns no results."} -->
 	<p></p>
 	<!-- /wp:paragraph -->
 	<!-- /wp:query-no-results --></div>
