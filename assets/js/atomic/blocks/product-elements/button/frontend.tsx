35354eaf1bb55258a72d466c6d5dea0655cab3b0
David Arenas <david.arenas@automattic.com>
2023-10-06 12:26:07 +0200

    Remove `afterLoad` callback

--- assets/js/atomic/blocks/product-elements/button/frontend.tsx
+++ assets/js/atomic/blocks/product-elements/button/frontend.tsx
@@ -59,128 +59,120 @@ const getTextButton = (
 	return inTheCart.replace( '###', numberOfItems.toString() );
 };
 
-const { state, actions } = store< Store >(
-	'woo',
-	{
-		state: {
-			get slideInAnimation() {
-				const { animationStatus } = getContext< Context >();
-				return animationStatus === AnimationStatus.SLIDE_IN;
-			},
-			get slideOutAnimation() {
-				const { animationStatus } = getContext< Context >();
-				return animationStatus === AnimationStatus.SLIDE_OUT;
-			},
-			get numberOfItemsInTheCart() {
-				const { productId } = getContext< Context >();
-				const product = getProductById( state.cart, productId );
-				return product?.quantity || 0;
-			},
-			get hasCartLoaded(): boolean {
-				return !! state.cart;
-			},
-			get addToCartText(): string {
-				const context = getContext< Context >();
-				// We use the temporary number of items when there's no animation, or the
-				// second part of the animation hasn't started.
-				if (
-					context.animationStatus === AnimationStatus.IDLE ||
-					context.animationStatus === AnimationStatus.SLIDE_OUT
-				) {
-					return getTextButton(
-						context.addToCartText,
-						state.inTheCartText!,
-						context.temporaryNumberOfItems
-					);
-				}
+const { state, actions } = store< Store >( 'woo', {
+	state: {
+		get slideInAnimation() {
+			const { animationStatus } = getContext< Context >();
+			return animationStatus === AnimationStatus.SLIDE_IN;
+		},
+		get slideOutAnimation() {
+			const { animationStatus } = getContext< Context >();
+			return animationStatus === AnimationStatus.SLIDE_OUT;
+		},
+		get numberOfItemsInTheCart() {
+			const { productId } = getContext< Context >();
+			const product = getProductById( state.cart, productId );
+			return product?.quantity || 0;
+		},
+		get hasCartLoaded(): boolean {
+			return !! state.cart;
+		},
+		get addToCartText(): string {
+			const context = getContext< Context >();
+			// We use the temporary number of items when there's no animation, or the
+			// second part of the animation hasn't started.
+			if (
+				context.animationStatus === AnimationStatus.IDLE ||
+				context.animationStatus === AnimationStatus.SLIDE_OUT
+			) {
 				return getTextButton(
 					context.addToCartText,
 					state.inTheCartText!,
-					state.numberOfItemsInTheCart
+					context.temporaryNumberOfItems
 				);
-			},
-			get displayViewCart(): boolean {
-				const context = getContext< Context >();
-				if ( ! context.displayViewCart ) return false;
-				if ( ! state.hasCartLoaded ) {
-					return context.temporaryNumberOfItems > 0;
-				}
-				return state.numberOfItemsInTheCart > 0;
-			},
+			}
+			return getTextButton(
+				context.addToCartText,
+				state.inTheCartText!,
+				state.numberOfItemsInTheCart
+			);
 		},
-		actions: {
-			*addToCart() {
-				yield import( './frontend-add-to-cart' );
-				yield actions.__addToCart();
-			},
-			handleAnimationEnd: ( event: AnimationEvent ) => {
-				const context = getContext< Context >();
-				if ( event.animationName === 'slideOut' ) {
-					// When the first part of the animation (slide-out) ends, we move
-					// to the second part (slide-in).
-					context.animationStatus = AnimationStatus.SLIDE_IN;
-				} else if ( event.animationName === 'slideIn' ) {
-					// When the second part of the animation ends, we update the
-					// temporary number of items to sync it with the cart and reset the
-					// animation status so it can be triggered again.
-					context.temporaryNumberOfItems =
-						state.numberOfItemsInTheCart;
-					context.animationStatus = AnimationStatus.IDLE;
-				}
-			},
-		},
-		callbacks: {
-			syncTemporaryNumberOfItemsOnLoad: () => {
-				const context = getContext< Context >( 'woo' );
-				// If the cart has loaded when we instantiate this element, we sync
-				// the temporary number of items with the number of items in the cart
-				// to avoid triggering the animation. We do this only once, but we
-				// use useLayoutEffect to avoid the useEffect flickering.
-				if ( state.hasCartLoaded ) {
-					context.temporaryNumberOfItems =
-						state.numberOfItemsInTheCart;
-				}
-			},
-			startAnimation: () => {
-				const context = getContext< Context >( 'woo' );
-				// We start the animation if the cart has loaded, the temporary number
-				// of items is out of sync with the number of items in the cart, the
-				// button is not loading (because that means the user started the
-				// interaction) and the animation hasn't started yet.
-				if (
-					state.hasCartLoaded &&
-					context.temporaryNumberOfItems !==
-						state.numberOfItemsInTheCart &&
-					! context.isLoading &&
-					context.animationStatus === AnimationStatus.IDLE
-				) {
-					context.animationStatus = AnimationStatus.SLIDE_OUT;
-				}
-			},
+		get displayViewCart(): boolean {
+			const context = getContext< Context >();
+			if ( ! context.displayViewCart ) return false;
+			if ( ! state.hasCartLoaded ) {
+				return context.temporaryNumberOfItems > 0;
+			}
+			return state.numberOfItemsInTheCart > 0;
 		},
 	},
-	{
-		afterLoad: () => {
-			// Subscribe to changes in Cart data.
-			subscribe( () => {
-				const cartData = select( storeKey ).getCartData();
-				const isResolutionFinished =
-					select( storeKey ).hasFinishedResolution( 'getCartData' );
-				if ( isResolutionFinished ) {
-					state.cart = cartData;
-				}
-			}, storeKey );
-
-			// This selector triggers a fetch of the Cart data. It is done in a
-			// `requestIdleCallback` to avoid potential performance issues.
-			requestIdleCallback( () => {
-				if ( ! state.hasCartLoaded ) {
-					select( storeKey ).getCartData();
-				}
-			} );
+	actions: {
+		*addToCart() {
+			yield import( './frontend-add-to-cart' );
+			yield actions.__addToCart();
 		},
+		handleAnimationEnd: ( event: AnimationEvent ) => {
+			const context = getContext< Context >();
+			if ( event.animationName === 'slideOut' ) {
+				// When the first part of the animation (slide-out) ends, we move
+				// to the second part (slide-in).
+				context.animationStatus = AnimationStatus.SLIDE_IN;
+			} else if ( event.animationName === 'slideIn' ) {
+				// When the second part of the animation ends, we update the
+				// temporary number of items to sync it with the cart and reset the
+				// animation status so it can be triggered again.
+				context.temporaryNumberOfItems = state.numberOfItemsInTheCart;
+				context.animationStatus = AnimationStatus.IDLE;
+			}
+		},
+	},
+	callbacks: {
+		syncTemporaryNumberOfItemsOnLoad: () => {
+			const context = getContext< Context >( 'woo' );
+			// If the cart has loaded when we instantiate this element, we sync
+			// the temporary number of items with the number of items in the cart
+			// to avoid triggering the animation. We do this only once, but we
+			// use useLayoutEffect to avoid the useEffect flickering.
+			if ( state.hasCartLoaded ) {
+				context.temporaryNumberOfItems = state.numberOfItemsInTheCart;
+			}
+		},
+		startAnimation: () => {
+			const context = getContext< Context >( 'woo' );
+			// We start the animation if the cart has loaded, the temporary number
+			// of items is out of sync with the number of items in the cart, the
+			// button is not loading (because that means the user started the
+			// interaction) and the animation hasn't started yet.
+			if (
+				state.hasCartLoaded &&
+				context.temporaryNumberOfItems !==
+					state.numberOfItemsInTheCart &&
+				! context.isLoading &&
+				context.animationStatus === AnimationStatus.IDLE
+			) {
+				context.animationStatus = AnimationStatus.SLIDE_OUT;
+			}
+		},
+	},
+} );
+
+// Subscribe to changes in Cart data.
+subscribe( () => {
+	const cartData = select( storeKey ).getCartData();
+	const isResolutionFinished =
+		select( storeKey ).hasFinishedResolution( 'getCartData' );
+	if ( isResolutionFinished ) {
+		state.cart = cartData;
 	}
-);
+}, storeKey );
+
+// This selector triggers a fetch of the Cart data. It is done in a
+// `requestIdleCallback` to avoid potential performance issues.
+requestIdleCallback( () => {
+	if ( ! state.hasCartLoaded ) {
+		select( storeKey ).getCartData();
+	}
+} );
 
 interface WooTestCtx {
 	emoji: string;
