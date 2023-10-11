a7d772f51049f8967bb2cb905e03f08235cf60ed
Tung Du <dinhtungdu@gmail.com>
2023-10-11 22:25:06 +0700

    feat(collection filters): add utility to transform product collection query to store api/ product collection data route params

---
+++ assets/js/blocks/collection-filters/utils.ts
@@ -0,0 +1,52 @@
+/**
+ * Internal dependencies
+ */
+import { ProductCollectionQuery } from '../product-collection/types';
+
+function taxonomyMapper( taxonomy: string ) {
+	const map: Record< string, string > = {
+		product_tag: 'tag',
+		product_cat: 'cat',
+	};
+
+	return taxonomy in map ? map[ taxonomy ] : `_unstable_tax_${ taxonomy }`;
+}
+
+export function formatQuery( query: ProductCollectionQuery ) {
+	const sharedParams: Array< keyof ProductCollectionQuery > = [
+		'exclude',
+		'offset',
+		'order',
+		'search',
+	];
+
+	const mappedParams: { key: keyof ProductCollectionQuery; map: string }[] = [
+		{ key: 'orderBy', map: 'orderby' },
+		{ key: 'pages', map: 'page' },
+		{ key: 'parents', map: 'parent' },
+		{ key: 'perPage', map: 'per_page' },
+		{ key: 'woocommerceStockStatus', map: 'stock_status' },
+		{ key: 'woocommerceOnSale', map: 'on_sale' },
+		{ key: 'woocommerceHandPickedProducts', map: 'include' },
+	];
+
+	return Object.assign(
+		{},
+		...sharedParams.map(
+			( key ) => key in query && { [ key ]: query[ key ] }
+		),
+		...mappedParams.map(
+			( param ) =>
+				param.key in query && { [ param.map ]: query[ param.key ] }
+		),
+		...Object.entries( query.taxQuery ).map( ( [ taxonomy, terms ] ) => ( {
+			[ taxonomyMapper( taxonomy ) ]: terms,
+		} ) ),
+		'woocommerceAttributes' in query && {
+			attributes: query.woocommerceAttributes.map( ( attribute ) => ( {
+				attribute: attribute.taxonomy,
+				term_id: attribute.termId,
+			} ) ),
+		}
+	);
+}
