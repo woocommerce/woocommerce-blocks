6531f2fcef67a29388308487a9e912300c9e12d4
Alba Rincón <alba.rincon@automattic.com>
2023-10-11 14:57:31 +0200

    Add fallbacks to all the pattern titles, descriptions and buttons

--- patterns/store-info-alt-image-and-text.php
+++ patterns/store-info-alt-image-and-text.php
@@ -6,10 +6,22 @@
  */
 
 use Automattic\WooCommerce\Blocks\Patterns\PatternsHelper;
-$images = PatternsHelper::get_pattern_images( 'woocommerce-blocks/alt-image-and-text' );
+$content = PatternsHelper::get_pattern_content( 'woocommerce-blocks/alt-image-and-text' );
+$images  = PatternsHelper::get_pattern_images( 'woocommerce-blocks/alt-image-and-text' );
 
 $image1 = PatternsHelper::get_image_url( $images, 0, 'images/pattern-placeholders/crafting-pots.png' );
 $image2 = PatternsHelper::get_image_url( $images, 1, 'images/pattern-placeholders/hand-made-pots.png' );
+
+$first_title  = $content['titles'][0]['default'] ?? '';
+$second_title = $content['titles'][1]['default'] ?? '';
+$third_title  = $content['titles'][2]['default'] ?? '';
+$fourth_title = $content['titles'][3]['default'] ?? '';
+$fifth_title  = $content['titles'][4]['default'] ?? '';
+
+$first_description  = $content['descriptions'][0]['default'] ?? '';
+$second_description = $content['descriptions'][1]['default'] ?? '';
+
+$button = $content['buttons'][0]['default'] ?? '';
 ?>
 
 <!-- wp:group {"align":"wide"} -->
@@ -29,15 +41,15 @@ $image2 = PatternsHelper::get_image_url( $images, 1, 'images/pattern-placeholder
 		<!-- wp:column {"verticalAlignment":"center","width":"50%"} -->
 		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:50%">
 			<!-- wp:paragraph {"placeholder":"Content…","style":{"typography":{"textTransform":"uppercase"}}} -->
-			<p style="text-transform:uppercase"><?php esc_html_e( 'The goods', 'woo-gutenberg-products-block' ); ?></p>
+			<p style="text-transform:uppercase"><?php echo esc_html( $first_title ); ?></p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-			<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0"><?php esc_html_e( 'Created with love and care in Australia', 'woo-gutenberg-products-block' ); ?></h3>
+			<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0"><?php echo esc_html( $second_title ); ?></h3>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'All items are 100% hand-made, using the potter’s wheel or traditional techniques.', 'woo-gutenberg-products-block' ); ?></p>
+			<p><?php echo esc_html( $first_description ); ?></p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:list -->
@@ -68,22 +80,22 @@ $image2 = PatternsHelper::get_image_url( $images, 1, 'images/pattern-placeholder
 		<!-- wp:column {"verticalAlignment":"center","width":"48%"} -->
 		<div class="wp-block-column is-vertically-aligned-center" style="flex-basis:48%">
 			<!-- wp:paragraph {"placeholder":"Content…","style":{"typography":{"textTransform":"uppercase"}}} -->
-			<p style="text-transform:uppercase"><?php esc_html_e( 'About us', 'woo-gutenberg-products-block' ); ?></p>
+			<p style="text-transform:uppercase"><?php echo esc_html( $third_title ); ?></p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:heading {"level":3,"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
-			<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0"><?php esc_html_e( 'Marl is an independent studio and artisanal gallery', 'woo-gutenberg-products-block' ); ?></h3>
+			<h3 class="wp-block-heading" style="margin-top:0;margin-bottom:0"><?php echo esc_html( $fourth_title ); ?></h3>
 			<!-- /wp:heading -->
 
 			<!-- wp:paragraph -->
-			<p><?php esc_html_e( 'We specialize in limited collections of handmade tableware. We collaborate with restaurants and cafes to create unique items that complement the menu perfectly. Please get in touch if you want to know more about our process and pricing.', 'woo-gutenberg-products-block' ); ?></p>
+			<p><?php echo esc_html( $second_description ); ?></p>
 			<!-- /wp:paragraph -->
 
 			<!-- wp:buttons {"style":{"spacing":{"blockGap":"0"}},"fontSize":"small"} -->
 			<div class="wp-block-buttons has-custom-font-size has-small-font-size">
 				<!-- wp:button {"className":"is-style-outline"} -->
 				<div class="wp-block-button is-style-outline">
-					<a class="wp-block-button__link wp-element-button"><?php esc_html_e( 'Learn more', 'woo-gutenberg-products-block' ); ?></a>
+					<a class="wp-block-button__link wp-element-button"><?php echo esc_html( $button ); ?></a>
 				</div>
 				<!-- /wp:button -->
 			</div>
