47493f12f055937d3fd0f8220256602177be1bee
Thomas Roberts <thomas.roberts@automattic.com>
2023-10-16 14:37:47 +0100

    Move Label component to components package

--- packages/components/index.js
+++ packages/components/index.js
@@ -1,3 +1,4 @@
 export { default as CheckboxList } from './checkbox-list';
+export { default as Label } from './label';
 export { default as FormattedMonetaryAmount } from './formatted-monetary-amount';
 export { Chip, RemovableChip } from './chip';
