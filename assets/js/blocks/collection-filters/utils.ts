526e048309723bdb014c0811f602544f6d1cc18b
Tung Du <dinhtungdu@gmail.com>
2023-10-20 11:50:22 +0700

    move private function out

--- assets/js/blocks/collection-filters/utils.ts
+++ assets/js/blocks/collection-filters/utils.ts
@@ -3,55 +3,55 @@
  */
 import { ProductCollectionQuery } from '@woocommerce/blocks/product-collection/types';
 
+const sharedParams: Array< keyof ProductCollectionQuery > = [
+	'exclude',
+	'offset',
+	'order',
+	'search',
+];
+
+const mappedParams: { key: keyof ProductCollectionQuery; map: string }[] = [
+	{ key: 'orderBy', map: 'orderby' },
+	{ key: 'pages', map: 'page' },
+	{ key: 'parents', map: 'parent' },
+	{ key: 'perPage', map: 'per_page' },
+	{ key: 'woocommerceStockStatus', map: 'stock_status' },
+	{ key: 'woocommerceOnSale', map: 'on_sale' },
+	{ key: 'woocommerceHandPickedProducts', map: 'include' },
+];
+
+function mapTaxonomy( taxonomy: string ) {
+	const map: Record< string, string > = {
+		product_tag: 'tag',
+		product_cat: 'cat',
+	};
+
+	return map[ taxonomy ] || `_unstable_tax_${ taxonomy }`;
+}
+
+function getTaxQueryMap( taxQuery: ProductCollectionQuery[ 'taxQuery' ] ) {
+	return Object.entries( taxQuery ).map( ( [ taxonomy, terms ] ) => ( {
+		[ mapTaxonomy( taxonomy ) ]: terms,
+	} ) );
+}
+
+function getAttributeQuery(
+	woocommerceAttributes: ProductCollectionQuery[ 'woocommerceAttributes' ]
+) {
+	if ( ! woocommerceAttributes ) {
+		return {};
+	}
+	return {
+		attributes: woocommerceAttributes.map( ( attribute ) => ( {
+			attribute: attribute.taxonomy,
+			term_id: attribute.termId,
+		} ) ),
+	};
+}
+
 export function formatQuery( query: ProductCollectionQuery ) {
 	if ( ! query ) {
 		return {};
-	}
-
-	const sharedParams: Array< keyof ProductCollectionQuery > = [
-		'exclude',
-		'offset',
-		'order',
-		'search',
-	];
-
-	const mappedParams: { key: keyof ProductCollectionQuery; map: string }[] = [
-		{ key: 'orderBy', map: 'orderby' },
-		{ key: 'pages', map: 'page' },
-		{ key: 'parents', map: 'parent' },
-		{ key: 'perPage', map: 'per_page' },
-		{ key: 'woocommerceStockStatus', map: 'stock_status' },
-		{ key: 'woocommerceOnSale', map: 'on_sale' },
-		{ key: 'woocommerceHandPickedProducts', map: 'include' },
-	];
-
-	function mapTaxonomy( taxonomy: string ) {
-		const map: Record< string, string > = {
-			product_tag: 'tag',
-			product_cat: 'cat',
-		};
-
-		return map[ taxonomy ] || `_unstable_tax_${ taxonomy }`;
-	}
-
-	function getTaxQueryMap( taxQuery: ProductCollectionQuery[ 'taxQuery' ] ) {
-		return Object.entries( taxQuery ).map( ( [ taxonomy, terms ] ) => ( {
-			[ mapTaxonomy( taxonomy ) ]: terms,
-		} ) );
-	}
-
-	function getAttributeQuery(
-		woocommerceAttributes: ProductCollectionQuery[ 'woocommerceAttributes' ]
-	) {
-		if ( ! woocommerceAttributes ) {
-			return {};
-		}
-		return {
-			attributes: woocommerceAttributes.map( ( attribute ) => ( {
-				attribute: attribute.taxonomy,
-				term_id: attribute.termId,
-			} ) ),
-		};
 	}
 
 	const test = Object.assign(
