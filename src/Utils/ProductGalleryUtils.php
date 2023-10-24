fd939b2cdb3494d4304f50686cace93b4bd08cad
roykho <roykho77@gmail.com>
2023-10-24 14:41:58 -0700

    Product Gallery: Add cropped image support

--- src/Utils/ProductGalleryUtils.php
+++ src/Utils/ProductGalleryUtils.php
@@ -6,6 +6,8 @@ namespace Automattic\WooCommerce\Blocks\Utils;
  * {@internal This class and its methods are not intended for public use.}
  */
 class ProductGalleryUtils {
+	const CROP_IMAGE_SIZE = 500;
+	const CROP_IMAGE_SIZE_NAME = '_woo_blocks_product_gallery_crop_full';
 
 	/**
 	 * When requesting a full-size image, this function may return an array with a single image.
@@ -18,9 +20,10 @@ class ProductGalleryUtils {
 	 * @param string $size Image size.
 	 * @param array  $attributes Attributes.
 	 * @param string $wrapper_class Wrapper class.
+	 * @param bool   $crop_images Whether to crop images.
 	 * @return array
 	 */
-	public static function get_product_gallery_images( $post_id, $size = 'full', $attributes = array(), $wrapper_class = '' ) {
+	public static function get_product_gallery_images( $post_id, $size = 'full', $attributes = array(), $wrapper_class = '', $crop_images = false ) {
 		$product_gallery_images = array();
 		$product                = wc_get_product( $post_id );
 
@@ -28,7 +31,13 @@ class ProductGalleryUtils {
 			$all_product_gallery_image_ids = self::get_product_gallery_image_ids( $product );
 
 			if ( 'full' === $size || 'full' !== $size && count( $all_product_gallery_image_ids ) > 1 ) {
+				$size = $crop_images ? self::CROP_IMAGE_SIZE_NAME : $size;
+
 				foreach ( $all_product_gallery_image_ids as $product_gallery_image_id ) {
+					if ( $crop_images ) {
+						self::maybe_generate_intermediate_image( $product_gallery_image_id, self::CROP_IMAGE_SIZE_NAME );
+					}
+
 					$product_image_html = wp_get_attachment_image(
 						$product_gallery_image_id,
 						$size,
@@ -92,5 +101,36 @@ class ProductGalleryUtils {
 		}
 
 		return $unique_image_ids;
+	}
+
+	/**
+	 * Generates the intermediate image sizes when needed
+	 *
+	 * @param int	 $attachment_id Attachment ID.
+	 * @param string $size Image size.
+	 */
+	public static function maybe_generate_intermediate_image( $attachment_id, $size ) {
+		$metadata   = image_get_intermediate_size( $attachment_id, $size );
+		$upload_dir = wp_upload_dir();
+		$image_path = '';
+
+		if ( $metadata ) {
+			$image_path = $upload_dir['basedir'] . '/' . $metadata['path'];
+		}
+
+		/*
+		 * We need to check both if the size metadata exists and if the file exists.
+		 */
+		if ( image_get_intermediate_size( $attachment_id, $size ) && file_exists( $image_path ) ) {
+			return;
+		}
+
+		$image_path         = wp_get_original_image_path( $attachment_id );
+		$new_image_metadata = image_make_intermediate_size( $image_path, self::CROP_IMAGE_SIZE, self::CROP_IMAGE_SIZE, true );
+		$image_metadata     = wp_get_attachment_metadata( $attachment_id );
+
+		$image_metadata['sizes'][$size] = $new_image_metadata;
+
+		wp_update_attachment_metadata( $attachment_id, $image_metadata );
 	}
 }
