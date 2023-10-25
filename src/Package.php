761f3a43c4d6da49a11b3596adfe6b651783d3fd
Daniel Dudzic <daniel.dudzic@automattic.com>
2023-10-25 14:44:26 +0200

    Do a version bump to 11.4.1

--- src/Package.php
+++ src/Package.php
@@ -109,7 +109,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '11.4.0';
+					$version = '11.4.1';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
