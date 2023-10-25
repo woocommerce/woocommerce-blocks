1f583031d56b7fa96a84af3cdd929f41b150baa0
Alba Rincón <alba.rincon@automattic.com>
2023-10-25 14:11:54 +0200

    Fix spacing

--- patterns/header-essential.php
+++ patterns/header-essential.php
@@ -6,21 +6,25 @@
  * Block Types: core/template-part/header
  */
 ?>
+
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"40px","bottom":"40px","left":"40px","right":"40px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group alignfull" style="padding-top:40px;padding-right:40px;padding-bottom:40px;padding-left:40px">
 	<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
-	<div class="wp-block-group"><!-- wp:site-logo /--></div>
+	<div class="wp-block-group">
+		<!-- wp:site-logo /-->
+	</div>
 	<!-- /wp:group -->
 
-	<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+	<!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 	<div class="wp-block-group">
-		<!-- wp:navigation {"ref":1855,"style":{"spacing":{"blockGap":"24px"}}} /-->
-
 		<!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 		<div class="wp-block-group">
 			<!-- wp:woocommerce/mini-cart {"hasHiddenPrice":true} /-->
-			<!-- wp:woocommerce/customer-account {"displayStyle":"icon_only","iconClass":"wc-block-customer-account__account-icon"} /--></div>
+			<!-- wp:woocommerce/customer-account {"displayStyle":"icon_only","iconClass":"wc-block-customer-account__account-icon"} /-->
+		</div>
 		<!-- /wp:group -->
+
+		<!-- wp:navigation {"style":{"spacing":{"blockGap":"24px"}}} /-->
 	</div>
 	<!-- /wp:group -->
 </div>
