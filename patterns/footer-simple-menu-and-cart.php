b8bcc4f776aba795e4d288e0a25dda311987390e
Alba Rincón <alba.rincon@automattic.com>
2023-10-26 10:02:10 +0200

    Remove mini cart

--- patterns/footer-simple-menu-and-cart.php
+++ patterns/footer-simple-menu-and-cart.php
@@ -14,7 +14,6 @@
 		<!-- wp:group {"style":{"spacing":{"blockGap":"24px"}},"layout":{"type":"flex","flexWrap":"wrap"}} -->
 		<div class="wp-block-group">
 			<!-- wp:search {"label":"<?php esc_html_e( 'Search', 'woo-gutenberg-products-block' ); ?>","showLabel":false,"placeholder":"<?php esc_html_e( 'Search', 'woo-gutenberg-products-block' ); ?>","buttonText":"<?php esc_html_e( 'Search', 'woo-gutenberg-products-block' ); ?>","buttonUseIcon":true,"query":{"post_type":"product"},"width":100,"widthUnit":"%"} /-->
-			<!-- wp:woocommerce/mini-cart /-->
 		</div>
 		<!-- /wp:group -->
 
