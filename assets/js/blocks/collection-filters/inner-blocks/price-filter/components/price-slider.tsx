5cc7f3c0b36e2ce0ac7b8f591247d65f89822409
Tung Du <dinhtungdu@gmail.com>
2023-10-30 22:41:38 +0700

    break the edit into smaller components

---
+++ assets/js/blocks/collection-filters/inner-blocks/price-filter/components/price-slider.tsx
@@ -0,0 +1,63 @@
+/**
+ * Internal dependencies
+ */
+import { EditProps } from '../types';
+import { getFormattedPrice } from '../utils';
+
+export const PriceSlider = ( { attributes, context }: EditProps ) => {
+	const { showInputFields } = attributes;
+	const { minPrice, maxPrice, formattedMinPrice, formattedMaxPrice } =
+		getFormattedPrice( context.collectionData );
+
+	const onChange = () => null;
+
+	const priceMin = showInputFields ? (
+		<input
+			className="min"
+			type="text"
+			value={ minPrice }
+			onChange={ onChange }
+		/>
+	) : (
+		<span>{ formattedMinPrice }</span>
+	);
+
+	const priceMax = showInputFields ? (
+		<input
+			className="max"
+			type="text"
+			value={ maxPrice }
+			onChange={ onChange }
+		/>
+	) : (
+		<span>{ formattedMaxPrice }</span>
+	);
+
+	return (
+		<>
+			<div className="range">
+				<div className="range-bar"></div>
+				<input
+					type="range"
+					className="min"
+					min={ minPrice }
+					max={ maxPrice }
+					value={ minPrice }
+					onChange={ onChange }
+				/>
+				<input
+					type="range"
+					className="max"
+					min={ minPrice }
+					max={ maxPrice }
+					value={ maxPrice }
+					onChange={ onChange }
+				/>
+			</div>
+			<div className="text">
+				{ priceMin }
+				{ priceMax }
+			</div>
+		</>
+	);
+};
