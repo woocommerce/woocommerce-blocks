916bb99ebbe042851a6f298b6f96700154e8c12b
Mike Jolley <mike.jolley@me.com>
2023-10-31 13:05:23 +0000

    Notice Banner Docs

--- storybook/preview.js
+++ storybook/preview.js
@@ -19,4 +19,11 @@ export const parameters = {
 		options: {},
 		manual: true,
 	},
+	docs: {
+		toc: {
+			headingSelector: 'h2, h3',
+			title: 'Table of Contents',
+			disable: false,
+		},
+	},
 };
