197e3c20ee933b0f4ecf32b95e5de5f0c3a8f5fd
Alba Rincón <alba.rincon@automattic.com>
2023-10-31 08:19:07 +0100

    Fix url

--- patterns/hero-product-3-split.php
+++ patterns/hero-product-3-split.php
@@ -23,7 +23,7 @@ $third_description  = $content['descriptions'][2]['default'] ?? '';
 <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"left":"0"}}}} -->
 <div class="wp-block-columns alignwide"><!-- wp:column -->
 	<div class="wp-block-column">
-		<!-- wp:cover {"url":"https://store.local/wp-content/plugins/woocommerce-blocks/images/pattern-placeholders/man-person-winter-photography-statue-coat.png","dimRatio":0,"minHeight":800,"minHeightUnit":"px","isDark":false,"layout":{"type":"constrained"}} -->
+		<!-- wp:cover {"url":"<?php echo esc_url( PatternsHelper::get_image_url( $images, 0, 'images/pattern-placeholders/man-person-winter-photography-statue-coat.png' ) ); ?>","dimRatio":0,"minHeight":800,"minHeightUnit":"px","isDark":false,"layout":{"type":"constrained"}} -->
 		<div class="wp-block-cover is-light" style="min-height:800px">
 			<span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
 			<img
