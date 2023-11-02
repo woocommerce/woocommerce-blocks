42122fe2dbc82858ec9e1202c969369cbabbb61f
Jeremy Pry <jeremy.pry@gmail.com>
2023-11-01 21:37:40 -0500

    Add schema data and adjust instantiation method for the class

--- src/Domain/Services/OrderSourceAttribution.php
+++ src/Domain/Services/OrderSourceAttribution.php
@@ -5,6 +5,12 @@ namespace Automattic\WooCommerce\Blocks\Domain\Services;
 
 use Automattic\WooCommerce\Blocks\Assets\Api as AssetApi;
 use Automattic\WooCommerce\Internal\Features\FeaturesController;
+use Automattic\WooCommerce\Internal\Orders\SourceAttributionController;
+use Automattic\WooCommerce\StoreApi\Schemas\ExtendSchema;
+use Automattic\WooCommerce\StoreApi\Schemas\V1\CheckoutSchema;
+use Automattic\WooCommerce\StoreApi\StoreApi;
+use Exception;
+use WP_Error;
 
 /**
  * Class OrderSourceAttribution
@@ -18,24 +24,47 @@ class OrderSourceAttribution {
 	 *
 	 * @var AssetApi
 	 */
-	protected $asset_api;
+	private $asset_api;
 
 	/**
 	 * Instance of the features controller.
 	 *
 	 * @var FeaturesController
 	 */
-	protected $features_controller;
+	private $features_controller;
+
+	/**
+	 * ExtendSchema instance.
+	 *
+	 * @var ExtendSchema
+	 */
+	private $extend_schema;
+
+	/**
+	 * Instance of the source attribution controller.
+	 *
+	 * @var SourceAttributionController
+	 */
+	private $source_attribution_controller;
 
 	/**
 	 * Constructor.
 	 *
-	 * @param AssetApi           $asset_api           Instance of the asset API.
-	 * @param FeaturesController $features_controller Features controller.
+	 * @param AssetApi                    $asset_api                     Instance of the asset API.
+	 * @param ExtendSchema                $extend_schema                 ExtendSchema instance.
+	 * @param FeaturesController          $features_controller           Features controller.
+	 * @param SourceAttributionController $source_attribution_controller Instance of the source attribution controller.
 	 */
-	public function __construct( AssetApi $asset_api, FeaturesController $features_controller ) {
-		$this->asset_api           = $asset_api;
-		$this->features_controller = $features_controller;
+	public function __construct(
+		AssetApi $asset_api,
+		ExtendSchema $extend_schema,
+		FeaturesController $features_controller,
+		SourceAttributionController $source_attribution_controller
+	) {
+		$this->asset_api                     = $asset_api;
+		$this->features_controller           = $features_controller;
+		$this->extend_schema                 = $extend_schema;
+		$this->source_attribution_controller = $source_attribution_controller;
 	}
 
 	/**
@@ -46,6 +75,13 @@ class OrderSourceAttribution {
 	 */
 	public function init() {
 		if ( ! $this->features_controller->feature_is_enabled( 'order_source_attribution' ) ) {
+			return;
+		}
+
+		$this->extend_api();
+
+		// Bail early on admin requests to avoid asset registration.
+		if ( is_admin() ) {
 			return;
 		}
 
@@ -83,5 +119,81 @@ class OrderSourceAttribution {
 	 */
 	private function enqueue_scripts() {
 		wp_enqueue_script( 'wc-blocks-order-source-attribution' );
+	}
+
+	/**
+	 * Extend the Store API.
+	 *
+	 * @return void
+	 */
+	private function extend_api() {
+		try {
+			$this->extend_schema->register_endpoint_data(
+				[
+					'endpoint'        => CheckoutSchema::IDENTIFIER,
+					'namespace'       => 'woocommerce/order-source-attribution',
+					'schema_callback' => $this->get_schema_callback(),
+				]
+			);
+			$this->extend_schema->register_update_callback(
+				[
+					'namespace' => 'woocommerce/order-source-attribution',
+					'callback'  => function( $data ) {
+						// todo: Save the data to the order here.
+					},
+				]
+			);
+		} catch ( Exception $e ) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement
+			// Do nothing.
+		}
+	}
+
+	/**
+	 * Get the schema callback.
+	 *
+	 * @return callable
+	 */
+	private function get_schema_callback() {
+		return function() {
+			$schema = [];
+			$fields = $this->source_attribution_controller->get_fields();
+
+			$validate_callback = function( $value ) {
+				if ( ! is_string( $value ) && null !== $value ) {
+					return new WP_Error(
+						'api-error',
+						sprintf(
+							/* translators: %s is the property type */
+							esc_html__( 'Value of type %s was posted to the source attribution callback', 'woo-gutenberg-products-block' ),
+							gettype( $value )
+						)
+					);
+				}
+
+				return true;
+			};
+
+			$sanitize_callback = function( $value ) {
+				return sanitize_text_field( $value );
+			};
+
+			foreach ( $fields as $field ) {
+				$schema[ $this->source_attribution_controller->get_prefixed_field( $field ) ] = [
+					'description' => sprintf(
+						/* translators: %s is the field name */
+						__( 'Source attribution field: %s', 'woo-gutenberg-products-block' ),
+						esc_html( $field )
+					),
+					'type'        => [ 'string', 'null' ],
+					'context'     => [],
+					'arg_options' => [
+						'validate_callback' => $validate_callback,
+						'sanitize_callback' => $sanitize_callback,
+					],
+				];
+			}
+
+			return $schema;
+		};
 	}
 }
