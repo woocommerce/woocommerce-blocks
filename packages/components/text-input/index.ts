f3414c7bfe4d9a255e7b4beff1329e34a1a5ad63
Thomas Roberts <thomas.roberts@automattic.com>
2023-11-07 12:05:06 +0000

    Move text-input, validated-text-input and validation-error to package

--- packages/components/text-input/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as TextInput } from '../../checkout/components/text-input/text-input';
-export { default as ValidatedTextInput } from '../../checkout/components/text-input/validated-text-input';
