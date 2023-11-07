37e7551400b6b257cedb9f009dc0514a2d2d2eec
Jeremy Pry <jeremy.pry@gmail.com>
2023-11-06 22:48:38 -0600

    Add definition for sourcebuster and update code using it

--- assets/js/extensions/order-source-attribution/index.ts
+++ assets/js/extensions/order-source-attribution/index.ts
@@ -4,22 +4,12 @@
 import { CHECKOUT_STORE_KEY } from '@woocommerce/block-data';
 import { dispatch } from '@wordpress/data';
 
-const sb = window.sbjs || {};
+/**
+ * Internal dependencies
+ */
+import { sbjs } from './sourcebuster.d.ts';
+
 dispatch( CHECKOUT_STORE_KEY ).__internalSetExtensionData(
 	'order-source-attribution',
-	{
-		type: sb.get.current.typ,
-		url: sb.get.current_add.rf,
-		utm_campaign: sb.get.current.cmp,
-		utm_source: sb.get.current.src,
-		utm_medium: sb.get.current.mdm,
-		utm_content: sb.get.current.cnt,
-		utm_id: sb.get.current.id,
-		utm_term: sb.get.current.trm,
-		session_entry: sb.get.current_add.ep,
-		session_start_time: sb.get.current_add.fd,
-		session_pages: sb.get.session.pgs,
-		session_count: sb.get.udata.vst,
-		user_agent: sb.get.udata.uag,
-	}
+	sbjs.get()
 );
