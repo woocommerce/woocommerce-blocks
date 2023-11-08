e46badb3968abfcf18126f7684a673d1cb680b06
Luigi <gigitux@gmail.com>
2023-11-08 18:45:16 +0100

    bump version

--- src/Package.php
+++ src/Package.php
@@ -109,7 +109,7 @@ class Package {
 				NewPackage::class,
 				function ( $container ) {
 					// leave for automated version bumping.
-					$version = '11.5.1';
+					$version = '11.5.2';
 					return new NewPackage(
 						$version,
 						dirname( __DIR__ ),
