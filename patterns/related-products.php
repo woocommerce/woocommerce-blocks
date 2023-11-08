6013022f133166dd87987becc1a34b3c1e903383
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-11-08 12:47:13 +0100

    Add margin to Related Products heading

--- patterns/related-products.php
+++ patterns/related-products.php
@@ -11,8 +11,8 @@
 <div class="wp-block-woocommerce-related-products alignwide">
 		<!-- wp:query {"queryId":0,"query":{"perPage":5,"pages":0,"offset":0,"postType":"product","order":"asc","orderBy":"title","author":"","search":"","exclude":[],"sticky":"","inherit":false},"displayLayout":{"type":"flex","columns":5},"namespace":"woocommerce/related-products","lock":{"remove":true,"move":true}} -->
 		<div class="wp-block-query">
-			<!-- wp:heading -->
-			<h2 class="wp-block-heading">
+			<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}}} -->
+			<h2 class="wp-block-heading" style="margin-top:var(--wp--preset--spacing--30);margin-bottom:var(--wp--preset--spacing--30)">
 				<?php
 					echo sprintf(
 						esc_html__(
