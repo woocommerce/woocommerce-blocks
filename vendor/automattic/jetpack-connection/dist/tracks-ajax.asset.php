ec1c38493a29da8005fa347a8d4a07b077760a51
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-11-08 19:22:04 +0100

    Adding /vendor directory to release

---
+++ vendor/automattic/jetpack-connection/dist/tracks-ajax.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2c644cc46566ed615c42');
---
+++ vendor/automattic/jetpack-connection/dist/tracks-callables.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a96178e4d62fb695caa0');
