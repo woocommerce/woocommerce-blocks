f2e27fe1433a85c735a266df8ec9737db3bf62e7
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-11-09 12:00:53 +0100

    Add Top Rated collection

---
+++ assets/js/blocks/product-collection/collections/top-rated.tsx
@@ -0,0 +1,47 @@
+/**
+ * External dependencies
+ */
+import { InnerBlockTemplate } from '@wordpress/blocks';
+import { Icon, starEmpty } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import {
+	DEFAULT_ATTRIBUTES,
+	INNER_BLOCKS_PRODUCT_TEMPLATE,
+} from '../constants';
+
+const collection = {
+	name: 'woocommerce-blocks/product-collection/top-rated',
+	title: 'Top Rated',
+	icon: <Icon icon={ starEmpty } />,
+	description: 'Products gaining popularity based on recent activity.',
+	keywords: [],
+	scope: [],
+};
+
+const attributes = {
+	...DEFAULT_ATTRIBUTES,
+	displayLayout: {
+		type: 'flex',
+		columns: 3,
+	},
+	query: {
+		...DEFAULT_ATTRIBUTES.query,
+		inherit: false,
+		orderBy: 'rating',
+		order: 'desc',
+		perPage: 9,
+		pages: 1,
+	},
+	collection: collection.name,
+};
+
+const innerBlocks: InnerBlockTemplate[] = [ INNER_BLOCKS_PRODUCT_TEMPLATE ];
+
+export default {
+	...collection,
+	attributes,
+	innerBlocks,
+};
