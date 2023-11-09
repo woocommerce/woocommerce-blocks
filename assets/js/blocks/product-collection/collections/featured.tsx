2009efd6d70743763fc1d2f2c74e194fa308e4ee
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-11-09 11:59:59 +0100

    Add Featured collection

---
+++ assets/js/blocks/product-collection/collections/featured.tsx
@@ -0,0 +1,46 @@
+/**
+ * External dependencies
+ */
+import { InnerBlockTemplate } from '@wordpress/blocks';
+import { Icon, starFilled } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import {
+	DEFAULT_ATTRIBUTES,
+	INNER_BLOCKS_PRODUCT_TEMPLATE,
+} from '../constants';
+
+const collection = {
+	name: 'woocommerce-blocks/product-collection/featured',
+	title: 'Featured',
+	icon: <Icon icon={ starFilled } />,
+	description: 'Products that have been marked as featured.',
+	keywords: [ 'best selling' ],
+	scope: [],
+};
+
+const attributes = {
+	...DEFAULT_ATTRIBUTES,
+	displayLayout: {
+		type: 'flex',
+		columns: 3,
+	},
+	query: {
+		...DEFAULT_ATTRIBUTES.query,
+		inherit: false,
+		featured: true,
+		perPage: 9,
+		pages: 1,
+	},
+	collection: collection.name,
+};
+
+const innerBlocks: InnerBlockTemplate[] = [ INNER_BLOCKS_PRODUCT_TEMPLATE ];
+
+export default {
+	...collection,
+	attributes,
+	innerBlocks,
+};
