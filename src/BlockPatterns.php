f16589075e6fe25686882d38946584eb0ef56bce
Alba Rincón <alba.rincon@automattic.com>
2023-11-09 14:49:16 +0100

    Load content and images on registration if the patterns is in the dictionary

--- src/BlockPatterns.php
+++ src/BlockPatterns.php
@@ -4,6 +4,7 @@ namespace Automattic\WooCommerce\Blocks;
 use Automattic\WooCommerce\Blocks\AI\Connection;
 use Automattic\WooCommerce\Blocks\Images\Pexels;
 use Automattic\WooCommerce\Blocks\Domain\Package;
+use Automattic\WooCommerce\Blocks\Patterns\PatternsHelper;
 use Automattic\WooCommerce\Blocks\Patterns\PatternUpdater;
 use Automattic\WooCommerce\Blocks\Patterns\ProductUpdater;
 
@@ -129,6 +130,8 @@ class BlockPatterns {
 			return;
 		}
 
+		$dictionary = PatternsHelper::get_patterns_dictionary();
+
 		foreach ( $files as $file ) {
 			$pattern_data = get_file_data( $file, $default_headers );
 
@@ -226,10 +229,17 @@ class BlockPatterns {
 				$pattern_data['description'] = translate_with_gettext_context( $pattern_data['description'], 'Pattern description', 'woo-gutenberg-products-block' );
 			}
 
+			$pattern_data_from_dictionary = $this->get_pattern_from_dictionary( $dictionary, $pattern_data['slug'] );
+
 			// The actual pattern content is the output of the file.
 			ob_start();
+			if ( ! is_null( $pattern_data_from_dictionary ) ) {
+				$content = $pattern_data_from_dictionary['content'];
+				$images  = $pattern_data_from_dictionary['images'] ?? array();
+			}
 			include $file;
 			$pattern_data['content'] = ob_get_clean();
+
 			if ( ! $pattern_data['content'] ) {
 				continue;
 			}
@@ -353,5 +363,23 @@ class BlockPatterns {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Filter the patterns dictionary to get the pattern data corresponding to the pattern slug.
+	 *
+	 * @param array  $dictionary The patterns dictionary.
+	 * @param string $slug The pattern slug.
+	 *
+	 * @return array|null
+	 */
+	private function get_pattern_from_dictionary( $dictionary, $slug ) {
+		foreach ( $dictionary as $pattern_dictionary ) {
+			if ( $pattern_dictionary['slug'] === $slug ) {
+				return $pattern_dictionary;
+			}
+		}
+
+		return null;
 	}
 }
