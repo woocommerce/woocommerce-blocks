ad52a498b2dbc56a916d34dca8176c4fb9feb193
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-11-09 12:00:08 +0100

    Add On Sale collection

---
+++ assets/js/blocks/product-collection/collections/on-sale.tsx
@@ -0,0 +1,46 @@
+/**
+ * External dependencies
+ */
+import { InnerBlockTemplate } from '@wordpress/blocks';
+import { Icon, percent } from '@wordpress/icons';
+
+/**
+ * Internal dependencies
+ */
+import {
+	DEFAULT_ATTRIBUTES,
+	INNER_BLOCKS_PRODUCT_TEMPLATE,
+} from '../constants';
+
+const collection = {
+	name: 'woocommerce-blocks/product-collection/on-sale',
+	title: 'On Sale',
+	icon: <Icon icon={ percent } />,
+	description: 'Products currently marked on sale.',
+	keywords: [],
+	scope: [],
+};
+
+const attributes = {
+	...DEFAULT_ATTRIBUTES,
+	displayLayout: {
+		type: 'flex',
+		columns: 3,
+	},
+	query: {
+		...DEFAULT_ATTRIBUTES.query,
+		inherit: false,
+		woocommerceOnSale: true,
+		perPage: 9,
+		pages: 1,
+	},
+	collection: collection.name,
+};
+
+const innerBlocks: InnerBlockTemplate[] = [ INNER_BLOCKS_PRODUCT_TEMPLATE ];
+
+export default {
+	...collection,
+	attributes,
+	innerBlocks,
+};
