fe4b161815a17aa628debe08b3a094e629c4d07e
Alexandre Lara <allexandrelara@gmail.com>
2023-11-13 14:45:19 -0300

    Add doc comments to TemplateChangeDetector class

--- assets/js/atomic/utils/blocks-registration-manager/template-change-detector.ts
+++ assets/js/atomic/utils/blocks-registration-manager/template-change-detector.ts
@@ -15,6 +15,16 @@ export interface TemplateChangeDetectorObserver {
 	run( subject: TemplateChangeDetectorSubject ): void;
 }
 
+/**
+ * This class implements the TemplateChangeDetectorSubject interface and is responsible for detecting changes in the
+ * current template or page and notifying any observers of these changes. It maintains a list of observers and provides methods
+ * to add observers and notify them of changes.
+ *
+ * The class also provides methods to get the previous and current template IDs and whether the editor is in a post or page.
+ *
+ * The `checkIfTemplateHasChangedAndNotifySubscribers` method is the main method of the class. It checks if the current
+ * template has changed and, if so, notifies all observers.
+ */
 export class TemplateChangeDetector implements TemplateChangeDetectorSubject {
 	private previousTemplateId: string | undefined;
 	private currentTemplateId: string | undefined;
@@ -54,6 +64,14 @@ export class TemplateChangeDetector implements TemplateChangeDetectorSubject {
 		return this.isPostOrPage;
 	}
 
+	/**
+	 * Parses the template ID.
+	 *
+	 * This method takes a template or a post ID and returns it parsed in the expected format.
+	 *
+	 * @param {string | number | undefined} templateId - The template ID to parse.
+	 * @return {string | undefined} The parsed template ID.
+	 */
 	private parseTemplateId(
 		templateId: string | number | undefined
 	): string | undefined {
@@ -63,6 +81,11 @@ export class TemplateChangeDetector implements TemplateChangeDetectorSubject {
 		return templateId?.split( '//' )[ 1 ];
 	}
 
+	/**
+	 * Checks if the current template or page has changed and notifies subscribers.
+	 *
+	 * If the current template ID has changed and is not undefined (which means that it is not a page, post or template), it notifies all subscribers.
+	 */
 	public checkIfTemplateHasChangedAndNotifySubscribers(): void {
 		this.previousTemplateId = this.currentTemplateId;
 
