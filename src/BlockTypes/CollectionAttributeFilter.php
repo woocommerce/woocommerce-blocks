3106442506ec879568d307179d547be464b085e1
Tung Du <dinhtungdu@gmail.com>
2023-11-15 07:53:07 +0700

    WIP checkbox list

--- src/BlockTypes/CollectionAttributeFilter.php
+++ src/BlockTypes/CollectionAttributeFilter.php
@@ -68,45 +68,38 @@ final class CollectionAttributeFilter extends AbstractBlock {
 		ob_start();
 		echo '<ul class="wc-block-checkbox-list wc-block-components-checkbox-list wc-block-stock-filter-list">';
 		foreach ( $options as $option ) {
-			printf(
-				$this->get_list_item_template( $attributes ),
-				$option['slug'] . '-' . $option['term_id'],
-				$option['name'],
-				$option['count'],
-				sprintf( _n( '%d product', '%d products', $option['count'], 'woo-gutenberg-products-block' ), $option['count'] )
-			);
+			$this->render_list_item_template( $option, $attributes['showCounts'] );
 		}
 		echo '</ul>';
 		return ob_get_clean();
 	}
 
-	private function get_list_item_template( $attributes ) {
-		if ( ! $attributes['showCounts'] ) {
-			return '<li>
-				<div class="wc-block-components-checkbox wc-block-checkbox-list__checkbox">
-					<label for="%1$s">
-						<input id="%1$s" class="wc-block-components-checkbox__input" type="checkbox" aria-invalid="false">
-						<svg class="wc-block-components-checkbox__mark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>
-						<span class="wc-block-components-checkbox__label">%2$s</span>
-					</label>
-				</div>
-			</li>';
-		}
+	private function render_list_item_template( $option, $show_counts ) {
+		$count_html = $show_counts ?
+			sprintf( '<span class="wc-filter-element-label-list-count">
+				<span aria-hidden="true">%1$s</span>
+				<span class="screen-reader-text">%2$s</span>
+				</span>',
+				$option['count'],
+				sprintf( _n( '%d product', '%d products', $option['count'], 'woo-gutenberg-products-block' ), $option['count'] )
+			) :
+			'';
 
-		return '<li>
+		$template = '<li>
 			<div class="wc-block-components-checkbox wc-block-checkbox-list__checkbox">
 				<label for="%1$s">
 					<input id="%1$s" class="wc-block-components-checkbox__input" type="checkbox" aria-invalid="false">
 					<svg class="wc-block-components-checkbox__mark" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20"><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"></path></svg>
-					<span class="wc-block-components-checkbox__label">
-						%2$s
-						<span class="wc-filter-element-label-list-count">
-							<span aria-hidden="true">%3$s</span>
-							<span class="screen-reader-text">%4$s</span>
-						</span>
-					</span>
+					<span class="wc-block-components-checkbox__label">%2$s%3$s</span>
 				</label>
 			</div>
 		</li>';
+
+		printf(
+			$template,
+			$option['slug'] . '-' . $option['term_id'],
+			$option['name'],
+			$count_html
+		);
 	}
 }
