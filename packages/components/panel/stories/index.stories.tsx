50fd990c96fc6cba8650868077427044216bf718
Thomas Roberts <thomas.roberts@automattic.com>
2023-11-16 14:06:01 +0000

    Add Panel story

---
+++ packages/components/panel/stories/index.stories.tsx
@@ -0,0 +1,93 @@
+/**
+ * External dependencies
+ */
+import type { StoryFn, Meta } from '@storybook/react';
+
+/**
+ * Internal dependencies
+ */
+import Panel, { type PanelProps } from '..';
+
+export default {
+	title: 'External Components/Panel',
+	component: Panel,
+	argTypes: {
+		children: {
+			control: 'null',
+			description: "The content element to display in the panel's body",
+			table: {
+				type: {
+					summary: 'ReactNode',
+				},
+			},
+		},
+		className: {
+			description: 'Additional CSS classes to be added to the panel.',
+			control: 'text',
+			table: {
+				type: {
+					summary: 'string',
+				},
+			},
+		},
+		initialOpen: {
+			description: 'Whether the panel body should be visible by default.',
+			table: {
+				type: {
+					summary: 'boolean',
+				},
+			},
+		},
+		title: {
+			control: 'null',
+			description: "The React element to display as the panel's title",
+			table: {
+				type: {
+					summary: 'ReactNode',
+				},
+			},
+		},
+		titleTag: {
+			control: 'text',
+			description:
+				'The HTML tag used to display the title element. Defaults to `div`.',
+			table: {
+				type: {
+					summary: 'string',
+				},
+			},
+		},
+	},
+} as Meta< PanelProps >;
+
+const Template: StoryFn< PanelProps > = ( args ) => {
+	return (
+		<Panel { ...args }>
+			<div style={ { paddingBottom: '.750em' } }>
+				This is the content rendered inside the panel.
+			</div>
+		</Panel>
+	);
+};
+
+export const Default = Template.bind( {} );
+Default.args = {
+	title: (
+		<div style={ { paddingBottom: '.375em', marginBottom: '.375em' } }>
+			Title
+		</div>
+	),
+	hasBorder: true,
+	initialOpen: false,
+};
+
+export const InitialOpen = Template.bind( {} );
+InitialOpen.args = {
+	title: (
+		<div style={ { paddingBottom: '.375em', marginBottom: '.375em' } }>
+			Title
+		</div>
+	),
+	hasBorder: true,
+	initialOpen: true,
+};
