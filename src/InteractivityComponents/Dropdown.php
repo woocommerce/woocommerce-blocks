1a377cf8512e92091516de96c044413b36b34f45
Sam Seay <samueljseay@gmail.com>
2023-11-20 14:14:49 +0800

    Fix indentation of HTML in server Dropdown rendering

--- src/InteractivityComponents/Dropdown.php
+++ src/InteractivityComponents/Dropdown.php
@@ -74,7 +74,7 @@ class Dropdown {
 					</ul>
 				</div>
 			</div>
-								</div>
+		</div>
 		<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="30" height="30" >
 			<path d="M17.5 11.6L12 16l-5.5-4.4.9-1.2L12 14l4.5-3.6 1 1.2z" ></path>
 		</svg>      
