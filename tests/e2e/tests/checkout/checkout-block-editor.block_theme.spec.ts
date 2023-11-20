80b56361ae4d72433b3d2c456128c4206d5c5296
Thomas Roberts <thomas.roberts@automattic.com>
2023-11-20 15:52:25 +0000

    Add test for render/insert once

--- tests/e2e/tests/checkout/checkout-block-editor.block_theme.spec.ts
+++ tests/e2e/tests/checkout/checkout-block-editor.block_theme.spec.ts
@@ -1,13 +1,28 @@
 /**
  * External dependencies
  */
-import { test as base } from '@woocommerce/e2e-playwright-utils';
+import { test as base, expect } from '@woocommerce/e2e-playwright-utils';
+import { BlockData } from '@woocommerce/e2e-types';
 
 /**
  * Internal dependencies
  */
 import { CheckoutPage } from './checkout.page';
 
+const blockData: BlockData = {
+	name: 'Checkout',
+	slug: 'woocommerce/checkout',
+	mainClass: '.wp-block-woocommerce-checkout',
+	selectors: {
+		editor: {
+			block: '.wp-block-woocommerce-checkout',
+			insertButton: "//button//span[text()='Checkout']",
+		},
+		frontend: {
+			block: '.wp-block-woocommerce-checkout',
+		},
+	},
+};
 const test = base.extend< { pageObject: CheckoutPage } >( {
 	pageObject: async ( { page }, use ) => {
 		const pageObject = new CheckoutPage( {
@@ -25,6 +40,26 @@ test.describe( 'Merchant → Checkout block', () => {
 				postType: 'wp_template',
 			} );
 			await editorUtils.enterEditMode();
+		} );
+		test( 'renders without crashing and can only be inserted once', async ( {
+			page,
+			editorUtils,
+		} ) => {
+			const blockPresence = await editorUtils.getBlockByName(
+				blockData.slug
+			);
+			expect( blockPresence ).toBeTruthy();
+
+			await editorUtils.openGlobalBlockInserter();
+			await page.getByPlaceholder( 'Search' ).fill( blockData.slug );
+			const checkoutBlockButton = page.getByRole( 'option', {
+				name: blockData.name,
+				exact: true,
+			} );
+			await expect( checkoutBlockButton ).toHaveAttribute(
+				'aria-disabled',
+				'true'
+			);
 		} );
 	} );
 } );
