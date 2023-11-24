7509e5ed514d2d0cf8e54288fc56cb4b9b3a62f0
Karol Manijak <20098064+kmanijak@users.noreply.github.com>
2023-11-24 08:33:08 +0100

    Bring back Display controls in Product Collection Toolbar

--- assets/js/blocks/product-collection/edit/toolbar-controls/index.tsx
+++ assets/js/blocks/product-collection/edit/toolbar-controls/index.tsx
@@ -9,14 +9,15 @@ import { BlockControls } from '@wordpress/block-editor';
  */
 import { setQueryAttribute } from '../../utils';
 import DisplaySettingsToolbar from './display-settings-toolbar';
+import DisplayLayoutToolbar from './display-layout-toolbar';
 import PatternChooserToolbar from './coolection-chooser-toolbar';
 import type { ProductCollectionEditComponentProps } from '../../types';
 
 export default function ToolbarControls(
 	props: ProductCollectionEditComponentProps
 ) {
-	const { attributes, openPatternSelectionModal } = props;
-	const { query } = attributes;
+	const { attributes, openPatternSelectionModal, setAttributes } = props;
+	const { query, displayLayout } = attributes;
 
 	const setQueryAttributeBind = useMemo(
 		() => setQueryAttribute.bind( null, props ),
@@ -33,6 +34,10 @@ export default function ToolbarControls(
 					<DisplaySettingsToolbar
 						query={ query }
 						setQueryAttribute={ setQueryAttributeBind }
+					/>
+					<DisplayLayoutToolbar
+						displayLayout={ displayLayout }
+						setAttributes={ setAttributes }
 					/>
 				</>
 			) }
