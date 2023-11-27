d503b4dadde2333953b3a8300484df475cc63b3e
Thomas Roberts <thomas.roberts@automattic.com>
2023-11-27 18:07:18 +0000

    Check account is created correctly

--- tests/e2e/tests/checkout/checkout-block.shopper.block_theme.side_effects.spec.ts
+++ tests/e2e/tests/checkout/checkout-block.shopper.block_theme.side_effects.spec.ts
@@ -6,8 +6,8 @@ import { expect, test as base } from '@woocommerce/e2e-playwright-utils';
 /**
  * Internal dependencies
  */
-import { REGULAR_PRICED_PRODUCT_NAME } from '../checkout/constants';
-import { CheckoutPage } from '../checkout/checkout.page';
+import { REGULAR_PRICED_PRODUCT_NAME } from './constants';
+import { CheckoutPage } from './checkout.page';
 
 const test = base.extend< { checkoutPageObject: CheckoutPage } >( {
 	checkoutPageObject: async ( { page }, use ) => {
@@ -19,12 +19,14 @@ const test = base.extend< { checkoutPageObject: CheckoutPage } >( {
 } );
 
 test.describe( 'Shopper → Account', () => {
+	// Become a logged out user.
 	test.use( {
 		storageState: {
 			origins: [],
 			cookies: [],
 		},
 	} );
+
 	test.beforeAll( async ( { requestUtils } ) => {
 		await requestUtils.rest( {
 			method: 'PUT',
@@ -44,7 +46,11 @@ test.describe( 'Shopper → Account', () => {
 		await frontendUtils.goToCheckout();
 	} );
 
-	test( 'Shopper can log in to an existing account', async ( { page } ) => {
+	test( 'Shopper can log in to an existing account and can create an account', async ( {
+		requestUtils,
+		checkoutPageObject,
+		page,
+	} ) => {
 		//Get the login link from checkout page.
 		const loginLink = page.getByText( 'Log in.' );
 		const loginLinkHref = await loginLink.getAttribute( 'href' );
@@ -54,5 +60,31 @@ test.describe( 'Shopper → Account', () => {
 			`${ process.env.WORDPRESS_BASE_URL }/my-account/?redirect_to`
 		);
 		expect( loginLinkHref ).toContain( `checkout` );
+
+		await requestUtils.rest( {
+			method: 'PUT',
+			path: 'wc/v3/settings/account/woocommerce_enable_signup_and_login_from_checkout',
+			data: { value: 'yes' },
+		} );
+
+		await page.reload();
+
+		const createAccount = page.getByLabel( 'Create an account?' );
+		await createAccount.check();
+		const testEmail = `test${ Math.random() * 10 }@example.com`;
+		await checkoutPageObject.fillInCheckoutWithTestData( {
+			email: testEmail,
+		} );
+		await checkoutPageObject.placeOrder();
+
+		// Get users from API with same email used when purchasing.
+		await requestUtils
+			.rest( {
+				method: 'GET',
+				path: `wc/v3/customers?email=${ testEmail }`,
+			} )
+			.then( ( response ) => {
+				expect( response[ 0 ].email ).toBe( testEmail );
+			} );
 	} );
 } );
