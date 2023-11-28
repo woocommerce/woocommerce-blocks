6233636c7c4621d4cf87e53e3a679592be5fb1b7
Albert Juhé Lluveras <contact@albertjuhe.com>
2023-11-28 15:45:21 +0100

    Add Reviews by Category block tests

---
+++ tests/e2e/tests/reviews-by-category/reviews-by-category.block_theme.spec.ts
@@ -0,0 +1,146 @@
+/**
+ * External dependencies
+ */
+import { expect, test } from '@woocommerce/e2e-playwright-utils';
+import { default as WooCommerceRestApi } from '@woocommerce/woocommerce-rest-api';
+
+const blockData = {
+	name: 'woocommerce/reviews-by-category',
+	selectors: {
+		frontend: {
+			firstReview:
+				'.wc-block-review-list-item__item:first-child .wc-block-review-list-item__text p',
+		},
+	},
+};
+
+test.describe( `${ blockData.name } Block`, () => {
+	let categoryId: number;
+	let productId: number;
+	let firstReviewId: number;
+	let secondReviewId: number;
+
+	// Create category, product and reviews.
+	test.beforeAll( async ( { baseURL } ) => {
+		const api = new WooCommerceRestApi( {
+			url: baseURL,
+			consumerKey: process.env.CONSUMER_KEY,
+			consumerSecret: process.env.CONSUMER_SECRET,
+			version: 'wc/v3',
+		} );
+		await api
+			.post( 'products/categories', {
+				name: 'Products with reviews',
+			} )
+			.then( ( response ) => {
+				categoryId = response.data.id;
+			} );
+		await api
+			.post( 'products', {
+				name: 'Product with reviews',
+				type: 'simple',
+				regular_price: '12.99',
+				categories: [ { id: categoryId } ],
+			} )
+			.then( ( response ) => {
+				productId = response.data.id;
+			} );
+		await api
+			.post( 'products/reviews', {
+				product_id: productId,
+				review: 'Nice album!',
+				reviewer: 'John Doe',
+				reviewer_email: 'john.doe@example.com',
+				rating: 5,
+			} )
+			.then( ( response ) => {
+				firstReviewId = response.data.id;
+			} );
+		await api
+			.post( 'products/reviews', {
+				product_id: productId,
+				review: 'Not bad.',
+				reviewer: 'John Doe',
+				reviewer_email: 'john.doe@example.com',
+				rating: 4,
+			} )
+			.then( ( response ) => {
+				secondReviewId = response.data.id;
+			} );
+	} );
+
+	// Remove category, product and reviews.
+	test.afterAll( async ( { baseURL } ) => {
+		const api = new WooCommerceRestApi( {
+			url: baseURL,
+			consumerKey: process.env.CONSUMER_KEY,
+			consumerSecret: process.env.CONSUMER_SECRET,
+			version: 'wc/v3',
+		} );
+		await api.delete( `products/reviews/${ firstReviewId }`, {
+			force: true,
+		} );
+		await api.delete( `products/reviews/${ secondReviewId }`, {
+			force: true,
+		} );
+		await api.delete( `products/${ productId }`, {
+			force: true,
+		} );
+		await api.delete( `products/categories/${ categoryId }`, {
+			force: true,
+		} );
+	} );
+
+	test( 'renders a review in the editor and the frontend', async ( {
+		admin,
+		editor,
+		page,
+		editorUtils,
+	} ) => {
+		await admin.createNewPost();
+		await editor.insertBlock( { name: blockData.name } );
+		const productCheckbox = page.getByLabel(
+			'Products with reviews, has 2 reviews'
+		);
+		productCheckbox.check();
+		await expect( productCheckbox ).toBeChecked();
+		const doneButton = page.getByRole( 'button', { name: 'Done' } );
+		await doneButton.click();
+
+		await expect( page.getByText( 'Nice album!' ) ).toBeVisible();
+
+		await editorUtils.publishAndVisitPost();
+
+		await expect( page.getByText( 'Nice album!' ) ).toBeVisible();
+	} );
+
+	test( 'can change sort order in the frontend', async ( {
+		admin,
+		editor,
+		page,
+		frontendUtils,
+		editorUtils,
+	} ) => {
+		await admin.createNewPost();
+		await editor.insertBlock( { name: blockData.name } );
+		const productCheckbox = page.getByLabel(
+			'Products with reviews, has 2 reviews'
+		);
+		productCheckbox.check();
+		await expect( productCheckbox ).toBeChecked();
+		const doneButton = page.getByRole( 'button', { name: 'Done' } );
+		await doneButton.click();
+		await editorUtils.publishAndVisitPost();
+
+		const block = await frontendUtils.getBlockByName( blockData.name );
+		let firstReview;
+		firstReview = block.locator( blockData.selectors.frontend.firstReview );
+		await expect( firstReview ).toHaveText( 'Not bad.' );
+
+		const select = page.getByLabel( 'Order by' );
+		select.selectOption( 'Highest rating' );
+
+		firstReview = block.locator( blockData.selectors.frontend.firstReview );
+		await expect( firstReview ).toHaveText( 'Nice album!' );
+	} );
+} );
