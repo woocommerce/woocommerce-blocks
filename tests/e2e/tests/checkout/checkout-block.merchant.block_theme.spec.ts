a20d58c93d5d24f1d9aaeebd65238c865f8cd6dc
Thomas Roberts <thomas.roberts@automattic.com>
2023-12-01 11:31:40 +0000

    Remove unneeded spinner wait time

--- tests/e2e/tests/checkout/checkout-block.merchant.block_theme.spec.ts
+++ tests/e2e/tests/checkout/checkout-block.merchant.block_theme.spec.ts
@@ -41,22 +41,13 @@ test.describe( 'Merchant → Checkout', () => {
 	// `as string` is safe here because we know the variable is a string, it is defined above.
 	const blockSelectorInEditor = blockData.selectors.editor.block as string;
 
-	test.beforeEach( async ( { editorUtils, admin, editor, page } ) => {
+	test.beforeEach( async ( { editorUtils, admin, editor } ) => {
 		await admin.visitSiteEditor( {
 			postId: 'woocommerce/woocommerce//page-checkout',
 			postType: 'wp_template',
 		} );
 		await editorUtils.enterEditMode();
 		await editor.openDocumentSettingsSidebar();
-
-		await page
-			// Spinner was used instead of the progress bar in an earlier version of
-			// the site editor.
-			.locator( '.edit-site-canvas-loader, .edit-site-canvas-spinner' )
-			// Bigger timeout is needed for larger entities, for example the large
-			// post html fixture that we load for performance tests, which often
-			// doesn't make it under the default 10 seconds.
-			.waitFor( { state: 'hidden', timeout: 60_000 } );
 	} );
 
 	test.describe( 'Can adjust T&S and Privacy Policy options', () => {
