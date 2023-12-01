007c05f21e4b2270070b04f61eea9fe1e1c0d224
Saad Tarhi <saad.trh@gmail.com>
2023-12-01 15:50:45 +0100

    Remove unnecessary login as admin

--- tests/e2e/block-theme.setup.ts
+++ tests/e2e/block-theme.setup.ts
@@ -8,24 +8,7 @@ import {
 } from '@woocommerce/e2e-utils';
 import { test as setup, expect } from '@woocommerce/e2e-playwright-utils';
 
-/**
- * Internal dependencies
- */
-import { admin as adminData } from './test-data/data/data';
-
 setup( 'Sets up the block theme', async ( { admin } ) => {
-	await admin.page.goto( `/wp-admin`, { waitUntil: 'commit' } );
-
-	const isLoggedOut = await admin.page
-		.locator( 'input[name="log"]' )
-		.isVisible();
-
-	if ( isLoggedOut ) {
-		await admin.page.fill( 'input[name="log"]', adminData.username );
-		await admin.page.fill( 'input[name="pwd"]', adminData.password );
-		await admin.page.click( 'text=Log In' );
-	}
-
 	await cli(
 		`npm run wp-env run tests-cli -- wp theme install ${ BLOCK_THEME_SLUG } --activate`
 	);
