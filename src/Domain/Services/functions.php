48ee2ddf7c1361e43e41dc56ed25a7337ee4541b
Thomas Roberts <thomas.roberts@automattic.com>
2023-12-05 17:41:05 +0000

    Add woocommerce_blocks_register_checkout_field function

---
+++ src/Domain/Services/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+use Automattic\WooCommerce\Blocks\Package;
+
+if ( ! function_exists( 'woocommerce_blocks_register_checkout_field' ) ) {
+
+	/**
+	 * Register a checkout field.
+	 *
+	 * @param array $options Field arguments.
+	 * @throws Exception If field registration fails.
+	 */
+	function woocommerce_blocks_register_checkout_field( $options ) {
+		$checkout_fields = Package::container()->get( \Automattic\WooCommerce\Blocks\Domain\Services\CheckoutFields::class );
+		$result          = $checkout_fields->register_checkout_field( $options );
+		if ( is_wp_error( $result ) ) {
+			throw new Exception( $result->get_error_message() );
+		}
+	}
+}
