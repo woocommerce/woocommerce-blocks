58d422b7ac423202c5a37673f4ef43a9659d177e
Nadir Seghir <nadir.seghir@gmail.com>
2023-12-08 14:11:33 +0100

    fix unit test

--- assets/js/settings/shared/default-fields.ts
+++ assets/js/settings/shared/default-fields.ts
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Internal dependencies
  */
 import { getSetting } from './utils';
@@ -67,10 +72,128 @@ export interface BillingAddress extends ShippingAddress {
 }
 export type CountryAddressFields = Record< string, AddressFields >;
 
+export const defaultFieldsDefinition: AddressFields = {
+	first_name: {
+		label: __( 'First name', 'woo-gutenberg-products-block' ),
+		optionalLabel: __(
+			'First name (optional)',
+			'woo-gutenberg-products-block'
+		),
+		autocomplete: 'given-name',
+		autocapitalize: 'sentences',
+		required: true,
+		hidden: false,
+		index: 10,
+	},
+	last_name: {
+		label: __( 'Last name', 'woo-gutenberg-products-block' ),
+		optionalLabel: __(
+			'Last name (optional)',
+			'woo-gutenberg-products-block'
+		),
+		autocomplete: 'family-name',
+		autocapitalize: 'sentences',
+		required: true,
+		hidden: false,
+		index: 20,
+	},
+	company: {
+		label: __( 'Company', 'woo-gutenberg-products-block' ),
+		optionalLabel: __(
+			'Company (optional)',
+			'woo-gutenberg-products-block'
+		),
+		autocomplete: 'organization',
+		autocapitalize: 'sentences',
+		required: false,
+		hidden: false,
+		index: 30,
+	},
+	address_1: {
+		label: __( 'Address', 'woo-gutenberg-products-block' ),
+		optionalLabel: __(
+			'Address (optional)',
+			'woo-gutenberg-products-block'
+		),
+		autocomplete: 'address-line1',
+		autocapitalize: 'sentences',
+		required: true,
+		hidden: false,
+		index: 40,
+	},
+	address_2: {
+		label: __( 'Apartment, suite, etc.', 'woo-gutenberg-products-block' ),
+		optionalLabel: __(
+			'Apartment, suite, etc. (optional)',
+			'woo-gutenberg-products-block'
+		),
+		autocomplete: 'address-line2',
+		autocapitalize: 'sentences',
+		required: false,
+		hidden: false,
+		index: 50,
+	},
+	country: {
+		label: __( 'Country/Region', 'woo-gutenberg-products-block' ),
+		optionalLabel: __(
+			'Country/Region (optional)',
+			'woo-gutenberg-products-block'
+		),
+		autocomplete: 'country',
+		required: true,
+		hidden: false,
+		index: 60,
+	},
+	city: {
+		label: __( 'City', 'woo-gutenberg-products-block' ),
+		optionalLabel: __( 'City (optional)', 'woo-gutenberg-products-block' ),
+		autocomplete: 'address-level2',
+		autocapitalize: 'sentences',
+		required: true,
+		hidden: false,
+		index: 70,
+	},
+	state: {
+		label: __( 'State/County', 'woo-gutenberg-products-block' ),
+		optionalLabel: __(
+			'State/County (optional)',
+			'woo-gutenberg-products-block'
+		),
+		autocomplete: 'address-level1',
+		autocapitalize: 'sentences',
+		required: true,
+		hidden: false,
+		index: 80,
+	},
+	postcode: {
+		label: __( 'Postal code', 'woo-gutenberg-products-block' ),
+		optionalLabel: __(
+			'Postal code (optional)',
+			'woo-gutenberg-products-block'
+		),
+		autocomplete: 'postal-code',
+		autocapitalize: 'characters',
+		required: true,
+		hidden: false,
+		index: 90,
+	},
+	phone: {
+		label: __( 'Phone', 'woo-gutenberg-products-block' ),
+		optionalLabel: __( 'Phone (optional)', 'woo-gutenberg-products-block' ),
+		autocomplete: 'tel',
+		type: 'tel',
+		required: true,
+		hidden: false,
+		index: 100,
+	},
+};
+
 /**
  * Default field properties.
  */
-export const defaultFields: AddressFields =
-	getSetting< AddressFields >( 'defaultFields' );
+export const defaultFields: AddressFields = getSetting< AddressFields >(
+	'defaultFields',
+	defaultFieldsDefinition
+);
 
 export default defaultFields;
