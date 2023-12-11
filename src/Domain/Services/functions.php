9d25427146deb4eee0e7b4bb83e73f87113275aa
Thomas Roberts <thomas.roberts@automattic.com>
2023-12-11 14:48:22 +0000

    Ensure register field runs only when woocommerce_blocks_loaded fired

--- src/Domain/Services/functions.php
+++ src/Domain/Services/functions.php
@@ -11,6 +11,19 @@ if ( ! function_exists( 'woocommerce_blocks_register_checkout_field' ) && Packag
 	 * @throws Exception If field registration fails.
 	 */
 	function woocommerce_blocks_register_checkout_field( $options ) {
+
+		// Check if `woocommerce_blocks_loaded` ran. If not then the CheckoutFields class will not be available yet.
+		// In that case, re-hook `woocommerce_blocks_loaded` and try running this again.
+		$woocommerce_blocks_loaded_ran = did_action( 'woocommerce_blocks_loaded' );
+		if ( ! $woocommerce_blocks_loaded_ran ) {
+			add_action(
+				'woocommerce_blocks_loaded',
+				function() use ( $options ) {
+					woocommerce_blocks_register_checkout_field( $options );
+				}
+			);
+			return;
+		}
 		$checkout_fields = Package::container()->get( \Automattic\WooCommerce\Blocks\Domain\Services\CheckoutFields::class );
 		$result          = $checkout_fields->register_checkout_field( $options );
 		if ( is_wp_error( $result ) ) {
